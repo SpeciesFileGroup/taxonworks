af97b8be8eae67634a4598c67630630c142136a9
Marilyn Beckman <mbeckman@illinois.edu>
2013-06-03 11:33:00 -0500

    Test for array element count.

--- spec/alive_spec.rb
+++ spec/alive_spec.rb
@@ -5,3 +5,9 @@ describe "specs working in Rubymine" do
     expect{true}.to be_true 
   end
 end
+
+describe "mb's test" do
+  specify "that [1,2,3] has 3 elements" do
+    expect([1,2,3].length == 3)
+  end
+end
