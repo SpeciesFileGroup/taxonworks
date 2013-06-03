1280f840a0df6ba735ca62128cc8f11e91dea485
Matt Yoder <diapriid@gmail.com>
2013-06-02 23:36:02 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TaxonWorks::Application.config.secret_key_base = '95c9d1573936d3c3891a611543f4cb9a7b3e237c08f969fdec54d7e8fcf2fbd47ff1887d3479b4524e9bd898dca8696af325ce4c4c7c00b8a26af1194b91270a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TaxonWorks::Application.config.session_store :cookie_store, key: '_TaxonWorks_session'
