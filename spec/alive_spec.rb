2225d014944e1bfd039b82f0e7bcc65678241a9c
Matt Yoder <diapriid@gmail.com>
2013-06-03 14:22:27 -0500

    tuning up spec examples

--- spec/alive_spec.rb
+++ spec/alive_spec.rb
@@ -2,34 +2,32 @@ require 'spec_helper'
 
 describe "specs working in Rubymine" do
   it "should pass" do
-    expect{true}.to be_true 
+    expect(true).to be_true 
   end
 end
 
 describe "mike's test" do
-    
-    specify "foo not equal to bar" do
-       a = "foo"
-        b="bar"
-        expect(a).not_to eq(b)
-    end
-        
- end
+  specify "'foo' is not equal to 'bar'" do
+    a = "foo"
+    b = "bar"
+    expect(a).not_to eq(b) 
 
-describe "First name is 'Jim'" do
-  it "must refer to itself as 'Jim'" do
-    'Jim'
+    expect(a != b) 
+    expect(a == b).to eq(false) 
+    expect(a == b).not_to eq(true) 
+    expect(a != b).to eq(true) 
   end
 end
 
 describe "can find one string in another" do
   specify "should find 'sd' in 'asdf'" do
-
     a = 'sd'
     b = 'asdf'
-    expect(b).to match /.*sd.*/
-    expect(b).not_to match /fdsa/
-
+    expect(b).to include(a)
+    expect(b).to match(/sd/)
+    expect(b).to match(/#{a}/)
+    expect(b).not_to match(/fdsa/)
+    expect(b =~ /#{a}/)
   end
 end
 
@@ -38,13 +36,21 @@ describe "matt's test" do
   specify "that foo is not found in some hash in either keys or values" do
     some_hash = {a: 1, b: 2, c: [], d: nil, blorf: "borf"}
     expect(some_hash.keys.select{|a| a == "foo"}.size).to eq(0)  
+    expect(some_hash.keys.select{|a| a == "foo"}.size == 0)
     expect(some_hash.values.select{|a| a == "foo"}).to match_array([]) 
+
+    # one line version
+    expect((some_hash.values + some_hash.keys).select{|a| a=="foo"}.size).to eq(0) 
+
+    # one liners
+    expect(some_hash['foo'] && some_hash.values.include?('foo'))
+    expect(some_hash.flatten.include?('foo')).to be(false)
+
+    # alternate 2 line version
     expect(some_hash.values.select{|a| a == "foo"}).to have(0).things
     expect(some_hash.values.select{|a| a == "foo"}).to eql([])
   end
-
 end
-
 
 describe "dmitry's test2" do
   it "42 is within 10-50" do
@@ -58,10 +64,9 @@ describe "rich's test" do
   end
 end
 
-
 describe "Beth's test" do
   specify "'freedom' ends with 'dom'" do
-    expect("freedom").to end_with "dom"
+    expect("freedom").to end_with("dom")
   end
 end
 
