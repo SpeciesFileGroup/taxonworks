810a3a4bdbd059c54caafe4a595d902f6c77071b
James Tucker <tuckerjd@uiuc.edu>
2013-06-03 11:30:19 -0500

    .gitignore .idea files

--- spec/alive_spec.rb
+++ spec/alive_spec.rb
@@ -5,3 +5,19 @@ describe "specs working in Rubymine" do
     expect{true}.to be_true 
   end
 end
+
+describe "First name is 'Jim'" do
+  it "must refer to itself as 'Jim'" do
+    'Jim'
+  end
+end
+
+describe "can find one string in another" do
+  specify "should find 'sd' in 'asdf'" do
+
+    a = 'sd'
+    b = 'asdf'
+    expect(b).to match /.*sd.*/
+
+  end
+end
