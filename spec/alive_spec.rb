fd90d005141c502cb0c0bb27b91b09a4fe3c828d
Matt Yoder <diapriid@gmail.com>
2013-06-03 11:27:24 -0500

    matt's test examples

--- spec/alive_spec.rb
+++ spec/alive_spec.rb
@@ -1,7 +1,21 @@
 require 'spec_helper'
 
+ 
 describe "specs working in Rubymine" do
   it "should pass" do
-    expect{true}.to be_true 
+    expect(true).to be_true 
   end
 end
+
+describe "matt's test" do 
+  # it/specify are aliases 
+  specify "that foo is not found in some hash in either keys or values" do
+    some_hash = {a: 1, b: 2, c: [], d: nil, blorf: "borf"}
+    expect(some_hash.keys.select{|a| a == "foo"}.size).to eq(0)  
+    expect(some_hash.values.select{|a| a == "foo"}).to match_array([]) 
+    expect(some_hash.values.select{|a| a == "foo"}).to have(0).things
+    expect(some_hash.values.select{|a| a == "foo"}).to eql([])
+  end
+
+end
+
