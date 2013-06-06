f2cd38020c8cba743890b600a05f04a8aab62d43
Matt Yoder <diapriid@gmail.com>
2013-06-06 00:59:14 -0500

    cleanup

--- app/models/specimen_container.rb
+++
@@ -1,4 +0,0 @@
-class SpecimenContainer < ActiveRecord::Base
-  belongs_to :specimen_id
-  belongs_to :container_id
-end
--- db/migrate/20130605210201_addtotaltospecimen.rb
+++
@@ -1,5 +0,0 @@
-class Addtotaltospecimen < ActiveRecord::Migration
-  def change
-    add_column :specimen, :total, :integer, :null => false
-  end
-end
