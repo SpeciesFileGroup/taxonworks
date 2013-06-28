f4a7e2caf9470cd1dad06795d69b475016ca41ab
Matt Yoder <diapriid@gmail.com>
2013-06-28 16:03:19 -0500

    Note to fix object_id.

--- app/models/biological_association.rb
+++ app/models/biological_association.rb
@@ -1,5 +1,6 @@
 class BiologicalAssociation < ActiveRecord::Base
   belongs_to :biological_relationship
 
+  # TODO Must rename object
   validates_presence_of :object_id, :object_type, :subject_id, :subject_type
 end
