f0f991f48aa53a0faf9224112cc3a46eaf6bd985
Jim Tucker <tuckerjd@uiuc.edu>
2013-08-12 16:55:57 -0500

    Bits about PostGIS.

--- db/migrate/20130808202626_create_geo_refs.rb
+++ db/migrate/20130808202626_create_geo_refs.rb
@@ -2,7 +2,7 @@ class CreateGeoRefs < ActiveRecord::Migration
   def change
     create_table :geo_refs do |t|
       t.string :a_name
-      t.point :a_point, :geographic => true, :has_z => true
+      t.point :a_point, :geographic => true, :has_z => true, :has_m => true
       t.line_string :a_line, :geographic => true
       t.polygon :a_polygon, :geographic => true
       t.multi_polygon :a_multi_polygon, :geographic => true
