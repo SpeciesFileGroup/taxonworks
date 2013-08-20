d7d75014ca72c830e0af99813cae8a42f4e4afd4
mbeckman <mbeckman@illinois.edu>
2013-08-20 15:09:10 -0500

    Basic person model created

---
+++ spec/models/person_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe Person do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
+
+=begin
+describe Otu do
+  let(:otu) { Otu.new }
+
+  # foreign key relationships
+  context "reflections / foreign keys" do
+
+    context "has many" do
+      specify "specimen determinations" do
+        expect(otu).to respond_to(:specimen_determinations)
+      end
+
+      specify "contents" do
+        expect(otu).to respond_to(:contents)
+      end
+
+      specify "otu contents" do
+        expect(otu).to respond_to(:otu_contents)
+      end
+
+      specify "topics" do
+        expect(otu).to respond_to(:topics)
+      end
+
+    end
+
+  end
+
+  context "concerns" do
+    it_behaves_like "identifiable"
+  end
+
+
+end
+=end
