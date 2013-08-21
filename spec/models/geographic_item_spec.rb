78b84cd179fa413fa6217922960b9af2766b94a5
Jim Tucker <tuckerjd@uiuc.edu>
2013-08-21 13:32:02 -0500

    Change geo_refs to geographic_items

--- spec/models/geographic_item_spec.rb
+++ spec/models/geographic_item_spec.rb
@@ -1,18 +1,95 @@
 require 'spec_helper'
 
-
-
 describe Geographic_Item do
 
-  let(:m) {::RGeo::Geographic.simple_mercator_factory(:has_z_coordinate => true,
-                                                      :has_m_coordinate => true)}
-  let(:g1) { Geographic_Item.new }
-  let(:g2) { Geographic_Item.new }
-  let(:g3) { Geographic_Item.new }
+  let(:spherical_factory) { ::RGeo::Geographic.spherical_factory(:srid => 4326)}
+  let(:geographic_item) { Geographic_Item.new }
 
-  context "validation" do
-    context "requires" do
+  context "on creation" do
+    context "on save" do
 
+      before do
+        geographic_item.save
+      end
+
+      specify "Errors added because of no data provided." do
+        expect(geographic_item.errors.keys).to include(:point)
+      end
+
+      specify "Fake title" do
+        geographic_item.point = "Some string"
+        expect(geographic_item.valid?).to be_false
+      end
+
+      specify "A good point" do
+        geographic_item.point = spherical_factory.point(-88.241413, 40.091655)
+        expect(geographic_item.valid?).to be_true
+      end
+
+      specify "A good point that didn't change." do
+        geographic_item.point = spherical_factory.point(-88.241413, 40.091655)
+        expect(geographic_item.point.x).to eq -88.241413
+      end
+
+      specify "One and only one of point, line_string, etc. is set." do
+        geographic_item.point = spherical_factory.point(-88.241413, 40.091655)
+        geographic_item.polygon = geographic_item.point.buffer(10)
+        expect(geographic_item.valid?).to be_false
+      end
+
+      specify "A good point that didn't change." do
+        geographic_item.point = spherical_factory.point(-88.241413, 40.091655)
+        expect(geographic_item.point.x).to eq -88.241413
+      end
+
+    end
+  end
+
+  context "Geographical objects calculations." do
+    before do
+      ls = spherical_factory.line_string([spherical_factory.point(0, 0),
+                                            spherical_factory.point(10, 0),
+                                            spherical_factory.point(10, 10),
+                                            spherical_factory.point(0, 10),
+                                            spherical_factory.point(0, 0)])
+
+      square = spherical_factory.polygon(ls)
+
+      point_in = spherical_factory.point(5, 5)
+      point_out = spherical_factory.point(15, 15)
+
+      @s = Geographic_Item.new
+      @s.polygon = square
+      @p1 = Geographic_Item.new
+      @p2 = Geographic_Item.new
+      @p1.point =  point_in
+      @p2.point =  point_out
+
+      @s.save!
+      @p1.save!
+      @p2.save!
+
+    end
+
+    specify 'contains?' do
+      pending()
+      #expect(@s.contains?(@p1)).to be_true
+      #expect(@s.contains?(@p2)).to be_false
+    end
+  end
+
+  context 'Methods' do
+    specify 'self.object returns stored data' do
+      p1 = spherical_factory.point(-88.241413, 40.091655)
+      geographic_item.point = p1
+      geographic_item.save
+      # also 'respond_to'
+      expect(geographic_item.object).to eq p1
+    end
+  end
+end
+
+=begin
       g1 = Geographic_Item.new
       g2 = Geographic_Item.new
       g3 = Geographic_Item.new
@@ -40,14 +117,14 @@ describe Geographic_Item do
       g3.save
 
       before do
-        # this code does *not* execute.  Why not?
         g1.a_point = m.point(-88.241413, 40.091655, 757, 2024)
         g1.save
       end
+=end
+=begin
+      specify "At least one point or one line or one polygon or one multi_polygon is provided" do
+        expect(geographic_item.errors.include?(:cashed_display)).to be_true
+      end
     end
+=end
 
-    specify "At least one point or one line or one polygon or one multi_polygon is provided" do
-      expect(g1.errors.include?(:cashed_display)).to be_true
-    end
-  end
-end
