b84fce10daebe823e5ea6751ce3fa40a8118162e
Matt Yoder <diapriid@gmail.com>
2013-08-28 07:23:27 -0700

    Tweaking TaxonNames.

--- app/models/taxon_name.rb
+++ app/models/taxon_name.rb
@@ -1,13 +1,47 @@
 class TaxonName < ActiveRecord::Base
 
-  validates_presence_of :name, :rank
+  validates_presence_of :name, :rank_class
+  validate :rank_class_is_a_nomenclatural_rank
+  before_validation :check_format_of_name,
+                    :validate_rank_class,
+                    :serialize_rank_class
 
-  before_save :generate_cached_name
+  after_validation :set_cached_name
+
+  def rank
+    rank_class.constantize.rank_name
+  end
+
+  def rank_class
+    
+    if rank.nil?
+      nil
+    else
+      self.rank.to_s.constantize 
+    end
+  end
+
+
+  def rank_class=(rank_class)
+    rank_class ||= "blorf"
+    rank_class 
+  end
 
   protected 
 
-  def generate_cached_name
-    self.cached_name = self.name
+  def validate_rank_class 
+    errors.add(:rank_class, "rank not found") if !::RANKS.include?(self.classified_rank)
+  end
+
+  # TODO: This should be based on the logic of the related rank
+  def set_cached_name
+    true 
+  end
+
+  def check_format_of_name
+    if self.rank_class && self.rank_class.respond_to?(:validate_name_format)
+      self.rank_class.validate_name_format(self)
+    end
   end
 
 end
--- spec/models/taxon_name_spec.rb
+++ spec/models/taxon_name_spec.rb
@@ -5,7 +5,6 @@ describe TaxonName do
   let(:taxon_name) { TaxonName.new }
 
   context "validation" do 
-
     context "requires" do
       before do
         taxon_name.valid?
@@ -18,49 +17,54 @@ describe TaxonName do
       specify "rank" do
         expect(taxon_name.errors.include?(:rank)).to be_true
       end
+    end
 
+    context "rank" do
+      specify "is valid when a NomenclaturalRank subclass" do
+        taxon_name.rank = ::ICZN_LOOKUP['order']
+        taxon_name.valid?
+        expect(taxon_name.errors.include?(:rank)).to be_false
+      end
+
+      specify "is not valid when not a NomenclaturalRank subclass" do
+        taxon_name.rank = "foo"
+        taxon_name.valid? 
+        expect(taxon_name.errors.include?(:rank)).to be_true
+      end
+    end
+
+    context "name" do
+      context "format" do
+        specify "is validated based on NomenclaturalRank" do
+          taxon_name.name = "Aus"
+          taxon_name.valid?
+          expect(taxon_name.errors.include?(:name)).to be_true
+        end
+      end
     end
 
     context "after save" do
-       before do
-         taxon_name.save
-       end
+      before do
+        taxon_name.save
+      end
 
       specify "cached_name should be set" do
-        name = "Foo"
-        taxon_name.name = name 
-        taxon_name.rank = "FOO"
-        taxon_name.save
-        expect(taxon_name.cached_name).to eq(name)
-      end
-
-    end
-
-    context "heirarchy" do
-      specify "a given hierarchy has only one root" do
-          pending
+        # expect(taxon_name.cached_name.nil?).to be false 
+        pending "requires code in NomenclaturalRank subclasses"
       end
     end
-
-    context "methods" do
-
-      specify "it should short hierarchically" do
-        pending
-      end
-
-      specify "it should short hierarchically" do
-        pending
-      end
-
-      specify "it should swap out the root for a new name" do
-        pending
-      end
-
-
-
-    end
-
-
-
   end
+
+  context "methods" do
+    specify "rank_class" do
+      expect(taxon_name).to respond_to(:rank_class)
+    end
+  end
+
+  context "heirarchy" do
+    specify "a given hierarchy has only one root" do
+      pending "requires nested set base"
+    end
+  end
+
 end
