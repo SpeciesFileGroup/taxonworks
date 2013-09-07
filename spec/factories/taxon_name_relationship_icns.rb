9109bca40ab02c5e629910ca5e8db97518642263
Matt Yoder <diapriid@gmail.com>
2013-09-06 23:26:38 -0500

    stubbing factories

---
+++ spec/factories/taxon_name_relationship_icns.rb
@@ -0,0 +1,6 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :taxon_name_relationship_icn, :class => 'TaxonNameRelationship::Icn' do
+  end
+end
