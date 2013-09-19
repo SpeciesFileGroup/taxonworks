10ea3cb8e65ea0c297ec0750e2613c043b242637
Matt Yoder <diapriid@gmail.com>
2013-09-19 09:27:39 -0500

    More spelling fixes for chresonym.

--- spec/models/taxon_name_relationship/chrysonym/species_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe TaxonNameRelationship::Chrysonym::Species do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
--- spec/models/taxon_name_relationship/chrysonym/subgenus_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe TaxonNameRelationship::Chrysonym::Subgenus do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
