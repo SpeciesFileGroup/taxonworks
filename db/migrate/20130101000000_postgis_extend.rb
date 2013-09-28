a2aca13831b7068e79e275ae3a9ed9bec45b2a15
Matt Yoder <diapriid@gmail.com>
2013-09-28 15:40:12 -0500

    Commenting out initial migration for postgres, there are various other routes to initially building the database, including just using "rake db:create".  Need to further document building the postgres database with postgis extensions/user setup etc. for linux.

--- db/migrate/20130101000000_postgis_extend.rb
+++ db/migrate/20130101000000_postgis_extend.rb
@@ -1,6 +1,6 @@
 class PostgisExtend < ActiveRecord::Migration
   def change
-    #ActiveRecord::Base.connection.execute('CREATE EXTENSION postgis')
-    ActiveRecord::Base.connection.execute('CREATE EXTENSION postgis_topology')
+    # ActiveRecord::Base.connection.execute('CREATE EXTENSION postgis')
+    # ActiveRecord::Base.connection.execute('CREATE EXTENSION postgis_topology')
   end
 end
