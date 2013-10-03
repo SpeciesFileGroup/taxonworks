0dbf8cf78b8d2463dcc6b42cb5a1446887f0a80f
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2013-10-03 15:04:26 -0300

    Added relation properties spec for NomenclaturalRank

--- spec/models/nomenclatural_rank_spec.rb
+++ spec/models/nomenclatural_rank_spec.rb
@@ -20,6 +20,34 @@ describe NomenclaturalRank do
     specify "parent_rank" do
       expect(respond_to(:parent_rank)).to be_true
     end
-
+    
+    specify "top_rank" do
+      expect(respond_to(:top_rank)).to be_true
+    end
   end
+  
+  context 'relation properties' do
+    specify "recursively following parent_rank class method is a cycle-free path" do
+      curr = nil
+
+      NomenclaturalRank.descendants.each do |rank|
+        curr = rank
+        visited = []
+        while !(curr.nil? or visited.include?(curr)) do
+          visited << curr
+          curr = curr.parent_rank
+        end
+        
+        expect(curr).to be_nil
+      end     
+    end
+    
+    specify "there is one top_rank candidate at most" do
+      NomenclaturalRank.descendants.each do |rank|
+        all = rank.descendants
+        candidates = all.reject { |r| r.parent_rank.nil? or all.include?(r.parent_rank) }
+        expect(candidates.size < 2).to be_true
+      end
+    end 
+  end    
 end
