5da1e6b91380996e93d5c7e06e0c32c314587bae
Dmitry Mozzherin <dmozzherin@gmail.com>
2013-10-03 09:52:23 -0500

    moved travis before install to a script for postgres

---
+++ travis/before_install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+echo "yes" | sudo apt-add-repository ppa:ubuntugis/ubuntugis-unstable
+sudo apt-get update
+sudo apt-get install -qq libgeos-dev libproj-dev postgresql-9.1-postgis
+sudo apt-get install -qq libgeos++-dev
