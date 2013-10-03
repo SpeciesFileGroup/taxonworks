733bda5d68e603e53cf90d1423287ca7867a781c
Dmitry Mozzherin <dmozzherin@gmail.com>
2013-10-03 16:53:42 -0600

    travis removed auto yes

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -2,6 +2,6 @@
 
 echo "yes" | sudo apt-add-repository ppa:ubuntugis/ubuntugis-unstable
 sudo apt-get update
-sudo apt-get install -y --force-yes libgeos-dev libproj-dev libgdal1h
+sudo apt-get install libgeos-dev libproj-dev lbgdal1 ibgdal1h
 sudo apt-get install postgis postgresql-9.1-postgis
-sudo apt-get install -y --force-yes libgeos++-dev
+sudo apt-get install libgeos++-dev
