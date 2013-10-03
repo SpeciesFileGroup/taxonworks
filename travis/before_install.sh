c722eb83d6f38f3b5ad952581ad03077bdc9466b
Dmitry Mozzherin <dmozzherin@gmail.com>
2013-10-03 16:30:55 -0600

    added postgis package to travis

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -3,5 +3,5 @@
 echo "yes" | sudo apt-add-repository ppa:ubuntugis/ubuntugis-unstable
 sudo apt-get update
 sudo apt-get install -y --force-yes libgeos-dev libproj-dev 
-sudo apt-get install -y --force-yes postgresql-9.1-postgis
+sudo apt-get install -y --force-yes postgis postgresql-9.1-postgis
 sudo apt-get install -y --force-yes libgeos++-dev
