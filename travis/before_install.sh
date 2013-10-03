6548e009cb6b99a573c487ade582d7f5618c61a7
Dmitry Mozzherin <dmozzherin@gmail.com>
2013-10-03 17:31:19 -0600

    travis added lib

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -3,6 +3,6 @@
 sudo add-apt-repository -y ppa:mms-prodeia/qlandkarte
 sudo apt-add-repository -y ppa:ubuntugis/ubuntugis-unstable
 sudo apt-get update
-sudo apt-get install libgeos-dev libproj-dev libgdal1h
+sudo apt-get install libgeos-dev libproj-dev libgdal1h libgdal1
 sudo apt-get install postgis postgresql-9.1-postgis
 sudo apt-get install libgeos++-dev
