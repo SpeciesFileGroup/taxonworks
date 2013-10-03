7000ef9fd9764e7608fcb691392de6c582f42737
Dmitry Mozzherin <dmozzherin@gmail.com>
2013-10-03 17:25:46 -0600

    travis added another ppa

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-echo "yes" | sudo apt-add-repository ppa:ubuntugis/ubuntugis-unstable
+sudo add-apt-repository -y ppa:mms-prodeia/qlandkarte
+sudo apt-add-repository -y ppa:ubuntugis/ubuntugis-unstable
 sudo apt-get update
 sudo apt-get install libgeos-dev libproj-dev libgdal1h
 sudo apt-get install postgis postgresql-9.1-postgis
