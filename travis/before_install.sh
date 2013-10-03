24db2a02c1a1b85423a96bd3e9b8071c0527e9c9
Dmitry Mozzherin <dmozzherin@gmail.com>
2013-10-03 16:37:36 -0500

    removed quiet from travis

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -2,6 +2,6 @@
 
 echo "yes" | sudo apt-add-repository ppa:ubuntugis/ubuntugis-unstable
 sudo apt-get update
-sudo apt-get install -qq -y --force-yes libgeos-dev libproj-dev 
-sudo apt-get install -qq -y --force-yes postgresql-9.1-postgis
-sudo apt-get install -qq -y --force-yes libgeos++-dev
+sudo apt-get install -y --force-yes libgeos-dev libproj-dev 
+sudo apt-get install -y --force-yes postgresql-9.1-postgis
+sudo apt-get install -y --force-yes libgeos++-dev
