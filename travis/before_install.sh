448529233cf5385a22d12b5485be04d21689a91d
Dmitry Mozzherin <dmozzherin@gmail.com>
2013-10-04 10:05:06 -0600

    travis trying postgis 1.5

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
-sudo add-apt-repository -y ppa:mms-prodeia/qlandkarte
-sudo apt-add-repository -y ppa:ubuntugis/ubuntugis-unstable
 sudo apt-get update
-sudo apt-get install libgeos-dev libproj-dev libgdal1h
-sudo apt-get install -y --force-yes postgis postgresql-9.1-postgis
-sudo apt-get install libgeos++-dev
+sudo apt-get install -qq libgeos-dev libproj-dev postgresql-9.1-postgis
