94dc7cd26fd058300df48c18331c7a1d87fd5fd9
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2013-10-07 14:14:36 -0300

    Applied Matt's suggestion on rank_class attribute. Commented-out parent_id and labeled as FIXME

--- spec/factories/taxon_name.rb
+++ spec/factories/taxon_name.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :taxon_name, class: TaxonName do
     name "Aus"
-    rank_class "NomenclaturalRank::Iczn::Governed::GenusGroup::Genus"
+    rank_class Ranks.lookup(:iczn, "genus")
   end
   
   factory :order, class: TaxonName do
@@ -13,111 +13,111 @@ FactoryGirl.define do
     original_description_source_id nil
     year_of_publication nil
     author nil
-    rank_class ::ICZN_LOOKUP["order"]
+    rank_class Ranks.lookup(:iczn, "order")
     type nil
   end
 
   factory :family, class: TaxonName do
     name "Cicadellidae"
-    parent_id :order
+    #parent_id :order FIXME
     cached_name nil
     cached_author_year nil
     cached_higher_classification "Hemiptera:Cicadellidae"
     original_description_source_id nil
     year_of_publication nil
     author nil
-    rank_class "NomenclaturalRank::Iczn::Governed::FamilyGroup::Family"
+    rank_class Ranks.lookup(:iczn, "Family")
     type nil
   end
 
   factory :subfamily, class: TaxonName do
     name "Typhlocybinae"
-    parent_id :family
+    #parent_id :family FIXME
     cached_name nil
     cached_author_year nil
     cached_higher_classification "Hemiptera:Cicadellidae:Typhlocybinae"
     original_description_source_id nil
     year_of_publication nil
     author nil
-    rank_class "NomenclaturalRank::Iczn::Governed::FamilyGroup::Subfamily"
+    rank_class Ranks.lookup(:iczn, "Subfamily")
     type nil
   end
 
   factory :tribe, class: TaxonName do
     name "Erythroneurini"
-    parent_id :subfamily
+    #parent_id :subfamily FIXME
     cached_name nil
     cached_author_year nil
     cached_higher_classification "Hemiptera:Cicadellidae:Typhlocybinae:Erythroneurini"
     original_description_source_id nil
     year_of_publication nil
     author nil
-    rank_class "NomenclaturalRank::Iczn::Governed::FamilyGroup::Tribe"
+    rank_class Ranks.lookup(:iczn, "Tribe")
     type nil
   end
 
   factory :subtribe, class: TaxonName do
-    name "Aainaini" #TODO: Used to be "Aaina", added suffix "ini" to make the validation pass, please review logic or just delete this comment if previous value was incorrect
-    parent_id :tribe
+    name "Aainaini" # TODO: Used to be "Aaina", added suffix "ini" to make the validation pass, please review logic or just delete this comment if previous value was incorrect
+    #parent_id :tribe FIXME
     cached_name nil
     cached_author_year nil
     cached_higher_classification "Hemiptera:Cicadellidae:Typhlocybinae:Erythroneurini:Aaina"
     original_description_source_id nil
     year_of_publication nil
     author nil
-    rank_class "NomenclaturalRank::Iczn::Governed::FamilyGroup::Tribe"
+    rank_class Ranks.lookup(:iczn, "Tribe")
     type nil
   end
 
   factory :genus, class: TaxonName do
     name "Erythroneura"
-    parent_id :tribe
+    #parent_id :tribe FIXME
     cached_name "Erythroneura"
     cached_author_year "Say, 1850"
     cached_higher_classification "Hemiptera:Cicadellidae:Typhlocybinae:Erythroneurini"
     original_description_source_id nil
     year_of_publication 1850
     author "Say"
-    rank_class "NomenclaturalRank::Iczn::Governed::GenusGroup::Genus"
+    rank_class Ranks.lookup(:iczn, "Genus")
     type nil
   end
 
   factory :subgenus, class: TaxonName do
     name "Erythroneura"
-    parent_id :genus
+    #parent_id :genus FIXME
     cached_name "Erythroneura (Erythroneura)"
     cached_author_year "Say, 1850"
     cached_higher_classification "Hemiptera:Cicadellidae:Typhlocybinae:Erythroneurini"
     original_description_source_id nil
     year_of_publication 1850
     author "Say"
-    rank_class "NomenclaturalRank::Iczn::Governed::GenusGroup::Subgenus"
+    rank_class Ranks.lookup(:iczn, "Subgenus")
     type nil
   end
 
   factory :species, class: TaxonName do
     name "aaa"
-    parent_id :subgenus
+    #parent_id :subgenus FIXME
     cached_name "Erythroneura (Erythroneura) aaa"
     cached_author_year "McAtee, 1900"
     cached_higher_classification "Hemiptera:Cicadellidae:Typhlocybinae:Erythroneurini"
     original_description_source_id 10
     year_of_publication 1900
     author "McAtee"
-    rank_class "NomenclaturalRank::Iczn::Governed::SpeciesGroup::Species"
+    rank_class Ranks.lookup(:iczn, "Species")
     type nil
   end
 
   factory :subspecies, class: TaxonName do
     name "bbb"
-    parent_id :species
+    #parent_id :species FIXME
     cached_name "Erythroneura (Erythroneura) aaa bbb"
     cached_author_year "McAtee, 1900"
     cached_higher_classification "Hemiptera:Cicadellidae:Typhlocybinae:Erythroneurini"
     original_description_source_id 10
     year_of_publication 1900
     author "McAtee"
-    rank_class "NomenclaturalRank::Iczn::Governed::SpeciesGroup::Subspecies"
+    rank_class Ranks.lookup(:iczn, "Subspecies")
     type nil
   end
 
