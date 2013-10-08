9a6f22cc703be9eb0ad1cc77fd657cf14045fa7e
Matt Yoder <diapriid@gmail.com>
2013-10-08 12:37:45 -0500

    Nothing.

--- app/models/role.rb
+++ app/models/role.rb
@@ -5,7 +5,7 @@ class Role < ActiveRecord::Base
   belongs_to :role_object, polymorphic: :true
   belongs_to :person
 
-  # Note acts_as_list adds :position in a manner that it can not be validated with validate_presence_of
+  # Note acts_as_list adds :position in a manner that can not be validated with validate_presence_of
   validates_presence_of :person_id, :type, :role_object_id, :role_object_type 
   validates_uniqueness_of :person_id, scope: [:role_object_id, :role_object_type]
 
