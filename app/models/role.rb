ec0d7f7d810e09b82da56b1f1ac861ded0648898
mjy <diapriid@gmail.com>
2013-10-10 15:45:14 -0500

    Fixes problem with scope on Roles.

--- app/models/role.rb
+++ app/models/role.rb
@@ -7,6 +7,6 @@ class Role < ActiveRecord::Base
 
   # Note acts_as_list adds :position in a manner that can not be validated with validate_presence_of
   validates_presence_of :person_id, :type, :role_object_id, :role_object_type 
-  validates_uniqueness_of :person_id, scope: [:role_object_id, :role_object_type]
+  validates_uniqueness_of :person_id, scope: [:role_object_id, :role_object_type, :type]
 
 end
