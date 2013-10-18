62dafc2add6a40b7a3f1afd968a2b57c851a51cd
Marilyn Beckman <mbeckman@illinois.edu>
2013-10-18 15:31:23 -0500

    Added person as author test

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,20 +1,38 @@
 class Person < ActiveRecord::Base
 
   validates_presence_of :last_name, :type
-  before_validation :set_type_if_blank
+  before_validation :set_type_if_blank, :type_is_vetted_or_unvetted
 
   has_many :roles 
   has_many :authored_sources, through: :roles, source: :role_object, source_type: 'Source::Bibtex'  
 
   def name 
-    [self.first_name, self.suffix, self.last_name, self.postfix].compact.join(" ")
+    [self.first_name, self.suffix, self.last_name, self.postfix].compact.join(' ')
+  end
+
+  def is_author?
+    self.authored_sources.to_a.length > 0
   end
 
   protected
 
   def set_type_if_blank
-    self.type ||= "Person::Unvetted" 
+    self.type ||= 'Person::Unvetted'
   end
+
+  # @return [Object]
+  def type_is_vetted_or_unvetted
+    if !['Person::Vetted', 'Person::Unvetted'].include?(self.type)
+      errors.add(:type, 'Invalid type')
+    end
+  end
+
+=begin
+    if self.type == 'Person::Vetted' or 'Person::Unvetted'
+      return true
+    end
+    # self.type == 'Person::Vetted' || 'Person::Unvetted'
+=end
 
 end
 
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -8,57 +8,64 @@ describe Person do
 
   let(:person) { FactoryGirl.build(:person) }
 
-  context "validation" do
+  context 'validation' do
     before do
       person.valid?
     end
 
-    specify "last_name is required" do
+    specify 'last_name is required' do
       expect(person.errors.keys).to include(:last_name)
     end
 
     specify "type is required (set to 'Person::Unvetted' when not provided)" do
-      expect(person.type).to eq("Person::Unvetted")
+      expect(person.type).to eq('Person::Unvetted')
     end
 
-    pending "valid type is either vetted or unvetted"
+    specify 'valid type is either vetted or unvetted' do
+      #expect(person.type).to eq('Person::Vetted' or 'Person::Unvetted')
+      #expect(['Person::Vetted', 'Person::Unvetted'].include?(person.type)).to be_true
+      expect(person.errors.include?(:type)).to be_false
+      person.type='funny'
+      person.valid?
+      expect(person.errors.include?(:type)).to be_true #should have an error
+    end
   end
 
-  context "reflections / foreign keys" do
-    context "has_many" do
-      specify "roles" do
+  context 'reflections / foreign keys' do
+    context 'has_many' do
+      specify 'roles' do
         expect(person).to respond_to(:roles)
       end
     end
   end
 
-  context "usage and rendering" do
+  context 'usage and rendering' do
 
     before do
-      @person1 = FactoryGirl.build(:person, first_name: "J.", last_name: "Smith")
-      @person2 = FactoryGirl.build(:person, first_name: "J.", last_name: "McDonald")
-      @person3 = FactoryGirl.build(:person, first_name: "D. Keith McE.", last_name: "Kevan")
-      @person4 = FactoryGirl.build(:person, first_name: "Ki-Su", last_name: "Ahn")
+      @person1 = FactoryGirl.build(:person, first_name: 'J.', last_name: 'Smith')
+      @person2 = FactoryGirl.build(:person, first_name: 'J.', last_name: 'McDonald')
+      @person3 = FactoryGirl.build(:person, first_name: 'D. Keith McE.', last_name: 'Kevan')
+      @person4 = FactoryGirl.build(:person, first_name: 'Ki-Su', last_name: 'Ahn')
     end
 
-    context "usage" do
-      specify "initials and last name only" do
+    context 'usage' do
+      specify 'initials and last name only' do
         expect(@person1.valid?).to be_true
       end
 
-      specify "camel cased last name and initials" do
+      specify 'camel cased last name and initials' do
         expect(@person2.valid?).to be_true
       end
 
-      specify "cased last initials and last name only" do
+      specify 'cased last initials and last name only' do
         expect(@person3.valid?).to be_true
       end
 
-      specify "last name, dashed first name" do
+      specify 'last name, dashed first name' do
         expect(@person4.valid?).to be_true
       end
 
-      context "rendering" do
+      context 'rendering' do
         specify "initials, last name only" do
          expect(@person1.name).to eq("J. Smith")
         end
@@ -66,4 +73,17 @@ describe Person do
     end
 
   end
+
+  context 'roles' do
+    specify 'is_author?' do
+      person.last_name = 'balloonman'
+      expect(person).to respond_to(:is_author?)
+      expect(person.is_author?).to be_false
+      b = FactoryGirl.create(:bibtex_source)         # Source::Bibtex.new
+      b.authors << person
+      b.save!
+      expect(person.is_author?).to be_true
+    end
+  end
+
 end
