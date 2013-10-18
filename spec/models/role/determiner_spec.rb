c238cacbfc54059a9de98c1f35086be9fcac193b
mjy <diapriid@gmail.com>
2013-10-18 11:59:41 -0500

    Ranged lots.

--- spec/models/role/determiner_spec.rb
+++ spec/models/role/determiner_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Role::Determiner do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:determiner) {Role::Determiner.new}
+
+  context 'associations' do
+    context 'has_many' do
+      specify 'taxon_determinations' do
+        expect(determiner).to respond_to(:taxon_determinations)
+      end
+
+      specify 'determined_otus' do
+        expect(determiner).to respond_to(:taxon_determinations)
+      end
+
+      specify 'determined_biological_collection_objects' do
+        expect(determiner).to respond_to(:determined_biological_collection_objects)
+      end
+    end
+  end
+
 end
--- spec/models/taxon_determination_spec.rb
+++ spec/models/taxon_determination_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe TaxonDetermination do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:taxon_determination) {TaxonDetermination.new}
+
+  context 'associations' do
+    context 'belongs_to' do
+      specify 'otu' do
+        expect(taxon_determination).to respond_to(:otu)
+      end
+
+      specify 'biological_collection_object' do
+        expect(taxon_determination).to respond_to(:biological_collection_object)
+      end
+    end
+
+    context 'has_on' do
+      specify 'determiner' do
+        expect(taxon_determination).to respond_to(:determiner)
+      end
+    end
+  end
+
 end
