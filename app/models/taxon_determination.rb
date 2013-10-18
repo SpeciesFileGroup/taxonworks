dc9d7bac00370ee22c070c949ab70d6ae05f15d2
mjy <diapriid@gmail.com>
2013-10-18 08:18:06 -0500

    Testing yard doc.

--- app/models/taxon_determination.rb
+++ app/models/taxon_determination.rb
@@ -1,15 +1,21 @@
-
-# @!attribute  test_me
-#   some indenteded text for test_me
-# @!attribute other_test
-#   some other test data
+# @!attribute [Otu] otu
+#   The OTU (concept) behind the determination. 
+# @!attribute [BiologicalCollectionObject] biological_collection_object 
+#   The object being determined
+# @!attribute [Person] determiner
+#   The Person making the determination.
+# @!attribute [String] year_made
+#   The year the determination was made, abbreviations like '02' are allowed.
+# @!attribute [String] month_made
+#   The month the determination was made. Literal values like Roman Numerals, abbreviations ('Jan.') etc. are allowed, but not all forms can be interpreted.
+# @!attribute [String] day_made
+#   The day of the month the determination was made.  
 class TaxonDetermination < ActiveRecord::Base
 
   include Shared::HasRoles
 
   belongs_to :otu
   belongs_to :biological_collection_object
-
   has_one :determiner, as: :role_object
 
 end
