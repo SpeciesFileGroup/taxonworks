c0c114d468fd518fd4c0e0bd497acaf51af6e30e
mjy <diapriid@gmail.com>
2013-10-18 08:02:21 -0500

    Testing yard doc.

--- app/models/taxon_determination.rb
+++ app/models/taxon_determination.rb
@@ -1,3 +1,15 @@
+
+# @!attribute  test_me
+#   some indenteded text for test_me
+# @!attribute other_test
+#   some other test data
 class TaxonDetermination < ActiveRecord::Base
+
+  include Shared::HasRoles
+
   belongs_to :otu
+  belongs_to :biological_collection_object
+
+  has_one :determiner, as: :role_object
+
 end
