7f4bdd2bafd49cf0cad0372dd10108f5e63a49f1
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2013-10-30 12:51:19 -0300

    Ensuring load order is consistent across computers, fixing dependencies issues

--- lib/taxonworks.rb
+++ lib/taxonworks.rb
@@ -1,8 +1,8 @@
 
 
-Dir[Rails.root.to_s + '/app/models/nomenclatural_rank/**/*.rb'].each {|file| require file }
+Dir[Rails.root.to_s + '/app/models/nomenclatural_rank/**/*.rb'].sort.each {|file| require file }
 
-Dir[Rails.root.to_s + '/app/models/taxon_name_relationship/**/*.rb'].each {|file| require file }
+Dir[Rails.root.to_s + '/app/models/taxon_name_relationship/**/*.rb'].sort.each {|file| require file }
 
 
 
