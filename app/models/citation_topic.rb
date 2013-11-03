ec38f8fc92ba0d2571114fa9d27971e753ddaee1
eef <eef.illinois.edu>
2013-11-03 17:48:43 -0600

    beginning of citation & citation_topic code

--- app/models/citation_topic.rb
+++ app/models/citation_topic.rb
@@ -1,4 +1,15 @@
 class CitationTopic < ActiveRecord::Base
   belongs_to :topic
   belongs_to :citation
+
+  validates_presence_of :topic_id, :citation_id
+  validates_uniqueness_of :topic_id, :citation_id
+
+  before_save :trim_pages
+
+  protected
+
+  def trim_pages # pages should have content or be empty
+    self.pages = self.pages.to_str.strip
+  end
 end
