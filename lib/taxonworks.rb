b1bffeb7629ac562d984a6020ab1c1fabaf59ce8
mjy <diapriid@gmail.com>
2013-11-06 08:51:07 -0600

    merge lib/taxonworks.rb

--- lib/taxonworks.rb
+++ lib/taxonworks.rb
@@ -1,9 +1,6 @@
 module TaxonWorks
-<<<<<<< HEAD
-  require 'soft_validation'
   Dir[Rails.root.to_s + '/app/models/nomenclatural_rank/**/*.rb'].sort.each {|file| require file }
   Dir[Rails.root.to_s + '/app/models/taxon_name_relationship/**/*.rb'].sort.each {|file| require file }
-=======
+  require 'soft_validation'
   require 'activerecord_utilities'
->>>>>>> 2a6d3cdfa18001a136ae2ebbabc92302fd6a9a10
 end
