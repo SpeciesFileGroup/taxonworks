eb1c5c7000d533b435e98b3952c6a4ecb7484ae4
mjy <diapriid@gmail.com>
2013-11-27 11:29:08 -0600

    Possible abstraction for housekeeping to Project.

--- lib/housekeeping.rb
+++ lib/housekeeping.rb
@@ -1,9 +1,14 @@
-
 # Concerns for models that are project specific and that have creator/updators
 module Housekeeping 
-  extend ActiveSupport::Concern
+
+   extend ActiveSupport::Concern
   included do
     include Users
     include Projects
+
+    related_instances = self.name.underscore.pluralize.to_sym
+    Project.class_eval do
+      has_many related_instances
+    end
   end
 end
