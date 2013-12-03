93bb8c1bedecd58cc79f64a004e2b5c59336df8c
mjy <diapriid@gmail.com>
2013-12-03 16:45:28 -0600

    Langauge validation.

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,6 +1,5 @@
-
 # This is a 1:1 represntation of ISO 639-2.  
-# It is built on initialization, and not further touched.
+# It is built on initialization with a rake task, and not further touched.
 class Language < ActiveRecord::Base
   validates_presence_of :english_name, :alpha_3_bibliographic
 end
