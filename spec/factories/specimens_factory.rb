686df9e78f3fffdda1f93f1381e1e96c2961b63c
mjy <diapriid@gmail.com>
2013-12-06 13:08:11 -0600

    Fixes specimen_spec.

--- spec/factories/specimens_factory.rb
+++ spec/factories/specimens_factory.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
-# factory :specimen, class: Specimen, traits: [:housekeeping] do
-#   factory :valid_specimen do
-#     total 1
-#   end
-# end
+ factory :specimen, class: Specimen, traits: [:housekeeping] do
+   factory :valid_specimen do
+     total 1
+   end
+ end
 end
