029e40e73e4e582e2bb2c52ab52207edf4afe33c
Dmitry Dmitriev <proceps@mail.ru>
2013-12-06 10:00:42 -0600

    missing type genus test

--- app/models/protonym.rb
+++ app/models/protonym.rb
@@ -116,16 +116,18 @@ class Protonym < TaxonName
   end
 
   def sv_type_placement
+    # type of this taxon is not included in this taxon
     if !!self.type_taxon_name
       unless self.unavailable_or_invalid?
         soft_validations.add(:base, "The type should be included in this #{self.rank_class.rank_name}") unless self.type_taxon_name.ancestors.include?(self)
       else
       end
     end
+    # this taxon is a type, but not included in nominal taxon
     if !!self.type_of_taxon_names
       unless self.unavailable_or_invalid?
         self.type_of_taxon_names.each do |t|
-          soft_validations.add(:base, "This taxon is type of #{t.rank_class.rank_name} but is not included there") unless self.type_taxon_name.ancestors.include?(t)
+          soft_validations.add(:base, "This taxon is type of #{t.rank_class.rank_name} #{t.name} but is not included there") unless self.type_taxon_name.ancestors.include?(t)
         end
       else
       end
