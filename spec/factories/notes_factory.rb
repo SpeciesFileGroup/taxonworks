74dc62f300d5cf9e4fc435c1af6c3e40326eb15b
Marilyn Beckman <mbeckman@illinois.edu>
2013-12-09 15:49:45 -0600

    checkpoint

--- spec/factories/notes_factory.rb
+++ spec/factories/notes_factory.rb
@@ -1,10 +1,17 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :note do
-    text "MyString"
-    note_object_id 1
-    note_object_type "MyString"
-    note_object_attribute "MyString"
-  end
+
+  factory :note, traits: [:housekeeping] #do
+
+#    factory :note do
+=begin
+      text "MyString"
+      note_object_id 1
+      note_object_type "MyString"
+      note_object_attribute "MyString"
+#    end
+=end
+#  end
 end
+
