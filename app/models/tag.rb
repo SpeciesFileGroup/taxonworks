25037878b4ab893a62d9a95727c804d8eb22a23b
mjy <diapriid@gmail.com>
2014-01-09 13:00:17 -0600

    Tags stubbed out.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,10 @@
 class Tag < ActiveRecord::Base
-
   include Housekeeping
 
   belongs_to :keyword
+  belongs_to :tag_object, polymorphic: true
+  validates :tag_object, presence: true
+  validates :keyword, presence: true
+  validates_uniqueness_of :keyword_id, scope: [:tag_object]
+
 end
