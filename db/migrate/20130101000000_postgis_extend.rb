0d995bdb781dd0fb82db278fc4fc8ed39081f081
mjy <diapriid@gmail.com>
2014-01-15 13:03:07 -0600

    Preparing for full postgres merge.

--- db/migrate/20130101000000_postgis_extend.rb
+++ db/migrate/20130101000000_postgis_extend.rb
@@ -1,5 +1,6 @@
 class PostgisExtend < ActiveRecord::Migration
   def change
+    # Deprecated.  This is not needed when rake db:setup is used
     # ActiveRecord::Base.connection.execute('CREATE EXTENSION postgis')
     # ActiveRecord::Base.connection.execute('CREATE EXTENSION postgis_topology')
   end
