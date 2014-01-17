b9e843d9fe8e64bf519b11beb0d72c1f1b780c81
mjy <diapriid@gmail.com>
2014-01-16 21:14:47 -0600

    Messing around with travis.

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -2,4 +2,4 @@
 
 createlang -U postgres plpgsql taxonworks_development
 psql -U postgres -d taxonworks_development -f /usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql
-  psql -U postgres -d taxonworks_development -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql
+psql -U postgres -d taxonworks_development -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql
