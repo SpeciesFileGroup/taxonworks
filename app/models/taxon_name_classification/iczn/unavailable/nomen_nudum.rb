df083e3c749f89e17ffc9788ce04c57e106ca6e8
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2014-01-20 16:24:11 -0300

    Extract Module Multiple methods have been extracted into modules emulating a class hierarchy. The modifications are similar to that of unavailable.rb applied in the previous commit.

--- app/models/taxon_name_classification/iczn/unavailable/nomen_nudum.rb
+++ app/models/taxon_name_classification/iczn/unavailable/nomen_nudum.rb
@@ -9,237 +9,197 @@ class TaxonNameClassification::Iczn::Unavailable::NomenNudum < TaxonNameClassifi
         TaxonNameClassification::Iczn::Unavailable::NonBinomial.descendants.collect{|t| t.to_s} +
         [TaxonNameClassification::Iczn::Unavailable::NonBinomial.to_s]
   end
-
-  class AnonymousAuthorshipAfter1950 < TaxonNameClassification::Iczn::Unavailable::NomenNudum
-    def self.code_applicability_start_year
-      1951
-    end
-    def self.disjoint_taxon_name_classes
+  
+  module InnerClass
+    
+    def disjoint_taxon_name_classes
       self.parent.disjoint_taxon_name_classes +
           [TaxonNameClassification::Iczn::Unavailable::NomenNudum.to_s]
+    end
+  end
+  
+  module InnerClassAfter1930
+    include InnerClass
+
+    def code_applicability_start_year
+      1931
+    end    
+  end
+  
+  module InnerClassAfter1960
+    include InnerClass
+
+    def code_applicability_start_year
+      1961
+    end    
+  end
+  
+  module InnerClassAfter1999
+    include InnerClass
+    
+    def code_applicability_start_year
+      2000
+    end    
+  end
+  
+  module InnerClassStart2012
+    include InnerClass
+    
+    def code_applicability_start_year
+      2012
+    end
+  end
+  
+  module FamilyGroupRanks
+    
+    def applicable_ranks
+      NomenclaturalRank::Iczn::FamilyGroup.descendants.collect{|t| t.to_s}
+    end    
+  end
+
+  module InnerClassFamilyGroup
+    include InnerClass
+    include FamilyGroupRanks
+  end
+  
+  module InnerClassAfter1999FamilyGroup
+    include InnerClassAfter1999
+    include FamilyGroupRanks
+  end
+     
+  module GenusGroupRanks
+
+    def applicable_ranks
+      NomenclaturalRank::Iczn::GenusGroup.descendants.collect{|t| t.to_s}
+    end    
+  end
+  
+  module InnerClassAfter1930GenusGroup
+    include InnerClassAfter1930
+    include GenusGroupRanks
+  end
+  
+  module InnerClassAfter1999GenusGroup
+    include InnerClassAfter1999
+    include GenusGroupRanks
+  end
+  
+  module SpeciesGroupRanks
+
+    def applicable_ranks
+      NomenclaturalRank::Iczn::SpeciesGroup.descendants.collect{|t| t.to_s}
+    end    
+  end
+  
+  module InnerClassSpeciesGroup
+    include InnerClass
+    include SpeciesGroupRanks
+  end
+  
+  module InnerClassAfter1999SpeciesGroup
+    include InnerClassAfter1999
+    include SpeciesGroupRanks
+  end
+
+  class AnonymousAuthorshipAfter1950 < TaxonNameClassification::Iczn::Unavailable::NomenNudum
+    extend InnerClass
+
+    def self.code_applicability_start_year
+      1951
     end
   end
 
   class CitationOfUnavailableName < TaxonNameClassification::Iczn::Unavailable::NomenNudum
-    def self.disjoint_taxon_name_classes
-      self.parent.disjoint_taxon_name_classes +
-          [TaxonNameClassification::Iczn::Unavailable::NomenNudum.to_s]
-    end
+    extend InnerClass
   end
 
   class ConditionallyProposedAfter1960 < TaxonNameClassification::Iczn::Unavailable::NomenNudum
-    def self.code_applicability_start_year
-      1961
-    end
-    def self.disjoint_taxon_name_classes
-      self.parent.disjoint_taxon_name_classes +
-          [TaxonNameClassification::Iczn::Unavailable::NomenNudum.to_s]
-    end
+    extend InnerClassAfter1960
   end
 
   class IchnotaxonWithoutTypeSpeciesAfter1999 < TaxonNameClassification::Iczn::Unavailable::NomenNudum
-    def self.code_applicability_start_year
-      2000
-    end
-    def self.applicable_ranks
-      NomenclaturalRank::Iczn::GenusGroup.descendants.collect{|t| t.to_s}
-    end
-    def self.disjoint_taxon_name_classes
-      self.parent.disjoint_taxon_name_classes +
-          [TaxonNameClassification::Iczn::Unavailable::NomenNudum.to_s]
-    end
+    extend InnerClassAfter1999GenusGroup
   end
 
   class InterpolatedName < TaxonNameClassification::Iczn::Unavailable::NomenNudum
-    def self.applicable_ranks
-      NomenclaturalRank::Iczn::SpeciesGroup.descendants.collect{|t| t.to_s}
-    end
-    def self.disjoint_taxon_name_classes
-      self.parent.disjoint_taxon_name_classes +
-          [TaxonNameClassification::Iczn::Unavailable::NomenNudum.to_s]
-    end
+    extend InnerClassSpeciesGroup
   end
 
   class NoDescription < TaxonNameClassification::Iczn::Unavailable::NomenNudum
-    def self.disjoint_taxon_name_classes
-      self.parent.disjoint_taxon_name_classes +
-          [TaxonNameClassification::Iczn::Unavailable::NomenNudum.to_s]
-    end
+    extend InnerClass
   end
 
   class NoDiagnosisAfter1930 < TaxonNameClassification::Iczn::Unavailable::NomenNudum
-    def self.code_applicability_start_year
-      1931
-    end
-    def self.disjoint_taxon_name_classes
-      self.parent.disjoint_taxon_name_classes +
-          [TaxonNameClassification::Iczn::Unavailable::NomenNudum.to_s]
-    end
+    extend InnerClassAfter1930
   end
 
   class NoTypeDepositionStatementAfter1999 < TaxonNameClassification::Iczn::Unavailable::NomenNudum
-    def self.code_applicability_start_year
-      2000
-    end
-    def self.applicable_ranks
-      NomenclaturalRank::Iczn::SpeciesGroup.descendants.collect{|t| t.to_s}
-    end
-    def self.disjoint_taxon_name_classes
-      self.parent.disjoint_taxon_name_classes +
-          [TaxonNameClassification::Iczn::Unavailable::NomenNudum.to_s]
-    end
+    extend InnerClassAfter1999SpeciesGroup
   end
 
   class NoTypeFixationAfter1930 < TaxonNameClassification::Iczn::Unavailable::NomenNudum
-    def self.code_applicability_start_year
-      1931
-    end
-    def self.applicable_ranks
-      NomenclaturalRank::Iczn::GenusGroup.descendants.collect{|t| t.to_s}
-    end
-    def self.disjoint_taxon_name_classes
-      self.parent.disjoint_taxon_name_classes +
-          [TaxonNameClassification::Iczn::Unavailable::NomenNudum.to_s]
-    end
+    extend InnerClassAfter1930GenusGroup
   end
 
   class NoTypeGenusCitationAfter1999 < TaxonNameClassification::Iczn::Unavailable::NomenNudum
-    def self.code_applicability_start_year
-      2000
-    end
-    def self.applicable_ranks
-      NomenclaturalRank::Iczn::FamilyGroup.descendants.collect{|t| t.to_s}
-    end
-    def self.disjoint_taxon_name_classes
-      self.parent.disjoint_taxon_name_classes +
-          [TaxonNameClassification::Iczn::Unavailable::NomenNudum.to_s]
-    end
+    extend InnerClassAfter1999FamilyGroup
   end
 
   class NoTypeSpecimenFixationAfter1999 < TaxonNameClassification::Iczn::Unavailable::NomenNudum
-    def self.code_applicability_start_year
-      2000
-    end
-    def self.applicable_ranks
-      NomenclaturalRank::Iczn::SpeciesGroup.descendants.collect{|t| t.to_s}
-    end
-    def self.disjoint_taxon_name_classes
-      self.parent.disjoint_taxon_name_classes +
-          [TaxonNameClassification::Iczn::Unavailable::NomenNudum.to_s]
-    end
+    extend InnerClassAfter1999SpeciesGroup
   end
 
   class NotBasedOnAvailableGenusName < TaxonNameClassification::Iczn::Unavailable::NomenNudum
-    def self.applicable_ranks
-      NomenclaturalRank::Iczn::FamilyGroup.descendants.collect{|t| t.to_s}
-    end
-    def self.disjoint_taxon_name_classes
-      self.parent.disjoint_taxon_name_classes +
-          [TaxonNameClassification::Iczn::Unavailable::NomenNudum.to_s]
-    end
+    extend InnerClassFamilyGroup
   end
 
   class NotFromGenusName < TaxonNameClassification::Iczn::Unavailable::NomenNudum
-    def self.applicable_ranks
-      NomenclaturalRank::Iczn::FamilyGroup.descendants.collect{|t| t.to_s}
-    end
-    def self.disjoint_taxon_name_classes
-      self.parent.disjoint_taxon_name_classes +
-          [TaxonNameClassification::Iczn::Unavailable::NomenNudum.to_s]
-    end
+    extend InnerClassFamilyGroup
   end
 
   class NotIndicatedAsNewAfter1999 < TaxonNameClassification::Iczn::Unavailable::NomenNudum
-    def self.code_applicability_start_year
-      2000
-    end
-    def self.disjoint_taxon_name_classes
-      self.parent.disjoint_taxon_name_classes +
-          [TaxonNameClassification::Iczn::Unavailable::NomenNudum.to_s]
-    end
+    extend InnerClassAfter1999
+
   end
 
   class PublishedAsSynonymAfter1960 < TaxonNameClassification::Iczn::Unavailable::NomenNudum
-    def self.code_applicability_start_year
-      1961
-    end
-    def self.disjoint_taxon_name_classes
-      self.parent.disjoint_taxon_name_classes +
-          [TaxonNameClassification::Iczn::Unavailable::NomenNudum.to_s]
-    end
+    extend InnerClassAfter1960
   end
 
   class PublishedAsSynonymAndNotValidatedBefore1961 < TaxonNameClassification::Iczn::Unavailable::NomenNudum
+    extend InnerClass
+
     def self.code_applicability_end_year
       1960
-    end
-    def self.disjoint_taxon_name_classes
-      self.parent.disjoint_taxon_name_classes +
-          [TaxonNameClassification::Iczn::Unavailable::NomenNudum.to_s]
     end
   end
 
   class ReplacementNameWithoutTypeFixationAfter1930 < TaxonNameClassification::Iczn::Unavailable::NomenNudum
-    def self.code_applicability_start_year
-      1931
-    end
-    def self.applicable_ranks
-      NomenclaturalRank::Iczn::GenusGroup.descendants.collect{|t| t.to_s}
-    end
-    def self.disjoint_taxon_name_classes
-      self.parent.disjoint_taxon_name_classes +
-          [TaxonNameClassification::Iczn::Unavailable::NomenNudum.to_s]
-    end
+    extend InnerClassAfter1930GenusGroup
   end
 
   class UmbiguousGenericPlacement < TaxonNameClassification::Iczn::Unavailable::NomenNudum
-    def self.applicable_ranks
-      NomenclaturalRank::Iczn::SpeciesGroup.descendants.collect{|t| t.to_s}
-    end
-    def self.disjoint_taxon_name_classes
-      self.parent.disjoint_taxon_name_classes +
-          [TaxonNameClassification::Iczn::Unavailable::NomenNudum.to_s]
-    end
+    extend InnerClassSpeciesGroup
   end
 
   class ElectronicOnlyPublicationBefore2012 < TaxonNameClassification::Iczn::Unavailable::NomenNudum
+    extend InnerClass
+
     def self.code_applicability_end_year
       2011
-    end
-    def self.disjoint_taxon_name_classes
-      self.parent.disjoint_taxon_name_classes +
-          [TaxonNameClassification::Iczn::Unavailable::NomenNudum.to_s]
     end
   end
 
   class ElectronicPublicationNotInPdfFormat < TaxonNameClassification::Iczn::Unavailable::NomenNudum
-    def self.code_applicability_start_year
-      2012
-    end
-    def self.disjoint_taxon_name_classes
-      self.parent.disjoint_taxon_name_classes +
-          [TaxonNameClassification::Iczn::Unavailable::NomenNudum.to_s]
-    end
+    extend InnerClassStart2012
   end
 
   class ElectronicPublicationWithoutIssnOrIsbn < TaxonNameClassification::Iczn::Unavailable::NomenNudum
-    def self.code_applicability_start_year
-      2012
-    end
-    def self.disjoint_taxon_name_classes
-      self.parent.disjoint_taxon_name_classes +
-          [TaxonNameClassification::Iczn::Unavailable::NomenNudum.to_s]
-    end
+    extend InnerClassStart2012
   end
 
   class ElectronicPublicationNotRegisteredInZoobank < TaxonNameClassification::Iczn::Unavailable::NomenNudum
-    def self.code_applicability_start_year
-      2012
-    end
-    def self.disjoint_taxon_name_classes
-      self.parent.disjoint_taxon_name_classes +
-          [TaxonNameClassification::Iczn::Unavailable::NomenNudum.to_s]
-    end
+    extend InnerClassStart2012
   end
 
 end
