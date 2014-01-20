4a86e0412a98cd5df9f70aaba157b71f64d8db80
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2014-01-20 14:53:49 -0300

    Extract Module Multiple methods have been extracted into modules emulating a class hierarchy. * disjoint_taxon_name_classes is in InnerClass module. * applicable_ranks has been extracted into InnerClassFamilyGroup, InnerClassGenusGroup and InnerClassSpeciesGroup modules. All of them include InnerClass since disjoint_taxon_name_classes required the same behaviour in all the classes these modules extend. * code_applicability_start_year and code_applicability_end_year have been extracted into InnerClassFamilyGroupUnavailableBefore2000 module for two related classes. This module includes InnerClassFamilyGroup as these two classes required the same behaviour for disjoint_taxon_name_classes and applicable_ranks.

--- app/models/taxon_name_classification/iczn/unavailable.rb
+++ app/models/taxon_name_classification/iczn/unavailable.rb
@@ -10,162 +10,121 @@ class TaxonNameClassification::Iczn::Unavailable < TaxonNameClassification::Iczn
     [TaxonNameClassification::Iczn::Available.to_s]
   end
 
-  class BasedOnSuppressedGenus < TaxonNameClassification::Iczn::Unavailable
-    def self.applicable_ranks
-      NomenclaturalRank::Iczn::FamilyGroup.descendants.collect{|t| t.to_s}
-    end
-    def self.disjoint_taxon_name_classes
+  module InnerClass
+    def disjoint_taxon_name_classes
       self.parent.disjoint_taxon_name_classes +
           [TaxonNameClassification::Iczn::Unavailable.to_s]
     end
+  end
+  
+  module InnerClassFamilyGroup
+    include InnerClass
+    
+    def applicable_ranks
+      NomenclaturalRank::Iczn::FamilyGroup.descendants.collect{|t| t.to_s}
+    end
+  end
+  
+  module InnerClassGenusGroup
+    include InnerClass
+    
+    def applicable_ranks
+      NomenclaturalRank::Iczn::GenusGroup.descendants.collect{|t| t.to_s}
+    end
+  end
+  
+  module InnerClassSpeciesGroup
+    include InnerClass
+    
+    def applicable_ranks
+      NomenclaturalRank::Iczn::SpeciesGroup.descendants.collect{|t| t.to_s}
+    end
+  end
+  
+  module InnerClassFamilyGroupUnavailableBefore2000
+    include InnerClassFamilyGroup
+    
+    def code_applicability_start_year
+      1931
+    end
+    def code_applicability_end_year
+      1960
+    end
+  end
+
+  class BasedOnSuppressedGenus < TaxonNameClassification::Iczn::Unavailable
+    extend InnerClassFamilyGroup
   end
 
   class IncorrectOriginalSpelling < TaxonNameClassification::Iczn::Unavailable
-    def self.disjoint_taxon_name_classes
-      self.parent.disjoint_taxon_name_classes +
-          [TaxonNameClassification::Iczn::Unavailable.to_s]
-    end
+    extend InnerClass
   end
 
   class LessThanTwoLetters < TaxonNameClassification::Iczn::Unavailable
-    def self.disjoint_taxon_name_classes
-      self.parent.disjoint_taxon_name_classes +
-          [TaxonNameClassification::Iczn::Unavailable.to_s]
-    end
+    extend InnerClass
   end
 
   class NotLatin < TaxonNameClassification::Iczn::Unavailable
-    def self.disjoint_taxon_name_classes
-      self.parent.disjoint_taxon_name_classes +
-          [TaxonNameClassification::Iczn::Unavailable.to_s]
-    end
+    extend InnerClass
   end
 
   class NotLatinizedAfter1899 < TaxonNameClassification::Iczn::Unavailable
+    extend InnerClassFamilyGroup
+
     def self.code_applicability_start_year
       1900
-    end
-    def self.applicable_ranks
-      NomenclaturalRank::Iczn::FamilyGroup.descendants.collect{|t| t.to_s}
-    end
-    def self.disjoint_taxon_name_classes
-      self.parent.disjoint_taxon_name_classes +
-          [TaxonNameClassification::Iczn::Unavailable.to_s]
     end
   end
 
   class NotLatinizedBefore1900AndNotAccepted < TaxonNameClassification::Iczn::Unavailable
+    extend InnerClassFamilyGroup
+
     def self.code_applicability_end_year
       1899
-    end
-    def self.applicable_ranks
-      NomenclaturalRank::Iczn::FamilyGroup.descendants.collect{|t| t.to_s}
-    end
-    def self.disjoint_taxon_name_classes
-      self.parent.disjoint_taxon_name_classes +
-          [TaxonNameClassification::Iczn::Unavailable.to_s]
     end
   end
 
   class NotNominativePlural < TaxonNameClassification::Iczn::Unavailable
-    def self.applicable_ranks
-      NomenclaturalRank::Iczn::FamilyGroup.descendants.collect{|t| t.to_s}
-    end
-    def self.disjoint_taxon_name_classes
-      self.parent.disjoint_taxon_name_classes +
-          [TaxonNameClassification::Iczn::Unavailable.to_s]
-    end
+    extend InnerClassFamilyGroup
   end
 
   class NotNounInNominativeSingular < TaxonNameClassification::Iczn::Unavailable
-    def self.applicable_ranks
-      NomenclaturalRank::Iczn::GenusGroup.descendants.collect{|t| t.to_s}
-    end
-    def self.disjoint_taxon_name_classes
-      self.parent.disjoint_taxon_name_classes +
-          [TaxonNameClassification::Iczn::Unavailable.to_s]
-    end
+    extend InnerClassGenusGroup
   end
 
   class NotNounOrAdjective < TaxonNameClassification::Iczn::Unavailable
-    def self.applicable_ranks
-      NomenclaturalRank::Iczn::SpeciesGroup.descendants.collect{|t| t.to_s}
-    end
-    def self.disjoint_taxon_name_classes
-      self.parent.disjoint_taxon_name_classes +
-          [TaxonNameClassification::Iczn::Unavailable.to_s]
-    end
+    extend InnerClassSpeciesGroup
   end
 
   class NotScientificPlural < TaxonNameClassification::Iczn::Unavailable
-    def self.applicable_ranks
-      NomenclaturalRank::Iczn::FamilyGroup.descendants.collect{|t| t.to_s}
-    end
-    def self.disjoint_taxon_name_classes
-      self.parent.disjoint_taxon_name_classes +
-          [TaxonNameClassification::Iczn::Unavailable.to_s]
-    end
+    extend InnerClassFamilyGroup
   end
 
   class PreLinnean < TaxonNameClassification::Iczn::Unavailable
+    extend InnerClass
+
     def self.code_applicability_end_year
       1757
-    end
-    def self.disjoint_taxon_name_classes
-      self.parent.disjoint_taxon_name_classes +
-          [TaxonNameClassification::Iczn::Unavailable.to_s]
     end
   end
 
   class UnavailableAndNotUsedAsValidBefore2000 < TaxonNameClassification::Iczn::Unavailable
-    def self.code_applicability_start_year
-      1931
-    end
-    def self.code_applicability_end_year
-      1960
-    end
-    def self.applicable_ranks
-      NomenclaturalRank::Iczn::FamilyGroup.descendants.collect{|t| t.to_s}
-    end
-    def self.disjoint_taxon_name_classes
-      self.parent.disjoint_taxon_name_classes +
-          [TaxonNameClassification::Iczn::Unavailable.to_s]
-    end
+    extend InnerClassFamilyGroupUnavailableBefore2000
   end
 
   class UnavailableAndRejectedByAuthorBefore2000 < TaxonNameClassification::Iczn::Unavailable
-    def self.code_applicability_start_year
-      1931
-    end
-    def self.code_applicability_end_year
-      1960
-    end
-    def self.applicable_ranks
-      NomenclaturalRank::Iczn::FamilyGroup.descendants.collect{|t| t.to_s}
-    end
-    def self.disjoint_taxon_name_classes
-      self.parent.disjoint_taxon_name_classes +
-          [TaxonNameClassification::Iczn::Unavailable.to_s]
-    end
+    extend InnerClassFamilyGroupUnavailableBefore2000
   end
 
   class UnavailableUnderIcn < TaxonNameClassification::Iczn::Unavailable
-    def self.disjoint_taxon_name_classes
-      self.parent.disjoint_taxon_name_classes +
-          [TaxonNameClassification::Iczn::Unavailable.to_s]
-    end
+    extend InnerClass
   end
 
   class VarietyOrFormAfter1960 < TaxonNameClassification::Iczn::Unavailable
+    extend InnerClassSpeciesGroup
+    
     def self.code_applicability_start_year
       1961
-    end
-    def self.applicable_ranks
-      NomenclaturalRank::Iczn::SpeciesGroup.descendants.collect{|t| t.to_s}
-    end
-    def self.disjoint_taxon_name_classes
-      self.parent.disjoint_taxon_name_classes +
-          [TaxonNameClassification::Iczn::Unavailable.to_s]
     end
   end
 
