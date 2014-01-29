4f095561948d63548797800011213299e78b5258
Dmitry Dmitriev <arboridia@gmail.com>
2014-01-28 18:40:24 -0600

    type update

--- app/models/type_material.rb
+++ app/models/type_material.rb
@@ -1,8 +1,10 @@
 class TypeMaterial < ActiveRecord::Base
   belongs_to :source
 
- include Housekeeping
+  include Housekeeping
   include Shared::Citable
+  include SoftValidation
+
 
   # Keys are valid values for type_type, values are
   # required Class for material
@@ -37,6 +39,14 @@ class TypeMaterial < ActiveRecord::Base
   has_many :type_designator_roles, class_name: 'TypeDesignator', as: :role_object
   has_many :type_designators, through: :type_designator_roles, source: :person
 
+  scope :where_taxon_name, -> (taxon_name) {where(protonym_id: taxon_name)}
+  scope :with_type_string, -> (base_string) {where('type_type LIKE ?', "#{base_string}" ) }
+  scope :with_type_array, -> (base_array) {where('type_type IN (?)', base_array ) }
+  scope :not_self, -> (id) {where('id != ?', id )}
+
+
+  soft_validate(:sv_single_primary_type, set: :single_primary_type)
+
   validates :protonym, presence: true
   validates :material, presence: true
   validates_presence_of :type_type
@@ -51,6 +61,17 @@ class TypeMaterial < ActiveRecord::Base
       if (code == :iczn && !ICZN_TYPES.keys.include?(self.type_type)) || (code == :icn && !ICN_TYPES.keys.include?(self.type_type))
         errors.add(:type_type, 'Not a legal type for the nomenclatural code provided') 
       end
+    end
+  end
+
+  def sv_single_primary_type
+    primary_types = TypeMaterial.with_type_array('holotype', 'neotype', 'lectotype').not_self(self)
+    syntypes = TypeMaterial.with_type_array('syntype', 'syntypes')
+    if self.type_type = 'syntype' || self.type_type = 'syntypes'
+      errors.add(:type_type, 'Other primary types selected for the taxon are conflicting with the syntypes') unless primary_types.empty?
+    end
+    if self.type_type = 'holotype' || self.type_type = 'neotype' || self.type_type = 'lectotype'
+      errors.add(:type_type, 'More than one primary types associated with the taxon') unless primary_types.empty? && syntypes.empty?
     end
   end
 
