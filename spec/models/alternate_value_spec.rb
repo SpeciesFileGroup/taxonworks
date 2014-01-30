c9b93234cb2f8739b3b45f6a3b8caa22cb4ccf18
mjy <diapriid@gmail.com>
2014-01-30 11:54:42 -0600

    Moving from .create to .build paradigm.

--- spec/models/alternate_value_spec.rb
+++ spec/models/alternate_value_spec.rb
@@ -38,7 +38,7 @@ describe AlternateValue do
     end
 
     specify 'alternate_object_attribute is legal column of alternate_object' do
-      alternate_value.alternate_object = FactoryGirl.create(:valid_protonym)
+      alternate_value.alternate_object = FactoryGirl.build(:valid_protonym)
       alternate_value.alternate_object_attribute = 'foo'
       alternate_value.valid?
       expect(alternate_value.errors.include?(:alternate_object_attribute)).to be_true
@@ -48,7 +48,7 @@ describe AlternateValue do
     end
 
     specify 'value is not identical to existing value' do
-      alternate_value.alternate_object = FactoryGirl.create(:valid_otu, name: 'foo')
+      alternate_value.alternate_object = FactoryGirl.build(:valid_otu, name: 'foo')
       alternate_value.alternate_object_attribute = 'name'
       alternate_value.value = 'foo'
       alternate_value.valid?
@@ -70,8 +70,8 @@ describe AlternateValue do
 
   context 'scopes' do
     specify 'with_alternate_value_on' do
-      o = FactoryGirl.create(:valid_otu)
-      o.alternate_values << FactoryGirl.create(:valid_alternate_value_abbreviation, alternate_object: o, value: 'foo') 
+      o = FactoryGirl.build(:valid_otu)
+      o.alternate_values << FactoryGirl.build(:valid_alternate_value_abbreviation, alternate_object: o, value: 'foo') 
       o.save!
 
       expect(Otu.with_alternate_value_on(:name, 'foo')).to have(1).things
@@ -81,15 +81,15 @@ describe AlternateValue do
 
   context 'use' do
     specify 'adding an alternate value' do 
-      o = FactoryGirl.create(:valid_otu)
-      o.alternate_values << FactoryGirl.create(:valid_alternate_value_abbreviation, alternate_object: o, value: 'foo') 
+      o = FactoryGirl.build(:valid_otu)
+      o.alternate_values << FactoryGirl.build(:valid_alternate_value_abbreviation, alternate_object: o, value: 'foo') 
       expect(o.save).to be_true
       expect(o.alternate_values).to have(1).things
     end
 
     specify 'original_value' do
       expect(alternate_value).to respond_to(:original_value)
-      v = FactoryGirl.create(:valid_alternate_value_abbreviation, value: 'foo', alternate_object_attribute: 'name') 
+      v = FactoryGirl.build(:valid_alternate_value_abbreviation, value: 'foo', alternate_object_attribute: 'name') 
       expect(v.original_value).to eq('my concept') # see the otu_factory
     end
   end
