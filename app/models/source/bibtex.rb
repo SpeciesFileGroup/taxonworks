ab58328340d48e6b8e429f376fcfa9a1abd755ee
mjy <diapriid@gmail.com>
2014-02-11 12:21:37 -0600

    Refactoring date methods.

--- app/models/source/bibtex.rb
+++ app/models/source/bibtex.rb
@@ -511,43 +511,15 @@ class Source::Bibtex < Source
 
 #endregion has_<attribute>? section
 
-#region time/date related
-  # @return[Time] returns nomenclature_date as stored in the db, if not computed yet, computes it.
-  #   returns nil if there is no :year set. Does NOT set nomenclature_date in the object.
+  #region time/date related
+  # @return[Time] alias for nomenclature_date, computed if not yet saved
   def date
-    return(self.nomenclature_date) if !(self.nomenclature_date.nil?)
-    return(self.generate_nomenclature_date) if !(self.year.nil?)
-    return(nil)
+    set_nomenclature_date if !self.persisted?
+    self.nomenclature_date
   end
 
-  # @return[Boolean] sets the nomenclature_date within the object.
   def set_nomenclature_date
-    self.nomenclature_date = self.generate_nomenclature_date
-  end
-
-  # TODO: Abstract to a Date/Time module
-  # @return[Time] a UTC time (Uses Time instead of Date so that it can be saved as a UTC object -
-  #   see http://www.ruby-doc.org/core-2.0.0/Time.html)
-  #   returns nomenclature_date based on computation of the values of :year, :month, :day.
-  #   Should only ever be called after validation!
-  #    if :year is empty, return nil
-  #    if :month is empty, returns 12/31/:year
-  #    if :day is empty, returns the last day of the month
-  def generate_nomenclature_date
-    if self.year.nil?
-      nil
-    elsif self.month.nil?
-      Time.utc(self.year, 12, 31)
-    elsif self.day.nil?
-      tmp = Time.utc(self.year, self.month)
-      if tmp.month == 12 # want the last day of december
-        Time.utc(self.year, 12, 31)
-      else # time + 1 month - 1 day (60 sec * 60 min *24 hours)
-        Time.utc(self.year, tmp.month + 1) - 86400
-      end
-    else
-      Time.utc(self.year, self.month, self.day)
-    end
+    self.nomenclature_date = Utilities::Dates.nomenclature_date(self.day, self.month, self.year) 
   end
 
 #endregion    time/date related
