7da393df781403db1dba3b1e23973070f77b8b0e
mjy <diapriid@gmail.com>
2014-02-19 14:38:36 -0600

    GeographicArea tests.

--- app/models/geographic_area_type.rb
+++ app/models/geographic_area_type.rb
@@ -3,7 +3,6 @@ class GeographicAreaType < ActiveRecord::Base
   include Housekeeping::Users
 
   has_many :geographic_areas, inverse_of: :geographic_area_type
-
   validates_presence_of :name
 
 end
--- spec/models/geographic_area_type_spec.rb
+++ spec/models/geographic_area_type_spec.rb
@@ -1,22 +1,22 @@
 require 'spec_helper'
 
 describe GeographicAreaType do
-  let(:geographic_area_type) {FactoryGirl.create(:p_geographic_area_type)}
+  let(:geographic_area_type) {FactoryGirl.build(:geographic_area_type)}
 
   context 'associations' do
     context 'has_many' do
-
+      specify 'geographic_areas' do
+        expect(geographic_area_type).to respond_to(:geographic_areas)
+      end
     end
   end
 
   context 'validation' do
+    before(:each) {
+      geographic_area_type.valid?
+    }
     specify 'name' do
-      expect(geographic_area_type).to respond_to(:name)
-    end
-
-    specify 'name' do
-      expect(geographic_area_type).to respond_to(:name)
-      expect(geographic_area_type.name.blank?).to be_false
+      expect(geographic_area_type.errors.include?(:name)).to be_true
     end
   end
 end
