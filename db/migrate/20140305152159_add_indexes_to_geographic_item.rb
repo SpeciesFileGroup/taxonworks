c31a240a08c8566d3a5d96cb6e61bcc4d59c7228
Jim Tucker <tuckerjd@uiuc.edu>
2014-03-05 11:36:40 -0600

    Correction of spelling of Indexes.

--- db/migrate/20140305152159_add_indexes_to_geographic_item.rb
+++ db/migrate/20140305152159_add_indexes_to_geographic_item.rb
@@ -1,4 +1,4 @@
-class AddIndexsToGeographicItem < ActiveRecord::Migration
+class AddIndexesToGeographicItem < ActiveRecord::Migration
   def change
 
     GeographicItem.connection.execute('CREATE INDEX point_gix ON geographic_items USING GIST (point);')
