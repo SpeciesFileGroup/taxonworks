f3b8f29b590740884a4e04b2b68d85c54505f723
mjy <diapriid@gmail.com>
2014-03-10 14:03:34 -0500

    Image test cleanup.

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,27 +3,24 @@ class Image < ActiveRecord::Base
   include Housekeeping::Users
   include Shared::Identifiable
   include Shared::Notable
-  #include Shared::AlternateValues # can an image have alternate values?
-  #include Shared::DataAttributes # can an image have attributes?
   include Shared::Taggable
 
-
-  # attr_accessible :image_file
-  has_attached_file :image_file, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image_file, :styles => { :medium => "300x300>", :thumb => "100x100>" } # , :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image_file, :content_type => /\Aimage\/.*\Z/
-  # validates_attachment_presence :image_file     # can't have this and call 'delete_attached_file'
+  validates_attachment_presence :image_file    
   validates_attachment_size :image_file,  greater_than: 1.kilobytes
 
-  # before_destroy :delete_attached_file
+  # TODO: this shouldn't be required
+  before_destroy :delete_attached_file
 
   #TODO create :default_url => "/images/:style/missing.png"?
   #TODO is paperclip content-type the same as we mean for content type?
   #TODO create large version of image_file
 
+  protected
+
   def delete_attached_file
-    # This call is needed until we figure out why paperclip isn't deleting the files on destroy
-    image_file = nil
-    self.save
+    image_file.destroy 
   end
 
 end
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,25 +1,24 @@
 require 'spec_helper'
 
 describe Image do
-
-  # default recommended tests for paperclip
-  # (default associated columns are image_file_file_name, image_file_content_type(mime type?), image_file_file_size,
-  # image_file_updated_at)
-  it { should have_attached_file(:image_file) }
-  it { should validate_attachment_presence(:image_file) }
-  it { should validate_attachment_content_type(:image_file).
-                allowing('image/png', 'image/gif').
-                rejecting('text/plain', 'text/xml') }
-  it { should validate_attachment_size(:image_file).
-                greater_than(1.kilobytes) }
+  # Taken verbatim from the doc. 
+  context 'default paperclip tests' do
+    it { should have_attached_file(:image_file) }
+    it { should validate_attachment_presence(:image_file) }
+    it { should validate_attachment_content_type(:image_file).
+         allowing('image/png', 'image/gif').
+         rejecting('text/plain', 'text/xml') }
+    it { should validate_attachment_size(:image_file).
+         greater_than(1.kilobytes) }
+  end
 
   # paperclip MD5 add-on tests
   pending 'it should have a computed MD5 checksum on save'
 
   # TW add on attributes
   pending 'valid images have a user_file_name'
-  pending 'it should save height & width on save'
-  pending 'user_image_name? '  #todo what is this and how is it different from user_file_name?
+  pending 'it should save height'
+  pending 'it should save width' 
 
   context 'should manipulate the file system' do
     specify 'by saving the image on object save' do
@@ -29,20 +28,18 @@ describe Image do
 
       #expect(i.update(image_file: nil)).to be_true
       #expect(i.save).to be_true
-      foo = 9
       expect(i.destroy).to be_true
-      foo = 1
       #check the file system that the file is gone
     end
-
   end
 
+  # TODO: Leave testing out here- needs to be abstracted, and will only add lenght here.
   context 'concerns' do
-    it_behaves_like 'identifiable'
-    it_behaves_like 'notable'
-    #it_behaves_like 'alternate_values'
-    #it_behaves_like 'data_attributes'
-    it_behaves_like 'taggable'
+    # it_behaves_like 'identifiable'
+    # it_behaves_like 'notable'
+    #  #it_behaves_like 'alternate_values'
+    #  #it_behaves_like 'data_attributes'
+    # it_behaves_like 'taggable'
   end
 
 end
