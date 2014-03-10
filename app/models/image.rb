7339be289104cd86a24d96114587384eef1a864c
eef <eef@illinois.edu>
2014-03-10 13:18:07 -0500

    checkpoint adding test

--- app/models/image.rb
+++ app/models/image.rb
@@ -11,11 +11,19 @@ class Image < ActiveRecord::Base
   # attr_accessible :image_file
   has_attached_file :image_file, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image_file, :content_type => /\Aimage\/.*\Z/
-  validates_attachment_presence :image_file
+  # validates_attachment_presence :image_file     # can't have this and call 'delete_attached_file'
   validates_attachment_size :image_file,  greater_than: 1.kilobytes
+
+  # before_destroy :delete_attached_file
 
   #TODO create :default_url => "/images/:style/missing.png"?
   #TODO is paperclip content-type the same as we mean for content type?
   #TODO create large version of image_file
+
+  def delete_attached_file
+    # This call is needed until we figure out why paperclip isn't deleting the files on destroy
+    image_file = nil
+    self.save
+  end
 
 end
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -21,6 +21,22 @@ describe Image do
   pending 'it should save height & width on save'
   pending 'user_image_name? '  #todo what is this and how is it different from user_file_name?
 
+  context 'should manipulate the file system' do
+    specify 'by saving the image on object save' do
+      i=FactoryGirl.build(:valid_image)
+      expect(i.save).to be_true
+      #check the file system for the file
+
+      #expect(i.update(image_file: nil)).to be_true
+      #expect(i.save).to be_true
+      foo = 9
+      expect(i.destroy).to be_true
+      foo = 1
+      #check the file system that the file is gone
+    end
+
+  end
+
   context 'concerns' do
     it_behaves_like 'identifiable'
     it_behaves_like 'notable'
