0f482d510a37cf5bf8d4b6c58149d917064bfecd
mjy <diapriid@gmail.com>
2014-03-28 16:47:17 -0500

    Resolves respositories specs.

--- spec/controllers/repositories_controller_spec.rb
+++ spec/controllers/repositories_controller_spec.rb
@@ -23,7 +23,7 @@ describe RepositoriesController do
   # This should return the minimal set of attributes required to create a valid
   # Repository. As you add validations to Repository, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { { "name" => "MyString" } }
+  let(:valid_attributes) { FactoryGirl.build(:valid_repository).attributes }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
