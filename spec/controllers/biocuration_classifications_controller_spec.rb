87b39e6fd3017ed5a278afbba7e0e470adcaa330
mjy <diapriid@gmail.com>
2014-03-28 16:44:40 -0500

    Biocuration errors resolved.

--- spec/controllers/biocuration_classifications_controller_spec.rb
+++ spec/controllers/biocuration_classifications_controller_spec.rb
@@ -23,7 +23,7 @@ describe BiocurationClassificationsController do
   # This should return the minimal set of attributes required to create a valid
   # BiocurationClassification. As you add validations to BiocurationClassification, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { FactoryGirl.build(:valid_biocuration_class).attributes }
+  let(:valid_attributes) { FactoryGirl.build(:valid_biocuration_classification).attributes }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
