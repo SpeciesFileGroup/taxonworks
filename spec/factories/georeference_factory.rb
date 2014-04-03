6ea38011bf67552639d5f141c353a8d8a19f409d
mjy <diapriid@gmail.com>
2014-04-03 14:23:34 -0500

    Jim notes.

--- spec/factories/georeference_factory.rb
+++ spec/factories/georeference_factory.rb
@@ -3,12 +3,27 @@ FactoryGirl.define do
   factory :georeference, traits: [:creator_and_updater] do
 
     factory :valid_georeference do
-
-      collecting_event { FactoryGirl.build(:valid_collecting_event, 'Somebody\'s office')}
-
+      association :collecting_event, factory: :valid_collecting_event
+      association :geographic_itme, factory: :valid_geographic_item
+      type 'Georeference::VerbatimData'
     end
 
     factory :point_georeference do
+
+    end
+
+
+    # This goes in /georeference/verbatim_data_factory.rb
+    factory :georeference_verbatim_data, class: 'Georeference::VerbatimData', traits: [:houskeeping] do
+      factory :valid_georeference_verbatim_data do
+        association :collecting_event, factory: :valid_collecting_event, verbatim_latitude: '23.2323', verbatim_longitude: '23.23232'
+        association :geographic_itme, factory: :balid_geographic_item
+        # type 'Georeference::VerbatimData'
+         
+      end
+
+    # identical in geolocate
+    # should also have api_request
 
     end
 
