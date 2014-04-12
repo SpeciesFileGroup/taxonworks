2b21504f134de3b7ea181812c84959d75f17c578
mjy <diapriid@gmail.com>
2014-04-11 22:06:23 -0500

    Working on users/projects.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,14 @@
 class Project < ActiveRecord::Base
- include Housekeeping::Users
+
+  store_accessor :workbench_settings, :worker_tasks
+ 
+  include Housekeeping::Users
   has_many :project_members
   has_many :users, through: :project_members
 
   validates_presence_of :name
+
+
+
+
 end
