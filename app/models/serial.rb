2f35019814589bc7b1603ebd0e24d6ca01aacc28
eef <eef@illinois.edu>
2014-04-14 19:32:22 -0500

    checkpoint - not checking duplicates correctly

--- app/models/serial.rb
+++ app/models/serial.rb
@@ -1,5 +1,5 @@
 # Serial - represents a journal or other serial publication
-# Currently the only attribute is the full_name.
+#
 # It will support abbreviations through Shared::AlternateValues.
 # Serials are Notable, Taggable, and Identifiable.
 #
@@ -9,19 +9,23 @@ class Serial < ActiveRecord::Base
   include Housekeeping::Users
   include Shared::Identifiable
   include Shared::Notable
-  include Shared::AlternateValues
-# TODO should serials have dataAttributes?
-#  include Shared::DataAttributes
+  include Shared::AlternateValues   # abbreviations, alternate titles, language translations
+  include Shared::DataAttributes
   include Shared::Taggable
 
   # Class constants
   # Class variables
   # Callbacks
   # Associations, in order: belongs_to, has_one,has_many
+  has_many :serial_chronologies_as_succeeding, foreign_key: :subject_serial_id
+  has_many :serial_chronologies_as_preceding, foreign_key: :subject_object_id
+  has_many :succeeding_serials, through: :serial_chronologies_as_subject, class_name: 'Serial'
+  has_many :preceding_serials, through: :serial_chronologies_as_object, class_name: 'Serial'
+
   # Scopes, clustered by function
 
   # "Hard" Validations
-  validate :not_empty
+  validates_presence_of :name
 
   # "Soft" Validations
   soft_validate(:sv_duplicate?)
@@ -33,21 +37,22 @@ class Serial < ActiveRecord::Base
   # Instance methods
   def duplicate?
     # Boolean is there another serial with the same name?
-    #TODO write this
-    False
+    if self.new_record?
+      Serial.exists?(name: self.name)
+    else
+      dup = Serial.where("name = ? AND id <> ?", self.name, self.id)
+    end
+
   end
   protected
 
 
   def sv_duplicate?
     if self.duplicate?
-      soft_validations.add(:full_name, 'There is another serial with this name in the database.')
+      soft_validations.add(:name, 'There is another serial with this name in the database.')
     end
   end
 
-  def not_empty
-     # a serial must have content in some field
-    !self.full_name.blank?
-  end
+  # TODO soft validation of name matching an alternate value for name of a different serial
 
 end
