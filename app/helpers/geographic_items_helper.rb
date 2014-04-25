2304b90f45d982a72097e1616eacd4579b800505
jrflood <jrflood@illinois.edu>
2014-04-25 14:05:50 -0500

    Locate map display to first coordinate of the geo...item

--- app/helpers/geographic_items_helper.rb
+++ app/helpers/geographic_items_helper.rb
@@ -2,4 +2,9 @@ module GeographicItemsHelper
   def json_tag(geographic_item)
     geographic_item.to_geo_json.html_safe
   end
+
+  def center_coord_tag(geographic_item)
+    json_tag(geographic_item) =~ /(-{0,1}\d+\.{0,1}\d*),(-{0,1}\d+\.{0,1}\d*)/  #get longitude, latitude of first point of geo...item
+    "#{$1}, #{$2}".html_safe
+  end
 end
