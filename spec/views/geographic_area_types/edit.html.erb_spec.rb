f99970d3913f9e5efeb2467c3258d4e601c90550
Jim Tucker <tuckerjd@uiuc.edu>
2014-06-05 09:04:48 -0500

    Fixing index issue with GeoObject tables.

--- spec/views/geographic_area_types/edit.html.erb_spec.rb
+++ spec/views/geographic_area_types/edit.html.erb_spec.rb
@@ -2,11 +2,10 @@ require 'spec_helper'
 
 describe 'geographic_area_types/edit' do
   before(:each) do
-    @geographic_area_type = assign(:geographic_area_type, stub_model(GeographicAreaType,
-                                                                     :name          => 'StubAreaType',
-                                                                     :created_by_id => 1,
-                                                                     :updated_by_id => 1
-    ))
+    @geographic_area_type = assign(:geographic_area_type,
+                                   stub_model(GeographicAreaType,
+                                              :name          => 'StubAreaType'
+                                   ))
   end
 
   it 'renders the edit geographic_area_type form' do
@@ -15,6 +14,7 @@ describe 'geographic_area_types/edit' do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form[action=?][method=?]", geographic_area_type_path(@geographic_area_type), "post" do
       assert_select "input#geographic_area_type_name[name=?]", "geographic_area_type[name]"
+      # on the geographic_area_types/edit view, created_by and updated_by are no longer represented
       # assert_select "input#geographic_area_type_created_by_id[name=?]", "geographic_area_type[created_by_id]"
       # assert_select "input#geographic_area_type_updated_by_id[name=?]", "geographic_area_type[updated_by_id]"
     end
