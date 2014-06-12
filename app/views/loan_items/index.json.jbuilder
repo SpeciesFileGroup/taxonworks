15ff69367f8f1f895d535692afad5711c8d2b82b
Jim Tucker <tuckerjd@uiuc.edu>
2014-06-12 12:51:02 -0500

    LoanItem view and spec/view w/attributes.

--- app/views/loan_items/index.json.jbuilder
+++ app/views/loan_items/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@loan_items) do |loan_item|
-  json.extract! loan_item, :id
+  json.extract! loan_item, :id, :loan_id, :collection_object_id, :date_returned, :collection_object_status, :position, :created_by_id, :updated_by_id, :project_id, :container_id
   json.url loan_item_url(loan_item, format: :json)
 end
--- spec/views/loan_items/index.html.erb_spec.rb
+++ spec/views/loan_items/index.html.erb_spec.rb
@@ -3,13 +3,39 @@ require 'spec_helper'
 describe "loan_items/index" do
   before(:each) do
     assign(:loan_items, [
-      stub_model(LoanItem),
-      stub_model(LoanItem)
+      stub_model(LoanItem,
+        :loan_id => 1,
+        :collection_object_id => 2,
+        :collection_object_status => "Collection Object Status",
+        :position => 3,
+        :created_by_id => 4,
+        :updated_by_id => 5,
+        :project_id => 6,
+        :container_id => 7
+      ),
+      stub_model(LoanItem,
+        :loan_id => 1,
+        :collection_object_id => 2,
+        :collection_object_status => "Collection Object Status",
+        :position => 3,
+        :created_by_id => 4,
+        :updated_by_id => 5,
+        :project_id => 6,
+        :container_id => 7
+      )
     ])
   end
 
   it "renders a list of loan_items" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => 1.to_s, :count => 2
+    assert_select "tr>td", :text => 2.to_s, :count => 2
+    assert_select "tr>td", :text => "Collection Object Status".to_s, :count => 2
+    assert_select "tr>td", :text => 3.to_s, :count => 2
+    assert_select "tr>td", :text => 4.to_s, :count => 2
+    assert_select "tr>td", :text => 5.to_s, :count => 2
+    assert_select "tr>td", :text => 6.to_s, :count => 2
+    assert_select "tr>td", :text => 7.to_s, :count => 2
   end
 end
