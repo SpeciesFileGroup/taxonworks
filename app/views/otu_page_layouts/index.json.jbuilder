17414a4e89774969e9a04ef5555bd800ccd0ee8e
Jim Tucker <tuckerjd@uiuc.edu>
2014-06-12 13:00:16 -0500

    OtuPageLayout view and spec/view w/attributes.

--- app/views/otu_page_layouts/index.json.jbuilder
+++ app/views/otu_page_layouts/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@otu_page_layouts) do |otu_page_layout|
-  json.extract! otu_page_layout, :id
+  json.extract! otu_page_layout, :id, :name, :created_by_id, :updated_by_id, :project_id
   json.url otu_page_layout_url(otu_page_layout, format: :json)
 end
--- spec/views/otu_page_layouts/show.html.erb_spec.rb
+++ spec/views/otu_page_layouts/show.html.erb_spec.rb
@@ -2,11 +2,20 @@ require 'spec_helper'
 
 describe "otu_page_layouts/show" do
   before(:each) do
-    @otu_page_layout = assign(:otu_page_layout, stub_model(OtuPageLayout))
+    @otu_page_layout = assign(:otu_page_layout, stub_model(OtuPageLayout,
+      :name => "Name",
+      :created_by_id => 1,
+      :updated_by_id => 2,
+      :project_id => 3
+    ))
   end
 
   it "renders attributes in <p>" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
+    rendered.should match(/Name/)
+    rendered.should match(/1/)
+    rendered.should match(/2/)
+    rendered.should match(/3/)
   end
 end
