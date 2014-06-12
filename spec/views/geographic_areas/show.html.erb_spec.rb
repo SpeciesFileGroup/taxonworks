3f3274690a0067b9f14086846a21082dbb292dd4
Jim Tucker <tuckerjd@uiuc.edu>
2014-06-12 13:53:52 -0500

    Problem with spec/view/geographic_areas/show

--- spec/views/geographic_areas/show.html.erb_spec.rb
+++ spec/views/geographic_areas/show.html.erb_spec.rb
@@ -12,41 +12,32 @@ describe 'geographic_areas/show' do
                                               :data_origin             => 'Someplace',
                                               :tdwgID                  => 'Tdwg',
                                               :level0_id               => 1,
-                                              :level1_id               => 1,
-                                              :level2_id               => 1,
+                                              :level1_id               => 0,
+                                              :level2_id               => 0,
                                               :created_by_id           => 12,
                                               :updated_by_id           => 13
                                    ).as_new_record)
     @geographic_area_type = assign(:geographic_area_type,
                                    stub_model(GeographicAreaType,
                                               :id => 1,
-                                              :name => 'AreaType'))
+                                              :name => 'AreaType')).as_new_record
   end
 
   it 'renders attributes in <p>' do
-    pending 'reconstruction of the geographic_area/show view or spec'
     render
+    pending 'reconstruction of the geographic_area/show view or spec'
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     rendered.should match(/Name/)
     rendered.should match(/1/)
-    rendered.should match(/2/)
-    rendered.should match(/3/)
-    rendered.should match(/4/)
-    rendered.should match(/5/)
-    rendered.should match(/6/)
-    rendered.should match(/Iso 3166 A2/)
-    rendered.should match(/9/)
-    rendered.should match(/Iso 3166 A3/)
-    rendered.should match(/10/)
+    rendered.should match(/AreaType/)
+    rendered.should match(/AA/)
+    rendered.should match(/AAA/)
+    rendered.should match(/Someplace/)
     rendered.should match(/Tdwg/)
-    rendered.should match(/11/)
-    rendered.should match(/Gadm Valid From/)
-    rendered.should match(/Gadm Valid To/)
-    rendered.should match(/Data Origin/)
-    rendered.should match(/Adm0 A3/)
-    rendered.should match(/Ne/)
+    rendered.should match(/1/)
+    rendered.should match(/0/)
+    rendered.should match(/0/)
     rendered.should match(/12/)
     rendered.should match(/13/)
-    rendered.should match(/14/)
   end
 end
