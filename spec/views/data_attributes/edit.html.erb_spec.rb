67b6564f43142b2625c1a11278e54ce06a3a1ee5
Jim Tucker <tuckerjd@uiuc.edu>
2014-06-12 12:46:58 -0500

    DataAttribute view and spec/view w/attributes.

--- spec/views/data_attributes/edit.html.erb_spec.rb
+++ spec/views/data_attributes/edit.html.erb_spec.rb
@@ -2,7 +2,17 @@ require 'spec_helper'
 
 describe "data_attributes/edit" do
   before(:each) do
-    @data_attribute = assign(:data_attribute, stub_model(DataAttribute))
+    @data_attribute = assign(:data_attribute, stub_model(DataAttribute,
+      :type => "",
+      :attribute_subject_id => 1,
+      :attribute_subject_type => "MyString",
+      :controlled_vocabulary_term_id => 1,
+      :import_predicate => "MyString",
+      :value => "MyText",
+      :created_by_id => 1,
+      :updated_by_id => 1,
+      :project_id => 1
+    ))
   end
 
   it "renders the edit data_attribute form" do
@@ -10,6 +20,15 @@ describe "data_attributes/edit" do
 
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form[action=?][method=?]", data_attribute_path(@data_attribute), "post" do
+      assert_select "input#data_attribute_type[name=?]", "data_attribute[type]"
+      assert_select "input#data_attribute_attribute_subject_id[name=?]", "data_attribute[attribute_subject_id]"
+      assert_select "input#data_attribute_attribute_subject_type[name=?]", "data_attribute[attribute_subject_type]"
+      assert_select "input#data_attribute_controlled_vocabulary_term_id[name=?]", "data_attribute[controlled_vocabulary_term_id]"
+      assert_select "input#data_attribute_import_predicate[name=?]", "data_attribute[import_predicate]"
+      assert_select "textarea#data_attribute_value[name=?]", "data_attribute[value]"
+      assert_select "input#data_attribute_created_by_id[name=?]", "data_attribute[created_by_id]"
+      assert_select "input#data_attribute_updated_by_id[name=?]", "data_attribute[updated_by_id]"
+      assert_select "input#data_attribute_project_id[name=?]", "data_attribute[project_id]"
     end
   end
 end
--- spec/views/data_attributes/show.html.erb_spec.rb
+++ spec/views/data_attributes/show.html.erb_spec.rb
@@ -2,11 +2,30 @@ require 'spec_helper'
 
 describe "data_attributes/show" do
   before(:each) do
-    @data_attribute = assign(:data_attribute, stub_model(DataAttribute))
+    @data_attribute = assign(:data_attribute, stub_model(DataAttribute,
+      :type => "Type",
+      :attribute_subject_id => 1,
+      :attribute_subject_type => "Attribute Subject Type",
+      :controlled_vocabulary_term_id => 2,
+      :import_predicate => "Import Predicate",
+      :value => "MyText",
+      :created_by_id => 3,
+      :updated_by_id => 4,
+      :project_id => 5
+    ))
   end
 
   it "renders attributes in <p>" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
+    rendered.should match(/Type/)
+    rendered.should match(/1/)
+    rendered.should match(/Attribute Subject Type/)
+    rendered.should match(/2/)
+    rendered.should match(/Import Predicate/)
+    rendered.should match(/MyText/)
+    rendered.should match(/3/)
+    rendered.should match(/4/)
+    rendered.should match(/5/)
   end
 end
