decebc0632cbbba02d17219b790f310e0a661937
mjy <diapriid@gmail.com>
2014-06-13 17:26:46 -0500

    make some more taxon name tests pass.

--- spec/features/taxon_names_spec.rb
+++ spec/features/taxon_names_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'TaxonNames', TaxonName do
+describe 'TaxonNames', base_class: TaxonName  do
 
   it_behaves_like 'a_login_required_and_project_selected_controller'
 
