8ea70b074d7b2fb4f5afd90f3298e7edda3d6734
Jim Tucker <tuckerjd@uiuc.edu>
2014-06-23 11:52:00 -0500

    Fixing GeographicItem spec/model errors.

--- app/models/geographic_item.rb
+++ app/models/geographic_item.rb
@@ -38,12 +38,19 @@ class GeographicItem < ActiveRecord::Base
   # include_collecting_event
 
   # SELECT * FROM "geographic_items" INNER JOIN "georeferences" ON "georeferences"."geographic_item_id" = "geographic_items"."id" INNER JOIN "collecting_events" ON "collecting_events"."id" = "georeferences"."collecting_event_id"
-  scope :geo_with_collecting_event, -> {joins(:collecting_events_through_georeferences)}
+  scope :geo_with_collecting_event, -> { joins(:collecting_events_through_georeferences) }
   # SELECT * FROM "geographic_items" INNER JOIN "georeferences" ON "georeferences"."error_geographic_item_id" = "geographic_items"."id" INNER JOIN "collecting_events" ON "collecting_events"."id" = "georeferences"."collecting_event_id"
-  scope :err_with_collecting_event, -> {joins('INNER JOIN "georeferences" ON "georeferences"."error_geographic_item_id" = "geographic_items"."id" INNER JOIN "collecting_events" ON "collecting_events"."id" = "georeferences"."collecting_event_id"')}
+  scope :err_with_collecting_event, -> { joins('INNER JOIN "georeferences" ON "georeferences"."error_geographic_item_id" = "geographic_items"."id" INNER JOIN "collecting_events" ON "collecting_events"."id" = "georeferences"."collecting_event_id"') }
   # TODO: is this just an 'or' of the two above 'joins', or os there a better way?
-  scope :all_with_collecting_event, -> {joins('INNER JOIN "georeferences" ON "georeferences"."geographic_item_id" = "geographic_items"."id" INNER JOIN "collecting_events" ON "collecting_events"."id" = "georeferences"."collecting_event_id"')}
-  scope :include_collecting_event, -> {includes(:collecting_events_through_georeferences)}
+  # scope :all_with_collecting_event, -> { joins('INNER JOIN "georeferences" ON "georeferences"."geographic_item_id" = "geographic_items"."id" INNER JOIN "collecting_events" ON "collecting_events"."id" = "georeferences"."collecting_event_id"') }
+  scope :include_collecting_event, -> { includes(:collecting_events_through_georeferences) }
+
+  def self.all_with_collecting_event
+    g = GeographicItem.geo_with_collecting_event
+    e = GeographicItem.err_with_collecting_event
+    r = g + e
+    GeographicItem.where('id in (?)', r.map(&:id))
+  end
 
   # A scope that includes an 'is_valid' attribute (True/False) for the passed geographic_item.  Uses St_IsValid.
   def self.with_is_valid_geometry_column(geographic_item)
