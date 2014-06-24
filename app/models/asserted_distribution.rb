58ede5778792e09347b2482eff0079d856daf145
Dmitry Dmitriev <arboridia@gmail.com>
2014-06-24 16:28:48 -0500

    asserted distribution

--- app/models/asserted_distribution.rb
+++ app/models/asserted_distribution.rb
@@ -1,6 +1,33 @@
 class AssertedDistribution < ActiveRecord::Base
+
+  include Housekeeping::Users
+  include SoftValidation
+
   belongs_to :otu
   belongs_to :geographic_area
   belongs_to :source
   belongs_to :project
+
+  validates_presence_of :otu_id, :geographic_area_id
+
+  scope :with_otu_id, -> (otu_id) {where(otu_id: otu_id)}
+  scope :with_geographic_area_id, -> (geographic_area_id) {where(geographic_area_id: geographic_area_id)}
+  scope :not_self, -> (id) {where('asserted_distribution.id <> ?', id )}
+
+
+  #  validates_uniqueness_of :name, scope: :taxon_name_id
+
+  before_validation :check_required_fields
+
+  soft_validate(:sv_missing_source, set: :missing_source)
+
+
+  #region Soft validation
+
+  def sv_missing_source
+    soft_validations.add(:source_id, 'Source is missing') if self.source_id.nil?
+  end
+
+  #end region
+
 end
--- spec/models/asserted_distribution_spec.rb
+++ spec/models/asserted_distribution_spec.rb
@@ -1,5 +1,36 @@
 require 'spec_helper'
 
 describe AssertedDistribution do
-  skip "add some examples to (or delete) #{__FILE__}"
+
+  let(:asserted_distribution) { AssertedDistribution.new }
+
+  # foreign key relationships
+  context 'reflections / foreign keys' do
+    context 'respond to' do
+
+      specify 'otu' do
+        expect(asserted_distribution).to respond_to(:otu)
+      end
+
+      specify 'source' do
+        expect(asserted_distribution).to respond_to(:source)
+      end
+
+      specify 'geographic_area' do
+        expect(asserted_distribution).to respond_to(:geographic_area)
+      end
+    end
+  end
+
+  context 'soft validation' do
+    specify 'missing source' do
+      ad = FactoryGirl.build_stubbed(:valid_asserted_distribution, source: nil)
+      ad.soft_validate(:missing_source)
+      expect(ad.soft_validations.messages_on(:source_id).count).to eq(1)
+      ad.source_id = 1
+      ad.soft_validate(:missing_source)
+      expect(ad.soft_validations.messages_on(:source_id).empty?).to be_truthy
+    end
+  end
+
 end
