87ef93ab580f7f65265c4c9a22cca98a650b1635
Dmitry Dmitriev <arboridia@gmail.com>
2014-06-26 17:00:19 -0500

    asserted distribution

--- app/models/asserted_distribution.rb
+++ app/models/asserted_distribution.rb
@@ -10,7 +10,8 @@ class AssertedDistribution < ActiveRecord::Base
 
   validates_presence_of :otu_id, message: 'Taxon is not specified'
   validates_presence_of :geographic_area_id, message: 'Geographic area is not selected'
-  validates_presence_of :source
+  validates_presence_of :source_id, message: 'Source is not selected'
+  validates_uniqueness_of :geographic_area_id, scope: [:otu_id, :source_id], message: 'Duplicate record'
 
   scope :with_otu_id, -> (otu_id) {where(otu_id: otu_id)}
   scope :with_geographic_area_id, -> (geographic_area_id) {where(geographic_area_id: geographic_area_id)}
@@ -30,11 +31,11 @@ class AssertedDistribution < ActiveRecord::Base
     unless ga.nil?
       if self.is_absent == true
         presence = AssertedDistribution.without_is_absent.with_geographic_area_id(self.geographic_area_id) # this returns an array, not a single GA so test below is not right
-        soft_validations.add(:otu_id, "Taxon is reported as present in #{presence.geographic_area.name}") unless presence.empty?
+        soft_validations.add(:geographic_area_id, "Taxon is reported as present in #{presence.first.geographic_area.name}") unless presence.empty?
       else
         areas = [ga.level0_id, ga.level1_id, ga.level2_id].compact
         presence = AssertedDistribution.with_is_absent.with_geographic_area_array(areas)
-        soft_validations.add(:otu_id, "Taxon is reported as missing in #{presence.geographic_area.name}") unless presence.empty?
+        soft_validations.add(:geographic_area_id, "Taxon is reported as missing in #{presence.first.geographic_area.name}") unless presence.empty?
       end
     end
   end
