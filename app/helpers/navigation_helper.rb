294ab217007764ec8cd7229ec07941d8bbc0b926
mjy <diapriid@gmail.com>
2014-07-15 23:06:49 -0500

    Previous next by id for show records.  Implemented for Otus as example.

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -9,6 +9,24 @@ module NavigationHelper
   end
 
 
+  def forward_back_links(instance)
+    content_tag(:span,  (previous_link(instance) + ' | ' + next_link(instance)).html_safe )
+  end
+
+  # A previous record link.  Assumes the instance has a project_id! 
+  def previous_link(instance)
+    text = 'Previous'
+    link = instance.class.order(id: :desc).with_project_id(instance.project_id).where(['id < ?', instance.id]).limit(1).first
+    link.nil? ? text : link_to(text, link)
+  end
+
+  # A next record link.  Assumes the instance has a project_id! 
+  def next_link(instance)
+    text = 'Next'
+    link = instance.class.order(id: :asc).with_project_id(instance.project_id).where(['id > ?', instance.id]).limit(1).first
+    link.nil? ? text : link_to(text, link)
+  end
+
 
   def new_path_for_model(model)
     send("new_#{model.name.downcase}_path")
@@ -53,5 +71,8 @@ module NavigationHelper
     link_to('Edit', edit_object_path(object))
   end
 
+  def destroy_object_link(object)
+    link_to('Destroy', object, method: :delete, data: { confirm: 'Are you sure?' })
+  end
 
 end
