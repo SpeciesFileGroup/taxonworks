32fce6db0c1c0485bbfa12f287ef60de4cadbc76
mjy <diapriid@gmail.com>
2014-07-23 16:01:13 -0500

    Fixes with name and parent name since squeel is removed.

--- app/models/geographic_area.rb
+++ app/models/geographic_area.rb
@@ -98,20 +98,26 @@ class GeographicArea < ActiveRecord::Base
 
   # TODO: Test
   # A scope. Matches GeographicAreas that have name and parent name.
+  # Like:
+  #   GeographicArea.with_name_and_parent_names(%w{Champaign Illinois})
   scope :with_name_and_parent_name, -> (names) {
-    joins(:parent).
-      where(name: names[0], parent: {name: names[1]})
+    joins('join geographic_areas ga on ga.id = geographic_areas.parent_id').
+    where(name: names[0]).
+    where(['ga.name = ?', names[1] ])
   }
 
   # TODO: Test, or extend a general method
   # Matches GeographicAreas that match name, parent name, parent name
   # Like:
-  #   GeographicArea.with_name_and_parents(%{United\ States Illinois Champaign})
-  scope :with_name_and_parents, -> (names) {
-    joins(parent: :parent).
-      where(name: names[0].to_s, parent: {name: names[1].to_s, parent: {name: names[2]}})
+  #   GeographicArea.with_name_and_parent_names(%w{Champaign Illinois United\ States})
+  scope :with_name_and_parent_names, -> (names) {
+    joins('join geographic_areas ga on ga.id = geographic_areas.parent_id').
+    joins('join geographic_areas gb on gb.id = ga.parent_id').
+    where(name: names[0]).
+    where(['ga.name = ?', names[1] ]).
+    where(['gb.name = ?', names[2] ])
   }
-
+ 
   # Route out to a scope given the length of the
   # search array.  Could be abstracted to 
   # build nesting on the fly if we actually
