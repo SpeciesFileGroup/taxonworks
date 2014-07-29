3fa360d3885940147e2601bd509ae2aaccb2be59
mjy <diapriid@gmail.com>
2014-07-29 14:05:58 -0500

    Remove that nasty recursion.

--- spec/factories/project_source_factory.rb
+++ spec/factories/project_source_factory.rb
@@ -3,7 +3,7 @@
 FactoryGirl.define do
   factory :project_source, traits: [:creator_and_updater] do
     factory :valid_project_source do
-      association :project, factory: :valid_project_source
+      association :project, factory: :valid_project
       association :source, factory: :valid_source
     end
   end
