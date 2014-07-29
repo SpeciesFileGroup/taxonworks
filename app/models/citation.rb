60b98ae84b5af22de6efa3ef9fedf7824e7e4981
mjy <diapriid@gmail.com>
2014-07-29 13:37:05 -0500

    Fixes citation_object_id column to the correct type.  Fixes uniqueness scope for Citation.

--- app/models/citation.rb
+++ app/models/citation.rb
@@ -4,11 +4,10 @@ class Citation < ActiveRecord::Base
   include Housekeeping
 
   belongs_to :citation_object, polymorphic: :true
-
   belongs_to :source, inverse_of: :citations
 
   has_many :citation_topics, inverse_of: :citation
 
   validates_presence_of :citation_object_id, :citation_object_type, :source_id
-  validates_uniqueness_of :citation_object_id, :citation_object_type, :source_id
+  validates_uniqueness_of :source_id, scope: [:citation_object_type, :citation_object_id]
 end
