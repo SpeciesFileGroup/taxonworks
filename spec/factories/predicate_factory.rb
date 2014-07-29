e348ac7c4c554aa4867619d874fe0919ba023636
Jim Tucker <tuckerjd@uiuc.edu>
2014-07-29 14:31:56 -0500

    Human_factory includes association

--- spec/factories/predicate_factory.rb
+++ spec/factories/predicate_factory.rb
@@ -1,8 +1,10 @@
 FactoryGirl.define do
   factory :controlled_vocabulary_term_predicate, class: Predicate, traits: [:housekeeping] do
     factory :valid_controlled_vocabulary_term_predicate do
-      name 'Color'
-      definition 'A food group, like "purple".'
+      # name 'Color'
+      # definition 'A food group, like "purple".'
+      name { Faker::Lorem.word }
+      definition { Faker::Lorem.sentence }
     end
   end
 end
--- spec/factories/source/human_factory.rb
+++ spec/factories/source/human_factory.rb
@@ -2,5 +2,6 @@
 
 FactoryGirl.define do
   factory :source_human, :class => 'Source::Human', traits: [:creator_and_updater] do
+    association :person, factory: :valid_person
   end
 end
