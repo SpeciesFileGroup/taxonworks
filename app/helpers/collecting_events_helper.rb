5a0acfd5ba9bbc4ff099ba549d02827c9cdfc802
mbeckman <mbeckman@illinois.edu>
2014-08-05 10:00:27 -0500

    Change collecting event tag to reference verbatim_locality instead of cached

--- app/helpers/collecting_events_helper.rb
+++ app/helpers/collecting_events_helper.rb
@@ -1,5 +1,6 @@
 module CollectingEventsHelper
   def collecting_event_tag(collecting_event)
-    collecting_event.cached.blank? ? 'CACHED VALUE NOT BUILT - CONTACT ADMIN' : collecting_event.cached
+    # collecting_event.cached.blank? ? 'CACHED VALUE NOT BUILT - CONTACT ADMIN' : collecting_event.cached
+    collecting_event.verbatim_locality
   end
 end
