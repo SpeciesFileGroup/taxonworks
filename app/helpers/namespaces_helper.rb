c2d98128506ff341c3b0d8ad8cbf57f9a54cd8f1
Jim Tucker <tuckerjd@uiuc.edu>
2014-08-18 14:51:49 -0500

    Working on NamespacesHelper tests.

--- app/helpers/namespaces_helper.rb
+++ app/helpers/namespaces_helper.rb
@@ -1,2 +1,21 @@
 module NamespacesHelper
+
+  def self.namespace_tag(namespace)
+    return nil if namespace.nil?
+    namespace.name
+  end
+
+  def namespace_tag(namespace)
+    NamespacesHelper.namespace_tag(namespace)
+  end
+
+  def namespace_link(namespace)
+    return nil if namespace.nil?
+    link_to(NamespacesHelper.namespace_tag(namespace).html_safe, namespace)
+  end
+
+  def namespaces_search_form
+    render('/namespaces/quick_search_form')
+  end
+
 end
