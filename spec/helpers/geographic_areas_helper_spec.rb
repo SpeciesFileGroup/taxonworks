322e9552d32d90b5e5a7600b883c015888cf1984
Jim Tucker <tuckerjd@uiuc.edu>
2014-08-20 10:02:39 -0500

    More group 2 testing.

--- spec/helpers/geographic_areas_helper_spec.rb
+++ spec/helpers/geographic_areas_helper_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 # Specs in this file have access to a helper object that includes
 # the LoansHelper. For example:
 #
-# describe LoansHelper do
+# describe GeographicAreasHelper do
 #   describe "string concat" do
 #     it "concats two strings with spaces" do
 #       expect(helper.concat_strings("this","that")).to eq("this that")
@@ -14,19 +14,19 @@ describe GeographicAreasHelper, :type => :helper do
   context 'a geographic_area needs some helpers' do
     before(:all) {
       @geographic_area  = FactoryGirl.create(:valid_geographic_area)
-      @cvt_name = @geographic_area.name
+      @geographic_area_name = @geographic_area.name
     }
 
     specify '::geographic_area_tag' do
-      expect(GeographicAreasHelper.geographic_area_tag(@geographic_area)).to eq(@cvt_name)
+      expect(GeographicAreasHelper.geographic_area_tag(@geographic_area)).to eq(@geographic_area_name)
     end
 
     specify '#geographic_area_tag' do
-      expect(geographic_area_tag(@geographic_area)).to eq(@cvt_name)
+      expect(geographic_area_tag(@geographic_area)).to eq(@geographic_area_name)
     end
 
     specify '#geographic_area_link' do
-      expect(geographic_area_link(@geographic_area)).to have_link(@cvt_name)
+      expect(geographic_area_link(@geographic_area)).to have_link(@geographic_area_name)
     end
 
     specify "#geographic_area_search_form" do
