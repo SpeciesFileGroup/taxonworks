0dbc4a21672c815b2c74253c9fd04c094de5ed7e
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2014-08-25 17:47:16 -0300

    Further progress with password reset feature

--- spec/views/users/forgot_password.html.erb_spec.rb
+++ spec/views/users/forgot_password.html.erb_spec.rb
@@ -5,8 +5,11 @@ describe 'users/forgot_password', :type => :view do
   it 'renders the email password reset form' do
     render
     
-    assert_select 'form[id=?]', 'forgot_password', 1 do
-      assert_select '[method=post][action=?]', '/user/forgot_password'
+    assert_select 'form', nil, 1 do
+      assert_select '[method=post][action=?]', '/send_password_reset'
+      assert_select 'input[type=?]', 'submit', 1 do
+        assert_select '[value=?]', 'Send e-mail'
+      end
     end
     
     assert_select 'label[for=?]', 'email', 1
---
+++ spec/views/users/send_password_reset.html.erb_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe 'users/send_password_reset', :type => :view do
+        
+  it 'renders the password reset email sent notification page' do
+    render
+  end
+  
+end
