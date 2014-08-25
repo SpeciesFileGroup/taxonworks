7f8685103520fffe097ca1a331ddbf511f4bd890
Jim Tucker <tuckerjd@uiuc.edu>
2014-08-25 11:25:18 -0500

    Feature tests for Repositories, Namespaces, GeographicAreas, including index, list, and show (with previous/next)

--- spec/features/namespaces_spec.rb
+++ spec/features/namespaces_spec.rb
@@ -2,19 +2,57 @@ require 'rails_helper'
 
 describe 'Namespaces', :type => :feature do
 
-  it_behaves_like 'an_administrator_login_required_controller' do 
+  it_behaves_like 'an_administrator_login_required_controller' do
     let(:index_path) { namespaces_path }
     let(:page_index_name) { 'Namespaces' }
-  end 
+  end
 
   describe 'GET /Namespaces' do
-    before {
+    before do
       sign_in_administrator
-      visit namespaces_path }
+      visit namespaces_path
+    end
+    
     specify 'an index name is present' do
       expect(page).to have_content('Namespaces')
     end
   end
+  
+  describe 'GET /namespaces/list' do
+    before do
+      sign_in_user_and_select_project
+      $user_id = 1; $project_id = 1
+      # this is so that there are more than one page of namespaces
+      5.times { FactoryGirl.create(:valid_namespace) }
+      visit '/namespaces/list'
+    end
+
+    specify 'that it renders without error' do
+      expect(page).to have_content 'Listing Namespaces'
+    end
+
+  end
+
+  describe 'GET /namespaces/n' do
+    before {
+      sign_in_user_and_select_project
+      $user_id = 1; $project_id = 1
+      3.times { FactoryGirl.create(:valid_namespace) }
+      all_namespaces = Namespace.all.map(&:id)
+      # there *may* be a better way to do this, but this version *does* work
+      visit "/namespaces/#{all_namespaces[1]}"
+    }
+
+    specify 'there is a \'previous\' link' do
+      expect(page).to have_link('Previous')
+    end
+
+    specify 'there is a \'next\' link' do
+      expect(page).to have_link('Next')
+    end
+
+  end
+
 end
 
 
