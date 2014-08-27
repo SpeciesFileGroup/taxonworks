60f614e30290207e2f7692ba343758f21066f36d
mbeckman <mbeckman@illinois.edu>
2014-08-27 17:43:22 -0500

    Add tests to users spec

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -23,12 +23,13 @@ describe 'Users' do
         expect(page).to have_content("#{@user.email}")
         expect(page).to have_link('Account')
         expect(page).to have_link('Sign out')
+        expect(page).to_not have_link('Administration')
       end
 
       context 'in order to edit self' do
         before {
-          visit user_path(@user)
-          # click_link 'Account'
+          # visit user_path(@user)
+          click_link 'Account'
         }
 
         it 'should have information and links' do
@@ -52,13 +53,36 @@ describe 'Users' do
             expect(page).to have_button('Update User') # where is submit button "Update User"?
           end
 
-          it 'should let user edit their account information' do
+          it 'should let user edit all fields of their account information' do
+            fill_in 'Name', with: 'Modified Name'
             fill_in 'Email', with: 'edit_user_modified@example.com'
             fill_in 'Password', with: '1234ZZZ!'
             fill_in 'Password confirmation', with: '1234ZZZ!'
             click_button 'Update User'
 
             expect(page).to have_css('p.alert.alert--success', 'Changes to your account information have been saved.')
+          end
+
+          before { visit edit_user_path(@user) } # click_link 'Edit account'
+
+          it 'should let user edit only their email address' do
+            fill_in 'Email', with: 'second_edit_user_modified@example.com'
+            # fill_in 'Password', with: '1234ZZZ!'
+            # fill_in 'Password confirmation', with: '1234ZZZ!'
+            click_button 'Update User'
+
+            expect(page).to have_css('p.alert.alert--success', 'Changes to your account information have been saved.')
+          end
+
+          before { visit edit_user_path(@user) } # click_link 'Edit account'
+
+          it 'should let user edit their account information' do
+            fill_in 'Email', with: 'third_edit_user_modified@example.com'
+            fill_in 'Password', with: '1234ZZZ!'
+            fill_in 'Password confirmation', with: nil
+            click_button 'Update User'
+
+            expect(page).to_not have_css('p.alert.alert--success', 'Changes to your account information have been saved.')
           end
         end
       end
@@ -76,8 +100,8 @@ describe 'Users' do
 
     context 'when user is administrator' do
       before {
-        sign_in_administrator      # not a member of a project
-        visit root_path   # Dashboard
+        sign_in_administrator # not a member of a project
+        visit root_path # Dashboard
       }
 
       it 'should have information and actions' do
@@ -89,7 +113,7 @@ describe 'Users' do
 
 
       before {
-        visit users_path  # users index
+        visit users_path # users index
       }
 
       it 'should list users' do
@@ -100,83 +124,83 @@ describe 'Users' do
     end
 
 
-    # describe 'GET /users' do    # this is users/index page? Only signed in admins can see it
-    #   # context 'when administrator' do
-    #   #   before {
-    #   #     sign_in_administrator
-    #   #     visit users_path
-    #   #   }
-    #   #   it 'should list users' do
-    #   #     expect(page).to have_selector('h1', text: 'Users')
-    #   #     expect(page).to have_content("#{@user.email}")
-    #   #     expect(page).to have_link('Administration')
-    #   #   end
-    #   # end
-    #
-    #   context 'when not an administrator' do   # how would a non administrator get to this page?
-    #     before { sign_in_administrator }
-    #     it 'should redirect to dashboard and provide a notice'
-    #   end
-    # end
+# describe 'GET /users' do    # this is users/index page? Only signed in admins can see it
+#   # context 'when administrator' do
+#   #   before {
+#   #     sign_in_administrator
+#   #     visit users_path
+#   #   }
+#   #   it 'should list users' do
+#   #     expect(page).to have_selector('h1', text: 'Users')
+#   #     expect(page).to have_content("#{@user.email}")
+#   #     expect(page).to have_link('Administration')
+#   #   end
+#   # end
+#
+#   context 'when not an administrator' do   # how would a non administrator get to this page?
+#     before { sign_in_administrator }
+#     it 'should redirect to dashboard and provide a notice'
+#   end
+# end
 
-    # describe 'GET /users/:id' do    # this is user show
-    #
-    #   context 'when logged in on dashboard' do
-    #     before {
-    #       sign_in_user
-    #       visit user_path(@user)
-    #     }
-    #
-    #   end
-    #
-    #
-    #   # it 'should show my basic attributes' do
-    #   #   expect(page).to have_link("Edit user information", href: user_information_path)
-    #   #   expect(page).to have_link("View my statistics", href: user_statistics_path)
-    #   #   # what projects I belong, currently logged into projects,
-    #   # end
-    #
-    #
-    #   # context 'when editing self' do         # this is either edit oneself or an admin editing someone else
-    #   #   before {
-    #   #     sign_in_user
-    #   #     visit edit_user_path(@user)
-    #   #   }
-    #   #
-    #   #   it 'should let user edit their account information' do
-    #   #     txt = "Edit user #{@user.id}"
-    #   #     expect(page).to have_selector('h1', txt)
-    #   #     expect(page).to have_title("#{txt} | TaxonWorks")
-    #   #     fill_in 'Email', with: 'edit_user_modified@example.com'
-    #   #     fill_in 'Password', with: '1234ZZZ!'
-    #   #     fill_in 'Password confirmation', with: '1234ZZZ!'
-    #   #     click_button 'Update User'
-    #   #
-    #   #     expect(page).to have_css('p.alert.alert--success', 'Changes to your account information have been saved.')
-    #   #
-    #   #
-    #   #   end
-    #   end
+# describe 'GET /users/:id' do    # this is user show
+#
+#   context 'when logged in on dashboard' do
+#     before {
+#       sign_in_user
+#       visit user_path(@user)
+#     }
+#
+#   end
+#
+#
+#   # it 'should show my basic attributes' do
+#   #   expect(page).to have_link("Edit user information", href: user_information_path)
+#   #   expect(page).to have_link("View my statistics", href: user_statistics_path)
+#   #   # what projects I belong, currently logged into projects,
+#   # end
+#
+#
+#   # context 'when editing self' do         # this is either edit oneself or an admin editing someone else
+#   #   before {
+#   #     sign_in_user
+#   #     visit edit_user_path(@user)
+#   #   }
+#   #
+#   #   it 'should let user edit their account information' do
+#   #     txt = "Edit user #{@user.id}"
+#   #     expect(page).to have_selector('h1', txt)
+#   #     expect(page).to have_title("#{txt} | TaxonWorks")
+#   #     fill_in 'Email', with: 'edit_user_modified@example.com'
+#   #     fill_in 'Password', with: '1234ZZZ!'
+#   #     fill_in 'Password confirmation', with: '1234ZZZ!'
+#   #     click_button 'Update User'
+#   #
+#   #     expect(page).to have_css('p.alert.alert--success', 'Changes to your account information have been saved.')
+#   #
+#   #
+#   #   end
+#   end
 
-    # context 'when editing someone else' do
-    #   context 'and logged in as administrator' do
-    #     before {
-    #       sign_in_administrator
-    #       visit edit_user_path(@user)
-    #     }
-    #     it 'should render' do
-    #     end
-    #   end
-    #
-    #   context 'and not logged in as administrator' do  # where is this prompt?
-    #     it 'should redirect to dashboard and provide a notice'
-    #   end
-    #
-    #   context 'and logged in as a project_administrator' do        # where is this prompt?
-    #     before {
-    #     }
-    #     it 'should redirect to dashboard and provide a notice'
-    #   end
-    # end
+# context 'when editing someone else' do
+#   context 'and logged in as administrator' do
+#     before {
+#       sign_in_administrator
+#       visit edit_user_path(@user)
+#     }
+#     it 'should render' do
+#     end
+#   end
+#
+#   context 'and not logged in as administrator' do  # where is this prompt?
+#     it 'should redirect to dashboard and provide a notice'
+#   end
+#
+#   context 'and logged in as a project_administrator' do        # where is this prompt?
+#     before {
+#     }
+#     it 'should redirect to dashboard and provide a notice'
+#   end
+# end
   end
 end
