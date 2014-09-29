e5b3a9962829f038a910efa42c1945ed6ee8e78f
mjy <diapriid@gmail.com>
2014-09-29 15:44:17 -0500

    Adds hub tab ordering.

--- app/assets/javascripts/from_mx.js
+++ app/assets/javascripts/from_mx.js
@@ -7,22 +7,21 @@ function initialize_js(root) {
   find('*[data-mx-autocomplete-url]').mx_autocompleter();
   find("*[data-observe-field]").mx_field_observer();
   find("*[data-autoquery]").mx_autoquery();
-
+  find("*[data-sortable]").mx_sortable();
 /*
 
 
 
-
+  find("*[data-sortable-table]").mx_sortable_table();
    find("input[data-color-picker]").mx_color_picker();
    find("a[data-ajaxify], input[data-ajaxify]").ajaxify();
    find("*[data-insert-content]").mx_insert_content();
-   find("*[data-sortable]").mx_sortable();
+ 
    find("*[data-tooltip]").mx_tooltip();
    find("*[data-observe-select]").mx_select_observer();
    find("*[data-basic-modal]").basicModal();
    find("*[data-inplace-editor]").mx_inplace_editor();
    find("*[data-sticky-header]").mx_sticky_header();
-   find("*[data-sortable-table]").mx_sortable_table();
    find("*[data-save-warning]").mx_save_warning();
    find("*[data-figure-marker]").mx_figure_marker();
 */
