3dda9605d7453af8a55de5853a58e217e73bdac7
mjy <diapriid@gmail.com>
2014-10-05 23:03:30 -0500

    Image form upload and show now working.  Fixes for type amterials form.

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,13 +1,6 @@
 module ImagesHelper
 
-  def self.image_tag(image)
-    return nil if image.nil?
-    image.to_param
-  end
-
-  def image_tag(image)
-    ImagesHelper.image_tag(image)
-  end
+  # !! Rails already provides image_tag, i.e. it is not required here.
 
   def image_link(image)
     return nil if image.nil?
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ TaxonWorks::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = false # true # was false until oct/2014
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
