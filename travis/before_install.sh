5f77f6075fa6bce4959cb122b526ea28c16467f3
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2014-10-26 10:37:31 -0300

    Second attempt to get Travis work with a web browser

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -2,6 +2,3 @@
 
 sudo apt-get update
 sudo apt-get install -qq libgeos-dev libproj-dev #postgresql-9.1-postgis
-# Allow web browsers in JS feature tests to work
-export DISPLAY=:99.0
-sh -e /etc/init.d/xvfb start
