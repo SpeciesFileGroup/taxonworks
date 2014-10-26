76d8923b042a4de4f443beb6b63c7b0dc29abf0c
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2014-10-26 11:56:29 -0300

    Is Travis already using ramfs?

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -94,7 +94,7 @@ RSpec.configure do |config|
   end
 
   config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.strategy = :truncation
   end
 
   config.before(:each, js: true) do
--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -1,9 +1,9 @@
 RSpec.configure do |config|
-  config.before(:all, type: :helper) do
+  config.before(:example) do
     ProjectsAndUsers.spin_up_projects_users_and_housekeeping
   end 
 
-  config.after(:all, type: :helper) { 
-    ProjectsAndUsers.clean_slate
-  }
+#  config.after(:all, type: :helper) { 
+#    ProjectsAndUsers.clean_slate
+#  }
 end
