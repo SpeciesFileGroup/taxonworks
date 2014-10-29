57afcc45679c15d8702e6e6c9592d55edda226b6
mjy <diapriid@gmail.com>
2014-10-29 15:11:01 -0500

    Stubs related tasks/data rendering tab.  Example variable use for SCSS. Experiment with "panes" i.e. styling divs.

--- app/helpers/workbench/navigation_helper.rb
+++ app/helpers/workbench/navigation_helper.rb
@@ -1,6 +1,7 @@
 # Methods for 1) generating paths; or 2) generating links.
 module Workbench::NavigationHelper
 
+
   def quick_bar
     render(partial: '/navigation/quick_bar') if is_data_controller?
   end
--- config/initializers/constants/navigation.rb
+++ config/initializers/constants/navigation.rb
@@ -1,4 +1,4 @@
 require 'yaml'
 DATA_RELATIONS_FOR_NAVIGATION = YAML.load_file(Rails.root + 'config/interface/data_relations_for_navigation.yml')
 TASK_RELATIONS_FOR_NAVIGATION = YAML.load_file(Rails.root + 'config/interface/task_relations_for_navigation.yml')
-
+RELATED_FOR_NAVIGATION = YAML.load_file(Rails.root + 'config/interface/related_hub_tab.yml')
