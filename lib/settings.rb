76baa92a4634f557e3b38d5286e40524f7df462c
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2014-11-17 19:07:22 -0300

    Added database dumps directory to Settings library

--- lib/settings.rb
+++ lib/settings.rb
@@ -7,6 +7,7 @@ module Settings
   ]
   
   VALID_SECTIONS = [
+    :database_dumps_directory,
     :exception_notification
   ]
     
@@ -14,9 +15,36 @@ module Settings
     invalid_sections = hash.keys - VALID_SECTIONS
     raise "#{invalid_sections} are not valid sections" unless invalid_sections.empty?
 
-    if hash[:exception_notification]
-      settings = hash[:exception_notification]
-      
+    load_exception_notification(config, hash[:exception_notification])
+    load_database_dumps_directory(hash[:database_dumps_directory])
+  end
+  
+  def self.load_from_file(config, path)
+    self.load_from_hash(config, symbolize_keys(YAML.load_file(path)))
+  end
+  
+  def self.db_dumps_dir
+    @@DB_DUMPS_DIR
+  end
+  
+  private
+  def self.symbolize_keys(hash)
+    hash.inject({}) do |h, (k, v)|
+      h[k.is_a?(String) ? k.to_sym : k] = (v.is_a?(Hash) ? symbolize_keys(v) : v)
+      h
+    end
+  end
+  
+  def self.load_database_dumps_directory(path)
+    if !path.nil?
+      full_path = File.absolute_path(path)
+      raise "Directory #{full_path} does not exist" unless Dir.exists?(full_path)
+      @@DB_DUMPS_DIR = full_path
+    end
+  end
+  
+  def self.load_exception_notification(config, settings)
+    if settings      
       missing = EXCEPTION_NOTIFICATION_SETTINGS - settings.keys
       raise "Missing #{missing} settings in exception_notification" unless missing.empty?
       
@@ -26,20 +54,7 @@ module Settings
       raise ":exception_recipients must be an Array" unless settings[:exception_recipients].class == Array
       
       config.middleware.use ExceptionNotification::Rack, email: settings
-    end
-    
-  end
-  
-  def self.load_from_file(config, path)
-    self.load_from_hash(config, symbolize_keys(YAML.load_file(path)))
-  end
-  
-  private
-  def self.symbolize_keys(hash)
-    hash.inject({}) do |h, (k, v)|
-      h[k.is_a?(String) ? k.to_sym : k] = (v.is_a?(Hash) ? symbolize_keys(v) : v)
-      h
-    end
+    end    
   end
 
 end
