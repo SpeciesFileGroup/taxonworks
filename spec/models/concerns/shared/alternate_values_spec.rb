ea2ddc150f2a4779f577d772c8a76a934cc92b26
mjy <diapriid@gmail.com>
2014-12-12 16:13:24 -0600

    Citable instance methods tests.

--- spec/models/concerns/shared/alternate_values_spec.rb
+++ spec/models/concerns/shared/alternate_values_spec.rb
@@ -19,9 +19,9 @@ describe 'AlternateValues', :type => :model do
       class_with_alternate_values.save
 
       misspelled  = AlternateValue::Misspelling.create(
-          value: 'tast1',
-          alternate_value_object_attribute: 'string',
-          alternate_value_object: class_with_alternate_values)
+        value: 'tast1',
+        alternate_value_object_attribute: 'string',
+        alternate_value_object: class_with_alternate_values)
 
       alt_spell  = AlternateValue::AlternateSpelling.create(
         value: 'test1',
@@ -29,46 +29,43 @@ describe 'AlternateValues', :type => :model do
         alternate_value_object: class_with_alternate_values)
 
       trans = AlternateValue::Translation.create(
-          value: 'gibberish',
-          language: FactoryGirl.create(:valid_language),
-          alternate_value_object_attribute: 'string',
-          alternate_value_object: class_with_alternate_values
+        value: 'gibberish',
+        language: FactoryGirl.create(:valid_language),
+        alternate_value_object_attribute: 'string',
+        alternate_value_object: class_with_alternate_values
       )
       abbrv = AlternateValue::Abbreviation.create(
-          value: 'tst',
-          alternate_value_object_attribute: 'string',
-          alternate_value_object: class_with_alternate_values
+        value: 'tst',
+        alternate_value_object_attribute: 'string',
+        alternate_value_object: class_with_alternate_values
       )
     end
 
-    specify 'alternate_valued? with none attached' do
-      expect(instance_with_alternate_values.alternate_valued?).to eq(false)
+    context 'instance methods' do
+      specify 'alternate_valued? with none attached' do
+        expect(instance_with_alternate_values.alternate_valued?).to eq(false)
+      end
+
+      specify 'alternate_valued? with some attached' do
+        instance_with_alternate_values.string = 'Foo'
+        instance_with_alternate_values.alternate_values << AlternateValue::Misspelling.new(alternate_value_object_attribute: :string, value: 'Fu')
+        expect(instance_with_alternate_values.alternate_valued?).to eq(true)
+      end
     end
 
-    specify 'alternate_valued? with some attached' do
-      instance_with_alternate_values.string = 'Foo'
-      instance_with_alternate_values.alternate_values << AlternateValue::Misspelling.new(alternate_value_object_attribute: :string, value: 'Fu')
-      expect(instance_with_alternate_values.alternate_valued?).to eq(true)
+    context 'class methods' do
+      specify '#all_values_for' do
+        expect(class_with_alternate_values.all_values_for('string')).to \
+          eq(['gibberish', 'test1', 'Testing alternate values', 'tst', 'tast1'].sort)
+      end
+
+      specify '.with_alternate_value_on' do
+        # find me all the TestAlternateValue objects that have an alternate value of 'test1' on attribute 'string'
+        expect(TestAlternateValue.with_alternate_value_on('string', 'test1').to_a).to eq([class_with_alternate_values])
+        expect(TestAlternateValue.with_alternate_value_on('string', 'foo').to_a).to eq([])
+      end
     end
 
-
-
-    # TODO add a countering example once we add an alternate value to our instance_with_alternate_values
-
-
-
-    specify '#all_values_for' do
-      expect(class_with_alternate_values.all_values_for('string')).to \
-            eq(['gibberish', 'test1', 'Testing alternate values', 'tst', 'tast1'].sort)
-    end
-
-    specify '.with_alternate_value_on' do
-      # find me all the TestAlternateValue objects that have an alternate value of 'test1' on attribute 'string'
-      expect(TestAlternateValue.with_alternate_value_on('string', 'test1').to_a).to \
-        eq([class_with_alternate_values])
-      expect(TestAlternateValue.with_alternate_value_on('string', 'foo').to_a).to  \
-      eq([])
-    end
   end
 end
 
