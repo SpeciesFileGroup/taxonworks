0c9808b88b42a7ae42683f24ed270226d10f7a5d
mbeckman <mbeckman@illinois.edu>
2015-01-05 16:58:55 -0600

    Add def in_use? to test if current object shared across projects (people, namespaces, serials, sources) is used somewhere (therefore cannot be deleted)

--- app/models/concerns/shared/is_data.rb
+++ app/models/concerns/shared/is_data.rb
@@ -32,4 +32,12 @@ module Shared::IsData
     self.class.ancestors.include?(Shared::Taggable)
   end
 
+  # Also need to check has_one relationships
+  def is_in_use?
+    self.class.reflect_on_all_associations(:has_many).each do |r|
+      return true if self.send(r.name).count > 0
+    end
+    false
+  end
+
 end
