35622c13ec8519224d8a5a55a8bdad5de09d7234
mbeckman <mbeckman@illinois.edu>
2015-01-12 12:30:21 -0600

    Implement shared across project, shared data, user defined _show for serials (need to position "Similar serials" under See)

--- app/helpers/serials_helper.rb
+++ app/helpers/serials_helper.rb
@@ -9,4 +9,11 @@ module SerialsHelper
     SerialsHelper.serial_tag(serial)
   end
 
+  def tasks_serials_serial_similar_link(serial)
+    # options = {}
+    # priority = [serial.container, serial.identifiers.first, serial ].compact.first
+    # link_to('Similar serials', verify_accessions_task_path(by: priority.metamorphosize.class.name.tableize.singularize.to_sym, id: priority.to_param))
+    link_to('Similar serials', tasks_serials_serial_similar_path(@serial))
+  end
+
 end
