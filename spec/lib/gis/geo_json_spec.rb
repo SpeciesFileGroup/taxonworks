c1e3e4c3641a3ada2c054184d3ad0f81d93fba4d
Jim Tucker <tuckerjd@uiuc.edu>
2015-01-20 11:32:59 -0600

    spec for Gis::GeoJSON.feature_collection([collecting_event, georeference, geographic_area, geographic_item])

--- spec/lib/gis/geo_json_spec.rb
+++ spec/lib/gis/geo_json_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Gis::GeoJSON do
 
-  before(:each) do
+  before(:all) do
     clean_slate_geo
     generate_political_areas_with_collecting_events
   end
@@ -43,6 +43,12 @@ describe Gis::GeoJSON do
         expect(Gis::GeoJSON.feature_collection([@ce_p1]).to_json).to eq('{"type":"FeatureCollection","features":[{"type":"Feature","geometry":{"type":"Point","coordinates":[36.5,27.5,0.0]},"properties":{"collecting_event":{"id":4}},"id":0}]}')
       end
 
+    end
+  end
+
+  context 'collecting_event, geographic_area, georeference, geographic_item' do
+    specify 'that feature_collection can take an array of types of objects' do
+      expect(Gis::GeoJSON.feature_collection([@ce_old_boxia_2, @area_q, @gr_m4, @item_p4]).to_json).to eq('{"type":"FeatureCollection","features":[{"type":"Feature","geometry":{"type":"Point","coordinates":[34.5,25.5,0.0]},"properties":{"collecting_event":{"id":19}},"id":0},{"type":"Feature","geometry":{"type":"MultiPolygon","coordinates":[[[[33.0,28.0,0.0],[37.0,28.0,0.0],[37.0,26.0,0.0],[33.0,26.0,0.0],[33.0,28.0,0.0]]]]},"properties":{"geographic_area":{"id":10}},"id":1},{"type":"Feature","geometry":{"type":"Point","coordinates":[33.5,24.5,0.0]},"properties":{"georeference":{"id":10}},"id":2},{"type":"Feature","geometry":{"type":"MultiPolygon","coordinates":[[[[36.0,25.0,0.0],[37.0,25.0,0.0],[37.0,24.0,0.0],[36.0,24.0,0.0],[36.0,25.0,0.0]]]]},"properties":{"geographic_item":{"id":16}},"id":3}]}')
     end
   end
 end
