fdb9111d3909d1038983e7160ab23898ff8a72ab
mbeckman <mbeckman@illinois.edu>
2015-01-22 11:37:01 -0600

    Stupid non conventional naming scheme for data_attributes strikes again.

--- app/helpers/data_attributes_helper.rb
+++ app/helpers/data_attributes_helper.rb
@@ -3,8 +3,8 @@ module DataAttributesHelper
   def add_data_attribute_link(object: object, attribute: nil)
     link_to('Add data attribute', new_data_attribute_path(
       data_attribute: { 
-          data_attribute_object_type: object.class.base_class.name,
-          data_attribute_object_id: object.id})) if object.has_data_attributes?
+          attribute_subject_type: object.class.base_class.name,
+          attribute_subject_id: object.id})) if object.has_data_attributes?
   end
 
   def data_attribute_tag(data_attribute)
