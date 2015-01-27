f49ce4c1587f6f8b056e0999b653b12f580c9f6f
mjy <diapriid@gmail.com>
2015-01-27 12:42:23 -0600

    Tweaks from Tuesday mornings code review sessions.

--- app/controllers/tasks/gis/locality_controller.rb
+++ app/controllers/tasks/gis/locality_controller.rb
@@ -2,50 +2,11 @@ class Tasks::Gis::LocalityController < ApplicationController
   include TaskControllerConfiguration
 
   def nearby
-    @related_routes = UserTasks.related_routes('nearby_locality_task')
-    @collecting_event = CollectingEvent.find(params[:id])
     # @nearby_distance = 5000
-    @nearby_distance  = params['nearby_distance'].to_i
-    if @nearby_distance == 0
-      @nearby_distance = CollectingEvent::NEARBY_DISTANCE
-    end
+    @related_routes = UserTasks.related_routes('nearby_locality_task') 
+    @collecting_event = CollectingEvent.find(params[:id])
 
-    case @nearby_distance.to_s.length
-      when 1..2
-        decade = 10
-      when 3
-        decade = 100
-      when 4
-        decade = 1000
-      when 5
-        decade = 10000
-      when 6
-        decade = 100000
-      when 7
-        decade = 1000000
-      when 8
-        decade = 10000000
-      else
-        decade = 10
-    end
-    digit = (@nearby_distance.to_f / decade.to_f).round
-
-    case digit
-      when 0..1
-        digit = 1
-      when 2
-        digit = 2
-      when 3..5
-        digit = 5
-      when 6..10
-        digit = 1
-        decade *= 10
-    end
-
-    # radio_button('post', 'digit2', '10000')
-    params['digit1'] = digit.to_s
-    params['digit2'] = decade.to_s
-    @nearby_distance = digit * decade
+    @nearby_distance = Utilities::Geo.nearby_from_params(params) 
     @collecting_events = @collecting_event.find_others_within_radius_of(@nearby_distance)
   end
 
--- lib/utilities/geo.rb
+++ lib/utilities/geo.rb
@@ -107,4 +107,47 @@ module Utilities::Geo
     '\\' + sprintf("u%04X", char.ord)
   end
 
+  def self.nearby_from_params(params)
+  nearby_distance  = params['nearby_distance'].to_i
+    if nearby_distance == 0
+      nearby_distance = CollectingEvent::NEARBY_DISTANCE
+    end
+
+    case nearby_distance.to_s.length
+      when 1..2
+        decade = 10
+      when 3
+        decade = 100
+      when 4
+        decade = 1000
+      when 5
+        decade = 10000
+      when 6
+        decade = 100000
+      when 7
+        decade = 1000000
+      when 8
+        decade = 10000000
+      else
+        decade = 10
+    end
+    digit = (nearby_distance.to_f / decade.to_f).round
+
+    case digit
+      when 0..1
+        digit = 1
+      when 2
+        digit = 2
+      when 3..5
+        digit = 5
+      when 6..10
+        digit = 1
+        decade *= 10
+    end
+
+    params['digit1'] = digit.to_s
+    params['digit2'] = decade.to_s
+    nearby_distance = digit * decade
+  end
+
 end
