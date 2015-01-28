47db5d8c8afcf1b5ce0c669a3762798dd1ed4901
eef <eef@illinois.edu>
2015-01-28 13:34:14 -0600

    TW-103 created spec- returns all active record descendants; selects all models that have alternate values; checks that ALTERNATE_VALUES_FOR is set, not empty and only contains valid attribute names.

--- spec/taxon_works_models_spec.rb
+++ spec/taxon_works_models_spec.rb
@@ -4,23 +4,25 @@ require 'rails_helper'
 describe TaxonWorks do
   #let(:model_data) {} # Models that inherit ActiveRecord::Base
 
+  # Since Rails doesn't load classes unless it needs them, so you must eager load them to get all the models.
+  Rails.application.eager_load!
+
   #todo need to fix it so that the error message appears along with the project name.
   context 'Models that have alternate values' do
     ActiveRecord::Base.descendants.each { |model|
+
       if model < Shared::AlternateValues
-        it 'should define the array ALTERNATE_VALUES_FOR' do
-          expect(model::ALTERNATE_VALUES_FOR).to be_an(Array), "model #{model}"
+        it "#{model} should define the array ALTERNATE_VALUES_FOR" do
+          expect(model::ALTERNATE_VALUES_FOR).to be_an(Array), "#{model} is missing ALTERNATE_VALUES_FOR"
         end
 
-        it 'should have 1 or more values in ALTERNATE_VALUES_FOR' do
-          expect(model::ALTERNATE_VALUES_FOR.count).to be > 0, "model #{model}"
+        it "#{model} should have 1 or more values in ALTERNATE_VALUES_FOR" do
+          expect(model::ALTERNATE_VALUES_FOR.count).to be > 0, "#{model} ALTERNATE_VALUES_FOR.count is 0"
         end
 
-        it 'should have only strings in ALTERNATE_VALUES_FOR' do
+        it "#{model} should have only valid columns in ALTERNATE_VALUES_FOR" do
           model::ALTERNATE_VALUES_FOR.each { |val|
-            pending("#{val} should be a column in #{model}")
-            fail()
-            # expect(val).to be_a(???) , "model #{model}"
+            expect(model.attribute_names.include?(val.to_s)).to be_truthy, "#{val} (from ALTERNATE_VALUES_FOR) is not a valid attribute of #{model}"
           }
         end
       end
