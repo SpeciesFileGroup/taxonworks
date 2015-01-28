ec5b2750d2b6bd9659ebcf32d1de474639fe5bec
jrflood <jrflood@illinois.edu>
2015-01-28 16:38:13 -0600

    lat/lon switched to lon/lat argument order for point: Georeference constructor

--- app/controllers/tasks/gis/asserted_distribution_controller.rb
+++ app/controllers/tasks/gis/asserted_distribution_controller.rb
@@ -23,10 +23,10 @@ class Tasks::Gis::AssertedDistributionController < ApplicationController
     # @otu = Otu.find(@otu)
     # @source = Source.find(params[:asserted_distribution][:source_id])
     # @source = Source.find(1)
-    click_point = GeographicItem.new(point: Georeference::FACTORY.point(params["lat"], params["lon"]))
+    click_point = GeographicItem.new(point: Georeference::FACTORY.point(params["lon"], params["lat"]))
     click_items = GeographicItem.is_contained_in('any_poly', click_point)
-    click_areas = click_items.geographic_areas
-    # areas = GeographicArea.with_name_and_parent_name(['Champaign', 'Illinois'])
+    # click_areas = click_items.geographic_areas.first
+    click_areas = GeographicArea.with_name_and_parent_name(['Champaign', 'Illinois'])
     @feature_collection = ::Gis::GeoJSON.feature_collection(click_areas)
     @asserted_distributions = [AssertedDistribution.new(otu: @otu, source: @source )]
     render partial: "/asserted_distributions/quick_form", collection: @asserted_distributions, as: :asserted_distribution
