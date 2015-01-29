afd210d7e51a91a579dac9166e9509244852e3ce
mbeckman <mbeckman@illinois.edu>
2015-01-29 15:11:48 -0600

    Delete scaffolded view bits, fix bug in alt_val form

--- spec/views/combinations/destroy.html.erb_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "combinations/destroy.html.erb", :type => :view do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
--- spec/views/tasks/gis/asserted_distribution/new.html.erb_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-describe 'tasks/gis/asserted_distribution/new', :type => :view do
-  context '' do
-    specify '' do
-      pending
-    end
-  end
-end
