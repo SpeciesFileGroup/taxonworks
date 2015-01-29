787353a244caf299327627877b6100becda3ee30
Jim Tucker <tuckerjd@uiuc.edu>
2015-01-29 09:27:29 -0600

    TW-86 table partial for AssertedDistribution#new with data. Also fixed problem with display_coords

--- app/controllers/tasks/gis/asserted_distribution_controller.rb
+++ app/controllers/tasks/gis/asserted_distribution_controller.rb
@@ -23,13 +23,14 @@ class Tasks::Gis::AssertedDistributionController < ApplicationController
     # @otu = Otu.find(@otu)
     # @source = Source.find(params[:asserted_distribution][:source_id])
     # @source = Source.find(1)
-    click_point = GeographicItem.new(point: Georeference::FACTORY.point(params["lon"], params["lat"]))
+    click_point = GeographicItem.new(point: Georeference::FACTORY.point(params['lon'], params['lat']))
     click_items = GeographicItem.is_contained_in('any_poly', click_point)
-    # click_areas = click_items.geographic_areas.first
-    click_areas = GeographicArea.with_name_and_parent_name(['Champaign', 'Illinois'])
+    click_areas = click_items.map(&:geographic_areas).flatten
+    @click_area_names = click_areas.map(&:name)
+    # click_areas = GeographicArea.with_name_and_parent_name(['Champaign', 'Illinois'])
     @feature_collection = ::Gis::GeoJSON.feature_collection(click_areas)
     @asserted_distributions = [AssertedDistribution.new(otu: @otu, source: @source )]
-    render partial: "/asserted_distributions/quick_form", collection: @asserted_distributions, as: :asserted_distribution
+    render partial: '/asserted_distributions/quick_form', collection: @asserted_distributions, as: :asserted_distribution
 
   end
 end
