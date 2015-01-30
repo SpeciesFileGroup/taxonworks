b67041b5bc30d05238e2f5b8a939a9be218d1794
Jim Tucker <tuckerjd@uiuc.edu>
2015-01-30 10:02:01 -0600

    TW-92 table partial for AssertedDistribution#new with data

--- app/controllers/tasks/gis/asserted_distribution_controller.rb
+++ app/controllers/tasks/gis/asserted_distribution_controller.rb
@@ -22,7 +22,7 @@ class Tasks::Gis::AssertedDistributionController < ApplicationController
     click_point = GeographicItem.new(point: Georeference::FACTORY.point(params['longitude'], params['latitude']))
     click_items = GeographicItem.is_contained_in('any_poly', click_point)
     click_areas = click_items.map(&:geographic_areas).flatten
-    @click_area_names = click_areas.map(&:name)
+    # @click_area_names = click_areas.map(&:name)
     # click_areas = GeographicArea.with_name_and_parent_name(['Champaign', 'Illinois'])
     @feature_collection = ::Gis::GeoJSON.feature_collection(click_areas)
     # @asserted_distributions = [AssertedDistribution.new(otu: @otu, source: @source )]
