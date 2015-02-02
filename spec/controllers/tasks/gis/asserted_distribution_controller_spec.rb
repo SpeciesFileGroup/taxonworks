880ea97efa7b64cce94aafb90a04d2dd69d05cd7
Jim Tucker <tuckerjd@uiuc.edu>
2015-02-02 09:46:54 -0600

    Fixing asserted_distribution_controller_spec.

--- spec/controllers/tasks/gis/asserted_distribution_controller_spec.rb
+++ spec/controllers/tasks/gis/asserted_distribution_controller_spec.rb
@@ -1,17 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe Tasks::Gis::AssertedDistributionController, :type => :controller do
+describe Tasks::Gis::AssertedDistributionController, :type => :controller do
+  before(:each) {
+    sign_in
+  }
+
+  let(:valid_otu) { FactoryGirl.create(:valid_otu) }
+  let(:valid_source) { FactoryGirl.create(:valid_source) }
 
   describe "GET new" do
     it "returns http success" do
-      get :new
+      # the following invocation is here so that the valid_source object gets
+      # instantiated.
+      valid_source.id
+      get 'new', asserted_distribution: {otu_id: valid_otu.id}
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET create" do
     it "returns http success" do
-      get :create
+      get 'create'
       expect(response).to have_http_status(:success)
     end
   end
