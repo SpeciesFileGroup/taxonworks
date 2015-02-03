01202bb796cbc0bd951dd7a6617f09d357cec5ce
eef <eef@illinois.edu>
2015-02-03 16:32:32 -0600

    TW-116 created new collecting event spec

--- spec/features/collecting_events_spec.rb
+++ spec/features/collecting_events_spec.rb
@@ -48,5 +48,26 @@ describe 'CollectingEvents', :type => :feature do
 
   end
 
-end
+  end
+
+  context 'creating a new collecting event' do
+    before {
+      sign_in_user_and_select_project  # logged in and project selected
+      visit collecting_events_path # when I visit the collecting_events_path
+    }
+
+    specify 'it has a new link' do
+      expect(page).to have_link('New')
+    end
+
+    specify 'following the new link & create a new collecting event' do
+      click_link('New') # when I click the new link
+      # I fill out the verbatim_label field with "This is a label.\nAnd a second line."
+       fill_in 'Verbatim label', with: 'This is a label.\nAnd a second line.'
+
+      click_button 'Create Collecting event' # when I click the 'Create Collecting event' button
+      # then I get the message "Collecting event was successfully created"
+      expect(page).to have_content('Collecting event was successfully created.')
+    end
+  end
 end
