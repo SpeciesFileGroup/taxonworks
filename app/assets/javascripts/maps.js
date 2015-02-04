ddc5ee447d33dd0b1eaa35ee6ae792ed96f9d692
jrflood <jrflood@illinois.edu>
2015-02-04 14:37:42 -0600

    TW-93 followup, preparing code for refactoring, eliminating commented code tested to be unused

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -9,6 +9,7 @@ var data; // TaxonWorks jSON data object
 //var gPoints = [];		// arrays for pushing
 //var gLinePoints = []; // google maps graphic
 //var gPolyPoints = []; // objects by type
+//                      // rendered via add_shapes_to_map
 
 // var lPoints = [];
 
@@ -316,7 +317,7 @@ function reset_center_and_bounds() {        // used to
 
 function get_Data() {       //this is the scanner version; no google objects are created
     reset_center_and_bounds();
-    /*		get data object encoded as geoJSON and disseminate to google and leaflet arrays
+    /*		get data object encoded as geoJSON and disseminate to google (deprecated: and leaflet arrays)
      Assumptions:
      data is a hash
      Multi- geometry types are composed of simple (homogeneous) types: Point, LineString, Polygon
@@ -455,167 +456,8 @@ function getTypeData(thisType) {        // this version does not create google o
     ;
 };        //getFeatureData
 
-//function get_data() {         // this version creates the google objects, without preserving features
-//    /*		get data object encoded as geoJSON and disseminate to google and leaflet arrays
-//     Assumptions:
-//     data is a hash
-//     Multi- geometry types are composed of simple (homogeneous) types: Point, LineString, Polygon
-//     these are collected as xPoints[], xLinePoints[], xPolyPoints[]; x = g | l for google and leafletjs respectively
-//     this leaves ambiguous the association of attributes to the objects (e.g., color, etc.)
-//     New realization: there may or may not be GeometryCollections, which may contain any type, including GeometryCollection !  $#!+
-//     */
-//    if (typeof (data) != 'undefined') {
-//        var dataArray = [];
-//        if (data instanceof Array) {
-//        }      // if already an array, then do nothing
-//        else    // convert it to an array
-//        {
-//            dataArray[0] = data;
-//            data = [];
-//            data[0] = dataArray[0];
-//        }
-//        ;
-//        for (var i = 0; i < data.length; i++) {
-//            if (typeof (data[i].type) != "undefined") {
-//                if (data[i].type == "GeometryCollection") {
-//                    for (var j = 0; j < data[i].geometries.length; j++) {
-//                        getTypeData(data[i].geometries[j]);
-//                    }
-//                    ;
-//                }
-//                else {
-//                    getTypeData(data[i]);
-//                }    //data.type
-//            }
-//            ;     //data[i] != undefined
-//        }        // for i
-//    }
-//    ;         //data != undefined
-//};
-//
-//// detect and extract geometry types from higher level enumerator, recursible
-//function getTypeData(thisType) {
-//
-//    if (thisType.type == "GeometryCollection") {
-//        for (var i = 0; i < thisType.geometries.length; i++) {
-//            if (typeof (thisType.geometries[i].type) != "undefined") {
-//                getTypeData(thisType.geometries[i]);		//  recurse if GeometryCollection
-//            }
-//            ;     //thisType != undefined
-//        }
-//        ;       //for i
-//    }
-//    ;
-//
-//    if (thisType.type == "Point") {
-//        xgtlt(thisType.coordinates[0]);
-//        ygtlt(thisType.coordinates[1]); //box check
-//        gPoints.push(new google.maps.LatLng(thisType.coordinates[1], thisType.coordinates[0]));
-//    }
-//    ;
-//
-//    if (thisType.type == "MultiPoint") {
-//        for (var l = 0; l < thisType.coordinates.length; l++) {
-//            //xgtlt(thisType.coordinates[j][k][l][0]);
-//            //ygtlt(thisType.coordinates[j][k][l][1]); //box check
-//            gPoints.push(new google.maps.LatLng(thisType.coordinates[l][1], thisType.coordinates[l][0]));
-//        }
-//        ;
-//    }
-//    ;
-//
-//    if (thisType.type == "LineString") {
-//        var m = gLinePoints.length;
-//        gLinePoints[m] = [];
-//        for (var l = 0; l < thisType.coordinates.length; l++) {
-//            //xgtlt(thisType.coordinates[j][k][l][0]);
-//            //ygtlt(thisType.coordinates[j][k][l][1]); //box check
-//            gLinePoints[m].push(new google.maps.LatLng(thisType.coordinates[l][1], thisType.coordinates[l][0]));
-//        }
-//        ;
-//    }
-//    ;
-//
-//    if (thisType.type == "MultiLineString") {
-//        for (var k = 0; k < thisType.coordinates.length; k++) {   //k enumerates linestrings, l enums points
-//            var m = gLinePoints.length;
-//            gLinePoints[m] = [];
-//            for (var l = 0; l < thisType.coordinates[k].length; l++) {
-//                //xgtlt(thisType.coordinates[j][k][l][0]);
-//                //ygtlt(thisType.coordinates[j][k][l][1]); //box check
-//                gLinePoints[m].push(new google.maps.LatLng(thisType.coordinates[k][l][1], thisType.coordinates[k][l][0]));
-//            }
-//            ;
-//        }
-//        ;
-//    }
-//    ;
-//
-//    if (thisType.type == "Polygon") {
-//        for (var k = 0; k < thisType.coordinates.length; k++) {
-//            // k enumerates polygons, l enumerates points
-//            var m = gPolyPoints.length;
-//            gPolyPoints[m] = [];		//create a new coordinate/point array for this (m/n) polygon
-//            for (var l = 0; l < thisType.coordinates[k].length; l++) {
-//                //xgtlt(thisType.coordinates[j][k][l][0]);
-//                //ygtlt(thisType.coordinates[j][k][l][1]); //box check
-//                gPolyPoints[m].push(new google.maps.LatLng(thisType.coordinates[k][l][1], thisType.coordinates[k][l][0]));
-//            }
-//            ;
-//        }
-//        ;
-//    }
-//    ;
-//
-//    if (thisType.type == "MultiPolygon") {
-//        for (var j = 0; j < thisType.coordinates.length; j++) {		// j iterates over multipolygons   *-
-//            for (var k = 0; k < thisType.coordinates[j].length; k++) {  //k iterates over polygons
-//                var m = gPolyPoints.length;
-//                gPolyPoints[m] = []; //create a new coordinate/point array for this (m/n) polygon
-//                for (var l = 0; l < thisType.coordinates[j][k].length; l++) {
-//                    xgtlt(thisType.coordinates[j][k][l][0]);
-//                    ygtlt(thisType.coordinates[j][k][l][1]); //box check
-//                    gPolyPoints[m].push(new google.maps.LatLng(thisType.coordinates[j][k][l][1], thisType.coordinates[j][k][l][0]));
-//                }
-//                ;
-//            }
-//            ;
-//        }
-//        ;
-//    }
-//    ;
-//};        //getTypeData
-//
-//
-//function createPoint(coords, color) {
-//    return new google.maps.Marker({
-//        position: coords,
-//        map: map
-//    });
-//};
-//
-////		function createLine(coords, color) {
-////			return new google.maps.Polyline({
-////				paths: coords,
-////				geodesic: false,
-////				strokeColor: "#FF0000",
-////				strokeOpacity: 0.5,
-////				strokeWeight: 3
-////			});
-////		};
-//
-//function createPolygon(coords, color) {
-//    return new google.maps.Polygon({
-//        paths: coords,
-//        strokeColor: "black",
-//        strokeOpacity: 0.8,
-//        strokeWeight: 1,
-//        fillColor: color,
-//        fillOpacity: 0.3
-//    });
-//};
 
-function xgtlt(xtest) {
+function xgtlt(xtest) {         // point-wise x-bound extender
 
     if (xtest < 0) {            // if western hemisphere,
         if (xtest > xmaxm) {    // xmaxm initially -180
@@ -635,7 +477,7 @@ function xgtlt(xtest) {
     }
 };
 
-function ygtlt(ytest) {
+function ygtlt(ytest) {         // point-wise y-bound extender
     if (ytest > ymax) {
         ymax = ytest;
     }
