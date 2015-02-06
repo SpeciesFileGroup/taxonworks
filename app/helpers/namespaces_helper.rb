95f83b124738857d340876a7372ec9b50672b7b1
mbeckman <mbeckman@illinois.edu>
2015-02-06 17:03:46 -0600

    Fix namespace name

--- app/helpers/namespaces_helper.rb
+++ app/helpers/namespaces_helper.rb
@@ -18,4 +18,12 @@ module NamespacesHelper
     render('/namespaces/quick_search_form')
   end
 
+  def namespace_from_identifier_tag(identifier)
+    if identifier.respond_to?(:namespace)
+      namespace_tag(identifier.namespace)
+    else
+      nil
+    end
+  end
+
 end
