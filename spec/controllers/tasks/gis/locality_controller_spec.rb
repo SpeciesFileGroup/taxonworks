d18930181508cbd87881b9cdec12511a94c2640a
Jim Tucker <tuckerjd@uiuc.edu>
2015-02-11 09:12:15 -0600

    Converted 1 pending for locality_controller.

--- spec/controllers/tasks/gis/locality_controller_spec.rb
+++ spec/controllers/tasks/gis/locality_controller_spec.rb
@@ -1,11 +1,17 @@
 require 'rails_helper'
 
 describe Tasks::Gis::LocalityController, :type => :controller do
-
+  before(:each) {
+    sign_in
+    generate_ce_test_objects
+  }
+  after(:each) {
+    clean_slate_geo
+  }
   describe "GET nearby" do
     it "returns http success" do
-      pending 'proper specification of the route'
-      get :nearby
+      # pending 'proper specification of the route'
+      get :nearby, {:id => @ce_p1.to_param}
       expect(response).to have_http_status(:success)
     end
   end
