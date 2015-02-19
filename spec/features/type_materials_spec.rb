2f0c12fe7af4763de2fa9a6668dda0dccf28a445
eef <eef@illinois.edu>
2015-02-19 16:27:27 -0600

    TW-129 Not finished Need to get TW-130 resolved first.

--- spec/features/type_materials_spec.rb
+++ spec/features/type_materials_spec.rb
@@ -7,9 +7,9 @@ RSpec.describe "TypeMaterials", :type => :feature do
     let(:page_index_name) { 'Type Materials' }
   end
 
-  context 'when signed in an a project is selected' do
+  context 'when signed in and a project is selected' do
     before {
-      sign_in_user_and_select_project
+      sign_in_user_and_select_project # logged in and project selected
     }
 
     context 'with some records created' do
@@ -57,6 +57,47 @@ RSpec.describe "TypeMaterials", :type => :feature do
 
         end
       end
+    end
+
+    context 'creating a new type_materials' do
+      let(:p) {FactoryGirl.create(:root_taxon_name,
+                                  user_project_attributes(@user, @project).merge(source: nil)) }
+      #  - a root taxon name is created
+      specify 'the type_materials_path has a new link' do
+        visit type_materials_path # when I visit the type_material_path
+        expect(page).to have_link('New') # it has a new link
+      end
+      context 'testing the new type_materials form' do
+      before {
+        @family  = FactoryGirl.create(:valid_taxon_name)
+          #  - a family 'Aidae' with parent root is created  (valid taxon factory default)
+        @genus  = FactoryGirl.create(:iczn_genus, name: 'Bus', parent: @family)
+          #  - a genus 'Bus' with parent 'Aidae' is created
+        @species = FactoryGirl.create(:iczn_species, name: 'bus', parent: @genus)
+          #  - a species 'bus' with parent 'Bus' is created
+        @namespace = FactoryGirl.create(:namespace, name: 'INHS long name', short_name: 'INHSIC')
+          #  - a namespace short name 'INHSIC' is created
+        @specimen = FactoryGirl.create(:valid_specimen)    #  - a specimen is created
+        @specimen.identifiers <<  Identifier.new(type:'Identifier::Local::CatalogNumber',
+                                                 namespace: @namespace,
+                                                 identifier: '1234')
+   #  - an identifier with Namespace 'INHSIC' and identifier '1234' attached to specimen is created
+        @specimen.save!
+      }
+      pending 'filling out the form', js: true do
+        click_link('New') # when I click the new link
+
+        fill_autocomplete('protonym_id_for_type_material', with: 'bus') # I fill out the name field with "bus"
+        # I click 'Bus bus (species)' from drop down list
+        fill_autocomplete('biological_object_id_for_type_material', with: 'INHSIC 1234')
+        # I fill out the Material field with 'INHSIC 1234'
+        #      and I click on the only record returned*
+        select('paratype', from: 'type_material_type_type') # select 'paratype' from the dropdown
+        click_button 'Create Type material' # click the 'Create type material' button
+        # then I get the message "Type material (paratype) for Aus bus was successfully created"
+        expect(page).to have_content('Type material (paratype) for Aus bus was successfully created')
+      end
+    end
     end
   end
 end
