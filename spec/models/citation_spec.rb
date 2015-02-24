fda7baf6fc2bedc23dd3946798cad7212d1ef738
Jim Tucker <tuckerjd@uiuc.edu>
2015-02-24 14:05:57 -0600

    TW-143 constructing spec tests for Citation.

--- spec/models/citation_spec.rb
+++ spec/models/citation_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Citation, :type => :model do
-  let(:citation) {FactoryGirl.build(:citation) }
+  let(:citation) { FactoryGirl.build(:citation) }
 
   context 'associations' do
     context 'belongs_to' do
@@ -16,8 +16,18 @@ describe Citation, :type => :model do
   end
 
   context 'validation' do
-    skip ' for all required fields (:citation_object_id, :citation_object_type, :source_id).'
-    skip 'uniqueness'
+    let(:o) { FactoryGirl.create(:valid_otu) }
+    let(:s) { FactoryGirl.create(:valid_source) }
+    # let(:c1) { FactoryGirl.create(:valid_citation, {otu: o, source: s}) }
+    # let(:c2) { FactoryGirl.build(:valid_citation, otu: o, source: s) }
+
+    pending 'further development'
+    specify 'uniqueness' do
+      # expect(c1.valid?).to be_truthy
+      # expect(c2.valid?).to be_falsey
+      # expect(c2.errors).to include('duplicate')
+    end
+    pending ' for all required fields (:citation_object_id, :citation_object_type, :source_id).'
   end
 
 end
