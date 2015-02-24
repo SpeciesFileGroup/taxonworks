faf0ff49251e99bf856dd20f9b202e285c60db93
mjy <diapriid@gmail.com>
2015-02-24 16:48:50 -0600

    Basic container label tests.

--- app/models/container_label.rb
+++ app/models/container_label.rb
@@ -1,4 +1,14 @@
+# A buffer for holding specifications for physical labels of the container
+#
 class ContainerLabel < ActiveRecord::Base
+
+  acts_as_list scope: [:container]
+
   include Housekeeping
   include Shared::IsData 
+
+  belongs_to :container
+
+  validates_presence_of :container
+  validates_presence_of :label
 end
--- spec/models/container_label_spec.rb
+++ spec/models/container_label_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 describe ContainerLabel, :type => :model do
-  skip "add some examples to (or delete) #{__FILE__}"
+  let(:container_label) {ContainerLabel.new}
+
+  context 'associations' do
+    specify 'container' do
+      expect(container_label.container = Container.new()).to be_truthy
+    end
+  end
+
+  context 'validation' do
+    before { container_label.valid? }
+    
+    specify 'label is required' do
+      expect(container_label.errors.include?(:label)).to be_truthy 
+    end
+
+    specify 'container is required' do
+      expect(container_label.errors.include?(:container)).to be_truthy 
+    end
+
+
+
+  end
+
+
 end
