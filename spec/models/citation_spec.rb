b0d9ba031f9df5447ead9928c81abc7c894fa64e
Jim Tucker <tuckerjd@uiuc.edu>
2015-02-25 08:33:47 -0600

    TW-144 constructing spec tests for Citation. Validation non-blankness of all required fields (citation_object_id, citation_object_type, source_id).

--- spec/models/citation_spec.rb
+++ spec/models/citation_spec.rb
@@ -20,13 +20,22 @@ describe Citation, :type => :model do
     let(:s) { FactoryGirl.create(:valid_source) }
     let(:c1) { FactoryGirl.create(:valid_citation, {citation_object: o, source: s}) }
     let(:c2) { FactoryGirl.build(:valid_citation, citation_object: o, source: s) }
+    let(:c3) { FactoryGirl.build(:citation) }
 
     specify 'uniqueness' do
       expect(c1.valid?).to be_truthy
       expect(c2.valid?).to be_falsey
       expect(c2.errors.messages[:source_id][0]).to eq('has already been taken')
     end
-    pending ' for all required fields (:citation_object_id, :citation_object_type, :source_id).'
+
+    specify 'for all required fields (:citation_object_id, :citation_object_type, :source_id).' do
+      expect(c3.valid?).to be_falsey
+      messages = c3.errors.messages
+      expect(messages[:citation_object_id][0]).to eq('can\'t be blank')
+      expect(messages[:citation_object_type][0]).to eq('can\'t be blank')
+      expect(messages[:source_id][0]).to eq('can\'t be blank')
+    end
+
   end
 
 end
