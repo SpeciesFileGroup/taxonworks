be2e1e00e55328e6329de3a0a0a6c778d270f790
Jim Tucker <tuckerjd@uiuc.edu>
2015-02-25 12:14:40 -0600

    TW-147 constructing spec tests for CitationTopic. Validation of uniqueness of citation_id, topic_id.

--- spec/models/citation_topic_spec.rb
+++ spec/models/citation_topic_spec.rb
@@ -1,30 +1,61 @@
 require 'rails_helper'
 
 describe CitationTopic, :type => :model do
-  let(:citation_topic) { CitationTopic.new }
+  let(:ct) { CitationTopic.new }
+  let(:s) { FactoryGirl.create(:valid_source) }
+  let(:oa) { FactoryGirl.create(:valid_otu) }
+  let(:ob) { FactoryGirl.create(:valid_otu) }
+  let(:ca) { FactoryGirl.create(:valid_citation, {citation_object: oa, source: s}) }
+  let(:cb) { FactoryGirl.create(:valid_citation, {citation_object: ob, source: s}) }
+  let(:ta) { FactoryGirl.create(:valid_topic) }
+  let(:tb) { FactoryGirl.create(:valid_topic) }
+  let(:cta) { FactoryGirl.create(:citation_topic, {topic: ta, citation: ca}) }
+  let(:ctb) { FactoryGirl.build(:citation_topic, {topic: ta, citation: cb}) }
+
 
   context 'citation_topic' do
     context 'validation' do
       before do
-        citation_topic.valid?
+        ct.valid?
       end
 
       context 'required fields' do
+
         specify 'citation_id' do
-          expect(citation_topic.errors.include?(:citation_id)).to be_truthy
+          expect(ct.errors.include?(:citation_id)).to be_truthy
         end
 
         specify 'topic_id' do
-          expect(citation_topic.errors.include?(:topic_id)).to be_truthy
+          expect(ct.errors.include?(:topic_id)).to be_truthy
         end
       end
 
       context 'no duplicates' do
-        # create citation
-        # create topic
-        # create cite_topic
-        # save & reload
-        # try to save again - maybe with added pages
+
+        specify 'of topic' do
+          expect(cta.valid?).to be_truthy
+          # ctb.topic is same as cta.topic
+          expect(ctb.valid?).to be_falsey
+          expect(ctb.errors.messages[:topic_id][0]).to eq('has already been taken')
+          # change the topic to second one
+          ctb.topic = tb
+          # retest
+          expect(ctb.valid?).to be_truthy
+        end
+
+        specify 'of citation' do
+          expect(cta.valid?).to be_truthy
+          # ctb.citation is same as cta.citation
+          ctb.citation = ca
+          expect(ctb.valid?).to be_falsey
+          expect(ctb.errors.messages[:citation_id][0]).to eq('has already been taken')
+          # change the topic to second one
+          ctb.topic = tb
+          # change the citation to second one
+          ctb.citation = cb
+          # retest
+          expect(ctb.valid?).to be_truthy
+        end
       end
     end
   end
