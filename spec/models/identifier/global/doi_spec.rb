874ce036559722593b2898de32bd3cced7a5df93
Jim Tucker <tuckerjd@uiuc.edu>
2015-02-26 09:32:13 -0600

    TW-148 constructing spec tests for Identifier::Global::Doi, Validation that #identifier is validly formatted.

--- spec/models/identifier/global/doi_spec.rb
+++ spec/models/identifier/global/doi_spec.rb
@@ -5,18 +5,26 @@ describe Identifier::Global::Doi, :type => :model do
   context 'DOI.Org' do
     let(:id) { FactoryGirl.build(:identifier_global_doi) }
 
-    specify '#identifier is validly formatted' do
-      # identifier is empty
-      expect(id.valid?).to be_falsey
+    context '#identifier is validly formatted' do
 
-      id.identifier = Faker::Lorem.word
-      expect(id.valid?).to be_falsey
+      specify 'empty' do
+        # identifier is empty
+        expect(id.valid?).to be_falsey
+      end
 
-      id.identifier = '10.12345/TaxonWorks-' + Faker::Number.number(45)
-      expect(id.valid?).to be_truthy
+      specify 'any old word' do
+        id.identifier = Faker::Lorem.word
+        expect(id.valid?).to be_falsey
+      end
+      specify '10.12345/TaxonWorks-' do
+        id.identifier = '10.12345/TaxonWorks-' + Faker::Number.number(45)
+        expect(id.valid?).to be_truthy
+      end
 
-      id.identifier = '10.' + Faker::Number.decimal(4, 4) + '/TaxonWorks-' + Faker::Number.number(45)
-      expect(id.valid?).to be_truthy
+      specify '10.1234.5678/TaxonWorks-' do
+        id.identifier = '10.' + Faker::Number.decimal(4, 4) + '/TaxonWorks-' + Faker::Number.number(45)
+        expect(id.valid?).to be_truthy
+      end
     end
   end
 end
