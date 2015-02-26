dcff3ea66938c0194df3da25605e0d907cde4b9b
Jim Tucker <tuckerjd@uiuc.edu>
2015-02-26 15:28:45 -0600

    TW-152 constructing spec tests for Identifier::Global::Uuid, Validation that #identifier is validly formatted.

--- app/models/identifier/global/uuid.rb
+++ app/models/identifier/global/uuid.rb
@@ -1,2 +1,17 @@
-class Identifier::Global::Uuid < Identifier::Global 
+class Identifier::Global::Uuid < Identifier::Global
+  # Universally Unique IDentifier
+  validate :using_uuid_class
+  # validates :identifier, :format => {:with => /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/i, :message => 'Invalid UUID.'}
+
+  def using_uuid_class
+    unless identifier.nil?
+      retval = true
+      /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/i =~ identifier
+      if $&.nil?
+        errors.add(:identifier, "#{identifier} is not a valid UUID.")
+        retval = false
+      end
+      retval
+    end
+  end
 end
--- spec/models/identifier/global/uuid_spec.rb
+++ spec/models/identifier/global/uuid_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 describe Identifier::Global::Uuid, :type => :model do
-  skip "add some examples to (or delete) #{__FILE__}"
+  context 'UUID' do
+    let(:id) { FactoryGirl.build(:identifier_global_uuid) }
+
+    context '#identifier is validly formatted' do
+
+      specify 'empty' do
+        # identifier is empty
+        expect(id.valid?).to be_falsey
+        expect(id.errors.messages[:identifier][0]).to eq('can\'t be blank')
+      end
+
+      specify 'any old word' do
+        phrase        = Faker::Lorem.word
+        id.identifier = phrase
+        expect(id.valid?).to be_falsey
+        expect(id.errors.messages[:identifier][0]).to eq("#{phrase} is not a valid UUID.")
+      end
+
+      specify 'ca761232ed42DEADBEEF00aa0057b223' do
+        phrase        = 'ca761232ed42DEADBEEF00aa0057b223'
+        id.identifier = phrase
+        expect(id.valid?).to be_falsey
+        expect(id.errors.messages[:identifier][0]).to eq("#{phrase} is not a valid UUID.")
+      end
+
+      specify 'ca761232-ed42-DEAD-BEEF-00aa0057b223' do
+        id.identifier = 'ca761232-ed42-DEAD-BEEF-00aa0057b223'
+        expect(id.valid?).to be_truthy
+      end
+
+    end
+  end
 end
