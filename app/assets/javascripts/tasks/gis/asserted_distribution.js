6bf47045814ad399ecbaed7fb80181250d0e26f1
jrflood <jrflood@illinois.edu>
2015-03-02 10:56:09 -0600

    TW-93 code cleanup, revert asserted distribution to single map

--- app/assets/javascripts/tasks/gis/asserted_distribution.js
+++ app/assets/javascripts/tasks/gis/asserted_distribution.js
@@ -1,15 +1,15 @@
 
 
-function initADmap(canvas1, canvas2)
+function initADmap(canvas)  //1, canvas2)
 {
 
    if (document.getElementById('feature_collection') != null)  {
         var newadwidget = $("#feature_collection");
         var fcdata = newadwidget.data('feature-collection');
 
-        var map = initialize(canvas1, fcdata);
+        var map = initialize(canvas, fcdata);
         add_map_listeners(map);
-        map = initialize(canvas2, fcdata);
-        add_map_listeners(map);
+        //map = initialize(canvas2, fcdata);
+        //add_map_listeners(map);
    }
 }
