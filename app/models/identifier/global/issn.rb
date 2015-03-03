c8ca1e89dd2952dac1169418c6c388ed5a15f832
Jim Tucker <tuckerjd@uiuc.edu>
2015-03-03 10:35:37 -0600

    TW-150 constructing spec tests for Identifier::Global::Issn, Validation that #identifier is validly formatted, correct number of digits, and proper checksum.

--- app/models/identifier/global/issn.rb
+++ app/models/identifier/global/issn.rb
@@ -12,5 +12,42 @@ class Identifier::Global::Issn < Identifier::Global
     The format of the ISSN is an eight digit number, divided by a hyphen into two four-digit numbers.
     The last digit, which may be 0–9 or an X, is a check digit.
 =end
-  validates :identifier, :format => {:with => /\A\d\d\d\d-\d\d\d[\dxX]\z/, :message => 'Invalid ISSN.'}
+  # validates :identifier, :format => {:with => /\A\d\d\d\d-\d\d\d[\dxX]\z/, :message => 'Invalid ISSN.'}
+  validate :using_issn_class
+
+  # Examples from issn.org: ISSN 0317-8471, ISSN 1050-124X
+
+  def using_issn_class
+    retval = true
+    unless identifier.nil?
+      issn = identifier.upcase
+
+      # 'ISSN 1234-567X'
+      /^ISSN (?<part_1>\d{4})-(?<part_2>\d{3})(?<last>.)$/ =~ issn
+
+      if part_1.nil? or part_2.nil? or last.nil?
+        errors.add(:identifier, "'#{identifier}' is an improperly formed ISSN.")
+        return false
+      end
+
+      data = part_1 + part_2
+      sum = 0
+
+      data.each { |c|
+        sum += c.to_i
+      }
+       sum = (sum % 11)
+
+      if sum == 10
+        sum = 'X'
+      end
+
+      # sum and/or last might be an 'X'
+      if sum.to_s == last
+        errors.add(:identifier, "'#{identifier}' has bad check digit.")
+        return false
+      end
+    end
+    retval
   end
+end
--- spec/models/identifier/global/issn_spec.rb
+++ spec/models/identifier/global/issn_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 describe Identifier::Global::Issn, :type => :model do
-  skip "add some examples to (or delete) #{__FILE__}"
+  context 'ISSN' do
+    let(:id) { FactoryGirl.build(:identifier_global_issn) }
+
+    context '#identifier is validly formatted' do
+
+      specify 'empty' do
+        # identifier is empty
+        expect(id.valid?).to be_falsey
+        expect(id.errors.messages[:identifier][0]).to eq('can\'t be blank')
+      end
+
+      specify 'any old word' do
+        phrase        = Faker::Lorem.word
+        id.identifier = phrase
+        expect(id.valid?).to be_falsey
+        expect(id.errors.messages[:identifier][0]).to eq("'#{phrase}' is an improperly formed ISSN.")
+      end
+
+    end
+  end
 end
