5a8815db275fd64ec6e1ce821701d47741e5ffb2
Jim Tucker <tuckerjd@uiuc.edu>
2015-03-03 09:16:25 -0600

    TW-149 constructing spec tests for Identifier::Global::Isbn, Validation that #identifier is validly formatted, correct number of digits, and proper checksum.

--- app/models/identifier/global/isbn.rb
+++ app/models/identifier/global/isbn.rb
@@ -20,8 +20,92 @@ class Identifier::Global::Isbn < Identifier::Global
   # all of this means that it is difficult to validate an ISBN.
 
   # validations contains at least 10 digit & only digits, dashes, or spaces
- validates :identifier, :format => {:with => /\A[\d\s-]*\z/, :message => 'Only digits, spaces and dashes allowed.'}
- # doesn't allow blank identifier, because can only create an object if it has an identifier.
+  # validates :identifier, :format => {:with => /\A[\d\s-]*\z/, :message => 'Only digits, spaces and dashes allowed.'}
+  # doesn't allow blank identifier, because can only create an object if it has an identifier.
+
+  validate :using_isbn_class
+
+  def using_isbn_class
+    retval = true
+    unless identifier.nil?
+      isbn = identifier.upcase
+      # 'ISBN-13: 978-0-596-52068-7'
+      isbn.gsub!('ISBN-10', '')
+      type = $&
+      isbn.gsub!('ISBN-13', '')
+      type ||= $&
+      type.gsub!('ISBN-', '') unless type.nil?
+      isbn.gsub!('ISBN', '')
+      # ': 978-0-596-52068-7'
+      isbn.gsub!(':', '')
+      # ' 978-0-596-52068-7'
+      isbn.strip!
+      # '978-0-596-52068-7' or '978 0 596 52068 7'
+      # if there are spaces or hyphens, check to see that they are in the right places
+      /^(\d{3}[ -]{0,1}){0,1}\d{1,5}[ -]{0,1}\d{1,5}[ -]{0,1}\d{1,5}[ -]{0,1}(?<last>.)$/ =~ isbn
+
+      isbn.gsub!(' ', '')
+      isbn.gsub!('-', '')
+      # if there are *any* non-digits left, it is improperly formed
+      # NB! 'X' is a valid digit in this case
+      /[^\dX]/ =~ isbn
+      unless $&.nil?
+        errors.add(:identifier, "'#{identifier}' is an improperly formed ISBN.")
+        return false
+      end
+      # the final (checksum) digit has alreaqdy been identified, so we need 12 or 9 here
+      /^(?<isbn_13>\d{12})\d$|^(?<isbn_10>\d{9}).$/ =~ isbn
+
+      if ($&.nil?) or
+        (isbn_10.nil? and isbn_13.nil?) or
+        ((isbn_10.nil? and type == '10') or (isbn_13.nil? and type == '13'))
+        errors.add(:identifier, "'#{identifier}' has the wrong number of digits.")
+        return false
+      end
+
+      data  = isbn.slice(0, isbn.size - 1)
+      sum   = 0
+      index = 0
+      # last  = 10 if last == 'X'
+
+      if data.size == 9
+        data.reverse!
+        data.each_char { |c|
+          factor = (index + 2)
+          sum    += factor * c.to_i
+          index  += 1
+        }
+
+        check_byte = 11 - (sum % 11)
+        if check_byte == 10
+          check_byte = 'X'
+        else
+          if check_byte == 11
+            check_byte = 0
+          end
+        end
+      else
+        data.each_char { |c|
+          factor = (((index % 2) * 2) + 1)
+          sum    += factor * c.to_i
+          index  += 1
+        }
+        check_byte = 10 - (sum % 10)
+        if check_byte == 10
+          check_byte = 0;
+        end
+      end
+
+      # check_byte and/or last might be an 'X'
+      if check_byte.to_s != last
+        errors.add(:identifier, "'#{identifier}' has bad check digit.")
+        return false
+      end
+
+    end
+    retval
+  end
+
   def ten_or_thirteen_digits?
     #TODO beth will talk to Jim when she gets back from vacation on how to correctly extract only the digits from the string.
     #count = 0
--- spec/models/identifier/global/isbn_spec.rb
+++ spec/models/identifier/global/isbn_spec.rb
@@ -1,5 +1,109 @@
 require 'rails_helper'
 
 describe Identifier::Global::Isbn, :type => :model do
-  skip "add some examples to (or delete) #{__FILE__}"
+  context 'ISBN' do
+    let(:id) { FactoryGirl.build(:identifier_global_isbn) }
+
+    context '#identifier is validly formatted' do
+
+      specify 'empty' do
+        # identifier is empty
+        expect(id.valid?).to be_falsey
+        expect(id.errors.messages[:identifier][0]).to eq('can\'t be blank')
+      end
+
+      specify 'any old word' do
+        phrase        = Faker::Lorem.word
+        id.identifier = phrase
+        expect(id.valid?).to be_falsey
+        expect(id.errors.messages[:identifier][0]).to eq("'#{phrase}' is an improperly formed ISBN.")
+      end
+
+      specify 'ISBN-10: 978-0-59652-068-7' do
+        phrase        = 'ISBN-10: 978-0-59652-068-7'
+        id.identifier = phrase
+        expect(id.valid?).to be_falsey
+        expect(id.errors.messages[:identifier][0]).to eq("'#{phrase}' has the wrong number of digits.")
+      end
+
+      specify 'ISBN-10: 978-0-596-52068-7' do
+        phrase        = 'ISBN-10: 978-0-596-52068-7'
+        id.identifier = phrase
+        expect(id.valid?).to be_falsey
+        expect(id.errors.messages[:identifier][0]).to eq("'#{phrase}' has the wrong number of digits.")
+      end
+
+      specify 'ISBN-13: 0-596-52068-7' do
+        phrase        = 'ISBN-13: 0-596-52068-7'
+        id.identifier = phrase
+        expect(id.valid?).to be_falsey
+        expect(id.errors.messages[:identifier][0]).to eq("'#{phrase}' has the wrong number of digits.")
+      end
+
+      specify '69780596520687' do
+        phrase        = '69780596520687'
+        id.identifier = phrase
+        expect(id.valid?).to be_falsey
+        expect(id.errors.messages[:identifier][0]).to eq("'#{phrase}' has the wrong number of digits.")
+      end
+
+      # ISBN 978-0-596-52068-7
+      #
+      # ISBN-13: 978-0-596-52068-7
+      #
+      # 978 0 596 52068 7
+      #
+      # 9780596520687
+      #
+      # ISBN-10 0-596-52068-9
+      #
+      # 0-596-52068-9
+
+      specify 'isbn 978-0-596-52068-7' do
+        phrase        = 'isbn 978-0-596-52068-7'
+        id.identifier = phrase
+        expect(id.valid?).to be_truthy
+      end
+
+      specify 'ISBN-13: 978-0-596-52068-7' do
+        id.identifier = 'ISBN-13: 978-0-596-52068-7'
+        expect(id.valid?).to be_truthy
+      end
+
+      specify '978 0 596 52068 7' do
+        id.identifier = '978 0 596 52068 7'
+        expect(id.valid?).to be_truthy
+      end
+
+      specify '9780596520687' do
+        id.identifier = '9780596520687'
+        expect(id.valid?).to be_truthy
+      end
+
+      specify 'ISBN-10 0-596-52068-9' do
+        id.identifier = 'ISBN-10 0-596-52068-9'
+        expect(id.valid?).to be_truthy
+      end
+
+      specify '0-596-52068-9' do
+        id.identifier = '0-596-52068-9'
+        expect(id.valid?).to be_truthy
+      end
+
+      specify '99942-05-96-X' do
+        id.identifier = '99942-05-96-X'
+        expect(id.valid?).to be_truthy
+      end
+
+      specify '99942-05-97-8' do
+        id.identifier = '99942-05-97-8'
+        expect(id.valid?).to be_truthy
+      end
+
+      specify 'Computational complexity by Christos H. Papadimitriou' do
+        id.identifier = '0-2015-3082-1'
+        expect(id.valid?).to be_truthy
+      end
+    end
+  end
 end
