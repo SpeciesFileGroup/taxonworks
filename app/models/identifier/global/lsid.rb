a998f5135ef0cffb8297430687842056fb99bba7
Jim Tucker <tuckerjd@uiuc.edu>
2015-03-05 13:51:24 -0600

    TW-155 constructing spec tests for Identifier::Global::Lsid, Validation that #identifier is validly formatted.

--- app/models/identifier/global/lsid.rb
+++ app/models/identifier/global/lsid.rb
@@ -1,2 +1,12 @@
-class Identifier::Global::Lsid < Identifier::Global 
+class Identifier::Global::Lsid < Identifier::Global
+  validate :using_lsid_class
+
+  def using_lsid_class
+    unless identifier.nil?
+      lsid = identifier
+
+      errors.add(:identifier, "'#{identifier}' is not a valid LSID.")
+      return false
+    end
+  end
 end
--- spec/models/identifier/global/lsid_spec.rb
+++ spec/models/identifier/global/lsid_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 describe Identifier::Global::Lsid, :type => :model do
-  skip "add some examples to (or delete) #{__FILE__}"
+  context 'LSID' do
+    let(:id) { FactoryGirl.build(:identifier_global_lsid) }
+
+    context '#identifier is validly formatted' do
+
+      specify 'empty' do
+        # identifier is empty
+        expect(id.valid?).to be_falsey
+      end
+
+      specify 'any old word' do
+        id.identifier = Faker::Lorem.word
+        expect(id.valid?).to be_falsey
+      end
+    end
+  end
 end
