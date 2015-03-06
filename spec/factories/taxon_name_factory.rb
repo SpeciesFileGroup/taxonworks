d09148632183959c150b2d9dd3e041db30e01d44
mjy <diapriid@gmail.com>
2015-03-05 23:19:32 -0600

    There is NO valid_taxon_name factory any more.  If you want to instantiate a name use valid_protonym or valid_combination.

--- spec/factories/taxon_name_factory.rb
+++ spec/factories/taxon_name_factory.rb
@@ -2,9 +2,12 @@
 
 FactoryGirl.define do
   factory :taxon_name, traits: [:housekeeping] do
-    factory :valid_taxon_name, traits: [:housekeeping, :parent_is_root]  do
-      name 'Adidae'
-      rank_class Ranks.lookup(:iczn, 'Family')
-    end
+    # THERE IS NO :valid_taxon_name, use :valid_protonym or :valid_combination instead
+    #
+#   factory :valid_taxon_name, traits: [:housekeeping, :parent_is_root]  do
+#     name 'Adidae'
+#     type 'Protonym' # !! must be present 
+#     rank_class Ranks.lookup(:iczn, 'Family')
+#   end
   end
 end
