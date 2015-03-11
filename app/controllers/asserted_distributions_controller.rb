329273f222a00689d7f0e47ee320ebf883430044
mbeckman <mbeckman@illinois.edu>
2015-03-11 16:38:26 -0500

    TW-188, add project scoping to set_<model> methods in controllers

--- app/controllers/asserted_distributions_controller.rb
+++ app/controllers/asserted_distributions_controller.rb
@@ -105,7 +105,7 @@ class AssertedDistributionsController < ApplicationController
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_asserted_distribution
-    @asserted_distribution = AssertedDistribution.find(params[:id])
+    @asserted_distribution = AssertedDistribution.with_project_id($project_id).find(params[:id])
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
