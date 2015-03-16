a9d2e5a2d535bcbf172be4d096211b7f6f3120d4
mjy <diapriid@gmail.com>
2015-03-16 13:42:12 -0500

    Cleanup user display and editing to look more like other show/edit/new forms.  Remove unused variables.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,25 +8,21 @@ class UsersController < ApplicationController
   # GET /users
   def index
     @users = User.all
-    @page_title = 'Users'
   end
 
   # GET /signup
   def new
     @user = User.new
-    @page_title = 'Create account'
   end
 
   # GET /users/:id
   def show
     @user = User.find(params[:id])
-    @page_title = "User #{@user.id}"
   end
 
   # GET /users/:id/edit
   def edit
     @user = User.find(params[:id])
-    @page_title = "Edit user #{@user.id}"
   end
 
   # POST /users
@@ -37,7 +33,6 @@ class UsersController < ApplicationController
       # TODO: Email the user their information.
       redirect_to root_path
     else
-      @page_title = 'Create account'
       render 'new'
     end
   end
@@ -49,7 +44,6 @@ class UsersController < ApplicationController
       flash[:success] = 'Changes to your account information have been saved.'
       redirect_to @user
     else
-      @page_title = "Edit user #{@user.id}"
       render 'edit'
     end
   end
