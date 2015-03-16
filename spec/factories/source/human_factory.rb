707f86723d6a76fa37ed9d3591e12009db5f2b24
mjy <diapriid@gmail.com>
2015-03-16 13:37:18 -0500

    Valid factory for human sources.

--- spec/factories/source/human_factory.rb
+++ spec/factories/source/human_factory.rb
@@ -1,6 +1,11 @@
 # TODO: Unsatisfactory validation, should (only?) invoke with acceptes_nested_attributes_for
 FactoryGirl.define do
   factory :source_human, class: Source::Human, traits: [:creator_and_updater] do
-    factory :valid_source_human 
+    factory :valid_source_human do |source|
+
+      after(:build) do |source|
+        source.people << FactoryGirl.create(:valid_person) 
+      end
+    end 
   end
 end
