849846d8c848508624e779f0f614bddcae202de9
mjy <diapriid@gmail.com>
2015-03-16 13:36:38 -0500

    Specs and caching for human sources.

--- app/models/source/human.rb
+++ app/models/source/human.rb
@@ -4,30 +4,26 @@ class Source::Human < Source
 
   has_many :roles
   has_many :source_source_roles, class_name: 'SourceSource', as: :role_object
-  has_many :people, through: :source_source_roles #, source: :person
+  has_many :people, through: :source_source_roles 
 
-  #TODO set cached values!
-  before_save :set_cached_values
+  accepts_nested_attributes_for :people
+
+  validate :at_least_one_person_is_provided
 
   def authority_name
-    # TODO need to use full last name with suffix not just last_name
-    case  self.people.count
-      when 0
-        return ('')  # is this even a valid case?
-       when 1
-        return (people[0].last_name)
-      else
-        p_array = Array.new
-        for i in 0..(self.people.count-1) do
-          p_array.push(self.people[i].last_name)
-        end
-        p_array.to_sentence(:last_word_connector =>' & ')
-    end
+    last_names = people.collect{|p| p.last_name}
+    last_names.to_sentence(last_word_connector: ' & ', two_words_connector: ' & ')
   end
+
   protected
 
-  def set_cached_values
-    self.cached_author_string = authority_name
-    # TODO what should the cached (full ref string) be?
+  def set_cached
+    self.cached = self.authority_name 
+  end
+
+  def at_least_one_person_is_provided
+    if self.people.size == 0 # size not count
+      errors.add(:base, 'at least one person must be provided')     
+    end
   end
 end
--- spec/models/source/human_spec.rb
+++ spec/models/source/human_spec.rb
@@ -1,55 +1,95 @@
 require 'rails_helper'
 
 describe Source::Human, :type => :model do
+  let(:tom) { Person.create(last_name: 'Smith', first_name: 'Tom')  }
+  let(:sue) { Person.create(last_name: 'Sunny', first_name: 'Sue')  }
+  let(:franklin) { Person.create(last_name: 'Roosevelt', first_name: 'Franklin')  }
 
   let(:source_human) { Source::Human.new }
 
   context 'associations' do
-
-    skip 'does authority work correctly?' # all author last names.
     context 'has_many' do
-      skip 'with multiple authors - is the ordering correct'
-
-    end
-
-    context 'belongs_to' do
-
-    end
-
-    context 'has_one' do
-      context 'person' do
-        specify 'responds to :people' do
-          expect(source_human).to respond_to(:people)
-        end
-
-        specify 'add a person, save the source' do
-          p1 = FactoryGirl.create(:valid_person)
-          expect(source_human.save).to be_truthy
-          expect(source_human.people << p1).to be_truthy   #must have source & person must be saved before << operator
-          expect(source_human.people.first).to eq(p1.becomes(Person::Unvetted))
-          expect(source_human.save).to be_truthy
-        end
+      specify '#people' do
+        expect(source_human.people << Person.new).to be_truthy
       end
-      specify 'source_source' do
-        expect(source_human).to respond_to(:source_source_roles)
+
+      specify '#source_source_roles' do
+        expect(source_human.source_source_roles << SourceSource.new).to be_truthy
       end
-      specify 'role' do
-        expect(source_human).to respond_to(:roles)
+
+      specify '#roles' do
+        expect(source_human.roles << Role.new).to be_truthy
       end
     end
-
   end
 
   context 'validation' do
-
+    before { source_human.save }
+    specify 'at least one person is provided' do
+      expect(source_human.errors.include?(:base)).to be_truthy 
+    end
   end
 
-  specify 'create person with role of human source'
+  context 'usage' do
+    specify 'add a person, save the source' do
+      expect(source_human.people << tom).to be_truthy   #must have source & person must be saved before << operator
+      expect(source_human.people.first).to eq(tom.metamorphosize)
+      expect(source_human.save).to be_truthy
+    end
 
-  specify 'a source_human should have a person'
-  specify 'a source_source role should have a source'
+    specify 'add multiple people, save the source' do
+      expect(source_human.people << tom).to be_truthy 
+      expect(source_human.people << sue).to be_truthy 
+      expect(source_human.save).to be_truthy
+      expect(source_human.people.count).to eq(2)
+    end
+  end
 
-  skip 'are the cached values set correctly'
+  context '#cached'  do
+    before {source_human.people << tom }
+    specify '#cached is set before_save' do
+      source_human.save!
+      expect(source_human.cached.blank?).to be_falsey
+    end
 
+    specify '#cached is composed of last names' do
+      source_human.save!
+      expect(source_human.cached).to eq(tom.last_name)
+    end
+
+    specify '#cached with two people is joined with &' do
+      source_human.people << sue
+      source_human.save
+      expect(source_human.cached).to eq(tom.last_name + ' & ' + sue.last_name)
+    end 
+
+    specify '#cached with three (or more) people comman with & between last two' do
+      source_human.people << sue
+      source_human.people << franklin 
+      source_human.save
+      expect(source_human.cached).to eq(tom.last_name + ', ' + sue.last_name + ' & ' + franklin.last_name)
+    end 
+  end
+
+  context 'position' do
+    before {  
+      source_human.people << tom
+      source_human.people << sue 
+      source_human.people << franklin 
+      source_human.save
+    }
+    specify 'is calculated' do
+      expect(source_human.source_source_roles.first.position).to eq(1)
+      expect(source_human.source_source_roles.second.position).to eq(2)
+      expect(source_human.source_source_roles.third.position).to eq(3)
+    end
+
+    specify 'orders people' do
+      source_human.reload
+      expect(source_human.people.first.first_name).to eq('Tom')
+      expect(source_human.people.second.first_name).to eq('Sue')
+      expect(source_human.people.third.first_name).to eq('Franklin')
+    end
+  end
 
 end
