146346fc6651f6d8d4221c530ed5f8623b67dd85
Jim Tucker <tuckerjd@uiuc.edu>
2015-03-17 11:32:32 -0500

    TW-155 lsid model spec - test that #identifier is validly formatted. Model and spec.

--- app/models/identifier/global/lsid.rb
+++ app/models/identifier/global/lsid.rb
@@ -1,12 +1,29 @@
 class Identifier::Global::Lsid < Identifier::Global
+  # urn:lsid:Orthoptera.speciesfile.org:TaxonName:1
+=begin
+  • "URN"
+  • "LSID"
+  • authority identification
+  • namespace identification
+  • object identification
+  • optionally: revision identification. If revision field is omitted then the trailing colon is also omitted.
+=end
   validate :using_lsid_class
 
   def using_lsid_class
+    retval = true
     unless identifier.nil?
-      lsid = identifier
+      lsid = identifier.split(':')
+      # this is a test of http://rubular.com/regexes/13295
+      /SID=([\s\S]*?)LSID=([\s\S]*?)Auth=([\s\S]*)/ =~ identifier
 
-      errors.add(:identifier, "'#{identifier}' is not a valid LSID.")
-      return false
+      unless lsid.length.between?(5, 6)
+        unless lsid[0].upcase == 'URN' and lsid[1].upcase == 'LSID'
+          errors.add(:identifier, "'#{identifier}' is not a valid LSID.")
+          retval = false
+        end
+      end
     end
+    return retval
   end
 end
--- spec/models/identifier/global/lsid_spec.rb
+++ spec/models/identifier/global/lsid_spec.rb
@@ -9,12 +9,35 @@ describe Identifier::Global::Lsid, :type => :model do
       specify 'empty' do
         # identifier is empty
         expect(id.valid?).to be_falsey
+        expect(id.errors.messages[:identifier][0]).to eq('can\'t be blank')
       end
 
       specify 'any old word' do
-        id.identifier = Faker::Lorem.word
+        phrase        = Faker::Lorem.word
+        id.identifier = phrase
         expect(id.valid?).to be_falsey
+        expect(id.errors.messages[:identifier][0]).to eq("'#{phrase}' is not a valid LSID.")
       end
+
+      specify 'urn:lsid:orthoptera.speciesfile.org:Taxonname:1' do
+        phrase        = 'urn:lsid:orthoptera.speciesfile.org:Taxonname:1'
+        id.identifier = phrase
+        expect(id.valid?).to be_truthy
+      end
+
+      specify 'ISBN-10: 978-0-59652-068-7' do
+        phrase        = 'ISBN-10: 978-0-59652-068-7'
+        id.identifier = phrase
+        expect(id.valid?).to be_falsey
+        expect(id.errors.messages[:identifier][0]).to eq("'#{phrase}' is not a valid LSID.")
+      end
+
+      specify 'urn:lsid:orthoptera.speciesfile.org:Taxonname:1:revision=44' do
+        phrase        = 'urn:lsid:orthoptera.speciesfile.org:Taxonname:1:revision=44'
+        id.identifier = phrase
+        expect(id.valid?).to be_truthy
+      end
+
     end
   end
 end
