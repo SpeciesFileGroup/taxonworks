4efe77846637628f351b28d30bde7be3e3c991ab
mjy <diapriid@gmail.com>
2015-03-25 15:16:10 -0500

    Cleanup.

--- app/controllers/collecting_events_controller.rb
+++ app/controllers/collecting_events_controller.rb
@@ -28,7 +28,6 @@ class CollectingEventsController < ApplicationController
   # POST /collecting_events.json
   def create
     @collecting_event = CollectingEvent.new(collecting_event_params)
-    # byebug
     respond_to do |format|
       if @collecting_event.save
         format.html { redirect_to @collecting_event, notice: 'Collecting event was successfully created.' }
