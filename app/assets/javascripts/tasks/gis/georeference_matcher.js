9e8c3e307df049ea58ff803115574194ef265eae
jrflood <jrflood@illinois.edu>
2015-04-13 15:00:58 -0500

    TW-236, TW-226 configure link click visibility of georeferences forms - connectivity works,  next current georeference.

--- app/assets/javascripts/tasks/gis/georeference_matcher.js
+++ app/assets/javascripts/tasks/gis/georeference_matcher.js
@@ -51,5 +51,58 @@ $(document).on('ready page:load', function () {
 
         return;
     });
+
+    $(".filter-gr").click(function (event) {
+
+        // unhide this form
+        $("#_filter_gr_form").removeAttr("hidden");
+        // hide everything else:  tag; drawing; recent;
+        $("#_tag_gr_form").attr("hidden", true)
+        $("#_draw_gr_form").attr("hidden", true)
+        $("#_recent_gr_form").attr("hidden", true)
+        event.preventDefault();
+
+        return;
+    });
+    $(".tag-gr").click(function (event) {
+
+        // unhide this form
+        $("#_tag_gr_form").removeAttr("hidden");
+        // hide everything else: filter; drawing; recent;
+        $("#_filter_gr_form").attr("hidden", true)
+        $("#_draw_gr_form").attr("hidden", true)
+        $("#_recent_gr_form").attr("hidden", true)
+
+        event.preventDefault();
+
+        return;
+    });
+    $(".draw-gr").click(function (event) {
+
+        // unhide this form
+        $("#_draw_gr_form").removeAttr("hidden");
+        // hide everything else: filter; tag; recent;
+        $("#_filter_gr_form").attr("hidden", true)
+        $("#_tag_gr_form").attr("hidden", true)
+        $("#_recent_gr_form").attr("hidden", true)
+
+        event.preventDefault();
+
+        return;
+    });
+    $(".recent-gr").click(function (event) {
+
+        // unhide this form
+        $("#_recent_gr_form").removeAttr("hidden");
+        // hide everything else: filter; tag; drawing;
+        $("#_filter_gr_form").attr("hidden", true)
+        $("#_tag_gr_form").attr("hidden", true)
+        $("#_draw_gr_form").attr("hidden", true)
+
+        event.preventDefault();
+
+        return;
+    });
+
     return;
 });
--- app/helpers/match_georeferences_helper.rb
+++ app/helpers/match_georeferences_helper.rb
@@ -11,4 +11,16 @@ module MatchGeoreferencesHelper
   def link_to_recent_ce
     link_to('recent', '', class: 'recent-ce')
   end
+  def link_to_filter_gr
+    link_to('filter', '', class: 'filter-gr')
+  end
+  def link_to_tag_gr
+    link_to('tag', '', class: 'tag-gr')
+  end
+  def link_to_draw_gr
+    link_to('draw', '', class: 'draw-gr')
+  end
+  def link_to_recent_gr
+    link_to('recent', '', class: 'recent-gr')
+  end
 end
