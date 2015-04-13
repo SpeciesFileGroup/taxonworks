e878ce217d9b278f134de83c7dfa6819074b6888
jrflood <jrflood@illinois.edu>
2015-04-13 14:50:49 -0500

    TW-236, TW-226 configure link click visibility of collecting events forms - connectivity works, georeferences next.

--- app/assets/javascripts/tasks/gis/georeference_matcher.js
+++ app/assets/javascripts/tasks/gis/georeference_matcher.js
@@ -3,11 +3,11 @@ $(document).on('ready page:load', function () {
     $(".filter-ce").click(function (event) {
 
         // unhide this form
-        $(this).removeAttr("hidden");
+        $("#_filter_ce_form").removeAttr("hidden");
         // hide everything else:  tag; drawing; recent;
-        $(".tag-ce").attr("hidden", true)
-        $(".draw-ce").attr("hidden", true)
-        $(".recent-ce").attr("hidden", true)
+        $("#_tag_ce_form").attr("hidden", true)
+        $("#_draw_ce_form").attr("hidden", true)
+        $("#_recent_ce_form").attr("hidden", true)
         event.preventDefault();
 
         return;
@@ -15,11 +15,11 @@ $(document).on('ready page:load', function () {
     $(".tag-ce").click(function (event) {
 
         // unhide this form
-        $(this).removeAttr("hidden");
+        $("#_tag_ce_form").removeAttr("hidden");
         // hide everything else: filter; drawing; recent;
-        $(".filter-ce").attr("hidden", true)
-        $(".draw-ce").attr("hidden", true)
-        $(".recent-ce").attr("hidden", true)
+        $("#_filter_ce_form").attr("hidden", true)
+        $("#_draw_ce_form").attr("hidden", true)
+        $("#_recent_ce_form").attr("hidden", true)
 
         event.preventDefault();
 
@@ -28,11 +28,11 @@ $(document).on('ready page:load', function () {
     $(".draw-ce").click(function (event) {
 
         // unhide this form
-        $(this).removeAttr("hidden");
+        $("#_draw_ce_form").removeAttr("hidden");
         // hide everything else: filter; tag; recent;
-        $(".filter-ce").attr("hidden", true)
-        $(".tag-ce").attr("hidden", true)
-        $(".recent-ce").attr("hidden", true)
+        $("#_filter_ce_form").attr("hidden", true)
+        $("#_tag_ce_form").attr("hidden", true)
+        $("#_recent_ce_form").attr("hidden", true)
 
         event.preventDefault();
 
@@ -41,11 +41,11 @@ $(document).on('ready page:load', function () {
     $(".recent-ce").click(function (event) {
 
         // unhide this form
-        $(this).removeAttr("hidden");
+        $("#_recent_ce_form").removeAttr("hidden");
         // hide everything else: filter; tag; drawing;
-        $(".filter-ce").attr("hidden", true)
-        $(".tag-ce").attr("hidden", true)
-        $(".draw-ce").attr("hidden", true)
+        $("#_filter_ce_form").attr("hidden", true)
+        $("#_tag_ce_form").attr("hidden", true)
+        $("#_draw_ce_form").attr("hidden", true)
 
         event.preventDefault();
 
