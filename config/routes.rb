e6e240cd3469161604589a465b5c1c78d79083f5
Jim Tucker <tuckerjd@uiuc.edu>
2015-04-17 08:04:50 -0500

    Removing comments

--- config/routes.rb
+++ config/routes.rb
@@ -241,16 +241,6 @@ TaxonWorks::Application.routes.draw do
       get 'drawn_georeferences'
 
       post 'batch_create'
-
-      # get 'filtered_collecting_events', as: 'filtered_collecting_events'
-      # get 'recent_collecting_events', as: 'recent_collecting_events'
-      # get 'tagged_collecting_events', as: 'tagged_collecting_events'
-      # get 'drawn_collecting_events', as: 'drawn_collecting_events'
-      #
-      # get 'filtered_georeferences', as: 'filtered_georeferences'
-      # get 'recent_georeferences', as: 'recent_georeferences'
-      # get 'tagged_georeferences', as: 'tagged_georeferences'
-      # get 'drawn_georeferences', as: 'drawn_georeferences'
     end
 
     scope :serials, controller: 'tasks/serials/similar' do
