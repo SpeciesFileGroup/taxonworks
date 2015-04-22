d2778a06aa65c3065b6e50e7e02bd7592771e17b
mjy <diapriid@gmail.com>
2015-04-22 11:02:21 -0500

    Fix bad attr_, need to check what point of this class is.

--- lib/dynamic_content.rb
+++ lib/dynamic_content.rb
@@ -3,13 +3,12 @@ class DynamicContent
 
   DYNAMIC_CONTENT_CLASSES = self.descendants
 
-  attr_accessible :otu
+  attr_accessor :otu
 
   def initialize(otu: nil, keywords: nil)
     @otu = otu
     return false if otu.nil?
   end 
-
 
   # !! All view related properties should be defined in a helper/view
   # !! e.g. we shouldn't need a render_header? instance method here
@@ -21,7 +20,6 @@ class DynamicContent
   def public_header
     'No header defined for this section type.  Contact your developer.'
   end
-
 
   def data
     []    
