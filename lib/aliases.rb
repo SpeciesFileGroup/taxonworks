afa13d1d064d1d315e87d6edbc87e96befc2d5fd
mjy <diapriid@gmail.com>
2015-04-22 10:58:07 -0500

    Remove old experiment.

--- lib/aliases.rb
+++
@@ -1,4 +0,0 @@
-module Aliases
-  Lot = CollectionObject::BiologicalCollectionObject::PhysicalBiologicalObject::Lot
-  Specimen = CollectionObject::BiologicalCollectionObject::PhysicalBiologicalObject::Specimen 
-end
