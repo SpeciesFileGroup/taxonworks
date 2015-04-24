0d8f32a14d146c3aa9929f82b19f56cef084d9bb
eef <eef@illinois.edu>
2015-04-24 12:45:31 -0500

    TW-241 - checkpoint alternate value spec - fixed the alternate value form to only show the white-listed values in ALTERNATE_VALUES_FOR

--- spec/features/alternate_values_spec.rb
+++ spec/features/alternate_values_spec.rb
@@ -48,4 +48,31 @@ describe "AlternateValues", :type => :feature do
     describe 'the partial form rendered in context of NEW/EDIT on some other page' do
     end
   end
+
+  context 'create an alternate value' do
+    pending 'for Source::Bibtex', js: true do
+=begin
+    with a Source::BibTeX created (containing at least title)
+    when I show that record
+       then there is a link "Add alternate value"
+         when I click that link
+            and I select "title" from Alternate value object attribute
+            and I fill out value
+            and Type has "abbreviation" selected
+            then when I click "Create Alternate value"
+                I see the message "Alternate value was successfully created."
+                   I see the alternate value rendered under Annotations/Alternate value in the show
+=end
+      sign_in_user_and_select_project
+      # create a Source::Bibtex record
+      src_bibtex = factory_girl_create_for_user(:soft_valid_bibtex_source_article, @user)
+      src_bibtex.save!
+      visit (source_path(src_bibtex))
+      expect(page).to have_link('Add alternate value')
+      expect(page.has_selector?('h3', 'Annotations')).to be_falsey
+      expect(page.has_selector?('h4', 'Alternate values')).to be_falsey
+      click_link('Add alternate value')
+
+    end
+  end
 end
