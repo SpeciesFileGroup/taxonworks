e4997653d15fe4be63e5bfc1665e1064dd2ff537
jrflood <jrflood@illinois.edu>
2015-05-01 14:57:18 -0500

    TW-229 georeference selector map transfers selected feature to Current Georeference map

--- app/assets/javascripts/tasks/gis/georeference_matcher.js
+++ app/assets/javascripts/tasks/gis/georeference_matcher.js
@@ -269,91 +269,100 @@ function add_click_services_to_match_georeferences_map(map, event) {     // clic
     // sets mouseover/mouseout behavior for buttons via forEach(function(feature))
     //   in map.data corresponding to "button_nnnn" where nnnn is area id
     // resizes, recenters map based on new features
-    /*    var mapLatLng = event.latLng;
+/*    var mapLatLng = event.latLng;
 
-     //$("#map_coords").html('Coordinates: Latitude = ' + mapLatLng.lat().toFixed(6) + ' , Longitude = ' + mapLatLng.lng().toFixed(6)) ;
-     //if(new_asserted_distribution_check_preemption()) {return;};
-     //
-     //$("#latitude").val(mapLatLng.lat());
-     //$("#longitude").val(mapLatLng.lng());
+    //$("#map_coords").html('Coordinates: Latitude = ' + mapLatLng.lat().toFixed(6) + ' , Longitude = ' + mapLatLng.lng().toFixed(6)) ;
+    //if(new_asserted_distribution_check_preemption()) {return;};
+    //
+    //$("#latitude").val(mapLatLng.lat());
+    //$("#longitude").val(mapLatLng.lng());
 
-     $.get( 'generate_choices', $('#_select_gr_form').serialize(), function(local_data) {
-     $("#choices").html(local_data['html']);      // local_data contains html(selection forms)
-     // quick_new_asserted_distribution_form and feature collection geoJSON
-     map.data.forEach(function(feature) {map.data.remove(feature);});    // clear the map.data
+    $.get( 'generate_choices', $('#_select_gr_form').serialize(), function(local_data) {
+            $("#choices").html(local_data['html']);      // local_data contains html(selection forms)
+            // quick_new_asserted_distribution_form and feature collection geoJSON
+            map.data.forEach(function(feature) {map.data.remove(feature);});    // clear the map.data
 
-     map.data.addGeoJson(local_data['feature_collection']);      // add the geo features corresponding to the forms
+            map.data.addGeoJson(local_data['feature_collection']);      // add the geo features corresponding to the forms
 
-     // select buttons of the form: "button_nnnn" with jquery, and bind the listener events
+            // select buttons of the form: "button_nnnn" with jquery, and bind the listener events
 
-     $("[id^=button_]").mouseover(function() {       // set mouseover for each area
-     var this_id = this.id;
-     var area_id = this_id.slice(7,this_id.length);      // 'button_'.length, 'button_abc...xyz'.length
-     map.data.forEach(function(feature) {        // find by geographic area id
-     //this_feature = map.data.getFeatureById(jj); // not used, 0-reference fault in google maps
-     this_feature = feature;
-     this_property = this_feature.getProperty('geographic_area');
-     if(this_property.id != area_id) {
-     //map.data.getFeatureById(01).getProperty('geographic_area')
-     //map.data.overrideStyle(this_feature, {fillColor: '#000000'});     //  black
-     map.data.overrideStyle(this_feature, {strokeWeight: 0.0});       // erase borders
-     map.data.overrideStyle(this_feature, {fillOpacity: 0.0});       // transparent
-     }
-     if(this_property.id == area_id) {
-     map.data.overrideStyle(this_feature, {fillColor: '#FF0000'});  //  red
-     map.data.overrideStyle(this_feature, {strokeWeight: 2});       //embolden borders
-     map.data.overrideStyle(this_feature, {fillOpacity: 1.0});       // transparent
-     }
-     });
-     })
+            $("[id^=button_]").mouseover(function() {       // set mouseover for each area
+                var this_id = this.id;
+                var area_id = this_id.slice(7,this_id.length);      // 'button_'.length, 'button_abc...xyz'.length
+                map.data.forEach(function(feature) {        // find by geographic area id
+                    //this_feature = map.data.getFeatureById(jj); // not used, 0-reference fault in google maps
+                    this_feature = feature;
+                    this_property = this_feature.getProperty('geographic_area');
+                    if(this_property.id != area_id) {
+                        //map.data.getFeatureById(01).getProperty('geographic_area')
+                        //map.data.overrideStyle(this_feature, {fillColor: '#000000'});     //  black
+                        map.data.overrideStyle(this_feature, {strokeWeight: 0.0});       // erase borders
+                        map.data.overrideStyle(this_feature, {fillOpacity: 0.0});       // transparent
+                    }
+                    if(this_property.id == area_id) {
+                        map.data.overrideStyle(this_feature, {fillColor: '#FF0000'});  //  red
+                        map.data.overrideStyle(this_feature, {strokeWeight: 2});       //embolden borders
+                        map.data.overrideStyle(this_feature, {fillOpacity: 1.0});       // transparent
+                    }
+                });
+            })
 
-     $("[id^=button_]").mouseout(function() {        // set mouseout for each area (condensed)
-     var this_id = this.id;                      // var this since it goes out of scope with .forEach
-     map.data.forEach(function(feature) {        // find by geographic area id
-     if(feature.getProperty('geographic_area').id == this_id.slice(7,this_id.length)) { map.data.revertStyle(); }
-     // 'button_'.length, 'button_abc...xyz'.length
-     });
-     })
+            $("[id^=button_]").mouseout(function() {        // set mouseout for each area (condensed)
+                var this_id = this.id;                      // var this since it goes out of scope with .forEach
+                map.data.forEach(function(feature) {        // find by geographic area id
+                    if(feature.getProperty('geographic_area').id == this_id.slice(7,this_id.length)) { map.data.revertStyle(); }
+                    // 'button_'.length, 'button_abc...xyz'.length
+                });
+            })
 
-     var data = local_data['feature_collection'];
-     var bounds = {};
-     getData(data, bounds);
-     var center_lat_long = get_window_center(bounds);
-     map.setCenter(center_lat_long);
-     map.setZoom(bounds.gzoom);
-     //map.fitBounds(bounds.box);
-     },
-     'json' // I expect a JSON response
-     );
-     */
-}
+            var data = local_data['feature_collection'];
+            var bounds = {};
+            getData(data, bounds);
+            var center_lat_long = get_window_center(bounds);
+            map.setCenter(center_lat_long);
+            map.setZoom(bounds.gzoom);
+            //map.fitBounds(bounds.box);
+        },
+        'json' // I expect a JSON response
+    );
+*/}
 
 
 function add_match_georeferences_map_listeners(map) {      // 4 listeners, one for map as a whole 3 for map.data features
     // When the user clicks, set 'isColorful', changing the color of the feature.
-    map.data.addListener('click', function (event) {
-        if (event.feature.getProperty('isColorful')) {           // reset selected color if
+    map.data.addListener('click', function(event) {
+        if(event.feature.getProperty('isColorful')) {           // reset selected color if
             event.feature.setProperty('isColorful', false);     // previously selected
             event.feature.setProperty('fillColor', "#440000");  // to dimmer red
         }
         else {      // if not already "Colorful", make it so
             event.feature.setProperty('isColorful', true);
             event.feature.setProperty('fillColor', "#CC0000");  //brighter red
-        }
-        ;
+    // selectable area has been clicked, get the feature
+            var selected_feature_georeference_id = event.feature["A"].georeference.id;
+          //  alert(selected_feature_georeference_id + '\n' + $("#_select_gr_form").data('feature-collection').features[0].properties['georeference'].id) ;  //$("#_select_gr_form").data('feature-collection').features[0].properties['georeference'].id
+            var feature_collection = $("#_select_gr_form").data('feature-collection');
+            for (var i = 0; i < feature_collection.features.length; i++) {
+                if (selected_feature_georeference_id == feature_collection.features[i].properties['georeference'].id) {
+                    var fc = {"type": "FeatureCollection", "features": []};
+                    fc.features.push(feature_collection.features[i]);
+                    initializeMap("select_gr_canvas", fc);
+                }
+            }
+        };
         add_click_services_to_match_georeferences_map(map, event);
     });
 
     // When the user hovers, tempt them to click by outlining the letters.
     // Call revertStyle() to remove all overrides. This will use the style rules
     // defined in the function passed to setStyle()
-    map.data.addListener('mouseover', function (event) {
+    map.data.addListener('mouseover', function(event) {
         map.data.revertStyle();
         map.data.overrideStyle(event.feature, {fillColor: '#880000'});  // mid-level red
         map.data.overrideStyle(event.feature, {strokeWeight: 2});       //embolden borders
     });
 
-    map.data.addListener('mouseout', function (event) {
+    map.data.addListener('mouseout', function(event) {
         map.data.revertStyle();
     });
 
