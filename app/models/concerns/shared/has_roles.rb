f37fe42edbfb0517ef01253ecda818b1759d782d
eef <eef@illinois.edu>
2015-06-03 11:02:48 -0500

    TW-305 Role cascade delete working

--- app/models/concerns/shared/has_roles.rb
+++ app/models/concerns/shared/has_roles.rb
@@ -2,7 +2,7 @@ module Shared::HasRoles
 
   extend ActiveSupport::Concern
   included do
-    has_many :roles, as: :role_object
+    has_many :roles, as: :role_object, dependent: :destroy
   end
 
   def has_roles?
--- spec/models/concerns/shared/has_roles_spec.rb
+++ spec/models/concerns/shared/has_roles_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-describe 'Citable', :type => :model do
+describe 'Has_roles', :type => :model do
   let(:class_with_roles) { TestHasRole.new } 
  
   context "associations" do
@@ -11,6 +11,16 @@ describe 'Citable', :type => :model do
   context "methods" do
     specify "has_roles?" do
       expect(class_with_roles.has_roles?).to eq(false)
+    end
+    context 'object with roles on destroy' do
+      specify 'attached roles are destroyed' do
+        expect(Role.count).to eq(0)
+        class_with_roles.roles << FactoryGirl.build(:valid_role)
+        class_with_roles.save
+        expect(Role.count).to eq(1)
+        expect(class_with_roles.destroy).to be_truthy
+        expect(Role.count).to eq(0)
+      end
     end
   end
 end
