85fc114e0412ec6136c96b1820ebe85ddaa4a650
mjy <diapriid@gmail.com>
2015-06-11 14:17:40 -0500

    Dummy crop method, for future reference.

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -11,4 +11,14 @@ module ImagesHelper
     render('/images/quick_search_form')
   end
 
+
+  # Clip the image with css clip.
+  # image is a Image instance
+  # <%= css_clip(image: @depiction.image, height: 200, width: 0, x: 5, y: 20)  -%>
+  def css_clip(image: nil, size: :medium, x: 0, y: 0, height: nil, width: nil) 
+    content_tag :div do
+      image_tag(image.image_file(size), style: "position:absolute; left: 100px; top: 50px; clip: rect(#{x}px,#{y}px,#{height}px,#{width}px);", width: image.width, height: image.height).html_safe
+    end
+  end  
+
 end
