c0ee6c673ed221df0fdd0b04a696f45fd19ab399
jrflood <jrflood@illinois.edu>
2015-06-29 15:26:48 -0500

    TW-317 map generation widget, broken: gets google undefined error in initializeMap (??); no features in feature collections either

--- app/assets/javascripts/tasks/gis/otu_distribution_data.js
+++ app/assets/javascripts/tasks/gis/otu_distribution_data.js
@@ -5,11 +5,11 @@ _init_otu_distribution_data_widget = function init_otu_distribution_data() {
     if (document.getElementById('_displayed_distribution_form') != null) {
       var newfcdata = $("#_displayed_distribution_form");
       var fcdata = newfcdata.data('feature-collection');
-      var map_canvas = newfcdata.data('map-canvas')
-      initializeMap(map_canvas, fcdata);
+      var map_canvas = newfcdata.data('map-canvas');
+      var otu_map = initializeMap(map_canvas, fcdata);
       //add_map_listeners();
     }
   }
-}
+};
 $(document).ready(_init_otu_distribution_data_widget);
 $(document).on("page:load", _init_otu_distribution_data_widget);
