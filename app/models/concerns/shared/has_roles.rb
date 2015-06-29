a8a7d5b0fd205dedfec0c437120154ed9ee1d08b
mjy <diapriid@gmail.com>
2015-06-29 11:39:21 -0500

    Add generic accepts_nested_attributes_for roles relationship to the has_roles concerns.

--- app/models/concerns/shared/has_roles.rb
+++ app/models/concerns/shared/has_roles.rb
@@ -3,6 +3,9 @@ module Shared::HasRoles
   extend ActiveSupport::Concern
   included do
     has_many :roles, -> { order(:position) }, as: :role_object, dependent: :destroy
+    has_many :people, through: :roles
+
+    accepts_nested_attributes_for :roles 
   end
 
   def has_roles?
