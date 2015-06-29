6349d58cdd1c6575f61330db91110d10cc732101
Jim Tucker <tuckerjd@uiuc.edu>
2015-06-29 08:40:02 -0500

    altered view.

--- app/helpers/taxon_determinations_helper.rb
+++ app/helpers/taxon_determinations_helper.rb
@@ -2,7 +2,7 @@ module TaxonDeterminationsHelper
 
   def taxon_determination_tag(taxon_determination) 
     return nil if taxon_determination.nil?
-    "foo" 
-    #otu_tag(taxon_determination.otu) + " for " + object_tag(taxon_determination.biological_collection_object.metamorphosize) 
+    # "foo"
+    otu_tag(taxon_determination.otu) + " for " + object_tag(taxon_determination.biological_collection_object.metamorphosize)
   end
 end
