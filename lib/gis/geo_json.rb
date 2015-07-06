ce8f0d8ad65a8394c6130a9dec1e56b3084ab231
Jim Tucker <tuckerjd@uiuc.edu>
2015-07-06 09:29:43 -0500

    TW-317 Changed distribution from FeatureCollection to Aggregation.

--- lib/gis/geo_json.rb
+++ lib/gis/geo_json.rb
@@ -37,7 +37,34 @@ module Gis::GeoJSON
      }
 =end
 
+# @param objects [Array feature_collections]
+# @return [JSON object]
+  def self.aggregation(objects, properties = nil)
+    count  = 0
+    result = {
+      'type'     => 'Aggregation',
+      'features' => []
+    }
+    objects.each_with_index do |object, i|
+      unless object.nil?
+        if object.class.to_s == 'Hash'
+          json = object.merge('id' => i + 1)
+        else
+          json = object.to_geo_json_feature.merge('id' => i + 1) # offset by one, since google getFeatureById(0) fails
+        end
+        result['features'].push(json)
+        count += 1
+      end
+    end
+    unless properties.nil?
+      result['properties'] = {properties.to_s => count}
+    end
+    result
+  end
+
+
 # @param objects [Array of instances that respond to .to_geo_json_feature]
+# @return [geo_JSON object]
   def self.feature_collection(objects, properties = nil)
     count  = 0
     result = {
