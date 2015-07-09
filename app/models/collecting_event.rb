46f08bfb0378476d984e4eac8bff3e7d56d8928b
mjy <diapriid@gmail.com>
2015-07-09 17:11:39 -0500

    Temporarily disable cached value setting.

--- app/models/collecting_event.rb
+++ app/models/collecting_event.rb
@@ -98,15 +98,18 @@ class CollectingEvent < ActiveRecord::Base
 
   accepts_nested_attributes_for :verbatim_data_georeference
 
-  before_validation :check_verbatim_geolocation_uncertainty,
-                    :check_date_range,
-                    :check_elevation_range
-  before_save :set_cached
+  validate :check_verbatim_geolocation_uncertainty,
+    :check_date_range,
+    :check_elevation_range
+
+  # TODO: write specs, this is failing when GeographicArea is set.
+  # before_save :set_cached
   before_save :set_times_to_nil_if_form_provided_blank
 
   validates_uniqueness_of :md5_of_verbatim_label, scope: [:project_id], unless: 'verbatim_label.blank?'
   validates_presence_of :verbatim_longitude, if: '!verbatim_latitude.blank?'
   validates_presence_of :verbatim_latitude, if: '!verbatim_longitude.blank?'
+  
   validates :geographic_area, presence: true, allow_nil: true
 
   validates :time_start_hour,
@@ -750,13 +753,11 @@ TODO: @mjy: please fill in any other paths you can think of for the acquisition 
 
   def set_cached
     string = nil
-
     if verbatim_label.blank?
       string = [country_name, state_name, county_name, "\n", verbatim_locality, start_date, end_date, "\n", verbatim_collectors].compact.join
     else
       string = [verbatim_label, print_label, document_label].compact.first
     end
-
     string      = "[#{self.id.to_param}]" if string.strip.length == 0
     self.cached = string
   end
