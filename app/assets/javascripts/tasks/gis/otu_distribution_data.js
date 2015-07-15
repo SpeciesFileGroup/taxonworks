91bb602a1f1190ff0f5f8c8f57a03df20217ffe4
jrflood <jrflood@illinois.edu>
2015-07-15 16:43:57 -0500

    TW-317 map generation widget,  mouseover in FireFox resolved by amending google version from v=3.exp to v=3.

--- app/assets/javascripts/tasks/gis/otu_distribution_data.js
+++ app/assets/javascripts/tasks/gis/otu_distribution_data.js
@@ -1,7 +1,5 @@
-/*var _init_otu_distribution_data_widget;
-paradigm shift for mouseover fault isolation
-_init_otu_distribution_data_widget = */
-function init_otu_distribution_data() {
+var _init_otu_distribution_data_widget;
+_init_otu_distribution_data_widget = function init_otu_distribution_data() {
   if ($('#otu_distribution_widget').length) {
     if (document.getElementById('_displayed_distribution_form') != null) {
       var newfcdata = $("#_displayed_distribution_form");
@@ -12,20 +10,20 @@ function init_otu_distribution_data() {
       var otu_map = initializeComplexMap(map_canvas, fcdata);
       add_otu_distribution_data_listeners(otu_map);
 
-      //$('input[type="checkbox"]').change(function () {    // on checkbox change, reset to original feature data
-      //  otu_map = initializeComplexMap(map_canvas, newfcdata.data('feature-collection'));
-      //  add_otu_distribution_data_listeners(otu_map);
-      //});
+      $('input[type="checkbox"]').change(function () {    // on checkbox change, reset to original feature data
+        otu_map = initializeComplexMap(map_canvas, newfcdata.data('feature-collection'));
+        add_otu_distribution_data_listeners(otu_map);
+      });
     }
   }
 };
 
 function add_otu_distribution_data_listeners(map) {
 
-  map.data.addListener('click', function (event) {
-    map.data.revertStyle();
-    map.data.overrideStyle(event.feature, {fillOpacity: 0.5});
-  });
+  //map.data.addListener('click', function (event) {
+  //  map.data.revertStyle();
+  //  map.data.overrideStyle(event.feature, {fillOpacity: 0.5});
+  //});
 
   map.data.addListener('mouseover', function (event) {     // interim color shift on mousover paradigm changed to opacity
     map.data.revertStyle();
@@ -39,11 +37,11 @@ function add_otu_distribution_data_listeners(map) {
   map.data.addListener('mouseout', function (event) {
     map.data.revertStyle();
     map.data.overrideStyle(event.feature, {fillOpacity: 0.3});  // dimmer
-    //$("#displayed_distribution_style").html('');
+    $("#displayed_distribution_style").html('');
   });
 }
 
-//$(document).ready(_init_otu_distribution_data_widget);
-//$(document).on("page:load", _init_otu_distribution_data_widget);
+$(document).ready(_init_otu_distribution_data_widget);
+$(document).on("page:load", _init_otu_distribution_data_widget);
 
-google.maps.event.addDomListener(window, 'load', init_otu_distribution_data);
+//google.maps.event.addDomListener(window, 'load', init_otu_distribution_data);
