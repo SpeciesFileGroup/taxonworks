3c27dfca3526e3f0a9e4f409af824020fbf8b725
eef <eef@illinois.edu>
2015-07-17 17:11:57 -0500

    Fixed bug that wasn't setting cached values correctly in bibtex role authors

--- app/models/role/source_role.rb
+++ app/models/role/source_role.rb
@@ -3,4 +3,9 @@ class Role::SourceRole < Role
   include Shared::SharedAcrossProjects
 
   validates :project_id, absence: true
+  after_save :update_source_cached
+
+  def update_source_cached
+    self.role_object.save
+  end
 end
