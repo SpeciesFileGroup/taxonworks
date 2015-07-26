1b5ad6972a9722180745e35722f1791ec574853f
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2015-07-26 18:52:14 -0300

    Made migrations compatible with Rails 4.2 (without affecting the original behaviour)

--- db/migrate/20130808202626_create_geographic_items.rb
+++ db/migrate/20130808202626_create_geographic_items.rb
@@ -2,11 +2,11 @@ class CreateGeographicItems < ActiveRecord::Migration
   def change
     create_table :geographic_items do |t|
       t.string :a_name, :geographic => false
-      t.point :a_point, :geographic => true, :has_z => true, :has_m => true
+      t.st_point :a_point, :geographic => true, :has_z => true, :has_m => true
       t.line_string :a_simple_line, :geographic => true
       t.line_string :a_complex_line, :geographic => true
       t.line_string :a_linear_ring, :geographic => true
-      t.polygon :a_polygon, :geographic => true
+      t.st_polygon :a_polygon, :geographic => true
       t.multi_point :a_multi_point, :geographic => true, :has_z => true, :has_m => true
       t.multi_line_string :a_multi_line_string, :geographic => true
       t.multi_polygon :a_multi_polygon, :geographic => true
--- db/migrate/20130822185529_tweak_points2.rb
+++ db/migrate/20130822185529_tweak_points2.rb
@@ -6,9 +6,9 @@ class TweakPoints2 < ActiveRecord::Migration
                                       :multi_point,
                                       :multi_line_string,
                                       :multi_polygon
-    add_column :geographic_items, :point, :point, :geographic => true, :has_z => true, :has_m => true
+    add_column :geographic_items, :point, :st_point, :geographic => true, :has_z => true, :has_m => true
     add_column :geographic_items, :line_string, :line_string, :geographic => true, :has_z => true, :has_m => true
-    add_column :geographic_items, :polygon, :polygon, :geographic => true, :has_z => true, :has_m => true
+    add_column :geographic_items, :polygon, :st_polygon, :geographic => true, :has_z => true, :has_m => true
     add_column :geographic_items, :multi_point, :multi_point, :geographic => true, :has_z => true, :has_m => true
     add_column :geographic_items, :multi_line_string, :multi_line_string, :geographic => true, :has_z => true, :has_m => true
     add_column :geographic_items, :multi_polygon, :multi_polygon, :geographic => true, :has_z => true, :has_m => true
