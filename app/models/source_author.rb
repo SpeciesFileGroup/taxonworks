2505ebeb71a18727b6184ff74af9b959656dbd83
mjy <diapriid@gmail.com>
2015-07-29 16:33:40 -0500

    Remove redundant line.

--- app/models/source_author.rb
+++ app/models/source_author.rb
@@ -1,6 +1,5 @@
 # Namespaced at root for convenience sake (so we don't have to reference it as Role::SourceRole::SourceAuthor)
 class SourceAuthor < Role::SourceRole
-  include Housekeeping::Users
 
   def self.human_name
     'Author'
