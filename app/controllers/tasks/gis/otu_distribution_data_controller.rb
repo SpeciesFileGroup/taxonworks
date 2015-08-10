688057cb5d904547de18e260d6e0cddccbf31bdb
Jim Tucker <tuckerjd@uiuc.edu>
2015-08-10 13:22:06 -0500

    TW-337 Taxon name distribution.

--- app/controllers/tasks/gis/otu_distribution_data_controller.rb
+++ app/controllers/tasks/gis/otu_distribution_data_controller.rb
@@ -1,6 +1,6 @@
 class Tasks::Gis::OtuDistributionDataController < ApplicationController
- 
- # show_for_otu 
+
+  # show_for_otu
   def show
     id = params[:id]
     if id.blank?
@@ -9,12 +9,14 @@ class Tasks::Gis::OtuDistributionDataController < ApplicationController
       @otu = Otu.find(id)
     end
     @distribution = Distribution.new(otus: [@otu])
-    render 'show'
+    @type_tag     = 'Otu'
+    render('show')
   end
 
   def show_for_taxon_name
     @distribution = Distribution.new(otus: Otu.for_taxon_name(TaxonName.find(params[:id])))
-    render '/tasks/gis/otu_distribution_data/show'
+    @type_tag     = 'Taxon name'
+    render('show')
   end
 
 
