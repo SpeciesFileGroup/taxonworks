d1e8fe2f0499a899c84a11c5ff88e8f6dc186d6c
Jim Tucker <tuckerjd@uiuc.edu>
2015-08-21 12:49:02 -0500

    More work for geometery-less feature.

--- app/helpers/tasks/gis/otu_distribution_data_helper.rb
+++ app/helpers/tasks/gis/otu_distribution_data_helper.rb
@@ -1,7 +1,15 @@
 module Tasks::Gis::OtuDistributionDataHelper
   def otu_aggregation(otu)
-    retval = otu.distribution_geoJSON
+    retval                      = otu.distribution_geoJSON
     retval['properties']['tag'] = object_tag(otu)
     retval
+  end
+
+  def without_shape?(distributions)
+    count = 0
+    distributions.each { |dist|
+      count += 1 if dist.geographic_area.geographic_items.count == 0
+    }
+    ", #{count} without shape" if count > 0
   end
 end
