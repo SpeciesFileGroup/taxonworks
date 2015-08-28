3581a5652d1386ad593bdcb4bebc56efa81d2550
jrflood <jrflood@illinois.edu>
2015-08-28 13:39:28 -0500

    Round trip to topics/lookup_topic.  Sortable now working. Submit fails during rendering of app/views/otu_page_layouts/show. This uses app/data/shared/project/show and fails because no related data is defined.  Use its own show??

--- app/helpers/otu_page_layouts_helper.rb
+++ app/helpers/otu_page_layouts_helper.rb
@@ -1,2 +1,12 @@
 module OtuPageLayoutsHelper
+  def self.otu_page_layout_tag(otu_page_layout)
+    return nil if otu_page_layout.nil?
+    otu_page_layout.name
+  end
+
+  def otu_page_layout_tag(otu_page_layout)
+    OtuPageLayoutsHelper.otu_page_layout_tag(otu_page_layout)
+  end
+
+
 end
