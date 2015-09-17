5e8019adde349478bda3b357b2a451edca1bcb62
mbeckman <mbeckman@illinois.edu>
2015-09-17 10:08:37 -0500

    Bug in lot.rb fixed (via Matt)

--- app/models/lot.rb
+++ app/models/lot.rb
@@ -8,6 +8,6 @@ class Lot < CollectionObject::BiologicalCollectionObject
   protected
 
   def size_of_total
-    errors.add(:total, "total must be > 1") if self.total.nil? || not(self.total > 1) 
+    errors.add(:total, "total must be > 1") if self.total.nil? || !(self.total > 1)
   end
 end
