14fee0c143831494224d8f09e96993e4b820c404
mjy <diapriid@gmail.com>
2015-09-23 09:45:58 -0500

    Add missing biocuration classifications helper tag method.

--- app/helpers/biocuration_classifications_helper.rb
+++ app/helpers/biocuration_classifications_helper.rb
@@ -1,2 +1,6 @@
 module BiocurationClassificationsHelper
+  def biocuration_classification_tag(biocuration_classification)
+    return nil if biocuration_classification.nil?
+    biocuration_classification.biocuration_class.name
+  end
 end
