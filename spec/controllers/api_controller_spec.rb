863bad020b430ac8eb4287233e8ed666ba4ceb39
mjy <diapriid@gmail.com>
2015-10-20 13:13:47 -0500

    Stubs.

---
+++ spec/controllers/api_controller_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe ApiController, :type => :controller do
+# before(:each) {
+#   sign_in
+# }
+
+  context 'sessions_current_user is set' do
+    specify 'with a token in header' do
+    end
+    specify 'with a token in params' do
+  end
+
+  context 'with invalid token' do
+    specify 'with a token in header' do
+    end
+
+    specify 'with a token in params' do
+    end
+  end
+
+
+end
+
+
+
+ 
+
+
+
+end
