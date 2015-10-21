857264d346c920f193095d5230d65e12a532c147
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2015-10-21 15:55:17 -0500

    Small adjustment

--- spec/features/api_spec.rb
+++ spec/features/api_spec.rb
@@ -9,7 +9,7 @@ describe ApiController, :type => :feature do
     let(:user) { FactoryGirl.create(:valid_user, :user_valid_token) }
 
       it 'shows a JSON success response' do
-        visit api_path + ".json?project_id=1&token=#{user.api_access_token}" 
+        visit api_path + "?project_id=1&token=#{user.api_access_token}" 
           # NOTE: using "find" because js:true causes the response to be wrapped inside an html document
           expect(JSON.parse(find('pre').text)).to eq({ "success" => true })
       end
@@ -18,7 +18,7 @@ describe ApiController, :type => :feature do
     context 'with an invalid token' do
 
       it 'shows a JSON failure response' do
-        visit api_path + ".json?project_id=1&token=FOO" 
+        visit api_path + "?project_id=1&token=FOO" 
           # NOTE: using "find" because js:true causes the response to be wrapped inside an html document
           expect(JSON.parse(find('pre').text)).to eq({ "success" => false })
       end
