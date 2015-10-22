1f7f728c9a33c59b0f5c73012a7ab0dc0c591714
mjy <diapriid@gmail.com>
2015-10-22 16:03:46 -0500

    Destroyable nested tags, notes.

--- app/models/concerns/shared/notable.rb
+++ app/models/concerns/shared/notable.rb
@@ -7,12 +7,18 @@ module Shared::Notable
     # Validation happens on the parent side!
     has_many :notes, as: :note_object, validate: true, dependent: :destroy
 
-    accepts_nested_attributes_for :notes
+    accepts_nested_attributes_for :notes, reject_if: :reject_notes, allow_destroy: true
   end
 
   def concatenated_notes_string
     s = notes.order(updated_at: :desc).collect { |n| n.note_string }.join('||')
     s == "" ? nil : s
+  end
+
+  protected
+
+  def reject_notes(attributed)
+    attributed['text'].blank? 
   end
 
 end
