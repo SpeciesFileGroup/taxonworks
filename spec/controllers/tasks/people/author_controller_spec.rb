e7f1296e601395231649b2c945508dc5e70b1d04
Jim Tucker <tuckerjd@uiuc.edu>
2015-12-18 08:46:36 -0600

    TW-361 - small steps. Fixing rspecs for helper and controller.

---
+++ spec/controllers/tasks/people/author_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Tasks::People::AuthorController, type: :controller do
+  before(:each) {
+    sign_in
+  }
+
+  describe "GET #list" do
+    it "returns http success" do
+      get :list
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
--- spec/helpers/tasks/people/author_helper_spec.rb
+++ spec/helpers/tasks/people/author_helper_spec.rb
@@ -10,6 +10,6 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe AuthorHelper, type: :helper do
+RSpec.describe Tasks::People::AuthorHelper, type: :helper do
   pending "add some examples to (or delete) #{__FILE__}"
 end
