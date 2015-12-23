86bc91e81089be42a67ae70a34f4a6d607440bad
Jim Tucker <tuckerjd@uiuc.edu>
2015-12-23 12:57:37 -0600

    TW-362 - Fleshing out Sources display.

--- app/controllers/tasks/people/author_controller.rb
+++ app/controllers/tasks/people/author_controller.rb
@@ -6,6 +6,7 @@ class Tasks::People::AuthorController < ApplicationController
   end
 
   def source_list_for_author
-
+    @person  = Person.find(params[:id])
+    @sources = Source.where('false')
   end
 end
