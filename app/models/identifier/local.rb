2656678323bc5daddfc11c891dab6856d6cc9d0f
mjy <diapriid@gmail.com>
2016-01-04 16:55:47 -0600

    Remove local identifier constraint.

--- app/models/identifier/local.rb
+++ app/models/identifier/local.rb
@@ -2,16 +2,19 @@
 # made to ensure global uniqueness.  While most identifiers are intended to be unique globally, few
 # consider mechanisms for ensuring this.
 #
-# Local identifiers may of the same type may be stacked on a single record without defining the relation
+# Local identifiers of the same type may be stacked on a single record without defining the relation
 # between each identifier (see conceptual difference in Identfier::Global).
 #
-# Local identifiers require a namespace.  See Namespace.  
+# Local identifiers require a namespace. See Namespace.  
 # 
-# Only one namespaced identifier is allowed per type per object, i.e. you can't have, on a single specimen:
+# Multiple local identfiers of the same namespace can be applied to the same object, while this is rarely useful in real life
+# it does have physical-world analogs, see in particular Accession numbers on Collecting Events linked to Specimens that are in the process of 
+# being accessioned.
+#
 #   Foo 123 (CatalogNumber) 
 #   Foo 345 (CatalogNumber)
 #
-# However you *can* have:
+# You can also do this on the same object: 
 #   Foo 123 (CatalogNumber)
 #   Bar 123 (CatalogNumber)
 #  
@@ -22,9 +25,6 @@ class Identifier::Local < Identifier
 
   validates :namespace, presence: true
   validates_uniqueness_of :identifier, scope: [:namespace_id, :project_id, :type]
-
-  # multiple identifiers could easily be attached to the same object, e.g. a specimen with two identifier labels, same collecting event with multiple accession codes.
-  # validates_uniqueness_of :namespace, scope: [:identifier_object_type, :identifier_object_id, :type]
 
   # Exact match on identifier + namespace
   # @param [String, String]
--- spec/models/identifier/local_spec.rb
+++ spec/models/identifier/local_spec.rb
@@ -27,12 +27,11 @@ describe Identifier::Local, :type => :model do
       expect(i2.save).to be_truthy
     end
 
-    specify 'you can not add multiple identifiers of the same type to a single object' do
+    specify 'you can add multiple identifiers of the same type with the same namespace to a single object as long as the identifier differs' do
       i1 = Identifier::Local::CatalogNumber.new(namespace: namespace, identifier_object: specimen1, identifier: 123)
       i2 = Identifier::Local::CatalogNumber.new(namespace: namespace, identifier_object: specimen1, identifier: 345)
       expect(i1.save).to be_truthy
-      expect(i2.save).to be_falsey
-      expect(i2.errors.include?(:namespace)).to be_truthy
+      expect(i2.save).to be_truthy
     end
 
     specify 'you can not add the same namespaced identifier to more than one object (of the same type)' do
