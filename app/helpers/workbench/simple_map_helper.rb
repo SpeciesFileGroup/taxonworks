18f50f1f0f0bb0d8dc2160a001a926cd126e1f10
Jim Tucker <tuckerjd@uiuc.edu>
2016-01-29 12:43:46 -0600

    Early steps toward independant map helper.

---
+++ app/helpers/workbench/simple_map_helper.rb
@@ -0,0 +1,10 @@
+module Workbench::SimpleMapHelper
+
+  def simple_map(map_objects)
+    render(partial: 'layouts/map_header')
+    render(partial: 'shared/data/gis/simple_map',
+           locals:  {map_objects: map_objects})
+      # fail
+  end
+
+end
