4c1b4a1b9968faacda50b2539f56931c7417eacb
mjy <diapriid@gmail.com>
2016-02-05 16:36:54 -0600

    Move the constant inside the module.

--- lib/utilities/geo.rb
+++ lib/utilities/geo.rb
@@ -1,3 +1,6 @@
+# General purpose Geo related methods
+module Utilities::Geo
+
 =begin
 To add a new (discovered) symbol:
   1) To find the Unicode string for any character, use Utilities::Geo.uni_string('c') (remove the first '\').
@@ -9,16 +12,20 @@ To add a new (discovered) symbol:
 =end
 # degree symbols, in addition to 'd', 'o', and '*'
 # \u00b0  "°"  \u00ba  "º"  \u02da  "˚"  \u030a  "?"  \u221e "∞"  \u222b "∫"
-
+#
 # tick symbols, in addition to "'" ("\u0027""), and '"' ("\u0022")
 # \u00a5  "¥"  \u00b4  "´"
 # \u02B9  "ʹ"  \u02BA  "ʺ"  \u02BB  "ʻ"  \u02BC  "ʼ"  \u02CA "ˊ"
 # \u02EE  "ˮ"  \u2032  "′"  \u2033  "″"
+#
 
-SPECIAL_LATLONG_SYMBOLS = "do*\u00b0\u00ba\u02DA\u030a\u221e\u222b\u0027\u00b4\u02B9\u02BA\u02BB\u02BC\u02CA\u02EE\u2032\u2033\u0022"
+  SPECIAL_LATLONG_SYMBOLS = "do*\u00b0\u00ba\u02DA\u030a\u221e\u222b\u0027\u00b4\u02B9\u02BA\u02BB\u02BC\u02CA\u02EE\u2032\u2033\u0022"
+
+  LAT_LON_REGEXP = Regexp.new(/(?<lon>-?\d+\.?\d*),?\s*(?<lat>-?\d+\.?\d*)/)
+
 # DMS_REGEX = "(?<degrees>-*\d+)[do*\u00b0\u00ba\u02DA\u030a\u221e\u222b]\s*(?<minutes>\d+\.*\d*)[\u0027\u00a5\u00b4\u02b9\u02bb\u02bc\u02ca\u2032]*\s*((?<seconds>\d+\.*\d*)[\u0027\u00a5\u00b4\u02b9\u02ba\u02bb\u02bc\u02ca\u02ee\u2032\u2033\u0022]+)*"
 
-module Utilities::Geo
+
   # http://en.wikiversity.org/wiki/Geographic_coordinate_conversion
   # http://stackoverflow.com/questions/1774985/converting-degree-minutes-seconds-to-decimal-degrees
   # http://stackoverflow.com/questions/1774781/how-do-i-convert-coordinates-to-google-friendly-coordinates
@@ -30,7 +37,6 @@ module Utilities::Geo
   # EARTH_RADIUS       = 6371000 # km, 3959 miles (mean Earth radius) # Not used?
   # RADIANS_PER_DEGREE = ::Math::PI/180.0
   # DEGREES_PER_RADIAN = 180.0/::Math::PI
-
 
   ONE_WEST  = 111319.490779206 # meters/degree
   # ONE_WEST  = 111319.444444444 # meters/degree (calculated mean)
