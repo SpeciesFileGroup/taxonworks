c25e96b17648f28f500a229a26fb9fa4f9b88ea7
mjy <diapriid@gmail.com>
2016-02-12 15:56:54 -0600

    with_any_value_for fix (beta).

--- app/models/concerns/shared/alternate_values.rb
+++ app/models/concerns/shared/alternate_values.rb
@@ -31,7 +31,8 @@ module Shared::AlternateValues
     # @param [:symbol] the column name/attribute
     # @param [String, Integer, etc] the value to look for
     # Use
-    #   Otu.with_alternate_value_on(:name, 'foo')
+    #   Source.with_alternate_value_on(:title, 'f
+    #   oo')
     def with_alternate_value_on(a, b)
       joins(:alternate_values).where(alternate_values: {alternate_value_object_attribute: a, value: b})
     end
@@ -40,7 +41,7 @@ module Shared::AlternateValues
     # @param [:symbol] the column name/attribute
     # @param [String, Integer, etc] the value to look for
     # Use
-    #   Otu.with_alternate_value_on(:name, 'foo')
+    #   Source.with_any_value_for(:title, 'foo')
     def with_any_value_for(attribute, value)
       self_table = self.arel_table
       alternate_value_table = AlternateValue.arel_table
@@ -48,7 +49,7 @@ module Shared::AlternateValues
       a = alternate_value_table[:value].eq(value).and(alternate_value_table[:alternate_value_object_attribute].eq(attribute))
       b = self_table[attribute].eq(value)
 
-      self.joins(:alternate_values).where(a.or(b).to_sql)
+      self.includes(:alternate_values).where(a.or(b).to_sql).references(:alternate_values)
     end
 
   end
