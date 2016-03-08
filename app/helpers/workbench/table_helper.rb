f4199ba9efe10f80ec86f966801112e13391e8a0
mbeckman <mbeckman@illinois.edu>
2016-03-08 14:49:24 -0600

    Begin list view modifications using fancy_metadata_cells_tag

--- app/helpers/workbench/table_helper.rb
+++ app/helpers/workbench/table_helper.rb
@@ -2,11 +2,11 @@
 module Workbench::TableHelper
 
   def fancy_th_tag(group: nil, name: '')
-    content_tag(:th, data: { group: group } ) do
-        content_tag(:span, name)
+    content_tag(:th, data: {group: group}) do
+      content_tag(:span, name)
     end
   end
-  
+
   # Hidden action links 
   # data-attributes:
   #  data-show
@@ -16,11 +16,13 @@ module Workbench::TableHelper
   # This is very important, it must be set to make work the options for the context menu.
   # Use the class ".table-options" to hide those options on the table
   #
-  def show_edit_delete_cells_tag(object)
+  def fancy_metadata_cells_tag(object)
     m = object.metamorphosize
-    content_tag(:td, (link_to 'Show', m), class: 'table-options', data: {show: true}) +  
-    content_tag(:td, edit_object_link(object), class: 'table-options', data: {edit: true}) +
-    content_tag(:td, (link_to 'Destroy', m, method: :delete, data: {confirm: 'Are you sure?'}), class: 'table-options', data: {delete: true}) 
+    content_tag(:td, object_tag(object.updater)) +
+        content_tag(:td, object_time_since_update_tag(object)) +
+        content_tag(:td, (link_to 'Show', m), class: 'table-options', data: {show: true}) +
+        content_tag(:td, edit_object_link(object), class: 'table-options', data: {edit: true}) +
+        content_tag(:td, (link_to 'Destroy', m, method: :delete, data: {confirm: 'Are you sure?'}), class: 'table-options', data: {delete: true})
   end
 
 end
