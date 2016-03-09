ea48696b14f3da025b0fe12e68e885a3b9f0fd3e
José Luis Pereira <pepelui@outlook.com>
2016-03-08 22:44:53 -0300

    Mixing hub bar with quick bar

--- app/assets/javascripts/workbench/favorites_page.js
+++ app/assets/javascripts/workbench/favorites_page.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
-  if($('[data-current-tab="favorite"]').length) {
+  if($('#favorite-page').length) {
 	var 
 	favoritesTask = new carrouselTask("Favorites-Task",3,1);
 	favoritesTaskPanel = new carrouselData("Favorites-Task",1,2);
 	favoritesData = new carrouselData("Favorites-Data",99,2);
 	favoritesData.filterChilds();
-
 	 $('.data_card').mousedown(function(event) {
 	  	if((event.which) == 1) {
 	    	location.href = $(this).children("a").attr('href');
