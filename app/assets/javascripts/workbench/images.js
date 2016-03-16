5bbd77f01a1a53784d3cec359d64fa1d62a5d830
José Luis Pereira <pepelui@outlook.com>
2016-03-16 13:57:41 -0300

    CSS, HTML and JS changes on show and attributes view

--- app/assets/javascripts/workbench/images.js
+++ app/assets/javascripts/workbench/images.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 function images() {
     var $easyzoom = $('.easyzoom').easyZoom();
 
-    $('.easyzoom').on('click', function() {
-     location.href = ($(this).children('a').attr('href'));
-    });
+ //   $('.easyzoom').on('click', function() {
+ //    location.href = ($(this).children('a').attr('href'));
+ //   });
 }
