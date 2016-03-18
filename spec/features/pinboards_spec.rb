7d99ed28566e2368bd2f2d12b07d1a9ef0912e3d
mjy <diapriid@gmail.com>
2016-03-18 12:22:50 -0500

    Pass on pinboard.

--- spec/features/pinboards_spec.rb
+++ spec/features/pinboards_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe "Pinboards", :type => :feature do
+RSpec.describe "Pinboards", type: :feature do
 
-  context 'pinning items' do
+  context 'signed in witha project selected' do
+    before {sign_in_user_and_select_project}
+    context 'pinning items' do
+
 =begin
   with an otu created (named "Pinny")
   when I show that otu
@@ -15,26 +18,28 @@ RSpec.describe "Pinboards", :type => :feature do
   then I see a header 'Otus' under Pinboard
   and I ses a link to 'Pinny'
 =end
-    specify 'create a pin', js:true do
-      # added js:true for debugging purposes only
-      sign_in_user_and_select_project
-      otu = Otu.new(name: 'Pinny', by: @user, project: @project)
-      otu.save!
-      visit otus_path
-      expect(page).to have_link('Pinny')
-      click_link('Pinny')
-      # expect(page).to have_link('Add to pinboard')
-      expect(page.has_link?('Add to pinboard')).to be_truthy
 
-      click_link('Add to pinboard')
-      expect(page).to have_content('Pinboard item was successfully created.')
-      expect(page).to have_content('this record is pinned!')
-      expect(page.has_link?('Add to pinboard')).to be_falsey
+      before {
+        Otu.create(name: 'Pinny', by: @user, project: @project)
+      }
+      
+      specify 'create a pin' do # fails with js: true, likely because of js styling of items in recent list
+        visit otus_path
 
-      visit root_path
-      expect(page).to have_selector('h3', 'Otus')
-      expect(page).to have_link('Pinny')
+        expect(page).to have_link('Pinny')
+
+        click_link('Pinny')
+        expect(page.has_link?('Add to pinboard')).to be_truthy
+
+        click_link('Add to pinboard')
+        expect(page).to have_content('Pinboard item was successfully created.')
+        expect(page).to have_content('this record is pinned!')
+        expect(page.has_link?('Add to pinboard')).to be_falsey
+
+        visit root_path
+        expect(page).to have_selector('h3', 'Otus')
+        expect(page).to have_link('Pinny')
+      end
     end
-  end
- 
+  end 
 end
