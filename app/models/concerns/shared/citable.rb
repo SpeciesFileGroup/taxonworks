69dee9872708cb691dd94b027192c8932a23c317
mjy <diapriid@gmail.com>
2016-03-27 23:21:11 -0500

    Checkpoint, citable oldest working.

--- app/models/concerns/shared/citable.rb
+++ app/models/concerns/shared/citable.rb
@@ -4,15 +4,77 @@ module Shared::Citable
   extend ActiveSupport::Concern
 
   included do
-    has_many :citations, as: :citation_object, validate: false, dependent: :destroy
-    has_many :sources, through: :citations
+    related_class = self.name
+    related_table_name = self.table_name
 
-    has_one :origin_citation, -> {where is_original: true}, as: :citation_object, class_name: 'Citation'
+    has_many :citations, as: :citation_object, validate: false, dependent: :destroy
+    has_many :sources, -> { uniq }, through: :citations
+
+    has_one :origin_citation, -> {where(is_original: true)}, as: :citation_object, class_name: 'Citation'
     has_one :source, through: :origin_citation
-  end 
-  
+
+   # oldest first!
+   #  scope :order_by_source_date, -> { 
+   #    joins("LEFT OUTER JOIN citations c on #{related_table_name}.id = c.citation_object_id LEFT OUTER JOIN sources s ON c.source_id = s.id").
+   #    group("#{related_table_name}.id, COALESCE(s.cached_nomenclature_date, Now())").
+   #    where("c.citation_object_type = '#{related_class}'").
+   #    order("(COALESCE(s.cached_nomenclature_date, Now()))")
+   #  } 
+
+    # yougest first!
+    scope :order_by_yougest_source_date, -> { 
+      joins("LEFT OUTER JOIN citations c on #{related_table_name}.id = c.citation_object_id LEFT OUTER JOIN sources s ON c.source_id = s.id").
+      group("#{related_table_name}.id, COALESCE(s.cached_nomenclature_date, Now())").order("COALESCE(s.cached_nomenclature_date, Now()) DESC").
+      where("c.citation_object_type = '#{related_class}'")
+    } 
+
+
+    accepts_nested_attributes_for :citations, reject_if: :reject_citations, allow_destroy: true
+    accepts_nested_attributes_for :origin_citation, reject_if: :reject_citations, allow_destroy: true
+
+
+  define_singleton_method "order_by_source_date" do
+    d =  Arel::Attribute.new(Arel::Table.new(:sources), :cached_nomenclature_date)
+    r  = Arel::Attribute.new(Arel::Table.new(related_table_name), :id)
+    f1 = Arel::Nodes::NamedFunction.new('Now', [] )
+    func = Arel::Nodes::NamedFunction.new('COALESCE', [d, f1])
+
+    #  inner_joins = joins(:citations,  :sources).arel.join_sources
+    #  left_joins = inner_joins.map do |join|
+    #    Arel::Nodes::OuterJoin.new(join.left, join.right)
+    #  end
+
+    joins("LEFT OUTER JOIN citations on #{related_table_name}.id = citations.citation_object_id LEFT OUTER JOIN sources ON citations.source_id = sources.id").
+      where("citations.citation_object_type = '#{related_class}'").
+      group(r, func).
+      order(func)
+  end
+
+    #  has_one :oldest_by_citation, -> { order_by_source_date.limit(1) } 
+  end
+
+  class_methods do
+
+    def oldest_by_citation
+      order_by_source_date.limit(1).to_a.first
+    end
+
+#   def most_recent_by_citation
+#     order_by_source_date.limit(1).to_a.first
+#   end
+
+
+
+  end
+
   def cited?
     self.citations.any?
+  end
+
+  protected 
+
+  def reject_citations(attributed)
+    attributed['source_id'].blank? # || attributed['type'].blank?
   end
 
 end
--- spec/models/concerns/shared/citable_spec.rb
+++ spec/models/concerns/shared/citable_spec.rb
@@ -1,17 +1,36 @@
 require 'rails_helper'
 
-describe 'Citable', type: :model do
+describe 'Citable', type: :model, group: [:nomenclature] do
   let(:class_with_citations) { TestCitable.new } 
+  let(:citation) { FactoryGirl.build(:citation, source: source) }
+
+  let!(:source) { FactoryGirl.create(:valid_source_bibtex, year: 1905) }
+
+  context 'creation' do
+    specify 'with #new, base must be saved' do
+      class_with_citations.save!
+      class_with_citations.citations << Citation.new(source: source)
+      expect(class_with_citations.save!).to be_truthy
+      expect(class_with_citations.citations(true).count).to eq(1)
+    end
+
+    specify 'with #new, unsaved base raises (creator_id not being set)' do
+      class_with_citations.citations << Citation.new(source: source)
+      expect{class_with_citations.save!}.to raise_error(ActiveRecord::StatementInvalid)
+      expect(class_with_citations.citations(true).count).to eq(0)
+    end
+  end
+
   context 'associations' do
     context 'has_many' do
-    specify '#citations - includes creating a citation' do
-      expect(class_with_citations).to respond_to(:citations) # tests that the method citations exists
-      expect(class_with_citations.citations.to_a).to eq([]) # there are no citations yet.
+      specify '#citations - includes creating a citation' do
+        expect(class_with_citations).to respond_to(:citations) # tests that the method citations exists
+        expect(class_with_citations.citations.to_a).to eq([]) # there are no citations yet.
 
-      expect(class_with_citations.citations << FactoryGirl.build(:citation, source: FactoryGirl.create(:valid_source_bibtex))).to be_truthy
-      expect(class_with_citations.citations.size).to eq(1)
-      expect(class_with_citations.save).to be_truthy
-    end
+        expect(class_with_citations.citations << citation).to be_truthy
+        expect(class_with_citations.citations.size).to eq(1)
+        expect(class_with_citations.save).to be_truthy
+      end
     end
 
     context 'has_one' do
@@ -19,10 +38,23 @@ describe 'Citable', type: :model do
         expect(class_with_citations).to respond_to(:source) 
       end
 
+      specify '#source can be set with =' do
+        expect(class_with_citations.source = source).to be_truthy
+        expect(class_with_citations.save).to be_truthy
+        expect(class_with_citations.source).to eq(source)
+      end
+
+      specify '#source can be set with nested attributes' do
+        t = TestCitable.new( origin_citation_attributes: {source_id: source.to_param} )  
+        expect(t.save).to be_truthy
+        expect(t.citations(true).count).to eq(1)
+        expect(t.citations.first.is_original?).to be_truthy
+      end
+
       context 'with a is_original citation' do
         before do 
-          a = FactoryGirl.build(:citation, source: FactoryGirl.create(:valid_source_bibtex), is_original: true)
-          class_with_citations.citations << a
+          citation.is_original = true
+          class_with_citations.citations << citation
           class_with_citations.save!
         end
 
@@ -39,23 +71,67 @@ describe 'Citable', type: :model do
     end
 
     specify 'cited? with some citations' do
-      class_with_citations.citations << Citation.new(source: FactoryGirl.create(:valid_source_bibtex))
+      class_with_citations.citations << citation 
       expect(class_with_citations.cited?).to eq(true)
     end
   end
 
   context 'object with citations' do
-     context 'on destroy' do
-       specify 'attached citations are destroyed' do
-         cite =  Citation.new(source: FactoryGirl.create(:valid_source_bibtex))
-         class_with_citations.citations << cite
-         class_with_citations.save
-         expect(class_with_citations.citations.count).to eq(1)
-         expect(class_with_citations.destroy)
-         expect(Citation.count).to eq(0)
-       end
-     end
+    context 'on destroy' do
+      specify 'attached citations are destroyed' do
+        class_with_citations.save
+        class_with_citations.citations << citation 
+        expect(class_with_citations.citations.count).to eq(1)
+        expect(class_with_citations.destroy)
+        expect(Citation.count).to eq(0)
+      end
+    end
   end
+
+  context 'ordering by source date' do
+
+    let!(:a) { TestCitable.create }
+    let!(:b) { TestCitable.create } 
+    let!(:c) { TestCitable.create } 
+
+    let!(:young_source) { FactoryGirl.create(:valid_source_bibtex, year: 2010) }
+    let!(:old_source) { FactoryGirl.create(:valid_source_bibtex, year: 1998) }
+    let!(:oldest_source) { FactoryGirl.create(:valid_source_bibtex, year: 1758) }
+
+    before do
+      Citation.create(citation_object: a, source: oldest_source) 
+      Citation.create(citation_object: b, source: young_source)                
+      Citation.create(citation_object: c, source: old_source)   
+    end
+
+    specify "sanity test, 3 citations exist" do
+      expect(Citation.count).to eq(3)
+    end
+
+    specify "#order_by_source_date" do
+      expect(TestCitable.order_by_source_date.to_a).to eq([a, c, b])
+    end
+
+    specify ".oldest_by_citation" do
+      expect(TestCitable.oldest_by_citation).to eq(a)
+    end
+
+    specify "#order_by_source_date.first" do
+      expect(TestCitable.order_by_source_date.to_a.first).to eq(a)
+    end
+
+    context 'with nils' do
+      let!(:d) { TestCitable.create } 
+      let!(:nil_source) { FactoryGirl.create(:valid_source_bibtex, year: nil) }
+
+      specify "with nils" do
+        Citation.create(citation_object: d, source: nil_source)  
+        expect(TestCitable.order_by_source_date.to_a).to eq([a,c,b,d])
+      end
+    end
+
+  end
+
 end
 
 class TestCitable < ActiveRecord::Base
