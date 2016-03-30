fb0714bfe8ae07ffeca0caaa834403ec2fe5725e
mjy <diapriid@gmail.com>
2016-03-29 21:47:35 -0500

    Tweak to handle users/projects.

--- db/migrate/20160324151603_migrate_original_sources.rb
+++ db/migrate/20160324151603_migrate_original_sources.rb
@@ -4,7 +4,7 @@ class MigrateOriginalSources < ActiveRecord::Migration
     [TaxonName, TaxonNameRelationship,Georeference, BiologicalAssociationsGraph, TypeMaterial].each do |k|
       k.all.each do |r|
         next if r.source_id.nil?
-        Citation.create!(is_original: true, citation_object: r, source_id: r.source_id)
+        Citation.create!(is_original: true, citation_object: r, source_id: r.source_id, project_id: r.project_id, created_by_id: r.created_by_id, updated_by_id: r.updated_by_id)
       end
     end
   end
