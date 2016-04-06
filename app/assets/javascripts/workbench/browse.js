da06c997b66de05c3d510a857c23c1d7c5abf7f2
José Luis Pereira <pepelui@outlook.com>
2016-04-06 13:50:53 -0300

    Taxon names browse: Filter effects

--- app/assets/javascripts/workbench/browse.js
+++ app/assets/javascripts/workbench/browse.js
@@ -4,26 +4,28 @@ $(document).ready(function() {
   }
 });
 function browseView() {
-	var timeEffect = 150;
-
 	$('#filterBrowse').on('click', '.navigation-item', function(selector) {
 		if($(this).attr('data-filter-reset') === 'reset') {
 			$('[data-filter]').each( function() {
-				$($(this).attr('data-filter')).show(timeEffect);
+				$($(this).attr('data-filter')).animate({
+            	fontSize: '100%'
+        		});
 				$($(this).children()).attr('data-icon', 'show');
 			});
 		}
 		else {
 			if($(this).children().attr('data-icon') == "show") {
 				$($(this).children()).attr('data-icon', 'hide');
-				$($(this).attr('data-filter')).css('font-size','0px');
+				$($(this).attr('data-filter')).animate({
+            	fontSize: '0px'
+        		});
 			}
 			else {
 				$($(this).children()).attr('data-icon', 'show');
-				$($(this).attr('data-filter')).css('font-size','100%');;			
+				$($(this).attr('data-filter')).animate({
+            	fontSize: '100%'
+        		});			
 			}
 		}
 	});
-
-
 }
