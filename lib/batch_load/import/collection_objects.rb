e1991663ad072c054404e506f479a4d38366caaf
Jim Tucker <tuckerjd@uiuc.edu>
2016-04-14 11:53:34 -0500

    Steps for CollectionObject batch load, with new (04/13/16) CollectionObjectTest.txt (headers).

--- lib/batch_load/import/collection_objects.rb
+++ lib/batch_load/import/collection_objects.rb
@@ -13,42 +13,67 @@ module BatchLoad
 
     # process each row for information:
     def build_collection_objects
-      i          = 1 # accounting for headers
+      i       = 1 # accounting for headers
       # identifier namespace
-      local_name = csv.headers[0]
-      ns1        = Namespace.where(short_name: local_name).first
-      ns2 - Namespace.first_or_create(name: 'Arbitrary Name', short_name: 'ns2')
-      id1 - row[local_name]
+      header1 = csv.headers[0] # should be 'collection_object_identifier_namespace_short_name'
+      header2 = csv.headers[1] # should be 'collection_object_identifier_identifier'
+      header5 = csv.headers[5] # should be 'collecting_event_identifier_namespace_short_name'
+      header6 = csv.headers[6] # should be 'collecting_event_identifier_identifier'
+      header7 = csv.headers[7] # should be 'collecting_event_identifier_type'
       csv.each do |row|
-        # find a namespace (ns1) with a short_name of headers[0] (local_name)
+        co_namespace = row[header1]
+        co_id        = row[header2]
+        next if (co_namespace.nil? or co_id.nil?) # no namespace to search!
+        # find a namespace (ns1) with a short_name of row[headers[0]] (id1)
         # find a collection_object which has an identifier which has a namespace (ns1), and a cached of
         # (ns1.short_name + ' ' + identifier.identifier)
-        co  = CollectionObject.joins(:identifiers).where(identifiers: {cached: "#{ns1.short_name} #{id1}"}).first
-        otu = Otu.find_or_create_by(name: row[2])
-        td  = TaxonDetermination.create(otu:               otu,
-                                        collection_object: co)
-        ce  = CollectingEvent.create(verbatim_locality:                row['verbatim_location'],
-                                     verbatim_geolocation_uncertainty: row['error'],
-                                     verbatim_date:                    row['date'],
-                                     start_date_day:                   row['day'],
-                                     start_date_month:                 row['month'],
-                                     start_date_year:                  row['year'],
-                                     verbatim_longitude:               row['longitude'],
-                                     verbatim_latitude:                row['latitude'],
-                                     verbatim_method:                  row['method'])
-        gr  = Georeference.create(collecting_event: ce,
-                                  type:             'Georeference::GeoLocate')
+        # ns1    = Namespace.where(short_name: id1).first
+        long         = row['longitude'] # longitude
+        lat          = row['latitude'] # latitude
+        method       = row['method']
+        error        = (row['error'].to_s + ' ' + row['georeference_error_units'].to_s).strip
+        ce_namespace = row[header5]
+        co           = CollectionObject.joins(:identifiers).where(identifiers: {cached: "#{co_namespace} #{co_id}"}).first
+        otu          = Otu.find_or_create_by(name: row['otu'])
+        td           = TaxonDetermination.create(otu:                          otu,
+                                                 biological_collection_object: co)
+        ce           = CollectingEvent.create(verbatim_locality:                row['verbatim location'],
+                                              verbatim_geolocation_uncertainty: error,
+                                              verbatim_date:                    row['date'],
+                                              start_date_day:                   row['day'],
+                                              start_date_month:                 row['month'],
+                                              start_date_year:                  row['year'],
+                                              verbatim_longitude:               long,
+                                              verbatim_latitude:                lat,
+                                              verbatim_method:                  method)
 
-        co.collecting_ecent = ce
+        case method.downcase
+          when 'geolocate'
+            # faking a Georeference::GeoLocate:
+            #   1) create the Georeference, using the newly created collecting_event
+            gr                 = Georeference::GeoLocate.create(collecting_event: ce)
+            #   2) build a fake iframe response in the form '52.65|-106.333333|3036|Unavailable'
+            text               = "#{lat}|#{long}|#{Utilities::Geo.elevation_in_meters(error)}|Unavailable"
+            #   3) use that fake to stimulate the parser to create the object
+            gr.iframe_response = text
+            gr.save
+          else
+            # nothing to do?
+        end unless method.nil?
 
-        Identifier::Local::TripCode.new(namespace: ns2, identifier: row['collecting event #'])
+        ns_ce = Namespace.where(short_name: ce_namespace).first
+        ce_id = Identifier.new(namespace:  ns_ce,
+                               type:       'Identifier::' + row[header7],
+                               identifier: row[header6])
+        ce.identifiers << ce_id
+        co.collecting_event = ce
         # error_radius:     Utilities::Geo.elevation_in_meters(row['error']))
         # ident = Identifier.find_or_create_by(identifier:   row[0],
         #                                      namespace_id: n_s.id,
         #                                      type:         'Identifier::Local::Import',
         # )
 
-        i += 1
+        i                   += 1
       end
       @total_lines = i - 1
     end
--- spec/lib/batch_load/import/collection_objects_spec.rb
+++ spec/lib/batch_load/import/collection_objects_spec.rb
@@ -3,7 +3,28 @@ require 'rails_helper'
 describe BatchLoad::Import::CollectionObjects, type: :model do
 
   let(:file_name) { 'spec/files/batch/collection_object/CollectionObjectTest.txt' }
+  let(:psuc) { [0, 35614, 39024, 37926, 38919, 38866, 35628, 39770, 35423] }
   let(:csv_args) { {headers: true, col_sep: "\t", encoding: 'UTF-8'} }
+  let(:ns_1) { Namespace.create(name: 'PSUC', short_name: 'PSUC') }
+  let(:ns_2) { Namespace.create(name: 'Import', short_name: 'Collecting Event No.') }
+  let(:co_1) { FactoryGirl.create(:valid_collection_object) }
+  let(:id_1) { FactoryGirl.create(:valid_identifier, namespace: ns_1, identifier: psuc[1]) }
+  let(:co_2) { FactoryGirl.create(:valid_collection_object) }
+  let(:id_2) { FactoryGirl.create(:valid_identifier, namespace: ns_1, identifier: psuc[2]) }
+  let(:co_3) { FactoryGirl.create(:valid_collection_object) }
+  let(:id_3) { FactoryGirl.create(:valid_identifier, namespace: ns_1, identifier: psuc[3]) }
+  let(:co_4) { FactoryGirl.create(:valid_collection_object) }
+  let(:id_4) { FactoryGirl.create(:valid_identifier, namespace: ns_1, identifier: psuc[4]) }
+  let(:co_5) { FactoryGirl.create(:valid_collection_object) }
+  let(:id_5) { FactoryGirl.create(:valid_identifier, namespace: ns_1, identifier: psuc[5]) }
+  let(:co_6) { FactoryGirl.create(:valid_collection_object) }
+  let(:id_6) { FactoryGirl.create(:valid_identifier, namespace: ns_1, identifier: psuc[6]) }
+  let(:co_7) { FactoryGirl.create(:valid_collection_object) }
+  let(:id_7) { FactoryGirl.create(:valid_identifier, namespace: ns_1, identifier: psuc[7]) }
+  let(:co_8) { FactoryGirl.create(:valid_collection_object) }
+  let(:id_8) { FactoryGirl.create(:valid_identifier, namespace: ns_1, identifier: psuc[8]) }
+  let(:co_9) { FactoryGirl.create(:valid_collection_object) }
+  let(:id_9) { FactoryGirl.create(:valid_identifier, namespace: ns_1, identifier: psuc[0]) }
   # let(:basic_headers) { ['PSUC', 'Species', 'OTU', 'Confirmed?', 'Collecting Event#',
   #                        'Date', 'Month', 'Day', 'Year',
   #                        'Verbatim Location',
@@ -37,9 +58,19 @@ describe BatchLoad::Import::CollectionObjects, type: :model do
 
   context 'initialzation' do
     it 'begins to take form' do
+      ns_2
+      co_1.identifiers << id_1
+      co_2.identifiers << id_2
+      co_3.identifiers << id_3
+      co_4.identifiers << id_4
+      co_5.identifiers << id_5
+      co_6.identifiers << id_6
+      co_7.identifiers << id_7
+      co_8.identifiers << id_8
+      co_9.identifiers << id_9
       bingo = import
       expect(bingo).to be_truthy
-      expect(Namespace.count).to eq(1)
+      expect(Namespace.count).to eq(2)
       expect(Otu.count).to eq(1)
     end
   end
