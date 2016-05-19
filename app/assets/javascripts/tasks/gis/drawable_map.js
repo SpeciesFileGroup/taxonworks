17bb2d0e6581190edbdc4a9a104c8f950dddd5d3
jrflood <jrflood@illinois.edu>
2016-05-19 15:21:57 -0500

    shuffle map instantiation to avoid near-fatal issue with hidden google maps instance.

--- app/assets/javascripts/tasks/gis/drawable_map.js
+++ app/assets/javascripts/tasks/gis/drawable_map.js
@@ -5,20 +5,33 @@ _init_drawable_map = function init_drawable_map() {
   var drawable_map;  // intended for use to display on a map objects which know how to GeoJSON themselves
   var drawable_map_shell = $('#_drawable_map_outer');
   if (drawable_map_shell.length) {
-    drawable_map_shell.removeAttr('hidden');
-    drawable_map = initializeGoogleMapWithDrawManager(_drawable_map_form);
+    //drawable_map = initializeGoogleMapWithDrawManager(_drawable_map_form);
+    //drawable_map_shell.attr('hidden', true);
 
-    $(".map_toggle").click(function (event) {
-      $("#area_selector").attr('hidden', true);
-      $("#map_selector").removeAttr('hidden');
-      $(".on_selector").removeAttr('hidden');  // expose the other link
+    $(".map_toggle").click(function (event) {           // switch to the map
+      $("#area_selector").attr('hidden', true);         // hide the area selector
+      $("#map_selector").removeAttr('hidden');          // reveal the map
+      $(".on_selector").removeAttr('hidden');           // expose the other link
       $(".map_toggle").attr('hidden', true);
+      drawable_map = initializeGoogleMapWithDrawManager(_drawable_map_form);
+      google.maps.event.addListener(drawable_map[1], 'overlaycomplete', function (event) {
+          // Remove the last created shape if it exists.
+          if (gr_last != null) {
+            if (gr_last[0] != null) {
+              removeItemFromMap(gr_last[0]);
+            }
+          }
+          gr_last = [event.overlay, event.type];
+          var feature = buildFeatureCollectionFromShape(gr_last[0], gr_last[1]);
+          $("#drawn_area_shape").val(JSON.stringify(feature[0]));
+        }
+      );
     });
 
-    $(".on_selector").click(function (event) {
-      $("#map_selector").attr('hidden', true);
-      $("#area_selector").removeAttr('hidden');
-      $(".map_toggle").removeAttr('hidden');  // expose the other link
+    $(".on_selector").click(function (event) {          // switch to the area by name selector
+      $("#map_selector").attr('hidden', true);          // hide the map
+      $("#area_selector").removeAttr('hidden');         // reveal the area selector
+      $(".map_toggle").removeAttr('hidden');            // expose the other link
       $(".on_selector").attr('hidden', true);
     });
 
@@ -28,18 +41,7 @@ _init_drawable_map = function init_drawable_map() {
       }
     );
 
-    google.maps.event.addListener(drawable_map[1], 'overlaycomplete', function (event) {
-      // Remove the last created shape if it exists.
-      if (gr_last != null) {
-        if (gr_last[0] != null) {
-          removeItemFromMap(gr_last[0]);
-        }
-        }
-      gr_last = [event.overlay, event.type];
-      var feature = buildFeatureCollectionFromShape(gr_last[0], gr_last[1]);
-      $("#drawn_area_shape").val(JSON.stringify(feature[0]));
-      }
-    );
+
   }
 };
 
