ce863942cbb7137d4f405b840ed971d1dc12813f
José Luis Pereira <pepelui@outlook.com>
2016-05-29 19:11:56 -0300

    Minor changes for Tasks and otu picker

--- app/assets/javascripts/workbench/favorites_page.js
+++ app/assets/javascripts/workbench/favorites_page.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 	favoritesTask = new carrouselTask("Favorites-Task",8,1);
 	favoritesTask.filterChilds();
 	favoritesTaskPanel = new carrouselData("Favorites-Task",1,2);
-	favoritesData = new carrouselData("Favorites-Data",99,2);
+	favoritesData = new carrouselData("Favorites-Data",99,1);
 	favoritesData.filterChilds();
 	 $('.data_card').mousedown(function(event) {
 	  	if((event.which) == 1) {
