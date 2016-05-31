c3ec9b1c1e9e9a184c7ac03f5d4b76161f097ca4
Jim Tucker <tuckerjd@uiuc.edu>
2016-05-31 11:05:18 -0500

    TW-403: Fixed parent_taxon_name_id dropped during BatchLoad::Import::TaxonifiToTaxonworks.new().

--- lib/batch_load/import/taxonifi_to_taxonworks.rb
+++ lib/batch_load/import/taxonifi_to_taxonworks.rb
@@ -1,31 +1,32 @@
 module BatchLoad
 
-  # Batch loading of CSV formatted taxon names via Taxonifi 
+  # Batch loading of CSV formatted taxon names via Taxonifi
   class Import::TaxonifiToTaxonworks < BatchLoad::Import
 
     # The Taxonifi Name collection
     attr_accessor :name_collection
 
-    # The parent for the top level names 
+    # The parent for the top level names
     attr_accessor :parent_taxon_name
 
     # The id of the parent taxon name, computed automatically as Root if not provided
-    attr_accessor :parent_taxon_name_id 
+    attr_accessor :parent_taxon_name_id
 
     # The code (Rank Class) that new names will use
     attr_accessor :nomenclature_code
 
-    # Whether to create an OTU as well 
+    # Whether to create an OTU as well
     attr_accessor :also_create_otu
 
     # Required to handle some defaults
     attr_accessor :project_id
 
-    def initialize( nomenclature_code: nil, parent_taxon_name_id: nil, also_create_otu: false, **args)
-      @nomenclature_code = nomenclature_code
-      @also_create_otu = also_create_otu
+    def initialize(nomenclature_code: nil, parent_taxon_name_id: nil, also_create_otu: false, **args)
+      @nomenclature_code    = nomenclature_code
+      @also_create_otu      = also_create_otu
+      @parent_taxon_name_id = parent_taxon_name_id
 
-      super(args)     
+      super(args)
     end
 
     def parent_taxon_name
@@ -34,13 +35,13 @@ module BatchLoad
         begin
           @parent_taxon_name = TaxonName.find(parent_taxon_name_id)
         rescue ActiveRecord::RecordNotFound
-          @errors = [ 'Provided parent taxon name id not valid.' ]
+          @errors = ['Provided parent taxon name id not valid.']
         end
       else
         @parent_taxon_name = Project.find(@project_id).root_taxon_name
       end
 
-      @parent_taxon_name      
+      @parent_taxon_name
     end
 
     def nomenclature_code
@@ -67,7 +68,7 @@ module BatchLoad
     end
 
     def build_protonyms
-      if name_collection.nil? 
+      if name_collection.nil?
         @file_errors.push "No names were readable in the file."
         return
       end
@@ -77,7 +78,7 @@ module BatchLoad
       total_lines = 0
 
       name_collection.collection.each do |n|
-        i = n.row_number + 1
+        i  = n.row_number + 1
         rp = nil
 
         if @processed_rows[i]
@@ -85,20 +86,20 @@ module BatchLoad
         else
           rp = BatchLoad::RowParse.new
           @processed_rows.merge!(i => rp)
-        end 
+        end
 
         p = Protonym.new(
-          name: n.name,
-          year_of_publication: n.year.to_s,
-          rank_class: Ranks.lookup(nomenclature_code, n.rank),
-          by: @user,
-          also_create_otu: also_create_otu,
-          project: @project,
-          verbatim_author: (n.parens ? n.author_with_parens : nil),
+          name:                          n.name,
+          year_of_publication:           n.year.to_s,
+          rank_class:                    Ranks.lookup(nomenclature_code, n.rank),
+          by:                            @user,
+          also_create_otu:               also_create_otu,
+          project:                       @project,
+          verbatim_author:               (n.parens ? n.author_with_parens : nil),
           taxon_name_authors_attributes: taxon_name_authors_hash(n)
         )
 
-        p.parent = (n.parent.nil? ? parent_taxon_name : parents[n.parent.id] )
+        p.parent = (n.parent.nil? ? parent_taxon_name : parents[n.parent.id])
 
         rp.objects[:protonyms] ||= []
         rp.objects[:protonyms].push(p)
@@ -110,18 +111,18 @@ module BatchLoad
 
       @total_data_lines = total_lines
 
-      true 
+      true
     end
 
     def taxon_name_authors_hash(taxonifi_name)
-      author_attributes = [] 
-      taxonifi_name.authors.each do |a| 
+      author_attributes = []
+      taxonifi_name.authors.each do |a|
         suffix = a.suffix.join(' ') if !a.suffix.nil?
         author_attributes.push({
-          last_name: a.last_name,
-          first_name: [a.first_name, a.initials_string].compact.join(' '),
-          suffix: suffix, # this might not be right, have to 2x check
-        })
+                                 last_name:  a.last_name,
+                                 first_name: [a.first_name, a.initials_string].compact.join(' '),
+                                 suffix:     suffix, # this might not be right, have to 2x check
+                               })
       end
       author_attributes
     end
