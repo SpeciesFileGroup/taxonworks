6c0a49b2b43c73e5af94801c5798a2b38a307a02
jrflood <jrflood@illinois.edu>
2016-05-31 14:35:34 -0500

    map-drawing-modes passed as parameter

--- app/helpers/workbench/drawable_map_helper.rb
+++ app/helpers/workbench/drawable_map_helper.rb
@@ -2,11 +2,12 @@ module Workbench::DrawableMapHelper
 
   # @param [Objects] map_objects an array of objects which know how to GeoJSON themselves
   # @param [String] border_string set to 'border="1"' if the table should have borders
-  def drawable_map(map_objects, border_string = '')
+  def drawable_map(map_objects, border_string = '', drawing_modes = 'marker,circle,polygon,polyline,rectangle')
     render(partial: 'layouts/map_header')
     render(partial: 'shared/data/gis/drawable_map',
            locals:  {map_objects:   map_objects,
-                     border_string: border_string})
+                     border_string: border_string,
+                     drawing_modes: drawing_modes.to_s})
     # fail
   end
 
