d8971bb00310930285965ea4a80c2bb293fba330
Jim Tucker <tuckerjd@uiuc.edu>
2016-06-01 09:33:44 -0500

    Minor edit.

--- app/helpers/workbench/drawable_map_helper.rb
+++ app/helpers/workbench/drawable_map_helper.rb
@@ -7,7 +7,7 @@ module Workbench::DrawableMapHelper
     render(partial: 'shared/data/gis/drawable_map',
            locals:  {map_objects:   map_objects,
                      border_string: border_string,
-                     drawing_modes: drawing_modes.to_s})
+                     drawing_modes: drawing_modes})
     # fail
   end
 
