330102e104e5fcbdfaa6e8e8fc37811299e07241
José Luis Pereira <pepelui@outlook.com>
2016-06-08 15:17:35 -0300

    New feature: hide main and quick bar button on the top

--- app/assets/javascripts/workbench/quick_bar.js
+++ app/assets/javascripts/workbench/quick_bar.js
@@ -1,7 +1,22 @@
 $(document).ready(function() {
 	if($("#quick_bar").length) {
-	    	Mousetrap.bind('ctrl+h ctrl+h', function(e) {
+		Mousetrap.bind('ctrl+h ctrl+h', function(e) {
 			window.location = "/hub";
 		});
-  	}
+
+		$('body').append("<div class='button-collapse-header button-collapse-arrow-up'></div>");
+		$('.button-collapse-header').on('click', function() {
+			if($(this).hasClass('button-collapse-arrow-down')) {
+				$(this).removeClass('button-collapse-arrow-down');
+				$(this).addClass('button-collapse-arrow-up');
+			}
+			else {
+				$(this).addClass('button-collapse-arrow-down');
+				$(this).removeClass('button-collapse-arrow-up');
+			}
+			$('#quick_bar').slideToggle(250);
+			$('#header_bar').slideToggle(250);
+		});
+	}
 });
+
