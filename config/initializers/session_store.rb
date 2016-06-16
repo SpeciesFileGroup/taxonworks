60857eea6061d0eed44d3bca18e105c539bad908
mjy <diapriid@gmail.com>
2016-06-16 09:21:02 -0500

    Expire production cookies after 24 hours.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-TaxonWorks::Application.config.session_store :cookie_store, key: '_TaxonWorks_session'
+TaxonWorks::Application.config.session_store(
+  :cookie_store, 
+  key: '_TaxonWorks_session',
+  expire_after: (Rails.env == 'production' ? 24.hours : 14.days) 
+)
