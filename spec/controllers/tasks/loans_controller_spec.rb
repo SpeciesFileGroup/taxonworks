25f2a4b4e89929402f122d6a8ee7d3dded5e2775
Jim Tucker <tuckerjd@uiuc.edu>
2016-07-20 08:14:14 -0500

    TW-410: Fix rspec failure because of change in routing.

--- spec/controllers/tasks/loans_controller_spec.rb
+++ spec/controllers/tasks/loans_controller_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 describe Tasks::LoansController, type: :controller do
-
   before(:each) {
     sign_in
   }
+  let(:loan) { FactoryGirl.create(:valid_loan) }
 
   describe 'GET #complete' do
     it 'returns http success' do
-      get :complete
+      get :complete, {:id => loan.to_param}
       expect(response).to have_http_status(:success)
     end
   end
