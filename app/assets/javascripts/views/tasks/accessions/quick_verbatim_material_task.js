1feaf67a0606201e43ff9da239797be60faec4f0
José Luis Pereira <pepelui@outlook.com>
2016-07-20 16:19:37 -0300

    Changed quick verbatim coffee to javascript

---
+++ app/assets/javascripts/views/tasks/accessions/quick_verbatim_material_task.js
@@ -0,0 +1,29 @@
+// quick_verbatim_material_task.js
+
+function update_attribute_names(link) {
+  new_id = new Date().getTime();
+  regex = new RegExp("_update_", "g");
+  $('input[name*="_update_"]').each(function(i) {
+    a = $(this).attr('name').replace(regex, new_id); 
+    b = $(this).attr('id').replace(regex, new_id); 
+    $(this).attr('name', a);  
+    $(this).attr('id', b);
+  });
+}
+
+function bind_remove_row_link(link) {
+  $('a[class*="_update_"]').each(function(i) {
+   $(this).on("click",function (event) {
+    $(this).parents('.biocuration_totals_row').hide();
+    event.preventDefault(); 
+  });
+   $(this).attr('class', 'remove_row');
+ });     
+}
+
+$(document).ready(function() {
+  $(".add_total_row").on("click", function(event) {
+    update_attribute_names(this);
+    bind_remove_row_link(this);
+  });
+});
