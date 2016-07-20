359f84224630f261d9c976929130e19216c0f6ee
José Luis Pereira <pepelui@outlook.com>
2016-07-20 12:37:49 -0300

    Changed hub coffee to javascript

---
+++ app/assets/javascripts/views/hub/hub_bar.js
@@ -0,0 +1,5 @@
+$(document).ready( function() {
+  if ($('#hub_tabs').length > 0) {
+   $('#' + $('#hub_tabs').data('currentTab')).addClass('current_hub_category');
+	}
+});
--- app/assets/javascripts/views/hub/hub_bar.js.coffee
+++
@@ -1,3 +0,0 @@
-$(document).ready ->
-  if $('#hub_tabs').length
-   $('#' + $('#hub_tabs').data('currentTab')).addClass('current_hub_category')
