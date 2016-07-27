39a52f6647ded81238da5ea5df47089da5b096c2
mbeckman <mbeckman@illinois.edu>
2016-07-27 15:12:11 -0500

    Change made by Matt so SF names of superfamily rank could be imported without error message about suffixes.

--- app/models/nomenclatural_rank/iczn/family_group/superfamily_group.rb
+++ app/models/nomenclatural_rank/iczn/family_group/superfamily_group.rb
@@ -7,7 +7,10 @@ class NomenclaturalRank::Iczn::FamilyGroup::SuperfamilyGroup < NomenclaturalRank
   def self.validate_name_format(taxon_name)
     super
     return true if taxon_name.name.length < 2
-    taxon_name.errors.add(:name, 'name must end in -oidea') if not(taxon_name.name =~ /.*oidea\Z/)
+
+    # @todo: @mjy: superfamily group rank in SF does not have type genus nor any naming constraints. Following line was commented out for temporary import compatibility.
+    # taxon_name.errors.add(:name, 'name must end in -oidea') if not(taxon_name.name =~ /.*oidea\Z/)
+
   end
 
   def self.valid_name_ending
