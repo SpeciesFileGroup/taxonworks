c4f86b6dfb7fca2b453c34bf27623ea4e2d061a3
Jim Tucker <tuckerjd@uiuc.edu>
2016-08-02 09:38:57 -0500

    Fixing loan_helper rspec to confrom to new version of loans_helper.loan_tag, Link still fails.

--- spec/helpers/loans_helper_spec.rb
+++ spec/helpers/loans_helper_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe LoansHelper, :type => :helper do
   context 'a loan needs some helpers' do
     let(:loan) { FactoryGirl.create(:valid_loan)  }
-    let(:tag_string) { "[#{loan.to_param}]" }
+    let(:tag_string) { "#{loan.to_param} - " }
 
     specify '#loan_tag' do
       expect(helper.loan_tag(loan)).to eq(tag_string)
