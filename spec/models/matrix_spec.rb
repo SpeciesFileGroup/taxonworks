1205559069350b3491e636526fa8e9154e25df00
mjy <diapriid@gmail.com>
2016-08-07 12:04:59 -0500

    Matrix reference MatrixColumnItems.

--- spec/models/matrix_spec.rb
+++ spec/models/matrix_spec.rb
@@ -2,15 +2,22 @@ require 'rails_helper'
 
 RSpec.describe Matrix, type: :model do
 
-
   let(:matrix) {Matrix.new}
+
+  context 'associations' do 
+    context 'has_many' do
+      specify '#matrix_column_items' do
+        expect(matrix.matrix_column_items << MatrixColumnItem.new).to be_truthy
+      end
+    end
+
+  end
 
   context :validation do
     before {matrix.valid?}
     specify 'name is required' do
       expect(matrix.errors.include?(:name)).to be_truthy
     end
-
   end
 
 end
