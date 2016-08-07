47e5430c6303f383487dbdb7ae22cab986db739c
mjy <diapriid@gmail.com>
2016-08-07 12:06:04 -0500

    Add a reference_count attribute to matrix columns, for set addition/subtraction purposes.

--- app/models/matrix_column.rb
+++ app/models/matrix_column.rb
@@ -7,6 +7,12 @@ class MatrixColumn < ActiveRecord::Base
   belongs_to :matrix
   belongs_to :descriptor
 
+  after_initialize :set_reference_count
+
+  def set_reference_count
+    self.reference_count ||= 0
+  end
+
   acts_as_list
 
   validates_presence_of :matrix_id, :descriptor_id
