e1b2c9a3e9b64a127a82fa2c403c9f31367b9c6e
Fauzi Kliman <fauzikliman@gmail.com>
2016-08-16 14:59:42 -0700

    Passing GeneAttribute and Descriptor model/controller specs

--- spec/models/gene_attribute_spec.rb
+++ spec/models/gene_attribute_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe GeneAttribute, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:gene_attribute) {GeneAttribute.new}
+
+  context 'validation' do
+    context 'fails when not given' do
+      before(:each){
+        gene_attribute.valid?
+      }
+
+      specify 'descriptor_id' do
+        expect(gene_attribute.errors.include?(:descriptor_id)).to be_truthy
+      end
+
+      specify 'sequence_id' do
+        expect(gene_attribute.errors.include?(:sequence_id)).to be_truthy
+      end
+    end
+    
+    context 'passes when given' do
+      specify 'descriptor_id and sequence_id' do
+        gene_attribute = FactoryGirl.create(:valid_gene_attribute)
+        expect(gene_attribute.valid?).to be_truthy
+      end
+    end
+  end
 end
