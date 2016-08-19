fb7b206639e7cd2f4a7dbe5393b87eb9fea38e5d
mjy <diapriid@gmail.com>
2016-08-19 17:06:51 -0600

    Fix botched rebase merge for matrix/data.yml

--- app/models/matrix.rb
+++ app/models/matrix.rb
@@ -5,17 +5,10 @@ class Matrix < ActiveRecord::Base
   include Shared::IsData
   include Shared::Taggable
   include Shared::Notable
-<<<<<<< 78e56ca3c2632af3840134e92a40c450e3f7cb5d
   include Shared::DataAttributes
 
   validates_presence_of :name
 
   has_many :matrix_column_items, dependent: :destroy
-=======
-
-  belongs_to :project
-
-  validates_presence_of :name
->>>>>>> Scaffolded matrix model, basics integrated in TW interface. Tests updated.
 
 end
