92857d0db82279917a84260e028e8a343b3a57ec
Jim Tucker <tuckerjd@uiuc.edu>
2016-08-19 11:12:10 -0500

    Responding to changes in Container WRT descendants, parents.

--- app/helpers/containers_helper.rb
+++ app/helpers/containers_helper.rb
@@ -10,6 +10,11 @@ module ContainersHelper
     link_to(container_tag(container.metamorphosize).html_safe, container.metamorphosize)
   end
 
+  def container_parent_link(container)
+    return nil if container.container_item.parent.nil?
+    link_to(container.container_item.parent.contained_object)
+  end
+
   def containers_search_form
     render('/containers/quick_search_form')
   end
@@ -17,7 +22,7 @@ module ContainersHelper
   def container_collection_item_count(container)
     return content_tag(:em, 'no container provided') if container.blank?
     v = container.all_collection_objects.count
-    v == 0 ? 'empty' : v 
+    v == 0 ? 'empty' : v
   end
 
   # TODO: move content to containers/_card
@@ -27,11 +32,11 @@ module ContainersHelper
     content_tag(:div, class: :draw_container) do
       content_tag(:div) do
         container_tag(container)
-      end + 
+      end +
 
       content_tag(:div) do
         content_tag(:ul) do
-          container.container_items.collect{ |a| content_tag(:li, container_item_tag(a)) } 
+          container.container_items.collect { |a| content_tag(:li, container_item_tag(a)) }
         end
       end
     end
