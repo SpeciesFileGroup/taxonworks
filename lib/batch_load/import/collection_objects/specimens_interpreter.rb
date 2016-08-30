d0d59de6cd0ce8ac3c07ab5b47d8cf66ac08c891
Fauzi Kliman <fauzikliman@gmail.com>
2016-08-29 17:47:39 -0700

    Batch load for collection objects specimens completed, adds identifiers if not blank. Requires more testing though

--- lib/batch_load/import/collection_objects/specimens_interpreter.rb
+++ lib/batch_load/import/collection_objects/specimens_interpreter.rb
@@ -1,6 +1,6 @@
 # TODO: THIS IS A GENERATED STUB, it does not function
 module BatchLoad
-  class Import::CollectionObjects::SpecimenInterpreter < BatchLoad::Import
+  class Import::CollectionObjects::SpecimensInterpreter < BatchLoad::Import
 
     def initialize(**args)
       @collection_objects = {}
@@ -9,6 +9,14 @@ module BatchLoad
 
     # TODO: update this
     def build_collection_objects
+      # Castor            CTR
+      # Morphbank         MBK
+      # DRMLabVoucher     DRMLV
+      # DRMFieldVoucher   DRMFV
+      namespace_castor = Namespace.find_by(name: 'Castor')
+      namespace_morphbank = Namespace.find_by(name: 'Morphbank')
+      namespace_drm_lab_voucher = Namespace.find_by(name: 'DRMLabVoucher')
+      namespace_drm_field_voucher = Namespace.find_by(name: 'DRMFieldVoucher')
 
       i = 1
       # loop throw rows
@@ -20,11 +28,46 @@ module BatchLoad
         @processed_rows[i] = parse_result
 
         begin # processing
-           # use a BatchLoad::ColumnResolver or other method to match row data to TW 
-           #  ...
-           
+          # use a BatchLoad::ColumnResolver or other method to match row data to TW 
+          #  ...
+
+          # Text for identifiers
+          co_identifier_castor_text = row['guid']
+          co_identifier_morphbank_text = row['morphbank_specimen_id']
+          co_identifier_drm_field_voucher_text = row['specimen_number']
+          co_identifier_drm_lab_voucher_text = "#{row['voucher_number_prefix']}#{row['voucher_number_stirng']}"
+
+          # Identifiers
+          co_identifier_castor = { namespace: namespace_castor,
+                                   type: 'Identifier::Local::CollectionObject',
+                                   identifier: co_identifier_castor_text }
+
+          co_identifier_morphbank = { namespace: namespace_morphbank,
+                                      type: 'Identifier::Local::CollectionObject',
+                                      identifier: co_identifier_morphbank_text }
+                                          
+          co_identifier_drm_field_voucher = { namespace: namespace_drm_field_voucher,
+                                              type: 'Identifier::Local::CollectionObject',
+                                              identifier: co_identifier_drm_field_voucher_text }
+                                                  
+          co_identifier_drm_lab_voucher = { namespace: namespace_drm_lab_voucher,
+                                            type: 'Identifier::Local::CollectionObject',
+                                            identifier: co_identifier_drm_lab_voucher_text }                                     
+
+          # Collection object
+          co_identifiers = Array.new
+          co_identifiers.push(co_identifier_castor)             if !co_identifier_castor_text.blank?
+          co_identifiers.push(co_identifier_morphbank)          if !co_identifier_morphbank_text.blank?
+          co_identifiers.push(co_identifier_drm_field_voucher)  if !co_identifier_drm_field_voucher_text.blank?
+          co_identifiers.push(co_identifier_drm_lab_voucher)    if !co_identifier_drm_lab_voucher_text.blank?
+
+          co_attributes = { type: 'Specimen', total: 1, identifiers_attributes: co_identifiers }
+          co = CollectionObject.new(co_attributes)
+
+          parse_result.objects[:collection_object].push(co);
         rescue
            # ....
+           # puts "SOMETHING WENT WRONG WITH COLLECTION OBJECT SPECIMENS INTERPRETER BATCH LOAD"
         end
         i += 1
       end
