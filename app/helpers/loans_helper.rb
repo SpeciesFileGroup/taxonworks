b308758e45890809b1332acba6ede3acc8e78900
mjy <diapriid@gmail.com>
2016-08-31 11:59:26 -0500

    Tweak loan_tag().

--- app/helpers/loans_helper.rb
+++ app/helpers/loans_helper.rb
@@ -2,9 +2,7 @@ module LoansHelper
 
   def loan_tag(loan)
     return nil if loan.nil?
-     
-    v = [loan.id, *loan.people.collect{|a| a.name}.join(', '), loan.recipient_address].compact.join(" - ").gsub(/\n/, '; ') 
-    v.blank? ? "[#{loan.to_param}]" : v
+    [loan.id, loan.people.collect{|a| a.name}.join(', '), loan.recipient_address].delete_if{|b| b.blank? }.join(" - ").gsub(/\n/, '; ') 
   end
 
   def loan_link(loan)
--- spec/helpers/loans_helper_spec.rb
+++ spec/helpers/loans_helper_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe LoansHelper, :type => :helper do
   context 'a loan needs some helpers' do
     let(:loan) { FactoryGirl.create(:valid_loan)  }
-    let(:tag_string) { "#{loan.to_param} - " }
+    let(:tag_string) { "#{loan.to_param}" }
 
     specify '#loan_tag' do
       expect(helper.loan_tag(loan)).to eq(tag_string)
@@ -14,7 +14,7 @@ describe LoansHelper, :type => :helper do
     end
 
     specify '.loan_link' do
-      expect(loan_link(loan)).to have_link(tag_string)
+      expect(loan_link(loan)).to have_link( tag_string )
     end
 
     specify ".loan_search_form" do
