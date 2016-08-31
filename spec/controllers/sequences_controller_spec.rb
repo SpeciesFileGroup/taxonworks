d0c2ed0f4e064da1e1db88af5b8061ecfbf364cd
Fauzi Kliman <fauzikliman@gmail.com>
2016-08-31 16:37:18 -0700

    Passing update controller tests for Sequences and SequenceRelationships

--- spec/controllers/sequences_controller_spec.rb
+++ spec/controllers/sequences_controller_spec.rb
@@ -105,14 +105,15 @@ RSpec.describe SequencesController, type: :controller do
   describe "PUT #update" do
     context "with valid params" do
       let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
+        { sequence_type: "DNA", sequence: "random sequence" }
       }
 
       it "updates the requested sequence" do
         sequence = Sequence.create! valid_attributes
         put :update, {id: sequence.to_param, sequence: new_attributes}, session: valid_session
         sequence.reload
-        skip("Add assertions for updated state")
+
+        expect(sequence.sequence == new_attributes[:sequence])
       end
 
       it "assigns the requested sequence as @sequence" do
