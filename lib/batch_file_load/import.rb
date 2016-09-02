7e42b45fe5d0b9f62c72b976aaa42aa178c072f7
Fauzi Kliman <fauzikliman@gmail.com>
2016-09-02 14:52:20 -0700

    Finished batch file load with simple Otus example

--- lib/batch_file_load/import.rb
+++ lib/batch_file_load/import.rb
@@ -1,10 +1,13 @@
 module BatchFileLoad
   class Import
 
-    attr_accessor :errors
-    attr_accessor :processed_files
-    attr_accessor :files
-
+    attr_reader :processed_files
+    attr_reader :processed
+    attr_reader :errors
+    attr_reader :file_errors
+    attr_reader :filenames
+    attr_reader :file_contents
+    
     def initialize(project_id: nil, user_id: nil, files: nil, import_level: :warn)
       @project_id = project_id
       @user = User.find(user_id)
@@ -20,7 +23,6 @@ module BatchFileLoad
       # in which this would break the metadata
       @processed_files = { names: [], objects: [] }
 
-
       @filenames = []
       @file_contents = []
 
@@ -33,41 +35,105 @@ module BatchFileLoad
         @file_contents.push(file.tempfile.read.force_encoding('utf-8'))
       end
 
+      @errors = []
+      @file_errors = []
+
       @total_records_created = 0
 
       build
     end
 
+    # Attempts to save each object from the files into the database
     def create
-      get_all_objects.each do |object|
-        object.save
+      if ready_to_create?
+        @total_records_created = 0
+
+        get_all_objects.each do |object|
+          if object.save
+            @total_records_created += 1
+          end
+        end
+      else
+        @errors << "Import level #{@import_level} has prevented creation." unless import_level_ok?
+        @errors << "One of project_id, user_id or files has not been provided." unless valid?
       end
     end
 
+    # Returns the number of objects that were successfully saved to the database
     def total_records_created
-      raise 'This method must be provided in the respective subclass.'
+      @total_records_created
     end
 
+    # Returns the total number of projects that got processed from each file
+    def total_records_processed
+      get_all_objects.length
+    end
+
+    # Returns the number of files that got processed
+    def total_files_processed
+      @processed_files[:names].length
+    end
+
+    # Checks if valid housekeeping and file attributes were supplied
     def valid?
       @project_id && @user && @filenames && @file_contents
     end
 
+    # Anything can happen
+    def warn_level_ok?
+      true
+    end
+
+    # There must be records from each file
+    def file_strict_level_ok?
+      @filenames.each_with_index do |filename, index|
+        return false if filename != @processed_files[:names][index] || @processed_files[:objects][index].empty?
+      end
+    end
+
+    # Every record must be valid
+    def object_strict_level_ok?
+      get_all_objects.each do |object|
+        return false if !object.valid?
+      end
+    end
+
+    # There must be records from every file and every record must be valid
+    def file_object_strict_level_ok?
+      file_strict_level_ok? && object_strict_level_ok?
+    end
+
     protected
 
+    # Subclass implemented function that is responsible for interpreting the imported data from the files
     def build
       raise 'This method must be provided in the respective subclass.'
     end
 
     private
 
+    # Returns true if ready to create all the objects and store in the database
     def ready_to_create?
       valid? && @processed && import_level_ok?
     end
 
+    # Checks if a file passes the specificed import level
     def import_level_ok?
-      @processed_files[:objects]
+      case @import_level.to_sym
+        when :warn
+          warn_level_ok?
+        when :file_strict 
+          file_strict_level_ok?
+        when :object_strict 
+          object_strict_level_ok?
+        when :file_object_strict 
+          file_object_strict_level_ok?
+        else
+          false
+      end
     end
 
+    # Returns an array that has every object from each file
     def get_all_objects
       all_objects = []
 
