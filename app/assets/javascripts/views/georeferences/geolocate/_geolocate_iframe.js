2fb5ab10cf35f3353bfb30b73160c71a73e98674
José Luis Pereira <pepelui@outlook.com>
2016-09-02 14:16:51 -0300

    _geolocate_iframe.js update

--- app/assets/javascripts/views/georeferences/geolocate/_geolocate_iframe.js
+++ app/assets/javascripts/views/georeferences/geolocate/_geolocate_iframe.js
@@ -1,14 +1,20 @@
 function displayMessage(event) {
-  message = undefined
-  if (event.origin != "http://www.museum.tulane.edu") {
+  message = undefined;
+
+  if (event.origin !== "http://www.museum.tulane.edu") {
     message = "iframe url does not have permision to interact with me";
   }
   else {
     // All of the data parsing happens model side.
-    document.getElementById("georeference_iframe_response").value = event.data
+    document.getElementById("georeference_iframe_response").value = event.data;
   }
+  return;
 }
 
-window.addEventListener("message", function(event){
-    displayMessage(event);
-});
+if (window.addEventListener) {
+	window.addEventListener("message", displayMessage, false);
+} else if (window.attachEvent) {
+	window.attachEvent("onmessage", displayMessage);
+}
+
+
