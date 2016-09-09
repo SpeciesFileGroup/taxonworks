042913c2b256e5a53223418a71c9fb8d262cd9ef
mjy <diapriid@gmail.com>
2016-09-09 18:11:00 -0500

    Confidence/confidence level models.

--- app/models/confidence.rb
+++ app/models/confidence.rb
@@ -1,4 +1,28 @@
+# A Confidence is an annotation that links a user-defined confidence level to a
+# data object. It is an assertion as to the quality of that data.
+#
+# @!attribute confidence_level_id 
+#   @return [Integer]
+#     the controlled vocabulary term used in the confidence 
+#
+# @!attribute confidence_object_id
+#   @return [Integer]
+#      Rails polymorphic. The id of of the object being annotated.
+#
+# @!attribute confidence_object_type
+#   @return [String]
+#      Rails polymorphic.  The type of the object being annotated. 
+#
+# @!attribute project_id
+#   @return [Integer]
+#   the project ID
+#
+# @!attribute position
+#   @return [Integer]
+#     a user definable sort code on the tags on an object, handled by acts_as_list
+#
 class Confidence < ActiveRecord::Base
+
   include Housekeeping
   include Shared::IsData
 
@@ -9,7 +33,8 @@ class Confidence < ActiveRecord::Base
 
   belongs_to :controlled_vocabulary_term, foreign_key: :confidence_level_id
 
-  validates_presence_of :confidence_id
+  validates :confidence_level, presence: true
 
   validates_uniqueness_of :confidence_level_id, scope: [:confidence_object_id, :confidence_object_type]
+
 end
--- spec/models/confidence_spec.rb
+++ spec/models/confidence_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe Confidence, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Confidence, type: :model, group: :confidence do
+
+  let(:confidence) { Confidence.new }
+
+  context 'validation' do
+    before { confidence.save }
+
+    specify 'confidence_level_id' do
+      expect(confidence.errors.include?(:confidence_level)).to be_truthy
+    end
+  end
+
+  context 'associations' do
+    specify '#confidence_level' do
+      expect(confidence.confidence_level = ConfidenceLevel.new()).to be_truthy
+    end
+
+    specify '#confidence_object' do
+      expect(confidence.confidence_object = Specimen.new()).to be_truthy
+    end
+  end 
+
 end
