840dae77d4f56e5d3b00f5d826fb36cc0aa84912
jrflood <jrflood@illinois.edu>
2016-09-17 18:03:22 -0500

    incremental progress passing params ffrom form

--- app/controllers/tasks/collection_objects/area_and_date_controller.rb
+++ app/controllers/tasks/collection_objects/area_and_date_controller.rb
@@ -14,10 +14,33 @@ class Tasks::CollectionObjects::AreaAndDateController < ApplicationController
     @collection_objects = CollectionObject.where('false')
   end
 
-  # POST
+  # GET
   def set_area
+    message = ''
+    value = params['gr_geographic_item_attributes_shape']
+    if value.blank?
+      @georeferences = Georeference.where('false')
+    else
+      feature = RGeo::GeoJSON.decode(value, :json_parser => :json)
+      geometry = feature.geometry
+      this_type = geometry.geometry_type.to_s.downcase
+      geometry = geometry.as_text
+      radius = feature['radius']
+      case this_type
+        when 'point'
+          @georeferences = Georeference.joins(:geographic_item).where(GeographicItem.within_radius_of_wkt_sql(geometry, radius))
+        when 'polygon'
+          @georeferences = Georeference.joins(:geographic_item).where(GeographicItem.contained_by_wkt_sql(geometry))
+        else
+      end
+      if @georeferences.length == 0
+        message = 'no objects contained in drawn shape'
+      end
+      render_gr_select_json(message)
+    end
+
+
     @geographic_area          = GeographicArea.find(params[:geographic_area_id])
-    @collection_objects_count = 3
   end
 
   # GET
