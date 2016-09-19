172121e9b6b9dddba7667b095dd044b111c1b42f
Jim Tucker <tuckerjd@uiuc.edu>
2016-09-19 14:03:03 -0500

    More steps in generated area and date task. Results list works.

--- app/controllers/tasks/collection_objects/area_and_date_controller.rb
+++ app/controllers/tasks/collection_objects/area_and_date_controller.rb
@@ -12,18 +12,17 @@ class Tasks::CollectionObjects::AreaAndDateController < ApplicationController
   # find all of the objects within the supplied area and within the supplied data range
   def find
     @geographic_area_id = params[:geographic_area_id]
-    @shape_in = params[:drawn_area_shape]
+    @shape_in           = params[:drawn_area_shape]
 
     @collection_objects = GeographicItem.gather_selected_data(@geographic_area_id, @shape_in, 'CollectionObject')
     # generate map and/or list
-    # render json: {'tasks/accessions/report/dwca/table', collection_objects: @collection_objects}
-    render json: {html: @collection_objects_count.to_s}
+    render_co_select_json('')
   end
 
   # GET
   def set_area
-    @geographic_area_id = params[:geographic_area_id]
-    @shape_in = params[:drawn_area_shape]
+    @geographic_area_id       = params[:geographic_area_id]
+    @shape_in                 = params[:drawn_area_shape]
     @collection_objects_count = GeographicItem.gather_selected_data(@geographic_area_id, @shape_in, 'CollectionObject').count
 
     render json: {html: @collection_objects_count.to_s}
@@ -42,4 +41,13 @@ class Tasks::CollectionObjects::AreaAndDateController < ApplicationController
 
   end
 
+  def render_co_select_json(message)
+    render json: {message: message, html: co_render_to_html}
+  end
+
+  def co_render_to_html
+    render_to_string(partial: 'tasks/accessions/report/dwca/table',
+                     locals:  {collection_objects: @collection_objects}
+    )
+  end
 end
