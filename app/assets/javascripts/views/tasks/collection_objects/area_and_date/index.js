8ce578252a0e0fd66128e212e76c63bb5b0ef89e
Jim Tucker <tuckerjd@uiuc.edu>
2016-09-22 09:53:37 -0500

    Steps on the path of selecting the date range.

--- app/assets/javascripts/views/tasks/collection_objects/area_and_date/index.js
+++ app/assets/javascripts/views/tasks/collection_objects/area_and_date/index.js
@@ -1,77 +1,112 @@
 var _init_map_table;
 
 _init_map_table = function init_map_table() {
-  if ($("#set_area_form").length) {
-    var result_map;  // intended for use to display on a map objects which know how to GeoJSON themselves
-    var result_collection;
-    //var area_selector = $("#geographic_area_id_for_by_area");
+  if ($("#co_by_area_and_date").length) {
+    if ($("#set_area_form").length) {
+      var result_map;  // intended for use to display on a map objects which know how to GeoJSON themselves
+      var result_collection;
+      //var area_selector = $("#geographic_area_id_for_by_area");
 
-    $(".result_map_toggle").click(function (event) {           // switch to the map view
-      $("#area_count").text('####');
-      $("#show_list").attr('hidden', true);         // hide the list view
-      $("#show_map").removeAttr('hidden');          // reveal the map
-      $(".result_list_toggle").removeAttr('hidden');           // expose the other link
-      $(".result_map_toggle").attr('hidden', true);
-      $("[name='[geographic_area_id]']").attr('value', '');
-      result_map = _init_simple_map();
-      result_map = TW.vendor.lib.google.maps.initializeMap('simple_map_canvas', result_collection);
-    });
+      $(".result_map_toggle").click(function (event) {           // switch to the map view
+        $("#area_count").text('####');
+        $("#show_list").attr('hidden', true);         // hide the list view
+        $("#show_map").removeAttr('hidden');          // reveal the map
+        $(".result_list_toggle").removeAttr('hidden');           // expose the other link
+        $(".result_map_toggle").attr('hidden', true);
+        $("[name='[geographic_area_id]']").attr('value', '');
+        result_map = _init_simple_map();
+        result_map = TW.vendor.lib.google.maps.initializeMap('simple_map_canvas', result_collection);
+      });
 
-    $(".result_list_toggle").click(function (event) {          // switch to the list view
-      $("#area_count").text('####');
-      $("#show_map").attr('hidden', true);          // hide the map
-      $("#show_list").removeAttr('hidden');         // reveal the area selector
-      $(".result_map_toggle").removeAttr('hidden');            // expose the other link
-      $(".result_list_toggle").attr('hidden', true);
-      $("#drawn_area_shape").attr('value', '');
-    });
+      $(".result_list_toggle").click(function (event) {          // switch to the list view
+        $("#area_count").text('####');
+        $("#show_map").attr('hidden', true);          // hide the map
+        $("#show_list").removeAttr('hidden');         // reveal the area selector
+        $(".result_map_toggle").removeAttr('hidden');            // expose the other link
+        $(".result_list_toggle").attr('hidden', true);
+        $("#drawn_area_shape").attr('value', '');
+      });
 
-    $("#set_area").click(function (event) {      // register the click handler for the made-from-scratch-button
-      $("#area_count").text('----');
-        //var geo_id = $("input[name='[geographic_area_id]']").val();
-        $.get('set_area', $("#set_area_form").serialize(), function (local_data) {
-            var popcorn = local_data;
-            $("#area_count").text(local_data.html);
+      $("#set_area").click(function (event) {      // register the click handler for the made-from-scratch-button
+          $("#area_count").text('----');
+          //var geo_id = $("input[name='[geographic_area_id]']").val();
+          $.get('set_area', $("#set_area_form").serialize(), function (local_data) {
+              var popcorn = local_data;
+              $("#area_count").text(local_data.html);
+            }, 'json'  // I expect a json response
+          );
+
+          event.preventDefault();
+        }
+      );
+      //event.preventDefault();
+      //$("#set_area").on("ajax:success", function (e, data) {
+      //    $("#area_count").text(data.html);
+      //    return true;
+      //  }
+      //).on("#ajax:errror", function (e, xhr, status, error) {
+      //    $("area_count").text("<p>set_area error => " + error + "</p>")
+      //  }
+      //);
+      $("#find_area_and_date_commit").click(function (event) {
+        //$("#result_span").text('**********');
+        $.get('find', $("#set_area_form").serialize(), function (local_data) {
+          var html = local_data.html;
+          var message = local_data.message;
+          result_collection = local_data.feature_collection;
+          if (message.length == 0) {
+            $("#result_span").text("Total: " + local_data.collection_objects_count);
+          }
+          else {
+            $("#result_span").text(message);
+          }
+          $("#show_list").html(html);
+          if (local_data.feature_collection) {
+            if ($("#show_map").attr("hidden") != "hidden") {
+              //$("#show_map").removeAttr('hidden');
+              result_map = TW.vendor.lib.google.maps.initializeMap('simple_map_canvas', result_collection);
+            }
+          }
           }, 'json'  // I expect a json response
         );
-
         event.preventDefault();
+      })
+    }
+
+    function set_control(control, input, format, year, today) {
+      if (control.length) {
+        control.datepicker({dateFormat: format, changeMonth: true, changeYear: true, yearRange: "1700:" + year})
+        input.val(today)
       }
-    );
-    //event.preventDefault();
-    //$("#set_area").on("ajax:success", function (e, data) {
-    //    $("#area_count").text(data.html);
-    //    return true;
-    //  }
-    //).on("#ajax:errror", function (e, xhr, status, error) {
-    //    $("area_count").text("<p>set_area error => " + error + "</p>")
-    //  }
-    //);
-    $("#find_area_and_date_commit").click(function (event) {
-      //$("#result_span").text('**********');
-      $.get('find', $("#set_area_form").serialize(), function (local_data) {
-        var html = local_data.html;
-        var message = local_data.message;
-        result_collection = local_data.feature_collection;
-        if (message.length == 0) {
-          $("#result_span").text("Total: " + local_data.collection_objects_count);
-          }
-        else {
-          $("#result_span").text(message);
-        }
-        $("#show_list").html(html);
-        if (local_data.feature_collection) {
-          if ($("#show_map").attr("hidden") != "hidden") {
-            //$("#show_map").removeAttr('hidden');
-            result_map = TW.vendor.lib.google.maps.initializeMap('simple_map_canvas', result_collection);
-          }
-        }
-        }, 'json'  // I expect a json response
-      );
-      event.preventDefault();
-    })
+    }
+
+    var today = new Date();
+    var year = today.getFullYear();
+    today = today.toLocaleDateString('en-US', {year: 'numeric', month: '2-digit', day: '2-digit'});
+    var format = 'dd-MM-yy';
+    var dateInput;
+
+    //if ($("#st_fixedpicker").length) {
+    //  $("#st_fixedpicker").datepicker({dateFormat: format, changeMonth: true, changeYear: true, yearRange: "1700:" + year});
+    //  dateInput = $("#st_flexpicker");
+    //  dateInput.val(today.toLocaleDateString());
+    //  dateInput.datepicker("onSelect",  dateInput);
+    //  //dateInput.datepicker('setDate', $.datepicker.parseDate(format, dateInput.val()));
+    //}
+
+    set_control($("#st_fixedpicker"), $("#st_flexpicker"), format, year, today);
+
+    //if ($("#en_fixedpicker").length) {
+    //  $("#en_fixedpicker").datepicker({dateFormat: format, changeMonth: true, changeYear: true, yearRange: "1700:" + year});
+    //  dateInput = $("#en_flexpicker");
+    //  dateInput.val(today.toLocaleDateString());
+    //  //dateInput.datepicker('setDate', $.datepicker.parseDate(format, dateInput.val()));
+    //}
+
+    set_control($("#en_fixedpicker"), $("#en_flexpicker"), format, year, today);
   }
 };
+
 
 $(document).ready(_init_map_table);
 $(document).on('page:load', _init_map_table);
