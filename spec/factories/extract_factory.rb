ade75807128bd1823e0331a5a85f859abdb5e329
Fauzi Kliman <fauzikliman@gmail.com>
2016-09-23 12:39:00 -0700

    Updated extracts table by removing quantity_concentration column, adding optional concentration_value and concentration_unit columns

--- spec/factories/extract_factory.rb
+++ spec/factories/extract_factory.rb
@@ -2,8 +2,9 @@ FactoryGirl.define do
   factory :extract, traits: [:housekeeping] do
     factory :valid_extract do
       quantity_value "9.99"
-      quantity_unit "MyString"
-      quantity_concentration "9.99"
+      quantity_unit "meters"
+      concentration_value "9.99"
+      concentration_unit "meters"
       verbatim_anatomical_origin "MyString"
       year_made 1
       month_made 1
