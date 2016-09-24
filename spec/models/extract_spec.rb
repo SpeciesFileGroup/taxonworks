f02a28e7e890e1721ef6d8f0344c2b372e6fb2c9
Fauzi Kliman <fauzimerfoo@gmail.com>
2016-09-23 17:59:58 -0700

    Added validations for year made, month made, day made, and basic class description for Extract model

--- spec/models/extract_spec.rb
+++ spec/models/extract_spec.rb
@@ -1,36 +1,80 @@
 require 'rails_helper'
 
-RSpec.describe Extract, type: :model do
-  let(:extract) {Extract.new}
+RSpec.describe Extract, type: :model, group: :extract do
 
   context 'validations' do
     context 'fails when not given' do
+      let(:empty_extract) {
+        Extract.new
+      }
+
       before(:each){
-        extract.valid?
+        empty_extract.valid?
       }
 
       specify 'quantity_value' do
-        expect(extract.errors.include?(:quantity_value)).to be_truthy
+        expect(empty_extract.errors.include?(:quantity_value)).to be_truthy
       end
 
       specify 'quantity_unit' do
-        expect(extract.errors.include?(:quantity_unit)).to be_truthy
+        expect(empty_extract.errors.include?(:quantity_unit)).to be_truthy
       end
 
       specify 'verbatim_anatomical_origin' do
-        expect(extract.errors.include?(:verbatim_anatomical_origin)).to be_truthy
+        expect(empty_extract.errors.include?(:verbatim_anatomical_origin)).to be_truthy
       end
 
       specify 'year_made' do
-        expect(extract.errors.include?(:year_made)).to be_truthy
+        expect(empty_extract.errors.include?(:year_made)).to be_truthy
       end
 
       specify 'month_made' do
-        expect(extract.errors.include?(:month_made)).to be_truthy
+        expect(empty_extract.errors.include?(:month_made)).to be_truthy
       end
 
       specify 'day_made' do
-        expect(extract.errors.include?(:day_made)).to be_truthy
+        expect(empty_extract.errors.include?(:day_made)).to be_truthy
+      end
+    end
+
+    
+    context 'fails when given invalid dates' do
+      let(:valid_extract){
+        FactoryGirl.build(:valid_extract)
+      }
+
+      after(:each){
+        expect(valid_extract.valid?).to be_falsey
+      }
+
+      context 'year_made' do
+        specify '< 1000' do 
+          valid_extract.year_made = 1000
+        end
+
+        specify '> Time.now.year + 5' do 
+          valid_extract.year_made = Time.now.year + 6
+        end
+      end
+
+      context 'month_made' do
+        specify '< 1' do
+          valid_extract.month_made = 0
+        end
+
+        specify '> 12' do
+          valid_extract.month_made = 13
+        end
+      end
+
+      context 'day_made' do
+        specify '< 1' do
+          valid_extract.day_made = 0
+        end
+
+        specify '> 31' do
+          valid_extract.day_made = 32
+        end
       end
     end
 
