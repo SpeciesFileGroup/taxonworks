9df4f9dd365e9853942eebd25f04ee62006da0c5
jrflood <jrflood@illinois.edu>
2016-09-28 14:16:56 -0500

    Slider control from zhangtasdq/range-picker subbed in for evaluation/integration.

--- app/assets/javascripts/views/tasks/collection_objects/area_and_date/index.js
+++ app/assets/javascripts/views/tasks/collection_objects/area_and_date/index.js
@@ -121,6 +121,38 @@ _init_map_table = function init_map_table() {
       );
       event.preventDefault();
     }
+
+    function dateFormat(date, fmt) {
+      var o = {
+        "M+": date.getMonth() + 1,
+        "d+": date.getDate(),
+      };
+      if (/(y+)/.test(fmt)) {
+        fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+      }
+      for (var k in o) {
+        if (new RegExp("(" + k + ")").test(fmt)) {
+          fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        }
+      }
+      return fmt;
+    }
+
+    var startDate = new Date("1700/1/1"),
+      endDate = new Date(),
+      offset = endDate - startDate;
+
+    $("#double_date_range").rangepicker({
+      type: "double",
+      startValue: dateFormat(startDate, "yyyy/MM/dd"),
+      endValue: dateFormat(endDate, "yyyy/MM/dd"),
+      translateSelectLabel: function (currentPosition, totalPosition) {
+        var timeOffset = offset * ( currentPosition / totalPosition);
+        var date = new Date(+startDate + parseInt(timeOffset));
+        return dateFormat(date, "yyyy/MM/dd");
+      }
+    });
+
   }
 };
 
