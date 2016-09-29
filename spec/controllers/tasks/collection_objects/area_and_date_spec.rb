61a010bce84f70c1479365acad9f8e4385497ec0
Jim Tucker <tuckerjd@uiuc.edu>
2016-09-29 14:14:07 -0500

    Some RSpecs

--- spec/controllers/tasks/collection_objects/area_and_date_spec.rb
+++ spec/controllers/tasks/collection_objects/area_and_date_spec.rb
@@ -7,12 +7,43 @@ describe Tasks::CollectionObjects::AreaAndDateController, type: :controller do
     # sign_in
     generate_political_areas_with_collecting_events
   }
+  before(:each) {
+    sign_in
+  }
   after(:all) { clean_slate_geo }
 
-  xdescribe 'GET #index' do
+  describe 'GET #index' do
     it 'returns http success' do
       get(:index)
       expect(response).to have_http_status(:success)
     end
+  end
+
+  describe '#set_area' do
+    it 'renders count of collection objects in a specific names area' do
+      get(:set_area, {geographic_area_id: GeographicArea.where(name: 'Great Northern Land Mass').first.id})
+      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body)['html']).to eq('13')
+    end
+
+    it "renders count of collection objects in a drawn area" do
+      # {"type"=>"Feature", "geometry"=>{"type"=>"MultiPolygon", "coordinates"=>[[[[33, 28, 0], [37, 28, 0], [37, 26, 0], [33, 26, 0], [33, 28, 0]]]]}, "properties"=>{"geographic_item"=>{"id"=>23}}}
+      get(:set_area, {drawn_area_shape: GeographicArea
+                                          .where(name: 'Big Boxia')
+                                          .first
+                                          .default_geographic_item
+                                          .to_geo_json_feature})
+      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body)['html']).to eq('7')
+    end
+  end
+
+  describe '#set_date' do
+    it "renders count of collection objects based on the start and end dates" do
+      get(:set_date, {st_flexpicker: Date.yesterday.to_s, en_flexpicker: Date.tomorrow.to_s})
+      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body)['html']).to eq('17')
+    end
+
   end
 end
