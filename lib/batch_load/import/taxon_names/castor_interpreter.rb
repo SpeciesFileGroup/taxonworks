8cbebe16847bcea96c9ed085c17e6b8646f2a662
Fauzi Kliman <fauzimerfoo@gmail.com>
2016-10-13 23:42:12 -0700

    Modified castor taxon names importer

--- lib/batch_load/import/taxon_names/castor_interpreter.rb
+++ lib/batch_load/import/taxon_names/castor_interpreter.rb
@@ -1,33 +1,141 @@
 # TODO: THIS IS A GENERATED STUB, it does not function
 module BatchLoad
-  class Import::TaxonNames::CastorInterpreter < Import::TaxonifiToTaxonworks
+  class Import::TaxonNames::CastorInterpreter < BatchLoad::Import
+
+    # The parent for the top level names
+    attr_accessor :parent_taxon_name
+
+    # The id of the parent taxon name, computed automatically as Root if not provided
+    attr_accessor :parent_taxon_name_id
+
+    # The code (Rank Class) that new names will use
+    attr_accessor :nomenclature_code
+
+    # Whether to create an OTU as well
+    attr_accessor :also_create_otu
+
+    # Required to handle some defaults
+    attr_accessor :project_id
 
     def initialize(nomenclature_code: nil, parent_taxon_name_id: nil, also_create_otu: false, **args)
-      super
+      @nomenclature_code = nomenclature_code
+      @parent_taxon_name_id = parent_taxon_name_id
+      @also_create_otu = also_create_otu
+
+      super(args)
     end
     
-    protected
+    def parent_taxon_name 
+      Project.find(@project_id).root_taxon_name
+    end
+    
+    def parent_taxon_name_id
+      parent_taxon_name.id
+    end
 
-    def build
-      super
-     
-      if @processed
-        namespace_castor = Namespace.find_by(name: 'Castor')
+    def build_taxon_names
+      i = 0
+  
+      taxon_names = {}
 
-        @processed_rows.each do |processed_row_index, rp|
-          # You need to subtract 2 from processed_row_index because processed_row_index is 1 based
-          # so you need to subtract 1 from it to make it 0 based and csv object is missing the first RowParse
-          # which were the headers so you need to subtract 1 again from it to account for the missing row
-          csv_row_index = processed_row_index - 2
-          taxon_name_identifer_taxon_concept = ""
+      csv.each do |row|
+        i += 1
+        parse_result = BatchLoad::RowParse.new
+        parse_result.objects[:taxon_names] = []
+
+        @processed_rows[i] = parse_result
+
+        begin
+          next if row['rank'] == "complex"
+          next if row['rank'] == "species group"
+          next if row['rank'] == "series"
+          next if row['taxon_name'] == "unidentified"
+      
+          protonym_attributes = {
+            name: row['taxon_name'],
+            year_of_publication: year_of_publication(row['author_year']),
+            rank_class: Ranks.lookup(@nomenclature_code.to_sym, row['rank']),
+            by: @user,
+            also_create_otu: @also_create_otu,
+            project: @project,
+            verbatim_author: verbatim_author(row['author_year']),
+            taxon_name_authors_attributes: taxon_name_authors_attributes(verbatim_author(row['author_year']))
+          }
+
+          p = Protonym.new(protonym_attributes)
+          taxon_name_id = row['id']
+          parent_taxon_name_id = row['parent_id']
+          taxon_names[taxon_name_id] = p
+
+          if taxon_names[parent_taxon_name_id].nil?
+            p.parent = parent_taxon_name
+          else
+            p.parent = taxon_names[parent_taxon_name_id]
+          end
           
-          ap processed_row_index
-          ap csv[csv_row_index]
+          parse_result.objects[:taxon_names].push p
         end
       end
+    end
 
-      puts ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-      byebug
+    def build
+      if valid?
+        build_taxon_names
+        @processed = true
+      end
+    end
+
+    private
+
+    def year_of_publication(author_year)
+      split_author_year = author_year.split(" ")
+      year = split_author_year[split_author_year.length - 1]
+      return year
+    end
+
+    def verbatim_author(author_year)
+      split_author_year = author_year.split(" ")
+      verbatim_author = split_author_year[0...(split_author_year.length - 1)]
+      return verbatim_author
+    end
+
+    def taxon_name_authors_attributes(author_year)
+      author_info = verbatim_author(author_year)
+
+      multiple_author_query = "and"
+      multiple_author_index = author_info.index(multiple_author_query)
+
+      if multiple_author_index.nil?
+        return [author_info(author_info)]
+      else
+        multiple_author_index += multiple_author_query.length
+        split_author_info = author_info.split(multiple_author_query)
+
+        author_infos = []
+
+        split_author_info.each do |author_str|
+          author_infos.push author_info(author_str)
+        end
+
+        return author_infos
+      end
+    end
+
+    def author_info(author_string)
+      seperator_query = " "
+      separator_index = author_string.index(seperator_query)
+
+      last_name = author_string
+      first_name = ""
+
+      if !separator_index.nil?
+        separator_index += seperator_query.length
+        split_author_info = author_string.split(seperator_query)
+        last_name = split_author_info[0]
+        first_name = split_author_info[1] 
+      end
+
+      return { last_name: last_name, first_name: first_name, suffix: "suffix" }
     end
   end
 end
