e5781b26bba7196ef03d0f774ddc0cfa77737cc2
Jim Tucker <tuckerjd@uiuc.edu>
2016-10-18 14:37:55 -0500

    More paging miss-steps.

--- app/helpers/tasks/collection_objects/area_and_date/area_and_date_helper.rb
+++ app/helpers/tasks/collection_objects/area_and_date/area_and_date_helper.rb
@@ -3,13 +3,13 @@ module Tasks::CollectionObjects::AreaAndDate::AreaAndDateHelper
   def paging_info
     # <p><%= page_entries_info(@collection_objects) %></p> <%= paginate @collection_objects %>
     if @collection_objects.any?
-      page_entries_info(@collection_objects)
+      page_entries_info(@collection_objects, :remote => true)
     end
   end
 
   def paging
     if @collection_objects.any?
-      paginate(@collection_objects)
+      paginate(@collection_objects, :remote => true)
     end
   end
 end
