045f4d69df606c26788ed07afe274799244214a3
Jim Tucker <tuckerjd@uiuc.edu>
2016-10-18 09:38:52 -0500

    First step to paging, 25 per, subsequent pages must be intercepted by JScript, and requested like the first one is. Also, needs paging to appear in the map div.

--- app/helpers/tasks/collection_objects/area_and_date/area_and_date_helper.rb
+++ app/helpers/tasks/collection_objects/area_and_date/area_and_date_helper.rb
@@ -3,13 +3,13 @@ module Tasks::CollectionObjects::AreaAndDate::AreaAndDateHelper
   def paging_info
     # <p><%= page_entries_info(@collection_objects) %></p> <%= paginate @collection_objects %>
     if @collection_objects.any?
-      page_entries_info(@collection_objects, :remote => true)
+      page_entries_info(@collection_objects)
     end
   end
 
   def paging
     if @collection_objects.any?
-      paginate(@collection_objects, :remote => true)
+      paginate(@collection_objects)
     end
   end
 end
