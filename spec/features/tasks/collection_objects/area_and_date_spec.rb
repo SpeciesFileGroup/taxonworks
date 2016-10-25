0a242493ee32ca5e164dcfda64a7bf74115c92f5
Jim Tucker <tuckerjd@uiuc.edu>
2016-10-25 09:19:46 -0500

    Broken feature spec.

--- spec/features/tasks/collection_objects/area_and_date_spec.rb
+++ spec/features/tasks/collection_objects/area_and_date_spec.rb
@@ -8,23 +8,31 @@ describe 'tasks/gis/collection_objects/area_and_date', type: :feature, group: [:
   }
   after(:all) { clean_slate_geo }
 
+  let!(:gnlm) { GeographicArea.where(name: 'Great Northern Land Mass').first }
+
   describe '#set_area' do
     it 'renders count of collection objects in a specific names area' do
       visit(index_area_and_date_task_path)
       # {geographic_area_id: GeographicArea.where(name: 'Great Northern Land Mass').first.id})
       # expect(response).to have_http_status(:success)
       # expect(JSON.parse(response.body)['html']).to eq('16')
+      fill_autocomplete('geographic_area_id_for_by_area', with: 'Great Northern', select: gnlm.id)
+      # expect(page).to have_content('Land Mass')
+      click_button('Set')
+      expect(page).to have_text('16')
     end
 
     it 'renders count of collection objects in a drawn area' do
       # {"type"=>"Feature", "geometry"=>{"type"=>"MultiPolygon", "coordinates"=>[[[[33, 28, 0], [37, 28, 0], [37, 26, 0], [33, 26, 0], [33, 28, 0]]]]}, "properties"=>{"geographic_item"=>{"id"=>23}}}
-      xhr(:get, :set_area, {drawn_area_shape: GeographicArea
-                                                .where(name: 'Big Boxia')
-                                                .first
-                                                .default_geographic_item
-                                                .to_geo_json_feature})
-      expect(response).to have_http_status(:success)
+      visit(index_area_and_date_task_path)
+      # xhr(:get, :set_area, {drawn_area_shape: GeographicArea
+      #                                           .where(name: 'Big Boxia')
+      #                                           .first
+      #                                           .default_geographic_item
+      #                                           .to_geo_json_feature})
+      # expect(response).to have_http_status(:success)
       # expect(JSON.parse(response.body)['html']).to eq('10')
+
     end
   end
 
