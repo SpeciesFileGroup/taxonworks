ab33ee9705f32b4f79a8745d5a4c877299508acd
jrflood <jrflood@illinois.edu>
2016-10-25 14:33:00 -0500

    Fixed #set_area by name test

--- spec/features/tasks/collection_objects/area_and_date_spec.rb
+++ spec/features/tasks/collection_objects/area_and_date_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 describe 'tasks/gis/collection_objects/area_and_date', type: :feature, group: [:geo, :collection_objects] do
-  
-  let(:page_index_name) { 'asserted distributions' }
+
+  let(:page_index_name) { 'collection objects by area and date' }
   let(:index_path) { index_area_and_date_task_path }
 
   it_behaves_like 'a_login_required_and_project_selected_controller'
@@ -18,7 +18,7 @@ describe 'tasks/gis/collection_objects/area_and_date', type: :feature, group: [:
 
     let!(:gnlm) { GeographicArea.where(name: 'Great Northern Land Mass').first }
 
-    describe '#set_area' do # , js: true
+    describe '#set_area', js: true do #
       it 'renders count of collection objects in a specific names area' do
         visit(index_area_and_date_task_path)
         # {geographic_area_id: GeographicArea.where(name: 'Great Northern Land Mass').first.id})
@@ -27,12 +27,15 @@ describe 'tasks/gis/collection_objects/area_and_date', type: :feature, group: [:
         fill_autocomplete('geographic_area_id_for_by_area', with: 'Great Northern', select: gnlm.id)
         # expect(page).to have_content('Land Mass')
         click_button('Set')
-        expect(page).to have_text('16')
+        expect(find('#area_count')).to have_text('16')
       end
 
       it 'renders count of collection objects in a drawn area' do
-        # {"type"=>"Feature", "geometry"=>{"type"=>"MultiPolygon", "coordinates"=>[[[[33, 28, 0], [37, 28, 0], [37, 26, 0], [33, 26, 0], [33, 28, 0]]]]}, "properties"=>{"geographic_item"=>{"id"=>23}}}
         visit(index_area_and_date_task_path)
+        find('.map_toggle').click
+        fill_in('drawn_area_shape', with: '{"type"=>"Feature", "geometry"=>{"type"=>"MultiPolygon", "coordinates"=>[[[[33, 28, 0], [37, 28, 0], [37, 26, 0], [33, 26, 0], [33, 28, 0]]]]}, "properties"=>{"geographic_item"=>{"id"=>23}}}')
+        click_button('Set')
+        expect(find('#area_count')).to have_text('16')
         # xhr(:get, :set_area, {drawn_area_shape: GeographicArea
         #                                           .where(name: 'Big Boxia')
         #                                           .first
