d0ace1f0c9e085bee67f5a0c6a368e64eddced4e
Jim Tucker <tuckerjd@uiuc.edu>
2016-10-27 10:33:55 -0500

    Created CollectionObject.from_collecting_events.

--- app/controllers/tasks/collection_objects/area_and_date_controller.rb
+++ app/controllers/tasks/collection_objects/area_and_date_controller.rb
@@ -6,11 +6,11 @@ class Tasks::CollectionObjects::AreaAndDateController < ApplicationController
     @geographic_areas         = GeographicArea.where('false')
     @collection_objects       = CollectionObject.where('false')
     @collection_objects_count = 0
-    @early_date               = CollectionObject.order(:created_at).limit(1).pluck(:created_at).first
+    @early_date               = CollectionObject.where(project: sessions_current_project_id).order(:created_at).limit(1).pluck(:created_at).first
     if @early_date.blank?
       @early_date = '1700/01/01'
     end
-    @late_date                = CollectionObject.order(created_at: :desc).limit(1).pluck(:created_at).first
+    @late_date = CollectionObject.where(project: sessions_current_project_id).order(created_at: :desc).limit(1).pluck(:created_at).first
     if @late_date.blank?
       @late_date = Date.today.to_date
     end
@@ -27,19 +27,16 @@ class Tasks::CollectionObjects::AreaAndDateController < ApplicationController
     set_and_order_dates(params)
 
     if @shape_in.blank? and @geographic_area_id.blank? # missing "? " was fixed
-      area_objects = CollectionObject.where('false')
+      area_object_ids = CollectionObject.where('false')
     else
-      area_objects = GeographicItem.gather_selected_data(@geographic_area_id, @shape_in, 'CollectionObject')
+      area_object_ids = GeographicItem.gather_selected_data(@geographic_area_id, @shape_in, 'CollectionObject').map(&:id)
     end
 
-    if (@start_date.blank? || @end_date.blank?) || area_objects.count == 0
+    if (@start_date.blank? || @end_date.blank?) || area_object_ids.count == 0
       @collection_objects = CollectionObject.where('false')
     else
-      collecting_events = CollectingEvent.in_date_range(date_range_params) #
-      @collection_objects = CollectionObject.includes(:collecting_event)
-                              .where(collecting_event_id: collecting_events)
-                              .where(id: area_objects.map(&:id))
-                              .page(params[:page])
+      collecting_event_ids = CollectingEvent.where(project: sessions_current_project_id).in_date_range(date_range_params).pluck(:id)
+      @collection_objects  = CollectionObject.from_collecting_events(collecting_event_ids, area_object_ids, sessions_current_project_id).page(params[:page])
     end
 
     @collection_objects_count = @collection_objects.count
@@ -65,7 +62,7 @@ class Tasks::CollectionObjects::AreaAndDateController < ApplicationController
   def set_date
     set_and_order_dates(params)
     # range_sql = CollectingEvent.date_sql_from_dates(@start_date, @end_date, false)
-    collecting_events = CollectingEvent.in_date_range(date_range_params)
+    collecting_events         = CollectingEvent.in_date_range(date_range_params)
     @collection_objects       = CollectionObject.includes(:collecting_event)
                                   .where(collecting_event_id: collecting_events)
     @collection_objects_count = @collection_objects.count
@@ -114,12 +111,12 @@ class Tasks::CollectionObjects::AreaAndDateController < ApplicationController
 
   def set_and_order_dates(params)
     @start_date = params[:search_start_date]
-    @end_date = params[:search_end_date]
+    @end_date   = params[:search_end_date]
     if (@start_date > @end_date)
       params[:search_start_date] = @end_date
-      params[:search_end_date] = @start_date
-      @start_date = params[:search_start_date]
-      @end_date = params[:search_end_date]
+      params[:search_end_date]   = @start_date
+      @start_date                = params[:search_start_date]
+      @end_date                  = params[:search_end_date]
     end
 
   end
