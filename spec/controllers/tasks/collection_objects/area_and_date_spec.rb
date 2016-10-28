19ffe31ad00a33600b1a058ddcc55a2796f9c644
Jim Tucker <tuckerjd@uiuc.edu>
2016-10-28 10:23:42 -0500

    Refactoring to reduce area_and_date_controller code.

--- spec/controllers/tasks/collection_objects/area_and_date_spec.rb
+++ spec/controllers/tasks/collection_objects/area_and_date_spec.rb
@@ -12,14 +12,14 @@ describe Tasks::CollectionObjects::AreaAndDateController, type: :controller, gro
   }
   after(:all) { clean_slate_geo }
 
-  xdescribe 'GET #index' do
+  describe 'GET #index' do
     it 'returns http success' do
       get(:index)
       expect(response).to have_http_status(:success)
     end
   end
 
-  xdescribe '#set_date' do
+  describe '#set_date' do
     it 'renders count of collection objects based on the start and end dates' do
       get(:set_date, {search_start_date: Date.parse('1971/01/01').to_s.gsub('-', '/'), search_end_date: Date.parse('1980/12/31').to_s.gsub('-', '/')})
       expect(response).to have_http_status(:success)
@@ -30,28 +30,17 @@ describe Tasks::CollectionObjects::AreaAndDateController, type: :controller, gro
   describe '#find' do
     let!(:params) {
       {search_start_date: '1971/01/01',
-       search_end_date: '1980/12/31',
-       drawn_area_shape: GeographicArea
-                             .where(name: 'West Boxia')
-                             .first
-                             .default_geographic_item
-                             .to_geo_json_feature}
+       search_end_date:   '1980/12/31',
+       drawn_area_shape:  GeographicArea
+                            .where(name: 'West Boxia')
+                            .first
+                            .default_geographic_item
+                            .to_geo_json_feature}
     }
     it 'renders dwca of the selected collection objects, and geo_json feature collection' do
 
       xhr(:get, :find, params)
       expect(response).to have_http_status(:success)
-      # result = JSON.parse(response.body)
-      # expect(result['collection_objects_count']).to eq('3')
-      # features          = result['feature_collection']['features']
-      # georeference_id   = features[0]['properties']['georeference']['id']
-      # collecting_events = [Georeference.find(georeference_id).collecting_event.verbatim_label]
-      # georeference_id   = features[1]['properties']['georeference']['id']
-      # collecting_events.push(Georeference.find(georeference_id).collecting_event.verbatim_label)
-      # georeference_id = features[2]['properties']['georeference']['id']
-      # collecting_events.push(Georeference.find(georeference_id).collecting_event.verbatim_label)
-      # expect(collecting_events).to include('@ce_m1', '@ce_m1a', '@ce_m2 in Big Boxia')
     end
-
   end
 end
