31882ce670c8b35391efb5f9adcedc235e5ced64
José Luis Pereira <pepelui@outlook.com>
2016-11-07 03:05:42 -0300

    Added expand and compress buttons js, CSS changes on lock and increment checkbox

--- app/assets/javascripts/workbench/display.js
+++ app/assets/javascripts/workbench/display.js
@@ -13,3 +13,33 @@ $(document).ready(function() {
 		render_attribute_set_headers()
 	}
 });
+
+//Expand buttons
+$(document).ready(function() {
+	$('[expand-container]').each(function(i) {
+
+		var id = $(this).attr('expand-container');
+
+		if($(id).css('display') == "none") {
+			$(this).addClass('expand');
+		}
+		else {
+			$(this).addClass('contract');
+		}
+	});
+
+	$('[expand-container]').on('click', function() {
+		
+		var id = $(this).attr('expand-container');
+
+		if($(id).css('display') == "none") {
+			$(this).addClass('contract');
+			$(this).removeClass('expand');
+		}
+		else {
+			$(this).removeClass('contract');
+			$(this).addClass('expand');
+		}
+		$(id).toggle(250);
+	});
+});
