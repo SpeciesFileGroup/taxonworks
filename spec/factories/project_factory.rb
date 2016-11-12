b22bd0286810d7757354938697067dde07c137d2
mjy <diapriid@gmail.com>
2016-11-12 09:30:08 -0600

    Randomize project names in valid_project factory.

--- spec/factories/project_factory.rb
+++ spec/factories/project_factory.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :project, class: Project, traits: [:creator_and_updater] do
     # Don't include a name here 
     factory :valid_project do
-      name 'My Project'
+      name { Faker::Lorem.words(4) }
     end
   end
 end
