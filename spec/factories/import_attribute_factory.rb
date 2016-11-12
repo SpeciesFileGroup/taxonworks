786ea827670dfde14f7d331fefb6c4845d3a220f
mjy <diapriid@gmail.com>
2016-11-12 09:33:38 -0600

    Add valid_data_attribute_import_attribute factory.

--- spec/factories/import_attribute_factory.rb
+++ spec/factories/import_attribute_factory.rb
@@ -1,3 +1,9 @@
 FactoryGirl.define do
-  factory :data_attribute_import_attribute, class: ImportAttribute, traits: [:creator_and_updater] 
+  factory :data_attribute_import_attribute, class: ImportAttribute, traits: [:creator_and_updater] do
+    factory :valid_data_attribute_import_attribute  do
+      import_predicate { Faker::Lorem.words(2).join(" ") }
+      value { Faker::Number.number(5) }
+      association :attribute_subject, factory: :valid_otu
+    end
+  end
 end
