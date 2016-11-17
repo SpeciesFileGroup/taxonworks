f0f4ab79fbb4562b956eeef5ea20c99d9d2cec27
Jim Tucker <tuckerjd@uiuc.edu>
2016-11-17 12:30:42 -0600

    First steps to add by_taxon_name to area and date page.

--- app/assets/javascripts/views/tasks/collection_objects/area_and_date/index.js
+++ app/assets/javascripts/views/tasks/collection_objects/area_and_date/index.js
@@ -29,10 +29,7 @@ _init_map_table = function init_map_table() {
   
       $("#set_taxon_name").click(function (event) {
           $("taxon_count").text('????');
-        $("select_taxon_name").mx_spinner('show');
-    
-        // do the GET here.
-        $("select_taxon_name").mx_spinner('hide');
+          $("select_taxon_name")
           event.preventDefault();
         }
       );
