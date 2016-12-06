0eab89604e1be365431235d3c550651098f1a122
mjy <diapriid@gmail.com>
2016-12-06 15:25:28 -0600

    Fix helper to return correctly.

--- app/helpers/loan_recipients_helper.rb
+++ app/helpers/loan_recipients_helper.rb
@@ -1,12 +1,13 @@
 module LoanRecipientsHelper
 
-  def loan_recipient_tag(loan_recipient)
-    return nil if loan_recipient.nil?
-    role_tag(loan_recipient)
+  def loan_recipient_tag(loan)
+    return nil if loan.nil?
+    recipients = loan.loan_recipients.collect{|lr| person_tag(lr)}.join.html_safe
+    recipients.blank? ? 'No recipients defined!' : recipients
   end
 
-  def loan_recipient_link(loan_recipient)
-    link_to(loan_recipient_tag(loan_recipient).html_safe, loan_recipient.role_object.metamorphosize)
+  def loan_recipient_link(loan)
+    link_to(loan_recipient_tag(loan).html_safe, loan)
   end
 
 end
