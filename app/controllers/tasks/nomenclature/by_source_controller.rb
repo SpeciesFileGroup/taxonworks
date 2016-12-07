0e672e330519f5ad213fb80768d54cd535fdd77a
mjy <diapriid@gmail.com>
2016-12-07 13:58:00 -0600

    Handle project having now sources for browse nomenclature by source.

--- app/controllers/tasks/nomenclature/by_source_controller.rb
+++ app/controllers/tasks/nomenclature/by_source_controller.rb
@@ -1,10 +1,15 @@
 class Tasks::Nomenclature::BySourceController < ApplicationController
   include TaskControllerConfiguration
 
-  def index 
+  def index
+
+    if !ProjectSource.where(project_id: sessions_current_project_id).any? 
+      flash[:notice] = 'Create or add a source to your project first.'
+      redirect_to sources_path and return
+    end 
+
     @source = Source.find(params[:id]) if !params[:id].blank?
     @source ||= Project.find(sessions_current_project_id).project_sources.first.source
-    # @data = NomenclatureCatalog.data_for(@taxon_name)
   end
 
   protected
