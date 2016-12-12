4d5de247cded9795b8cbe967e09eb3ccaedfcc1c
mjy <diapriid@gmail.com>
2016-12-12 15:54:40 -0600

    Manual merge content experiment branch.

--- app/helpers/otu_page_layout_sections_helper.rb
+++ app/helpers/otu_page_layout_sections_helper.rb
@@ -5,4 +5,28 @@ module OtuPageLayoutSectionsHelper
     otu_page_layout_section.otu_page_layout.name + ": " + otu_page_layout_section.topic.name
   end
 
+#  def section_content_tag(otu_page_layout_section, contents)
+#    s = otu_page_layout_section
+#    return nil if s.nil?
+#
+#    case s.kind
+#
+#    when :standard
+#      # bad loop
+#      contents.each do |c|
+#        if c.topic == s.topic
+#         return  render('contents/block', content: c) 
+#        end
+#      end
+#
+#    when :dynamic
+#      return render(s.partial) 
+#    else
+#      return "oops"
+#    end
+#
+#  end
+
+
+
 end
