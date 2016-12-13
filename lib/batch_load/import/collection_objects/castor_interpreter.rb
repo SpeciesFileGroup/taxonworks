03975104bea7f5b094787d5fc111c2ae16e39840
Fauzi Kliman <fauzimerfoo@gmail.com>
2016-12-13 11:21:49 -0800

    Creates taxon determination between all collection objects and otus attached to a taxon name

--- lib/batch_load/import/collection_objects/castor_interpreter.rb
+++ lib/batch_load/import/collection_objects/castor_interpreter.rb
@@ -29,6 +29,7 @@ module BatchLoad
         parse_result = BatchLoad::RowParse.new
         parse_result.objects[:collection_object] = []
         parse_result.objects[:extract] = []
+        parse_result.objects[:taxon_determination] = []
 
         @processed_rows[i] = parse_result
 
@@ -60,7 +61,7 @@ module BatchLoad
 
           # Text for collection object identifiers
           co_identifier_castor_guid_text = row['guid']
-          co_identifier_castor_taxon_guid_text = row['taxon_guid']
+          # co_identifier_castor_taxon_guid_text = row['taxon_guid']
           co_identifier_morphbank_text = row['morphbank_specimen_id']
           co_identifier_drm_field_voucher_text = row['specimen_number']
           co_identifier_drm_lab_voucher_text = "#{row['voucher_number_prefix']}#{row['voucher_number_stirng']}"
@@ -70,9 +71,9 @@ module BatchLoad
                                    type: 'Identifier::Local::CollectionObject',
                                    identifier: co_identifier_castor_guid_text }
 
-          co_identifier_castor_taxon_guid = { namespace: namespace_castor,
-                                   type: 'Identifier::Local::TaxonConcept',
-                                   identifier: co_identifier_castor_taxon_guid_text }
+          # co_identifier_castor_taxon_guid = { namespace: namespace_castor,
+          #                          type: 'Identifier::Local::TaxonConcept',
+          #                          identifier: co_identifier_castor_taxon_guid_text }
 
           co_identifier_morphbank = { namespace: namespace_morphbank,
                                       type: 'Identifier::Local::CollectionObject',
@@ -92,7 +93,7 @@ module BatchLoad
           # Collection object
           co_identifiers = []
           co_identifiers.push(co_identifier_castor_guid)        if !co_identifier_castor_guid_text.blank?
-          co_identifiers.push(co_identifier_castor_taxon_guid)  if !co_identifier_castor_taxon_guid_text.blank?
+          # co_identifiers.push(co_identifier_castor_taxon_guid)  if !co_identifier_castor_taxon_guid_text.blank?
           co_identifiers.push(co_identifier_morphbank)          if !co_identifier_morphbank_text.blank?
           co_identifiers.push(co_identifier_drm_field_voucher)  if !co_identifier_drm_field_voucher_text.blank?
           co_identifiers.push(co_identifier_drm_lab_voucher)    if !co_identifier_drm_lab_voucher_text.blank?
@@ -108,6 +109,19 @@ module BatchLoad
 
           parse_result.objects[:collection_object].push(co);
 
+          # Taxon determination between this object and taxon name otus this object belongs to
+          taxon_names = TaxonName.with_namespaced_identifier('Castor', row["taxon_guid"])
+          taxon_name = nil
+          taxon_name = taxon_names.first if taxon_names.any?
+
+          if taxon_name
+            otus = taxon_name.otus
+
+            otus.each do |otu|
+              taxon_determination = TaxonDetermination.new(otu: otu, biological_collection_object: co)
+              parse_result.objects[:taxon_determination].push(taxon_determination) if taxon_name
+            end
+          end
         #rescue
            # ....
            # puts "SOMETHING WENT WRONG WITH COLLECTION OBJECT castor INTERPRETER BATCH LOAD"
