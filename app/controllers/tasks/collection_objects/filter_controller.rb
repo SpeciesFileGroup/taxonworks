5e46bbbd87ef5a0492ca0b3fd5a016af016b9e1f
mjy <diapriid@gmail.com>
2017-01-13 13:00:58 -0600

    Strike order() until we can resolve the grouping problem.

--- app/controllers/tasks/collection_objects/filter_controller.rb
+++ app/controllers/tasks/collection_objects/filter_controller.rb
@@ -39,7 +39,6 @@ class Tasks::CollectionObjects::FilterController < ApplicationController
     Queries::CollectionObjectFilterQuery.new(filter_params)
       .result
       .with_project_id(sessions_current_project_id)
-      .order('collection_objects.id')
       .includes(:repository, {taxon_determinations: [{otu: :taxon_name}]}, :identifiers)
   end
 
