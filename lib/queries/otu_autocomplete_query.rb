1945f76c73d96c615feecee4aad70b46d5be0661
mjy <diapriid@gmail.com>
2017-01-17 16:38:03 -0600

    Simplify Otu autocomplete/find by name queries.  In general move to pattern that references Queries:: directly, rather than through corresponding model with single purpose alias (e.g. Otu.find_for_autocomplete).

--- lib/queries/otu_autocomplete_query.rb
+++ lib/queries/otu_autocomplete_query.rb
@@ -11,36 +11,14 @@ module Queries
   #    
 
   # Lots of optimization possible, at minimum this is nice for nested OR
-  class OtuAutocompleteQuery
+  class OtuAutocompleteQuery < Queries::Query
     include Arel::Nodes
 
-    attr_accessor :terms
-
-    def initialize(string)
-      build_terms(string)
-    end
-
-    def terms=(string)
-      build_terms(string)
-    end
-
-    def build_terms(string)
-      @terms = string.split(/\s/).collect{|t| [t, "#{t}%"]}.flatten # , "#{t}%", "%#{t}%"
-    end
-
-    # All sorts of messed up, badly miss-maps attributes
-    # def query
-    #   table.join(taxon_name_table, Arel::Nodes::OuterJoin)
-    #     .on(table[:id].eq(taxon_name_table[:id]))
-    #     .where(named.or(taxon_name_named))
-    #     .take(40)
-    #     .project(table['name'], taxon_name_table['name'])
-    # end
-
     def where_sql
-      named.or(taxon_name_named).to_sql
+      with_project_id.and(named.or(taxon_name_named)).to_sql
     end
 
+    # @return [Scope]
     def all 
       # For references, this is equivalent: Otu.eager_load(:taxon_name).where(where_sql) 
       Otu.includes(:taxon_name).where(where_sql).references(:taxon_names).order(name: :asc).order('taxon_names.cached ASC')
@@ -54,12 +32,8 @@ module Queries
       Otu.arel_table
     end
 
-    def named
-      table[:name].matches_any(@terms)
-    end
-
     def taxon_name_named
-      taxon_name_table[:name].matches_any(@terms)
+      taxon_name_table[:name].matches_any(terms)
     end
   end
 end
