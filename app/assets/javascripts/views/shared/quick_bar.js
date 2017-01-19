9145504d44ea96e958cacca47051264eb6edb7eb
José Luis Pereira <pepelui@outlook.com>
2017-01-19 16:37:47 -0300

    Refactor quick_bar.js to the new pattern

--- app/assets/javascripts/views/shared/quick_bar.js
+++ app/assets/javascripts/views/shared/quick_bar.js
@@ -1,36 +1,54 @@
-$(document).ready(function() {
+var TW = TW || {};
+TW.views = TW.views || {};
+TW.views.shared = TW.views.shared || {};
+TW.views.shared.quick_bar = TW.views.shared.quick_bar || {};
 
-	if($("#quick_bar").length) {
+Object.assign(TW.views.shared.quick_bar, {
 
-		TW.workbench.keyboard.createShortcut("alt+h","Show/hide quick bar", "General shortcuts", function() {hideQuickBar()} );
+	init_quickBar: function() {
+		if($("#quick_bar").length) {
 
-		$('body').append("<div class='button-collapse-header'></div>");
-		$('.button-collapse-header').on('click', function() {
-			hideQuickBar(250);
-		});
+			TW.workbench.keyboard.createShortcut("alt+h","Show/hide quick bar", "General shortcuts", function() { TW.views.shared.quick_bar.hideQuickBar() } );
 
-		function hideQuickBar(time) {
-
-			if($('.button-collapse-header').css('display') == "none") {
-				$('.button-collapse-header').fadeIn(time);
-			}
-			else {
-				$('.button-collapse-header').fadeOut(0);
-			}			
-			$('#quick_bar').slideToggle(time);
-			$('#header_bar').slideToggle(time);
-			$('.task_bar').slideToggle(time);
+			$('body').append("<div class='button-collapse-header'></div>");
+			$('.button-collapse-header').on('click', function() {
+				TW.views.shared.quick_bar.hideQuickBar(250);
+			});
 		}
 
 		Mousetrap.bind('alt+ctrl+h', function(e) {
 			window.location = "/hub";
-		});
+		});			
 
 		if((window.location.href).indexOf('/tasks/') > 0) {
-			hideQuickBar(0);
+			this.handleEvents();
+			this.hideQuickBar(0);
 		}
+	},
 
 
+	handleEvents: function() {
+		$(".button-collapse-header").one("mouseenter", function() {
+			TW.views.shared.quick_bar.hideQuickBar();
+		});		
+	},
+
+	hideQuickBar: function(time) {
+
+		if($('.button-collapse-header').css('display') == "none") {
+			$('.button-collapse-header').fadeIn(time);
+		}
+		else {
+			$('.button-collapse-header').fadeOut(0);
+		}			
+		$('#quick_bar').slideToggle(time);
+		$('#header_bar').slideToggle(time);
+		$('.task_bar').slideToggle(time);
 	}
+	
+});
+
+$(document).ready( function() {
+	TW.views.shared.quick_bar.init_quickBar();
 });
 
