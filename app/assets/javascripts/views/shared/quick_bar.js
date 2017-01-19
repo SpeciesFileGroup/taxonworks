29b425e86c7269f082602b894030633013a0d491
José Luis Pereira <pepelui@outlook.com>
2017-01-19 19:38:53 -0300

    Improvements on quick bar hidden feature

--- app/assets/javascripts/views/shared/quick_bar.js
+++ app/assets/javascripts/views/shared/quick_bar.js
@@ -6,41 +6,76 @@ TW.views.shared.quick_bar = TW.views.shared.quick_bar || {};
 Object.assign(TW.views.shared.quick_bar, {
 
 	init_quickBar: function() {
+		var timer;		
+									
 		if($("#quick_bar").length) {
 
-			TW.workbench.keyboard.createShortcut("alt+h","Show/hide quick bar", "General shortcuts", function() { TW.views.shared.quick_bar.hideQuickBar() } );
+			TW.workbench.keyboard.createShortcut("alt+h","Show/hide quick bar", "General shortcuts", function(that) { 
+				TW.views.shared.quick_bar.hideShowBars();
+			});
 
 			$('body').append("<div class='button-collapse-header'></div>");
-			$('.button-collapse-header').on('click', function() {
-				TW.views.shared.quick_bar.hideQuickBar(250);
-			});
 		}
 
 		Mousetrap.bind('alt+ctrl+h', function(e) {
 			window.location = "/hub";
 		});			
 
-		if((window.location.href).indexOf('/tasks/') > 0) {
-			this.handleEvents();
-			this.hideQuickBar(0);
+		if((window.location.href).indexOf('/tasks/') > 0) {		
+			//hideBar = true;
+			this.hideShowBars(0);				
 		}
+
+		$('.button-collapse-header').on("mouseenter", function() {
+			if($("#quick_bar").attr("data-hide-quickbar-active")) {
+		        if(timer) {
+		            clearTimeout(timer);
+		            timer = null
+		        }		
+		        timer = setTimeout( function() {		
+					if(!$("#quick_bar").attr("data-hide-quickbar")) {
+						$("#quick_bar").attr("data-hide-quickbar", "true");
+						TW.views.shared.quick_bar.hideQuickBar();
+					}
+				}, 200);
+			}
+		});	
+		$('#quick_bar, .button-collapse-header, #header_bar').on("mouseout", function() {
+			if($("#quick_bar").attr("data-hide-quickbar-active")) {
+		        if(timer) {
+		            clearTimeout(timer);
+		            timer = null
+		        }
+                timer = setTimeout( function() {
+				    if (!TW.views.shared.quick_bar.checkHover()) {
+				    	$("#quick_bar").removeAttr("data-hide-quickbar");
+						TW.views.shared.quick_bar.hideQuickBar();
+				    }	
+				}, 200);	
+			}	    		
+		});	
 	},
 
-
-	handleEvents: function() {
-		$(".button-collapse-header").one("mouseenter", function() {
-			TW.views.shared.quick_bar.hideQuickBar();
-		});		
-	},
-
-	hideQuickBar: function(time) {
-
-		if($('.button-collapse-header').css('display') == "none") {
-			$('.button-collapse-header').fadeIn(time);
+	hideShowBars: function(time) {
+		if(!$("#quick_bar").attr("data-hide-quickbar-active")) {
+			$("#quick_bar").attr("data-hide-quickbar-active", "true");
 		}
 		else {
-			$('.button-collapse-header').fadeOut(0);
-		}			
+			$("#quick_bar").removeAttr("data-hide-quickbar-active");
+		}
+		TW.views.shared.quick_bar.hideQuickBar(time);
+	},
+
+	checkHover: function() {
+		if($('#quick_bar').is(':hover') || $('.button-collapse-header').is(':hover') || $('#header_bar').is(':hover')) {
+			//alert();
+			return true;
+		}
+		return false;
+	},
+
+	hideQuickBar: function(time) {	
+		$('.button-collapse-header').slideToggle(time);		
 		$('#quick_bar').slideToggle(time);
 		$('#header_bar').slideToggle(time);
 		$('.task_bar').slideToggle(time);
@@ -49,6 +84,7 @@ Object.assign(TW.views.shared.quick_bar, {
 });
 
 $(document).ready( function() {
-	TW.views.shared.quick_bar.init_quickBar();
+	var init_quickBar = TW.views.shared.quick_bar;
+	init_quickBar.init_quickBar();
 });
 
