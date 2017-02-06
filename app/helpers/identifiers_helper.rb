96359288862af9cc2915ae8835a9da3827c20fae
mjy <diapriid@gmail.com>
2017-02-06 11:24:39 -0600

    Remove reference to unused param.

--- app/helpers/identifiers_helper.rb
+++ app/helpers/identifiers_helper.rb
@@ -15,9 +15,7 @@ module IdentifiersHelper
     end
   end
 
-
-
-  def add_identifier_link(object: nil, attribute: nil)
+  def add_identifier_link(object: nil)
     link_to('Add identifier', new_identifier_path(
                                 identifier: {
                                     identifier_object_type: object.class.base_class.name,
