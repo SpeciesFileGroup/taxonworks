e0ff46a312f64c8949b9a90ee3861e3a6b001ca2
José Luis Pereira <pepelui@outlook.com>
2017-02-24 14:37:37 -0300

    Added nomenclature class on task filter

--- app/assets/javascripts/views/hub/task-filter.js
+++ app/assets/javascripts/views/hub/task-filter.js
@@ -52,7 +52,7 @@ var CarrouselTask = function (sec, rows, columns) {
     this.maxRow = maxRow;
     this.maxCards = maxRow * maxColumns;
     this.maxColumns = maxColumns;    
-    $(this.sectionTag).children(".task-section").css("width",((maxColumns*427) + "px"));
+    $(this.sectionTag).children(".task-section").css("width",((maxColumns*427.5) + "px"));
     if(maxRow < 99){
       $(this.sectionTag).children(".task-section").css("height",((maxRow*160) + "px"));
     }
