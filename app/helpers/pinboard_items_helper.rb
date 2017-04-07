1398e8587df051eee31402c50d51cc69200a3ebf
José Luis Pereira <pepelui@outlook.com>
2017-04-07 03:43:27 -0300

    ajax on pinboard buttons

--- app/helpers/pinboard_items_helper.rb
+++ app/helpers/pinboard_items_helper.rb
@@ -2,9 +2,9 @@ module PinboardItemsHelper
 
   def pin_item_to_pinboard_link(object, user)
     if !object.pinned?(user)
-      link_to(content_tag(:span, '', class: 'pin-button', data: { "pin-item-id": object.id }), pinboard_items_path(pinboard_item: {pinned_object_id: object.id, pinned_object_type: object.metamorphosize.class.name}), class: 'navigation-item' , method: :post)
+      link_to(content_tag(:span, '', class: 'pin-button'), pinboard_items_path(pinboard_item: {pinned_object_id: object.id, pinned_object_type: object.metamorphosize.class.name}), data: { "pin-button-item-id": object.id }, class: 'navigation-item', remote: true, method: :post)
     else
-      link_to('', pinboard_item_path(get_pinboard_item_form_object(object,user)), class: 'unpin-button', data: { "pin-item-id": object.id }, method: :delete)
+      link_to('', pinboard_item_path(get_pinboard_item_form_object(object,user)), class: 'unpin-button', data: { "pin-button-item-id": object.id }, method: :delete, remote: true)
     end
   end
 
