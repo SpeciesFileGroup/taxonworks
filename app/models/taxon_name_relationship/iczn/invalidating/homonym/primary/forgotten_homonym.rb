65b44212b2de5470788009184d48382bef6882b9
Dmitry Dmitriev <arboridia@gmail.com>
2017-04-13 14:38:02 -0500

    Added two subclasses of primary homonyms

---
+++ app/models/taxon_name_relationship/iczn/invalidating/homonym/primary/forgotten_homonym.rb
@@ -0,0 +1,34 @@
+class TaxonNameRelationship::Iczn::Invalidating::Homonym::Primary::ForgottenHomonym < TaxonNameRelationship::Iczn::Invalidating::Homonym::Primary
+
+  NOMEN_URI='http://purl.obolibrary.org/obo/NOMEN_0000106'
+
+  def self.disjoint_taxon_name_relationships
+    self.parent.disjoint_taxon_name_relationships +
+        self.collect_to_s(TaxonNameRelationship::Iczn::Invalidating::Homonym::Primary) +
+        self.collect_to_s(TaxonNameRelationship::Iczn::Invalidating::Homonym::Primary::SuppressedHomonym)
+  end
+
+  def object_status
+    'protected primary homonym'
+  end
+
+  def subject_status
+    'forgotten primary homonym'
+  end
+
+  def self.assignment_method
+    # bus.set_as_iczn_forgotten_homonym_of(aus)
+    :iczn_set_as_forgotten_homonym_of
+  end
+
+  def self.inverse_assignment_method
+    # aus.iczn_forgotten_homonym = bus
+    :iczn_forgotten_homonym
+  end
+
+  def self.nomenclatural_priority
+    :reverse
+  end
+
+
+end
