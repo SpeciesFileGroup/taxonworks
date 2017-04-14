c51712189d4ccdcdc529824fe8a215c7a50a0352
mjy <diapriid@gmail.com>
2017-04-14 09:20:42 -0500

    descriptors for matrices

--- app/models/matrix.rb
+++ app/models/matrix.rb
@@ -12,4 +12,9 @@ class Matrix < ActiveRecord::Base
   has_many :matrix_column_items, dependent: :destroy
   has_many :matrix_row_items, dependent: :destroy
 
+  has_many :matrix_rows
+  has_many :matrix_columns
+
+  has_many :descriptors, through: :matrix_columns 
+
 end
