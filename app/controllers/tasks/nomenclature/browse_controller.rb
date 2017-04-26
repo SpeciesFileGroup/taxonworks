dc00661f3f7f217a3215af98e54a15f81bbc5784
mjy <diapriid@gmail.com>
2017-04-26 10:40:12 -0500

    scope browse nomenclature to project, fix summary text

--- app/controllers/tasks/nomenclature/browse_controller.rb
+++ app/controllers/tasks/nomenclature/browse_controller.rb
@@ -2,7 +2,7 @@ class Tasks::Nomenclature::BrowseController < ApplicationController
   include TaskControllerConfiguration
 
   def index 
-    @taxon_name = TaxonName.find(params[:id]) if !params[:id].blank?
+    @taxon_name = TaxonName.find(params[:id]).where(project_id: sessions_current_project_id) if !params[:id].blank?
     @taxon_name ||= Project.find(sessions_current_project_id).root_taxon_name
     @data = NomenclatureCatalog.data_for(@taxon_name)
   end
