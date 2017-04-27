e2371b9ccf052a59096743a56d56a8d398c1a818
mjy <diapriid@gmail.com>
2017-04-27 10:58:45 -0500

    Add permissive headers to API calls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,6 +38,12 @@ class ApplicationController < ActionController::Base
         render(json: {success: false}, status: :unauthorized) && return
       end
     end
+
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+
     true
   end
 
