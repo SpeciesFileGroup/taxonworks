8f81e8119e0bfcd3dfaa43a8aca2c95423223c01
José Luis Pereira <pepelui@outlook.com>
2017-05-08 23:37:40 -0300

    Changes on browserify

--- config/initializers/browserify.rb
+++ config/initializers/browserify.rb
@@ -1,3 +1,3 @@
 Rails.application.config.tap do |c|
-  c.browserify_rails.commandline_options = "-t [ vueify --extensions .vue ] -t [ babelify --presets [ es2015 stage-0 ] --extensions .js --plugins [ syntax-async-functions transform-regenerator ] ]"
+  c.browserify_rails.commandline_options = "-t [ vueify --extensions .vue ] -t [ babelify --presets [ es2015 ] --extensions .js ]"
 end
