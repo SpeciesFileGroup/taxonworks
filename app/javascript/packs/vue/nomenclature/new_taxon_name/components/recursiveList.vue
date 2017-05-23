7cc419696d10bcda16e76a0f1db80ba332f71d6c
José Luis Pereira <pepelui@outlook.com>
2017-05-23 00:32:49 -0300

    Added tree view in Status picker #53

---
+++ app/javascript/packs/vue/nomenclature/new_taxon_name/components/recursiveList.vue
@@ -0,0 +1,23 @@
+<template>
+	<ul class="tree-status">
+		<li v-for="item, key in objectList">
+			<button type="button" :value="item.type" :disabled="item.disabled" class="button button-default"> 
+				{{ item.name }} 
+			</button>
+			<status-list :objectList="item"></status-list>
+		</li>
+	</ul>
+</template>
+
+<script>
+var recursiveList = require('./recursiveList.vue');
+
+export default {
+	components: {
+		statusList: recursiveList
+	},
+	name: 'status-list',
+	props: ['objectList'],
+}
+
+</script>
--- app/javascript/packs/vue/nomenclature/new_taxon_name/components/statusSelector.vue
+++ app/javascript/packs/vue/nomenclature/new_taxon_name/components/statusSelector.vue
@@ -1,7 +1,16 @@
 <template>
+
   <form class="content" v-if="rankClass">
+    <modal v-if="showModal" @close="showModal = false">
+      <h3 slot="header">Status</h3>
+      <div slot="body">
+        <recursive-list :objectList="tree"></recursive-list>
+      </div>
+    </modal>
     <h3>Status</h3>
-    <button type="button" @click="showAdvance = false">Common</button><button @click="showAdvance = true" type="button">Advance</button>
+    <button type="button" @click="showAdvance = false">Common</button>
+    <button @click="showAdvance = true" type="button">Advance</button>
+    <button @click="showModal = true" type="button">Show all</button>
     <div>
       <autocomplete v-if="showAdvance"
         :arrayList="allList"
@@ -26,10 +35,14 @@
   const GetterNames = require('../store/getters/getters').GetterNames;
   const MutationNames = require('../store/mutations/mutations').MutationNames;  
   const autocomplete = require('../../../components/autocomplete.vue');
+  const recursiveList = require('./recursiveList.vue');
+  const modal = require('../../../components/modal.vue');
 
   export default {
     components: {
-      autocomplete
+      autocomplete,
+      recursiveList,
+      modal
     },
     computed: {
       rankClass() {
@@ -48,32 +61,66 @@
       return {
         allList: [],
         commonList: [],
-        showAdvance: false
+        tree: undefined,
+        showAdvance: false,
+        showModal: false,
       }
     },
     watch: {
       'parent': function(newVal, oldVal) {
-        this.allList = this.getStatusListForThisRank(this.statusList[this.parent.nomenclatural_code].all,this.parent.rank_string);
-        this.commonList = this.getStatusListForThisRank(this.statusList[this.parent.nomenclatural_code].common,this.parent.rank_string);
+        this.tree = Object.assign({}, this.statusList[this.parent.nomenclatural_code].tree);
+        
+        this.getStatusListForThisRank(this.statusList[this.parent.nomenclatural_code].all,this.parent.rank_string).then( resolve => {
+          this.allList = resolve;
+          this.getTreeListForThisRank(this.tree, this.statusList[this.parent.nomenclatural_code].all, resolve);
+        });
+        this.getStatusListForThisRank(this.statusList[this.parent.nomenclatural_code].common,this.parent.rank_string).then( resolve => {
+          this.commonList = resolve;
+        });
       }
+    },
+    mounted: function() {
+      var that = this;
+      this.$on('closeModal', function () {
+        that.showModal = false;
+      }) 
     },
     methods: {
       getStatusListForThisRank(list, findStatus) {
-        var 
-        newList = [];
+        return new Promise(function (resolve, reject) {
+          var 
+          newList = [];
 
-        for (var key in list) {
-          var t = list[key].applicable_ranks;
-          t.find(function (item) {
+          for (var key in list) {
+            var t = list[key].applicable_ranks;
+            t.find(function (item) {
+              if(item == findStatus) {
+                newList.push(list[key]);
+                return true
+              }
+            });
+          };
+        resolve(newList);
+        });
+      },
+      getTreeListForThisRank(list, ranksList, filteredList) {
+        for(var key in list) {
+            Object.defineProperty(list[key], 'name', { value: ranksList[key].name });
+            Object.defineProperty(list[key], 'type', { value: ranksList[key].type });
 
-            if(item == findStatus) {
-              console.log(item);
-              newList.push(list[key]);
-              return true
+            if(filteredList.find(function(item) {
+              if(item.type == key) {
+                return true;
+              }
+            })) {
+              Object.defineProperty(list[key], 'disabled', { value: false });
             }
-          });
-        };
-        return newList;
+            else {
+              Object.defineProperty(list[key], 'disabled', { value: true });
+            }
+
+            this.getTreeListForThisRank(list[key], ranksList, filteredList);
+        }
       }
     }
   };
