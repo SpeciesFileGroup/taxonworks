2e387304ecf3e7cd3badcd23c8ea2ddb7341f8f0
mjy <diapriid@gmail.com>
2017-05-24 13:43:08 -0500

    Pushing missing files to stop some bleeding...

---
+++ app/helpers/annotations_helper.rb
@@ -0,0 +1,17 @@
+# Helpers that wrap sets of annotations of different types.
+module AnnotationsHelper 
+
+  # @return [String]
+  # Assumes the context is the object, not a multi-object summary
+  def annotations_summary_tag(object)
+    content_tag(:h2, 'Annotations', class: [:annotation__summary, 'title-section']) +
+      ( 
+       identifier_list_tag(object) +
+       data_attribute_list_tag(object) +
+       note_list_tag(object) +
+       tag_list_tag(object) 
+      ).html_safe
+  end
+
+
+end
