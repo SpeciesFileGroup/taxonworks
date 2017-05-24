eadb992b0dde377cacf3c0f4ea800c80d4a0d684
mjy <diapriid@gmail.com>
2017-05-24 14:30:02 -0500

    Fix syntax error, at least things should not raise everywhere.

--- app/helpers/annotations_helper.rb
+++ app/helpers/annotations_helper.rb
@@ -4,20 +4,22 @@ module AnnotationsHelper
   # @return [String]
   # Assumes the context is the object, not a multi-object summary
   def annotations_summary_tag(object)
-    content_tag(:div, class: [:content information
+    content_tag(:div, class: [:content]) + 
     content_tag(:h2, 'Annotations', class: [:annotation__summary, 'title-section']) +
       ( 
        identifier_list_tag(object) +
        data_attribute_list_tag(object) +
        note_list_tag(object) +
        tag_list_tag(object) 
+      ).html_safe
+
 
     # depictions
     # confidences
     # protocols
     # alternate values
 
-      ).html_safe
+
   end
 
 
