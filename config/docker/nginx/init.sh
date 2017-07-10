8aae7fbea06f881874e69902eb72201c2c91f92b
mjy <diapriid@gmail.com>
2017-07-10 07:39:21 -0600

    values for init.sh

--- config/docker/nginx/init.sh
+++ config/docker/nginx/init.sh
@@ -12,4 +12,4 @@ bundle exec erb /app/config/docker/secrets.yml.erb > /app/config/secrets.yml
 bundle exec erb /app/config/docker/database.yml.erb > /app/config/database.yml
 bundle exec erb /app/config/docker/application_settings.yml.erb > /app/config/application_settings.yml
 
-bundle exec rake tw:production:deploy:update_database
+bundle exec rake tw:production:deploy:update_database db_user=taxonworks_production database_host=db
