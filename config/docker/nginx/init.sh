0167ec943584fa9cb2ee5eeade4abd42ddfe34af
mjy <diapriid@gmail.com>
2017-07-10 10:55:26 -0600

    debug

--- config/docker/nginx/init.sh
+++ config/docker/nginx/init.sh
@@ -15,4 +15,7 @@ bundle exec erb /app/config/docker/application_settings.yml.erb > /app/config/ap
 bundle exec erb /app/config/docker/pgpass.erb > /root/.pgpass
 chmod 0600 /root/.pgpass
 
+ls -al /root
+cat /root/.pgpass
+
 bundle exec rake tw:production:deploy:update_database database_user=$POSTGRES_USER database_host=$POSTGRES_HOST 
