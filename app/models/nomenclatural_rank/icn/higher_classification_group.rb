a6425b42e2014b889acfa6ff0aa953dec364bd22
mjy <diapriid@gmail.com>
2017-07-09 23:51:41 -0600

    Check for presence before length/attributes. #53

--- app/models/nomenclatural_rank/icn/higher_classification_group.rb
+++ app/models/nomenclatural_rank/icn/higher_classification_group.rb
@@ -1,8 +1,10 @@
 class NomenclaturalRank::Icn::HigherClassificationGroup < NomenclaturalRank::Icn
 
   def self.validate_name_format(taxon_name)
-    taxon_name.errors.add(:name, 'must be capitalized') unless  !taxon_name.name.blank? && taxon_name.name == taxon_name.name.capitalize
-    taxon_name.errors.add(:name, 'must be at least two letters') unless taxon_name.name.length > 1
+    if taxon_name.present?
+      taxon_name.errors.add(:name, 'must be capitalized') unless taxon_name.name == taxon_name.name.capitalize
+      taxon_name.errors.add(:name, 'must be at least two letters') unless taxon_name.name.length > 1
+    end
   end
 
   def self.valid_parents
