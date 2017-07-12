99da4de580ef862bfb93b2e33c07c57c36c38969
mjy <diapriid@gmail.com>
2017-07-12 15:26:06 -0600

    Fix bad param for sqed.

--- app/models/sqed_depiction.rb
+++ app/models/sqed_depiction.rb
@@ -87,7 +87,7 @@ class SqedDepiction < ActiveRecord::Base
   # @return [Array of symbols]
   #   the (named) sections in this depiction that may have collecting event label metadata
   def collecting_event_sections
-    [:collecting_event_labels, :annotated_specimen] & extraction_metadata[:metadata_map].values
+    [:collecting_event_labels, :annotated_specimen] & extraction_metadata[:target_metadata_map].values
   end
 
   def nearby_sqed_depictions(before = 5, after = 5)
