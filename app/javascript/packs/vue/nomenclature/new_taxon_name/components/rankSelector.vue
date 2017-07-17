7e3fe6f6654e77ff12eb674bffce0d8fa9923603
José Luis Pereira <pepelui@outlook.com>
2017-07-17 15:19:59 -0300

    removed more and less options on rank selection

--- app/javascript/packs/vue/nomenclature/new_taxon_name/components/rankSelector.vue
+++ app/javascript/packs/vue/nomenclature/new_taxon_name/components/rankSelector.vue
@@ -1,17 +1,9 @@
 <template>
   <div class="field ranks-list" v-if="parent && rankGroup && ranks[rankGroup].length">
     <h4>Rank</h4>
-    <ul class="no_bullet " v-for="(group, key) in Object.keys(this.ranks)" v-if="!isMajor(rankGroup, group) && (showAll || group == childOfParent[rankGroup])">
-      <li v-if="((extendChildsList || (ranks[childOfParent[rankGroup]].lenght < maxChildsDisplay)) && (group == childOfParent[rankGroup]))">
-        <label class="middle" v-if="!showAll"><input type="radio" name="extendChild" @click="showAll = true" :checked="false"/> all... </label>
-        <label class="middle" v-else><input type="radio" name="extendChild" @click="showAll = false" :checked="false"/> less... </label>
-      </li>
-      <li v-for="(child, index) in (ranks[group])" v-if="(child.typical_use && (extendChildsList || (index < maxChildsDisplay)))">
+    <ul class="no_bullet " v-for="(group, key) in Object.keys(this.ranks)" v-if="!isMajor(rankGroup, group)">
+      <li v-for="(child, index) in (ranks[group])" v-if="child.typical_use">
         <label class="middle"><input type="radio" name="rankSelected" v-model="setRankClass" :checked="child.rank_class == this.setRankClass" :value="child.rank_class"/> {{ child.name }} </label>
-      </li>
-      <li v-if="(ranks[group].length > maxChildsDisplay) && (group == childOfParent[rankGroup]) && !showAll">
-        <label class="middle" v-if="extendChildsList"><input type="radio" name="extendChild" v-model="currentMaxDisplay" @click="extendChildsList = false" :value="maxChildsDisplay" :checked="false"/> less... </label>
-        <label class="middle" v-else><input type="radio" name="extendChild" v-model="currentMaxDisplay" :value="ranks[group].length" @click="extendChildsList = true" :checked="false"/> more... </label>
       </li>
     </ul>
   </div>
