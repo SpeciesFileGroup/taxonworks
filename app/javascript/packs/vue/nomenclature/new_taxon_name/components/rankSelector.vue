4f7a53fb3eed065b71b1b78a9b632129dceab150
José Luis Pereira <pepelui@outlook.com>
2017-07-17 17:34:57 -0300

    Added show all modal on rank picker #53

--- app/javascript/packs/vue/nomenclature/new_taxon_name/components/rankSelector.vue
+++ app/javascript/packs/vue/nomenclature/new_taxon_name/components/rankSelector.vue
@@ -1,20 +1,37 @@
 <template>
-  <div class="field ranks-list" v-if="parent && rankGroup && ranks[rankGroup].length">
+<div v-if="parent && rankGroup && ranks[rankGroup].length">
+  <modal class="taxon-modal" v-if="showModal" @close="showModal = false">
+    <h3 slot="header">Status</h3>
+    <div slot="body">
+      <ul class="tree-status" v-for="(group, key) in Object.keys(this.ranks)" v-if="!isMajor(rankGroup, group)">
+        <li v-for="(child, index) in (ranks[group])">
+          <button type="button" class="button button-default" name="rankSelected" @click="setRankClass = child.rank_class, showModal = false" :value="child.rank_class"> {{ child.name }} </button> 
+        </li>
+      </ul>
+    </div>
+  </modal>
+  <div class="field ranks-list">
     <h4>Rank</h4>
-    <ul class="no_bullet " v-for="(group, key) in Object.keys(this.ranks)" v-if="!isMajor(rankGroup, group)">
-      <li v-for="(child, index) in (ranks[group])" v-if="child.typical_use">
-        <label class="middle"><input type="radio" name="rankSelected" v-model="setRankClass" :checked="child.rank_class == this.setRankClass" :value="child.rank_class"/> {{ child.name }} </label>
+    <ul class="no_bullet" v-for="(group, key) in Object.keys(this.ranks)" v-if="!isMajor(rankGroup, group)">
+      <li v-for="(child, index) in (ranks[group])" v-if="checkDisplay(child)">
+        <label class="middle"><input type="radio" name="rankSelected" v-model="setRankClass" :checked="child.rank_class == setRankClass" :value="child.rank_class"/> {{ child.name }} </label>
       </li>
     </ul>
+    <button type="button"  @click="showModal = true">Show all</button>
+  </div>
   </div>
 </template>
 <script>
-         
+
+  const modal = require('../../../components/modal.vue');
   const childOfParent = require('../helpers/childOfParent')
   const GetterNames = require('../store/getters/getters').GetterNames;
   const MutationNames = require('../store/mutations/mutations').MutationNames;
 
   export default {
+    components: {
+      modal
+    },
     computed: {
       parent() {
         return this.$store.getters[GetterNames.GetParent]
@@ -37,16 +54,12 @@
     data: function() {
       return {
         childOfParent: childOfParent,
-        maxChildsDisplay: 4,
-        currentMaxDisplay: 4,
-        extendChildsList: false,
-        showAll: false,
+        showModal: false,
         defaultRanks: ['family', 'genus', 'species']
       }
     },
     watch: {
       'ranks': function(val, oldVal) {
-        this.reset();
         if(this.setRankClass == undefined) {
           this.ranks[this.childOfParent[this.rankGroup]].find(item => {
             if(this.defaultRanks.indexOf(item.name) >= 0) {
@@ -54,24 +67,18 @@
             }
           });
         }
-        else {
-          this.extendChildsList = true;
-          this.showAll = true;
-        }
       },
     }, 
     methods: {
       isMajor: function(groupName, findGroup) {
         return (Object.keys(this.ranks).indexOf(groupName) > Object.keys(this.ranks).indexOf(findGroup));
       },
-      reset: function() {
-        this.currentMaxDisplay = this.maxChildsDisplay;
-        this.extendChildsList = false
-        this.showAll = false;
-      },
       checkSameRank: function(rankRadio, rank) {
         let rankSplitted = rankRadio.split('::');
         return (rank == rankSplitted[rankSplitted.length-1]);
+      },
+      checkDisplay: function(child) {
+        return ((this.setRankClass == child.rank_class) || child.typical_use)
       }
     }
   };
