0d32fdf0fc7ec09c003f5a6ccec18dea978c2e31
mjy <diapriid@gmail.com>
2017-08-04 16:13:04 -0500

    Helpers to auto-set date for extracts. Validate units of extracts. Docs.

--- app/models/extract.rb
+++ app/models/extract.rb
@@ -1,5 +1,38 @@
-# An extract is the quantified physical entity that originated from a collection object
-
+# An Extract is the quantified physical entity that originated from a CollectionObject.  
+# Extracts are linked to their origin through an OriginRelationship.
+#
+# @!attribute quantity_value
+#   @return [Numeric]
+#    # @Merfoo, define with David
+#   
+# @!attribute quantity_unit
+#   @return [Numeric]
+#      # @Merfoo, define with David
+#  
+# @!attribute concentration_value 
+#   @return [Numeric]
+#      # @Merfoo, define with David
+#
+# @!attribute concentration_unit
+#   @return [Numeric]
+#      # @Merfoo, define with David
+#
+# @!attribute verbatim_anatomical_origin 
+#  @return [String]
+#    proxy for a OriginRelationship to an AnatomicalClass
+#
+# @!attribute year_made 
+#  @return [Integer]
+#    4 digit year the extract originated 
+#
+# @!attribute month_made 
+#  @return [Integer]
+#    2 digit month the extract originated
+#
+# @!attribute day_made 
+#  @return [Integer]
+#    2 digit day the extract originated
+#
 class Extract < ActiveRecord::Base
   include Housekeeping
   include Shared::IsData
@@ -8,11 +41,34 @@ class Extract < ActiveRecord::Base
  
   is_origin_for 'Sequence', 'Extract'
 
+  attr_accessor :is_made_now
+
+  before_validation :set_made, if: 'is_made_now'
+
   validates_presence_of :quantity_value 
   validates_presence_of :quantity_unit
+
+  validates :quantity_unit, with: :validate_units
   
   validates :year_made, date_year: { allow_blank: false }
   validates :month_made, date_month: { allow_blank: false }
   validates :day_made, date_day: { allow_blank: false }
+
+  protected
+
+  def set_made
+    write_attribute(:year_made, Time.now.year)
+    write_attribute(:month_made, Time.now.month)
+    write_attribute(:day_made, Time.now.day)
+  end
+
+  def validate_units
+    begin
+      RubyUnits::Unit.new(quantity_unit)
+    rescue ArgumentError, 'Unit not recognized'
+      errors.add(:quantity_unit, "'#{quantity_unit}' is an invalid quantity_unit")
+    end
+  end
+
 
 end
