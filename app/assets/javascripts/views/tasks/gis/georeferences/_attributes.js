f4777f236d8b98762b0d2ed61dc392ea18494924
mjy <diapriid@gmail.com>
2017-08-15 23:48:21 -0500

    More turbolinks:ready

--- app/assets/javascripts/views/tasks/gis/georeferences/_attributes.js
+++ app/assets/javascripts/views/tasks/gis/georeferences/_attributes.js
@@ -1,7 +1,8 @@
-var _init_georeference_google_map;      //DISPLAY/SHOW google map of georeference
 
-_init_georeference_google_map = function init_georeference_google_map()        // initialization function for georeference google map
-{
+//DISPLAY/SHOW google map of georeference
+// initialization function for georeference google map
+$document.on("turbolinks:load", function() {
+
   if ($('#georeference_google_map_canvas').length) {    //preempt omni-listener affecting wrong canvas
     if ($('#feature_collection').length) {
       var newfcdata = $("#feature_collection");
@@ -10,8 +11,5 @@ _init_georeference_google_map = function init_georeference_google_map()        /
       TW.vendor.lib.google.maps.initializeMap("georeference_google_map_canvas", fcdata);
     }
   }
-};
-
-$(document).ready(_init_georeference_google_map);
-$(document).on("page:load", _init_georeference_google_map);
+})
 
