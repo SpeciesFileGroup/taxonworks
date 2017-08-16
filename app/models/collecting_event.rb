cf629998c71dcd1569a86fea1cd16b872778eb41
Jim Tucker <tuckerjd@uiuc.edu>
2017-08-16 13:25:36 -0500

    Fixing 'ActiveRecord::HasManyThroughOrderError: Cannot have a has_many :through association 'CollectingEvent#error_geographic_items' which goes through 'CollectingEvent#georeferences' before the through association is defined.'

--- app/models/collecting_event.rb
+++ app/models/collecting_event.rb
@@ -221,8 +221,8 @@ class CollectingEvent < ApplicationRecord
   has_many :collection_objects, inverse_of: :collecting_event, dependent: :restrict_with_error
   has_many :collector_roles, class_name: 'Collector', as: :role_object, dependent: :destroy
   has_many :collectors, through: :collector_roles, source: :person
-  has_many :error_geographic_items, through: :georeferences, source: :error_geographic_item
   has_many :georeferences, dependent: :destroy
+  has_many :error_geographic_items, through: :georeferences, source: :error_geographic_item
   has_many :geographic_items, through: :georeferences # See also all_geographic_items, the union
   has_many :geo_locate_georeferences, class_name: 'Georeference::GeoLocate', dependent: :destroy
 
