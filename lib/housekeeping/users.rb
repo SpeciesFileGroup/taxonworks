7ef22eeea469c5f950d92bf30bcd851749a9eea2
Jim Tucker <tuckerjd@uiuc.edu>
2017-08-16 09:02:03 -0500

    Remove deprecation warning: Passing string instead of lambda/proc/block.

--- lib/housekeeping/users.rb
+++ lib/housekeeping/users.rb
@@ -1,26 +1,27 @@
 # Concern that provides housekeeping and related methods for models that belong_to a creator and updator
-module Housekeeping::Users 
+module Housekeeping::Users
   extend ActiveSupport::Concern
 
   included do
     related_instances = self.name.demodulize.underscore.pluralize.to_sym # if 'One::Two::Three' then :threes
-    related_class = self.name
+    related_class     = self.name
 
     belongs_to :creator, foreign_key: :created_by_id, class_name: 'User'
     belongs_to :updater, foreign_key: :updated_by_id, class_name: 'User'
 
-    validates :creator, presence: true, unless: 'self.class.name == "User" && self.self_created'
-    validates :updater, presence: true, unless: 'self.class.name == "User" && self.self_created'
+    clause = lambda {self.class.name == "User" && self.self_created}
+    validates :creator, presence: true, unless: clause # lambda, proc, or block
+    validates :updater, presence: true, unless: clause
 
-    before_validation(on: :create, unless: 'self.class.name == "User" && self.self_created') do
-      set_updated_by_id 
+    before_validation(on: :create, unless: clause) do
+      set_updated_by_id
       set_created_by_id
     end
 
     before_validation(on: :update) do
-      set_updated_by_id 
+      set_updated_by_id
     end
-    
+
     before_save(on: :update) do
       PaperTrail.whodunnit = $user_id
     end
@@ -67,7 +68,7 @@ module Housekeeping::Users
     self.created_by_id ||= $user_id
   end
 
-  def set_updated_by_id 
+  def set_updated_by_id
     if (self.changed? || self.new_record?) && !self.updated_by_id_changed? && self.by.blank?
       self.updated_by_id = $user_id
     end
