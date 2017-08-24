90d4f096ce6334c744f382ea71ea71c1ebe915ea
José Luis Pereira <pepelui@outlook.com>
2017-08-24 18:55:55 -0300

    Added soft validation after delete a type #53

--- app/javascript/packs/vue/nomenclature/new_taxon_name/store/actions/removeTaxonRelationship.js
+++ app/javascript/packs/vue/nomenclature/new_taxon_name/store/actions/removeTaxonRelationship.js
@@ -5,5 +5,6 @@ module.exports = function({ commit, state, dispatch }, relationship) {
 	removeTaxonRelationship(relationship).then( response => {
 		commit(MutationNames.RemoveTaxonRelationship, relationship);
 		dispatch('loadSoftValidation','taxonRelationshipList');
+		dispatch('loadSoftValidation','taxon_name');
 	});
 };
