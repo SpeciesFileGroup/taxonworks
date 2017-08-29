ad181b0ab3725f3eb4104556c3e147dba13fac7e
José Luis Pereira <pepelui@outlook.com>
2017-08-29 01:44:09 -0300

    Added transition effect for lists #53

--- app/javascript/packs/vue/nomenclature/new_taxon_name/components/listEntrys.vue
+++ app/javascript/packs/vue/nomenclature/new_taxon_name/components/listEntrys.vue
@@ -1,13 +1,11 @@
 <template>
-	<div v-if="list.length">
-	    <ul class="table-entrys-list">
-	    	<li v-for="item in list" class="flex-separate middle">
-		    	<div>
-			    	<span v-for="show in display" v-html="item[show] + ' '"></span>
-			    </div>
+	<div v-show="list.length">
+	    	<transition-group class="table-entrys-list" name="list-complete" tag="ul">
+	    	<li v-for="item, index in list" :key="item.id" class="list-complete-item flex-separate middle">
+			    <span><span v-for="show in display" v-html="item[show] + ' '"></span></span>
 		    	<span type="button" class="circle-button btn-delete" @click="removeStatus(item)">Remove</span>
 	    	</li>
-	    </ul>
+	    	</transition-group>
     </div>
 </template>
 <script>
@@ -27,13 +25,29 @@ export default {
 </script>
 
 <style type="text/css">
-  .table-entrys-list {
+.table-entrys-list {
   	padding: 0px;
+  	position: relative;
 
     li {
 		margin: 0px;
 		padding: 6px;
 		border-top: 1px solid #f5f5f5;
     }
-  }
+}
+.list-complete-item {
+  transition: all 1s, opacity 0.2s;
+
+}
+.list-complete-enter, .list-complete-leave-to
+{
+  opacity: 0;
+  font-size: 0px;
+  border:none;
+  transform: scale(0.0);
+}
+.list-complete-leave-active {
+	width: 100%;
+	position: absolute;
+}
 </style>
