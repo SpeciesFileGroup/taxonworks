2c4a7fe1ab72da64c64d217dcc886d15cb70f426
mjy <diapriid@gmail.com>
2017-09-18 23:07:01 -0500

    Remove toplevel references

--- lib/taxonworks.rb
+++ lib/taxonworks.rb
@@ -1,8 +1,8 @@
 module Taxonworks
-  require 'benchmark'
-
   # Our library, check to see if best place to include here (post 4.1)
   require 'activerecord_utilities'
+
+  require 'benchmark'
 
   # TODO: at what level is this required?!
   I18n.enforce_available_locales = false
