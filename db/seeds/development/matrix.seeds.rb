15bee07517a3dba68c26275c3846739488d68bf7
mjy <diapriid@gmail.com>
2017-09-26 22:27:07 -0500

    Add matrix seed

---
+++ db/seeds/development/matrix.seeds.rb
@@ -0,0 +1,79 @@
+raise 'not so fast' if Rails.env == 'production'
+
+project_id = ENV['project_id']
+user_id = project_id = ENV['project_id']
+raise 'provide project_id=123' if project_id.blank?
+raise 'provide user_id=123' if user_id.blank?
+
+$user_id = user_id 
+$project_id = project_id
+
+begin
+  ApplicationRecord.transaction do 
+    m = ObservationMatrix.create(name: 'Matrix demo')
+
+    d1 = Descriptor::Qualitative.create(name: 'Hair color')
+    d2 = Descriptor::Qualitative.create(name: 'Head size')
+    d3 = Descriptor::Qualitative.create(name: 'Foot smell')
+    d4 = Descriptor::Continuous.create(name: 'Tooth count')
+    d5 = Descriptor::Sample.create(name: 'Average speed')
+
+    r1 = Otu.create(name: 'Big turtle')
+    r2 = Otu.create(name: 'Little mouse')
+    r3 = Otu.create(name: 'Titanosaurus')
+    r4 = Specimen.create
+    r5 = Lot.create(total: 42)
+
+    cs1 = CharacterState.create(descriptor: d1, name: 'blue', label: '0')
+    cs2 = CharacterState.create(descriptor: d1, name: 'purple', label: '1')
+
+    cs3 = CharacterState.create(descriptor: d2, name: 'teeny tiny', label: 'a')
+    cs4 = CharacterState.create(descriptor: d2, name: 'medium', label: 'b')
+    cs5 = CharacterState.create(descriptor: d2, name: 'ginormous', label: 'c')
+
+    cs6 = CharacterState.create(descriptor: d3, name: 'like talcum powder', label: '0')
+    cs7 = CharacterState.create(descriptor: d3, name: 'ode de swamp', label: '1')
+    cs8 = CharacterState.create(descriptor: d3, name: 'green cheese', label: '2')
+
+    # row 1
+    o1 = Observation::Qualitative.create(otu: r1, descriptor: d1, character_state: cs1)
+    o2 = Observation::Qualitative.create(otu: r1, descriptor: d2, character_state: cs3)
+    o3 = Observation::Qualitative.create(otu: r1, descriptor: d3, character_state: cs7)
+    o4 = Observation::Continuous.create(otu: r1, continuous_value: 9)
+    o5 = Observation::Continuous.create(otu: r1, sample_min: 1, sample_max: 3)
+
+    # row 2
+    o6 = Observation::Qualitative.create(otu: r2, descriptor: d1, character_state: cs2)
+    o7 = Observation::Qualitative.create(otu: r2, descriptor: d2, character_state: cs4)
+    o8 = Observation::Qualitative.create(otu: r2, descriptor: d3, character_state: cs7)
+    o9 = Observation::Continuous.create(otu: r2, continuous_value: 8)
+    o10 = Observation::Continuous.create(otu: r2, sample_min: 1)
+
+    # row 3
+    o11 = Observation::Qualitative.create(otu: r3, descriptor: d1, character_state: cs2)
+    o12 = Observation::Qualitative.create(otu: r3, descriptor: d2, character_state: cs4)
+    o13 = Observation::Qualitative.create(otu: r3, descriptor: d3, character_state: cs8)
+    o14 = Observation::Continuous.create(otu: r3, continuous_value: 2)
+    o15 = Observation::Continuous.create(otu: r3, sample_min: 9)
+
+    # row 4
+    o11 = Observation::Qualitative.create(collection_object: r4, descriptor: d1, character_state: cs2)
+    o12 = Observation::Qualitative.create(collection_object: r4, descriptor: d2, character_state: cs4)
+    o13 = Observation::Qualitative.create(collection_object: r4, descriptor: d3, character_state: cs8)
+    o14 = Observation::Continuous.create(collection_object: r4, continuous_value: 2)
+    o15 = Observation::Continuous.create(collection_object: r4, sample_min: 9)
+
+    # row 5
+    # is empty
+
+    m.descriptors << [d1, d2, d3, d4, d5]
+    m.otus << [r1, r2, r3] 
+    m.collection_objects << [r4, r5] 
+
+    puts Rainbow("in Project #{$project_id}").purple
+    puts Rainbow("Built matrix with id #{m.id}.").blue
+    puts Rainbow("Built otus with ids #{[r1, r2, r3].collect{|z| z.id}.join(',')}.").blue
+  end
+rescue => e
+  puts Rainbow("Failed: #{e}").red
+end
