cd8e0a1e4e4a2771ba0d7544e1a6413e202ab106
José Luis Pereira <pepelui@outlook.com>
2017-10-13 01:22:36 -0300

    Minor fix for crud

--- app/javascript/packs/vue/components/annotator/request/crud.js
+++ app/javascript/packs/vue/components/annotator/request/crud.js
@@ -5,35 +5,43 @@ Vue.use(VueResource);
 Vue.http.headers.common['X-CSRF-Token'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 
 const create = function(url, data) {
-	return Vue.http.post(url, data).then(response => {
-		return response;
-	}, response => {
-		return response;
-	})
+	return new Promise(function(resolve, reject) {
+		Vue.http.post(url, data).then(response => {
+			return resolve(response);
+		}, response => {
+			return reject(response);
+		})
+	});
 }
 
 const update = function(url, data) {
-	return Vue.http.patch(url, data).then(response => {
-		return response;
-	}, response => {
-		return response;
-	})	
+	return new Promise(function(resolve, reject) {	
+		Vue.http.patch(url, data).then(response => {
+			return resolve(response);
+		}, response => {
+			return reject(response);
+		})
+	});
 }
 
 const destroy = function(url, data) {
-	return Vue.http.delete(url, data).then(response => {
-		return response;
-	}, response => {
-		return response;
-	})	
+	return new Promise(function(resolve, reject) {	
+		Vue.http.delete(url, data).then(response => {
+			return resolve(response);
+		}, response => {
+			return reject(response);
+		})
+	});	
 }
 
 const getList = function(url) {
-	return Vue.http.get(url).then(response => {
-		return response;
-	}, response => {
-		return response;
-	})	
+	return new Promise(function(resolve, reject) {	
+		Vue.http.get(url).then(response => {
+			return resolve(response);
+		}, response => {
+			return reject(response);
+		})	
+	});	
 }
 
 const vueCrud = {
