a34258635f02589db9a74e491cbd2b75878cef26
mjy <diapriid@gmail.com>
2017-10-16 22:19:16 -0500

    polymorphic annotator on depictin

--- app/models/depiction.rb
+++ app/models/depiction.rb
@@ -24,11 +24,11 @@ class Depiction < ApplicationRecord
   include Shared::Taggable
   include Shared::IsData
   include Shared::PolymorphicAnnotator
+  polymorphic_annotates(:depiction_object)
 
   acts_as_list scope: [:depiction_object_type, :depiction_object_id]
 
   belongs_to :image, inverse_of: :depictions
-  belongs_to :depiction_object, polymorphic: true
 
   has_one :sqed_depiction, dependent: :destroy
 
@@ -36,11 +36,4 @@ class Depiction < ApplicationRecord
 
   accepts_nested_attributes_for :image
   accepts_nested_attributes_for :depiction_object
-
-  # @return [DepictionObject]
-  #   alias to simplify reference across classes
-  def annotated_object
-    depiction_object
-  end
-
 end
