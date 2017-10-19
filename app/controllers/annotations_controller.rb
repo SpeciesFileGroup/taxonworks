0ede418d9898accb5ac4972d00a0e480bcfa811a
mjy <diapriid@gmail.com>
2017-10-18 21:54:28 -0500

    a little cross project checking

--- app/controllers/annotations_controller.rb
+++ app/controllers/annotations_controller.rb
@@ -8,7 +8,10 @@ class AnnotationsController < ApplicationController
 
   # GET /annotations/:global_id/metadata
   def metadata
-   @object = GlobalID::Locator.locate(params[:global_id])
+    @object = GlobalID::Locator.locate(params[:global_id])
+
+    render(json: {false}, status: :not_found) if @object.nil?
+    render(json: { 'message' => 'Record not found' }, status: :unauthorized) if not @object.project_id == sessions_current_project_id
   end
 
 end
