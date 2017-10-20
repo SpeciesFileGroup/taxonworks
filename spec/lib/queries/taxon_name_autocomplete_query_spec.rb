c0788ac073eee0254e217dc230370f2741124318
Dmitry Dmitriev <arboridia@gmail.com>
2017-10-20 14:23:25 -0500

    autocomplete query tests

--- spec/lib/queries/taxon_name_autocomplete_query_spec.rb
+++ spec/lib/queries/taxon_name_autocomplete_query_spec.rb
@@ -4,7 +4,7 @@ describe Queries::TaxonNameAutocompleteQuery, type: :model do
 
   let(:root) { FactoryGirl.create(:root_taxon_name)}
   let(:genus) { Protonym.create(name: 'Erasmoneura', rank_class: Ranks.lookup(:iczn, 'genus'), parent: root) }
-  let!(:species) { Protonym.create(name: 'vulnerata', rank_class: Ranks.lookup(:iczn, 'species'), parent: genus) }
+  let!(:species) { Protonym.create(name: 'vulnerata', rank_class: Ranks.lookup(:iczn, 'species'), parent: genus, verbatim_author: 'Fitch & Say', year_of_publication: 1800) }
 
   let(:name) { 'Erasmoneura vulnerata' }
 
@@ -27,6 +27,62 @@ describe Queries::TaxonNameAutocompleteQuery, type: :model do
     query.terms = name 
     expect(query.autocomplete).to include(species) 
   end
+
+  specify '#autocomplete_top_name 3' do
+    query.terms = 'Erasmon'
+    expect(query.autocomplete.first).to eq(genus)
+  end
+
+  specify '#autocomplete_top_cached 4' do
+    query.terms = 'vuln'
+    expect(query.autocomplete).to include(species)
+  end
+
+  specify '#autocomplete_top_name 5' do
+    query.terms = 'rasmon'
+    expect(query.autocomplete.first).to eq(genus)
+  end
+
+  specify '#autocomplete_top_name 6' do
+    query.terms = 'ulner'
+    expect(query.autocomplete.first).to eq(species)
+  end
+
+  specify '#autocomplete_top_name 7' do
+    query.terms = 'neur nerat'
+    expect(query.autocomplete.first).to eq(species)
+  end
+
+  specify '#autocomplete_top_name 8' do
+    query.terms = 'E vul'
+    expect(query.autocomplete.first).to eq(species)
+  end
+
+  specify '#autocomplete_top_name 9' do
+    query.terms = 'E. vul'
+    expect(query.autocomplete.first).to eq(species)
+  end
+
+  specify '#autocomplete_top_name 10' do
+    query.terms = 'Fitch'
+    expect(query.autocomplete.first).to eq(species)
+  end
+
+  specify '#autocomplete_top_name 11' do
+    query.terms = 'Say'
+    expect(query.autocomplete.first).to eq(species)
+  end
+
+  specify '#autocomplete_top_name 12' do
+    query.terms = '1800'
+    expect(query.autocomplete.first).to eq(species)
+  end
+
+  specify '#autocomplete_top_name 13' do
+    query.terms = 'Fitch 1800'
+    expect(query.autocomplete.first).to eq(species)
+  end
+
 
   # etc. ---
 
