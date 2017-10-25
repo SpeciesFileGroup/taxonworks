2060fa01af7c4ff01f4d76aac14f26db5d614dc9
mjy <diapriid@gmail.com>
2017-10-25 16:01:12 -0500

    check if object is community

--- app/controllers/annotations_controller.rb
+++ app/controllers/annotations_controller.rb
@@ -10,7 +10,7 @@ class AnnotationsController < ApplicationController
   def metadata
     @object = GlobalID::Locator.locate(params.require(:global_id))
     render(json: { success: false}, status: :not_found) if @object.nil?
-    render(json: { 'message' => 'Record not found' }, status: :unauthorized) if not @object.project_id == sessions_current_project_id
+    render(json: { 'message' => 'Record not found' }, status: :unauthorized) if !@object.is_community? && @object.project_id != sessions_current_project_id
   end
 
 end
