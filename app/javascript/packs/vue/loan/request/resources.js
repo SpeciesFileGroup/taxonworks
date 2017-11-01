5a778233fb585938606f6e071cd3c2e6a9c8941d
José Luis Pereira <pepelui@outlook.com>
2017-11-01 19:30:15 -0300

    added vuex storage, more progress on the interface

---
+++ app/javascript/packs/vue/loan/request/resources.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'; 
+import VueResource from 'vue-resource';
+
+Vue.use(VueResource);
+Vue.http.headers.common['X-CSRF-Token'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
+const getTagMetadata = function() {
+  return new Promise(function (resolve, reject) {
+    Vue.http.get('/tasks/loans/edit_loan/loan_item_metadata').then( response => {
+      return resolve(response.body);
+    }, response => {
+      return reject(response.body);
+    });
+  });
+}
+
+export {
+  getTagMetadata
+}
