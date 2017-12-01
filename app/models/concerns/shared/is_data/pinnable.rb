a1dfb25d72512336d1700464b4838716f52d9033
mjy <diapriid@gmail.com>
2017-12-01 14:53:01 -0600

    pinnable scopes

--- app/models/concerns/shared/is_data/pinnable.rb
+++ app/models/concerns/shared/is_data/pinnable.rb
@@ -4,14 +4,16 @@ module Shared::IsData::Pinnable
   extend ActiveSupport::Concern
 
   included do
-    # include Users
     has_many :pinboard_items, as: :pinned_object, dependent: :destroy
+  
+    scope :pinned_by, -> (user_id) { joins(:pinboard_items).where(pinboard_items: {user_id: user_id}) } 
+    scope :pinboard_inserted, -> { joins(:pinboard_items).where(pinboard_items: {is_inserted: true}) } 
   end
 
   # @return [Boolean]
   #   whether the object is pinned by the user
   def pinned?(user)
-    user.pinboard_items.for_object(self.metamorphosize).count > 0
+    user.pinboard_items.for_object(self.metamorphosize).any?
   end
 
   # @return [PinboardItem, nil]
@@ -25,6 +27,5 @@ module Shared::IsData::Pinnable
   def inserted?(user)
     pinboard_item_for(user).try(:is_inserted)
   end
-
 
 end
