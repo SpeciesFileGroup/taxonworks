9cc5a20a852ef6afc4affa406fefec7ccf77b31e
mjy <diapriid@gmail.com>
2017-12-07 16:39:59 -0600

    redo bad merge

--- spec/support/concerns/notable.rb
+++ spec/support/concerns/notable.rb
@@ -1,7 +1,7 @@
 # See spec/models/concerns/shared/ for functional tests on a fake Model, this only tests for include
 shared_examples 'notable' do
-  specify 'class includes Shared::Notable' do
-    expect(described_class.ancestors.map(&:name).include?('Shared::Notable')).to be true
+  specify 'class includes Shared::Notes' do
+    expect(described_class.ancestors.map(&:name).include?('Shared::Notes')).to be true
   end
 end
 
