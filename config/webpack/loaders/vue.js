960b26d2b965e5b10ea1b13bc86e69d521cca992
José Luis Pereira <pepelui@outlook.com>
2017-12-28 15:32:56 -0300

    Updated webpack config and packages

--- config/webpack/loaders/vue.js
+++ config/webpack/loaders/vue.js
@@ -1,13 +1,13 @@
+const { dev_server: devServer } = require('@rails/webpacker').config
+
+const isProduction = process.env.NODE_ENV === 'production'
+const inDevServer = process.argv.find(v => v.includes('webpack-dev-server'))
+const extractCSS = !(inDevServer && (devServer && devServer.hmr)) || isProduction
+
 module.exports = {
-  test: /.vue$/,
-  loader: 'vue-loader',
-  options: {
-    loaders: {
-      js: 'babel-loader',
-      file: 'file-loader',
-      scss: 'vue-style-loader!css-loader!postcss-loader!sass-loader',
-      sass: 'vue-style-loader!css-loader!postcss-loader!sass-loader?indentedSyntax'
-      styl: 'vue-style-loader!css-loader!postcss-loader!stylus-loader'      
-    }
-  }
+  test: /\.vue(\.erb)?$/,
+  use: [{
+    loader: 'vue-loader',
+    options: { extractCSS }
+  }]
 }
