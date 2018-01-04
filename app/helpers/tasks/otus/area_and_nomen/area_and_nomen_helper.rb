312c93fb900b512e6f105db330e8964e4abb8bbd
Jim Tucker <tuckerjd@uiuc.edu>
2018-01-04 10:13:01 -0600

    Taxonworks Issue #275: Refactor method name.

--- app/helpers/tasks/otus/area_and_nomen/area_and_nomen_helper.rb
+++ app/helpers/tasks/otus/area_and_nomen/area_and_nomen_helper.rb
@@ -15,7 +15,7 @@ module Tasks::Otus::AreaAndNomen::AreaAndNomenHelper
     end
   end
 
-  def otu_taxon_name_rank_select_tag(taxon_name: TaxonName.new, options: {})
+  def rank_for_otu_select_tag(taxon_name: TaxonName.new, options: {})
     select_tag(:rank_class, options_for_select(RANKS_SELECT_OPTIONS, selected: taxon_name.rank_string, options: options))
   end
 
