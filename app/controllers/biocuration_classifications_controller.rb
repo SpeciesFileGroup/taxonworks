136caa3d6d060516deddf5c13fd20b1a84b44580
mjy <diapriid@gmail.com>
2018-01-06 15:11:46 -0600

    biocuration JSON

--- app/controllers/biocuration_classifications_controller.rb
+++ app/controllers/biocuration_classifications_controller.rb
@@ -3,17 +3,21 @@ class BiocurationClassificationsController < ApplicationController
 
   before_action :set_biocuration_classification, only: [:update, :destroy]
 
+  # GET /biocuration_classifications.json
+  def index
+    @biocuration_classifications = BiocurationClassification.where(filter_params)
+  end
+
   # POST /biocuration_classifications
   # POST /biocuration_classifications.json
   def create
     @biocuration_classification = BiocurationClassification.new(biocuration_classification_params)
-
     respond_to do |format|
       if @biocuration_classification.save
-        format.html {redirect_back(fallback_location: (request.referer || root_path), notice: 'Biocuration classification was successfully created.')}
-        format.json { render json: @biocuration_classification, status: :created, location: @biocuration_classification }
+        format.html { redirect_back(fallback_location: (request.referer || root_path), notice: 'Biocuration classification was successfully created.')}
+        format.json { render :show, status: :created, location: @biocuration_classification }
       else
-        format.html {redirect_back(fallback_location: (request.referer || root_path), notice: 'Biocuration classification was NOT successfully created.')}
+        format.html { redirect_back(fallback_location: (request.referer || root_path), notice: 'Biocuration classification was NOT successfully created.')}
         format.json { render json: @biocuration_classification.errors, status: :unprocessable_entity }
       end
     end
@@ -44,13 +48,17 @@ class BiocurationClassificationsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_biocuration_classification
       @biocuration_classification = BiocurationClassification.with_project_id(sessions_current_project_id).find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def biocuration_classification_params
       params.require(:biocuration_classification).permit(:biocuration_class_id, :biological_collection_object_id)
     end
+
+    def filter_params
+      params.require(:biological_collection_object_id)
+      return params.permit(:biocuration_class_id, :biological_collection_object_id).merge(project_id: sessions_current_project_id)
+    end
+
 end
