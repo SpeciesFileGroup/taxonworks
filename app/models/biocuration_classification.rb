d07597e2421a810da065d53a7332dd6a68a92dce
mjy <diapriid@gmail.com>
2018-01-08 13:09:24 -0600

    Biocuration cleanup/spec

--- app/models/biocuration_classification.rb
+++ app/models/biocuration_classification.rb
@@ -1,5 +1,5 @@
-# A biocuration classification is...
-#   @todo
+# A BiocurationClassification classifies a CollectionObject within
+# a collection into some biologically-related category.  See BiocurationClass.
 #
 # @!attribute biocuration_class_id
 #   @return [Integer]
@@ -11,17 +11,17 @@
 #
 # @!attribute position
 #   @return [Integer]
-#   @todo
+#     used with acts_as_list
 #
 # @!attribute project_id
 #   @return [Integer]
 #   the project ID
 #
 class BiocurationClassification < ApplicationRecord
-  acts_as_list scope: [:biological_collection_object]
-
   include Housekeeping
   include Shared::IsData
+
+  acts_as_list scope: [:biological_collection_object_id, :project_id]
 
   belongs_to :biocuration_class, inverse_of: :biocuration_classifications
   belongs_to :biological_collection_object, class_name: 'CollectionObject::BiologicalCollectionObject', inverse_of: :biocuration_classifications
--- spec/models/biocuration_classification_spec.rb
+++ spec/models/biocuration_classification_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 describe BiocurationClassification, :type => :model do
   let(:biocuration_classification) {BiocurationClassification.new}
+  let(:biocuration_class) { FactoryBot.create(:valid_biocuration_class) } 
+  let(:specimen) { FactoryBot.create(:valid_specimen) } 
+
   context 'associations' do
     context 'belongs_to' do
       specify 'biological_object' do
@@ -17,17 +20,24 @@ describe BiocurationClassification, :type => :model do
     before(:each) do
       biocuration_classification.valid?
     end 
-    specify 'biological_collection_object is required' do
+
+    specify '#biological_collection_object is required' do
       expect(biocuration_classification.errors.include?(:biological_collection_object)).to be_truthy
     end
 
-    specify 'biocuration_class is required' do
+    specify '#biocuration_class is required' do
       expect(biocuration_classification.errors.include?(:biocuration_class)).to be_truthy
+    end
+
+    specify '#biocuration_class built through ids' do
+      biocuration_classification.biocuration_class_id = biocuration_class.id
+      biocuration_classification.biological_collection_object_id = specimen.id
+      expect(biocuration_classification.save!).to be_truthy
     end
 
     context 'with a biocuration class' do
       before(:each) {
-        biocuration_classification.biocuration_class = FactoryBot.create(:valid_biocuration_class)
+        biocuration_classification.biocuration_class = biocuration_class
       }
 
       specify 'a specimen can be biocuration classified' do
