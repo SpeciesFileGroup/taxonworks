bd4aa81f7704acac69ad12d7505b75176f128290
Jim Tucker <tuckerjd@uiuc.edu>
2018-01-19 10:33:14 -0600

    RubyMine and RuboCop inspection correction/suppression.

--- spec/routing/ranged_lot_categories_routing_spec.rb
+++ spec/routing/ranged_lot_categories_routing_spec.rb
@@ -1,34 +1,34 @@
-require "rails_helper"
+require 'rails_helper'
 
-describe RangedLotCategoriesController, :type => :routing do
-  describe "routing" do
+describe RangedLotCategoriesController, type: :routing do
+  describe 'routing' do
 
-    it "routes to #index" do
-      expect(get("/ranged_lot_categories")).to route_to("ranged_lot_categories#index")
+    it 'routes to #index' do
+      expect(get('/ranged_lot_categories')).to route_to('ranged_lot_categories#index')
     end
 
-    it "routes to #new" do
-      expect(get("/ranged_lot_categories/new")).to route_to("ranged_lot_categories#new")
+    it 'routes to #new' do
+      expect(get('/ranged_lot_categories/new')).to route_to('ranged_lot_categories#new')
     end
 
-    it "routes to #show" do
-      expect(get("/ranged_lot_categories/1")).to route_to("ranged_lot_categories#show", :id => "1")
+    it 'routes to #show' do
+      expect(get('/ranged_lot_categories/1')).to route_to('ranged_lot_categories#show', id: '1')
     end
 
-    it "routes to #edit" do
-      expect(get("/ranged_lot_categories/1/edit")).to route_to("ranged_lot_categories#edit", :id => "1")
+    it 'routes to #edit' do
+      expect(get('/ranged_lot_categories/1/edit')).to route_to('ranged_lot_categories#edit', id: '1')
     end
 
-    it "routes to #create" do
-      expect(post("/ranged_lot_categories")).to route_to("ranged_lot_categories#create")
+    it 'routes to #create' do
+      expect(post('/ranged_lot_categories')).to route_to('ranged_lot_categories#create')
     end
 
-    it "routes to #update" do
-      expect(put("/ranged_lot_categories/1")).to route_to("ranged_lot_categories#update", :id => "1")
+    it 'routes to #update' do
+      expect(put('/ranged_lot_categories/1')).to route_to('ranged_lot_categories#update', id: '1')
     end
 
-    it "routes to #destroy" do
-      expect(delete("/ranged_lot_categories/1")).to route_to("ranged_lot_categories#destroy", :id => "1")
+    it 'routes to #destroy' do
+      expect(delete('/ranged_lot_categories/1')).to route_to('ranged_lot_categories#destroy', id: '1')
     end
 
   end
--- spec/routing/routes/api_routing_spec.rb
+++ spec/routing/routes/api_routing_spec.rb
@@ -1,34 +1,34 @@
 require 'rails_helper'
 
-describe ApiController, :type => :routing do
+describe ApiController, type: :routing do
   describe 'routing' do
 
     it 'routes /api/ to #index with JSON format' do
-      expect(get('/api/')).to route_to('api#index', :format => :json)
+      expect(get('/api/')).to route_to('api#index', format: :json)
     end
 
     it 'routes /api/v1/ to #index with JSON format' do
-      expect(get('/api/v1')).to route_to('api#index', :format => :json)
+      expect(get('/api/v1')).to route_to('api#index', format: :json)
     end
 
     it 'routes /api/v1/images/{id} to images#show with JSON format' do
-			expect(get('/api/v1/images/1')).to route_to('images#show', :id => '1', :format => :json)
+			expect(get('/api/v1/images/1')).to route_to('images#show', id: '1', format: :json)
 		end
 
     it 'does not route /api/v1/images/foo to images#show' do
-      expect(get('/api/v1/images/foo')).not_to route_to('images#show', :id => 'foo', :format => :json)
+      expect(get('/api/v1/images/foo')).not_to route_to('images#show', id: 'foo', format: :json)
     end
 
     it 'routes /api/v1/collection_objects/{:id} to collection_objects#show' do
-      expect(get('/api/v1/collection_objects/1')).to route_to('collection_objects#show', :id => '1', :format => :json)
+      expect(get('/api/v1/collection_objects/1')).to route_to('collection_objects#show', id: '1', format: :json)
     end
 
     it 'does not route /api/v1/collection_objects/foo to collection_objects#show' do
-      expect(get('/api/v1/collection_objects/foo')).not_to route_to('collection_objects#show', :id => 'foo', :format => :json)
+      expect(get('/api/v1/collection_objects/foo')).not_to route_to('collection_objects#show', id: 'foo', format: :json)
     end
- 
+
     it 'routes /api/v1/collection_objects/by_identifier/{:identifier} to collection_objects#by_identifier' do
-      expect(get('/api/v1/collection_objects/by_identifier/ABCD')).to route_to('collection_objects#by_identifier', :identifier => "ABCD", :format => :json)
+      expect(get('/api/v1/collection_objects/by_identifier/ABCD')).to route_to('collection_objects#by_identifier', identifier: 'ABCD', format: :json)
     end
   end
 end
