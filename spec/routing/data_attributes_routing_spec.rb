c212d2a2470078849c80bf1f42b2604a87314e04
Jim Tucker <tuckerjd@uiuc.edu>
2018-01-19 13:09:23 -0600

    RubyMine and RuboCop inspection correction/suppression.

--- spec/routing/data_attributes_routing_spec.rb
+++ spec/routing/data_attributes_routing_spec.rb
@@ -1,22 +1,22 @@
-require "rails_helper"
+require 'rails_helper'
 
-describe DataAttributesController, :type => :routing do
-  describe "routing" do
+describe DataAttributesController, type: :routing do
+  describe 'routing' do
 
-    it "routes to #index" do
-      expect(get("/data_attributes")).to route_to("data_attributes#index")
+    it 'routes to #index' do
+      expect(get('/data_attributes')).to route_to('data_attributes#index')
     end
 
-    it "routes to #create" do
-      expect(post("/data_attributes")).to route_to("data_attributes#create")
+    it 'routes to #create' do
+      expect(post('/data_attributes')).to route_to('data_attributes#create')
     end
 
-    it "routes to #update" do
-      expect(put("/data_attributes/1")).to route_to("data_attributes#update", :id => "1")
+    it 'routes to #update' do
+      expect(put('/data_attributes/1')).to route_to('data_attributes#update', id: '1')
     end
 
-    it "routes to #destroy" do
-      expect(delete("/data_attributes/1")).to route_to("data_attributes#destroy", :id => "1")
+    it 'routes to #destroy' do
+      expect(delete('/data_attributes/1')).to route_to('data_attributes#destroy', id: '1')
     end
 
   end
--- spec/routing/documents_routing_spec.rb
+++ spec/routing/documents_routing_spec.rb
@@ -1,38 +1,38 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe DocumentsController, type: :routing do
-  describe "routing" do
+  describe 'routing' do
 
-    it "routes to #index" do
-      expect(:get => "/documents").to route_to("documents#index")
+    it 'routes to #index' do
+      expect(get: '/documents').to route_to('documents#index')
     end
 
-    it "routes to #new" do
-      expect(:get => "/documents/new").to route_to("documents#new")
+    it 'routes to #new' do
+      expect(get: '/documents/new').to route_to('documents#new')
     end
 
-    it "routes to #show" do
-      expect(:get => "/documents/1").to route_to("documents#show", :id => "1")
+    it 'routes to #show' do
+      expect(get: '/documents/1').to route_to('documents#show', id: '1')
     end
 
-    it "routes to #edit" do
-      expect(:get => "/documents/1/edit").to route_to("documents#edit", :id => "1")
+    it 'routes to #edit' do
+      expect(get: '/documents/1/edit').to route_to('documents#edit', id: '1')
     end
 
-    it "routes to #create" do
-      expect(:post => "/documents").to route_to("documents#create")
+    it 'routes to #create' do
+      expect(post: '/documents').to route_to('documents#create')
     end
 
-    it "routes to #update via PUT" do
-      expect(:put => "/documents/1").to route_to("documents#update", :id => "1")
+    it 'routes to #update via PUT' do
+      expect(put: '/documents/1').to route_to('documents#update', id: '1')
     end
 
-    it "routes to #update via PATCH" do
-      expect(:patch => "/documents/1").to route_to("documents#update", :id => "1")
+    it 'routes to #update via PATCH' do
+      expect(patch: '/documents/1').to route_to('documents#update', id: '1')
     end
 
-    it "routes to #destroy" do
-      expect(:delete => "/documents/1").to route_to("documents#destroy", :id => "1")
+    it 'routes to #destroy' do
+      expect(delete: '/documents/1').to route_to('documents#destroy', id: '1')
     end
 
   end
