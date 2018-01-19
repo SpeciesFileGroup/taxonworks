615f74666f24e715169c045c8402b6b28f419513
Jim Tucker <tuckerjd@uiuc.edu>
2018-01-19 11:39:50 -0600

    RubyMine and RuboCop inspection correction/suppression.

--- spec/routing/observation_matrix_row_items_routing_spec.rb
+++ spec/routing/observation_matrix_row_items_routing_spec.rb
@@ -1,38 +1,38 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe ObservationMatrixRowItemsController, type: :routing do
-  describe "routing" do
+  describe 'routing' do
 
-    it "routes to #index" do
-      expect(:get => "/observation_matrix_row_items").to route_to("observation_matrix_row_items#index")
+    it 'routes to #index' do
+      expect(get: '/observation_matrix_row_items').to route_to('observation_matrix_row_items#index')
     end
 
-    it "routes to #new" do
-      expect(:get => "/observation_matrix_row_items/new").to route_to("observation_matrix_row_items#new")
+    it 'routes to #new' do
+      expect(get: '/observation_matrix_row_items/new').to route_to('observation_matrix_row_items#new')
     end
 
-    it "routes to #show" do
-      expect(:get => "/observation_matrix_row_items/1").to route_to("observation_matrix_row_items#show", :id => "1")
+    it 'routes to #show' do
+      expect(get: '/observation_matrix_row_items/1').to route_to('observation_matrix_row_items#show', id: '1')
     end
 
-    it "routes to #edit" do
-      expect(:get => "/observation_matrix_row_items/1/edit").to route_to("observation_matrix_row_items#edit", :id => "1")
+    it 'routes to #edit' do
+      expect(get: '/observation_matrix_row_items/1/edit').to route_to('observation_matrix_row_items#edit', id: '1')
     end
 
-    it "routes to #create" do
-      expect(:post => "/observation_matrix_row_items").to route_to("observation_matrix_row_items#create")
+    it 'routes to #create' do
+      expect(post: '/observation_matrix_row_items').to route_to('observation_matrix_row_items#create')
     end
 
-    it "routes to #update via PUT" do
-      expect(:put => "/observation_matrix_row_items/1").to route_to("observation_matrix_row_items#update", :id => "1")
+    it 'routes to #update via PUT' do
+      expect(put: '/observation_matrix_row_items/1').to route_to('observation_matrix_row_items#update', id: '1')
     end
 
-    it "routes to #update via PATCH" do
-      expect(:patch => "/observation_matrix_row_items/1").to route_to("observation_matrix_row_items#update", :id => "1")
+    it 'routes to #update via PATCH' do
+      expect(patch: '/observation_matrix_row_items/1').to route_to('observation_matrix_row_items#update', id: '1')
     end
 
-    it "routes to #destroy" do
-      expect(:delete => "/observation_matrix_row_items/1").to route_to("observation_matrix_row_items#destroy", :id => "1")
+    it 'routes to #destroy' do
+      expect(delete: '/observation_matrix_row_items/1').to route_to('observation_matrix_row_items#destroy', id: '1')
     end
 
   end
--- spec/routing/observation_matrix_rows_routing_spec.rb
+++ spec/routing/observation_matrix_rows_routing_spec.rb
@@ -1,14 +1,14 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe ObservationMatrixRowsController, type: :routing do
-  describe "routing" do
+  describe 'routing' do
 
-    it "routes to #index" do
-      expect(:get => "/observation_matrix_rows").to route_to("observation_matrix_rows#index")
+    it 'routes to #index' do
+      expect(get: '/observation_matrix_rows').to route_to('observation_matrix_rows#index')
     end
 
-    it "routes to #show" do
-      expect(:get => "/observation_matrix_rows/1").to route_to("observation_matrix_rows#show", :id => "1")
+    it 'routes to #show' do
+      expect(get: '/observation_matrix_rows/1').to route_to('observation_matrix_rows#show', id: '1')
     end
 
   end
