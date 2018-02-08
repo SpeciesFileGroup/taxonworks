8ebe90e4b9488859f3f8c40c43eddbbdb62f7b17
mjy <diapriid@gmail.com>
2018-02-08 15:55:14 -0600

    cleanup

--- app/controllers/annotations_controller.rb
+++ app/controllers/annotations_controller.rb
@@ -2,10 +2,6 @@ class AnnotationsController < ApplicationController
 
   before_action :require_sign_in_and_project_selection
 
-  # GET /annotations/global_id
-  def show
-  end
-
   # GET /annotations/:global_id/metadata
   def metadata
     @object = GlobalID::Locator.locate(params.require(:global_id))
