28916519bf441bb8a3e428e1be96dbe526f29d0c
José Luis Pereira <pepelui@outlook.com>
2018-02-13 02:50:37 -0300

    New feature: select item from the recent list

--- app/javascript/packs/vue/citations/otus/components/sourceCitations.vue
+++ app/javascript/packs/vue/citations/otus/components/sourceCitations.vue
@@ -6,11 +6,9 @@
     <ul class="slide-panel-category-content">
       <li
         v-for="item in items"
+        @click="setOtu(item)"
         class="flex-separate middle slide-panel-category-item">
         <span v-html="item.citation_object_tag"/>
-        <div
-          class="circle-button btn-delete"
-          @click="removeCitation(item)"/>
       </li>
     </ul>
   </div>
@@ -26,10 +24,15 @@
       }
     },
     methods: {
-      removeCitation: function (item) {
+      removeCitation(item) {
         this.$http.delete('/citations/' + item.id).then(() => {
           this.$store.commit(MutationNames.RemoveSourceFormCitationList, item.id)
           this.$store.commit(MutationNames.RemoveOtuFormCitationList, item.id)
+        })
+      },
+      setOtu(item) {
+        this.$http.get(`/otus/${item.citation_object_id}`).then(response => {
+          this.$store.commit(MutationNames.SetOtuSelected, response.body)
         })
       }
     }
