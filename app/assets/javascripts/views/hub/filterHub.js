c7c4636e6a527b9b43119c17e6165c4838f9251a
José Luis Pereira <pepelui@outlook.com>
2018-02-14 18:24:42 -0300

    Fix status filter

--- app/assets/javascripts/views/hub/filterHub.js
+++ app/assets/javascripts/views/hub/filterHub.js
@@ -1,121 +1,126 @@
 var FilterHub = function () {
 
-		this.task_column = ($(window).width() > 1500) ? 3 : 2;
-		this.task_row = ($(window).width() > 1500) ? 4 : 3;
-		this.arrayData = [];
-		this.arrayTasks = [];
-		this.that = this;
-		this.handleEvents(this.that); 
-	};
+  this.task_column = ($(window).width() > 1500) ? 3 : 2;
+  this.task_row = ($(window).width() > 1500) ? 4 : 3;
+  this.arrayData = [];
+  this.arrayTasks = [];
+  this.that = this;
+  this.handleEvents(this.that);
+};
 
-	FilterHub.prototype.changeTaskSize = function(row,column) {
-		this.arrayTasks.forEach(function(element) {
-			element.changeSize(row,column);
-		});		
-	};
+FilterHub.prototype.changeTaskSize = function (row, column) {
+  this.arrayTasks.forEach(function (element) {
+    element.changeSize(row, column);
+  });
+};
 
-	FilterHub.prototype.handleEvents = function(that) {
-		$("#task_carrousel").each(function(i,element) {		
-			that.arrayTasks.push(new CarrouselTask("#"+$(element).attr("id"), that.task_row, that.task_column));
-		});
+FilterHub.prototype.handleEvents = function (that) {
+  $("#task_carrousel").each(function (i, element) {
+    that.arrayTasks.push(new CarrouselTask("#" + $(element).attr("id"), that.task_row, that.task_column));
+  });
 
-		$("[data-section]").each(function(i, element) {
-		    that.arrayData.push(new CarrouselData($(element).attr("data-section"),99,0));
-		});
+  $("[data-section]").each(function (i, element) {
+    that.arrayData.push(new CarrouselData($(element).attr("data-section"), 99, 0));
+  });
 
-		$('#search-filter').keyup(function() {
-			var
-				chain = $(this).val();
+  $('#search-filter').keyup(function () {
+    var
+      chain = $(this).val();
 
-			[that.arrayData, that.arrayTasks].forEach(function(element) {
-				element.forEach(function(element) {
-					element.filterKeys(chain);   
-				});             
-			});       
-		});	
+    [that.arrayData, that.arrayTasks].forEach(function (element) {
+      element.forEach(function (element) {
+        element.filterKeys(chain);
+      });
+    });
+  });
 
-		$('.reset-all-filters').on('click', function() {
-			that.changeAllSectionsFilter(that.arrayData); 
-			that.changeAllSectionsFilter(that.arrayTasks); 
-		});		
+  $('.reset-all-filters').on('click', function () {
+    that.changeAllSectionsFilter(that.arrayData);
+    that.changeAllSectionsFilter(that.arrayTasks);
+  });
 
-		//Keyboard Shortcuts
-		TW.workbench.keyboard.createShortcut("left","Show previous card tasks", "Hub tasks", function() {
-			that.arrayTasks.forEach(function(element) {
-				element.loadingUp();
-			});
-		});
-		
-		TW.workbench.keyboard.createShortcut("right","Show next card tasks", "Hub tasks", function() {
-			that.arrayTasks.forEach(function(element) {
-				element.loadingDown();
-			});
-		});		
+  //Keyboard Shortcuts
+  TW.workbench.keyboard.createShortcut("left", "Show previous card tasks", "Hub tasks", function () {
+    that.arrayTasks.forEach(function (element) {
+      element.loadingUp();
+    });
+  });
 
-		$('#filter [data-filter-category]').on('click', function() {
-			var
-		    	elementFilter = $(this).attr('data-filter-category');
+  TW.workbench.keyboard.createShortcut("right", "Show next card tasks", "Hub tasks", function () {
+    that.arrayTasks.forEach(function (element) {
+      element.loadingDown();
+    });
+  });
 
-			if(elementFilter === "reset") {
-		    	that.changeAllSectionsFilter(that.arrayData);
-		    	that.changeAllSectionsFilter(that.arrayTasks);
-			}
-			else {
-			    [that.arrayData, that.arrayTasks].forEach(function(element) {
-					element.forEach(function(element) {
-						element.changeFilter("data-category-"+ elementFilter);
-					});
-				});
-			}
-			if(that.allEmpty(that.arrayData) && that.allEmpty(that.arrayTasks)) {
-				$('[data-section="Supporting"] .reset-all-filters').fadeIn();
-			}
-			else {
-				$('.reset-all-filters').fadeOut(0);
-			}    
-		});		
+  $('#filter [data-filter-category]').on('click', function () {
+    var
+      elementFilter = $(this).attr('data-filter-category');
 
-		$('#filter .switch input').on('click', function() { 
-			switchFilter = this;
-			
-			[that.arrayData, that.arrayTasks].forEach(function(element) {
-				element.forEach(function(element) {
-			    	if(!$(switchFilter).is(':checked')) {
-			    		that.resetStatusFilter();
-			    		element.refresh();
-			    	}					
-				});
-			});			
-		});  
-	}
-		
-	FilterHub.prototype.resetStatusFilter = function() {
-		[this.arrayData, this.arrayTasks].forEach(function(element) {
-			element.forEach(function(element) {
-				element.setFilterStatus("data-category-prototype",false);
-				element.setFilterStatus("data-category-unknown",false);
-				element.setFilterStatus("data-category-stable",false);
-				element.setFilterStatus("data-category-complete",false);                  
-			});    
-		});
-	}
+    if($(this).parent().hasClass('filter-category') && !$(this).hasClass('activated')) {
+      that.resetAllStatusCards()
+    }
+    if (elementFilter === "reset") {
+      that.changeAllSectionsFilter(that.arrayData);
+      that.changeAllSectionsFilter(that.arrayTasks);
+    }
+    else {
+      [that.arrayData, that.arrayTasks].forEach(function (element) {
+        element.forEach(function (element) {
+          element.changeFilter("data-category-" + elementFilter);
+        });
+      });
+    }
+    if (that.allEmpty(that.arrayData) && that.allEmpty(that.arrayTasks)) {
+      $('[data-section="Supporting"] .reset-all-filters').fadeIn();
+    }
+    else {
+      $('.reset-all-filters').fadeOut(0);
+    }
+  });
 
-	FilterHub.prototype.changeAllSectionsFilter = function(arrayData) {
-	    arrayData.forEach(function(element) {
-	    	$("#search-filter").val("");
-	    	element.resetFilters();
-	    	element.filterChilds();
-	    	$('.reset-all-filters').fadeOut(0);
-	    });
-	}	
+  $('#filter .switch input').on('click', function (element) {
+    that.resetAllStatusCards()
+  });
+}
 
-	FilterHub.prototype.allEmpty = function(arraySection) {
-		inc = 0;
+FilterHub.prototype.resetAllStatusCards = function() {
+  var that = this;
 
-		arraySection.forEach(function (element) {
-			if(element.empty()) {
-				inc++
-			}
-		});
-		return (inc == arraySection.length);
-	}
+  [that.arrayData, that.arrayTasks].forEach(function (element) {
+    element.forEach(function (element) {
+      that.resetStatusFilter();
+      element.refresh();
+    });
+  });  
+}
+
+FilterHub.prototype.resetStatusFilter = function () {
+  [this.arrayData, this.arrayTasks].forEach(function (element) {
+    element.forEach(function (element) {
+      element.setFilterStatus("data-category-prototype", false);
+      element.setFilterStatus("data-category-unknown", false);
+      element.setFilterStatus("data-category-stable", false);
+      element.setFilterStatus("data-category-complete", false);
+    });
+  });
+}
+
+FilterHub.prototype.changeAllSectionsFilter = function (arrayData) {
+  arrayData.forEach(function (element) {
+    $("#search-filter").val("");
+    element.resetFilters();
+    element.filterChilds();
+    $('.reset-all-filters').fadeOut(0);
+  });
+}
+
+FilterHub.prototype.allEmpty = function (arraySection) {
+  var inc = 0;
+
+  arraySection.forEach(function (element) {
+    if (element.empty()) {
+      inc++
+    }
+  });
+  return (inc == arraySection.length);
+}
