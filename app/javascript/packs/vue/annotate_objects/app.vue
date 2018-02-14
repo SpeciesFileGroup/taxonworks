755a2241d40d6e7f3d14d479d5d1a3be23bd1b92
José Luis Pereira <pepelui@outlook.com>
2018-02-14 19:33:45 -0300

    Removed html commented code

--- app/javascript/packs/vue/annotate_objects/app.vue
+++ app/javascript/packs/vue/annotate_objects/app.vue
@@ -1,36 +1,36 @@
 <template>
-    <div id="annotate_object">
-        <div class="flex-wrap-row middle">
-            <button
-                    class="button button-default normal-input"
-                    @click="showModal = true"
-                    v-if="source">Change source
-            </button>
-            <button
-                    class="button button-default normal-input"
-                    @click="showModal = true"
-                    v-else>Select source
-            </button>
-        </div>
-        <!--<modal-->
-        <!--@close="showModal = false"-->
-        <!--v-if="showModal"-->
-        <!--@sourcepicker="loadSource">-->
-        <!--<h3 slot="header">Select source</h3>-->
-        <!--<div-->
-        <!--slot="body"-->
-        <!--id="source_panel">-->
-        <!--<autocomplete-->
-        <!--url="/sources/autocomplete"-->
-        <!--min="2"-->
-        <!--param="term"-->
-        <!--placeholder="Find source"-->
-        <!--event-send="sourcepicker"-->
-        <!--label="label"-->
-        <!--:autofocus="true"/>-->
-        <!--</div>-->
-        <!--</modal>-->
+  <div id="annotate_object">
+    <div class="flex-wrap-row middle">
+      <button
+        class="button button-default normal-input"
+        @click="showModal = true"
+        v-if="source">Change source
+      </button>
+      <button
+        class="button button-default normal-input"
+        @click="showModal = true"
+        v-else>Select source
+      </button>
     </div>
+    <modal
+      @close="showModal = false"
+      v-if="showModal"
+      @sourcepicker="loadSource">
+      <h3 slot="header">Select source</h3>
+      <div
+        slot="body"
+        id="source_panel">
+      <autocomplete
+        url="/sources/autocomplete"
+        min="2"
+        param="term"
+        placeholder="Find source"
+        event-send="sourcepicker"
+        label="label"
+        :autofocus="true"/>
+      </div>
+    </modal>
+  </div>
 </template>
 
 <script>
