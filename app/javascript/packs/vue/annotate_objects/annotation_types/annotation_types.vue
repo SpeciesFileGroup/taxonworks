5b65ca23d38d0d141777d0b387ccd1327c2fde1b
jrflood <jrflood@illinois.edu>
2018-02-15 15:21:29 -0600

    Stub for Vue-based annotation types.

---
+++ app/javascript/packs/vue/annotate_objects/annotation_types/annotation_types.vue
@@ -0,0 +1,37 @@
+<template>
+    <ul>
+        <li
+                v-for="label, key in list"
+                @click="selectType(key)">
+            <label>
+                <input
+                        name="annotation-type"
+                        type="radio"
+                        :value="key"
+                        @click="selectType(key)">
+                <span
+                        class="new-combination-rank-list-taxon-name"
+                        v-html="label"/>
+            </label>
+        </li>
+        <ul>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        list: {
+          tags: 'Tags (by Keyword)',
+          data_attributes: 'Data attribues (by Predicates)',
+          confidence: 'Confidence (by Confidence Level)'
+        }
+      }
+    },
+    methods: {
+      selectType(type) {
+        this.$emit('select', type)
+      }
+    }
+  }
+</script>
--- app/javascript/packs/vue/annotate_objects/app.vue
+++ app/javascript/packs/vue/annotate_objects/app.vue
@@ -1,65 +1,36 @@
 <template>
-  <div id="annotate_object">
-    <div class="flex-wrap-row middle">
-      <button
-        class="button button-default normal-input"
-        @click="showModal = true"
-        v-if="source">Change source
-      </button>
-      <button
-        class="button button-default normal-input"
-        @click="showModal = true"
-        v-else>Select source
-      </button>
-    </div>
-    <modal
-      @close="showModal = false"
-      v-if="showModal"
-      @sourcepicker="loadSource">
-      <h3 slot="header">Select source</h3>
-      <div
-        slot="body"
-        id="source_panel">
-      <autocomplete
-        url="/sources/autocomplete"
-        min="2"
-        param="term"
-        placeholder="Find source"
-        event-send="sourcepicker"
-        label="label"
-        :autofocus="true"/>
-      </div>
-    </modal>
-  </div>
+  <ul>
+    <li
+            v-for="label, key in list"
+            @click="selectType(key)">
+      <label>
+        <input
+                name="annotation-type"
+                type="radio"
+                :value="key"
+                @click="selectType(key)">
+        <span
+                class="new-combination-rank-list-taxon-name"
+                v-html="label"/>
+      </label>
+    </li>
+    <ul>
 </template>
 
 <script>
-//  const GetterNames = require('../store/getters/getters').GetterNames
-//  const MutationNames = require('../store/mutations/mutations').MutationNames
-  import autocomplete from '../components/autocomplete.vue'
-  import modal from '../components/modal.vue'
-
   export default {
-    data: function () {
+    data() {
       return {
-        showModal: false
-      }
-    },
-    components: {
-      autocomplete,
-      modal
-    },
-    computed: {
-      source() {
-        //return this.$store.getters[GetterNames.GetSourceSelected]
+        list: {
+          tags: 'Tags (by Keyword)',
+          data_attributes: 'Data attribues (by Predicates)',
+          confidence: 'Confidence (by Confidence Level)'
+        }
       }
     },
     methods: {
-      loadSource: function (item) {
-        this.$http.get('/sources/' + item.id).then(response => {
-          //this.$store.commit(MutationNames.SetSourceSelected, response.body)
-          this.showModal = false
-        })
+      selectType(type) {
+        this.$emit('select', type)
       }
     }
   }
