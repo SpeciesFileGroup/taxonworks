f582825a996e5a8265a0ebfc964a3c392f410110
jrflood <jrflood@illinois.edu>
2018-02-15 17:54:29 -0600

    Stub for models -- fails commented out.

--- app/javascript/packs/vue/annotate_objects/app.vue
+++ app/javascript/packs/vue/annotate_objects/app.vue
@@ -3,14 +3,20 @@
     <annotation-types @select="testResult = $event"/>
     <span>Selected: {{ testResult }}</span>
   </div>
+  <!--<div>-->
+  <!--<models @select="testResult = $event"/>-->
+  <!--<span>Selected: {{ testResult }}</span>-->
+  <!--</div>-->
 </template>
 <script>
 
 import AnnotationTypes from './annotation_types/annotation_types'
+// import Models from './models/models'
 
 export default {
   components: {
-    AnnotationTypes
+    AnnotationTypes/*,
+    Models*/
   },
   data() {
     return {
---
+++ app/javascript/packs/vue/annotate_objects/models/models.vue
@@ -0,0 +1,34 @@
+<template>
+    <ul>
+        <li v-for="label, key in list">
+            <label @click="selectModel(key)">
+                <input
+                        name="model"
+                        type="radio"
+                        :value="key">
+                <span
+                        class="new-combination-rank-list-taxon-name"
+                        v-html="label"/>
+            </label>
+        </li>
+    </ul>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        list: {
+          otu: 'by OTU',
+          taxon_name: 'by Taxon Name',
+          specimen: 'by Specimen'
+        }
+      }
+    },
+    methods: {
+      selectModel(type) {
+        this.$emit('select', type.valueOf())
+      }
+    }
+  }
+</script>
