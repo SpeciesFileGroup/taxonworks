bb2c3693c190f4a0980fec09a2545db239a114eb
José Luis Pereira <pepelui@outlook.com>
2018-02-15 18:34:48 -0300

    Fix and example

--- app/javascript/packs/vue/annotate_objects/app.vue
+++ app/javascript/packs/vue/annotate_objects/app.vue
@@ -1,37 +1,21 @@
 <template>
-  <ul>
-    <li
-            v-for="label, key in list"
-            @click="selectType(key)">
-      <label>
-        <input
-                name="annotation-type"
-                type="radio"
-                :value="key"
-                @click="selectType(key)">
-        <span
-                class="new-combination-rank-list-taxon-name"
-                v-html="label"/>
-      </label>
-    </li>
-    <ul>
+  <div>
+    <annotation-types @select="testResult = $event"/>
+    <span>Selected: {{ testResult }}</span>
+  </div>
 </template>
-
 <script>
-  export default {
-    data() {
-      return {
-        list: {
-          tags: 'Tags (by Keyword)',
-          data_attributes: 'Data attribues (by Predicates)',
-          confidence: 'Confidence (by Confidence Level)'
-        }
-      }
-    },
-    methods: {
-      selectType(type) {
-        this.$emit('select', type)
-      }
+
+import AnnotationTypes from './annotation_types/annotation_types'
+
+export default {
+  components: {
+    AnnotationTypes
+  },
+  data() {
+    return {
+      testResult: undefined
     }
-  }
+  },
+}
 </script>
