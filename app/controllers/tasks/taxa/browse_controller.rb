65922cc2c323758cc24fffcc8e33b25a1a61336b
mjy <diapriid@gmail.com>
2018-02-24 15:32:34 -0600

    cross-link browse taxa

--- app/controllers/tasks/taxa/browse_controller.rb
+++ app/controllers/tasks/taxa/browse_controller.rb
@@ -3,7 +3,11 @@ class Tasks::Taxa::BrowseController < ApplicationController
 
   # GET
   def index
+    if params[:otu_id]
     @otu = Otu.where(project_id: sessions_current_project_id).find(params.require(:otu_id))
+    else
+      @otu = Otu.where(project_id: sessions_current_project_id).first
+    end
   end
 
 end
