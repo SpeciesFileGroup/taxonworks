349b92aa1ad6469ecadf244bfda7c9ca32bb99de
jrflood <jrflood@illinois.edu>
2018-02-26 15:14:32 -0600

    Stub for models -- fails commented back in.

--- app/javascript/packs/vue/annotate_objects/app.vue
+++ app/javascript/packs/vue/annotate_objects/app.vue
@@ -3,15 +3,15 @@
     <annotation-types @select="testResult = $event"/>
     <span>Selected: {{ testResult }}</span>
   </div>
-  <!--<div>-->
-  <!--<models @select="testResult = $event"/>-->
-  <!--<span>Selected: {{ testResult }}</span>-->
-  <!--</div>-->
+    <div>
+        <models @select="testResult = $event"/>
+        <span>Selected: {{ testResult }}</span>
+    </div>
 </template>
 <script>
 
 import AnnotationTypes from './annotation_types/annotation_types'
-// import Models from './models/models'
+import Models from './models/models'
 
 export default {
   components: {
