e0539801ab7eb199e6ad980edf83c2c78771e98c
jrflood <jrflood@illinois.edu>
2018-03-02 12:34:46 -0600

    more stubs for selector specific endpoints

--- app/controllers/tasks/object_annotations/browse_annotations_controller.rb
+++ app/controllers/tasks/object_annotations/browse_annotations_controller.rb
@@ -7,10 +7,14 @@ class Tasks::BrowseAnnotationsController < ApplicationController
   end
 
   def get_type
-
+    render({json: params})
   end
 
   def process_submit
+    render({json: params})
+  end
+
+  def set_model
     render({json: params})
   end
 
