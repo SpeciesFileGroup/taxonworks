249479cb55130d49e064cda6f31c4dd243b0def3
José Luis Pereira <pepelui@outlook.com>
2018-03-02 19:12:20 -0300

    v-for example

--- app/javascript/packs/vue/browse_annotations/annotation_types/annotation_types.vue
+++ app/javascript/packs/vue/browse_annotations/annotation_types/annotation_types.vue
@@ -1,18 +1,21 @@
 <template>
-  <ul>
-    <li v-for="(label, key) in list">
-      <label @click="selectType(key)">
-        <input
-          :checked="value === key"
-          name="annotation-type"
-          type="radio"
-          :value="key">
-        <span
-          class="new-combination-rank-list-taxon-name"
-          v-html="label"/>
-      </label>
-    </li>
-  </ul>
+  <div>
+    <ul>
+      <li v-for="(label, key) in list">
+        <label @click="selectType(key)">
+          <input
+            :checked="value === key"
+            name="annotation-type"
+            type="radio"
+            :value="key">
+          <span
+            class="new-combination-rank-list-taxon-name"
+            v-html="label"/>
+        </label>
+      </li>
+    </ul>
+    <span v-for="(item, key) in result"> {{ key }} : {{ item }} </span>
+  </div>
 </template>
 
 <script>
