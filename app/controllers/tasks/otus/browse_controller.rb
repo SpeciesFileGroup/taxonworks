354c2d1527324d92d600d60f87f90545635c6dd5
mjy <diapriid@gmail.com>
2018-03-07 15:59:12 -0600

    no browsing OTUs if OTUs do not yet exist

--- app/controllers/tasks/otus/browse_controller.rb
+++ app/controllers/tasks/otus/browse_controller.rb
@@ -8,6 +8,7 @@ class Tasks::Otus::BrowseController < ApplicationController
     else
       @otu = Otu.where(project_id: sessions_current_project_id).first
     end
+    redirect_to new_otu_path, notice: 'No OTUs created, add one first.' and return if @otu.nil? 
   end
 
 end
