445d8e6a5b1bc8f9bb849b77006cecf0995bba6f
mjy <diapriid@gmail.com>
2018-03-09 21:30:10 -0600

    handle empty papertrail request

--- app/controllers/papertrail_controller.rb
+++ app/controllers/papertrail_controller.rb
@@ -3,6 +3,7 @@ class PapertrailController < ApplicationController
 
   # GET /papertrail
   def papertrail
+    redirect_to hub_path, notice: 'You requested a papertrail for nothing.' and return if params[:object_type].blank?
     @object = params[:object_type].constantize.find(params[:object_id])
      record_not_found if invalid_object(@object)
   end
