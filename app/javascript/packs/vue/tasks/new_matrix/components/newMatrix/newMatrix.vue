47abaf35413ca888e633d9eea7a39ccc74460e17
José Luis Pereira <pepelui@outlook.com>
2018-03-15 23:37:41 -0300

    Added switch component

--- app/javascript/packs/vue/tasks/new_matrix/components/newMatrix/newMatrix.vue
+++ app/javascript/packs/vue/tasks/new_matrix/components/newMatrix/newMatrix.vue
@@ -11,26 +11,8 @@
       @click="create"
       class="normal-input button button-submit"
       type="button">Create</button>
-    <div class="middle">
-      <label class="switch middle">
-        <div class="flex-separate middle space-between-switch-options">
-          <span>Column</span>
-          <span>Rows</span>
-        </div>
-        <input type="checkbox"/>
-        <div class="slider"></div>
-      </label>
-    </div>
-    <div class="middle">
-      <label class="switch middle">
-        <div class="flex-separate middle space-between-switch-options">
-          <span>Fixed</span>
-          <span>Dynamic</span>
-        </div>
-        <input type="checkbox"/>
-        <div class="slider"></div>
-      </label>
-    </div>
+    <switch-component :options="['Column', 'Row']" v-model="test"/>
+    <switch-component :options="['Fixed', 'Dynamic']" v-model="test2"/>
   </div>
 </template>
 
@@ -40,7 +22,12 @@ import { CreateMatrix } from '../../request/resources'
 import { MutationNames } from '../../store/mutations/mutations'
 import { GetterNames } from '../../store/getters/getters'
 
+import SwitchComponent from './switch.vue'
+
 export default {
+  components: {
+    SwitchComponent
+  },
   computed: {
     matrixName: {
       get() {
@@ -63,6 +50,12 @@ export default {
             !this.$store.getters[GetterNames.GetMatrix].id
     }
   },
+  data() {
+    return {
+      test: true,
+      test2: false
+    }
+  },
   methods: {
     create() {
       CreateMatrix(this.matrix).then(response => {
@@ -72,54 +65,3 @@ export default {
   }
 }
 </script>
-<style scoped>
-
-    .space-between-switch-options {
-      padding: 4px;
-    }
-    .switch input {
-      display: none;
-    }
-    .switch {
-      position: relative;
-      display: inline-block;
-      width: 150px;
-      height: 24px;
-      justify-content: space-around;
-      border-radius: 3px;
-      border:1px solid #efefef;
-      border-right: 0px;    
-      background-color: #efefef;
-      box-shadow:inset 0px 1px 4px 0px rgba(0,0,0,0.1); 
-    }
-    .slider {
-      position: absolute;
-      cursor: pointer;
-      top: 0;
-      left: 0;
-      right: 0;
-      bottom: 0;
-      -webkit-transition: .4s;
-      transition: .4s;
-    }
-
-    .slider:before {
-      position: absolute;
-      content: "";
-      height: 24px;
-      width: 75px;
-      left: 0px;
-      bottom: 0px;
-      border-radius: 3px;
-      border:1px solid #efefef;
-      background-color: #FFF;
-      transition: .3s;
-    }
-
-    input:checked + .slider:before {
-      -webkit-transform: translateX(75px);
-      -ms-transform: translateX(75px);
-      transform: translateX(75px);
-    }
-
-</style>
