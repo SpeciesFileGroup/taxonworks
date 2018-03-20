a24201fdd7f3d9d0de991df791408e0c003ad3af
mjy <diapriid@gmail.com>
2018-03-20 00:14:00 -0500

    move to after_initialize (may break some tests)

--- config/initializers/constants/model/descriptors.rb
+++ config/initializers/constants/model/descriptors.rb
@@ -1,3 +1,6 @@
 # Be sure to restart your server when you modify this file.
-DESCRIPTOR_TYPES = Descriptor.descendants.inject({}){|hsh,a| hsh.merge(a.name => a.human_name) }.freeze
+#
+Rails.application.config.after_initialize do 
+  DESCRIPTOR_TYPES = Descriptor.descendants.inject({}){|hsh,a| hsh.merge(a.name => a.human_name) }.freeze
+end
 
--- config/initializers/constants/model/observations.rb
+++ config/initializers/constants/model/observations.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
-OBSERVATION_TYPES = Observation.descendants.inject({}){|hsh,a| hsh.merge(a.name => a.human_name) }.freeze
-
+#
+Rails.application.config.after_initialize do 
+  OBSERVATION_TYPES = Observation.descendants.inject({}){|hsh,a| hsh.merge(a.name => a.human_name) }.freeze
+end
