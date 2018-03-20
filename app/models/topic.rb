812a6f2bd0e4d7f8943da3f9d8ee5e3ed2330d2e
Jim Tucker <tuckerjd@uiuc.edu>
2018-03-20 07:25:28 -0500

    With successful sanitize_sql_array example for Serial and Topic.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -7,39 +7,45 @@ class Topic < ControlledVocabularyTerm
 
   has_many :contents, inverse_of: :topic, dependent: :destroy
   has_many :otus, through: :contents
-  has_many :otu_page_layout_sections, -> { where(otu_page_layout_sections: {type: 'OtuPageLayoutSection::StandardSection'}) }, inverse_of: :topic
+  has_many :otu_page_layout_sections, -> { where(otu_page_layout_sections:
+                                                   {type: 'OtuPageLayoutSection::StandardSection'}) },
+           inverse_of: :topic
   has_many :otu_page_layouts, through: :otu_page_layout_sections
 
-  scope :used_on_klass, -> (klass) { joins(:citations).where(citations: {citation_object_type: klass} ) } 
+  scope :used_on_klass, -> (klass) { joins(:citations).where(citations: {citation_object_type: klass}) }
 
   # TODO: Deprecate for CVT + params (if not already done)
   def self.find_for_autocomplete(params)
-    term = "#{params[:term]}%"
-    where_string = "name LIKE '#{term}' OR name ILIKE '%#{term}' OR name = '#{term}' OR definition ILIKE '%#{term}'"
+    term = params.require(:term)
+    where_string = ['name LIKE ? ' \
+                    'OR name ILIKE ? ' \
+                    'OR name = ? ' \
+                    'OR definition ILIKE ?',
+                    term, '%' + term, term, term]
     ControlledVocabularyTerm.where(where_string).where(project_id: params[:project_id], type: 'Topic')
   end
 
 
   # @param used_on [String] one of `Citation` (default) or `Content`
   # @return [Scope]
-  #    the max 10 most recently used topics, as used on Content or Citation 
+  #    the max 10 most recently used topics, as used on Content or Citation
   def self.used_recently(used_on = 'Citation')
 
-   t = case used_on
-       when 'Citation'
-         CitationTopic.arel_table
-       when 'Content'
-         Content.arel_table
-       end
-   
-    p = Topic.arel_table 
+    t = case used_on
+          when 'Citation'
+            CitationTopic.arel_table
+          when 'Content'
+            Content.arel_table
+        end
+
+    p = Topic.arel_table
 
     # i is a select manager
     i = t.project(t['topic_id'], t['created_at']).from(t)
-      .where(t['created_at'].gt( 1.weeks.ago ))
-      .order(t['created_at'])
+          .where(t['created_at'].gt(1.weeks.ago))
+          .order(t['created_at'])
 
-    # z is a table alias 
+    # z is a table alias
     z = i.as('recent_t')
 
     Topic.joins(
@@ -47,7 +53,8 @@ class Topic < ControlledVocabularyTerm
     ).distinct.limit(10)
   end
 
-  # @params klass [String] if target is `Citation` then if provided limits to those classes with citations, if `Content` then not used
+  # @params klass [String] if target is `Citation` then if provided limits to those classes with citations,
+  # if `Content` then not used
   # @params target [String] one of `Citation` or `Content`
   # @return [Hash] topics optimized for user selection
   def self.select_optimized(user_id, project_id, klass, target = 'Citation')
@@ -56,14 +63,17 @@ class Topic < ControlledVocabularyTerm
       pinboard: Topic.pinned_by(user_id).where(project_id: project_id).to_a
     }
 
-    case target 
-    when 'Citation'
-      h[:recent] = Topic.where(project_id: project_id).used_on_klass(klass).used_recently('Citation').limit(10).distinct.to_a
-    when 'Content'
-      h[:recent] = Topic.joins(:contents).where(project_id: project_id).used_recently('Content').limit(10).distinct.to_a
+    case target
+      when 'Citation'
+        h[:recent] = Topic.where(project_id: project_id)
+                       .used_on_klass(klass).used_recently('Citation').limit(10).distinct.to_a
+      when 'Content'
+        h[:recent] = Topic.joins(:contents)
+                       .where(project_id: project_id).used_recently('Content').limit(10).distinct.to_a
     end
 
-    h[:quick] = (Topic.pinned_by(user_id).pinboard_inserted.where(project_id: project_id).to_a  + h[:recent][0..3]).uniq 
+    h[:quick] = (Topic.pinned_by(user_id)
+                   .pinboard_inserted.where(project_id: project_id).to_a + h[:recent][0..3]).uniq
     h
   end
 
