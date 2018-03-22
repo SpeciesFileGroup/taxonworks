b302db4592779234792da7641391dd75290e69c0
Jim Tucker <tuckerjd@uiuc.edu>
2018-03-22 13:04:27 -0500

    TaxonWorks Issue #352: Modified 'Identifier::Global' to a a resolution test for identifiers which are http requests.

--- app/models/identifier/global.rb
+++ app/models/identifier/global.rb
@@ -1,17 +1,19 @@
 # The identifier that is globally unique.
 #
-# Curators of a specific project assert one canonical global identifier per type for each object, this 
-# identifier is identified by having .relation.nil?.  If the curators feel there are multiple global identifiers 
-# for a given instance they must provide an explicit relationship between the canonical identifier and the 
+# Curators of a specific project assert one canonical global identifier per type for each object, this
+# identifier is identified by having .relation.nil?.  If the curators feel there are multiple global identifiers
+# for a given instance they must provide an explicit relationship between the canonical identifier and the
 # alternate identifiers.
 #
-# @!attribute relation 
+# @!attribute relation
 #   @return [String]
 #   Defines the relationship between the curator asserted canonical identifier and other identifiers
 #   of the same type. Must be provided for every global identifier of the same type beyond the first.
-#   Relations are drawn from skos (http://www.w3.org/TR/skos-reference/#mapping) 
+#   Relations are drawn from skos (http://www.w3.org/TR/skos-reference/#mapping)
 #
 class Identifier::Global < Identifier
+
+  include SoftValidation
 
   validates :namespace_id, absence: true
   validates :relation, inclusion: {in: ::SKOS_RELATIONS.keys}, allow_nil: true
@@ -20,16 +22,36 @@ class Identifier::Global < Identifier
   # Identifier can only be used once, i.e. mapped to a single TW concept
   validates_uniqueness_of :identifier, scope: [:project_id]
 
+  soft_validate(:sv_resolves?, set: :resolved)
+
   protected
 
   def set_cached
     update_column(:cached, identifier)
   end
 
+  #region Validation
+
   def permit_only_one_global_without_relation_supplied_per_type
-    if identifier_object && identifier_object.identifiers.where(type: type.to_s).where.not(id: id ).any?
-      errors.add(:relation, " an existing identifier of type #{type} exists, a relation for this identifier must be provided") if self.relation.nil?
+    if identifier_object && identifier_object.identifiers.where(type: type.to_s).where.not(id: id).any?
+      errors.add(:relation,
+                 " an existing identifier of type #{type} exists, a relation for this identifier must be provided"
+      ) if self.relation.nil?
     end
   end
+
+  #endregion
+
+  #region Soft validation
+  #
+  def sv_resolves?
+    responded = false
+    unless identifier.nil?
+      responded = Utilities::Net.resolves?(identifier)
+    end
+    soft_validations.add(:identifier, "Identifier '#{identifier}' does not resolve.") unless responded
+    responded
+  end
+  #endregion
 
 end
--- spec/models/identifier/global_spec.rb
+++ spec/models/identifier/global_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 describe Identifier::Global, type: :model, group: :identifiers do
 
   let!(:otu) { FactoryBot.create(:valid_otu) }
-  let(:global_identifier) {Identifier::Global.new}
+  let(:global_identifier) { Identifier::Global.new }
 
   context 'validation' do
     before {
@@ -20,7 +20,9 @@ describe Identifier::Global, type: :model, group: :identifiers do
     specify 'more than one global identifier with a valid relation is allowed per identifier type' do
       expect(otu.identifiers << Identifier::Global::Uri.new(identifier: 'http://abc.net/bar/22')).to be_truthy
       otu.reload
-      i = Identifier::Global::Uri.new(identifier: 'http://abc.net/bar/29', identifier_object: otu, relation: 'skos:closeMatch')
+      i = Identifier::Global::Uri.new(identifier:        'http://abc.net/bar/29',
+                                      identifier_object: otu,
+                                      relation:          'skos:closeMatch')
       expect(i.valid?).to be_truthy
     end
 
@@ -35,7 +37,9 @@ describe Identifier::Global, type: :model, group: :identifiers do
     specify 'identifer is unique within project (same type)' do
       expect(otu.identifiers << Identifier::Global::Uri.new(identifier: 'http://abc.net/bar/22')).to be_truthy
       otu.reload
-      i = Identifier::Global::Uri.new(identifier: 'http://abc.net/bar/22', identifier_object: otu, relation: 'skos:closeMatch')
+      i = Identifier::Global::Uri.new(identifier:        'http://abc.net/bar/22',
+                                      identifier_object: otu,
+                                      relation:          'skos:closeMatch')
       expect(i.valid?).to be_falsey
       expect(i.errors.include?(:identifier)).to be_truthy
     end
@@ -44,16 +48,19 @@ describe Identifier::Global, type: :model, group: :identifiers do
     # xspecify 'identifer is unique within project (different type)' do
     #   expect(otu.identifiers << Identifier::Global::Lsid.new(identifier: 'http://abc.net/bar/22')).to be_truthy
     #   otu.reload
-    #   i = Identifier::Global::Uri.new(identifier: 'http://abc.net/bar/22', identifier_object: otu,  relation: 'skos:closeMatch')
+    #   i = Identifier::Global::Uri.new(identifier: 'http://abc.net/bar/22', identifier_object: otu,  relation:
+    # 'skos:closeMatch')
     #   expect(i.valid?).to be_falsey
     #   expect(i.errors.include?(:identifier)).to be_truthy
     # end
 
     specify 'same identifer is allowed b/w project (same type)' do
       id = 'http://abc.net/bar/22'
-      expect(otu.identifiers << Identifier::Global::Uri.new(identifier: id )).to be_truthy
+      expect(otu.identifiers << Identifier::Global::Uri.new(identifier: id)).to be_truthy
       p = FactoryBot.create(:valid_project, name: 'New Project')
-      i = Identifier::Global::Uri.new(identifier: id, identifier_object: FactoryBot.create(:valid_otu, project_id: p.id), project_id: p.id)
+      i = Identifier::Global::Uri.new(identifier:        id,
+                                      identifier_object: FactoryBot.create(:valid_otu, project_id: p.id),
+                                      project_id:        p.id)
       expect(i.valid?).to be_truthy
       expect(i.errors.include?(:identifier)).to be_falsey
     end
@@ -67,7 +74,40 @@ describe Identifier::Global, type: :model, group: :identifiers do
 
   specify 'cached is set' do
     id = 'http://abc.net/bar/22'
-    i = Identifier::Global::Uri.create!(identifier: id, identifier_object: Otu.create(name: 'aaa'))
+    i  = Identifier::Global::Uri.create!(identifier: id, identifier_object: Otu.create(name: 'aaa'))
     expect(i.reload.cached).to eq(id)
+  end
+
+  describe 'soft validation' do
+
+    specify 'responding URI' do
+      global_identifier.identifier = 'http://data.nhm.ac.uk/object/a9bdc16d-c9ba-4e32-9311-d5250af2b5ac'
+      VCR.use_cassette('responding URI') do
+        global_identifier.soft_validate(:resolved)
+      end
+      expect(global_identifier.soft_validations.messages_on(:identifier).empty?).to be_truthy
+    end
+
+    specify 'non-responding URI' do
+      global_identifier.identifier = 'http://sandbox.speciesfile.org/object/a9bdc16d-c9ba-4e32-9311-d5250af2b5ac'
+      VCR.use_cassette('non-responding URI') do
+        global_identifier.soft_validate(:resolved)
+      end
+      expect(global_identifier.soft_validations.messages_on(:identifier).count).to eq(1)
+    end
+
+    specify 'non-conforming URI' do
+      global_identifier.identifier = 'data.nhm.ac.uk/object/a9bdc16d-c9ba-4e32-9311-d5250af2b5ac'
+      VCR.use_cassette('non-conforming URI') do
+        global_identifier.soft_validate(:resolved)
+      end
+      expect(global_identifier.soft_validations.messages_on(:identifier).count).to eq(1)
+    end
+
+    specify 'empty URI' do
+      global_identifier.identifier = ''
+      global_identifier.soft_validate(:resolved)
+      expect(global_identifier.soft_validations.messages_on(:identifier).count).to eq(1)
+    end
   end
 end
