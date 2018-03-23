95d9abf903f62d53e4c47905c8a0c12a32cb172e
José Luis Pereira <pepelui@outlook.com>
2018-03-23 16:07:08 -0300

    Check browser compatibility #336

---
+++ app/assets/javascripts/utilities/browser_compatibility.js
@@ -0,0 +1,46 @@
+/*
+<div class="modal-mask">
+  <div class="modal-wrapper">
+    <div class="modal-container">
+      <div class="modal-header">
+        <h3 class="browsercompatibility-title">Browser upgrade needed</h3>
+      </div>
+      <div class="modal-body">
+        <p>TaxonWorks has detected that you are using an outdated browser that will prevent you from accessing certain features. An update is required.</p>
+      </div>
+      <div class="modal-footer">
+        <a href="http://outdatedbrowser.com/">Click to download a compatible browser</a>
+      </div>
+    </div>
+  </div>
+</div>
+*/
+var TW = TW || {};
+TW.utilities = TW.utilities || {};
+TW.utilities.browserCompatibility = TW.utilities.browserCompatibility || {};
+
+
+TW.utilities.browserCompatibility = {
+
+  incompatibilityBrowserLists: ['MSIE', 'Trident/', 'Safari'],
+
+  injectMotal: function() {
+    if(this.incompatibilityBrowser()) {
+      $('<div class="modal-mask"> <div class="modal-wrapper"> <div class="modal-container"> <div class="modal-header"> <h3 class="browsercompatibility-title">Browser upgrade needed</h3> </div> <div class="modal-body"> <p>TaxonWorks has detected that you are using an outdated browser that will prevent you from accessing certain features. An update is required.</p></div><div class="modal-footer"><a href="http://outdatedbrowser.com/">Click to download a compatible browser</a> </div> </div> </div> </div>').appendTo( "body" );
+    };
+  },
+
+  incompatibilityBrowser: function() {
+    var ua = window.navigator.userAgent;
+    for(var i = 0; i < this.incompatibilityBrowserLists.length; i++) {
+      if(ua.indexOf(this.incompatibilityBrowserLists[i]) > 0) {
+        return true
+      }
+    }
+    return false;
+  }
+}
+
+$(document).on('turbolinks:load', function() {
+  TW.utilities.browserCompatibility.injectMotal();
+});
