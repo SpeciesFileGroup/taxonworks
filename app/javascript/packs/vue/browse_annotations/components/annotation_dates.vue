432eb42a26439ed1dd2bf2893b6dcc1ec0fc5104
José Luis Pereira <pepelui@outlook.com>
2018-03-26 17:19:26 -0300

    Example

--- app/javascript/packs/vue/browse_annotations/components/annotation_dates.vue
+++ app/javascript/packs/vue/browse_annotations/components/annotation_dates.vue
@@ -3,12 +3,12 @@
         <input v-model="annotation_dates.start"
                name="dateStart"
                type="date"
-               @change="selectDate()"
+               @change="getResult()"
         />
         <input v-model="annotation_dates.end"
                name="dateEnd"
                type="date"
-               @change="selectDate()"
+               @change="getResult()"
         />
 
         <span v-for="(item, key) in result"> {{ key }} : {{ item }} <br></span>
@@ -22,11 +22,6 @@
         type: String,
       }
     },
-    watch: {
-      value(newVal) {
-        this.getResult(newVal)
-      }
-    },
     data: function () {
       return {
         annotation_dates: {
@@ -36,19 +31,9 @@
         result: undefined
       }
     },
-    // mounted: function () {
-    //   this.$http.get('/tasks/browse_annotations/get_type_list').then(response => {
-    //     console.log(response); // this is necessary to show traffic?
-    //     this.list = response.body;
-    //   })
-    // },
     methods: {
-      selectDate(type) {
-        this.$emit('input', type.valueOf())
-      },
-      getResult(newVal) {
-        this.$http.post('/tasks/browse_annotations/set_dates', {annotationDates: newVal}).then(response => {
-          // console.log(response); // this is necessary to show traffic?
+      getResult() {
+        this.$http.post('/tasks/browse_annotations/set_dates', {annotationDates: this.annotation_dates}).then(response => {
           this.$emit('annotation_dates_selected', response.body);
           this.result = response.body;
         })
