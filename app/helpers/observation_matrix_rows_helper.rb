262e0fa676f7567be84226f0ca2970cf36434400
mjy <diapriid@gmail.com>
2018-03-26 16:04:50 -0500

    observation

--- app/helpers/observation_matrix_rows_helper.rb
+++ app/helpers/observation_matrix_rows_helper.rb
@@ -1,16 +1,16 @@
 module ObservationMatrixRowsHelper
 
-  def observation_matrix_row_tag(matrix_row)
-    return nil if matrix_row.nil?
-    get_matrix_row_name(matrix_row)
+  def observation_matrix_row_tag(observation_matrix_row)
+    return nil if observation_matrix_row.nil?
+    get_observation_matrix_row_name(observation_matrix_row)
   end
 
-  def observation_matrix_row_link(matrix_row)
-    return nil if matrix_row.nil?
-    link_to(observation_matrix_row_tag(matrix_row).html_safe, matrix_row)
+  def observation_matrix_row_link(observation_matrix_row)
+    return nil if observation_matrix_row.nil?
+    link_to(observation_matrix_row_tag(observation_matrix_row).html_safe, observation_matrix_row)
   end
 
-  def get_matrix_row_name(matrix_row)
-    object_tag(matrix_row.row_object)
+  def get_observation_matrix_row_name(observation_matrix_row)
+    object_tag(observation_matrix_row.row_object)
   end
 end
