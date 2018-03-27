dfcdad8df1f3859097b17d2a3d6f7ae939d1914d
mjy <diapriid@gmail.com>
2018-03-26 22:12:34 -0500

    fix browse link

--- app/controllers/tasks/nomenclature/browse_controller.rb
+++ app/controllers/tasks/nomenclature/browse_controller.rb
@@ -4,7 +4,7 @@ class Tasks::Nomenclature::BrowseController < ApplicationController
   def index
     @taxon_name = TaxonName.where(project_id: sessions_current_project_id).find(params[:id]) if !params[:id].blank?
     @taxon_name ||= Project.find(sessions_current_project_id).root_taxon_name
-    redirect_to :new_taxon_name_task, notice: 'No names to browse yet.' and return if params[:id].blank?
+    redirect_to :new_taxon_name_task, notice: 'No names to browse yet.' and return if @taxon_name.nil?
     @data = NomenclatureCatalog.data_for(@taxon_name)
   end
 
