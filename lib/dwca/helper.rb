c5631cebcc5b74d6d5f50c19d6d8f7c6eb40aa27
Jim Tucker <tuckerjd@uiuc.edu>
2018-04-03 08:33:02 -0500

    Rubocop edits.

--- lib/dwca/helper.rb
+++ lib/dwca/helper.rb
@@ -1,9 +1,11 @@
 module Dwca::Helper
 
   # Assume a single range.  Extend as we need for ranges.
+  # @param [String] eventDate
+  # @return [Cronic] parse result
   def parse_date(eventDate)
     # TODO: detect ranges
-    Chronic.parse(eventDate) 
+    Chronic.parse(eventDate)
   end
 
 end
--- lib/dwca/packer.rb
+++ lib/dwca/packer.rb
@@ -20,6 +20,8 @@ module Dwca::Packer
     attr_accessor :data, :eml, :meta, :zipfile, :scope, :total
     attr_reader :filename
 
+    # @param [Hash] args
+    # @return [Ignored]
     def initialize(record_scope)
       raise ArgumentError, 'must pass a scope' if !record_scope.kind_of?( ActiveRecord::Relation )
       @scope = record_scope
