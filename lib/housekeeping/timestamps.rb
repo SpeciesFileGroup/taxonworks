9e6f86ee912b5602b9fd910ad49b5d6b297fa5dc
Jim Tucker <tuckerjd@uiuc.edu>
2018-04-03 09:01:37 -0500

    Checking and adding @param and @return for RDoc.

--- lib/housekeeping/timestamps.rb
+++ lib/housekeeping/timestamps.rb
@@ -19,62 +19,76 @@ module Housekeeping::Timestamps
   end
 
   module ClassMethods
+    # @return [Scope]
     def first_created
       all.order(created_at: :asc).first
     end
 
+    # @return [Scope]
     def last_created
       all.order(created_at: :desc).first
     end
 
+    # @return [Scope]
     def first_updated
       all.order(updated_at: :asc).first
     end
 
+    # @return [Scope]
     def last_updated
       all.order(updated_at: :desc).first
     end
 
+    # @return [Scope]
     def created_this_week
       where(created_at: 1.weeks.ago..Time.now)
     end
 
+    # @return [Scope]
     def updated_this_week
       where(updated_at: 1.weeks.ago..Time.now)
     end
 
+    # @return [Scope]
     def created_today
       where(created_at: 1.day.ago..Time.now)
     end
 
+    # @return [Scope]
     def updated_today
       where(updated_at: 1.day.ago..Time.now)
     end
 
     #  Otu.created_in_last(2.weeks)
+    # @return [Scope]
     def created_in_last(time)
       where(created_at: time.ago..Time.now)
     end
 
     #  Otu.created_in_last(1.month)
+    # @return [Scope]
     def updated_in_last(time)
       where(updated_at: time.ago..Time.now)
     end
 
+    # @return [Scope]
     def created_last(number = 10)
       limit(number).order(created_at: :DESC)
     end
 
+    # @return [Scope]
     def updated_last(number = 10)
       limit(number).order(updated_at: :DESC)
     end
 
+    # @return [Scope]
     def where_dated_string(table, type)
       "#{table}.#{type}ated_at >= ? and #{table}.#{type}ated_at <= ?"
     end
 
   end
 
+  # @return [Array]
   def data_breakdown_for_chartkick_recent
     Rails.application.eager_load!
     data = []
