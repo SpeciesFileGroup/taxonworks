183b399d37cbb568b390a031830d46781e0efefc
José Luis Pereira <pepelui@outlook.com>
2018-04-05 11:03:54 -0300

    Added tag list for smart selector #346

--- app/javascript/packs/vue/tasks/new_matrix/components/rows/rowsView.vue
+++ app/javascript/packs/vue/tasks/new_matrix/components/rows/rowsView.vue
@@ -11,6 +11,7 @@
         name="rows-smart"/>
       <component 
         v-if="componentExist"
+        @send="create"
         :list="selectorLists[view]"
         :is="view + 'Component'"/>
     </div>
@@ -20,16 +21,30 @@
 
 import smartSelector from '../shared/smartSelector'
 import searchComponent from './search'
+import { 
+  default as quickComponent, 
+  default as recentComponent, 
+  default as pinboardComponent 
+} from '../shared/tag_list'
 import { GetSmartSelector } from '../../request/resources'
+import { ActionNames } from '../../store/actions/actions';
+import { GetterNames } from '../../store/getters/getters'
+
 
 export default {
   components: {
     smartSelector,
+    quickComponent,
+    recentComponent,
+    pinboardComponent,
     searchComponent
   },
   computed: {
     componentExist() {
       return this.$options.components[this.view + 'Component']
+    },
+    matrixId() {
+      return this.$store.getters[GetterNames.GetMatrix].id
     }
   },
   data() {
@@ -46,5 +61,15 @@ export default {
       this.selectorLists = response
     })
   },
+  methods: {
+    create(item) {
+      let data = {
+        controlled_vocabulary_term_id: item.id,
+        observation_matrix_id: this.matrixId,
+        type: 'ObservationMatrixRowItem::TaggedRowItem'
+      }
+      this.$store.dispatch(ActionNames.CreateRowItem, data)
+    }
+  }
 }
 </script>
---
+++ app/javascript/packs/vue/tasks/new_matrix/components/shared/tag_list.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <tag-item
+      class="button-submit"
+      v-for="(item, key) in list"
+      :key="key"
+      :item="item"
+      display="object_tag"
+      @select="sendItem"/>
+  </div>
+</template>
+<script>
+
+import TagItem from '../../../../components/tag_item.vue'
+
+export default {
+  components: {
+    TagItem
+  },
+  props: {
+    list: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    sendItem(item) {
+      this.$emit('send', item)
+    }
+  }
+}
+</script>
+
