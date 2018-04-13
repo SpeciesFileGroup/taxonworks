1f49420d117945246282186ebb117c348d4e4613
jrflood <jrflood@illinois.edu>
2018-04-13 11:41:17 -0500

    save before update of development and merge

--- app/javascript/packs/vue/browse_annotations/components/annotation_for.vue
+++ app/javascript/packs/vue/browse_annotations/components/annotation_for.vue
@@ -43,6 +43,7 @@
             // you will take the values of type and used_on from annotationType object.
             // annotationType.type
             // aanotationType.used_on
+            // this.smartSelector = annotationType.used_on
           }
         },
         deep: true
