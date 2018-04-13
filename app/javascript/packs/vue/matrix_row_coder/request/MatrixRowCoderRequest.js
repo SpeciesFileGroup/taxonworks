8ac00d4db8eab59551b9d4c87446d3d96a82fdff
José Luis Pereira <pepelui@outlook.com>
2018-04-13 10:56:32 -0300

    Removed API params

--- app/javascript/packs/vue/matrix_row_coder/request/MatrixRowCoderRequest.js
+++ app/javascript/packs/vue/matrix_row_coder/request/MatrixRowCoderRequest.js
@@ -61,7 +61,8 @@ export default class MatrixRowCoderRequest extends IMatrixRowCoderRequest {
 
   setApi ({apiBase, apiParams}) {
     this.apiBase = apiBase
-    this.apiParams = apiParams
+    this.apiParams = {}
+//    this.apiParams = {apiParams}
   }
 
   buildGetUrl (url, extraParams = {}) {
