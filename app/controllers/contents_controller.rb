4cf9a60b4cc4bb86560973f3cd6d863fa32ea375
Jim Tucker <tuckerjd@uiuc.edu>
2018-04-24 11:54:02 -0500

    #54 Replacing model.generate_download with Download.generate_csv via  model_controller.download.

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -5,6 +5,7 @@ class ContentsController < ApplicationController
 
   # GET /contents
   # GET /contents.json
+  # @return [Ignored]
   def index
     respond_to do |format|
       format.html do
@@ -19,20 +20,24 @@ class ContentsController < ApplicationController
 
   # GET /contents/1
   # GET /contents/1.json
+  # @return [Ignored]
   def show
   end
 
   # GET /contents/new
+  # @return [Ignored]
   def new
     @content = Content.new
   end
 
   # GET /contents/1/edit
+  # @return [Ignored]
   def edit
   end
 
   # POST /contents
   # POST /contents.json
+  # @return [Ignored]
   def create
     @content = Content.new(content_params)
 
@@ -49,6 +54,7 @@ class ContentsController < ApplicationController
 
   # PATCH/PUT /contents/1
   # PATCH/PUT /contents/1.json
+  # @return [Ignored]
   def update
     respond_to do |format|
       if @content.update(content_params)
@@ -63,32 +69,38 @@ class ContentsController < ApplicationController
 
   # DELETE /contents/1
   # DELETE /contents/1.json
+  # @return [Ignored]
   def destroy
-    @content.destroy
+    @content.destroy!
     respond_to do |format|
       format.html { redirect_to contents_url }
       format.json { head :no_content }
     end
   end
 
+  # @return [Ignored]
   def list
     @contents = Content.with_project_id(sessions_current_project_id).order(:id).page(params[:page])
   end
 
+  # @return [Ignored]
   def search
     if params[:id].blank?
-      redirect_to content_path, notice: 'You must select an item from the list with a click or tab press before clicking show.'
+      redirect_to content_path,
+                  notice: 'You must select an item from the list with a click or tab press before clicking show.'
     else
       redirect_to content_path(params[:id])
     end
   end
 
   # GET /contents/filter.json
+  # @return [Ignored]
   def filter
     @contents = filtered_content
     render '/contents/index'
   end
 
+  # @return [JSON]
   def autocomplete
     @contents = Content.find_for_autocomplete(params.merge(project_id: sessions_current_project_id))
     data = @contents.collect do |t|
@@ -105,12 +117,16 @@ class ContentsController < ApplicationController
   end
 
   # GET /contents/download
+  # @return [Ignored]
   def download
-    send_data Content.generate_download( Content.where(project_id: sessions_current_project_id) ), type: 'text', filename: "contents_#{DateTime.now}.csv"
+    send_data(Download.generate_csv(Content.where(project_id: sessions_current_project_id)),
+              type:     'text',
+              filename: "contents_#{DateTime.now}.csv")
   end
 
   private
 
+  # @return [ActionController::Parameters]
   def filtered_content
     p =  params.permit(:otu_id, :topic_id, :hours_ago, :most_recent_updates).to_h.symbolize_keys
     p[:most_recent_updates] = 10 if p.empty?
@@ -119,11 +135,13 @@ class ContentsController < ApplicationController
       .with_project_id(sessions_current_project_id)
   end
 
+  # @return [Ignored]
   def set_content
     @content = Content.with_project_id(sessions_current_project_id).find(params[:id])
     @recent_object = @content
   end
 
+  # @return [ActionController::Parameters]
   def content_params
     params.require(:content).permit(:text, :otu_id, :topic_id, :type)
   end
