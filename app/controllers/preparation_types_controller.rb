b67de627f0e218fbc42856b15162316351e88b04
Jim Tucker <tuckerjd@uiuc.edu>
2018-04-27 06:43:23 -0500

    #54 Replacing model.generate_download with Download.generate_csv via  model_controller.download.

--- app/controllers/preparation_types_controller.rb
+++ app/controllers/preparation_types_controller.rb
@@ -84,7 +84,9 @@ class PreparationTypesController < ApplicationController
 
   # GET /preparation_types/download
   def download
-    send_data PreparationType.generate_download(PreparationType.all), type: 'text', filename: "preparation_types_#{DateTime.now}.csv"
+    send_data( Download.generate_csv(PreparationType.all),
+               type: 'text',
+               filename: "preparation_types_#{DateTime.now}.csv")
   end
 
   # def autocomplete
--- app/models/preparation_type.rb
+++ app/models/preparation_type.rb
@@ -20,15 +20,4 @@ class PreparationType < ApplicationRecord
   has_many :collection_objects, dependent: :restrict_with_error
   validates_presence_of :name, :definition
 
-  def self.generate_download(scope)
-    CSV.generate do |csv|
-      csv << column_names
-      scope.order(id: :asc).find_each do |o|
-        csv << o.attributes.values_at(*column_names).collect { |i|
-          i.to_s.gsub(/\n/, '\n').gsub(/\t/, '\t')
-        }
-      end
-    end
-  end
-
 end
