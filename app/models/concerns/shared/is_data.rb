481ebde683688c947f506a9932396bec627d1eab
Jim Tucker <tuckerjd@uiuc.edu>
2018-05-09 09:21:37 -0500

    #373 Uniquify sources: Adding Stripper#tested_attributes().

--- app/models/concerns/shared/is_data.rb
+++ app/models/concerns/shared/is_data.rb
@@ -54,8 +54,8 @@ module Shared::IsData
     # @return [Scope]
     def similar(attr)
       klass = self
-      attr  = Stripper.strip_similar_attributes(klass, attr)
-      attr  = attr.select { |_kee, val| val.present? }
+      attr = Stripper.strip_similar_attributes(klass, attr)
+      attr = attr.select { |_kee, val| val.present? }
 
       scope = klass.where(attr)
       scope
@@ -65,7 +65,7 @@ module Shared::IsData
     # @return [Scope]
     def identical(attr)
       klass = self
-      attr  = Stripper.strip_identical_attributes(klass, attr)
+      attr = Stripper.strip_identical_attributes(klass, attr)
 
       scope = klass.where(attr)
       scope
@@ -111,9 +111,9 @@ module Shared::IsData
   # @return [Scope]
   def similar
     klass = self.class
-    attr  = Stripper.strip_similar_attributes(klass, attributes)
+    attr = Stripper.strip_similar_attributes(klass, attributes)
     # matching only those attributes from the instance which are not empty
-    attr  = attr.select { |_kee, val| val.present? }
+    attr = attr.select { |_kee, val| val.present? }
     if id
       scope = klass.where(attr).not_self(self)
     else
@@ -125,7 +125,7 @@ module Shared::IsData
   # @return [Scope]
   def identical
     klass = self.class
-    attr  = Stripper.strip_identical_attributes(klass, attributes)
+    attr = Stripper.strip_identical_attributes(klass, attributes)
     if id
       scope = klass.where(attr).not_self(self)
     else
@@ -149,7 +149,7 @@ module Shared::IsData
       begin # test to see if this class has an IGNORE_SIMILAR constant
         ig = add_class_list(klass::IGNORE_SIMILAR)
       rescue NameError
-        ig = RESERVED_ATTRIBUTES.dup
+        ig = RESERVED_ATTRIBUTES.dup.map(&:to_s)
       end
       attr.delete_if { |kee, _value| ig.include?(kee) }
       attr
@@ -161,7 +161,7 @@ module Shared::IsData
       begin # test to see if this class has an IGNORE_IDENTICAL constant
         ig = add_class_list(klass::IGNORE_IDENTICAL)
       rescue NameError
-        ig = RESERVED_ATTRIBUTES.dup
+        ig = RESERVED_ATTRIBUTES.dup.map(&:to_s)
       end
       attr.delete_if { |kee, _value| ig.include?(kee) }
       attr
@@ -170,11 +170,27 @@ module Shared::IsData
     # @param [Array] list of symbols to be ignored
     # @return [Array] of strings to be ignored
     def self.add_class_list(list)
-      ig_list  = RESERVED_ATTRIBUTES.dup
+      ig_list = RESERVED_ATTRIBUTES.dup
       add_list = list.dup if list.any?
-      ig_list  += add_list if add_list
+      ig_list += add_list if add_list
       # convert ignore list from symbols to strings for subsequent include test
       return ig_list.map(&:to_s)
+    end
+
+    # @param [Class] klass
+    # @param [Symbol, String] compare - one of [:identical, :similar] (optional, defaults to :identical)
+    # @return [Array] of symbols which represent the attributes which will be tested for a given class
+    def self.tested_attributes(klass, compare = :identical)
+      case compare.to_s
+        when 'similar'
+          obj = klass.new
+          obj.attributes.each_key { |kee| obj[kee] = 1 }
+          strip_similar_attributes(klass, obj.attributes).collect { |kee, _val| kee.to_sym }
+        # when 'identical'
+        #   strip_identical_attributes(klass, klass.new.attributes).collect { |kee, _val| kee.to_sym }
+        else
+          strip_identical_attributes(klass, klass.new.attributes).collect { |kee, _val| kee.to_sym }
+      end
     end
   end
 end
