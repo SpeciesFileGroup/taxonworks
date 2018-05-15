0786a579e7943cdc0de8c3b4f5f760177f9c70cf
mjy <diapriid@gmail.com>
2018-05-14 21:29:30 -0500

    pingz

--- app/controllers/ping_controller.rb
+++ app/controllers/ping_controller.rb
@@ -4,4 +4,24 @@ class PingController < ApplicationController
     render json: '{"pong": true}', status: :ok
   end
 
+  def pingz
+    if asset_exists?('zzzz.css')
+      render json: '{"pong": true}', status: :ok and return if !pathname.nil?
+    else
+      render json: '{"pong": false}', status: :service_unavailable 
+    end
+  end
+
+  protected
+
+  def asset_exists?(path)
+    begin
+      pathname = Rails.application.assets.resolve(path)
+      return !!pathname # double-bang turns String into boolean
+    rescue Sprockets::FileNotFound
+      return false
+    end
+  end
+
+
 end
