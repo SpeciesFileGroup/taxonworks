8d5c65d9571cf7d8851b2c015f799f740457f625
José Luis Pereira <pepelui@outlook.com>
2018-05-28 18:01:05 -0300

    Test

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -1,5 +1,6 @@
+console.log("Loading test configuration")
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
-console.log("Loading test configuration")
+
 module.exports = environment.toWebpackConfig()
