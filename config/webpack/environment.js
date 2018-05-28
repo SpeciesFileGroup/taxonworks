1f2e6f8579665c679a74aa434278db2ff8584932
José Luis Pereira <pepelui@outlook.com>
2018-05-27 21:53:19 -0300

    Removed resolve url loader

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -7,8 +7,6 @@ environment.plugins.append('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.append('vue', vue)
 environment.loaders.append('styl', styl)
 
-environment.loaders.get('sass').use.splice(-1, 0, {
-  loader: 'resolve-url-loader',
-});
+
 
 module.exports = environment
