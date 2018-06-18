004c93d8e3e0dd68eeca6a529167c099c7624029
José Luis Pereira <pepelui@outlook.com>
2018-06-18 17:34:07 -0300

    Added more mutations

--- app/javascript/vue/tasks/digitize/store/mutations/mutations.js
+++ app/javascript/vue/tasks/digitize/store/mutations/mutations.js
@@ -1,3 +1,17 @@
+import setCollectionObject from './CollectionObject/setCollectionObject'
+import setCollectionObjectBufferedCollectionEvent from './CollectionObject/setCollectionObjectBufferedCollectionEvent'
+import setCollectionObjectBufferedDeterminations from './CollectionObject/setCollectionObjectBufferedDeterminations'
+import setCollectionObjectBufferedOtherLabel from './CollectionObject/setCollectionObjectBufferedOtherLabel'
+import setCollectionObjectCollectionEventAttributes from './CollectionObject/setCollectionObjectCollectionEventAttributes'
+import setCollectionObjectConteiner from './CollectionObject/setCollectionObjectConteiner'
+import setCollectionObjectDeaccessionedAt from './CollectionObject/setCollectionObjectDeaccessionedAt'
+import setCollectionObjectDeaccessionReason from './CollectionObject/setCollectionObjectDeaccessionReason'
+import setCollectionObjectEventId from './CollectionObject/setCollectionObjectEventId'
+import setCollectionObjectPreparationId from './CollectionObject/setCollectionObjectPreparationId'
+import setCollectionObjectRangeLotId from './CollectionObject/setCollectionObjectRangeLotId'
+import setCollectionObjectRepositoryId from './CollectionObject/setCollectionObjectRepositoryId'
+import setCollectionObjectTotal from './CollectionObject/setCollectionObjectTotal'
+
 import setCollectionEventLabel from './setCollectionEventLabel'
 import setCollectionEventCollectors from './setCollectionEventCollectors'
 import setCollectionEventDate from './setCollectionEventDate'
@@ -29,10 +43,26 @@ const MutationNames = {
   SetCollectionEventTripIdentifier: 'setCollectionEventTripIdentifier',
   SetCollectionEventElevationPrecision: 'setCollectionEventElevationPrecision',
   SetCollectionEventMaximumElevation: 'setCollectionEventMaximumElevation',
-  SetCollectionEventMinimumElevation: 'setCollectionEventMinimumElevation'
+  SetCollectionEventMinimumElevation: 'setCollectionEventMinimumElevation',
+
+  SetCollectionObject: 'setCollectionObject',
+  SetCollectionObjectBufferedCollectionEvent: 'setCollectionObjectBufferedCollectionEvent',
+  SetCollectionObjectBufferedDeterminations: 'setCollectionObjectBufferedDeterminations',
+  SetCollectionObjectBufferedOtherLabel: 'setCollectionObjectBufferedOtherLabel',
+  SetCollectionObjectCollectionEventAttributes: 'setCollectionObjectCollectionEventAttributes',
+  SetCollectionObjectConteiner: 'setCollectionObjectConteiner',
+  SetCollectionObjectDeaccessionedAt: 'setCollectionObjectDeaccessionedAt',
+  SetCollectionObjectDeaccessionReason: 'setCollectionObjectDeaccessionReason',
+  SetCollectionObjectEventId: 'setCollectionObjectEventId',
+  SetCollectionObjectPreparationId: 'setCollectionObjectPreparationId',
+  SetCollectionObjectRangeLotId: 'setCollectionObjectRangeLotId',
+  SetCollectionObjectRepositoryId: 'setCollectionObjectRepositoryId',
+  SetCollectionObjectTotal: 'setCollectionObjectTotal'  
 }
 
 const MutationFunctions = {
+  [MutationNames.SetCollectionObject]: setCollectionObject,
+  [MutationNames.SetCollectionObjectBufferedCollectionEvent]: setCollectionObjectBufferedCollectionEvent,
   [MutationNames.SetCollectionEventLabel]: setCollectionEventLabel,
   [MutationNames.SetCollectionEventCollectors]: setCollectionEventCollectors,
   [MutationNames.SetCollectionEventDate]: setCollectionEventDate,
@@ -47,7 +77,18 @@ const MutationFunctions = {
   [MutationNames.SetCollectionEventTripIdentifier]: setCollectionEventTripIdentifier,
   [MutationNames.SetCollectionEventElevationPrecision]: setCollectionEventElevationPrecision,
   [MutationNames.SetCollectionEventMaximumElevation]: setCollectionEventMaximumElevation,
-  [MutationNames.SetCollectionEventMinimumElevation]: setCollectionEventMinimumElevation
+  [MutationNames.SetCollectionEventMinimumElevation]: setCollectionEventMinimumElevation,
+  [MutationNames.SetCollectionObjectBufferedDeterminations]: setCollectionObjectBufferedDeterminations,
+  [MutationNames.SetCollectionObjectBufferedOtherLabel]: setCollectionObjectBufferedOtherLabel,
+  [MutationNames.SetCollectionObjectCollectionEventAttributes]: setCollectionObjectCollectionEventAttributes,
+  [MutationNames.SetCollectionObjectConteiner]: setCollectionObjectConteiner,
+  [MutationNames.SetCollectionObjectDeaccessionedAt]: setCollectionObjectDeaccessionedAt,
+  [MutationNames.SetCollectionObjectDeaccessionReason]: setCollectionObjectDeaccessionReason,
+  [MutationNames.SetCollectionObjectEventId]: setCollectionObjectEventId,
+  [MutationNames.SetCollectionObjectPreparationId]: setCollectionObjectPreparationId,
+  [MutationNames.SetCollectionObjectRangeLotId]: setCollectionObjectRangeLotId,
+  [MutationNames.SetCollectionObjectRepositoryId]: setCollectionObjectRepositoryId,
+  [MutationNames.SetCollectionObjectTotal]: setCollectionObjectTotal
 }
 
 export {
