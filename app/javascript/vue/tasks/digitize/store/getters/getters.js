d29efb7a8f7cbcdce868679bee6374682417072b
José Luis Pereira <pepelui@outlook.com>
2018-06-29 16:22:13 -0300

    Added getters

--- app/javascript/vue/tasks/digitize/store/getters/getters.js
+++ app/javascript/vue/tasks/digitize/store/getters/getters.js
@@ -1,14 +1,20 @@
 import getCollectionEvent from './getCollectionEvent'
 import getCollectionEventLabel from './getCollectionEventLabel'
+import getTypeMaterial from './getTypeMaterial'
+import getDepictions from './getDepictions'
 
 const GetterNames = {
   GetCollectionEvent: 'getCollectionEvent',
   GetCollectionEventLabel: 'getCollectionEventLabel',
+  GetTypeMaterial: 'getTypeMaterial',
+  GetDepictions: 'getDepictions'
 }
 
 const GetterFunctions = {
   [GetterNames.GetCollectionEventLabel]: getCollectionEventLabel,
-  [GetterNames.GetCollectionEvent]: getCollectionEvent
+  [GetterNames.GetCollectionEvent]: getCollectionEvent,
+  [GetterNames.GetTypeMaterial]: getTypeMaterial,
+  [GetterNames.GetDepictions]: getDepictions
 }
 
 export {
