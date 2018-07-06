644957066371de697827dfb458250f02a20bc353
Jim Tucker <tuckerjd@uiuc.edu>
2018-07-06 11:25:10 -0500

    #428 Order @people by last name.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -17,6 +17,7 @@ class PeopleController < ApplicationController
         first_name = params[:firstname]
         # @people = Person.where(last_name: last_name, first_name: first_name).with_role(params[:roles])
         @people = Queries::Person::Filter.new(params).partial_complete #  partial_complete => broadest selection of names
+        @people = Person.where(id: @people.map(&:id)).order(:last_name)
         # stuff =[]
         # @people.each {|person|
         #   one = person.attributes
