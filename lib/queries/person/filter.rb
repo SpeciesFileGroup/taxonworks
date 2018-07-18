0add657744f3e292f1bfad3bc243f1ed48314a8b
Jim Tucker <tuckerjd@uiuc.edu>
2018-07-18 08:44:22 -0500

    #428 Refinements to Queries::Person::Filter#partial_complete, and tests.

--- lib/queries/person/filter.rb
+++ lib/queries/person/filter.rb
@@ -123,16 +123,16 @@ module Queries
       def first_and_last_wild_match
         last_name  = options[:last_name]
         first_name = options[:first_name]
-        # last_name  = last_name.nil? ? '' : last_name.gsub('*', '%')
-        # first_name = first_name.nil? ? '' : first_name.gsub('*', '%')
+        last_name  = last_name.nil? ? '' : last_name.gsub('*', '%')
+        first_name = first_name.nil? ? '' : first_name.gsub('*', '%')
         if first_name.blank?
-          return base_query.where(people_table[:last_name].matches(last_name.gsub('*', '%')))
+          return base_query.where(people_table[:last_name].matches(last_name))
         end
         if last_name.blank?
-          return base_query.where(people_table[:first_name].matches(first_name.gsub('*', '%')))
+          return base_query.where(people_table[:first_name].matches(first_name))
         end
-        base_query.where(people_table[:first_name].matches(first_name.gsub('*', '%'))
-                           .and(people_table[:last_name].matches(last_name.gsub('*', '%'))))
+        base_query.where(people_table[:first_name].matches(first_name)
+                           .and(people_table[:last_name].matches(last_name)))
       end
 
       def star_like(term)
