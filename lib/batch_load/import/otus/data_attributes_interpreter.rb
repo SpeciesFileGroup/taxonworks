3d5f16289ac1c1579ac813ac310607bd9e88428f
Jim Tucker <tuckerjd@uiuc.edu>
2018-08-03 11:15:59 -0500

    #474 Auto-generated data_attributes batch loader for Otus. Added files.

---
+++ lib/batch_load/import/otus/data_attributes_interpreter.rb
@@ -0,0 +1,44 @@
+# TODO: THIS IS A GENERATED STUB, it does not function
+module BatchLoad
+  class Import::Otu::DataAttributesInterpreter < BatchLoad::Import
+
+    def initialize(**args)
+      @otus = {}
+      super(args)
+    end
+
+    # TODO: update this
+    def build_otus
+      @total_data_lines = 0
+      i = 0
+
+      # loop throw rows
+      csv.each do |row|
+        i += 1
+
+        parse_result = BatchLoad::RowParse.new
+        parse_result.objects[:otu] = []
+
+        @processed_rows[i] = parse_result
+
+        begin # processing
+          # use a BatchLoad::ColumnResolver or other method to match row data to TW 
+          #  ...
+
+          @total_data_lines += 1
+        rescue
+           # ....
+        end
+      end
+
+      @total_lines = i
+    end
+
+    def build
+      if valid?
+        build_otus
+        @processed = true
+      end
+    end
+  end
+end
