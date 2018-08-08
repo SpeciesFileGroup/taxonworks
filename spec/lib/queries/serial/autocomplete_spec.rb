4026b66f80232279a3029da220f43a72174f2ab5
mjy <diapriid@gmail.com>
2018-08-08 16:42:45 -0500

    close #454
    requires some optimization ultimately

--- spec/lib/queries/serial/autocomplete_spec.rb
+++ spec/lib/queries/serial/autocomplete_spec.rb
@@ -7,162 +7,54 @@ describe Queries::Source::Autocomplete, type: :model do
   let(:p2) { FactoryBot.create(:valid_person, last_name: 'Jones') }
   let(:p3) { FactoryBot.create(:valid_person, last_name: 'Brandt', prefix: 'von') }
 
-  let(:j1) { FactoryBot.create(:valid_serial, name: 'Journal of Stuff and Things') }
-  let!(:a1) { AlternateValue::Abbreviation.create!(value: 'J. S. and T.', alternate_value_object: j1, alternate_value_object_attribute: :name) }
+  let!(:serial1) { FactoryBot.create(:valid_serial, name: 'Journal of Stuff and Things') }
+  let!(:serial2) { FactoryBot.create(:valid_serial, name: 'Journal of Applied Gadgets and Things') }
 
-  let!(:a2) { AlternateValue::Translation.create!(value: 'Diario de cosas y cosas', language: FactoryBot.create(:valid_language), alternate_value_object: j1, alternate_value_object_attribute: :name) }
+  let!(:a1) { AlternateValue::Abbreviation.create!(value: 'J. S. and T.', alternate_value_object: serial1, alternate_value_object_attribute: :name) }
+  let!(:a2) { AlternateValue::Translation.create!(value: 'Diario de cosas y cosas', language: FactoryBot.create(:valid_language), alternate_value_object: serial1, alternate_value_object_attribute: :name) }
+  let!(:a3) { AlternateValue::AlternateSpelling.create!(value: 'Journal of Stuph and Thingz', alternate_value_object: serial1, alternate_value_object_attribute: :name) }
 
+  let(:query) { Queries::Serial::Autocomplete.new('') }
 
-  
-  let!(:s2) { Source::Bibtex.create!(
-    bibtex_type: :article,
-    authors: [p1], 
-    year: 1920, 
-    title: 'Bad cops, good cops, rubocops.'
-  )}
-
-  let!(:s3) { Source::Bibtex.create!(
-    bibtex_type: :article,
-    author: 'Smith', 
-    year: 1921,
-    year_suffix: 'a',
-    title: 'Bad cops, good caps, rubycoats.'
-  )}
-
-  let!(:s4) { Source::Bibtex.create!(
-    bibtex_type: :article,
-    authors: [p2, p3, p1], 
-    year: 1924, 
-    title: 'We all got together and wrote about Things!'
-  )}
-
-  let!(:s5) { Source::Bibtex.create!(
-    bibtex_type: :article,
-    author: 'von Brandt & Bartholemew', 
-    year: 1940, 
-    title: 'Creatures from the Black Lagoon.',
-    serial: j1
-  )}
-
-  let!(:s6) { Source::Verbatim.create!(verbatim: 'foo' )  }
-
-  # Sources in prquoject 
-  # Sources not in project
-  
-  let(:query) { Queries::Source::Autocomplete.new('') }
-
-  specify '#autocomplete_exact_author 1' do
-    query.terms = 'Smith' 
-    expect(query.autocomplete_exact_author.map(&:id)).to contain_exactly(s2.id, s3.id)
+  specify '#autocomplete_exact_name' do
+    query.terms = 'Journal of Stuff and Things' 
+    expect(query.autocomplete_exact_name.map(&:id)).to contain_exactly(serial1.id)
   end
 
-  specify '#autocomplete_any_author 1' do
-    query.terms = 'Smith' 
-    expect(query.autocomplete_any_author.map(&:id)).to contain_exactly(s2.id, s3.id, s4.id)
+  specify '#autocomplete_begining_name' do
+    query.terms = 'Journal' 
+    expect(query.autocomplete_begining_name.map(&:id)).to contain_exactly(serial1.id, serial2.id)
   end
 
-  specify '#autocomplete_any_author 2' do
-    query.terms = 'Smit' 
-    expect(query.autocomplete_any_author.map(&:id)).to contain_exactly()
+  specify '#autocomplete_wildcard_name' do
+    query.terms = 'of and' 
+    expect(query.autocomplete_ordered_wildcard_pieces_in_name.map(&:id)).to contain_exactly(serial1.id, serial2.id)
   end
 
-  specify '#autocomplete_partial_author 1' do
-    query.terms = 'Smit' 
-    expect(query.autocomplete_partial_author.map(&:id)).to contain_exactly(s2.id, s3.id, s4.id)
+  specify '#autocomplete_exact_alternate_value(,"AlternateValue::AlternateSpelling")' do
+    query.terms = 'Journal of Stuph and Thingz' 
+    expect(query.autocomplete_exact_alternate_value('name', 'AlternateValue::AlternateSpelling').map(&:id)).to contain_exactly(serial1.id)
   end
 
-  specify '#autocomplete_year 1' do
-    query.terms = 'Smith, 1940, Bogus 1924a'
-    expect(query.autocomplete_year.map(&:id)).to contain_exactly(s4.id, s5.id)
+  specify '#autocomplete_begining_alternate_value' do
+    query.terms = 'Journal of Stup' 
+    expect(query.autocomplete_begining_alternate_value('name', 'AlternateValue::AlternateSpelling').map(&:id)).to contain_exactly(serial1.id)
   end
 
-  specify '#autocomplete_start_of_title 1' do
-    query.terms = 'Bad cops'
-    expect(query.autocomplete_start_of_title.map(&:id)).to contain_exactly(s2.id, s3.id)
+  specify '#autocomplete_ordered_wildcard_alternate_value' do
+    query.terms = 'Stup ngz' 
+    expect(query.autocomplete_ordered_wildcard_alternate_value('name', 'AlternateValue::AlternateSpelling').map(&:id)).to contain_exactly(serial1.id)
   end
 
-  specify '#autocomplete_ordered_wildcard_pieces_in_cached 1' do
-    query.terms = 'Bad rubyco'
-    expect(query.autocomplete_ordered_wildcard_pieces_in_cached.map(&:id)).to contain_exactly(s3.id)
+  specify '#autocomplete_exact_alternate_value(,"AlternateValue::Translation")' do
+    query.terms = 'Diario de cosas y cosas' 
+    expect(query.autocomplete_exact_alternate_value('name', 'AlternateValue::Translation').map(&:id)).to contain_exactly(serial1.id)
   end
 
-  specify '#autocomplete_ordered_wildcard_pieces_in_cached 2' do
-    query.terms = 'Smi rubyco'
-    expect(query.autocomplete_ordered_wildcard_pieces_in_cached.map(&:id)).to contain_exactly(s3.id)
+  specify '#autocomplete_exact_alternate_value(,"AlternateValue::Abbreviation")' do
+    query.terms = 'J. S. and T.' 
+    expect(query.autocomplete_exact_alternate_value('name', 'AlternateValue::Abbreviation').map(&:id)).to contain_exactly(serial1.id)
   end
-
-  specify '#autocomplete_ordered_wildcard_pieces_in_cached 3' do
-    query.terms = 'Smi 1921 rubyco'
-    expect(query.autocomplete_ordered_wildcard_pieces_in_cached.map(&:id)).to contain_exactly(s3.id)
-  end
-
-  specify '#autocomplete_year_letter 1' do
-    query.terms = '1921a'
-    expect(query.autocomplete_year_letter.map(&:id)).to contain_exactly(s3.id)
-  end
-
-  specify '#autocomplete_year_letter 2' do
-    query.terms = '1921'
-    expect(query.autocomplete_year_letter.map(&:id)).to contain_exactly()
-  end
-
-  specify '#autocomplete_exact_author_year_letter 1' do
-    query.terms = 'Smith 1921a'
-    expect(query.autocomplete_exact_author_year_letter.map(&:id).first).to eq(s3.id)
-  end
-
-  specify '#autocomplete_exact_author_year 1' do
-    query.terms = 'Smith 1921a'
-    expect(query.autocomplete_exact_author_year.map(&:id).first).to eq(s3.id)
-  end
-
-  specify '#autocomplete_wildcard_pieces_and_year' do
-    # !! cached currently renders von on the outside, so `von Brandt` won't match
-    query.terms = 'Jones, Brandt & Smith, 1924'
-    expect(query.autocomplete_wildcard_pieces_and_year.map(&:id).first).to eq(s4.id)
-  end
-
-  specify 'autocomplete_wildcard_author_exact_year' do
-    query.terms = 'ones andt 1924'
-    expect(query.autocomplete_wildcard_author_exact_year.map(&:id).first).to eq(s4.id)
-  end
-
-  specify 'autocomplete_cached_wildcard_anywhere 1' do
-    query.terms = 'andt Things about'
-    expect(query.autocomplete_cached_wildcard_anywhere.map(&:id).first).to eq(s4.id)
-  end
-
-  specify 'autocomplete_wildcard_anywhere_exact_year 1' do
-    query.terms = '1940 Lagoon'
-    expect(query.autocomplete_wildcard_anywhere_exact_year.map(&:id).first).to eq(s5.id)
-  end
-
-  specify 'autocomplete_wildcard_anywhere_exact_year 2' do
-    query.terms = 'Lagoon 1940'
-    expect(query.autocomplete_wildcard_anywhere_exact_year.map(&:id).first).to eq(s5.id)
-  end
-
-  specify 'autocomplete_wildcard_anywhere_exact_year 3' do
-    query.terms = 'Lagoon 1940 Black'
-    expect(query.autocomplete_wildcard_anywhere_exact_year.map(&:id).first).to eq(s5.id)
-  end
-
-  specify 'autocomplete_cached_wildcard_anywhere' do
-    query.terms = 'black brandt'
-    expect(query.autocomplete_cached_wildcard_anywhere.map(&:id).first).to eq(s5.id)
-  end
-
-  specify '#autocomplete 1' do
-    query.terms = 'Smith 1921a'
-    expect(query.autocomplete.map(&:id).first).to eq(s3.id)
-  end
-
-  specify '#autocomplete 2' do
-    query.terms = 'Smith 1921z'
-    expect(query.autocomplete.map(&:id).first).to eq(s3.id)
-  end
-
-
 
 
 end
