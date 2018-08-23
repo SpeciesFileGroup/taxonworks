dd230635af81b7eb6f64d3034152183ae13a1b66
jrflood <jrflood@illinois.edu>
2018-08-23 18:34:29 -0500

    #468 solved request params for app.vue issue. source_id param extracted

--- app/javascript/vue/tasks/nomenclature/by_source/components/nomen_source.vue
+++ app/javascript/vue/tasks/nomenclature/by_source/components/nomen_source.vue
@@ -10,12 +10,24 @@
     props: {
       value: {
         type: String,
-        default: window.location.href
+        default: window.location.href.split('by_source/')[1]
       },
       sourceText: {
         type: String,
         default: 'This is the source from which we derive all the taxa and otus'
       },
+    },
+    methods: {
+      items() {
+        alert(window.location.search);
+        return window.location.href.split('by_source/')[-1];
+      }
+    },
+    computed: {
+      itemS() {
+        alert(window.location.search);
+        return window.location.href.split('by_source/')[-1];
+      }
     }
   }
 </script>
