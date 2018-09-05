489832a9513805864bebdafb011b93715ff28c54
jrflood <jrflood@illinois.edu>
2018-09-05 17:48:26 -0500

    #468 otus_match_proxy.vue stubs for cites as aliases for Otus, methods  working, but with reactivity issues. Repainting version of code pushed.  Source search autocomplete working.

--- app/javascript/vue/tasks/nomenclature/by_source/components/nomen_source.vue
+++ app/javascript/vue/tasks/nomenclature/by_source/components/nomen_source.vue
@@ -3,10 +3,20 @@
       style="width:400px; height:200px;">
     <h2>Nomenclatural source: </h2>
     <pre>{{ sourceText }}</pre>
+    <autocomplete
+      url="/sources/autocomplete"
+      min="2"
+      param="term"
+      label="label"
+      placeholder="search for a Source"
+      @getItem="sourceID = $event.id; getSource()"
+    />
   </div>
 </template>
 <script>
+  import Autocomplete from "../../../../components/autocomplete";
   export default {
+    components: {Autocomplete},
     props: {
       value: {
         type: String,
@@ -21,15 +31,22 @@
       }
     },
   methods: {
-    getSource() {
-      let pieces = window.location.href.split('/')
+    getRequestSource() {
+      let pieces = window.location.href.split('/');
       this.sourceID = pieces[pieces.length - 1];
+      getSource();
+    },
+    getSource() {
       if (this.sourceID) {
         this.$http.get('/sources/' + this.sourceID + '.json').then(response => {
           this.sourceText = response.body.id + ': "' + response.body.cached + '"';
           this.$emit('sourceID', this.sourceID);
         })
       }
+    },
+    getNewSource(event) {
+      this.sourceID = event.id;
+      getSource();
     },
     getSelectOptions(onModel) {
       this.$http.get(this.selectOptionsUrl, {params: {klass: this.onModel}}).then(response => {
@@ -46,7 +63,9 @@
   }
   ,
   mounted: function () {
-    this.getSource()
+    let pieces = window.location.href.split('/');
+    this.sourceID = pieces[pieces.length - 1];
+    this.getSource();
     }
   }
 </script>
