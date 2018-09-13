af46205265d78ccf75f67e2362440a6d834857ee
mjy <diapriid@gmail.com>
2018-09-13 16:37:04 -0500

    Expose some annotation endpoints; biological associations API tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -1035,6 +1035,18 @@ TaxonWorks::Application.routes.draw do
       get '/character_states/:id/annotations',
         to: 'character_states#annotations'
 
+      # TODO: DRY
+      # Generate shallow routes for annotations based on model properties, like
+      # otu_citations GET /otus/:otu_id/citations(.:format) citations#index
+      ApplicationEnumeration.data_models.each do |m|
+        Shared::IsData::Annotation::ANNOTATION_TYPES.each do |t|
+          if m.send("has_#{t}?")
+            n = m.model_name
+            match "/#{n.route_key}/:#{n.param_key}_id/#{t}", to: "#{t}#index",  via: :get, constraints: {format: :json}, defaults: {format: :json}
+          end
+        end
+      end
+
     end
   end
 
