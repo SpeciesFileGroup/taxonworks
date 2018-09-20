d619793c80454a124d840f59c4c13b2b5cd510fa
mjy <diapriid@gmail.com>
2018-09-20 15:13:27 -0500

    confidences JSON; abstract out polymorphic params for filters

--- lib/queries/confidence/filter.rb
+++ lib/queries/confidence/filter.rb
@@ -3,12 +3,8 @@ module Queries
 
     # !! does not inherit from base query
     class Filter 
-
-      # General annotator options handling 
-      # happens directly on the params as passed
-      # through to the controller, keep them
-      # together here
-      attr_accessor :options
+      include Concerns::Polymorphic
+      polymorphic_klass(::Confidence)
 
       # Params specific to Confidence 
 
@@ -20,18 +16,20 @@ module Queries
         @confidence_level_id = [params[:confidence_level_id]].flatten.compact
         @object_global_id = params[:object_global_id]
         @confidence_object_type = params[:confidence_object_type]
-        @options = params
+        set_polymorphic_ids(params)
       end
 
       # @return [ActiveRecord::Relation]
       def and_clauses
         clauses = [
-          Queries::Annotator.annotator_params(options, ::Confidence),
           matching_confidence_level_id,
           matching_confidence_object_type,
           matching_object,
+          matching_polymorphic_ids
         ].compact
 
+        return nil if clauses.empty? 
+        
         a = clauses.shift
         clauses.each do |b|
           a = a.and(b)
@@ -76,7 +74,7 @@ module Queries
         if a = and_clauses
           ::Confidence.where(and_clauses)
         else
-          ::Confidence.none
+          ::Confidence.all
         end
       end
 
