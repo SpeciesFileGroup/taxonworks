55e20996d9542c5466f31c6777208c67637d1724
José Luis Pereira <pepelui@outlook.com>
2018-10-04 01:42:18 -0300

    Fix template render issue for taxon name row #515

--- app/javascript/vue/tasks/nomenclature/by_source/components/tables/taxon_names_row.vue
+++ app/javascript/vue/tasks/nomenclature/by_source/components/tables/taxon_names_row.vue
@@ -2,23 +2,33 @@
   <tr>
     <td>
       <input
-          class="pages"
-          type="text"
-          placeholder="Pages"
-          @input="changePage"
-          v-model="citation.pages">
+        class="pages"
+        type="text"
+        placeholder="Pages"
+        @input="changePage"
+        v-model="citation.pages">
     </td>
-    <td><span><a v-html="citation.citation_object.object_tag" @click="showObject()"/> {{ legend }} </span>
+    <td>
+      <span>
+        <a
+          v-html="citation.citation_object.object_tag"
+          @click="showObject()"/> 
+        {{ legend }}
+      </span>
     </td>
     <td>
       <radial-annotator :global-id="citation.citation_object.global_id"/>
     </td>
     <td>
       <otu-radial
-          :taxon-id="citation.citation_object_id"
-          :redirect="false"/>
+        :taxon-id="citation.citation_object_id"
+        :redirect="false"/>
     </td>
-    <td><span class="button circle-button btn-delete" @click="removeMe()"/></td>
+    <td>
+      <span
+        class="button circle-button btn-delete"
+        @click="removeMe()"/>
+    </td>
   </tr>
 </template>
 
@@ -40,38 +50,33 @@
     },
     data() {
       return {
-        // legend: 'invalid',
         pages: undefined,
         autoSave: undefined,
-        time: 3000
+        time: 3000,
+        legend: '' // <---- Making a data property will make reactive to the template. So when this value changes the template will be rendered again
       }
     },
-    computed: {
-      legend() {
-        return this.getNameData()
-      }
+    mounted() {
+      //Rich, this will make the call to retrieve the information when the component is mounted
+      this.getNameData()
     },
     methods: {
       showObject() {
-        // window.open(this.citation.citation_object.object_url, '_blank');
         window.open('/tasks/nomenclature/browse/' + this.citation.citation_object_id, '_blank');
       },
       getNameData() {
-        return new Promise((resolve) => {
-          let legend = 'invalid';
-          let id = this.citation.citation_object_id;
-          this.$http.get('/taxon_names/' + id + '.json').then(response => {
-            let taxon = response.body;
-            legend = ' ' + taxon.rank;
-            let invalid = (taxon.id != taxon.cached_valid_taxon_name_id);
-            if (invalid && (taxon.type == 'Combination')) {
-              legend = ' combination';
-            }
-            let authorYear = (taxon.cached_author_year == null) ? ' ()' : ' ' + taxon.cached_author_year;
-            legend = authorYear + legend;
-            // this.legend = legend;
-            return resolve(response.body);
-          });
+        let legend = 'invalid';
+        // Removed the line to retrieve the id. The object already have object_url.
+        this.$http.get(this.citation.citation_object.object_url).then(response => {
+          let taxon = response.body
+          legend = ' ' + taxon.rank // This will override 'invalid' always
+          let invalid = (taxon.id != taxon.cached_valid_taxon_name_id)
+          if (invalid && (taxon.type == 'Combination')) {
+            legend = ' combination' // Looking this line and the space at the start, the rank is not missing here? not sure if that was the idea.
+          }
+          let authorYear = (taxon.cached_author_year == null) ? ' ' : ' ' + taxon.cached_author_year
+          legend = authorYear + legend
+          this.legend = legend
         })
       },
       changePage() {
