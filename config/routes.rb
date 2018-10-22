9e235a7377a2d41677e4696b6dfc2425e7e0dd85
mjy <diapriid@gmail.com>
2018-10-21 20:08:50 -0500

    ANNOTATION_TYPES

--- config/routes.rb
+++ config/routes.rb
@@ -621,14 +621,13 @@ TaxonWorks::Application.routes.draw do
   # Generate shallow routes for annotations based on model properties, like
   # otu_citations GET /otus/:otu_id/citations(.:format) citations#index
   ApplicationEnumeration.data_models.each do |m|
-    Shared::IsData::Annotation::ANNOTATION_TYPES.each do |t|
+    ::ANNOTATION_TYPES.each do |t|
       if m.send("has_#{t}?")
         n = m.model_name
         match "/#{n.route_key}/:#{n.param_key}_id/#{t}", to: "#{t}#index", as: "#{n.singular}_#{t}", via: :get, constraints: {format: :json}, defaults: {format: :json}
       end
     end
   end
-
 
   ### End of data resources ###
 
@@ -1050,7 +1049,7 @@ TaxonWorks::Application.routes.draw do
       # Generate shallow routes for annotations based on model properties, like
       # otu_citations GET /otus/:otu_id/citations(.:format) citations#index
       ApplicationEnumeration.data_models.each do |m|
-        Shared::IsData::Annotation::ANNOTATION_TYPES.each do |t|
+        ::ANNOTATION_TYPES.each do |t|
           if m.send("has_#{t}?")
             n = m.model_name
             match "/#{n.route_key}/:#{n.param_key}_id/#{t}", to: "#{t}#index",  via: :get, constraints: {format: :json}, defaults: {format: :json}
