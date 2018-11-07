e8593144806b50de51c72164d97f7c619499fa10
jrflood <jrflood@illinois.edu>
2018-11-07 12:20:07 -0600

    #557 Post task generator boiler plate, Vue bits

---
+++ app/javascript/vue/tasks/collecting_events/app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Search and list localities</h1>
+  </div>
+</template>
+<script>
+  export default {
+  data(){
+    return {
+      letter: ''
+      }
+    }
+  }
+</script>
---
+++ app/javascript/vue/tasks/collecting_events/main.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import vueResource from 'vue-resource'
+
+var TW = TW || {};
+TW.views = TW.views || {};
+TW.views.tasks = TW.views.tasks || {};
+TW.views.tasks.collecting_events = TW.views.tasks.collecting_events || {};
+TW.views.tasks.collecting_events.search_locality = TW.views.tasks.collecting_events.search_locality || {};
+
+Object.assign(TW.views.tasks.collecting_events.search_locality, {
+
+  init: function () {
+    Vue.use(vueResource);
+
+    //var store = require('./store/store.js').newStore()
+    var App = require('./app.vue').default;
+    var token = $('[name="csrf-token"]').attr('content');
+    Vue.http.headers.common['X-CSRF-Token'] = token;
+
+    new Vue({
+      //store,
+      el: '#search_locality',
+      render: function (createElement) {
+        return createElement(App);
+      }
+    })
+  }
+});
+
+$(document).on('turbolinks:load', function () {
+  if ($('#search_locality').length) {
+    TW.views.tasks.collecting_events.search_locality.init();
+  }
+});
