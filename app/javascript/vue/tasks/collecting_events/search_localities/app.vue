ea589be02c36c1ef802f0297aa38dd1131ecdd78
Jim Tucker <tuckerjd@uiuc.edu>
2018-11-08 09:04:59 -0600

    #557 Working with VUE.

--- app/javascript/vue/tasks/collecting_events/search_localities/app.vue
+++ app/javascript/vue/tasks/collecting_events/search_localities/app.vue
@@ -1,13 +1,48 @@
 <template>
   <div>
+    <spinner
+      v-if="isLoading"
+      :full-screen="true"
+      legend="Loading..."
+      :logo-size="{ width: '100px', height: '100px'}"/>
     <h1>Search and List Localities</h1>
+    <alphabet-buttons
+      @keypress="getLocalities($event)"
+      ref="alphabetButtons"/>
   </div>
 </template>
 <script>
+  import AlphabetButtons from './components/alphabet_buttons'
+  import Spinner from 'components/spinner.vue'
+
   export default {
-  data(){
-    return {
-      letter: ''
+    components: {
+      AlphabetButtons,
+      Spinner
+    },
+    data() {
+      return {
+        isLoasing: false,
+        letter: '',
+        localityList: []
+      }
+    },
+    mounted() {
+      let urlParams = new URLSearchParams(window.location.search)
+      let letterParam = urlParams.get('letter').toUpperCase()
+
+      if (/([A-Z])$/.test(letterParam) && letterParam.length == 1) {
+        this.getLocalities(letterParam)
+        this.$refs.alphabetButtons.setSelectedLetter(letterParam)
+      }
+    },
+    methods: {
+      getLocalities(letter) {
+        this.isLoading = true
+        this.$http.get(`/collecting_events.json?localities_start_with=${letter}&shape=${dontKnowYet}&area_id=${dontKnowYet}`).then(response => {
+          this.localityList = response.body;
+          this.isLoading = false
+        })
       }
     }
   }
