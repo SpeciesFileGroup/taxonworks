c0fed64d258c705a305cae61330a32eef2ff5f33
Jim Tucker <tuckerjd@uiuc.edu>
2018-11-09 09:13:43 -0600

    #557 Working with VUE: Now we have an more correct autocomplete for  collecting events (sort of)..

--- app/javascript/vue/tasks/collecting_events/search_localities/app.vue
+++ app/javascript/vue/tasks/collecting_events/search_localities/app.vue
@@ -36,39 +36,15 @@
       return {
         isLoading: false,
         letter: '',
-        dontKnowYet: '',
         selected: undefined,
         localityList: []
       }
     },
     mounted() {
-      let urlParams = new URLSearchParams(window.location.search)
-      let letterParam = urlParams.get('letter').toUpperCase()
-
-      // if (/([A-Z])$/.test(letterParam) && letterParam.length == 1) {
-      //   this.getLocalities(letterParam)
-      //   this.$refs.alphabetButtons.setSelectedLetter(letterParam)
-      // }
-      this.getList(`/geographic_areas/select_options?target=CollectingEvent`).then(response => {
-        let result = response.body
-        Object.keys(result).forEach(key => (!result[key].length) && delete result[key]
-    )
-      this.smartGeographics = result
-      this.view = this.firstTabWithData(result);
-    })
     },
     methods: {
       sendCollectingEvent(item) {
         this.selected = item
-        // this.selected = ''
-        // this.$emit('select', item.id)
-      },
-      getLocalities(letter, dontKnowYet) {
-        this.isLoading = true
-        this.$http.get(`/collecting_events.json?localities_start_with=${letter}&shape=${dontKnowYet}&area_id=${dontKnowYet}`).then(response => {
-          this.localityList = response.body;
-        this.isLoading = false
-      })
       }
     }
   }
