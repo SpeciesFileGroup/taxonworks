4a3aa35e745044c1730b3961e1634bd125501930
jrflood <jrflood@illinois.edu>
2018-11-27 14:34:07 -0600

    #643 Port functionality of /tasks/gis/match_georeference#filter dates section

--- app/javascript/vue/tasks/collecting_events/search_localities/components/ce_filter.vue
+++ app/javascript/vue/tasks/collecting_events/search_localities/components/ce_filter.vue
@@ -6,14 +6,43 @@
         <td>Originating<br>between</td><td>Day</td><td>Month</td><td>Year</td><td>DatePicker</td>
       </tr>
       <tr>
-        <td></td><td><input type="text"></td><td><select></select></td><td><input type="text"><td><input type="date"></td>
+        <td></td>
+        <td>
+          <input type=text id="start_date_day" name="start_date_day" size="2" maxlength="2" />
+        </td>
+        <td>
+          <month-select name="start_date_month"/>
+        </td>
+        <td>
+          <input type=text id="start_date_year" name="start_date_year" size="4" maxlength="4" />
+        </td>
+        <td><input type="date"></td>
       </tr>
       <tr>
         <td>And</td><td>Day</td><td>Month</td><td>Year</td><td>DatePicker</td>
       </tr>
       <tr>
-        <td></td><td><input type="text"></td><td><select></select></td><td><input type="text"><td><input type="date"></td>
+        <td></td>
+        <td>
+          <input type=text name="end_date_day" id="end_date_day" size="2" maxlength="2"/>
+        </td>
+        <td>
+          <month-select name="end_date_month"/>
+        </td>
+        <td>
+          <input type=text name="end_date_year" id="end_date_year" size="4" maxlength="4"/>
+        </td>
+        <td><input type="date"></td>
       </tr>
     </table>
   </div>
 </template>
+<script>
+  import MonthSelect from './month_select'
+
+  export default {
+    components: {
+      MonthSelect
+    },
+  }
+</script>
---
+++ app/javascript/vue/tasks/collecting_events/search_localities/components/month_select.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <select name="name" v-model="selected">
+      <option value="" label=" "></option>
+      <option value="1">January</option>
+      <option value="2">February</option>
+      <option value="3">March</option>
+      <option value="4">April</option>
+      <option value="5">May</option>
+      <option value="6">June</option>
+      <option value="7">July</option>
+      <option value="8">August</option>
+      <option value="9">September</option>
+      <option value="10">October</option>
+      <option value="11">November</option>
+      <option value="12">December</option>
+    </select>
+    {{selected}}
+  </div>
+</template>
+<script>
+  export default {
+    props: {
+      name: {
+        type: String,
+        default: ''
+      }
+    },
+    data() {
+      return {
+        selected: ''
+      }
+    }
+  }
+</script>
