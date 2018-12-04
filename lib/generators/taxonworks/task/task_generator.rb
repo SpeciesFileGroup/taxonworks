4a6b38db41648350dbeac229d852e7dabff06410
Dmitry Dmitriev <arboridia@gmail.com>
2018-12-03 18:22:06 -0600

    source_suffix validate

--- lib/generators/taxonworks/task/task_generator.rb
+++ lib/generators/taxonworks/task/task_generator.rb
@@ -6,7 +6,10 @@ class Taxonworks::TaskGenerator < Rails::Generators::Base
 
   argument :controller_base_name, type: 'string', required: true, banner: '<controller_base_name>'
   argument :path_to_controller, type: 'string', required: true, banner: '<"path/to/controller_folder/">'
-  argument :methods_actions_names, type: 'array', required: true, banner: '<method_name:action:route_name>'
+  argument :methods_actions_names, type: 'array', required: false, banner: '<method_name:action:route_name>'
+  class_option :vue, type: :boolean, required: false, default: false
+
+  # This is a Thor task, all methods are invoked in order 
 
   def check_args
     error_str = ''
@@ -20,30 +23,36 @@ class Taxonworks::TaskGenerator < Rails::Generators::Base
     end
 
     if error_str.length > 0
-      puts error_str
+      puts Rainbow(error_str).red
+      abort
+    end
+
+    if options[:vue] && methods_actions_names.present?
+      puts Rainbow('--vue is incompatible with method action names, an index is created by default').red
       abort
     end
   end
 
   def process_args
+
     @paths = path_to_controller.split('/')
-    @route_methods = Array.new
-    @route_actions = Array.new
-    @route_names = Array.new
+    @route_methods = [] 
+    @route_actions = [] 
+    @route_names = [] 
 
-    methods_actions_names.each do |str|
-      split_str = str.split(':')
-      method    = split_str[0]
-      action    = split_str[1]
-      name      = split_str[2] || "#{method}_#{controller_base_name}"
+    if methods_actions_names.present?
+      methods_actions_names.each do |str|
+        split_str = str.split(':')
+        method = split_str[0]
+        action = split_str[1]
+        name = split_str[2] || "#{method}_#{controller_base_name}"
 
-      if split_str[0].blank? or split_str[1].blank?
-        abort(message = "ERROR: malformed method action name\n USAGE: method_name:action_verb:route_name\n")
+        abort(message = "ERROR: malformed method action name\n USAGE: method_name:action_verb:route_name\n") if bad_action_name?(split_str)
+
+        @route_methods.push(method)
+        @route_actions.push(action)
+        @route_names.push(name + '_task')
       end
-
-      @route_methods.push(method)
-      @route_actions.push(action)
-      @route_names.push(name + '_task')
     end
   end
 
@@ -88,6 +97,8 @@ class Taxonworks::TaskGenerator < Rails::Generators::Base
       route_str += "#{indent_str}  #{action} '#{@route_methods[index]}', as: '#{@route_names[index]}'\n"
     end
 
+    route_str += "#{indent_str}  get :index, as: 'index_#{controller_base_name}_task'\n" if options[:vue]
+
     route_str += "#{indent_str}end\n"
 
     scopes.length.downto(scope_index + 1) do
@@ -99,22 +110,15 @@ class Taxonworks::TaskGenerator < Rails::Generators::Base
     insert_into_file('config/routes.rb', route_str, after: "#{scopes[scope_index - 1]} do\n")
   end
 
+
+
   def add_to_user_task
     user_tasks_str = "\n"
 
     @route_names.each_with_index do |name, index|
       next if @route_actions[index] != 'get'
-
-      user_tasks_str += "#{name}:\n"
-      user_tasks_str += "  hub: true\n"
-      user_tasks_str += "  name: 'TODO: Task name'\n"
-      user_tasks_str += "  related:\n"
-      user_tasks_str += "  categories:\n"
-      user_tasks_str += "  status: prototype\n"
-      user_tasks_str += "  description: 'TODO: Task description'\n"
+      add_user_task(name) 
     end
-
-    append_to_file 'config/interface/hub/user_tasks.yml', user_tasks_str
   end
 
   def create_controller_folders
@@ -147,7 +151,59 @@ class Taxonworks::TaskGenerator < Rails::Generators::Base
     end
   end
 
+  def handle_vue
+    if options[:vue] 
+      create_app
+      create_main
+      create_vue_index
+      register_vue_in_application_js
+      add_user_task("index_#{controller_base_name}_task" )
+    end
+  end
+
+  def instruct_next_steps
+    if options[:vue]
+
+      puts "Next steps:"
+      puts Rainbow('* Restart your rails development server and confirm the task card exists').blue
+      puts Rainbow('* Update the generated code throught changing "vue-task" to something specific').blue
+    end
+
+  end
+
   private
+
+  def add_user_task(name)
+    user_tasks_str = "\n#{name}:\n"
+    user_tasks_str += "  hub: true\n"
+    user_tasks_str += "  name: 'TODO: Task name'\n"
+    user_tasks_str += "  related:\n"
+    user_tasks_str += "  categories:\n"
+    user_tasks_str += "  status: prototype\n"
+    user_tasks_str += "  description: 'TODO: Task description'\n"
+    append_to_file 'config/interface/hub/user_tasks.yml', user_tasks_str
+  end
+
+  def create_app
+    template 'app', "app/javascript/vue/tasks/#{path_to_controller}#{controller_base_name}/app.vue"
+  end
+
+  def create_main
+    template 'main', "app/javascript/vue/tasks/#{path_to_controller}#{controller_base_name}/main.js"
+  end
+
+  def create_vue_index
+    template 'vue_index', "app/views/tasks/#{path_to_controller}#{controller_base_name}/index.html.erb"
+  end
+
+  def register_vue_in_application_js
+    str =  "require('../vue/tasks/#{path_to_controller}#{controller_base_name}/main.js')" 
+    append_to_file 'app/javascript/packs/application.js', str
+  end
+
+  def bad_action_name?(array)
+    array[0].blank? or array[1].blank?
+  end
 
   def controller_class_name
     controller_base_name.titleize.tr(' ', '')
