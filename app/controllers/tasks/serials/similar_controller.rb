6207c8830eee49a6a849d34808a248d1b590e20f
mjy <diapriid@gmail.com>
2018-12-04 20:32:14 -0600

    cleanup similar serials; !! annotations protocol_relationships renamed (no space now)

--- app/controllers/tasks/serials/similar_controller.rb
+++ app/controllers/tasks/serials/similar_controller.rb
@@ -3,11 +3,7 @@ class Tasks::Serials::SimilarController < ApplicationController
 
   # GET tasks/serials/like/:id
   def like
-
     @serial = Serial.find(params[:id])
-
-    @similar_serials = @serial.nearest_by_levenshtein(@serial.name[0..254])
-    @related_routes = UserTasks.related_routes('similar_serials_task')
   end
 
 end
