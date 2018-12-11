d1db6bcfc1331dfbc69115d77dd8740b6214123e
Richard Flood <jrflood@Richards-MacBook-Pro.local>
2018-12-11 14:28:29 -0600

    Change endpoint call

--- app/javascript/vue/tasks/collecting_events/search_localities/app.vue
+++ app/javascript/vue/tasks/collecting_events/search_localities/app.vue
@@ -6,18 +6,6 @@
       legend="Loading..."
       :logo-size="{ width: '100px', height: '100px'}"/>
     <h1>Search and List Localities</h1>
-    <!--<h3>Geographic area</h3>-->
-    <!--<autocomplete-->
-      <!--url="/geographic_areas/autocomplete"-->
-      <!--label="label_html"-->
-      <!--min="2"-->
-      <!--:autofocus="true"-->
-      <!--@getItem="sendGeographic"-->
-      <!--placeholder="Select a geographic area"-->
-      <!--param="term"/>-->
-    <!--<alphabet-buttons-->
-      <!--@keypress="getLocalities($event)"-->
-      <!--ref="alphabetButtons"/>-->
     <collecting-event
       @itemid="selectedItem=$event"
     />
@@ -43,29 +31,13 @@
         localityList: []
       }
     },
-    // mounted() {
-    //   let urlParams = new URLSearchParams(window.location.search)
-    //   let letterParam = urlParams.get('letter').toUpperCase()
-    //
-    //   // if (/([A-Z])$/.test(letterParam) && letterParam.length == 1) {
-    //   //   this.getLocalities(letterParam)
-    //   //   this.$refs.alphabetButtons.setSelectedLetter(letterParam)
-    //   // }
-    //   this.getList(`/geographic_areas/select_options?target=CollectingEvent`).then(response => {
-    //     let result = response.body
-    //     Object.keys(result).forEach(key => (!result[key].length) && delete result[key]
-    // )
-    //   this.smartGeographics = result
-    //   this.view = this.firstTabWithData(result);
-    // })
-    // },
     methods: {
       sendGeographic(item) {
-        this.selected = ''
+        this.selected = '';
         this.$emit('select', item.id)
       },
       getLocalities(letter, dontKnowYet) {
-        this.isLoading = true
+        this.isLoading = true;
         this.$http.get(`/collecting_events.json?localities_start_with=${letter}&shape=${dontKnowYet}&area_id=${dontKnowYet}`).then(response => {
           this.localityList = response.body;
         this.isLoading = false
--- app/javascript/vue/tasks/collecting_events/search_localities/components/ce_tag.vue
+++ app/javascript/vue/tasks/collecting_events/search_localities/components/ce_tag.vue
@@ -66,7 +66,7 @@
           tag_ids.push(tag.id)
         });
         let params = {
-          tag_ids: tag_ids
+          keyword_ids: tag_ids
         };
         this.$http.get('/collecting_events', {params: params}).then(response => {
           this.collectingEventList = response.body.html;
