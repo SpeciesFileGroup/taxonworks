d14c810097e7d5b18aa7b9b03ce1b41beb120b7a
mjy <diapriid@gmail.com>
2019-01-15 09:52:25 -0600

    disambiguate label code

--- app/helpers/labels_helper.rb
+++ app/helpers/labels_helper.rb
@@ -1,16 +1,17 @@
 module LabelsHelper
 
-  def label_tag(label)
+  # !! Note that `label_tag` is a Rails reserved word, so we have to append and make exceptions
+  def taxonworks_label_tag(label)
     return nil if label.nil?
-    content_tag(:span, label.text, class: label.style) # TODO: properly reference style
+    content_tag(:span, label.text) # TODO: properly reference style
   end
 
   def label_link(label)
     return nil if label.nil?
     if label.label_object_id.blank?
-      label_tag(label)
+      taxonworks_label_tag(label)
     else
-      link_to(content_tag(:span, label.text, class: label.style), label.object_global_id) # TODO: properly reference style
+      link_to(content_tag(:span, label.text), label.object_global_id) # TODO: properly reference style
     end
   end
 
