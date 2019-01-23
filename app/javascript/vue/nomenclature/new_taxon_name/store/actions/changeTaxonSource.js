65980a0cb77cec164c4829c0ab8d81afbbe62d02
José Luis Pereira <pepelui@outlook.com>
2019-01-23 20:24:28 -0300

    Improvements in new taxon name

--- app/javascript/vue/nomenclature/new_taxon_name/store/actions/changeTaxonSource.js
+++ app/javascript/vue/nomenclature/new_taxon_name/store/actions/changeTaxonSource.js
@@ -2,7 +2,16 @@ import { changeTaxonSource } from '../../request/resources'
 import { MutationNames } from '../mutations/mutations'
 
 export default function ({ commit, state }, source) {
-  changeTaxonSource(state.taxon_name.id, source, state.taxon_name.origin_citation).then(response => {
-    commit(MutationNames.SetCitation, response)
-  })
+
+
+  state.taxon_name['origin_citation_attributes'] = {
+    id: (state.taxon_name.origin_citation == undefined ? null : state.taxon_name.origin_citation.id),
+    source_id: source.id,
+    is_original: true,
+    pages: (source == undefined ? null : source.pages)
+  }
+
+//  changeTaxonSource(state.taxon_name.id, source, state.taxon_name.origin_citation).then(response => {
+//    commit(MutationNames.SetCitation, response)
+//  })
 }
--- app/javascript/vue/nomenclature/new_taxon_name/store/actions/updateTaxonName.js
+++ app/javascript/vue/nomenclature/new_taxon_name/store/actions/updateTaxonName.js
@@ -4,7 +4,27 @@ import { MutationNames } from '../mutations/mutations'
 export default function ({ commit, state, dispatch }, taxon) {
   commit(MutationNames.SetSaving, true)
   return new Promise(function (resolve, reject) {
-    updateTaxonName(taxon).then(response => {
+
+      var taxon_name = {
+        id: taxon.id,
+        name: taxon.name,
+        parent_id: taxon.parent_id,
+        rank_class: taxon.rank_string,
+        year_of_publication: taxon.year_of_publication,
+        verbatim_author: taxon.verbatim_author,
+        etymology: taxon.etymology,
+        feminine_name: taxon.feminine_name,
+        masculine_name: taxon.masculine_name,
+        neuter_name: taxon.neuter_name,
+        roles_attributes: taxon.roles_attributes,
+        type: 'Protonym'
+    }
+
+    if(taxon.hasOwnProperty('origin_citation_attributes')) {
+      taxon_name.origin_citation_attributes = taxon.origin_citation_attributes
+    }
+
+    updateTaxonName(taxon_name).then(response => {
       if (!response.hasOwnProperty('taxon_name_author_roles')) {
         response['taxon_name_author_roles'] = []
       }
