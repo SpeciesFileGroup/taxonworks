c44dc4ed07832a1d96ab0ef483f61b8014499a85
jrflood <jrflood@illinois.edu>
2019-01-30 17:15:06 -0600

    #643 rearranged to get desired map instantiated.

--- app/javascript/vue/tasks/collecting_events/search_localities/components/leafletMap.vue
+++ app/javascript/vue/tasks/collecting_events/search_localities/components/leafletMap.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :style="{ height: height, width: width }">
+  <div id="map" :style="{ height: height, width: width }">
     <l-map style="height: height, width: width" :zoom="zoom" :center="center">
     <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
     <l-marker :lat-lng="marker"></l-marker>
@@ -8,6 +8,7 @@
 </template>
 
 <script>
+  import L from 'leaflet'
   import { LMap, LTileLayer, LMarker } from 'vue2-leaflet'
   export default {
     components: {
@@ -34,36 +35,44 @@
         required: false,
         default: 0
       },
-      zoom: {
-        type: Number,
-        default: 12
-      },
-      shapes: {
-        type: Object,
-        default: () => { return {} }
-      },
+      // zoom: {
+      //   type: Number,
+      //   default: 12
+      // },
+      // shapes: {
+      //   type: Object,
+      //   default: () => { return {} }
+      // },
     },
     data() {
       return {
         center: L.latLng(-41.2858, 174.78682),
-        url:'http://{www.esri.com/',  // url:'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+        // url:'http://www.esri.com/',  // url:'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+        url:'http://{s}.tile.osm.org/{z}/{x}/{y}.png',  //'http://{www.esri.com/' url:,
         attribution:'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-        marker: L.latLng(0, 0),
+        marker: L.latLng(-41.2858, 174.78682),  //L.latLng(0, 0),
+        map:  undefined,
+        shapes: {},
+        zoom: 12,
       }
     },
-    mounted() {
-      var map = L.map('map').setView([-41.2858, 174.78682], 14);
-      mapLink =
-        '<a href="http://www.esri.com/">Esri</a>';
-      wholink =
-        'i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community';
-      L.tileLayer(
-        'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-          attribution: '&copy; '+mapLink+', '+wholink,
-          maxZoom: 18,
-        }).addTo(map);
-
-    }
+    // mounted() {
+    //   map = L.map("map").setView([37.75, -122.23], 10);
+    //
+    //   L.esri.basemapLayer("Topographic").addTo(map);
+    // }
+    //   var map = L.map('map').setView([-41.2858, 174.78682], 14);
+    //   mapLink =
+    //     '<a href="http://www.esri.com/">Esri</a>';
+    //   wholink =
+    //     'i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community';
+    //   L.tileLayer(
+    //     'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    //       attribution: '&copy; '+mapLink+', '+wholink,
+    //       maxZoom: 18,
+    //     }).addTo(map);
+    //
+    // }
   }
 </script>
 <!--<style lang="sass">-->
