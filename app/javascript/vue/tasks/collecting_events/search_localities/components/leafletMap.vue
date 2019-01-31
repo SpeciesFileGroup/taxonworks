3af1a96d2e4a94cdcb4ad21bc0a4d5dd9bb16fa6
José Luis Pereira <pepelui@outlook.com>
2019-01-30 23:11:52 -0300

    Moved leaflet load to main.js

--- app/javascript/vue/tasks/collecting_events/search_localities/components/leafletMap.vue
+++ app/javascript/vue/tasks/collecting_events/search_localities/components/leafletMap.vue
@@ -8,8 +8,16 @@
 </template>
 
 <script>
-  import L from 'leaflet'
   import { LMap, LTileLayer, LMarker } from 'vue2-leaflet'
+
+  delete L.Icon.Default.prototype._getIconUrl;
+
+  L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+  });
+
   export default {
     components: {
       LMap,
