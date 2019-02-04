8513920586fa2c3142c1da4e6a6d5c591e457fb8
mjy <diapriid@gmail.com>
2019-02-04 14:44:57 -0600

    permit recent param

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -169,7 +169,7 @@ class SourcesController < ApplicationController
   end
 
   def filter_params
-    params.permit(:query_term, :project_id, author_ids: [])
+    params.permit(:query_term, :project_id, :recent, author_ids: [])
   end
 
 
