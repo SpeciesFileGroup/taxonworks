1f6a89dc65b9ebe9b31a712922a400a57dbbd212
José Luis Pereira <pepelui@outlook.com>
2019-02-21 17:19:18 -0300

    First steps for OTU Pages

---
+++ app/javascript/vue/tasks/otu/browse/app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Otu browser</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
---
+++ app/javascript/vue/tasks/otu/browse/config/api.js
@@ -0,0 +1,7 @@
+export default {
+  apiURL: '',
+  apiParams: {
+    project_id: 1,
+    token: ''
+  }
+};
