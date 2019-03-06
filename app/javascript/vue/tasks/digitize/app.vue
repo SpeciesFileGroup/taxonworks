6dcbedfe08bd48886b2a5a88a39cee7b908c06f8
José Luis Pereira <pepelui@outlook.com>
2019-03-06 13:37:09 -0300

    Fix #826

--- app/javascript/vue/tasks/digitize/app.vue
+++ app/javascript/vue/tasks/digitize/app.vue
@@ -3,6 +3,7 @@
     id="vue-all-in-one"
     v-shortkey="[getMacKey(), 'l']"
     @shortkey="setLockAll">
+    <h1>Comprehensive specimen digitization</h1>
     <spinner-component
       v-if="saving || loading"
       :full-screen="true"
