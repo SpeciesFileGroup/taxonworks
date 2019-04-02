c39397a3d02257e3e413c34727303a3408529119
mjy <diapriid@gmail.com>
2019-04-02 11:24:29 -0500

    Fix #901

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -57,10 +57,16 @@ class ImagesController < ApplicationController
   # DELETE /images/1
   # DELETE /images/1.json
   def destroy
-    @image.destroy!
-    respond_to do |format|
-      format.html { redirect_to images_url, notice: 'Image was successfully destroyed.' }
-      format.json { head :no_content }
+    if @image.destroy
+      respond_to do |format|
+        format.html { redirect_to images_url, notice: 'Image was successfully destroyed.' }
+        format.json { head :no_content }
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to images_url, notice: @image.errors.full_messages.join('. ') }
+        format.json { head :no_content, status: :im_used }
+      end
     end
   end
 
