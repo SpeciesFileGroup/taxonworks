bd2e3e2b5380fff9ab6f60211cfe3f9f83ab9b61
José Luis Pereira <pepelui@outlook.com>
2019-04-03 00:32:32 -0300

    Fix #407

--- app/javascript/vue/nomenclature/new_taxon_name/components/type.vue
+++ app/javascript/vue/nomenclature/new_taxon_name/components/type.vue
@@ -32,11 +32,22 @@
             :add-params="{ type: 'Protonym', 'nomenclature_group[]': childOfParent[getRankGroup.toLowerCase()] }"
             param="term"/>
         </hard-validation>
-        <a
-          v-if="showForThisGroup(['SpeciesGroup'], taxon)"
-          :href="`/tasks/type_material/edit_type_material?protonym_id=${taxon.id}`"
-          target="_blank">Add type specimens
-        </a>
+        <template v-if="showForThisGroup(['SpeciesGroup'], taxon)">
+          <a
+            :href="`/tasks/type_material/edit_type_material?protonym_id=${taxon.id}`"
+            target="_blank">Add type specimens
+          </a>
+          <hr>
+          <ul class="no_bullets">
+            <li
+              v-for="typeSpecimen in typeMaterialList"
+              :key="typeSpecimen.id">
+              <a
+                :href="`/tasks/type_material/edit_type_material?protonym_id=${taxon.id}&type_material_id=${typeSpecimen.id}`"
+                v-html="typeSpecimen.object_tag"/>
+            </li>
+          </ul>
+        </template>
       </div>
       <div v-else>
         <tree-display
@@ -107,6 +118,8 @@ import HardValidation from './hardValidation.vue'
 import getRankGroup from '../helpers/getRankGroup'
 import childOfParent from '../helpers/childOfParent'
 
+import { GetTypeMaterial } from '../request/resources.js'
+
 export default {
   components: {
     ListEntrys,
@@ -166,7 +179,8 @@ export default {
       objectLists: this.makeLists(),
       expanded: true,
       showAdvance: false,
-      childOfParent: childOfParent
+      childOfParent: childOfParent,
+      typeMaterialList: []
     }
   },
   watch: {
@@ -176,6 +190,17 @@ export default {
         this.refresh()
       },
       immediate: true
+    },
+    taxon: {
+      handler(newVal) {
+        if(newVal.id) {
+          GetTypeMaterial(newVal.id).then(response => {
+            this.typeMaterialList = response
+          }) 
+        }
+      },
+      immediate: true,
+      deep: true
     }
   },
   methods: {
