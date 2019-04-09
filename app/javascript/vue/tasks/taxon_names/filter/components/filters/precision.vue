673629123e19e8fb0e53ae98653f260339c69e41
José Luis Pereira <pepelui@outlook.com>
2019-04-09 17:38:07 -0300

    Added precision filter

---
+++ app/javascript/vue/tasks/taxon_names/filter/components/filters/precision.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>Precision</h2>
+    <label>
+      <input
+        type="checkbox">
+      Exact match only
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
