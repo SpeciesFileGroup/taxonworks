75192d82717c347494b6aa92031cc23f7120c89a
Hernán Lucas Pereira <hernan@cepave.edu.ar>
2019-04-22 16:29:08 -0300

    Clear VCR cache on Travis CRON event

--- .travis/spec_runner.sh
+++ .travis/spec_runner.sh
@@ -38,6 +38,9 @@ report() {
 START_TIME=$(date +%s)
 trap 'report' ERR
 
+# Refresh VCRs on CRON event
+[ "$TRAVIS_EVENT_TYPE" != "cron" ] || rm -rf spec/fixtures/vcr_cassettes/*
+
 # Precompile assets only if feature tests will be executed
 echo ${SPECS_TO_RUN[$TEST_WORKER]} | grep -qv "spec/features/" || \
 bundle exec rake assets:precompile
