4de9e8e6575ee27c450aff3a089fc2c3472bdb62
mjy <diapriid@gmail.com>
2019-04-23 14:08:30 -0500

    #947 work

--- app/controllers/tasks/accessions/report/dwc_controller.rb
+++ app/controllers/tasks/accessions/report/dwc_controller.rb
@@ -1,8 +1,18 @@
 class Tasks::Accessions::Report::DwcController < ApplicationController
   include TaskControllerConfiguration
 
+  # tasks/accessions/report/dwc
   def index 
-    @collection_objects = CollectionObject.order(:id).includes(:dwc_occurrence).with_project_id(sessions_current_project_id).page(params[:page])
+    respond_to do |format|
+      format.html do
+        @collection_objects = CollectionObject.order(:id).includes(:dwc_occurrence).with_project_id(sessions_current_project_id).page(params[:page]).per(params[:per] || 30)
+      end
+      format.json {
+        # TEMPORARY HACK! To be resolved with proper filter params at some points
+        # Currently only used in digitize recent modal
+        @collection_objects = CollectionObject.where(project_id: sessions_current_project_id).order(updated_at: :desc).includes(:dwc_occurrence).page(params[:page]).per(params[:per] || 30)
+      }
+    end
   end
 
   def row
