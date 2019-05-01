95dce5be0ad846c83fa0a7e2f072d5a9ce741212
mjy <diapriid@gmail.com>
2019-05-01 13:40:32 -0500

    Fix #941

--- spec/models/observation_spec.rb
+++ spec/models/observation_spec.rb
@@ -54,6 +54,31 @@ RSpec.describe Observation, type: :model, group: :matrix do
       o = FactoryBot.create(:valid_observation, otu: new, descriptor: o1.descriptor) 
       expect(Observation.copy(old.to_global_id.to_s, new.to_global_id.to_s)).to be_truthy
     end
+  end
+
+  context 'destroy' do
+
+    let(:observation_matrix) { FactoryBot.create(:valid_observation_matrix) }
+    let!(:o) { FactoryBot.create(:valid_observation, otu: otu) }
+    let!(:o1) { FactoryBot.create(:valid_observation, otu: otu) } # does not use same dd
+    let(:d) { o.descriptor }
+    let!(:c) { FactoryBot.create(:valid_observation_matrix_column, observation_matrix: observation_matrix, descriptor: d ) }
+    let!(:r) { FactoryBot.create(:valid_observation_matrix_row, observation_matrix: observation_matrix, otu: otu) }
+    let!(:r1) { FactoryBot.create(:valid_observation_matrix_row, observation_matrix: observation_matrix) }
+
+    specify '.destroy_row 1' do
+      expect(Observation.destroy_row(r.id)).to be_truthy
+    end
+
+    specify '.destroy_row 2' do
+      Observation.destroy_row(r1.id)
+      expect(Observation.all.map(&:id)).to contain_exactly(o1.id, o.id)
+    end
+
+    specify '.destroy_row 3' do
+      Observation.destroy_row(r.id)
+      expect(Observation.all.map(&:id)).to contain_exactly(o1.id)
+    end
 
   end
 
