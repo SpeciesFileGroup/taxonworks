e4168b1448425c3dc101ff89cc94ad1942f084df
José Luis Pereira <pepelui@outlook.com>
2019-05-02 20:03:38 -0300

    Added pagination component

---
+++ app/javascript/vue/tasks/people/author_by_letter/components/pagination.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <template>
+      <a
+        v-if="pagination['previousPage']"
+        class="cursor-pointer"
+        @click="sendPage(pagination.previousPage)">Previous
+      </a>
+      <span v-else>
+        Previous
+      </span>
+    </template>
+    <template>
+      <a        
+        v-if="pagination['nextPage']"
+        class="cursor-pointer"
+        @click="sendPage(pagination.nextPage)">Next
+      </a>
+      <span v-else>
+        Next
+      </span>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    pagination: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    sendPage(page) {
+      this.$emit('nextPage', { page: page })
+    }
+  }
+}
+</script>
