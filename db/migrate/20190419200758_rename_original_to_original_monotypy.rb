61e667dd8c4608087ecd632d4a5129e2fcd33b8a
mjy <diapriid@gmail.com>
2019-05-02 11:44:36 -0500

    Fix #733

--- db/migrate/20190419200758_rename_original_to_original_monotypy.rb
+++ db/migrate/20190419200758_rename_original_to_original_monotypy.rb
@@ -1,27 +1,6 @@
 class RenameOriginalToOriginalMonotypy < ActiveRecord::Migration[5.2]
 
   def change
-    #### Active record does not find classes which do not exist
-    #
-#    TaxonNameRelationship.where(type: 'TaxonNameRelationship::Typification::Genus::OriginalDesignation').find_each do |r|
-#      r.update_column(:type, 'TaxonNameRelationship::Typification::Genus::Original::OriginalDesignation')
-#    end
-#    TaxonNameRelationship.where(type: 'TaxonNameRelationship::Typification::Genus::SubsequentDesignation').find_each do |r|
-#      r.update_column(:type, 'TaxonNameRelationship::Typification::Genus::Subsequent::SubsequentDesignation')
-#    end
-#    TaxonNameRelationship.where(type: 'TaxonNameRelationship::Typification::Genus::Monotypy::Subsequent').find_each do |r|
-#      r.update_column(:type, 'TaxonNameRelationship::Typification::Genus::Subsequent::SubsequentMonotypy')
-#    end
-#    TaxonNameRelationship.where(type: 'TaxonNameRelationship::Typification::Genus::RulingByCommission').find_each do |r|
-#      r.update_column(:type, 'TaxonNameRelationship::Typification::Genus::Subsequent::RulingByCommission')
-#    end
-#    TaxonNameRelationship.where(type: 'TaxonNameRelationship::Typification::Genus::Monotypy::OriginalMonotypy').find_each do |r|
-#      r.update_column(:type, 'TaxonNameRelationship::Typification::Genus::Original::OriginalMonotypy')
-#    end
-#    TaxonNameRelationship.where(type: 'TaxonNameRelationship::Typification::Genus::Monotypy').find_each do |r|
-#      r.update_column(:type, 'TaxonNameRelationship::Typification::Genus::Original::OriginalMonotypy')
-#    end
-
     TaxonNameRelationship.connection.execute("UPDATE taxon_name_relationships SET type = 'TaxonNameRelationship::Typification::Genus::Original::OriginalDesignation' WHERE type = 'TaxonNameRelationship::Typification::Genus::OriginalDesignation';")
     TaxonNameRelationship.connection.execute("UPDATE taxon_name_relationships SET type = 'TaxonNameRelationship::Typification::Genus::Subsequent::SubsequentDesignation' WHERE type = 'TaxonNameRelationship::Typification::Genus::SubsequentDesignation';")
     TaxonNameRelationship.connection.execute("UPDATE taxon_name_relationships SET type = 'TaxonNameRelationship::Typification::Genus::Subsequent::SubsequentMonotypy' WHERE type = 'TaxonNameRelationship::Typification::Genus::Monotypy::Subsequent';")
