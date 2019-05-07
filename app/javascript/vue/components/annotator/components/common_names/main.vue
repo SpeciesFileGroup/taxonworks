23d4b1ec7fa4523372d22f0c0b000bca92975839
José Luis Pereira <pepelui@outlook.com>
2019-05-07 19:12:54 -0300

    Fix #960

--- app/javascript/vue/components/annotator/components/common_names/main.vue
+++ app/javascript/vue/components/annotator/components/common_names/main.vue
@@ -10,30 +10,47 @@
       </div>
       <label>Geographic area</label>
       <div class="horizontal-left-content separate-bottom">
-        <autocomplete
-          url="/geographic_areas/autocomplete"
-          param="term"
-          label="label_html"
-          display="label"
-          ref="geoAutocomplete"
-          placeholder="Select a geopgrahic area"
-          @getItem="common_name.geographic_area_id = $event.id"/>
-        <default-pinned
-          section="GeographicAreas"
-          type="GeographicArea"
-          @getLabel="$refs.geoAutocomplete.setLabel($event)"
-          @getId="common_name.geographic_area_id = $event"/>
+        <template v-if="selectedGeographic">
+          <span v-html="geographicLabel"/>
+          <span
+            class="circle-button button-default btn-undo"
+            @click="common_name.geographic_area_id = null; selectedGeographic = undefined"/>
+        </template>
+        <template v-else>
+          <autocomplete
+            url="/geographic_areas/autocomplete"
+            param="term"
+            label="label_html"
+            display="label"
+            ref="geoAutocomplete"
+            placeholder="Select a geopgrahic area"
+            @getItem="selectedGeographic = $event; common_name.geographic_area_id = $event.id"/>
+          <default-pinned
+            section="GeographicAreas"
+            type="GeographicArea"
+            @getLabel="$refs.geoAutocomplete.setLabel($event)"
+            @getId="common_name.geographic_area_id = $event"/>
+        </template>
       </div>
       <label>Language</label>
-      <autocomplete
-        class="separate-bottom"
-        url="/languages/autocomplete"
-        param="term"
-        label="label_html"
-        display="label"
-        ref="langAutocomplete"
-        placeholder="Select a language"
-        @getItem="common_name.language_id = $event.id"/>
+      <div class="horizontal-left-content separate-bottom">
+        <template v-if="selectedLenguage">
+          <span v-html="languageLabel"/>
+          <span
+            class="circle-button button-default btn-undo"
+            @click="common_name.language_id = null; selectedLenguage = undefined"/>
+        </template>
+        <template v-else>
+          <autocomplete
+            url="/languages/autocomplete"
+            param="term"
+            label="label_html"
+            display="label"
+            ref="langAutocomplete"
+            placeholder="Select a language"
+            @getItem="selectedLanguage = $event.label; common_name.language_id = $event.id"/>
+        </template>
+      </div>
       <div class="separate-bottom">
         <label>Start year</label>
         <input
@@ -61,7 +78,13 @@
         :disabled="!validate"
         @click="createNew"
         class="button normal-input button-submit">
-        Create
+        {{ common_name.id ? 'Update' : 'Create' }}
+      </button>
+      <button
+        class="button normal-input button-default"
+        @click="reset"
+        type="button">
+        Reset
       </button>
     </div>
     <table-list
@@ -69,6 +92,8 @@
       :header="['Name', 'Geographic area', 'Language', 'Start', 'End','']"
       :attributes="['name', ['geographic_area', 'object_tag'], 'language_tag', 'start_year', 'end_year']"
       :list="list"
+      :edit="true"
+      @edit="editCommon"
       @delete="removeItem"
       class="list"/>
   </div>
@@ -92,17 +117,32 @@ export default {
   computed: {
     validate() {
       return (this.common_name.name.length > 2 && this.common_name.otu_id != undefined)
+    },
+    geographicLabel() {
+      if(!this.selectedGeographic) return
+      return this.selectedGeographic.hasOwnProperty('object_tag') ? this.selectedGeographic.object_tag : this.selectedGeographic.label
+    },
+    languageLabel() {
+      if(!this.selectedLenguage) return
+      return this.selectedLenguage
     }
   },
   data() {
     return {
-      common_name: this.newCommonName()
+      common_name: this.newCommonName(),
+      selectedGeographic: undefined,
+      selectedLenguage: undefined
     }
   },
   mounted() {
     this.urlList = `/common_names.json?otu_id=${this.metadata.object_it}`
   },
   methods: {
+    reset() {
+      this.common_name = this.newCommonName()
+      this.selectedGeographic = undefined
+      this.selectedLenguage = undefined
+    },
     newCommonName() {
       return {
         id: undefined,
@@ -115,12 +155,32 @@ export default {
       }
     },
     createNew() {
-      this.create('/common_names', {common_name: this.common_name}).then(response => {
-        this.list.push(response.body)
-        this.common_name = this.newCommonName()
-        this.$refs.geoAutocomplete.setLabel('')
-        this.$refs.langAutocomplete.setLabel('')
-      })
+      if(this.common_name.id) {
+        this.update(`/common_names/${this.common_name.id}.json`, { common_name: this.common_name }).then(response => {
+          this.list[this.list.findIndex(item => { return item.id == this.common_name.id })] = response.body
+          this.reset()
+        })
+      }
+      else {
+        this.create('/common_names', {common_name: this.common_name}).then(response => {
+          this.list.push(response.body)
+          this.reset()
+        })
+      }
+    },
+    editCommon(common) {
+      this.common_name.id = common.id
+      this.common_name.name = common.name
+      if(common.hasOwnProperty('geographic_area')) {
+        this.common_name.geographic_area_id = common.geographic_area_id
+        this.selectedGeographic = common.geographic_area
+      }
+      if(common.language_tag) {
+        this.common_name.language_id = common.language_id
+        this.selectedLenguage = common.language_tag
+      }
+      this.common_name.start_year = common.start_year
+      this.common_name.end_year = common.end_year
     }
   }
 }
