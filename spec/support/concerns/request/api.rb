a826a85b7d24d526e0e7b0770fc344385e93a79c
Hernán Lucas Pereira <hernan@cepave.edu.ar>
2019-05-14 18:03:31 -0300

    Added taxon_name API spec, created new parameterized shared example

--- spec/support/concerns/request/api.rb
+++ spec/support/concerns/request/api.rb
@@ -20,3 +20,24 @@ shared_examples_for 'unauthorized response' do
 # end
 end
 
+shared_examples_for 'secured by both user and project token' do | factory, path |
+  let(:user) { FactoryBot.create(:valid_user, :user_valid_token) }
+  let(:headers) { { "Authorization": 'Token token=' + user.api_access_token, project_id: model.project_id } }
+  let(:model) { FactoryBot.create(factory) }
+
+  context 'without a user token' do
+    before { get path }
+    it_behaves_like 'unauthorized response'
+  end
+
+  context 'with a valid user token, without project_id' do
+    before { get path, headers: headers }
+    it_behaves_like 'unauthorized response'
+  end
+
+  context 'with a valid user token and valid project token (project set by proxy)' do
+    before { model.project.update(set_new_api_access_token: true) }
+    before { get path, headers: headers, params: { project_token: model.project.api_access_token } }
+    it_behaves_like 'a successful response'
+  end
+end
