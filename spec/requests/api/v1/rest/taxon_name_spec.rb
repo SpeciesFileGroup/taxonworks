e0acf867c8552f72054399f2aa516b66c987dd5c
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2019-06-14 19:24:27 -0300

    Enabled project_token-only API access

--- spec/requests/api/v1/rest/taxon_name_spec.rb
+++ spec/requests/api/v1/rest/taxon_name_spec.rb
@@ -8,18 +8,11 @@ describe 'Api::V1::TaxonNames', type: :request do
     # let(:headers) { { "Authorization": 'Token token=' + user.api_access_token, project_id: taxon_name.project_id } }
     # let(:path) { '/api/v1/taxon_names/' }
 
-    it_behaves_like 'secured by both user and project token', :valid_taxon_name, '/api/v1/taxon_names/'
-
-    # project token-only for now
-    xcontext 'with a valid user token and project_id' do
-      before { get path, headers: headers, params: { project_id: taxon_name.project_id } }
-      it_behaves_like 'a successful response'
-    end
+    it_behaves_like 'secured by user and project token', :valid_taxon_name, '/api/v1/taxon_names/'
   end
 
   context 'taxon_names/autocomplete' do
 
-    it_behaves_like 'secured by both user and project token', :valid_taxon_name, '/api/v1/taxon_names/autocomplete'
-
+    it_behaves_like 'secured by user and project token', :valid_taxon_name, '/api/v1/taxon_names/autocomplete'
   end
 end
