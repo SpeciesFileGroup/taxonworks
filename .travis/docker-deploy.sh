3699afe1068ce9f8c3bbf6333e7afa1092181002
Hernán Lucas Pereira <hernan@cepave.edu.ar>
2019-07-07 02:53:53 -0300

    Quick test with Firefox to confirm it is good to push to Docker Hub

--- .travis/docker-deploy.sh
+++ .travis/docker-deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-set -e
-set -x
+#set -e
+#set -x
 
 docker build .. -t sfgrp/taxonworks --build-arg REVISION=$(git rev-parse --short HEAD)
-docker-compose up -d
 
-# TODO: Perhaps perform integration test by simulating a browser login in and checking the commit hash is visible
-curl -v http://localhost/api/v1/ping
+REVISION=$(git rev-parse --short HEAD) docker-compose up --build --exit-code-from test
+
+docker-compose down --volumes
 
 # TODO: Push to Docker Hub here
