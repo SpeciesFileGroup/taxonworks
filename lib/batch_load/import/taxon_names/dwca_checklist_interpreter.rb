6d34f5d66c65b4982ce6e1e61fdf4f373efd1a33
Hernán Lucas Pereira <hernan@cepave.edu.ar>
2019-07-10 06:56:09 -0300

    DwC-A import WIP

--- lib/batch_load/import/taxon_names/dwca_checklist_interpreter.rb
+++ lib/batch_load/import/taxon_names/dwca_checklist_interpreter.rb
@@ -1,34 +1,77 @@
 module BatchLoad
-  class Import::TaxonName::DwcaChecklistInterpreter < BatchLoad::Import
+  class Import::TaxonNames::DwcaChecklistInterpreter < BatchLoad::Import
+    NAME_COMPONENTS = ['infraspecificepithet', 'specificepithet', 'subgenus', 'genus', 'scientificname']
 
     def initialize(**args)
       @taxon_names = {}
       super(args)
     end
 
-    # TODO: update this
+    def _build_taxon_names(records, parent)
+      records.each do |record|
+        begin
+          protonym_attributes = {
+            name: record[:row][NAME_COMPONENTS.detect { |x| !record[:row][x].blank? }],
+            parent: parent,
+            rank_class: Ranks.lookup(:iczn, record[:row]['taxonrank']),
+            by: @user,
+            also_create_otu: false,
+            project: @project,
+            verbatim_author: record[:row]['scientificnameauthorship']
+          }
+
+          protonym_attributes[:year_of_publication] = $1 if /(\s\d{4})(?!.*\d+)/ =~ protonym_attributes[:verbatim_author]
+
+          parse_result = BatchLoad::RowParse.new
+          name = Protonym.new(protonym_attributes)
+          parse_result.objects[:taxon_name] = [name]
+
+          if "typeGenus" == record[:row]["typestatus"]
+
+          end
+
+          @processed_rows[record[:rowno]] = parse_result
+
+          @total_data_lines += 1
+
+          _build_taxon_names(record[:children], name)
+        rescue Exception => ex
+          ap ex
+        end
+      end
+    end
+
     def build_taxon_names
       @total_data_lines = 0
       i = 0
 
-      # loop through rows
+      # build records tree
+      records = {}
       csv.each do |row|
         i += 1
+        raise "Duplicated taxonID #{row["taxonid"]}." unless records[row["taxonid"]].nil?
+        # Build records LUT
+        records[row["taxonid"]] = { row: row, rowno: i, children: [] }
+      end
 
-        parse_result = BatchLoad::RowParse.new
-        parse_result.objects[:taxon_name] = []
+      records.each do |k, v|
+        if not v[:row]["parentnameusageid"].blank?
+          if records[v[:row]["parentnameusageid"]].nil?
+            raise "Invalid parentNameUsageID #{v[:row]["parentnameusageid"]} points to no record within this dataset."
+          end
 
-        @processed_rows[i] = parse_result
+          parent = records[v[:row]["parentnameusageid"]]
 
-        begin # processing
-          # use a BatchLoad::ColumnResolver or other method to match row data to TW
-          #  ...
-
-          @total_data_lines += 1
-        rescue
-           # ....
+          parent[:children] << v
+          v[:parent] = parent
         end
       end
+
+      records = records.values.select { |v| v[:row]["parentnameusageid"].blank? }
+
+      # loop through rows
+
+      _build_taxon_names(records, Project.find(@project_id).root_taxon_name)
 
       @total_lines = i
     end
