d70a12b68947087199265b4314937d89a032a6cd
Hernán Lucas Pereira <hernan@cepave.edu.ar>
2019-07-19 19:58:32 -0300

    Provide an API-like response when and endpoint route doesn't exist

--- spec/requests/api/v1/base_spec.rb
+++ spec/requests/api/v1/base_spec.rb
@@ -14,4 +14,18 @@ describe Api::V1::BaseController, type: :request do
       expect(JSON.parse(response.body).dig('open_projects')).to contain_exactly({project.api_access_token => project.name})
     end
   end
+
+  context 'Invalid path' do
+    let(:path) { '/api/v1/this-path-does-not-exist' }
+
+    before { get path  }
+
+    specify 'status' do
+      expect(response).to be_not_found
+    end
+
+    specify 'message' do
+      expect(JSON.parse(response.body)['message']).to eq('Invalid route')
+    end
+  end
 end
