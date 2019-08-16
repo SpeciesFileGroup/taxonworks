991e1e693436606d7b455cb9a84dea30e2509905
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2019-08-16 17:06:43 -0300

    Update GEOLocate URL Old www.museum.tulane.edu host has not been working for some days now, it is unresolvable. Likely it was working as a redirection for backwards compatibility but now has been taken down.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_13_201512) do
+ActiveRecord::Schema.define(version: 2019_08_14_194707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
