b9631dc71b2e9803d2a1112a0651c540556f5c82
José Luis Pereira <pepelui@outlook.com>
2019-08-23 19:34:35 -0300

    Added switch slider component

---
+++ app/javascript/vue/components/form/switchSlider.vue
@@ -0,0 +1,55 @@
+<template>
+  <label class="switch">
+    <input
+      type="checkbox"
+      v-model="inputValue">
+    <div
+      class="slider round"
+      :class="{ 'slider-active': inputValue } "/>
+  </label>
+</template>
+
+<script>
+
+export default {
+  props: {
+    value: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    inputValue: {
+      get () {
+        return this.value
+      },
+      set (newValue) {
+        this.$emit('input', newValue)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .slider {
+    height: 28px;
+  }
+  .slider:before {
+    height: 20px;
+    width: 20px;
+  }
+  input + .slider-active {
+    background-color: #5D9ECE !important;
+  }
+
+  input + .slider-active::before {
+    -webkit-transform: translateX(32px);
+    -ms-transform: translateX(32px);
+    transform: translateX(32px);
+  }
+
+  input + .slider {
+    background-color: #F5F5F5;
+  }
+</style>
