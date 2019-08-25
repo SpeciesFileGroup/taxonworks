c79805f71b67ef7f267c0723f86c095676c654c4
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2019-08-25 20:07:37 -0300

    Update Faker calls to be compatible with new version

--- spec/factories/biological_property_factory.rb
+++ spec/factories/biological_property_factory.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :biological_property, class: BiologicalProperty, traits: [:housekeeping] do
     factory :valid_biological_property do
-      name { Faker::Lorem.characters(12) }
-      definition { Faker::Lorem.sentence(6, false, 3) }
+      name { Faker::Lorem.characters(number: 12) }
+      definition { Faker::Lorem.sentence(word_count: 6, supplemental: false, random_words_to_add: 3) }
     end
   end
 end
