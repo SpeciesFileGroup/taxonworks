216f2deff1cfd6d8b0e782b9c54320792cc368ca
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2019-10-10 18:29:40 -0300

    Download through UI (#1093)

--- spec/factories/download_factory.rb
+++ spec/factories/download_factory.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :download, class: Download, traits: [:housekeeping] do
     factory :valid_download do
-      name { "Sample File" }
-      description { "A sample file for testing" }
-      filename { "Sample.zip" }
+      name { Faker::Lorem.word }
+      description { Faker::Lorem.sentence }
+      filename { "#{Faker::Lorem.word}.zip" }
       src_file_path { Rails.root.join('spec/files/downloads/Sample.zip') }
       request { "/model/endpoint?params" }
       expires { 2.days.from_now }
--- spec/routing/downloads_routing_spec.rb
+++ spec/routing/downloads_routing_spec.rb
@@ -6,29 +6,16 @@ RSpec.describe DownloadsController, type: :routing do
       expect(:get => "/downloads").to route_to("downloads#index")
     end
 
-    it "routes to #new" do
-      expect(:get => "/downloads/new").to route_to("downloads#new")
+    it "routes to #list" do
+      expect(:get => "/downloads/list").to route_to("downloads#list")
     end
 
     it "routes to #show" do
       expect(:get => "/downloads/1").to route_to("downloads#show", :id => "1")
     end
 
-    it "routes to #edit" do
-      expect(:get => "/downloads/1/edit").to route_to("downloads#edit", :id => "1")
-    end
-
-
-    it "routes to #create" do
-      expect(:post => "/downloads").to route_to("downloads#create")
-    end
-
-    it "routes to #update via PUT" do
-      expect(:put => "/downloads/1").to route_to("downloads#update", :id => "1")
-    end
-
-    it "routes to #update via PATCH" do
-      expect(:patch => "/downloads/1").to route_to("downloads#update", :id => "1")
+    it "routes to #download_file" do
+      expect(:get => "/downloads/1/download_file").to route_to("downloads#download_file", :id => "1")
     end
 
     it "routes to #destroy" do
