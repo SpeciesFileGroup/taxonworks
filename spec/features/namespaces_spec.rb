83684765bcdc2aa75aeb5bde2029bd5de859d1e1
mjy <diapriid@gmail.com>
2019-10-16 22:17:50 -0500

    bump test gems, fix namespace missmatch

--- spec/features/namespaces_spec.rb
+++ spec/features/namespaces_spec.rb
@@ -6,46 +6,35 @@ describe 'Namespaces', type: :feature do
 
   it_behaves_like 'a_login_required_controller'
 
-  context 'signed in' do
+  context 'signed in'  do
     before { sign_in_user }
     context 'with some records created' do
       before {
-        5.times { factory_bot_create_for_user(:valid_namespace, @user) }
+        3.times { factory_bot_create_for_user(:valid_namespace, @user) }
       }
 
       describe 'GET /Namespaces' do
-        before {
-          visit namespaces_path
-        }
-
+        before {visit namespaces_path }
         it_behaves_like 'a_data_model_with_standard_index'
       end
 
       describe 'GET /namespaces/list' do
-        before {
-          visit list_namespaces_path
-        }
-
+        before { visit list_namespaces_path }
         it_behaves_like 'a_data_model_with_standard_list_and_records_created'
       end
 
       describe 'GET /namespaces/n' do
-        before {
-          visit namespace_path(Namespace.second)
-        }
-
+        before { visit namespace_path(Namespace.second) }
         it_behaves_like 'a_data_model_with_standard_show'
       end
 
       context 'creating a new namespace' do
-        before {
-          visit(namespaces_path)
-        }
+        before { visit(namespaces_path) }
 
         specify 'adding the new namespace' do
           click_link('New')
-          fill_in('Full name', with: 'Things Pat Collected')
-          fill_in('Short name', with: 'tpd')
+          fill_in(:namespace_name, with: 'Things Pat Collected')
+          fill_in(:namespace_short_name, with: 'tpd')
           click_button('Create Namespace')
           expect(page).to have_content("Namespace 'Things Pat Collected' was successfully created")
         end
