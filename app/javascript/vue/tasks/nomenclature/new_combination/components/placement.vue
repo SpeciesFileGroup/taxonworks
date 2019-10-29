029d44d1c1d92658305e19dab288d21661b9393a
José Luis Pereira <pepelui@outlook.com>
2019-10-29 12:36:57 -0300

    Fix

--- app/javascript/vue/tasks/nomenclature/new_combination/components/placement.vue
+++ app/javascript/vue/tasks/nomenclature/new_combination/components/placement.vue
@@ -2,14 +2,14 @@
   <button
     @click="create"
     type="button"
-    v-if="!combination.placement.same"
+    v-if="!combination.placement.same && taxon && parent"
     class="button button-submit normal-input">
-    Use as current placement
+    Move {{ taxon.name }} to {{ parent.name }}
   </button>
 </template>
 <script>
 
-import { CreatePlacement } from '../request/resources'
+import { CreatePlacement, GetTaxonName } from '../request/resources'
 
 export default {
   props: {
@@ -18,14 +18,31 @@ export default {
       required: true
     }
   },
+  data () {
+    return {
+      taxon: undefined,
+      parent: undefined
+    }
+  },
+  mounted () {
+    if(!this.combination.placement.same) {
+      GetTaxonName(this.combination.placement.target_id).then(response => {
+        this.taxon = response
+      })
+      GetTaxonName(this.combination.placement.new_parent_id).then(response => {
+        this.parent = response
+      })
+    }
+  },
   methods: {
     create () {
       let data = {
         taxon_name: {
-          parent_id: this.combination.placement.parent_id
+          id: this.combination.placement.target_id,
+          parent_id: this.combination.placement.new_parent_id
         }
       }
-      CreatePlacement(this.combination.placement.target, data).then(response => {
+      CreatePlacement(this.combination.placement.target_id, data).then(response => {
         TW.workbench.alert.create(`Updated parent of ${response.name} to ${response.parent.name}`, 'notice')
         this.$emit('created', response)
       })
