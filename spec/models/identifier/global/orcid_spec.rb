13480fcc6431aac8472c06688662f58baf3ab297
mjy <diapriid@gmail.com>
2019-10-31 14:05:33 -0500

    Faker/Gem update

--- spec/models/identifier/global/orcid_spec.rb
+++ spec/models/identifier/global/orcid_spec.rb
@@ -13,34 +13,34 @@ describe Identifier::Global::Orcid, type: :model, group: :identifiers do
       end
 
       specify 'any old word' do
-        phrase        = Faker::Lorem.word
+        phrase = Faker::Lorem.word
         id.identifier = phrase
         expect(id.valid?).to be_falsey
         expect(id.errors.messages[:identifier][0]).to eq("'#{phrase}' is an improperly formed ORCID ID.")
       end
 
       specify 'http://orcid.org/00100-03002-13825-00497' do
-        phrase        = 'http://orcid.org/00100-03002-13825-00497'
+        phrase = 'http://orcid.org/00100-03002-13825-00497'
         id.identifier = phrase
         expect(id.valid?).to be_falsey
         expect(id.errors.messages[:identifier][0]).to eq("'#{phrase}' is an improperly formed ORCID ID.")
       end
 
       specify 'http://orcid.org/0000-0002-1825-0096' do
-        phrase        = 'http://orcid.org/0000-0002-1825-0096'
+        phrase = 'http://orcid.org/0000-0002-1825-0096'
         id.identifier = phrase
         expect(id.valid?).to be_falsey
         expect(id.errors.messages[:identifier][0]).to eq("'#{phrase}' has bad check digit.")
       end
 
       specify 'http://orcid.org/0000-0002-1825-0097' do
-        phrase        = 'http://orcid.org/0000-0002-1825-0097'
+        phrase = 'http://orcid.org/0000-0002-1825-0097'
         id.identifier = phrase
         expect(id.valid?).to be_truthy
       end
 
       specify 'orcid.org/0000-0002-1825-0097' do
-        phrase        = 'http://orcid.org/0000-0002-1825-0097'
+        phrase = 'http://orcid.org/0000-0002-1825-0097'
         id.identifier = phrase
         expect(id.valid?).to be_truthy
       end
