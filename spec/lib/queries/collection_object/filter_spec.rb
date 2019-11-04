05d2ac1dee7392c623a76fa7e8807cac89826b00
mjy <diapriid@gmail.com>
2019-11-04 12:02:16 -0600

    fix non-deterministic spec

--- spec/lib/queries/collection_object/filter_spec.rb
+++ spec/lib/queries/collection_object/filter_spec.rb
@@ -299,8 +299,8 @@ describe Queries::CollectionObject::Filter, type: :model, group: [:geo, :collect
     end
 
     context 'identifiers' do
-      let!(:n1) { Namespace.create!(name: 'First', short_name: 'second'}
-      let!(:n1) { Namespace.create!(name: 'Third', short_name: 'fourth'}
+      let!(:n1) { Namespace.create!(name: 'First', short_name: 'second')}
+      let!(:n2) { Namespace.create!(name: 'Third', short_name: 'fourth')}
 
       let!(:i1) { Identifier::Local::CatalogNumber.create!(namespace: n1, identifier: '123', identifier_object: co1) } 
       let!(:i2) { Identifier::Local::CatalogNumber.create!(namespace: n2, identifier: '453', identifier_object: co2) } 
@@ -338,7 +338,6 @@ describe Queries::CollectionObject::Filter, type: :model, group: [:geo, :collect
       specify '#identifier_exact 2' do
         query.identifier_exact = false 
         query.identifier = '1'
-        byebug
         expect(query.all.map(&:id)).to contain_exactly(co1.id)
       end
     end
