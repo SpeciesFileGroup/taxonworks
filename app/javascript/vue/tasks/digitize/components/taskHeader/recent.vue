059437a0811c8f0e59a35aeb314254f7bea735d5
José Luis Pereira <pepelui@outlook.com>
2019-11-06 19:16:36 -0300

    added table items

--- app/javascript/vue/tasks/digitize/components/taskHeader/recent.vue
+++ app/javascript/vue/tasks/digitize/components/taskHeader/recent.vue
@@ -8,47 +8,48 @@
       v-if="showModal"
       @close="showModal = false">
       <h3 slot="header">Recent collection objects</h3>
-      <table
-        class="full_width"
-        slot="body">
-        <thead>
-          <tr>
-            <th>Family</th>
-            <th>Genus</th>
-            <th>Scientific name</th>
-            <th>Identifier</th>
-            <th>Biocuration attributes</th>
-            <th>Type</th>
-            <th>Level 1</th>
-            <th>Level 2</th>
-            <th>Level 3</th>
-            <th>Verbatim locality</th>
-            <th>Date start</th>
-            <th>Container</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr
-            v-for="(item, index) in list"
-            :key="item.id"
-            class="contextMenuCells"
-            :class="{ 'even': (index % 2 == 0) }"
-            @click="sendCO(item)">
-            <td>{{ item.dwc_attributes.family }}</td>
-            <td>{{ item.dwc_attributes.genus }}</td>
-            <td>{{ item.dwc_attributes.scientificName }}</td>
-            <td>{{ item.dwc_attributes.catalogNumber}}</td>
-            <td>{{ item.biocuration }}</td>
-            <td>{{ item.base_class }}</td>
-            <td>{{ item.dwc_attributes.country }}</td>
-            <td>{{ item.dwc_attributes.stateProvince }}</td>
-            <td>{{ item.dwc_attributes.county }}</td>
-            <td>{{ item.dwc_attributes.verbatimLocality }}</td>
-            <td>{{ item.dwc_attributes.eventDate }}</td>
-            <td>{{ item.dwc_attributes.container }}</td>
-          </tr>
-        </tbody>
-      </table>
+      <div slot="body">
+        <table class="full_width">
+          <thead>
+            <tr>
+              <th>Family</th>
+              <th>Genus</th>
+              <th>Scientific name</th>
+              <th>Identifier</th>
+              <th>Biocuration attributes</th>
+              <th>Level 1</th>
+              <th>Level 2</th>
+              <th>Level 3</th>
+              <th>Verbatim locality</th>
+              <th>Date start</th>
+              <th>Container</th>
+              <th>Updated at</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="(item, index) in list"
+              :key="item.id"
+              class="contextMenuCells"
+              :class="{ 'even': (index % 2 == 0) }"
+              @click="sendCO(item)">
+              <td>{{ item.dwc_attributes.family }}</td>
+              <td>{{ item.dwc_attributes.genus }}</td>
+              <td>{{ item.dwc_attributes.scientificName }}</td>
+              <td>{{ item.dwc_attributes.catalogNumber}}</td>
+              <td>{{ item.biocuration }}</td>
+              <td>{{ item.dwc_attributes.country }}</td>
+              <td>{{ item.dwc_attributes.stateProvince }}</td>
+              <td>{{ item.dwc_attributes.county }}</td>
+              <td>{{ item.dwc_attributes.verbatimLocality }}</td>
+              <td>{{ item.dwc_attributes.eventDate }}</td>
+              <td>{{ item.dwc_attributes.container }}</td>
+              <td>{{ item.update_at }}</td>
+            </tr>
+          </tbody>
+        </table>
+        <span>{{ list.length }} records.</span>
+      </div>
     </modal-component>
   </div>
 </template>
