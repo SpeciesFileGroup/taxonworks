22212f402b43a076a014fccc17afe5f01ea40244
mjy <diapriid@gmail.com>
2019-11-08 14:04:19 -0600

    container specs, endpoints, browse_collection object hax

--- app/controllers/containers_controller.rb
+++ app/controllers/containers_controller.rb
@@ -15,6 +15,15 @@ class ContainersController < ApplicationController
   def show
   end
 
+  # GET /containers/for/global_id
+  def for
+    object = GlobalID::Locator.locate(params.require(:global_id))
+    @container = object.respond_to?(:containable?) ? object.container : nil
+    render(json: { 'message' => 'Record not found' }, status: :unauthorized) if !object.is_community? && object.project_id != sessions_current_project_id
+    render(json: { success: false}, status: :not_found) and return if @container.nil?
+    render :show
+  end
+
   # GET /containers/new
   def new
     @container = Container.new
