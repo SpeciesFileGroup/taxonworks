4111c24af67ec5480de483af02307bf4434c4ff3
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2019-11-27 17:24:44 -0300

    Handle missing records at soft_validations_controller gracefully

--- app/controllers/soft_validations_controller.rb
+++ app/controllers/soft_validations_controller.rb
@@ -25,6 +25,8 @@ class SoftValidationsController < ApplicationController
 
   def get_object
     @object = GlobalID::Locator.locate(URI.decode(params[:global_id]))
+
+    raise ActiveRecord::RecordNotFound if @object.nil?
   end
 
 end
