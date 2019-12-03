38f7509bc817c3eeb5b9fb0329f99c5f8d0606f2
mjy <diapriid@gmail.com>
2019-12-03 17:01:49 -0600

    history-otu-taxon-name-id

--- lib/catalog/otu/entry_item.rb
+++ lib/catalog/otu/entry_item.rb
@@ -4,5 +4,15 @@ class Catalog::Otu::EntryItem < ::Catalog::EntryItem
     :otu_catalog_entry_item_tag
   end
 
+  def data_attributes
+    base_data_attributes.merge(
+      'history-otu-taxon-name-id' => taxon_name_global_id
+    )
+  end
+
+  def taxon_name_global_id
+    object&.taxon_name&.to_global_id&.to_s
+  end
+
 end
 
