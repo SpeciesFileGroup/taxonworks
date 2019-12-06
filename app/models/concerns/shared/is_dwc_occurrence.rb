a9789c9d4d1c4fc7599ca534b680c6166133786c
mjy <diapriid@gmail.com>
2019-12-06 13:31:05 -0600

    use update_columns to index

--- app/models/concerns/shared/is_dwc_occurrence.rb
+++ app/models/concerns/shared/is_dwc_occurrence.rb
@@ -4,6 +4,7 @@ module Shared::IsDwcOccurrence
   extend ActiveSupport::Concern
 
   included do
+    
     delegate :persisted?, to: :dwc_occurrence, prefix: :dwc_occurrence, allow_nil: true
 
     # @return Boolean, nil
@@ -35,11 +36,11 @@ module Shared::IsDwcOccurrence
   #   always touches the database
   def set_dwc_occurrence
     if dwc_occurrence_persisted?
-      dwc_occurrence.update!(dwc_occurrence_attributes)
+      dwc_occurrence.update_columns(dwc_occurrence_attributes)
     else
       create_dwc_occurrence!(dwc_occurrence_attributes)
     end
-    dwc_occurrence # .reload
+    dwc_occurrence
   end
 
   def dwc_occurrence_attributes
