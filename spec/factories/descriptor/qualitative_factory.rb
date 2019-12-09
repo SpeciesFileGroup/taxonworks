0e8763f8b9db771eccae8e31ef52baf6b7e3e76c
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2019-12-09 19:01:03 -0300

    Ensure unique values on for Faker methods (:each spec scope)

--- spec/factories/descriptor/qualitative_factory.rb
+++ spec/factories/descriptor/qualitative_factory.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :descriptor_qualitative, class: 'Descriptor::Qualitative', traits: [:housekeeping] do
     factory :valid_descriptor_qualitative do
-      name { Faker::Lorem.word }
+      name { Faker::Lorem.unique.word }
       type { 'Descriptor::Qualitative' }
     end
   end
