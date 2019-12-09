984897713ce206aa1f86d38577f9e458da107584
José Luis Pereira <pepelui@outlook.com>
2019-12-09 16:00:09 -0300

    Fix #1224

--- app/javascript/vue/tasks/digitize/store/actions/saveIdentifier.js
+++ app/javascript/vue/tasks/digitize/store/actions/saveIdentifier.js
@@ -4,10 +4,10 @@ import Vue from 'vue'
 
 export default function ({ commit, state }) {
   return new Promise((resolve, reject) => {
-    let identifier = state.identifier
-    if(state.collection_object.id && identifier.namespace_id && identifier.identifier && state.saveIdentifier) {
+    const identifier = state.identifier
+    if (state.collection_object.id && identifier.namespace_id && identifier.identifier && state.settings.saveIdentifier) {
       commit(MutationNames.SetIdentifierObjectId, state.container ? state.container.id : state.collection_object.id)
-      if(identifier.id) {
+      if (identifier.id) {
         UpdateIdentifier(identifier).then(response => {
           commit(MutationNames.SetIdentifier, response)
           const index = state.identifiers.findIndex(item => {
@@ -16,19 +16,17 @@ export default function ({ commit, state }) {
           Vue.set(state.identifiers, index, response)
           return resolve(response)
         })
-      }
-      else {
-        if(!state.identifiers.length) {
+      } else {
+        if (!state.identifiers.length) {
           CreateIdentifier(identifier).then(response => {
-            if(state.settings.increment) {
+            if (state.settings.increment) {
               response.identifier = state.identifier.identifier
             }
             commit(MutationNames.SetIdentifier, response)
             state.identifiers.push(response)
             return resolve(response)
           })
-        }
-        else {
+        } else {
           return resolve()
         }
       }
