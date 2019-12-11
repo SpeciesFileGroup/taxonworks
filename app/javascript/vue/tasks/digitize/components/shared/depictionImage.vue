a144a69fa21317344ead8938d25be49489448648
José Luis Pereira <pepelui@outlook.com>
2019-12-11 16:13:07 -0300

    Revert "Fix #1228"
    This reverts commit ade3e1a0a7410fee4587e51b4d9c820e75ed3399.

--- app/javascript/vue/tasks/digitize/components/shared/depictionImage.vue
+++ app/javascript/vue/tasks/digitize/components/shared/depictionImage.vue
@@ -2,7 +2,7 @@
   <div class="depiction-thumb-container">
     <modal
       v-if="viewMode"
-      @close="checkEdit"
+      @close="viewMode = false"
       :container-style="{ width: ((fullSizeImage ? depiction.image.width : depiction.image.alternatives.medium.width) + 'px')}">
       <h3 slot="header">View</h3>
       <div slot="body">
@@ -80,21 +80,7 @@ export default {
   data: function () {
     return {
       fullSizeImage: false,
-      viewMode: false,
-      editing: false
-    }
-  },
-  watch: {
-    viewMode(newVal) {
-      if(newVal) {
-        this.editing = false
-      }
-    },
-    depiction: {
-      handler(newVal) {
-        this.editing = true
-      },
-      deep: true
+      viewMode: false
     }
   },
   methods: {
@@ -107,20 +93,10 @@ export default {
       }
       UpdateDepiction(this.depiction.id, depiction).then(response => {
         TW.workbench.alert.create('Depiction was successfully updated.', 'notice')
-        this.editing = false
       })
     },
     deleteDepiction () {
       this.$emit('delete', this.depiction)
-    },
-    checkEdit() {
-      if(this.editing) {
-        if(window.confirm('You have unsaved changes and they will be lost. Are you sure you want to close?')) {
-          this.viewMode = false
-        }
-      } else {
-        this.viewMode = false
-      }
     }
   }
 }
