42a6fefe2f713b4e35ac0596d761204c1da8a01d
José Luis Pereira <pepelui@outlook.com>
2019-12-13 22:26:48 -0300

    Invert pill colors

--- app/helpers/controlled_vocabulary_terms_helper.rb
+++ app/helpers/controlled_vocabulary_terms_helper.rb
@@ -10,10 +10,10 @@ module ControlledVocabularyTermsHelper
     return nil if controlled_vocabulary_term.nil?
     content_tag(
       :span,
-      color_tag(controlled_vocabulary_term.css_color, controlled_vocabulary_term.name),
+      content_tag(:span, controlled_vocabulary_term.name),
       title: controlled_vocabulary_term.definition,
       class: ['pill', controlled_vocabulary_term.type.tableize.singularize],
-      style: ( controlled_vocabulary_term.css_color ? "background-color: #{ controlled_vocabulary_term.css_color};" : nil ),
+      style: ( controlled_vocabulary_term.css_color ? "background-color: #{ controlled_vocabulary_term.css_color}; color: #{ controlled_vocabulary_term.css_color}" : nil ),
       data: { 'global-id' => controlled_vocabulary_term.metamorphosize.to_global_id.to_s } )
   end
 
