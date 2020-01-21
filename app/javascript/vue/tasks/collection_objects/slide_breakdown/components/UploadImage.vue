8e7a1d22560b2f412063b19df02c3422167be7c1
José Luis Pereira <pepelui@outlook.com>
2020-01-21 18:37:12 -0300

    added metadata param

--- app/javascript/vue/tasks/collection_objects/slide_breakdown/components/UploadImage.vue
+++ app/javascript/vue/tasks/collection_objects/slide_breakdown/components/UploadImage.vue
@@ -3,6 +3,7 @@
     <dropzone
       class="dropzone-card separate-bottom"
       @vdropzone-success="success"
+      @vdropzone-sending="sending"
       ref="imageDropzone"
       id="image-dropzone"
       url="/images"
@@ -38,7 +39,10 @@ export default {
     success (file, response) {
       this.$refs.imageDropzone.removeFile(file)
       this.$emit('created',response)
-    }
+    },
+    sending: function (file, xhr, formData) {
+      formData.append('image[sled_image_attributes][metadata]', JSON.stringify([]))
+    },
   }
 }
 </script>
