547f0f8b4bc1463652e119d222dca37f8881d77f
mjy <diapriid@gmail.com>
2020-01-28 15:00:42 -0600

    add note

--- app/controllers/tasks/exports/coldp_controller.rb
+++ app/controllers/tasks/exports/coldp_controller.rb
@@ -8,10 +8,12 @@ class Tasks::Exports::ColdpController < ApplicationController
     redirect_to export_coldp_task_path, notice: 'Nothing selected' and return unless !params[:otu_id].blank?
     @otu = Otu.where(project_id: sessions_current_project_id).find(params.require(:otu_id))
     if @otu.taxon_name
-      download = ::Export::Coldp.download_async(@otu, request.url)
+      # !! Currently, in development, switch `download_async` to `download` !!
+      download = ::Export::Coldp.async_download(@otu, request.url)
       redirect_to download_file_download_path(download)
     else
       redirect_to export_coldp_task_path, notice: 'Please select an OTU that is linked to the nomenclature (has a taxon name).'
     end
   end
+
 end
