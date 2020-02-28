384b2093d73a06457d2979f7c1fa66222debf00d
José Luis Pereira <pepelui@outlook.com>
2020-02-28 15:26:30 -0300

    Added Italics button #1251

--- app/javascript/vue/tasks/sources/new_source/components/bibtex/title.vue
+++ app/javascript/vue/tasks/sources/new_source/components/bibtex/title.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="field">
     <label>Title</label><br>
+    <button
+      type="button"
+      @click="setItalics">
+      <i>Italics</i>
+    </button>
     <textarea 
       id="title"
+      ref="title"
       v-model="source.title"></textarea>
+
   </div>
 </template>
 
@@ -20,6 +27,19 @@ export default {
       },
       set (value) {
         this.$store.commit(MutationNames.SetSource, value)
+      }
+    }
+  },
+  methods: {
+    setItalics() {
+      let start = this.$refs.title.selectionStart
+      let end = this.$refs.title.selectionEnd
+      let title = this.source.title
+
+      if(end === start) {
+        this.source.title = title.slice(0, start) + '<i></i>' + title.slice(end)
+      } else {
+        this.source.title = title.slice(0, start) + '<i>' + title.slice(start, end) + '</i>' + title.slice(end)
       }
     }
   }
