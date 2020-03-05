ef7e13584d527183527988ebd5398b51478e2675
Matt <diapriid@gmail.com>
2020-03-05 15:44:21 -0600

    Update controlled_vocabulary_term_types.js
    Add definitions

--- app/javascript/vue/tasks/controlled_vocabularies/manage/constants/controlled_vocabulary_term_types.js
+++ app/javascript/vue/tasks/controlled_vocabularies/manage/constants/controlled_vocabulary_term_types.js
@@ -1,8 +1,8 @@
 export default {
   Keyword: 'A keyword is the value used in tag',
-  Topic: 'Help tip',
-  Predicate: 'Help tip',
-  ConfidenceLevel: 'Help tip',
-  BiologicalGroup: 'Help tip',
-  BiocurationClass: 'Help tip'
+  Topic: 'A topic is used to classify what is in a citation or a block of text content.',
+  Predicate: 'A predicate is the label for a custom field/attribute.',
+  ConfidenceLevel: 'A confidence level is a curator defined status that pertains to their confidence in some quality of the data it is applied to.',
+  BiologicalGroup: 'A biological group groups biocurations classes, like "Life Stage" or "Sex."',
+  BiocurationClass: 'A biocuration class is a biological property that curators use to describe their specimens, like Female, or adult.'
 }
