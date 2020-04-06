8f844adc45dbd8a61d3b25c22ce0e2e01c9812e4
José Luis Pereira <pepelui@outlook.com>
2020-04-06 17:54:28 -0300

    Clipboard tweak

--- app/javascript/vue/tasks/clipboard/request/resources.js
+++ app/javascript/vue/tasks/clipboard/request/resources.js
@@ -1,34 +1,6 @@
-import Vue from 'vue'
-import VueResource from 'vue-resource'
-
-Vue.use(VueResource)
-
-const ajaxCall = function (type, url, data = null) {
-  Vue.http.headers.common['X-CSRF-Token'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
-  return new Promise(function (resolve, reject) {
-    Vue.http[type](url, data).then(response => {
-      return resolve(response.body)
-    }, response => {
-      handleError(response.body)
-      return reject(response)
-    })
-  })
-}
-
-const handleError = function (json) {
-  if (typeof json !== 'object') return
-  let errors = Object.keys(json)
-  let errorMessage = ''
-
-  errors.forEach(function (item) {
-    errorMessage += json[item].join('<br>')
-  })
-
-  TW.workbench.alert.create(errorMessage, 'error')
-}
+import ajaxCall from 'helpers/ajaxCall'
 
 const UpdateClipboard = function (clipboard) {
-  //project_member[clipboard]={}
   return ajaxCall('put', `/project_members/update_clipboard.json`, { project_member: { clipboard: clipboard } })
 }
 
