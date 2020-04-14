8a8c10199c3dcad01913b4a9aa1bd65edddee740
José Luis Pereira <pepelui@outlook.com>
2020-04-14 11:26:39 -0300

    Fix table style

--- app/javascript/vue/components/table_list.vue
+++ app/javascript/vue/components/table_list.vue
@@ -18,19 +18,21 @@
           <td
             v-for="attr in attributes"
             v-html="getValue(item, attr)"/>
-          <td class="vue-table-options">
-            <radial-annotator
-              v-if="annotator"
-              :global-id="item.global_id"/>
-            <span
-              v-if="edit"
-              class="circle-button btn-edit"
-              @click="$emit('edit', Object.assign({}, item))"/>
-            <span
-              v-if="destroy"
-              class="circle-button btn-delete"
-              @click="deleteItem(item)">Remove
-            </span>
+          <td>
+            <div class="horizontal-left-content">
+              <radial-annotator
+                v-if="annotator"
+                :global-id="item.global_id"/>
+              <span
+                v-if="edit"
+                class="circle-button btn-edit"
+                @click="$emit('edit', Object.assign({}, item))"/>
+              <span
+                v-if="destroy"
+                class="circle-button btn-delete"
+                @click="deleteItem(item)">Remove
+              </span>
+            </div>
           </td>
         </tr>
       </transition-group>
