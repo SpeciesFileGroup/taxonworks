2ecbdf2952d2c943258bbf367b51c14a9c6768f3
José Luis Pereira <pepelui@outlook.com>
2020-04-15 19:23:58 -0300

    Moved checkbox

---
+++ app/javascript/vue/tasks/collection_objects/slide_breakdown/components/Assign/Metadata.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p>
+      <label>
+        <input
+          type="checkbox"
+          v-model="identifier.is_metadata_depiction">
+          Is metadata depiction
+      </label>
+    </p>
+  </div>
+</template>
+
+<script>
+import { GetterNames } from '../../store/getters/getters'
+import { MutationNames } from '../../store/mutations/mutations'
+
+export default {
+  computed: {
+    identifier: {
+      get () {
+        return this.$store.getters[GetterNames.GetIdentifier]
+      },
+      set (value) {
+        this.$store.commit(MutationNames.SetIdentifier, value)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
