7265fb84cd9aceb7c66b2e3bd65937d22aecac1c
José Luis Pereira <pepelui@outlook.com>
2020-04-23 13:17:42 -0300

    Change to current parent id #1045

--- app/javascript/vue/tasks/nomenclature/new_combination/components/placement.vue
+++ app/javascript/vue/tasks/nomenclature/new_combination/components/placement.vue
@@ -9,7 +9,7 @@
 </template>
 <script>
 
-import { CreatePlacement, GetTaxonName } from '../request/resources'
+import { CreatePlacement } from '../request/resources'
 
 export default {
   props: {
@@ -26,15 +26,15 @@ export default {
   },
   mounted () {
     let protonyms = Object.values(this.combination.protonyms)
-      this.taxon = protonyms.find(item => { return item.id == this.combination.placement.target_id })
-      this.parent = protonyms.find(item => { return item.id == this.combination.placement.new_parent_id })
+    this.taxon = protonyms.find(item => { return item.id == this.combination.placement.target_id })
+    this.parent = protonyms.find(item => { return item.id == this.combination.placement.current_parent_id })
   },
   methods: {
     create () {
       let data = {
         taxon_name: {
           id: this.combination.placement.target_id,
-          parent_id: this.combination.placement.new_parent_id
+          parent_id: this.combination.placement.current_parent_id
         }
       }
       CreatePlacement(this.combination.placement.target_id, data).then(response => {
