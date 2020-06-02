d1c181c98e79917fd522b70c9601d44e2f6bdfa8
José Luis Pereira <pepelui@outlook.com>
2020-06-01 23:27:25 -0300

    Tweak

--- app/javascript/vue/components/georeferences/map.vue
+++ app/javascript/vue/components/georeferences/map.vue
@@ -340,10 +340,9 @@ export default {
       return color
     },
     generateHue (index) {
-      index++
       const PHI = (1 + Math.sqrt(5)) / 2
       const n = index * PHI - Math.floor(index * PHI)
-      return `hsl(${Math.floor(n * 256)}, ${Math.floor(n * 70) + 40}% , ${(Math.floor((n) + 1) * 60) + 10}%)`
+      return `hsl(${Math.floor(n * 256)}, ${Math.floor(n * 50) + 100}% , ${(Math.floor((n) + 1) * 60) + 10}%)`
     },
     defaultShapeStyle () {
       return {
