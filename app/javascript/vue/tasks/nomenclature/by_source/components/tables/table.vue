f4b5023ee07e7f93a215ae20c66f15f35870c2d1
José Luis Pereira <pepelui@outlook.com>
2020-06-08 14:56:48 -0300

    Fixed sort by page #1536

--- app/javascript/vue/tasks/nomenclature/by_source/components/tables/table.vue
+++ app/javascript/vue/tasks/nomenclature/by_source/components/tables/table.vue
@@ -1,51 +1,29 @@
 <template>
   <table>
     <tr>
-      <th @click="sortTable('pages')">Pages</th>
+      <th @click="sortPages()">Pages</th>
       <th>Is original</th>
       <th @click="sortTable('citation_object.object_tag')">Object</th>
       <th>Radial</th>
       <th>Delete</th>
     </tr>
-    <row-components 
+    <row-components
       v-for="item in list"
       :key="item.id"
       @delete="removeCitation"
       :citation="item"/>
-  </table>  
+  </table>
 </template>
 
 <script>
 
 import RowComponents from './row_components'
-import SortArray from 'helpers/sortArray'
+import extendedTable from './extendedTable'
 
 export default {
+  mixins: [extendedTable],
   components: {
     RowComponents
-  },
-  props: {
-    list: {
-      type: Array,
-      required: true
-    }
-  },
-  data () {
-    return {
-      ascending: false
-    }
-  },
-  methods: {
-    removeCitation(citation) {
-      let index = this.list.findIndex((item) => { return citation.id == item.id })
-      if(index > -1) {
-        this.list.splice(index, 1)
-      }
-    },
-    sortTable (sortProperty) {
-      this.list = SortArray(sortProperty, this.list, this.ascending)
-      this.ascending = !this.ascending
-    }
   }
 }
 </script>
