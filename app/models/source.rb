a6b209d59b0808c90299efa226e7e19085e4afac
Dmitry Dmitriev <arboridia@gmail.com>
2020-06-25 15:23:06 -0500

    source. remove .order(name:)

--- app/models/source.rb
+++ app/models/source.rb
@@ -320,9 +320,9 @@ class Source < ApplicationRecord
             (Source.where(created_by_id: user_id, updated_at: 2.hours.ago..Time.now )
                 .order('created_at DESC')
                 .limit(5).order(:cached).to_a +
-            Source.where('"sources"."id" IN (?)', r.first(6) ).order(:name).to_a).uniq
+            Source.where('"sources"."id" IN (?)', r.first(6) ).to_a).uniq
         h[:quick] = ( Source.pinned_by(user_id).pinboard_inserted.where(pinboard_items: {project_id: project_id}).to_a
-        + Source.where('"sources"."id" IN (?)', r.first(4) ).order(:name).to_a).uniq
+        + Source.where('"sources"."id" IN (?)', r.first(4) ).to_a).uniq
     end
 
     h
