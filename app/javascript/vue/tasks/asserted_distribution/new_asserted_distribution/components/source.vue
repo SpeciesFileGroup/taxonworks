f0b8b0af72e10b39a09c522a136bb090a756b33e
José Luis Pereira <pepelui@outlook.com>
2020-06-25 18:06:20 -0300

    Moved selected source

--- app/javascript/vue/tasks/asserted_distribution/new_asserted_distribution/components/source.vue
+++ app/javascript/vue/tasks/asserted_distribution/new_asserted_distribution/components/source.vue
@@ -9,44 +9,45 @@
       pin-section="Sources"
       pin-type="Source"
       v-model="assertedDistribution.citation.source">
-      <div
-        slot="body"
-        class="horizontal-left-content middle margin-medium-bottom">
-        <label class="margin-small-right">
-          <input
-            class="pages"
-            v-model="assertedDistribution.citation.pages"
-            placeholder="Pages"
-            type="text">
-        </label>
-        <ul class="no_bullets context-menu">
-          <li>
-            <label>
-              <input
-                type="checkbox"
-                v-model="assertedDistribution.citation.is_original">
-              Is original
-            </label>
-          </li>
-          <li>
-            <label>
-              <input
-                v-model="assertedDistribution.is_absent"
-                type="checkbox">
-              Is absent
-            </label>
-          </li>
-        </ul>
+      <div slot="body">
+        <div
+          class="horizontal-left-content middle margin-medium-bottom">
+          <label class="margin-small-right">
+            <input
+              class="pages"
+              v-model="assertedDistribution.citation.pages"
+              placeholder="Pages"
+              type="text">
+          </label>
+          <ul class="no_bullets context-menu">
+            <li>
+              <label>
+                <input
+                  type="checkbox"
+                  v-model="assertedDistribution.citation.is_original">
+                Is original
+              </label>
+            </li>
+            <li>
+              <label>
+                <input
+                  v-model="assertedDistribution.is_absent"
+                  type="checkbox">
+                Is absent
+              </label>
+            </li>
+          </ul>
+        </div>
+        <template v-if="assertedDistribution.citation.source">
+          <p class="horizontal-left-content">
+            <span data-icon="ok"/>
+            <span v-html="assertedDistribution.citation.source.object_tag"/>
+            <span
+              class="button circle-button btn-undo button-default"
+              @click="unset"/>
+          </p>
+        </template>
       </div>
-      <template v-if="assertedDistribution.citation.source">
-        <p class="horizontal-left-content">
-          <span data-icon="ok"/>
-          <span v-html="assertedDistribution.citation.source.object_tag"/>
-          <span
-            class="button circle-button btn-undo button-default"
-            @click="unset"/>
-        </p>
-      </template>
     </smart-selector>
   </fieldset>
 </template>
