5dca3c8a9c25ef7b9f2307e668270889cba22413
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2020-07-07 14:47:23 -0300

    Added list of DwC-A datasets in index response

--- app/controllers/tasks/dwca_import/dwca_import_controller.rb
+++ app/controllers/tasks/dwca_import/dwca_import_controller.rb
@@ -3,6 +3,12 @@ class Tasks::DwcaImport::DwcaImportController < ApplicationController
 
   # GET
   def index
+    respond_to do |format|
+      format.html
+      format.json {
+        @datasets = ImportDataset::DwcChecklist.all.order(:updated_at, :description).page(params[:page]).per(params[:per] || 25)
+      }
+    end
   end
 
   # POST
---
+++ app/views/tasks/dwca_import/dwca_import/index.json.jbuilder
@@ -0,0 +1,7 @@
+json.array!(@datasets) do |dataset|
+  json.id dataset.id
+  json.description dataset.description
+  json.type dataset.type
+  json.created_at "#{time_ago_in_words(dataset.created_at)} ago"
+  json.updated_at "#{time_ago_in_words(dataset.dataset_records.maximum(:updated_at))} ago"
+end
