166b49b2533e970f3537aa90e839ebd55709b920
jrflood <jrflood@illinois.edu>
2020-08-19 11:18:56 -0500

    move sources, people inside user_authenticated

--- config/routes/api_v1.rb
+++ config/routes/api_v1.rb
@@ -8,17 +8,17 @@ namespace :api, defaults: {format: :json} do
     get :ping, controller: 'ping'
     get :pingz, controller: 'ping'
 
-    get '/sources', to: '/sources#api_index'
-    get '/sources/:id', to: '/sources#api_show'
-    get '/sources/autocomplete', to: '/sources#autocomplete'
-
-    get '/people', to: '/people#api_index'
-    get '/people/:id', to: '/people#api_show'
-    get '/people/autocomplete', to: '/people#autocomplete'
-
     # authenticated by user_token
     defaults authenticate_user: true do
       get '/user_authenticated', to: 'base#index'
+
+      get '/sources', to: '/sources#api_index'
+      get '/sources/:id', to: '/sources#api_show'
+      get '/sources/autocomplete', to: '/sources#autocomplete'
+
+      get '/people', to: '/people#api_index'
+      get '/people/:id', to: '/people#api_show'
+      get '/people/autocomplete', to: '/people#autocomplete'
     end
 
     # authenticated by project token
