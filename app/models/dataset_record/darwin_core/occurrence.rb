4cdc7725f1f6d402d1599cb1ef3bd79641650261
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2020-09-09 13:53:45 -0300

    Disable DwC occurrence indexing (VERY slow)

--- app/models/dataset_record/darwin_core/occurrence.rb
+++ app/models/dataset_record/darwin_core/occurrence.rb
@@ -39,7 +39,8 @@ class DatasetRecord::DarwinCore::Occurrence < DatasetRecord
         otu = parent.otus.first # TODO: Might require select-and-confirm functionality
 
         specimen = Specimen.create!({
-          total: get_field_value("individualCount") || 1
+          total: get_field_value("individualCount") || 1,
+          no_dwc_occurrence: true
         })
 
         determiners = parse_identifiedBy.map! { |n| Person.find_by(n) || Person::Unvetted.create!(n) }
