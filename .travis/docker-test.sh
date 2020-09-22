853794df183fe6ba3034835cde90acb7d572bc3c
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2020-09-22 11:27:04 -0300

    Fix

--- .travis/docker-test.sh
+++ .travis/docker-test.sh
@@ -4,9 +4,9 @@ cd "$(dirname "$0")"
 set -e
 set -x
 
-docker build .. -t sfgrp/taxonworks --build-arg REVISION=$(git rev-parse --short HEAD) --build-arg BUNDLER_WORKERS=3
-
 export REVISION=$(git rev-parse HEAD | cut -c1-9)
+
+docker build .. -t sfgrp/taxonworks --build-arg REVISION=$REVISION --build-arg BUNDLER_WORKERS=3
 
 for ver in `echo 10 12`; do
   export PG_VERSION=$ver
