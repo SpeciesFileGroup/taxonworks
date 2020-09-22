137a4ccb25005955360e725cc7f8837e78619594
Dmitry Dmitriev <arboridia@gmail.com>
2020-09-22 12:32:01 -0500

    interactive_key

--- lib/interactive_key.rb
+++ lib/interactive_key.rb
@@ -333,7 +333,7 @@ class InteractiveKey
         h[r_value[:object_at_rank]] = true if h[r_value[:object_at_rank]].nil?
       end
     end
-    return h
+    return h.keys
   end
 
   def eliminated_taxa
@@ -349,7 +349,7 @@ class InteractiveKey
         end
       end
     end
-    return h
+    return h.keys
   end
 
   def useful_descriptors
