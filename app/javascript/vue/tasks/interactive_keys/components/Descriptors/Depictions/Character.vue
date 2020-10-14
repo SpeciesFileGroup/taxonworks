1c9bbe3940d3f7d9684fdc0c00f9c4d2274e3327
José Luis Pereira <pepelui@outlook.com>
2020-10-13 23:08:09 -0300

    Added opacity

--- app/javascript/vue/tasks/interactive_keys/components/Descriptors/Depictions/Character.vue
+++ app/javascript/vue/tasks/interactive_keys/components/Descriptors/Depictions/Character.vue
@@ -1,19 +1,21 @@
 <template>
-  <div class="padding-small align-end">
+  <div
+    class="padding-small align-end">
     <label class="flex-separate  flex-wrap-column">
       <div
+        :style="{ opacity: characterState.status === 'useless' ? 0.3 : 1 }"
         v-for="depiction in depictions"
         :key="depiction.id">
         <img
           class="full_width"
           :src="depiction.image.alternatives.medium.image_file_url"/>
       </div>
-      <label :class="{ subtle: characterState.status === 'useless' }">
+      <label>
         <input
           type="checkbox"
           :value="characterState.id"
           v-model="selected">
-        {{ characterState.name }} ({{ characterState.number_of_objects }})
+        <span v-if="characterState.status === 'useless'">-</span> {{ characterState.name }} ({{ characterState.number_of_objects }})
       </label>
     </label>
   </div>
