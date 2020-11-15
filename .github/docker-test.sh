cb8b89b02b7fa2df220c92d3e5ad5504bb011f99
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2020-11-15 12:10:34 -0300

    Added deliver workflow

--- .github/docker-test.sh
+++ .github/docker-test.sh
@@ -4,9 +4,7 @@ cd "$(dirname "$0")"
 set -e
 set -x
 
-export REVISION=$(git rev-parse HEAD | cut -c1-9)
-
-docker build .. -t sfgrp/taxonworks --build-arg REVISION=$REVISION --build-arg BUNDLER_WORKERS=3
+docker build .. -t sfgrp/taxonworks --build-arg REVISION=$(echo $GITHUB_SHA | cut -c1-9) --build-arg BUNDLER_WORKERS=3
 
 for ver in `echo 10 12`; do
   export PG_VERSION=$ver
