706d3079b4a727f98b3872d9cdc77f9ce623e0df
José Luis Pereira <pepelui@outlook.com>
2020-12-11 17:57:47 -0300

    Changes on scalebar facet

--- app/javascript/vue/tasks/images/new_image/components/pixelsUnit.vue
+++ app/javascript/vue/tasks/images/new_image/components/pixelsUnit.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="panel content panel-section">
-    <h2>Pixels to centimeters</h2>
-    <div class="horizontal-left-content">
+    <h2 class="margin-remove-bottom">Scalebar</h2>
+    <span>Note: Will be converted to pixels per centimeters.</span>
+    <div class="horizontal-left-content margin-medium-top">
       <div class="field label-above">
         <label>Pixels</label>
         <input
           type="text"
           v-model="pixels"
+          @input="pixelsToUnit"
         >
       </div>
       <div class="field margin-small-left margin-small-right label-above">
@@ -14,14 +16,14 @@
         <label class="label-above">=</label>
       </div>
       <div class="field label-above">
-        <label>&nbsp;</label>
+        <label>Value</label>
         <input
           type="text"
-          :value="pixelValue"
-          disabled>
+          @input="unitToPixels"
+          v-model="unitValue">
       </div>
       <div class="field label-above">
-        <label>&nbsp;</label>
+        <label>Unit</label>
         <select v-model="selected">
           <option
             v-for="(item, key) in pxToUnits"
@@ -51,6 +53,19 @@ import { GetterNames } from '../store/getters/getters'
 import { MutationNames } from '../store/mutations/mutations'
 import { ActionNames } from '../store/actions/actions'
 
+const pxToUnits = {
+  mm: 0.2645833333,
+  cm: 0.0264583333,
+  um: 264.5833,
+  nm: 264583.3,
+  m: 0.0002645833,
+  in: 0.0104166667,
+  ft: 0.0008680556,
+  mi: 1.6440444056709E-7,
+  nmi: 1.4286355291577E-7,
+  P: 0.062499992175197
+}
+
 export default {
   computed: {
     pixelsToCm: {
@@ -67,30 +82,36 @@ export default {
   },
   data () {
     return {
-      pxToUnits: {
-        mm: 0.2645833333,
-        cm: 0.0264583333,
-        um: 264.5833,
-        nm: 264583.3,
-        m: 0.0002645833,
-        in: 0.0104166667,
-        ft: 0.0008680556,
-        mi: 1.6440444056709E-7,
-        nmi: 1.4286355291577E-7,
-        P: 0.062499992175197
-      },
-      selected: undefined,
-      pixels: null
+      pxToUnits: pxToUnits,
+      selected: pxToUnits.mm,
+      pixels: null,
+      unitValue: null,
+      isUnitLastSet: false
     }
   },
   watch: {
     pixelValue (newVal) {
       this.pixelsToCm = this.pixels ? this.pixels * this.pxToUnits.cm : null
+    },
+    selected () {
+      if (this.isUnitLastSet) {
+        this.unitToPixels()
+      } else {
+        this.pixelsToUnit()
+      }
     }
   },
   methods: {
     apply () {
       this.$store.dispatch(ActionNames.ApplyPixelToCentimeter)
+    },
+    pixelsToUnit () {
+      this.isUnitLastSet = false
+      this.unitValue = this.pixelValue
+    },
+    unitToPixels () {
+      this.isUnitLastSet = true
+      this.pixels = this.unitValue ? this.unitValue / this.selected : null
     }
   }
 }
