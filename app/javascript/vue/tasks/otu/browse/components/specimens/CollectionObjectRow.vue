c1ca7ba6f451c4f1cc054b714ed8a6b28cdf893d
José Luis Pereira <pepelui@outlook.com>
2021-01-31 21:10:00 -0300

    Layout and css changes

--- app/javascript/vue/tasks/otu/browse/components/specimens/CollectionObjectRow.vue
+++ app/javascript/vue/tasks/otu/browse/components/specimens/CollectionObjectRow.vue
@@ -5,11 +5,12 @@
       class="cursor-pointer inline">
       <div
         :data-icon="expand ? 'w_less' : 'w_plus'"
-        class="expand-box button-default separate-right"/>
+        class="expand-box button-circle button-default separate-right"/>
       <span>{{ ceLabel }}</span>
     </div>
     <template v-if="expand">
       <specimen-information
+        class="species-information-container"
         v-if="expand"
         :specimen="specimen"/>
     </template>
@@ -47,3 +48,8 @@ export default {
   }
 }
 </script>
+<style scoped>
+  .species-information-container {
+    margin-left: 20px;
+  }
+</style>
--- app/javascript/vue/tasks/otu/browse/components/specimens/TypeInformation.vue
+++ app/javascript/vue/tasks/otu/browse/components/specimens/TypeInformation.vue
@@ -5,16 +5,18 @@
       class="cursor-pointer inline">
       <div
         :data-icon="expand ? 'w_less' : 'w_plus'"
-        class="expand-box button-default separate-right"/>
+        class="expand-box circle-button button-default separate-right"/>
       <span>[<span v-html="types.map(type => `${type.type_type} of ${type.original_combination}`).join('; ')"/>] <span>{{ ceLabel }}</span></span>
     </div>
     <template v-if="expand">
       <type-data
+        class="species-information-container"
         v-for="type in types"
         :key="type.id"
         :type="type"/>
       <specimen-information
         v-if="expand"
+        class="species-information-container"
         :specimen="specimen"/>
     </template>
   </div>
@@ -57,3 +59,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  .species-information-container {
+    margin-left: 20px;
+  }
+</style>
