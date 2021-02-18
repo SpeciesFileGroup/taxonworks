954d8c3e86b67557a9497f8af90d0ad988623eab
José Luis Pereira <pepelui@outlook.com>
2021-02-18 16:15:25 -0300

    Fix #2044

--- app/javascript/vue/tasks/loan/store/actions/createBatchLoad.js
+++ app/javascript/vue/tasks/loan/store/actions/createBatchLoad.js
@@ -1,14 +1,14 @@
 import { MutationNames } from '../mutations/mutations'
 import { createBatchLoad } from '../../request/resources'
 
-export default function ({ commit, state }, object) {
+export default function ({ commit }, object) {
   commit(MutationNames.SetLoading, true)
   createBatchLoad(object).then(response => {
     if (Array.isArray(response.body)) {
-      response.forEach(function (element) {
+      response.body.forEach((element) => {
         commit(MutationNames.AddLoanItem, element)
       })
     }
     commit(MutationNames.SetLoading, false)
   })
-};
+}
