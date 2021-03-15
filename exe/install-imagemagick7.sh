d4c3b1b211cbc227072d0966e8b8067577e277a5
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2021-03-15 15:10:17 -0300

    ImageMagick install tweaks

--- exe/install-imagemagick7.sh
+++ exe/install-imagemagick7.sh
@@ -5,32 +5,35 @@ apt-get update
 apt-get build-dep -y libmagickcore-dev
 
 cd /usr/src/
+
+[ ! -d libde265-* ] && curl -sL $(curl -s https://api.github.com/repos/strukturag/libde265/releases/latest | jq --raw-output '.assets[0] | .browser_download_url') | tar xzf - && \
+  cd libde265-* && \
+  ./autogen.sh && \
+  ./configure && \
+  cd ..
+cd libde265-*
+make -j${MAKE_JOBS-3}
+make install
+cd ..
+
 [ ! -d libheif-* ] && \
   curl -sL $(curl -s https://api.github.com/repos/strukturag/libheif/releases/latest | jq --raw-output '.assets[0] | .browser_download_url') | tar xzf - && \
   cd libheif-* && \
   ./autogen.sh && \
   ./configure && \
   cd ..
-[ ! -d libde265-* ] && curl -sL $(curl -s https://api.github.com/repos/strukturag/libde265/releases/latest | jq --raw-output '.assets[0] | .browser_download_url') | tar xzf - && \
-  cd libde265-* && \
-  ./autogen.sh && \
-  ./configure && \
-  cd ..
+cd libheif-*
+make -j${MAKE_JOBS-3}
+make install
+cd ..
+
 [ ! -d ImageMagick-7* ] && curl -sL https://www.imagemagick.org/download/ImageMagick.tar.gz | tar xzf - && \
   cd ImageMagick-7*
   ./configure --with-modules=yes --with-heic=yes && \
   cd ..
-
-cd libde265-*
+cd ImageMagick-7*
 make -j${MAKE_JOBS-3}
 make install
-
-cd ../libheif-*
-make -j${MAKE_JOBS-3}
-make install
-
-cd ../ImageMagick-7*
-make -j${MAKE_JOBS-3}
-make install
+cd ..
 
 ldconfig
