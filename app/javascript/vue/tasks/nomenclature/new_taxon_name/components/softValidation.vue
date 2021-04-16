4def2625d94ccf81c97f3a14d5edf42d45505b87
José Luis Pereira <pepelui@outlook.com>
2021-04-16 11:34:00 -0300

    Added wrench icon into soft validation panel

--- app/javascript/vue/tasks/nomenclature/new_taxon_name/components/softValidation.vue
+++ app/javascript/vue/tasks/nomenclature/new_taxon_name/components/softValidation.vue
@@ -20,9 +20,7 @@
             type="button"
             class="button button-submit margin-small-left"
             @click="runFix(getFixPresent(errors[key].list))">
-            <span
-              class="small-icon"
-              data-icon="w_wrench">Fix all</span>
+            <span>Fix all</span>
           </button>
         </h3>
         <hr>
@@ -30,27 +28,21 @@
           v-for="list in errors[key].list"
           class="no_bullets">
           <li
-            class="flex-separate"
             v-for="error in list.soft_validations">
-            <div> 
-              <span data-icon="warning"/>
-              <span
-                v-html="error.message"
-                :title="error.description"/>
-              <span
-                v-if="error.resolution.length"
-                v-html="`[${error.resolution.map((path, index) => `<a href='${path}'>${index}</a>`).join(', ')}]`"/>
-            </div>
-            <div>
-              <button
-                v-if="error.fixable"
-                type="button"
-                data-icon="w_wrench"
-                class="button button-submit button-circle small-icon"
-                @click="runFix([{ global_id: list.global_id, only_methods: [error.soft_validation_method] }])">
-                Fix
-              </button>
-            </div>
+            <span data-icon="warning"/>
+            <button
+              v-if="error.fixable"
+              type="button"
+              class="button button-submit"
+              @click="runFix([{ global_id: list.global_id, only_methods: [error.soft_validation_method] }])">
+              Fix
+            </button>
+            <span
+              v-html="error.message"
+              :title="error.description"/>
+            <span
+              v-if="error.resolution.length"
+              v-html="`[${error.resolution.map((path, index) => `<a href='${path}'><span title='Fixable here (may leave page)' class='small-icon icon-without-space' data-icon='blue_wrench'/></a>`).join(', ')}]`"/>
           </li>
         </ul>
       </div>
