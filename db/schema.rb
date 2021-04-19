9e5315cae8e9e44932dcfad85d3f46b63fd37fb8
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2021-04-19 14:37:24 -0300

    Enable tablefunc extension (refs #2189)

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_18_165155) do
+ActiveRecord::Schema.define(version: 2021_04_19_173135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
   enable_extension "hstore"
   enable_extension "plpgsql"
   enable_extension "postgis"
+  enable_extension "tablefunc"
 
   create_table "alternate_values", id: :serial, force: :cascade do |t|
     t.text "value", null: false
@@ -804,18 +805,20 @@ ActiveRecord::Schema.define(version: 2021_04_18_165155) do
   end
 
   create_table "extracts", id: :serial, force: :cascade do |t|
+    t.decimal "quantity_value", null: false
+    t.string "quantity_unit", null: false
     t.string "verbatim_anatomical_origin"
-    t.integer "year_made"
-    t.integer "month_made"
-    t.integer "day_made"
+    t.integer "year_made", null: false
+    t.integer "month_made", null: false
+    t.integer "day_made", null: false
     t.integer "created_by_id", null: false
     t.integer "updated_by_id", null: false
     t.integer "project_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "repository_id"
+    t.decimal "concentration_value"
+    t.string "concentration_unit"
     t.index ["project_id"], name: "index_extracts_on_project_id"
-    t.index ["repository_id"], name: "index_extracts_on_repository_id"
   end
 
   create_table "gene_attributes", id: :serial, force: :cascade do |t|
@@ -2020,7 +2023,6 @@ ActiveRecord::Schema.define(version: 2021_04_18_165155) do
   add_foreign_key "dwc_occurrences", "users", column: "created_by_id"
   add_foreign_key "dwc_occurrences", "users", column: "updated_by_id"
   add_foreign_key "extracts", "projects"
-  add_foreign_key "extracts", "repositories"
   add_foreign_key "extracts", "users", column: "created_by_id"
   add_foreign_key "extracts", "users", column: "updated_by_id"
   add_foreign_key "gene_attributes", "controlled_vocabulary_terms"
