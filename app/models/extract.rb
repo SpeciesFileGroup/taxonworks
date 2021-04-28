9f427f7a62609e554636cf806283e5ba46b74df4
mjy <diapriid@gmail.com>
2021-04-28 15:07:12 -0500

    tweak Extract roles

--- app/models/extract.rb
+++ app/models/extract.rb
@@ -36,7 +36,7 @@ class Extract < ApplicationRecord
   has_many :sequences, through: :origin_relationships, source: :new_object, source_type: 'Sequence'
 
   has_many :extractor_roles, -> { order('roles.position ASC') }, class_name: 'Extractor', as: :role_object, dependent: :destroy, validate: true
-  has_many :extractors, through: :extractor_roles, source: :person
+  has_many :extractors, -> { order('roles.position ASC') }, through: :extractor_roles, source: :person, validate: true
 
   # validates_presence_of :quantity_value
   attr_accessor :is_made_now
