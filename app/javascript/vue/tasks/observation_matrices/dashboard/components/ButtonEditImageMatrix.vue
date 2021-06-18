3d2fe195c1a5be7dd92ffd1e38820506c17abac0
José Luis Pereira <pepelui@outlook.com>
2021-06-18 14:07:29 -0300

    Added default pinned button and matrix selector in observation matrices dashboard

--- app/javascript/vue/tasks/observation_matrices/dashboard/components/ButtonEditImageMatrix.vue
+++ app/javascript/vue/tasks/observation_matrices/dashboard/components/ButtonEditImageMatrix.vue
@@ -1,18 +1,60 @@
 <template>
-  <button
-    class="button normal-input button-default"
-    @click="openImageMatrix"
-    :disabled="!otuIds.length"
-  >
-    Edit image matrix
-  </button>
+  <div>
+    <button
+      class="button normal-input button-default"
+      @click="showModal = true"
+      :disabled="!otuIds.length"
+    >
+      Edit image matrix
+    </button>
+    <modal-component
+      v-if="showModal"
+      :container-style="{ width: '600px' }"
+      @close="showModal = false">
+      <template #header>
+        <h3>Select a matrix</h3>
+      </template>
+      <template #body>
+        <spinner-component
+          v-if="isLoading"
+          legend="Loading..."/>
+        <pin-component
+          class="button-circle"
+          type="ObservationMatrix"
+          @getItem="openImageMatrix($event.id)"
+          section="ObservationMatrices"/>
+        <ul class="no_bullets">
+          <li
+            class="margin-small-bottom"
+            v-for="item in observationMatrices"
+            :key="item.id">
+            <button
+              class="button normal-input button-default"
+              @click="openImageMatrix(item.id)">
+              {{ item.name }}
+            </button>
+          </li>
+        </ul>
+      </template>
+    </modal-component>
+  </div>
 </template>
 
 <script>
 
 import { RouteNames } from 'routes/routes'
+import { ObservationMatrix } from 'routes/endpoints'
+import ModalComponent from 'components/ui/Modal.vue'
+import SpinnerComponent from 'components/spinner.vue'
+import PinComponent from 'components/getDefaultPin.vue'
 
 export default {
+  components: {
+    ModalComponent,
+    SpinnerComponent,
+    PinComponent
+  },
+
   props: {
     otuIds: {
       type: Array,
@@ -20,9 +62,34 @@ export default {
     }
   },
 
+  data () {
+    return {
+      isLoading: false,
+      showModal: false,
+      observationMatrices: [],
+      matrix: undefined
+    }
+  },
+
+  watch: {
+    showModal: {
+      handler (newVal) {
+        if (newVal) {
+          this.isLoading = true
+          ObservationMatrix.all().then(response => {
+            this.observationMatrices = response.body
+            this.isLoading = false
+          })
+        }
+      },
+      immediate: true
+    }
+  },
+
   methods: {
-    openImageMatrix () {
-      window.open(`${RouteNames.ImageMatrix}?otu_filter=${this.otuIds.join('|')}`, '_blank')
+    openImageMatrix (matrixId) {
+      window.open(`${RouteNames.ImageMatrix}?observation_matrix_id=${matrixId}&otu_filter=${this.otuIds.join('|')}`, '_blank')
+      this.showModal = false
     }
   }
 }
