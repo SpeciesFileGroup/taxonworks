788a85b685910256a7b460338c8a83c8824d7144
Dmitry Dmitriev <arboridia@gmail.com>
2021-07-01 13:43:37 -0500

    otu_contents in observation_matrix

--- app/helpers/observation_matrices/export/otu_contents.rb
+++ app/helpers/observation_matrices/export/otu_contents.rb
@@ -5,8 +5,8 @@ module ObservationMatrices::Export::OtuContents
     m = opt[:observation_matrix]
 
     a = {}
-    otu_ids = otus.collect{|i| i.id}
-    contents = Content.select('contents.*, controlled_vocabulary_terms.name, observation_matrix_rows.id AS row_id').joins(:topic, :observation_matrix_rows).where('contents.otu_id IN (?)', otu_ids).where('observation_matrix_rows.observation_matrix_id = (?)', m.id).order(:otu_id, :topic_id)
+    otu_ids = m.otus.collect{|i| i.id}
+    contents = Content.select('contents.*, controlled_vocabulary_terms.name, observation_matrix_rows.id AS row_id').joins(:topic).joins('INNER JOIN observation_matrix_rows ON observation_matrix_rows.otu_id = contents.otu_id').where('contents.otu_id IN (?)', otu_ids).where('observation_matrix_rows.observation_matrix_id = (?)', m.id).order(:otu_id, :topic_id)
     CSV.generate do |csv|
       csv << ['otu_id', 'topic', 'text']
       contents.each do |i|
