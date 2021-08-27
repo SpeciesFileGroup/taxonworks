815ee09c30072aebb1f2c8bba543945cde96ccb6
José Luis Pereira <pepelui@outlook.com>
2021-08-27 12:37:51 -0300

    Add reindex button

--- app/javascript/vue/routes/endpoints/DwcOcurrence.js
+++ app/javascript/vue/routes/endpoints/DwcOcurrence.js
@@ -1,7 +1,9 @@
 import AjaxCall from 'helpers/ajaxCall'
 
 export const DwcOcurrence = {
-  metadata: (params) => AjaxCall('get', '/dwc_occurrences/metadata.json', { params }),
+  metadata: params => AjaxCall('get', '/dwc_occurrences/metadata.json', { params }),
 
-  generateDownload: (params) => AjaxCall('post', '/tasks/dwc/dashboard/generate_download.json', { params })
+  generateDownload: params => AjaxCall('post', '/tasks/dwc/dashboard/generate_download.json', params),
+
+  createIndex: params => AjaxCall('post', '/tasks/dwc/dashboard/create_index', params),
 }
