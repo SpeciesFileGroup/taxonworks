4e23a8cdf95d77e8d3bbc939b524808ef8bf578f
José Luis Pereira <pepelui@outlook.com>
2021-09-09 00:56:21 -0300

    Refactor map component into vue 3 composition API. Remove reactivity for shapes. Add non-reactive storage in Browse OTU

--- app/javascript/vue/tasks/otu/browse/components/CollectingEvents.vue
+++ app/javascript/vue/tasks/otu/browse/components/CollectingEvents.vue
@@ -8,19 +8,19 @@
         v-for="(item, index) in collectingEvents"
         :key="item.id">
         <li
-          v-if="index < max || showAll">
+          v-if="index < MAX_LIST || showAll">
           <a
             :href="`/collecting_events/${item.id}`"
             v-html="item.object_tag"/>
         </li>
       </template>
     </ul>
-    <template v-if="collectingEvents.length > max">
+    <template v-if="collectingEvents.length > MAX_LIST">
       <a
         v-if="!showAll"
         class="cursor-pointer"
         @click="showAll = true">Show all
-      </a> 
+      </a>
       <a
         v-else
         class="cursor-pointer"
@@ -28,7 +28,7 @@
       </a>
     </template>
     <switch-component
-      :options="tabs"
+      :options="TABS"
       v-model="view"/>
     <map-component
       width="100%"
@@ -38,127 +38,127 @@
   </section-panel>
 </template>
 
-<script>
+<script setup>
 
 import SectionPanel from './shared/sectionPanel'
 import MapComponent from 'components/georeferences/map.vue'
 import SwitchComponent from 'components/switch.vue'
-import extendSection from './shared/extendSections'
+import nonReactiveStore from '../store/nonReactiveStore.js'
 import { GetterNames } from '../store/getters/getters'
 import { MutationNames } from '../store/mutations/mutations'
 import { getUnique } from 'helpers/arrays.js'
+import { computed, ref, watch } from 'vue'
+import { useStore } from 'vuex'
 
-export default {
-  mixins: [extendSection],
-  components: {
-    SectionPanel,
-    MapComponent,
-    SwitchComponent
-  },
-  computed: {
-    isLoading () {
-      const loadState = this.$store.getters[GetterNames.GetLoadState]
-      return loadState.distribution && loadState.descendantsDistribution
-    },
-    collectingEvents: {
-      get () {
-        return this.$store.getters[GetterNames.GetCollectingEvents]
-      },
-      set (value) {
-        this.$store.commit(MutationNames.SetCollectingEvents, value)
-      }
-    },
-    georeferences () {
-      return this.$store.getters[GetterNames.GetGeoreferences]
-    },
-    descedantsGeoreferences () {
-      return this.$store.getters[GetterNames.GetDescendants].georeferences
-    },
-    collectionObjects () {
-      return this.$store.getters[GetterNames.GetCollectionObjects]
-    },
-    assertedDistributions () {
-      const ADs = this.$store.getters[GetterNames.GetAssertedDistributions]
-      const uniqueADs = getUnique(ADs, 'geographic_area_id')
+const MAX_LIST = 10
+const TABS = ['georeferences', 'asserted distributions', 'both']
 
-      return uniqueADs.map(item => {
-        let shape = item.geographic_area.shape
-        shape.properties.is_absent = item.is_absent
-
-        return shape
-      })
-    },
-    shapes () {
-      switch (this.view) {
-        case 'both':
-          return [].concat(this.assertedDistributions, this.geojson.features)
-        case 'georeferences':
-          return this.geojson.features
-        default:
-          return this.assertedDistributions
-      }
-    }
+const store = useStore()
+const props = defineProps({
+  status: {
+    type: String,
+    default: 'unknown'
   },
-  data () {
-    return {
-      max: 10,
-      showAll: false,
-      tabs: ['georeferences', 'asserted distributions', 'both'],
-      view: 'both',
-      geojson: {
-        features: []
-      }
-    }
+  title: {
+    type: String,
+    default: undefined
   },
-  watch: {
-    georeferences: {
-      handler (newVal) {
-        if (newVal) {
-          this.populateShapes()
-        }
-      }
-    },
-    descedantsGeoreferences: {
-      handler (newVal) {
-        if (newVal) {
-          this.populateShapes()
-        }
-      },
-      deep: true
-    }
-  },
-  methods: {
-    populateShapes () {
-      const georeferences = [].concat(this.descedantsGeoreferences, this.georeferences)
-      this.geojson.features = []
-
-      georeferences.forEach(geo => {
-        const popup = this.composePopup(geo)
-        if (geo.error_radius != null) {
-          geo.geo_json.properties.radius = geo.error_radius
-        }
-        if (popup) {
-          geo.geo_json.properties.popup = popup
-        }
-        this.geojson.features.push(geo.geo_json)
-      })
-    },
-    getCollectionObjectByGeoId (georeference) {
-      return this.collectionObjects.filter(co => co.collecting_event_id === this.getCEByGeo(georeference).id)
-    },
-    composePopup (geo) {
-      const ce = this.getCEByGeo(geo)
-      if (ce) {
-        return `<h4><b>Collection objects</b></h4>
-          ${this.getCollectionObjectByGeoId(geo).map(item => `<a href="/tasks/collection_objects/browse?collection_object_id=${item.id}">${item.object_tag}</a>`).join('<br>')}
-          <h4><b>Collecting event</b></h4>
-          <a href="/tasks/collecting_events/browse?collecting_event_id=${ce.id}">${ce.object_tag}</a>`
-      }
-      return undefined
-    },
-    getCEByGeo (georeference) {
-      return this.collectingEvents.find(ce => { return ce.id === georeference.collecting_event_id })
-    }
+  otu: {
+    type: Object,
+    required: true
   }
+})
+
+const georeferences = computed(() => store.getters[GetterNames.GetGeoreferences])
+
+const descedantsGeoreferences = computed(() => store.getters[GetterNames.GetDescendants].georeferences)
+
+const collectionObjects = computed(() => store.getters[GetterNames.GetCollectionObjects])
+
+const isLoading = computed(() => {
+  const loadState = store.getters[GetterNames.GetLoadState]
+
+  return loadState.distribution && loadState.descendantsDistribution
+})
+
+const collectingEvents = computed(() => ({
+  get () {
+    return store.getters[GetterNames.GetCollectingEvents]
+  },
+  set (value) {
+    store.commit(MutationNames.SetCollectingEvents, value)
+  }
+}))
+
+const assertedDistributions = computed(() => {
+  const ADs = store.getters[GetterNames.GetAssertedDistributions]
+  const uniqueADs = nonReactiveStore.geographicAreas
+
+  return uniqueADs.map(shape => {
+    shape.properties.is_absent = shape.is_absent
+
+    return shape
+  })
+})
+const shapes = computed(() => {
+  switch (view.value) {
+    case 'both':
+      return [].concat(assertedDistributions.value, geojson.value.features)
+    case 'georeferences':
+      return geojson.value.features
+    default:
+      return assertedDistributions.value
+  }
+})
+
+const showAll = ref(false)
+const view = ref('both')
+const geojson = ref({
+  features: []
+})
+
+watch(georeferences, newVal => {
+  if (newVal) {
+    populateShapes()
+  }
+})
+
+watch(descedantsGeoreferences, newVal => {
+  if (newVal) {
+    populateShapes()
+  }
+}, { deep: true })
+
+const populateShapes = () => {
+  const georeferencesArray = [].concat(descedantsGeoreferences.value, georeferences.value)
+  geojson.value.features = []
+
+  georeferencesArray.forEach(geo => {
+    const popup = composePopup(geo)
+
+    if (geo.error_radius != null) {
+      geo.geo_json.properties.radius = geo.error_radius
+    }
+    if (popup) {
+      geo.geo_json.properties.popup = popup
+    }
+
+    geojson.value.features.push(geo.geo_json)
+  })
 }
+
+const getCollectionObjectByGeoId = georeference => collectionObjects.value.filter(co => co.collecting_event_id === this.getCEByGeo(georeference).id)
+
+const composePopup = geo => {
+  const ce = getCEByGeo(geo)
+  if (ce) {
+    return `<h4><b>Collection objects</b></h4>
+      ${getCollectionObjectByGeoId(geo).map(item => `<a href="/tasks/collection_objects/browse?collection_object_id=${item.id}">${item.object_tag}</a>`).join('<br>')}
+      <h4><b>Collecting event</b></h4>
+      <a href="/tasks/collecting_events/browse?collecting_event_id=${ce.id}">${ce.object_tag}</a>`
+  }
+  return undefined
+}
+
+const getCEByGeo = georeference => collectingEvents.value.find(ce => ce.id === georeference.collecting_event_id)
 </script>
