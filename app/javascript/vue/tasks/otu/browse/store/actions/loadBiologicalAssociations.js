2ba671264920d26275551ab049f135ab6b0ed7ba
José Luis Pereira <pepelui@outlook.com>
2021-09-24 20:45:27 -0300

    Fix requests

--- app/javascript/vue/tasks/otu/browse/store/actions/loadBiologicalAssociations.js
+++ app/javascript/vue/tasks/otu/browse/store/actions/loadBiologicalAssociations.js
@@ -1,9 +1,17 @@
 import { MutationNames } from '../mutations/mutations'
 import { BiologicalAssociation } from 'routes/endpoints'
 
+const extend = [
+  'citations',
+  'object',
+  'subject',
+  'biological_relationship',
+  'source'
+]
+
 export default ({ state, commit }, globalId) =>
   new Promise((resolve, reject) => {
-    BiologicalAssociation.where({ any_global_id: globalId, extend: ['citations', 'object', 'subject', 'biological_relationship'] }).then(response => {
+    BiologicalAssociation.where({ any_global_id: globalId, extend }).then(response => {
       commit(MutationNames.SetBiologicalAssociations, state.biologicalAssociations.concat(response.body))
       resolve(response)
     }, error => {
