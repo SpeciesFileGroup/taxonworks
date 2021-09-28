affe112a5aea2777d5b3f0bb2d59aca16206ace5
José Luis Pereira <pepelui@outlook.com>
2021-09-28 18:10:50 -0300

    Add filter predicates

--- app/javascript/vue/tasks/collection_objects/filter/components/dwcDownload.vue
+++ app/javascript/vue/tasks/collection_objects/filter/components/dwcDownload.vue
@@ -1,17 +1,73 @@
 <template>
-  <v-btn
-    color="create"
-    medium
-    @click="download"
-  >
-    Download DwC
-  </v-btn>
+  <div>
+    <v-btn
+      color="primary"
+      medium
+      @click="setModalView(true)"
+    >
+      Download DwC
+    </v-btn>
+    <v-modal
+      @close="setModalView(false)"
+      v-if="showModal">
+      <template #header>
+        <h3>Download DwC</h3>
+      </template>
+      <template #body>
+        <h4>Filter by predicates</h4>
+        <div class="margin-small-bottom">
+          <div v-if="collectingEvents.length">
+            <p>Collecting events</p>
+            <ul class="no_bullets">
+              <li
+                v-for="item in collectingEvents"
+                :key="item.id">
+                <label>
+                  <input
+                    type="checkbox"
+                    :value="item.id"
+                    v-model="predicateParams.collecting_event_predicate_id">
+                  <span v-html="item.object_tag"/>
+                </label>
+              </li>
+            </ul>
+          </div>
+          <div v-if="collectionObjects.length">
+            <p>Collection objects</p>
+            <ul class="no_bullets">
+              <li v-for="item in collectionObjects">
+                <label>
+                  <input
+                    type="checkbox"
+                    :value="item.id"
+                    v-model="predicateParams.collection_object_predicate_id">
+                  <span v-html="item.object_tag"/>
+                </label>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </template>
+      <template #footer>
+        <v-btn
+          color="create"
+          medium
+          @click="download"
+        >
+          Download
+        </v-btn>
+      </template>
+    </v-modal>
+  </div>
 </template>
 <script setup>
-import VBtn from 'components/ui/VBtn/index.vue'
+
+import { reactive, ref, onBeforeMount } from 'vue'
 import { RouteNames } from 'routes/routes.js'
 import { DwcOcurrence } from 'routes/endpoints'
 import { transformObjectToParams } from 'helpers/setParam.js'
+import VBtn from 'components/ui/VBtn/index.vue'
+import VModal from 'components/ui/Modal.vue'
 
 const props = defineProps({
   params: {
@@ -25,8 +81,16 @@ const props = defineProps({
   }
 })
 
+const showModal = ref(false)
+const collectingEvents = ref([])
+const collectionObjects = ref([])
+const predicateParams = reactive({
+  collecting_event_predicate_id: [],
+  collection_object_predicate_id: []
+})
+
 const getFilterParams = params => {
-  const entries = Object.entries(params).filter(([key, value]) => !Array.isArray(value) || value.length)
+  const entries = Object.entries({...params, ...predicateParams }).filter(([key, value]) => !Array.isArray(value) || value.length)
   const data = Object.fromEntries(entries)
 
   data.per = props.total
@@ -40,6 +104,16 @@ const download = () => {
 
   DwcOcurrence.generateDownload({ ...downloadParams }).then(_ => {
     window.open(`${RouteNames.DwcDashboard}?${transformObjectToParams(downloadParams)}`)
+    setModalView(false)
   })
 }
+
+const setModalView = value => { showModal.value = value }
+
+onBeforeMount(() => {
+  DwcOcurrence.predicates().then(({ body }) => {
+    collectingEvents.value = body.collecting_event
+    collectionObjects.value = body.collection_object
+  })
+})
 </script>
