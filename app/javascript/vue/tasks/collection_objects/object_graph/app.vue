005026fecb95aaf2ceb5a31ca60c3cd1f3124a07
José Luis Pereira <pepelui@outlook.com>
2021-10-23 21:43:41 -0300

    Add test

--- app/javascript/vue/tasks/collection_objects/object_graph/app.vue
+++ app/javascript/vue/tasks/collection_objects/object_graph/app.vue
@@ -17,25 +17,34 @@
       stroke="black"
       stroke-width="2"
     />
-    <circle v-for="(node, i) in graph.nodes"
-      :cx="coords[i].x"
-      :cy="coords[i].y"
-      :r="20" :fill="colors[Math.ceil(Math.sqrt(node.index))]"
-      stroke="white" stroke-width="1"
-      @mousedown="currentMove = {x: $event.screenX, y: $event.screenY, node: node}"/>
+    <circle
+      v-for="node in graph.nodes"
+      :key="node.id"
+      :cx="coords[node.index].x"
+      :cy="coords[node.index].y"
+      :r="20"
+      stroke="white"
+      stroke-width="1"
+      :title="node.name"
+      :fill="colors[Math.ceil(Math.sqrt(node.index))]"
+      @mousedown="currentMove = { x: $event.screenX, y: $event.screenY, node: node }"
+    >
+      Test
+    </circle>
   </svg>
 </template>
 
 <script setup>
 import * as d3 from 'd3'
 import { ref, computed } from 'vue'
+import AjaxCall from 'helpers/ajaxCall'
 
 const graph = ref({
-  nodes: d3.range(100).map(i => ({ index: i, x: null, y: null })),
-  links: d3.range(99).map(i => ({ source: Math.floor(Math.sqrt(i)), target: i + 1 }))
+  nodes: [],
+  links: []
 })
-const width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
-const height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0) - 40
+const width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0) - 20
+const height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0) - 200
 const padding = 20
 const colors = ['#2196F3', '#E91E63', '#7E57C2', '#009688', '#00BCD4', '#EF6C00', '#4CAF50', '#FF9800', '#F44336', '#CDDC39', '#9C27B0']
 const simulation = ref(null)
@@ -49,25 +58,35 @@ const bounds = computed(() => ({
 }))
 
 const coords = computed(() => graph.value.nodes.map(node => ({
-  x: padding + (node.x - bounds.value.minX) * (width - 2*padding) / (bounds.value.maxX - bounds.value.minX),
-  y: padding + (node.y - bounds.value.minY) * (height - 2*padding) / (bounds.value.maxY - bounds.value.minY)
+  x: padding + (node.x - bounds.value.minX) * (width - 2 * padding) / (bounds.value.maxX - bounds.value.minX),
+  y: padding + (node.y - bounds.value.minY) * (height - 2 * padding) / (bounds.value.maxY - bounds.value.minY)
 })))
 
-simulation.value = d3.forceSimulation(graph.value.nodes)
-  .force('charge', d3.forceManyBody().strength(d => -100))
-  .force('link', d3.forceLink(graph.value.links))
-  .force('x', d3.forceX())
-  .force('y', d3.forceY())
+AjaxCall('get', '/graph/gid%3A%2F%2Ftaxon-works%2FLot%2F23887/object').then(({ body }) => {
+  graph.value = {
+    nodes: body.nodes.map(node => ({ ...node, x: null, y: null })),
+    links: body.edges.map(link => ({ 
+      source: body.nodes.findIndex(node => node.id === link.start_id),
+      target: body.nodes.findIndex(node => node.id === link.end_id) }))
+  }
+
+  simulation.value = d3.forceSimulation(graph.value.nodes)
+    .force('charge', d3.forceManyBody())
+    .force('link', d3.forceLink(graph.value.links))
+    .force('x', d3.forceX())
+    .force('y', d3.forceY())
+})
 
 const drag = e => {
   if (currentMove.value) {
     currentMove.value.node.fx = currentMove.value.node.x - (currentMove.value.x - e.screenX) * (bounds.value.maxX - bounds.value.minX) / (width - 2 * padding)
-    currentMove.value.node.fy = currentMove.value.node.y -(currentMove.value.y - e.screenY) * (bounds.value.maxY - bounds.value.minY) / (height - 2 * padding)
+    currentMove.value.node.fy = currentMove.value.node.y - (currentMove.value.y - e.screenY) * (bounds.value.maxY - bounds.value.minY) / (height - 2 * padding)
     currentMove.value.x = e.screenX
     currentMove.value.y = e.screenY
   }
 }
 const drop = () => {
+  if (!currentMove.value) return
   delete currentMove.value.node.fx
   delete currentMove.value.node.fy
   currentMove.value = null
