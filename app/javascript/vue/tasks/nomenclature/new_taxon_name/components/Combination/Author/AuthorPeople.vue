a479bed0bfeb5b94adaf1612ea95c035b3b27cc6
José Luis Pereira <pepelui@outlook.com>
2021-10-29 17:24:52 -0300

    Clone authors

--- app/javascript/vue/tasks/nomenclature/new_taxon_name/components/Combination/Author/AuthorPeople.vue
+++ app/javascript/vue/tasks/nomenclature/new_taxon_name/components/Combination/Author/AuthorPeople.vue
@@ -2,14 +2,25 @@
   <div class="flex-separate">
     <role-picker
       v-model="roles"
-      role-type="TaxonNameAuthor"
+      :role-type="ROLE_TAXON_NAME_AUTHOR"
     />
+    <v-btn
+      color="primary"
+      medium
+      :disabled="!source || isAlreadyClone"
+      @click="cloneFromSource"
+    >
+      Clone from source
+    </v-btn>
   </div>
 </template>
 <script setup>
 
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
+import { Source } from 'routes/endpoints'
+import { ROLE_TAXON_NAME_AUTHOR } from 'constants/index.js'
 import RolePicker from 'components/role_picker.vue'
+import VBtn from 'components/ui/VBtn/index.vue'
 
 const props = defineProps({
   modelValue: {
@@ -18,6 +29,7 @@ const props = defineProps({
   }
 })
 const emit = defineEmits(['update:modelValue'])
+const source = ref(null)
 
 const combination = computed({
   get: () => props.modelValue,
@@ -25,16 +37,46 @@ const combination = computed({
 })
 
 const roles = computed({
-  get () {
+  get: () => {
     const roles = combination.value.roles_attributes
 
     return roles
       ? roles.sort((a, b) => a.position - b.position)
       : []
   },
-  set (value) {
-    combination.value.roles_attributes = value
-  }
+  set: value => { combination.value.roles_attributes = value }
 })
+
+const isAlreadyClone = computed(() => {
+  if (source.value.author_roles.length === 0) return true
+
+  const authorsId = source.value.author_roles.map(author => Number(author.person.id))
+  const personsIds = roles.value.map(role => role.person_id || role.person.id)
+
+  return authorsId.every(id => personsIds.includes(id))
+})
+
+const cloneFromSource = () => {
+  const personsIds = roles.value.map(role => role.person.id)
+
+  const authorsPerson = source.value.author_roles
+    .filter(author => !personsIds.includes(Number(author.person.id)))
+    .map(author => ({
+      person_id: author.person.id,
+      type: ROLE_TAXON_NAME_AUTHOR,
+      first_name: author.person.first_name,
+      last_name: author.person.last_name
+    }))
+
+  roles.value = authorsPerson
+}
+
+const sourceId = combination.value.origin_citation_attributes?.source_id
+
+if (sourceId) {
+  Source.find(sourceId, { extend: ['roles'] }).then(({ body }) => {
+    source.value = body
+  })
+}
 
 </script>
