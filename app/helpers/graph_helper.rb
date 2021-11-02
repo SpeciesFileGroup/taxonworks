5fce576b622dea059bc2aa182ea88e37aa60f8f8
mjy <diapriid@gmail.com>
2021-11-02 09:00:52 -0500

    WIP

--- app/helpers/graph_helper.rb
+++ app/helpers/graph_helper.rb
@@ -6,123 +6,102 @@ module GraphHelper
 
     case object.class.base_class.name
     when 'CollectionObject'
-      collection_object_graph(object)
+      collection_object_graph(object).to_json
+    when 'CollectingEvent'
+      collecting_event_graph(object).to_json
     else
-      g = Export::Graph.new( object: object ) 
+      g = Export::Graph.new( object: object )
       g.to_json
-    end 
+    end
   end
 
-  def collection_object_graph(collection_object, graph = nil, target = nil)
+  def collecting_event_graph(collecting_event, graph: nil, target: nil, collection_objects: true)
+    c = collecting_event
+    return nil if c.nil?
+
+    g = initialize_graph(graph, c, target)
+
+    if collection_objects
+      c.collection_objects.each do |o|
+        collection_object_graph(o, graph: g, target: c, collecting_event: false)
+      end
+    end
+
+    c.collectors.each do |p|
+      g.add(p,c)
+    end
+    c
+    g
+  end
+
+  # @return Export::Graph
+  def collection_object_graph(collection_object, graph: nil, target:  nil, collecting_event: true)
+    c = collection_object
+    return nil if c.nil?
+
+    g = initialize_graph(graph, c, target)
+
+    collecting_event_graph(c.collecting_event, graph: g, target: c, collection_objects: false)
+
+    if r = c.repository
+      g.add(r, c)
+    end
+
+    c.biocuration_classes.each do |b|
+      g.add(b, c)
+    end
+
+    c.observations.each do |o|
+      g.add(o, c)
+    end
+
+    c.taxon_determinations.each do |d|
+      g.add(d, c)
+      g.add(d.otu, d)
+
+      d.determiners.each do |p|
+        g.add(p, d)
+      end
+
+      if d.otu.taxon_name
+        taxon_name_graph(d.otu.taxon_name, graph: g, target: d.otu)
+      end
+    end
+
+    c.all_biological_associations.each do |b|
+      g.add_node(b.biological_association_subject)
+      g.add_node(b.biological_association_object)
+      g.add_node(b.biological_relationship)
+
+      g.add_edge(b.biological_relationship, b.biological_association_subject)
+      g.add_edge(b.biological_relationship, b.biological_association_object)
+    end
+
+    g
+  end
+
+  def taxon_name_graph(taxon_name, graph: nil, target: nil)
+    t = taxon_name
+    return nil if t.nil?
+
+    g = initialize_graph(graph, t, target)
+
+    t.taxon_name_authors.each do |p|
+      g.add(p, t)
+    end
+    taxon_name_graph(t.parent, graph: g, target: t)
+
+    g
+  end
+
+  def initialize_graph(graph, object, target)
     g = graph
-    g ||= Export::Graph.new(object: collection_object)
-
-    g.to_json
-  end
-
-=begin
-
-
-
-    nodes = [
-      graph_node(collection_object),
-      graph_node(collection_object.collecting_event)
-    ]
-
-    edges = [
-      graph_edge(collection_object, collection_object.collecting_event, 'collecting event') 
-    ]
-
-    if collection_object.collecting_event
-      collection_object.collecting_event.collectors.each do |c|
-        edges.push graph_edge(collection_object.collecting_event, c)
-        nodes.push graph_node(c)
-      end     
-
-      collection_object.collecting_event.identifiers.each do |i|
-        nodes.push graph_node(i)
-        edges.push graph_edge(collection_object.collecting_event, i)
-      end
+    if g.nil?
+      g = Export::Graph.new(object: object)
+    else
+      g.add(object, target)
     end
-
-    collection_object.taxon_determinations.each do |t|
-      add_object_to_graph(t, collection_object, nodes, edges)
-      add_object_to_graph(t.otu, t, nodes, edges)
-
-      nomenclature_graph(nodes, edges, t.otu.taxon_name, t.otu)
-
-      t.determiners.each do |d|
-        nodes.push graph_node(d)
-        edges.push graph_edge(t,d)
-
-        d.identifiers.each do |i|
-          nodes.push graph_node(i)
-          edges.push graph_edge(d, i)
-        end
-      end
-
-      if r = collection_object.repository
-        nodes.push graph_node(r)
-        edges.push graph_edge(collection_object,r)
-
-        r.identifiers.each do |i|
-          add_object_to_graph(i, r, nodes, edges)
-        end       
-      end
-    end
-
-    collection_object.observations.each do |o|
-      add_object_to_graph(o, collection_object, nodes, edges)
-
-      o.identifiers.each do |i|
-        add_object_to_graph(i, o)
-      end       
-    end
-
-    collection_object.identifiers.each do |i|
-      add_object_to_graph(i, collection_object, nodes, edges)
-    end
-
-    collection_object.all_biological_associations.each do |b|
-      nodes.push graph_node(b.biological_association_subject)
-      nodes.push graph_node(b.biological_association_object)
-      nodes.push graph_node(b.biological_relationship)
-
-      edges.push graph_edge(b.biological_relationship, b.biological_association_subject)
-      edges.push graph_edge(b.biological_relationship, b.biological_association_object)
-    end
-
-    collection_object.biocuration_classes.each do |b|
-      add_object_to_graph(b, collection_object, nodes, edges)
-    end
-
-
-  end
-=end
-
-
-  def nomenclature_graph(nodes, edges, taxon_name, target)
-    return if taxon_name.nil?
-
-    nodes.push graph_node(taxon_name)
-    edges.push graph_edge(taxon_name, target)
-
-    taxon_name.taxon_name_authors.each do |a|
-      nodes.push graph_node(a)
-      edges.push graph_edge(taxon_name, a)
-
-      a.identifiers.each do |i|
-        nodes.push graph_node(i)
-        edges.push graph_edge(a, i)
-      end
-    end
-
-    nomenclature_graph(nodes, edges, taxon_name.parent, taxon_name)
-  end
-
-  def add_object_to_graph(object, object_origin, nodes, edges)
-    nodes.push graph_node(object)
-    edges.push graph_edge(object_origin, object)
+    g
   end
 
 # def add_graph_node(object, node_link = nil, nodes, edges)
@@ -145,31 +124,4 @@ module GraphHelper
 #   end
 # end
 
-
-  def graph_node(object, node_link = nil)
-    return nil if object.nil?
-    b = object.class.base_class.name
-    h = {
-      id: object.to_global_id.to_s,
-      name: label_for(object) || object.class.base_class.name,
-      color: NODE_COLORS[b] || '#000000'
-    }
-
-    h[:shape] = NODE_SHAPES[b] if !NODE_SHAPES[b].nil?
-    h[:link] = node_link unless node_link.blank?
-    h
-  end
-
-  def graph_edge(start_object, end_object, edge_label = nil, edge_link = nil)
-    return nil if start_object.nil? || end_object.nil?
-    h = {
-      start_id: start_object.to_global_id.to_s,
-      end_id: end_object.to_global_id.to_s
-    }
-
-    h[:label] = edge_label unless edge_label.blank?
-    h[:link] = edge_link unless edge_link.blank?
-    h
-  end
-
-  end
+end
--- lib/export/graph.rb
+++ lib/export/graph.rb
@@ -17,29 +17,27 @@ class Export::Graph
   }
 
   NODE_SHAPES = {
-    'Person' => 'person', 
-    'CollectionObject' => 'circle', 
-    'TaxonName' => 'square', 
-    'CollectingEvent' => 'circle', 
+    'Person' => 'person',
+    'CollectionObject' => 'circle',
+    'TaxonName' => 'square',
+    'CollectingEvent' => 'circle',
     'TaxonDetermination' => nil,
     'Identifier' => 'triangle',
-    'Otu' => 'hexagon', 
-    'User' => 'person', 
+    'Otu' => 'hexagon',
+    'User' => 'person',
     'ControlledVocabularyTerm' => nil,
-    'BiologicalRelationship' => nil ,
+    'BiologicalRelationship' => 'square' ,
     'Observation' => 'square',
     'Citation' => 'square',
     'Repository' => 'circle'
   }
 
-  RENDERABLE_ANNOTATIONS = [
-
-  ]
+  RENDERABLE_ANNOTATIONS = []
 
   attr_accessor :nodes
   attr_accessor :edges
 
-  attr_accessor :draw_annotations 
+  attr_accessor :draw_annotations
 
   # @params object [An AR record, nil]
   #   the "base" of the object, if rooted
@@ -60,39 +58,51 @@ class Export::Graph
   end
 
   def to_json
-    return { 
+    return {
       nodes: nodes,
       edges: edges
     }
   end
 
-  def add(object, object_origin = nil)
-    @nodes.push graph_node(object)
-    @edges.push graph_edge(object_origin, object)
+  def add(object, object_origin = nil, edge_label: nil, edge_link: nil)
+    add_node(object)
+    add_edge(object_origin, object, edge_label: edge_label, edge_link: edge_link) if !object_origin.nil?
   end
 
-  # def add_graph_node(object, node_link = nil, nodes, edges)
-  #   nodes.push graph_node(object, node_link)
+  # TODO factor view options out
+  def add_node(object, citations: true, identifiers: true)
+    @nodes.push graph_node(object)
 
-  #   object.citations.each do |c|
-  #     nodes.push graph_node(c)
-  #     edges.push graph_edge(object, c)
+    if citations && object.respond_to?(:citations)
+      object.citations.each do |c|
+        add_node(c, citations: false, identifiers: false)
+        add_node(c.source, citations: false, identifiers: true)
 
-  #     nodes.push graph_node(c.source)
-  #     edges.push graph_edge(c, c.source)
+        add_edge(c, object)
+        add_edge(c.source, c)
 
-  #     if c.source.is_bibtex?
-  #       c.source.source_authors.each do |a|
-  #         nodes.push graph_node(a)
-  #         edges.push graph_edge(c.source, a)
-  #       end
-  #     end
+        if c.source.is_bibtex?
+          c.source.authors.each do |p|
+            add_node(p, citations: false, identifiers: true)
+            add_edge(p, c.source)
+          end
+        end
+      end
+    end
 
-  #   end
-  # end
+    if identifiers && object.respond_to?(:identifiers)
+      object.identifiers.each do |i|
+        add_node(i, citations: false, identifiers: false)
+        add_edge(i, object)
+      end
+    end
+  end
 
+  def add_edge(object, object_origin = nil, edge_label: nil, edge_link: nil)
+    @edges.push graph_edge(object_origin, object, edge_label: edge_label, edge_link: edge_link)
+  end
 
-  def graph_node(object, node_link = nil)
+  def graph_node(object, node_link: nil)
     return nil if object.nil?
     b = object.class.base_class.name
     h = {
@@ -106,16 +116,28 @@ class Export::Graph
     h
   end
 
-  def graph_edge(start_object, end_object, edge_label = nil, edge_link = nil)
+  def graph_edge(start_object, end_object, edge_label: nil, edge_link: nil)
     return nil if start_object.nil? || end_object.nil?
     h = {
       start_id: start_object.to_global_id.to_s,
       end_id: end_object.to_global_id.to_s
     }
 
+    # TODO: change label to name
     h[:label] = edge_label unless edge_label.blank?
     h[:link] = edge_link unless edge_link.blank?
     h
+  end
+
+  # @return True
+  # !param graph [Export::Graph]
+  # Merges the edges and nodes from another graph to this one
+  def merge(graph)
+    @nodes += graph.nodes
+    @edges += graph.edges
+    @nodes.uniq!
+    @edges.uniq!
+    true
   end
 
 end
