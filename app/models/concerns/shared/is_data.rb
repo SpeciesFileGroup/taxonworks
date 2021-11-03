744cbfef70eef2dfd8d63ff79f355f834cfffaa1
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2021-11-03 18:42:26 -0300

    Fixed typo

--- app/models/concerns/shared/is_data.rb
+++ app/models/concerns/shared/is_data.rb
@@ -112,7 +112,7 @@ module Shared::IsData
     self.class.reflect_on_all_associations(:has_one).each do |r|
       if is_community? # *this* object is community, others we don't care about
         if o = send(r.name)
-          return false if o.respond_to(:project_id) && !p.include?(o.project)
+          return false if o.respond_to?(:project_id) && !p.include?(o.project)
         end
       end
     end
