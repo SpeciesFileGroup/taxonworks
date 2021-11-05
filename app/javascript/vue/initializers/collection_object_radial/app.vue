f0aa3d966e59149a07339f2b308feadb825fe7e8
José Luis Pereira <pepelui@outlook.com>
2021-11-05 11:56:59 -0300

    Fix label

--- app/javascript/vue/initializers/collection_object_radial/app.vue
+++ app/javascript/vue/initializers/collection_object_radial/app.vue
@@ -3,26 +3,25 @@
     :id="id"
     type="graph"
     button-class="btn-co-radial"
-    button-title="Collection object quick forms"
     :global-id="globalId"/>
 </template>
 <script>
 
-  import CollectionObjectRadial from 'components/radials/object/radial.vue'
+import CollectionObjectRadial from 'components/radials/object/radial.vue'
 
-  export default {
-    components: {
-      CollectionObjectRadial
+export default {
+  components: {
+    CollectionObjectRadial
+  },
+  props: {
+    id: {
+      type: String,
+      required: true
     },
-    props: {
-      id: {
-        type: String,
-        required: true
-      },
-      globalId: {
-        type: String,
-        required: true
-      }
+    globalId: {
+      type: String,
+      required: true
     }
   }
+}
 </script>
