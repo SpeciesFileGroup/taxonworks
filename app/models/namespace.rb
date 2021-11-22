717a466d6112c1ad53bb908e01b43cd50be8f8ea
mjy <diapriid@gmail.com>
2021-11-22 15:06:16 -0600

    WIP

--- app/models/namespace.rb
+++ app/models/namespace.rb
@@ -1,46 +1,61 @@
-# A Namespace is used to scope identifiers.  All identifiers (strings) within a namespace
-# must be unique.  This is the only constraint.
+# A Namespace is used to define a set of (i.e. scope) local (not intended to be globally unique) Identifiers.
+# All identifiers (strings) within a Namespace must be unique.  Namespaces used to attempt to provide
+# a degree of provenance to how the Identifier came to be, and to differentiate, in practical terms, one
+# identifier from another within the system.  In the case of the latter, they act as a label that lets 
+# the data curator disambiguate what might be otherwise two identical strings via a short mnemonic.
 #
-# In TW Identifiers + namespaces are used for differentiating/disambiguating one datum from another.
+# In natural history collection management many catalog numbers contain an institutional coden as 
+# part of their catalog number (as physically written).  In TaxonWorks we treat that coden as the basis
+# for a Namespace. Note that we also may use the CODEN in a second way, to identify the Repository.  
+# While the string is duplicate the meaning is not.  One use let's us quickly reference a Repository,
+# the second use let's us quickly narrow searches to a specific Identifier linked to our object.
+#
+# In TW Identifiers + Namespaces are used for differentiating/disambiguating one datum from another.
+# This applies to all local identifiers, not just what most might be familiar with in the 
+# DwC catalogNumber field. 
+#
+# Namespaces are shared across projects.
 #
 # Namespaces are minted in TW on a first come first serve basis.  Their short and
-# long names must be unique across instances.  When conflicts arise new values must be minted for
-# record keeping purposes.  In this case a verbatim_short_name can be provided, and this
+# long names must be unique. When conflicts arise new values must be minted for
+# record keeping purposes. In this case a verbatim_short_name can be provided, and this
 # value will be presented for reporting/searching purposes.  This is a strong restriction
-# that is intended to encourage users to think before they mint namespaces.
+# that is intended to encourage users to think before they mint a Namespace.
 #
-# In TW Namespaces DO NOT imply ownership! If an identifier has a namespace
+# In TW Namespaces DO NOT imply ownership! If an identifier has a Namespace
 # that includes a reference to some collection, it does not mean that that collection 'owns' the identified object.
 #
-# Namespaces in TW are *not* limited to things like collection repository codens.
-#
 # @!attribute institution
-#   @return [String]
-#   The institution (loosely, could be a person) responsible for minting this namespace.  Roughly, where to look for more information.
-#   This is NOT 1:1 with http://rs.tdwg.org/dwc/terms/ownerInstitutionCode, which implies ownership.  It is narrower in that
-#   it means the institution "owns", or minted the string, that's all.
+# @return [String]
+#   The institution (loosely, could be a person) responsible for minting this Namespace.  Roughly, where to look for more information.
+#   This is NOT 1:1 with http://rs.tdwg.org/dwc/terms/ownerInstitutionCode, which implies ownership. It is narrower in that
+#   it means the institution or individual "owns", or minted this Namespace, that's all.
 #
 # @!attribute name
-#   @return [String]
-#   The full name of the namespace. For example 'Illinois Natural History Collection Insect Collection'.
-#   This is similar to http://rs.tdwg.org/dwc/terms/institutionCode, but not identical, in that no ownership is not explicitly implied.
+# @return [String]
+#   The full, descriptive name of the namespace. For example 'Illinois Natural History Collection Insect Collection Identifiers'.
 #
 # @!attribute short_name
-#   @return [String]
-#   A short, realized version of the name.  For example "INHIC".
+# @return [String]
+#   A short, realized version of the name, as seen in the physical label if there is a physical form. For example "INHIC".
 #   This may be embedded in http://rs.tdwg.org/dwc/terms/institutionCode.
 #   We presently do not differentiate a http://rs.tdwg.org/dwc/terms/collectionCode in
 #   identifiers, that data may(?) fall into Repositories.
 #
 # @!attribute verbatim_short_name
-#   @return [String]
+# @return [String]
 #   TW enforces uniqueness of short names.  When a short name exists for historical reasons but it has already
 #   been included in TW then a new short name must be minted, and a verbatim_short_name used to indicate the
 #   physically/and or historically recorded value.
 #
 # @!attribute delimiter
-#   @return [String, nil]
-#      Defines the character(s) to be placed between the namespace short name and the identifier
+# @return [String, nil]
+#    Defines the character(s) to be placed between the namespace short name and the identifier
+#
+# @!attribute is_virtual
+# @return [Boolean]
+#   Defaults to false. If false then 
+#   
 #
 class Namespace < ApplicationRecord
   include Housekeeping::Users
