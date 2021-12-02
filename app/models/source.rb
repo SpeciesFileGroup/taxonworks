cd8f15b1ed76c23d5263628b2eba7e499579c1de
Dmitry Dmitriev <arboridia@gmail.com>
2021-12-02 14:48:27 -0600

    source recent

--- app/models/source.rb
+++ app/models/source.rb
@@ -297,14 +297,14 @@ class Source < ApplicationRecord
   # @return [Scope]
   #    the max 10 most recently used (1 week, could parameterize) TaxonName, as used 
   def self.used_recently(user_id, project_id, used_on = 'TaxonName')
-   Source.joins(:citations).merge(
-        Citation.where(
-          created_by_id: user_id,
-          project_id: project_id,
-          citation_object_type: used_on,
-          created_at: 1.week.ago..
-        ).order(created_at: :desc)
-      ).pluck(:source_id).uniq.first(10)
+   Source.select('sources.id').
+     joins(:citations)
+         .where(citations: {created_by_id: user_id,
+                project_id: project_id,
+                citation_object_type: used_on,
+                created_at: 1.week.ago..})
+        .order('citations.created_at DESC')
+      .pluck(:id).uniq
   end
 
   # @params target [String] a citable model name
