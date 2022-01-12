ea065d25f4a09a9c8ebac91432d74cdb131f4cf9
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2022-01-12 16:27:45 -0300

    Eager load at CI tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -11,7 +11,7 @@ TaxonWorks::Application.configure do
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
-  config.eager_load = false
+  config.eager_load = ENV["CI"].present?
 
   # Configure static asset server for tests with Cache-Control for performance.
   # config.serve_static_files                         = true
