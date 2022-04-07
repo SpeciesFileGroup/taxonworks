eeaa07e30b1a51a53763e83bc7b505d03acc2cd1
José Luis Pereira <pepelui@outlook.com>
2022-04-07 14:21:26 -0300

    Tweak

--- app/javascript/vue/tasks/uniquify/people/store/actions/processMerge.js
+++ app/javascript/vue/tasks/uniquify/people/store/actions/processMerge.js
@@ -2,30 +2,36 @@ import { People } from 'routes/endpoints'
 import { MutationNames } from '../mutations/mutations'
 
 export default ({ state, commit }) => {
+  let selectedPerson = state.selectedPerson
+  let foundPeople = [...state.foundPeople]
+  let matchPeople = [...state.matchPeople]
+
   function processMerge (mergeList) {
     const mergePerson = mergeList.pop()
     state.requestState.isMerging = true
 
-    People.merge(state.selectedPerson.id, {
+    People.merge(selectedPerson.id, {
       person_to_destroy: mergePerson.id,
       extend: ['roles']
     }).then(({ body }) => {
-      const personIndex = state.foundPeople.findIndex(person => person.id === state.selectedPerson.id)
+      const personIndex = foundPeople.findIndex(person => person.id === selectedPerson.id)
 
-      state.foundPeople = state.foundPeople.filter(people => mergePerson.id !== people.id)
-      state.matchPeople = state.matchPeople.filter(people => mergePerson.id !== people.id)
+      foundPeople = foundPeople.filter(people => mergePerson.id !== people.id)
+      matchPeople = matchPeople.filter(people => mergePerson.id !== people.id)
 
-      state.selectedPerson = body
+      selectedPerson = body
 
       if (personIndex > -1) {
-        state.foundPeople[personIndex] = state.selectedPerson
+        foundPeople[personIndex] = selectedPerson
       }
     }).finally(() => {
       if (mergeList.length) {
         processMerge(mergeList)
       } else {
-        People.find(state.selectedPerson.id, { extend: ['roles'] }).then(({ body }) => {
+        People.find(selectedPerson.id, { extend: ['roles'] }).then(({ body }) => {
           state.selectedPerson = body
+          state.foundPeople = foundPeople
+          state.matchPeople = matchPeople
           state.requestState.isMerging = false
           commit(MutationNames.SetMergePeople, [])
         })
