9b9f16fe5633a138bf8767a5afc1a220c36b8b58
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2022-04-27 18:39:01 -0300

    Fixed identifiers prefer scope and corrected class name in helper

--- app/models/identifier.rb
+++ app/models/identifier.rb
@@ -67,9 +67,11 @@ class Identifier < ApplicationRecord
   # TODO: DRY to IsData? Test.
   scope :with_type_string, -> (base_string) {where('type LIKE ?', "#{base_string}")}
 
-  scope :prefer, -> (type) {  order(Arel.sql("CASE WHEN identifiers.type = '#{type}' THEN 1 END ASC, \
-                                              CASE WHEN identifiers.type != '#{type}' THEN 2 END ASC, \
-                                               position ASC"))  }
+  scope :prefer, -> (type) { order(Arel.sql(<<~SQL)) }
+    CASE WHEN identifiers.type = '#{type}' THEN 1 \
+    WHEN identifiers.type != '#{type}' THEN 2 END ASC, \
+    position ASC
+  SQL
 
   # @return [String, Identifer]
   def self.prototype_identifier(project_id, created_by_id)
