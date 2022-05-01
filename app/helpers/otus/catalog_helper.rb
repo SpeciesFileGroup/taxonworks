f543c3f81341e100eb4d7c885ba7f91b836e3faa
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2022-05-01 12:09:54 -0300

    Small refactors + fixed repeated names in descendants

--- app/helpers/otus/catalog_helper.rb
+++ app/helpers/otus/catalog_helper.rb
@@ -58,14 +58,9 @@ module Otus::CatalogHelper
   # @param data [Hash] the data to return
   # @param similar_otus [Array] of otu_ids, the ids of OTUs to skip when assigning nodes (e.g. clones, or similar otus)
   def otu_descendants_and_synonyms(otu = self, data: {}, similar_otus: [], common_names: false, langage_alpha2: nil)
-    s = nil
-    if otu.name.present?
-      s = Otu.where(taxon_name: otu.taxon_name.id).where.not(id: otu.id).where.not("otus.name = ?", otu.name).to_a
-    else
-      s = Otu.where(taxon_name: otu.taxon_name.id).where.not(id: otu.id).where('otus.name is not null').to_a
-    end
+    s = Otu.where(taxon_name: otu.taxon_name).where.not(id: otu.id).where.not(name: otu.name.present? ? otu.name : nil).to_a
 
-    similar_otus += s.collect{|p| p.id}
+    similar_otus += s.collect { |p| p.id }
 
     synonyms = otu.taxon_name&.synonyms.where(type: 'Protonym').where.not(id: otu.taxon_name.id)&.order(:cached, :cached_author_year)
 
@@ -74,12 +69,12 @@ module Otus::CatalogHelper
              otu_clones: Otu.where(name: otu.name, taxon_name: otu.taxon_name).where.not(id: otu.id).pluck(:id),
              similar_otus: s.inject({}){|hsh, n| hsh[n.id] = label_for_otu(n) ; hsh },
              nomenclatural_synonyms: ( (synonyms&.collect{|l| full_original_taxon_name_tag(l) || taxon_name_tag(l) } || []) - [a]).uniq, # This is labels, combinations can duplicate
-             common_names: (common_names ? otu_inventory_common_names(otu, langage_alpha2) : []) ,
+             common_names: (common_names ? otu_inventory_common_names(otu, langage_alpha2) : []),
              descendants: []}
 
     if otu.taxon_name
-      otu.taxon_name.descendants.that_is_valid.order(:cached, :cached_author_year).each do |d|
-        if o = o = d.otus.order(name: 'DESC', id: 'ASC').first # arbitrary pick an OTU, prefer those without `name`. t since we summarize across identical OTUs, this is not an issue
+      otu.taxon_name.descendants.where(parent: otu.taxon_name).that_is_valid.order(:cached, :cached_author_year).each do |d|
+        if o = d.otus.order(name: 'DESC', id: 'ASC').first # arbitrary pick an OTU, prefer those without `name`. t since we summarize across identical OTUs, this is not an issue
           next if similar_otus.include?(o.id)
           data[:descendants].push otu_descendants_and_synonyms(o, data: data, similar_otus: similar_otus)
         end
