57b595c74799523b3775feb8b59feb58942b7544
José Luis Pereira <pepelui@outlook.com>
2022-05-03 13:55:36 -0300

    Add scaffold and person facet

---
+++ app/javascript/vue/tasks/people/filter/components/Facet/FacetPerson.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="field">
+    <label class="display-block">First name</label>
+    <input
+      type="text"
+      v-model="fields.first_name"
+    >
+    <label>
+      <input 
+        type="checkbox"
+        value="first_name"
+        v-model="fields.exact"
+      >
+      Exact
+    </label>
+  </div>
+  <div class="field">
+    <label class="display-block">Last name</label>
+    <input
+      type="text"
+      v-model="fields.last_name"
+    >
+    <label>
+      <input 
+        type="checkbox"
+        value="last_name"
+        v-model="fields.exact"
+      >
+      Exact
+    </label>
+  </div>
+  <div class="field">
+    <label class="display-block">Suffix</label>
+    <input 
+      type="text"
+      v-model="fields.suffix"
+    >
+    <label>
+      <input 
+        type="checkbox"
+        value="suffix"
+        v-model="fields.exact"
+      >
+      Exact
+    </label>
+  </div>
+  <div class="field">
+    <label class="display-block">Prefix</label>
+    <input 
+      type="text"
+      v-model="fields.prefix"
+    >
+    <label>
+      <input 
+        type="checkbox"
+        value="prefix"
+        v-model="fields.exact"
+      >
+      Exact
+    </label>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  modelValue: {
+    type: Object,
+    required: true
+  }
+})
+
+const emit = defineEmits('update:modelValue')
+
+const fields = computed({
+  get () {
+    return props.modelValue
+  },
+
+  set (value) {
+    emit('update:modelValue', value)
+  }
+})
+
+</script>
---
+++ app/javascript/vue/tasks/people/filter/components/filter.vue
@@ -0,0 +1,149 @@
+<template>
+  <div
+    class="panel vue-filter-container"
+    v-hotkey="shortcuts">
+    <div class="flex-separate content middle action-line">
+      <span>Filter</span>
+      <button
+        type="button"
+        data-icon="w_reset"
+        class="button circle-button button-default center-icon no-margin"
+        @click="resetFilter"
+      />
+    </div>
+
+    <div class="content">
+      <button
+        class="button button-default normal-input full_width margin-medium-bottom "
+        type="button"
+        @click="handleSearch"
+      >
+        Search
+      </button>
+      <facet-person
+        class="margin-large-bottom"
+        v-model="params.base"
+      />
+      <with-component
+        class="margin-large-bottom"
+        title="Sequences"
+        param="sequences"
+        v-model="params.with"
+      />
+      <keywords-component
+        class="margin-large-bottom"
+        v-model="params.keywords"
+        target="CollectionObject"
+      />
+      <identifier-component
+        class="margin-large-bottom"
+        v-model="params.identifier"
+      />
+      <user-component
+        class="margin-large-bottom"
+        v-model="params.user"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+import UserComponent from 'tasks/collection_objects/filter/components/filters/user'
+import IdentifierComponent from 'tasks/collection_objects/filter/components/filters/identifier'
+import KeywordsComponent from 'tasks/sources/filter/components/filters/tags'
+import platformKey from 'helpers/getPlatformKey.js'
+import WithComponent from 'tasks/sources/filter/components/filters/with'
+import FacetPerson from './Facet/FacetPerson.vue'
+import { computed, ref } from 'vue'
+
+const emit = defineEmits([
+  'parameters',
+  'reset'
+])
+
+const shortcuts = computed(() => {
+  const keys = {}
+
+  keys[`${platformKey()}+r`] = resetFilter
+  keys[`${platformKey()}+f`] = handleSearch
+
+  return keys
+})
+
+const parseParams = computed(() =>
+  ({
+    ...params.value.base,
+    ...params.value.with,
+    ...filterEmptyParams(params.value.user)
+  })
+)
+
+const resetFilter = () => {
+  emit('reset')
+  params.value = initParams()
+}
+
+const initParams = () => ({
+  base: {
+    name: undefined,
+    last_name: undefined,
+    first_name: undefined,
+    suffix: undefined,
+    prefix: undefined,
+    exact: [],
+    near_name: undefined,
+    levenshtein_cuttoff: 1,
+    regex: undefined,
+    with_role: [],
+    without_role: [],
+    role_total_min: undefined,
+    role_total_max: undefined,
+    born_after_year: undefined,
+    active_after_year: undefined,
+    active_before_year: undefined,
+    died_after_year: undefined,
+    repeated_total: undefined
+  },
+  with: {
+    first_name: undefined,
+    last_name: undefined
+  },
+  user: {
+    user_id: undefined,
+    user_target: undefined,
+    user_date_start: undefined,
+    user_date_end: undefined
+  },
+  identifier: {
+    identifier: undefined,
+    identifier_exact: undefined,
+    identifier_start: undefined,
+    identifier_end: undefined,
+    namespace_id: undefined
+  },
+})
+
+const params = ref(initParams())
+
+const handleSearch = () => {
+  emit('parameters', parseParams.value)
+}
+
+const filterEmptyParams = object => {
+  const keys = Object.keys(object)
+
+  keys.forEach(key => {
+    if (object[key] === '') {
+      delete object[key]
+    }
+  })
+  return object
+}
+
+</script>
+<style scoped>
+:deep(.btn-delete) {
+    background-color: #5D9ECE;
+  }
+</style>
