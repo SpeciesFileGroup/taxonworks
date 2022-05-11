bbac1a0df1b4f23e1668dfc576bd17725c4bf4c2
José Luis Pereira <pepelui@outlook.com>
2022-05-11 15:48:45 -0300

    Factor jQuery code to vanilla js

--- app/assets/javascripts/views/taxon_name/parent_navigator.js
+++ app/assets/javascripts/views/taxon_name/parent_navigator.js
@@ -5,31 +5,53 @@ TW.views.taxon_name.parent_navigator = TW.views.taxon_name.parent_navigator || {
 
 
 Object.assign(TW.views.taxon_name.parent_navigator, {
-	init: function() {
-		var effectSpeed = 250;
 
-		$(".switch-radio input:radio[name=display_herarchy]").on("click", function() {
-			switch($(this).val()) {
-				case 'valid':
-					$('[data-valid="valid"]').show(effectSpeed);
-					$('[data-valid="invalid"]').hide(effectSpeed);
-				break;
-				case 'invalid':
-					$('[data-valid="valid"]').hide(effectSpeed);
-					$('[data-valid="invalid"]').show(effectSpeed);
-				break;
-				case 'both':
-					$('[data-valid="valid"]').show(effectSpeed);
-					$('[data-valid="invalid"]').show(effectSpeed);
-				break;
-			}
-		});
+	addElementsClass (tag, klass) {
+		const elements = document.querySelectorAll(tag)
+
+		elements.forEach(element => {
+			element.classList.add(klass)
+		})
+	},
+
+	removeElementsClass (tag, klass) {
+		const elements = document.querySelectorAll(tag)
+
+		elements.forEach(element => {
+			element.classList.remove(klass)
+		})
+	},
+
+	toggleView (event) {
+		const value = event.target.value
+
+		switch (value) {
+			case 'valid':
+				this.removeElementsClass('[data-valid="valid"]', 'display-none')
+				this.addElementsClass('[data-valid="invalid"]', 'display-none')
+			break;
+			case 'invalid':
+				this.addElementsClass('[data-valid="valid"]', 'display-none')
+				this.removeElementsClass('[data-valid="invalid"]', 'display-none')
+			break;
+			case 'both':
+				this.removeElementsClass('[data-valid="valid"]', 'display-none')
+				this.removeElementsClass('[data-valid="invalid"]', 'display-none')
+			break;
+		}
+	},
+
+	init () {
+		const radioElements = document.querySelectorAll('input[name="display_herarchy"]')
+
+		radioElements.forEach(element => {
+			element.addEventListener('click', this.toggleView.bind(this))
+		})
 	}
 });
 
-$(document).on('turbolinks:load', function() {
-  if($("#show_taxon_name_hierarchy").length) {
-  	var init_parentNavigator = TW.views.taxon_name.parent_navigator;
-  	init_parentNavigator.init();
+document.addEventListener('turbolinks:load', () => {
+  if (document.querySelector("#show_taxon_name_hierarchy")) {
+  	TW.views.taxon_name.parent_navigator.init()
   }
-});
+})
