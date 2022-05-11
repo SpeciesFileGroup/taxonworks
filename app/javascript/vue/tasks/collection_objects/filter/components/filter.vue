0e205c42fcac3db8e71ef7e8fbf7625228cca152
José Luis Pereira <pepelui@outlook.com>
2022-05-11 13:05:03 -0300

    Add note facet #2966

--- app/javascript/vue/tasks/collection_objects/filter/components/filter.vue
+++ app/javascript/vue/tasks/collection_objects/filter/components/filter.vue
@@ -77,6 +77,10 @@
         class="margin-large-bottom"
         @onUserslist="usersList = $event"
         v-model="params.user"/>
+      <facet-notes
+        class="margin-large-bottom"
+        v-model="params.notes"
+      />
       <buffered-component v-model="params.buffered"/>
       <with-component
         class="margin-large-bottom"
@@ -106,6 +110,7 @@ import WithComponent from 'tasks/sources/filter/components/filters/with'
 import BufferedComponent from './filters/buffered.vue'
 import PreparationTypes from './filters/preparationTypes'
 import CollectorsComponent from './filters/shared/people'
+import FacetNotes from './filters/FacetNotes.vue'
 import { chunkArray } from 'helpers/arrays.js'
 
 import SpinnerComponent from 'components/spinner'
@@ -130,7 +135,8 @@ export default {
     RepositoryComponent,
     WithComponent,
     PreparationTypes,
-    CollectorsComponent
+    CollectorsComponent,
+    FacetNotes
   },
 
   emits: [
@@ -152,7 +158,7 @@ export default {
     },
 
     parseParams () {
-      return Object.assign({}, { preparation_type_id: this.params.preparation_type_id }, this.params.collectors, this.params.settings, this.params.buffered.text, this.params.buffered.exact, this.params.byRecordsWith, this.params.biocurations, this.params.relationships, this.params.loans, this.params.types, this.params.determination, this.params.identifier, this.params.keywords, this.params.geographic, this.params.repository, this.flatObject(this.params.collectingEvents, 'fields'), this.filterEmptyParams(this.params.user))
+      return Object.assign({}, { preparation_type_id: this.params.preparation_type_id }, this.params.notes, this.params.collectors, this.params.settings, this.params.buffered.text, this.params.buffered.exact, this.params.byRecordsWith, this.params.biocurations, this.params.relationships, this.params.loans, this.params.types, this.params.determination, this.params.identifier, this.params.keywords, this.params.geographic, this.params.repository, this.flatObject(this.params.collectingEvents, 'fields'), this.filterEmptyParams(this.params.user))
     },
 
     isParamsEmpty () {
@@ -173,6 +179,7 @@ export default {
         Object.keys(this.params.collectingEvents.fields).length ||
         Object.values(this.params.collectingEvents).find(item => item && item.length) ||
         Object.values(this.params.user).find(item => item) ||
+        Object.values(this.params.notes).find(item => item) ||
         Object.values(this.params.loans).find(item => item) ||
         Object.values(this.params.identifier).find(item => item) ||
         Object.values(this.params.byRecordsWith).some(item => item !== undefined) ||
@@ -263,6 +270,7 @@ export default {
           repository: undefined,
           preparation_type: undefined,
           dwc_indexed: undefined,
+          notes: undefined,
           with_buffered_collecting_event: undefined,
           with_buffered_determinations: undefined,
           with_buffered_other_labels: undefined
@@ -278,6 +286,10 @@ export default {
             exact_buffered_determinations: undefined,
             exact_buffered_other_labels: undefined
           }
+        },
+        notes: {
+          note_text: undefined,
+          note_exact: undefined,
         },
         relationships: {
           biological_relationship_ids: []
