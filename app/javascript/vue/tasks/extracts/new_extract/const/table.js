696296eaac8f12828fb1776881d023481d0eabfe
José Luis Pereira <pepelui@outlook.com>
2022-05-19 17:12:26 -0300

    Change extract list

---
+++ app/javascript/vue/tasks/extracts/new_extract/const/table.js
@@ -0,0 +1,17 @@
+export const TABLE_HEADERS = [
+  'Otus',
+  'Identifiers',
+  'Origin types',
+  'Origins',
+  'Verbatim anatomical origin',
+  'Date'
+]
+
+export const TABLE_ATTRIBUTES = [
+  'otus',
+  'identifiers',
+  'origins',
+  'originsType',
+  'verbatim_anatomical_origin',
+  'date'
+]
