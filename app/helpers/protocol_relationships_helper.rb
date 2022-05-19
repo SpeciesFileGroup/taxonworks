9937e89d22f119c823a27a47b2ff739da7f12eb3
mjy <diapriid@gmail.com>
2022-05-19 13:05:49 -0500

    Extract/protocol UI issues. Fix #2990

--- app/helpers/protocol_relationships_helper.rb
+++ app/helpers/protocol_relationships_helper.rb
@@ -1,7 +1,11 @@
 module ProtocolRelationshipsHelper
   def protocol_relationship_tag(protocol_relationship)
     return nil if protocol_relationship.nil?
-    protocol_tag(protocol_relationship.protocol) + ': ' + object_tag(protocol_relationship.protocol_relationship_object)
+    [
+      protocol_tag(protocol_relationship.protocol).html_safe,
+      (' used on ' + protocol_relationship.protocol_relationship_object_type + ': '),
+      tag.span( label_for(protocol_relationship.protocol_relationship_object).truncate(80), class: [:feedback, 'feedback-thin', 'feedback-secondary']).html_safe
+    ].join.html_safe
   end
 
   def label_for_protocol_relationship(protocol_relationship)
