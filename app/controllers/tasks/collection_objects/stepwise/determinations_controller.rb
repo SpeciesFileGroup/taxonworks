993f88925b9f2095aa8dd12b2758829e0e58837d
mjy <diapriid@gmail.com>
2022-05-19 16:10:15 -0500

    Only select COs without determinations

--- app/controllers/tasks/collection_objects/stepwise/determinations_controller.rb
+++ app/controllers/tasks/collection_objects/stepwise/determinations_controller.rb
@@ -5,8 +5,13 @@ class Tasks::CollectionObjects::Stepwise::DeterminationsController < Application
   end
 
   def data
+    s = ::Queries::CollectionObject::Filter.new(
+      taxon_determinations: false,
+      project_id: sessions_current_project_id
+    )
+
     render json: CollectionObject.select('buffered_determinations, count(buffered_determinations) count_buffered').where('buffered_determinations is not null')
-      .where(project_id: sessions_current_project_id)
+      .where(id: s.all)
       .group('buffered_determinations')
       .having('count(buffered_determinations) > ?', params[:count_cutoff] || 10)
       .order('count(buffered_determinations) DESC')
--- db/schema.rb
+++ db/schema.rb
@@ -662,7 +662,6 @@ ActiveRecord::Schema.define(version: 2022_04_21_212203) do
     t.boolean "is_public"
     t.string "type"
     t.integer "total_records"
-    t.string "sha2"
     t.index ["created_by_id"], name: "index_downloads_on_created_by_id"
     t.index ["filename"], name: "index_downloads_on_filename"
     t.index ["project_id"], name: "index_downloads_on_project_id"
