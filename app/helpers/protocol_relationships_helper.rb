5c51b7b1ff97462111135694b235e8fc559aa780
mjy <diapriid@gmail.com>
2022-05-19 10:21:47 -0500

    Protocol relationship endpoint fix work

--- app/helpers/protocol_relationships_helper.rb
+++ app/helpers/protocol_relationships_helper.rb
@@ -4,6 +4,11 @@ module ProtocolRelationshipsHelper
     protocol_tag(protocol_relationship.protocol) + ': ' + object_tag(protocol_relationship.protocol_relationship_object)
   end
 
+  def label_for_protocol_relationship(protocol_relationship)
+    return nil if protocol_relationship.nil?
+    label_for_protocol(protocol_relationship.protocol) + ': ' + label_for(protocol_relationship.protocol_relationship_object)
+  end
+
   def protocol_relationship_link(protocol_relationship)
     return nil if protocol_relationship.nil?
     link_to(protocol_relationship_tag(protocol_relationship), protocol_relationship)
