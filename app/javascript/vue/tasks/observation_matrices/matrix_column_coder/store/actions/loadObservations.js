a8e94155a0cd49b174cb32bdec2add9d3d957e0d
José Luis Pereira <pepelui@outlook.com>
2022-08-01 20:15:21 -0300

    Add per limit

--- app/javascript/vue/tasks/observation_matrices/matrix_column_coder/store/actions/loadObservations.js
+++ app/javascript/vue/tasks/observation_matrices/matrix_column_coder/store/actions/loadObservations.js
@@ -3,7 +3,7 @@ import { MutationNames } from '../mutations/mutations'
 import makeObservation from '../../helpers/makeObservation'
 
 export default ({ commit }, { descriptorId, extend }) => {
-  Observation.where({ descriptor_id: descriptorId, extend }).then(({ body }) => {
+  Observation.where({ descriptor_id: descriptorId, extend, per: 5000 }).then(({ body }) => {
     body.forEach(obs => commit(MutationNames.SetObservation, makeObservation(obs)))
   })
 }
