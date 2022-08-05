25c6e82ad1f4332e83991a755848a930aa734153
mjy <diapriid@gmail.com>
2022-08-05 09:50:56 -0500

    WIP

--- app/helpers/public_contents_helper.rb
+++ app/helpers/public_contents_helper.rb
@@ -1,2 +1,18 @@
 module PublicContentsHelper
+
+  def public_content_renderer(public_content, style = :default)
+
+    case style.to_sym
+    when :default
+      MARKDOWN_HTML.render(public_content.text).html_safe  
+    when :linked_otu # only link OTU
+
+    when :linked_all
+
+    end
+  end
+
+  def foo
+  end
+
 end
