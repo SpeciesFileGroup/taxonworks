be6b1c04eca4ceaba2a12e1e3bb3665382cfcd04
Dmitry Dmitriev <arboridia@gmail.com>
2022-08-16 17:14:35 -0500

    paper_catalog

--- lib/catalog/nomenclature/paper_catalog.rb
+++ lib/catalog/nomenclature/paper_catalog.rb
@@ -56,10 +56,10 @@ class PaperCatalog < Catalog::Nomenclature
   # Returns list of taxon_name_ids
   attr_accessor :taxon_name_ids
 
-  # @!otu_ids
+  # @!otu_id_filter_array
   #   @return [Hash]
   # Returns list of otu_ids
-  attr_accessor :otu_ids
+  attr_accessor :otu_id_filter_array
 
   # @!sources_hash
   #   @return [Hash]
@@ -84,7 +84,7 @@ class PaperCatalog < Catalog::Nomenclature
 
     @classification_scope = classification_scope
     @project_id = project_id
-    @rank_filter = rank_filter
+    @rank_filter = rank_filter.to_s.downcase.split('|')
     @otu_filter = otu_filter
     @taxon_name_filter = taxon_name_filter
     @accepted_only = accepted_only
@@ -92,10 +92,11 @@ class PaperCatalog < Catalog::Nomenclature
     @distribution = distribution
     @otu_contents = otu_contents
     @taxon_name_ids = []
-    @otu_ids = []
+    @otu_id_filter_array = otu_filter_array
 
     #Main logic
     @results_hash = {names: {}, literature: {}}
+    @position = 0
 
     build_the_list
   end
@@ -106,11 +107,23 @@ class PaperCatalog < Catalog::Nomenclature
     return {} if classification_scope.blank? && otu_filter.blank? && taxon_name_filter.blank?
 
     taxon = classification_scope.blank? ? nil : TaxonName.find(classification_scope.to_i)
-    unless taxon.nil?
+
+    if !taxon_name_filter.blank?
+      tn_ids = taxon_name_filter.to_s.split('|').map(&:to_i)
+      TaxonName.where(id: tn_ids).each do |t|
+        add_taxon_to_results(t) if t.id == t.cached_valid_taxon_name_id
+      end
+    elsif !taxon.nil?
       ancestors = get_ancestors(taxon)
       descendants = get_descendants(taxon)
     end
+    if results_hash.names.count > 0 && accepted_only.to_s != 'true'
+      synonyms = get_synonyms
+    end
+  end
 
+  def otu_filter_array
+    otu_filter.blank? ? nil : otu_filter.to_s.split('|').map(&:to_i)
   end
 
   def get_ancestors(taxon)
@@ -126,12 +139,7 @@ class PaperCatalog < Catalog::Nomenclature
     #"Protonym.named('Zygina').order_by_rank(RANKS)"
 
     tn.each do |t|
-      results_hash.names[t.id] = {name: t.cached,
-                                  author_year: t.author_year,
-                                  statuses: [],
-                                  synonyms: [],
-
-      }
+      add_taxon_to_results(t)
     end
   end
 
@@ -140,11 +148,31 @@ class PaperCatalog < Catalog::Nomenclature
     tn = TaxonName.find_by_sql("SELECT taxon_names.* FROM taxon_names INNER JOIN (SELECT a1.d, STRING_AGG(a1.str, '|') AS cl FROM (SELECT taxon_name_hierarchies.descendant_id AS d, a.name AS str FROM taxon_names AS a INNER JOIN taxon_name_hierarchies ON taxon_name_hierarchies.ancestor_id = a.id WHERE taxon_name_hierarchies.descendant_id IN (#{tn_ids.join(', ')}) ORDER BY taxon_name_hierarchies.descendant_id, taxon_name_hierarchies.generations DESC) as a1 GROUP BY a1.d) AS aa ON taxon_names.id = aa.d ORDER BY aa.cl")
 
     tn.each do |t|
-      results_hash.names[t.id] = {name: t.cached,
-                                  author_year: t.author_year,
-                                  statuses: [],
-                                  synonyms: [],
-      }
+      add_taxon_to_results(t)
+    end
+  end
+
+  def add_taxon_to_results(t)
+    next if !rank_filter.empty? && !rank_filter.include?(t.rank_name)
+    @position += 1
+    results_hash.names[t.id] = {name: t.cached_html,
+                                author_year: t.author_year,
+                                #original_name: t.cached_original_combination_html,
+                                #original_author_year: t.original_author_year,
+                                global_id: t.to_global_id.to_s,
+                                prosition: @position,
+                                statuses: [],
+                                synonyms: [],
+    }
+  end
+
+  def get_synonyms
+    TaxonName.select('taxon_names.*, citations.pages, sources.id AS source_id, sources.cached_author_string, sources.year, sources.cached AS source_cached')
+             .joins("LEFT OUTER JOIN citations ON citations.citation_object_id = taxon_names.id AND citations.citation_object_type = 'TaxonName'")
+             .joins("LEFT OUTER JOIN sources ON citations.source_id = sources.id")
+             .where(cached_valid_taxon_name_id: results_hash.names.keys)
+             .order(:cached_nomenclature_date, :cached_is_valid).each do |t|
+
     end
   end
 
