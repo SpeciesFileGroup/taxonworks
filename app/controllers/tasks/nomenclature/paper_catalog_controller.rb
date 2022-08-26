cb58547877a24e7d5ea44bedcc6a2fd4344751a1
mjy <diapriid@gmail.com>
2022-08-25 20:54:19 -0500

    Form work

--- app/controllers/tasks/nomenclature/paper_catalog_controller.rb
+++ app/controllers/tasks/nomenclature/paper_catalog_controller.rb
@@ -1,6 +1,6 @@
 class Tasks::Nomenclature::PaperCatalogController < ApplicationController
   include TaskControllerConfiguration
-
+  
   # GET /tasks/nomenclature/paper_catalog?taxon_name_id=123
   def index
     if params[:taxon_name_id]
@@ -9,9 +9,19 @@ class Tasks::Nomenclature::PaperCatalogController < ApplicationController
       @taxon_name = nil
     end
   end
-
+  
   def preview
     @taxon_name = TaxonName.where(project_id: sessions_current_project_id).find(params[:taxon_name_id])
-  end
+    @body, @sources = helpers.recursive_catalog_tag(@taxon_name)
 
+    byebug
+
+    if params[:submit] == 'Download'
+      s = render_to_string(partial: '/tasks/nomenclature/paper_catalog/preview', layout: false, formats: [:html])
+      send_data(s, filename: "#{taxon_name.name}_catalog_#{DateTime.now}.xml", type: 'text/plain') and return
+    end
+    
+    
+  end
+  
 end
