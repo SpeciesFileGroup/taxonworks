f50780ec8a0186407f99f504c86b00fecc7fd228
mjy <diapriid@gmail.com>
2022-09-28 16:01:21 -0500

    Fix row -> column param

--- app/controllers/observations_controller.rb
+++ app/controllers/observations_controller.rb
@@ -115,9 +115,9 @@ class ObservationsController < ApplicationController
     end
   end
 
-  # POST /observations/code_row.json?observation_matrix_row_id=123&observation_matrix_id=456&<observation params>
+  # POST /observations/code_row.json?observation_matrix_column_id=123&observation_matrix_id=456&<observation params>
   def code_row
-    if Observation.code_row(params[:observation_matrix_id], params[:observation_matrix_row_id], observation_params )
+    if Observation.code_row(params[:observation_matrix_id], params[:observation_matrix_column_id], observation_params )
       render json: {success: true}
     else
       render json: {success: false}
