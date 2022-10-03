b7e3fced90f7d2d988074adf1a90fa471c4984cd
mjy <diapriid@gmail.com>
2022-10-03 15:48:09 -0500

    Revert to allow one true, one false value to be created. UI update needed.

--- app/models/observation/presence_absence.rb
+++ app/models/observation/presence_absence.rb
@@ -3,15 +3,8 @@
 class Observation::PresenceAbsence < Observation
   validates :presence, inclusion: {in:  [true, false]}
 
-  # TODO: this validation is technically not correct. The use case
-  # is that citations exist for both a presence and and absence.
-  # For example if the OTU is poorly defined, a fish at different stages, etc.
-  #
-  # We will have modify the UI when this is changed
-  #
-  # The true validation is that one true, and one false Observation are allowed.
-  #
-  validates_uniqueness_of :descriptor_id, scope: [:observation_object_id, :observation_object_type], message: 'the observation already exists'
+  # !! Note that this allows one true, and one false value to be created.  The UI will have to be updated to reflect this. 
+  validates_uniqueness_of :descriptor_id, scope: [:presence, :observation_object_id, :observation_object_type], message: 'the observation already exists'
 
   protected
 
