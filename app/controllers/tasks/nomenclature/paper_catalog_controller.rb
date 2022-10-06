af016f8bdec57477de68f3b8935afb4ba55b9045
mjy <diapriid@gmail.com>
2022-10-06 15:23:36 -0500

    Zip and send HTML formatted paper catalog

--- app/controllers/tasks/nomenclature/paper_catalog_controller.rb
+++ app/controllers/tasks/nomenclature/paper_catalog_controller.rb
@@ -12,12 +12,22 @@ class Tasks::Nomenclature::PaperCatalogController < ApplicationController
 
   def preview
     @taxon_name = TaxonName.where(project_id: sessions_current_project_id).find(params[:taxon_name_id])
+
+    c = @taxon_name.descendants.that_is_valid.count
+    if c > 5000
+      redirect_to :index, message: "Export of #{c} is too presently too large." and return
+    end
+
     @data = helpers.recursive_catalog_tag(@taxon_name)
 
+    # TODO: move logic out
     if params[:submit] == 'Download'
       s = render_to_string(partial: '/tasks/nomenclature/paper_catalog/preview', locals: {taxon_name: @taxon_name, data: @data }, layout: false, formats: [:html])
-      send_data(s, filename: "#{@taxon_name.name}_catalog_#{DateTime.now}.xml", type: 'text/plain') and return
+      a = ::Export::PaperCatalog.export(@taxon_name, s)
+      send_data(File.read(a), filename: "#{@taxon_name.name}_paper_catalog_#{DateTime.now}.zip", type: 'application/zip') and return
     end
+
+    render :index
   end
 
 end
