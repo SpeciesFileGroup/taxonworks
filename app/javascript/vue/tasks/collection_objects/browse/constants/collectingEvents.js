3146879191516e7847c8545cf7804023c78628d3
mjy <diapriid@gmail.com>
2022-10-07 14:39:07 -0500

    Set CE order/display

--- app/javascript/vue/tasks/collection_objects/browse/constants/collectingEvents.js
+++ app/javascript/vue/tasks/collection_objects/browse/constants/collectingEvents.js
@@ -1,2 +1,11 @@
-export const PRIORITIZE_ATTRIBUTES = []
-export const HIDE_ATTRIBUTES = ['identifiers']
+export const PRIORITIZE_ATTRIBUTES = [
+    'id',
+    'verbatim_label',
+    'verbatim_collectors',
+    'verbatim_method',
+    'verbatim_habitat',
+    'verbatim_locality',
+    'verbatim_latitude',
+    'verbatim_longitude',
+    'verbatim_date']
+export const HIDE_ATTRIBUTES = ['geographic_area_id', 'identifiers', 'url_for', 'project_id', 'object_url', 'object_label', 'object_tag', 'cached', 'created_by_id', 'updated_by_id', 'base_class', 'md5_of_verbatim_label']
