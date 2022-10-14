895eddddd61eaa2a88ea66f11d081bbb136a01f5
José Luis Pereira <pepelui@outlook.com>
2022-10-14 09:04:33 -0500

    Add character state filter #3141

---
+++ app/javascript/vue/tasks/observation_matrices/matrix_column_coder/MatrixColumnCoder/Option/OptionCharacterStateFilter.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <VModal
+      v-if="showModal"
+      @close="showModal = false"
+    >
+      <template #header>
+        <h3>Show scored character states</h3>
+      </template>
+      <template #body>
+        <ul class="no_bullets">
+          <li
+            v-for="characterState in descriptor.characterStates"
+            :key="characterState.id"
+          >
+            <label>
+              <input
+                v-model="filterCharacterStates"
+                :value="characterState.id"
+                type="checkbox"
+              >
+              {{ characterState.name }}
+            </label>
+          </li>
+        </ul>
+      </template>
+    </VModal>
+    <VBtn
+      color="primary"
+      medium
+      @click="showModal = true"
+    >
+      Show scored character states
+    </VBtn>
+  </div>
+</template>
+
+<script setup>
+import { useStore } from 'vuex'
+import { computed, ref } from 'vue'
+import { GetterNames } from '../../store/getters/getters'
+import { MutationNames } from '../../store/mutations/mutations'
+import VModal from 'components/ui/Modal.vue'
+import VBtn from 'components/ui/VBtn/index.vue'
+
+const store = useStore()
+const showModal = ref(false)
+const filterCharacterStates = computed({
+  get: () => store.getters[GetterNames.GetDisplayCharacterStates],
+  set: value => store.commit(MutationNames.SetDisplayCharacterStates, value)
+})
+
+const descriptor = computed(() => store.getters[GetterNames.GetDescriptor])
+
+</script>
--- app/javascript/vue/tasks/observation_matrices/matrix_column_coder/store/getters/getRowObjects.js
+++ app/javascript/vue/tasks/observation_matrices/matrix_column_coder/store/getters/getRowObjects.js
@@ -1,1 +1,48 @@
-export default state => state.rowObjects
+export default state => {
+  const {
+    descriptor,
+    rowObjects,
+    observations
+  } = state
+
+  const {
+    showOnlyUnscoredRows,
+    characterStates
+  } = state.options
+
+  const rowObjectIdsWithObservations = characterStates.length
+    ? getRowObjectIdsByCharacterStateCreated(observations, characterStates)
+    : getRowObjectIdsByObservationCreated(observations)
+
+  if (characterStates.length) {
+    return rowObjects.filter(item => rowObjectIdsWithObservations.includes(item.id))
+  }
+
+  return showOnlyUnscoredRows
+    ? rowObjects.filter(item => !rowObjectIdsWithObservations.includes(item.id))
+    : rowObjects
+}
+
+function getRowObjectIdsByObservationCreated (observations) {
+  const ids = []
+
+  observations.forEach(({ id, rowObjectId }) => {
+    if (id) {
+      ids.push(rowObjectId)
+    }
+  })
+
+  return ids
+}
+
+function getRowObjectIdsByCharacterStateCreated (observations, characterStates) {
+  const ids = []
+
+  observations.forEach(({ id, characterStateId, rowObjectId }) => {
+    if (id && characterStates.includes(characterStateId)) {
+      ids.push(rowObjectId)
+    }
+  })
+
+  return ids
+}
