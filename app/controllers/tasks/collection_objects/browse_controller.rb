0bf7237780cc91371ad0550196492bbc731a9b94
mjy <diapriid@gmail.com>
2022-10-30 19:53:50 -0500

    Cleanup view and controller action

--- app/controllers/tasks/collection_objects/browse_controller.rb
+++ app/controllers/tasks/collection_objects/browse_controller.rb
@@ -1,24 +1,16 @@
 class Tasks::CollectionObjects::BrowseController < ApplicationController
   include TaskControllerConfiguration
 
-  before_action :set_collection_object, only: [:index]
-
   # GET
   def index
-     @data = ::Catalog::CollectionObject.data_for(@collection_object)
-  end
-
-  protected
-
-  def set_collection_object
-    id = params[:collection_object_id]
-    id ||= CollectionObject.with_project_id(sessions_current_project_id).limit(1).pluck(:id)[0]
-    redirect_to new_collection_object_path, notice: 'Create a collection object first.' and return if id.nil?
-    @collection_object = CollectionObject
-      .where(project_id: sessions_current_project_id)
-      .preload(:identifiers, :collecting_event, depictions: [:image], taxon_determinations: [:determiners])
-      .find(id)
-    @collection_object.set_dwc_occurrence
+    q = CollectionObject.where(project_id: sessions_current_project_id)
+    if id = params[:collection_object_id]
+      @collection_object = q.find(id)
+    elsif id = q.limit(1).pluck(:id).first
+      redirect_to browse_collection_objects_task_path(collection_object_id: id) and return
+    else
+      redirect_to new_collection_object_path, notice: 'Create a collection object first.' and return
+    end
   end
 
 end
