39f1372449960e0b8a88057e5f54a70572fce7ea
José Luis Pereira <pepelui@outlook.com>
2022-11-07 12:46:32 -0300

    Add by geographic area

--- app/javascript/vue/components/radials/filter/links/CollectingEvent.js
+++ app/javascript/vue/components/radials/filter/links/CollectingEvent.js
@@ -3,5 +3,10 @@ export const CollectingEvent = [
     label: 'By collection object',
     link: '/tasks/collecting_events/filter',
     params: ['collection_object_id']
+  },
+  {
+    label: 'By geographic area',
+    link: '/tasks/collecting_events/filter',
+    params: ['geographic_area_id']
   }
 ]
