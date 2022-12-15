1cd6acbad40e4e6a28d9fd32e4cad6fae52fe1bf
jlpereira <pepelui@outlook.com>
2022-12-15 19:06:03 -0300

    Add navigation arrows

--- app/javascript/vue/tasks/collection_objects/browse/components/CONavegation.vue
+++ app/javascript/vue/tasks/collection_objects/browse/components/CONavegation.vue
@@ -1,13 +1,36 @@
 <template>
   <div>
-    <button
-      type="button"
-      class="button normal-input button-default"
+    <VBtn
+      medium
+      color="primary"
+      :disabled="!previousByCurrent"
+      @click="loadCO(previousByCurrent)"
+    >
+      <VIcon
+        x-small
+        name="arrowLeft"
+      />
+    </VBtn>
+    <VBtn
+      medium
+      class="margin-small-left margin-small-right"
+      color="primary"
       @click="isVisible = true"
       :disabled="!navigate.current_collection_object"
     >
       Go
-    </button>
+    </VBtn>
+    <VBtn
+      medium
+      color="primary"
+      :disabled="!nextByCurrent"
+      @click="loadCO(nextByCurrent)"
+    >
+      <VIcon
+        x-small
+        name="arrowRight"
+      />
+    </VBtn>
     <modal-component
       v-if="isVisible"
       @close="isVisible = false"
@@ -65,12 +88,17 @@ import { computed, ref } from 'vue'
 import { GetterNames } from '../store/getters/getters'
 import ModalComponent from 'components/ui/Modal'
 import loadCO from '../utils/loadCO.js'
+import VBtn from 'components/ui/VBtn/index.vue'
+import VIcon from 'components/ui/VIcon/index.vue'
 
 const store = useStore()
 const navigate = computed(() => store.getters[GetterNames.GetNavigation])
 const collectionObject = computed(() => store.getters[GetterNames.GetCollectionObject])
+const currentNavigation = ref('identifier')
+
+const previousByCurrent = computed(() => navigate.value?.previous_by && navigate.value.previous_by[currentNavigation.value])
+const nextByCurrent = computed(() => navigate.value?.previous_by && navigate.value.next_by[currentNavigation.value])
 
 const isVisible = ref(false)
-
 
 </script>
