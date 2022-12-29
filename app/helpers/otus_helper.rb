3be9930f4df1cc08c783687a9230d44386c898f8
Dmitry Dmitriev <arboridia@gmail.com>
2022-12-29 16:19:23 -0600

    next otu error fix

--- app/helpers/otus_helper.rb
+++ app/helpers/otus_helper.rb
@@ -71,7 +71,7 @@ module OtusHelper
       unless t.nil?
         while o.empty?
           o = t&.otus.to_a
-          exit if t.nil?
+          break if t.nil?
           t = t.next_sibling
         end
       end
