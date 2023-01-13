79d6b34389ced1cb7e1b305c96552d9d9177d80e
mjy <diapriid@gmail.com>
2023-01-13 12:23:10 -0600

    queryParam.js update

--- app/javascript/vue/components/radials/filter/constants/queryParam.js
+++ app/javascript/vue/components/radials/filter/constants/queryParam.js
@@ -1,19 +1,24 @@
 import {
-  COLLECTION_OBJECT,
+  ASSERTED_DISTRIBUTION,
+  BIOLOGICAL_ASSOCIATION,
   COLLECTING_EVENT,
-  SOURCE,
+  COLLECTION_OBJECT,
   EXTRACT,
   IMAGE,
-  PEOPLE,
-  TAXON_NAME,
   OTU,
-  BIOLOGICAL_ASSOCIATION,
-  ASSERTED_DISTRIBUTION
+  PEOPLE,
+  SOURCE,
+  TAXON_NAME,
 } from 'constants/index.js'
 
 export const QUERY_PARAM = {
-  [SOURCE]: 'source_query',
-  [OTU]: 'otu_query',
+  [IMAGE]: 'image_query',
+  [ASSERTED_DISTRIBUTION]: 'asserted_distribution_query',
+  [BIOLOGICAL_ASSOCIATION]: 'biological_association_query',
+  [COLLECTING_EVENT]: 'collecting_event_query,',
   [COLLECTION_OBJECT]: 'collection_object_query',
+  [EXTRACT]: 'extract_query,',
+  [OTU]: 'otu_query',
+  [SOURCE]: 'source_query',
   [TAXON_NAME]: 'taxon_name_query'
 }
