97c4fc45d7eccaf2a7933d8f08e9786fd4ead275
mjy <diapriid@gmail.com>
2023-01-16 18:48:57 -0600

    spelling

--- app/javascript/vue/tasks/otu/filter/components/FilterView.vue
+++ app/javascript/vue/tasks/otu/filter/components/FilterView.vue
@@ -16,10 +16,16 @@
   <FacetBiologicalRelationship v-model="params" />
   <FacetDescriptor v-model="params" />
   <FacetDataAttribute v-model="params" />
+  <FacetUsers v-model="params" />
   <FacetCitations
     title="Citations"
     v-model="params"
   />
+  <FacetTags
+    v-model="params"
+    target="Otu"
+  />
+  <FacetIdentifiers v-model="params" />
   <FacetWith
     v-for="param in WITH_PARAM"
     :key="param"
@@ -31,23 +37,26 @@
 
 <script setup>
 import { computed } from 'vue'
-import FacetTaxonName from 'components/Filter/Facets/TaxonName/FacetTaxonName.vue'
-import FacetGeographicArea from 'components/Filter/Facets/shared/FacetGeographic.vue'
-import FacetWith from 'components/Filter/Facets/shared/FacetWith.vue'
-import FacetWKT from 'components/Filter/Facets/Otu/FacetWKT.vue'
 import FacetBiologicalRelationship from 'components/Filter/Facets/BiologicalAssociation/FacetBiologicalRelationship.vue'
 import FacetCollectingEvent from 'tasks/biological_associations/filter/components/Facet/FacetCollectingEvent.vue'
 import FacetDataAttribute from 'components/Filter/Facets/shared/FacetDataAttribute.vue'
 import FacetDescriptor from './Facet/FacetDescriptor.vue'
+import FacetGeographicArea from 'components/Filter/Facets/shared/FacetGeographic.vue'
 import FacetHistorialDeterminations from './Facet/FacetHistorialDeterminations.vue'
 import FacetOtuName from './Facet/FacetOtuName.vue'
+import FacetTaxonName from 'components/Filter/Facets/TaxonName/FacetTaxonName.vue'
+import FacetUsers from 'components/Filter/Facets/shared/FacetUsers.vue'
+import FacetWKT from 'components/Filter/Facets/Otu/FacetWKT.vue'
+import FacetWith from 'components/Filter/Facets/shared/FacetWith.vue'
+import FacetTags from 'components/Filter/Facets/shared/FacetTags.vue'
+import FacetIdentifiers from 'components/Filter/Facets/shared/FacetIdentifiers.vue'
 
 const WITH_PARAM = [
   'citations',
   'origin_citation',
   'biological_associations',
   'asserted_distributions',
-  'daterminations',
+  'determinations',
   'depictions',
   'observations',
   'taxon_name'
