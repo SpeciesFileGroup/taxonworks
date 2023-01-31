f42b25ab10d16f626ddc6644b2f2569177f299bc
mjy <diapriid@gmail.com>
2023-01-31 07:44:01 -0600

    Remove taxon_name_cached populator task.
    We will populate with maintenence task

--- db/migrate/20230113094500_populate_taxon_name_cached_author.rb
+++
@@ -1,8 +0,0 @@
-class PopulateTaxonNameCachedAuthor < ActiveRecord::Migration[6.1]
-  # This is a data migration touching only cached values.
-  def change
-    TaxonName.where.not(parent_id: nil).each do |t|
-      t.update_column(:cached_author, t.get_author)
-    end
-  end
-end
