02f9fb6b9d2ce01136f58d3635fc077d68d671ec
mjy <diapriid@gmail.com>
2023-02-03 08:15:27 -0600

    Descriptor.js

---
+++ app/javascript/vue/components/radials/filter/links/Descriptor.js
@@ -0,0 +1,17 @@
+import {
+  FILTER_OBSERVATION,
+  FILTER_OTU,
+  FILTER_SOURCE,
+} from '../constants/filterLinks'
+
+export const Desriptor = {
+  all: [
+    FILTER_OBSERVATION,
+    FILTER_OTU,
+    FILTER_SOURCE,
+  ],
+  ids: [ 
+    FILTER_OBSERVATION,
+    FILTER_OTU,
+  ]
+}
