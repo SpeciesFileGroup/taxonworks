18fe079fdf4952de2c2f7131221ec504bf8e33de
mjy <diapriid@gmail.com>
2023-03-05 09:43:07 -0600

    Revert serial autocomplete project_id optimization.
    Adds specs to illustrate issue when optimization is added back.

--- lib/queries/serial/autocomplete.rb
+++ lib/queries/serial/autocomplete.rb
@@ -33,24 +33,25 @@ module Queries
 
         queries.compact!
 
-        updated_queries = []
-        queries.each_with_index do |q ,i|
-          a = q
-          if project_id.present?
-            # a = q.joins(:project_sources).where(member_of_project_id.to_sql) if project_id && limit_to_project
-            a = a.select("serials.*, COUNT(project_sources.source_id) AS use_count, CASE WHEN project_sources.project_id IN (#{project_id.join(',')}) THEN project_sources.project_id ELSE NULL END AS in_project")
-                 .left_outer_joins(:sources)
-                 .joins('LEFT OUTER JOIN project_sources ON sources.id = project_sources.source_id')
-                 .where('project_sources.project_id IN (?) OR project_sources.project_id NOT IN (?)', project_id, project_id) # TODO: now an array, change
-                 .group('serials.id, project_sources.project_id')
-                 .order('in_project, use_count DESC')
-          end
-          a ||= q
-          updated_queries[i] = a
-        end
+    #   updated_queries = []
+    #   queries.each_with_index do |q ,i|
+    #     a = q
+    #     if project_id.present?
+    #       a = a.select("serials.*, COUNT(project_sources.source_id) AS use_count, CASE WHEN project_sources.project_id IN (#{project_id.join(',')}) THEN project_sources.project_id ELSE NULL END AS in_project")
+    #            .left_outer_joins(:sources)
+    #            .joins('LEFT OUTER JOIN project_sources ON sources.id = project_sources.source_id')
+    #            .where('project_sources.project_id IN (?) OR project_sources.project_id NOT IN (?)', project_id, project_id)
+    #            .group('serials.id, project_sources.project_id')
+    #            .order('in_project, use_count DESC')
+    #     end
+    #     a ||= q
+    #     updated_queries[i] = a
+    #   end
 
         result = []
-        updated_queries.each do |q|
+     
+     
+        queries.each do |q|
           result += q.to_a
           result.uniq!
           break if result.count > 19
