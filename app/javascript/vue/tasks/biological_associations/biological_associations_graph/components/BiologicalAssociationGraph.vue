65d6a4b073d591523bc5d9b017bd023f0aeb5c23
jlpereira <pepelui@outlook.com>
2023-03-22 18:56:53 -0300

    Add citation icon

--- app/javascript/vue/tasks/biological_associations/biological_associations_graph/components/BiologicalAssociationGraph.vue
+++ app/javascript/vue/tasks/biological_associations/biological_associations_graph/components/BiologicalAssociationGraph.vue
@@ -38,6 +38,41 @@
       v-model:selected-edges="selectedEdges"
       v-model:layouts="layouts"
     >
+      <template #edge-overlay="{ scale, pointAtLength, edgeId, edge }">
+        <g
+          v-if="getObjectByUuid(edgeId)?.citations.length"
+          class="edge-icon"
+          @contextmenu.stop="
+            ($event) => {
+              $event.preventDefault()
+              openCitationModalFor([edgeId])
+            }
+          "
+        >
+          <circle
+            :cx="pointAtLength(40 * scale).x"
+            :cy="pointAtLength(40 * scale).y"
+            :r="10 * scale"
+            :stroke="edge.color"
+            :stroke-width="2 * scale"
+            :fill="edge.sourceState === 'off' ? '#fcc' : '#fff'"
+          />
+          <path
+            v-if="getObjectByUuid(edgeId)?.citations.length"
+            class="marker"
+            :fill="configs.edge.normal.color(edge)"
+            :transform="makeTransform(pointAtLength(40 * scale), scale, 1.0)"
+            d="M5.9-2.1L4,4.5C3.8,5.1,3.1,5.5,2.5,5.5h-6.6c-0.7,0-1.5-0.6-1.8-1.3C-6,3.9-6,3.6-5.9,3.3c0-0.2,0-0.3,0-0.5
+		c0-0.1-0.1-0.2,0-0.3c0-0.2,0.2-0.3,0.3-0.5c0.2-0.4,0.5-0.9,0.5-1.3c0-0.1,0-0.3,0-0.4c0-0.1,0.2-0.2,0.2-0.4
+		c0.2-0.3,0.5-1,0.5-1.3c0-0.2-0.1-0.3,0-0.4C-4.4-2-4.2-2-4.1-2.2c0.2-0.2,0.5-0.9,0.5-1.3c0-0.1-0.1-0.2,0-0.4
+		c0-0.1,0.2-0.3,0.3-0.5C-3-4.8-2.9-5.7-2-5.5v0c0.1,0,0.2-0.1,0.4-0.1h5.5c0.3,0,0.6,0.2,0.8,0.4c0.2,0.3,0.2,0.6,0.1,0.9L2.8,2.3
+		C2.5,3.5,2.3,3.7,1.4,3.7h-6.3c-0.1,0-0.2,0-0.3,0.1s-0.1,0.2,0,0.3c0.2,0.4,0.6,0.5,1,0.5h6.6c0.3,0,0.6-0.2,0.6-0.4l2.2-7.1
+		c0-0.1,0-0.3,0-0.4C5.5-3.2,5.7-3.1,5.8-3C6-2.7,6-2.4,5.9-2.1z M-2.3-0.9h4.4c0.1,0,0.3-0.1,0.3-0.2l0.2-0.5c0-0.1,0-0.2-0.2-0.2
+		H-2c-0.1,0-0.3,0.1-0.3,0.2l-0.2,0.5C-2.5-1-2.4-0.9-2.3-0.9z M-1.7-2.8h4.4C2.8-2.8,2.9-2.9,3-3l0.2-0.5c0-0.1,0-0.2-0.2-0.2h-4.4
+		c-0.1,0-0.3,0.1-0.3,0.2L-1.8-3C-1.9-2.9-1.8-2.8-1.7-2.8z"
+          />
+        </g>
+      </template>
       <template #edge-label="{ edge, ...slotProps }">
         <VEdgeLabel
           :text="edge.label"
@@ -331,6 +366,16 @@ function setGraph(graphId) {
   loadGraph(graphId).then((_) => {
     graph.value.fitToContents()
   })
+}
+
+function makeTransform(position, scale, width) {
+  const posX = position.x
+  const posY = position.y
+
+  return [
+    `translate(${posX} ${posY})`,
+    `scale(${scale * width}, ${scale * width})`
+  ].join(' ')
 }
 
 function addNodeObject(obj) {
