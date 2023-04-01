9217e5730364d4064092d22e2b7debf74cf8fd87
jlpereira <pepelui@outlook.com>
2023-03-31 21:39:07 -0300

    Refactor object graph task

--- app/javascript/vue/tasks/graph/object_graph/app.vue
+++ app/javascript/vue/tasks/graph/object_graph/app.vue
@@ -1,47 +1,57 @@
 <template>
   <h1>Task: Object graph</h1>
-  <div class="horizontal-left-content middle">
-    <h3 v-if="currentNode">
-      Target: {{ currentNode.name }}
-    </h3>
-    <div
-      v-if="currentNode"
-      class="square-brackets margin-medium-left">
-      <ul class="no_bullets context-menu">
-        <li>
-          <a
-            :href="`/graph/${encodeURIComponent(currentGlobalId)}/object`"
-            target="_blank">
-            JSON
-          </a>
-        </li>
-        <li>
-          <v-btn
-            color="primary"
-            circle
-            @click="downloadTextFile(svgElement.outerHTML, 'image/svg+xml', 'graph.svg')"
-          >
-            <v-icon
-              color="white"
-              x-small
-              name="download"
-            />
-          </v-btn>
-        </li>
-        <li>
-          <radial-annotator :global-id="currentNode.id" />
-        </li>
-        <li>
-          <radial-navigation :global-id="currentNode.id" />
-        </li>
-      </ul>
+  <div class="panel content margin-medium-bottom">
+    <div class="horizontal-left-content middle">
+      <h3 v-if="currentNode">Target: {{ currentNode.name }}</h3>
+      <h3 v-else>Target: None</h3>
+      <div
+        v-if="currentNode"
+        class="square-brackets margin-medium-left"
+      >
+        <ul class="no_bullets context-menu">
+          <li>
+            <a
+              :href="`/graph/${encodeURIComponent(currentGlobalId)}/object`"
+              target="_blank"
+            >
+              JSON
+            </a>
+          </li>
+          <li>
+            <v-btn
+              color="primary"
+              circle
+              @click="
+                () =>
+                  downloadTextFile(
+                    svgElement.outerHTML,
+                    'image/svg+xml',
+                    'graph.svg'
+                  )
+              "
+            >
+              <v-icon
+                color="white"
+                x-small
+                name="download"
+              />
+            </v-btn>
+          </li>
+          <li>
+            <radial-annotator :global-id="currentNode.id" />
+          </li>
+          <li>
+            <radial-navigation :global-id="currentNode.id" />
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
-  <div class="graph-container">
+  <div class="graph-container panel">
     <div class="panel content graph-container__stats">
       <ul class="capitalize">
         <li
-          v-for="(value, key) in graph.stats"
+          v-for="(value, key) in stats"
           :key="key"
         >
           {{ key }}: {{ value }}
@@ -50,49 +60,10 @@
     </div>
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      :width="width+'px'"
-      :height="height+'px'"
+      :width="width + 'px'"
+      :height="height + 'px'"
       ref="svgElement"
-      @mousemove="drag($event)"
-      @mouseup="drop()"
-    >
-      <line
-        v-for="link in graph.links"
-        :key="link.index"
-        :x1="coords[link.source.index].x"
-        :y1="coords[link.source.index].y"
-        :x2="coords[link.target.index].x"
-        :y2="coords[link.target.index].y"
-        stroke="black"
-        stroke-width="2"
-      />
-      <g
-        v-for="node in graph.nodes"
-        class="cursor-pointer"
-        :key="node.id"
-        @mousedown="currentMove = { x: $event.screenX, y: $event.screenY, node: node }"
-        @mouseup="drop()"
-      >
-        <component
-          :is="componentForShape(node.shape)"
-          :size="size"
-          :x="coords[node.index].x || 0"
-          :y="coords[node.index].y || 0"
-          :color="node.color"
-          stroke="white"
-          stroke-width="1"
-          @dblclick="loadGraph(node.id)"
-        />
-        <text
-          :x="coords[node.index].x + size"
-          :y="coords[node.index].y"
-          dy=".3em"
-        >
-          {{ node.name }}
-        </text>
-        <title>{{ node.name }}</title>
-      </g>
-    </svg>
+    ></svg>
   </div>
   <v-spinner
     v-if="isLoading"
@@ -102,105 +73,180 @@
 
 <script setup>
 import * as d3 from 'd3'
-import { ref, computed } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 import SetParam from 'helpers/setParam'
 import AjaxCall from 'helpers/ajaxCall'
-import SvgCircle from './components/Svg/SvgCircle.vue'
-import SvgSquare from './components/Svg/SvgSquare.vue'
-import SvgTriangle from './components/Svg/SvgTriangle.vue'
-import SvgPerson from './components/Svg/SvgPerson.vue'
-import SvgHexagon from './components/Svg/SvgHexagon.vue'
-import SvgPentagon from './components/Svg/SvgPentagon.vue'
-import SvgOctagon from './components/Svg/SvgOctagon.vue'
 import RadialAnnotator from 'components/radials/annotator/annotator.vue'
 import RadialNavigation from 'components/radials/navigation/radial.vue'
 import VSpinner from 'components/spinner.vue'
 import VBtn from 'components/ui/VBtn/index.vue'
 import VIcon from 'components/ui/VIcon/index.vue'
 import { downloadTextFile } from 'helpers/files'
+import * as shapes from './components/Svg'
 
-const size = 10
-const width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0) - 20
-const height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0) - 200
-const isLoading = ref(false)
-const simulation = ref(null)
-const currentMove = ref(null)
-const currentGlobalId = ref()
+const size = 5
+
+const opt = {
+  size: 10
+}
+
+const width =
+  Math.max(document.documentElement.clientWidth, window.innerWidth || 0) - 20
+const height =
+  Math.max(document.documentElement.clientHeight, window.innerHeight || 0) - 200
+
+const stats = ref({})
 const svgElement = ref(null)
-const graph = ref({
-  nodes: [],
-  links: [],
-  stats: {}
-})
-const coords = computed(() => graph.value.nodes.map(node => ({
-  x: node.x,
-  y: node.y
-})))
+const currentNode = ref(null)
+const isLoading = ref(false)
+const currentGlobalId = ref(null)
+let nodes = []
+let links = []
+let simulation
 
-const currentNode = computed(() => graph.value.nodes.find(node => node.id === currentGlobalId.value))
+function initGraph() {
+  const graph = d3.select(svgElement.value)
+  graph.selectAll('g').remove()
+  const graphContainer = graph.append('g').attr('id', 'container')
+  const zoom = d3.zoom().on('zoom', function (e) {
+    graphContainer.attr('transform', e.transform)
+  })
 
-const AVAILABLE_SHAPES = {
-  square: SvgSquare,
-  circle: SvgCircle,
-  triangle: SvgTriangle,
-  person: SvgPerson,
-  hexagon: SvgHexagon,
-  pentagon: SvgPentagon,
-  octagon: SvgOctagon
-}
+  graph.call(zoom)
 
-const componentForShape = shapeType => AVAILABLE_SHAPES[shapeType] || SvgCircle
+  simulation = d3
+    .forceSimulation(nodes)
+    .force('charge', d3.forceManyBody().strength(-100))
+    .force('center', d3.forceCenter(width / 2, height / 2))
+    .force('link', d3.forceLink().links(links))
+    .on('tick', ticked)
 
-const drag = e => {
-  if (currentMove.value) {
-    currentMove.value.node.fx = currentMove.value.node.x - (currentMove.value.x - e.screenX)
-    currentMove.value.node.fy = currentMove.value.node.y - (currentMove.value.y - e.screenY)
-    currentMove.value.x = e.screenX
-    currentMove.value.y = e.screenY
+  function createNodes() {
+    graphContainer.append('g').attr('class', 'links')
+
+    graphContainer
+      .select('.links')
+      .selectAll('line')
+      .data(links)
+      .enter()
+      .append('line')
+
+    graphContainer.append('g').attr('class', 'nodes')
+
+    const nodeGroup = graphContainer
+      .select('.nodes')
+      .selectAll('.node')
+      .data(nodes)
+      .enter()
+      .append('g')
+      .attr('class', 'node')
+      .call(drag(simulation))
+
+    nodeGroup.append(function (d) {
+      return this.appendChild(shapes[d.shape || 'circle'](d, opt))
+    })
+
+    nodeGroup
+      .append('text')
+      .text(function (d) {
+        return d.name
+      })
+      .attr('dx', size * 2 + 4)
+      .attr('dy', size / 2)
+
+    nodeGroup
+      .on('click', (e, d) => {
+        currentNode.value = d
+      })
+      .on('dblclick', (e, d) => {
+        e.stopPropagation()
+        loadGraph(d.id)
+      })
   }
-}
-const drop = () => {
-  if (!currentMove.value) return
-  delete currentMove.value.node.fx
-  delete currentMove.value.node.fy
-  currentMove.value = null
-  simulation.value.alpha(1)
-  simulation.value.restart()
-}
 
-const loadGraph = globalId => {
-  currentGlobalId.value = globalId
-  isLoading.value = true
-  SetParam('/tasks/graph/object', 'global_id', globalId)
-  AjaxCall('get', `/graph/${encodeURIComponent(globalId)}/object`).then(({ body }) => {
-    graph.value = {
-      stats: body.stats,
-      nodes: body.nodes.map(node => ({ ...node, x: null, y: null })),
-      links: body.edges.map(link => ({
-        source: body.nodes.findIndex(node => node.id === link.start_id),
-        target: body.nodes.findIndex(node => node.id === link.end_id)
-      }))
+  createNodes()
+
+  function updateNodes() {
+    graphContainer.selectAll('.node').attr('transform', function (d) {
+      return `translate(${d.x}, ${d.y})`
+    })
+  }
+
+  function drag(simulation) {
+    function dragstarted(event) {
+      if (!event.active) simulation.alphaTarget(0.3).restart()
+      event.subject.fx = event.subject.x
+      event.subject.fy = event.subject.y
     }
 
-    simulation.value = d3.forceSimulation(graph.value.nodes)
-      .force('charge', d3.forceManyBody().strength(d => -80))
-      .force('link', d3.forceLink(graph.value.links))
-      .force('center', d3.forceCenter(width / 2, height / 2))
+    function dragged(event) {
+      event.subject.fx = event.x
+      event.subject.fy = event.y
+    }
 
-    isLoading.value = false
-  })
+    function dragended(event) {
+      if (!event.active) simulation.alphaTarget(0)
+      event.subject.fx = null
+      event.subject.fy = null
+    }
+
+    return d3
+      .drag()
+      .on('start', dragstarted)
+      .on('drag', dragged)
+      .on('end', dragended)
+  }
+
+  function updateLinks() {
+    graphContainer
+      .selectAll('line')
+      .attr('x1', (d) => d.source.x)
+      .attr('y1', (d) => d.source.y)
+      .attr('x2', (d) => d.target.x)
+      .attr('y2', (d) => d.target.y)
+  }
+
+  function ticked() {
+    updateNodes()
+    updateLinks()
+  }
 }
 
-const urlParams = new URLSearchParams(window.location.search)
-const globalId = urlParams.get('global_id')
+function loadGraph(globalId) {
+  currentGlobalId.value = globalId
+  isLoading.value = true
 
-if (globalId) {
-  loadGraph(globalId)
+  SetParam('/tasks/graph/object', 'global_id', globalId)
+  AjaxCall('get', `/graph/${encodeURIComponent(globalId)}/object`).then(
+    ({ body }) => {
+      stats.value = body.stats
+      nodes = body.nodes.map((node) => ({ ...node, x: null, y: null }))
+      links = body.edges.map((link) => ({
+        source: body.nodes.findIndex((node) => node.id === link.start_id),
+        target: body.nodes.findIndex((node) => node.id === link.end_id)
+      }))
+
+      initGraph()
+    }
+  )
+
+  isLoading.value = false
 }
 
+onMounted(() => {
+  const urlParams = new URLSearchParams(window.location.search)
+  const globalId = urlParams.get('global_id')
+
+  if (globalId) {
+    loadGraph(globalId)
+  }
+})
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
 .graph-container {
+  svg {
+    height: calc(100vh - 200px);
+  }
   position: relative;
 
   &__stats {
@@ -210,6 +256,12 @@ if (globalId) {
   ul {
     padding-left: 1em;
     margin: 0;
+  }
+
+  .links {
+    line {
+      stroke: #ccc;
+    }
   }
 }
 </style>
