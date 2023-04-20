8fc32aa06ce8eee26aed863d69234e13c9e83a76
Dmitry Dmitriev <arboridia@gmail.com>
2023-04-20 12:47:17 -0500

    image matrix error

--- lib/tools/image_matrix.rb
+++ lib/tools/image_matrix.rb
@@ -371,11 +371,10 @@ class Tools::ImageMatrix
     # Depictions is depictions with other attributes added
     depictions = nil
 
-    return h if observation_matrix_id.blank?
-
     if observation_matrix_id.to_i == 0 && !otu_filter.blank?
       depictions = observation_depictions_from_otu_filter
     else
+      return h if observation_matrix.nil?
       depictions = observation_matrix.observation_depictions
     end
 
