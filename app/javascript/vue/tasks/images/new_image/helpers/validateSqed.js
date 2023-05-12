4016c0455c5522370b172296564c0e52332077a6
jlpereira <pepelui@outlook.com>
2023-05-12 18:00:19 -0300

    Fix sqed validation

--- app/javascript/vue/tasks/images/new_image/helpers/validateSqed.js
+++ app/javascript/vue/tasks/images/new_image/helpers/validateSqed.js
@@ -1,7 +1,7 @@
 export default function (sqed) {
-  return !!(
-    sqed.boundary_color &&
-    (sqed.boundary_finder || sqed.metadata_map[0] === 'none') &&
+  return (
+    ((sqed.boundary_color && sqed.boundary_finder) ||
+      sqed.metadata_map[0] === 'none') &&
     sqed.layout
   )
 }
