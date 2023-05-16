6c0bdc0da1fb60b780f77dce9c9b4e802a6815b7
jlpereira <pepelui@outlook.com>
2023-05-16 15:18:56 -0300

    Fix sort name in filter nomenclature

--- app/javascript/vue/tasks/nomenclature/filter/utils/listParser.js
+++ app/javascript/vue/tasks/nomenclature/filter/utils/listParser.js
@@ -2,15 +2,13 @@ export function listParser(list) {
   return list.map((item) => ({
     id: item.id,
     global_id: item.global_id,
-    cached_html: `<a href="/tasks/nomenclature/browse?taxon_name_id=${item.id}">
-        ${item.cached_html}
-      </a>`,
+    cached_html: `<a title="${item.cached}" href="/tasks/nomenclature/browse?taxon_name_id=${item.id}">${item.cached_html}</a>`,
     cached_author_year: item.cached_author_year,
     original_combination: item.original_combination,
     cached_is_valid: item.cached_is_valid ? 'Yes' : 'No',
     rank: item.rank,
     parent: item?.parent
-      ? `<a href="/tasks/nomenclature/browse?taxon_name_id=${item.parent.id}">${item.parent.object_label}</a>`
+      ? `<a title="${item.parent.object_label}" href="/tasks/nomenclature/browse?taxon_name_id=${item.parent.id}">${item.parent.object_label}</a>`
       : ''
   }))
 }
