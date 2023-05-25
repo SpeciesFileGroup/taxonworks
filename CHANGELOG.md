42cfccce8dbf3559397a62ad6e5c1cd400bef1d5
jlpereira <pepelui@outlook.com>
2023-05-25 10:55:44 -0300

    Fix #3426

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -16,8 +16,10 @@ This project <em>does not yet</em> adheres to [Semantic Versioning](https://semv
 - Verbatim name contained 'Suffix' [#3425]
 - Observation matrix facet doesn't work in Filter descriptors
 - lock Is original and Is absent chekcboxes in citation form
+- Pagination doesn't work correctly in Filter biological associations [#3426]
 
-[3425]: https://github.com/SpeciesFileGroup/taxonworks/issues/3425
+[#3425]: https://github.com/SpeciesFileGroup/taxonworks/issues/3425
+[#3426]: https://github.com/SpeciesFileGroup/taxonworks/issues/3426
 
 ## [0.33.0] - 2023-05-19
 
--- app/javascript/vue/tasks/biological_associations/filter/utils/listParser.js
+++ app/javascript/vue/tasks/biological_associations/filter/utils/listParser.js
@@ -8,25 +8,28 @@ function parseRank(rank) {
 }
 
 export function listParser(result) {
-  return result.map((item) => ({
-    id: item.id,
-    global_id: item.global_id,
-    subject_taxonomy_order: parseRank(item.subject?.taxonomy?.order),
-    subject_taxonomy_family: parseRank(item.subject?.taxonomy?.family),
-    subject_taxonomy_genus: parseRank(item.subject?.taxonomy?.genus),
-    subject_object_tag: parseRank(item.subject.object_tag),
-    biological_property_subject: getBiologicalProperty(
-      item.biological_relationship_types,
-      'subject'
-    ),
-    biological_relationship: item.biological_relationship.object_tag,
-    biological_property_object: getBiologicalProperty(
-      item.biological_relationship_types,
-      'object'
-    ),
-    object_taxonomy_order: parseRank(item.object?.taxonomy?.order),
-    object_taxonomy_family: parseRank(item.object?.taxonomy?.family),
-    object_taxonomy_genus: parseRank(item.object?.taxonomy?.genus),
-    object_object_tag: parseRank(item.object.object_tag)
-  }))
+  return result.map((item) => {
+    console.log(item)
+    return {
+      id: item.id,
+      global_id: item.global_id,
+      subject_taxonomy_order: parseRank(item.subject?.taxonomy?.order),
+      subject_taxonomy_family: parseRank(item.subject?.taxonomy?.family),
+      subject_taxonomy_genus: parseRank(item.subject?.taxonomy?.genus),
+      subject_object_tag: parseRank(item.subject.object_tag),
+      biological_property_subject: getBiologicalProperty(
+        item.biological_relationship_types,
+        'subject'
+      ),
+      biological_relationship: item.biological_relationship.object_tag,
+      biological_property_object: getBiologicalProperty(
+        item.biological_relationship_types,
+        'object'
+      ),
+      object_taxonomy_order: parseRank(item.object?.taxonomy?.order),
+      object_taxonomy_family: parseRank(item.object?.taxonomy?.family),
+      object_taxonomy_genus: parseRank(item.object?.taxonomy?.genus),
+      object_object_tag: parseRank(item.object.object_tag)
+    }
+  })
 }
