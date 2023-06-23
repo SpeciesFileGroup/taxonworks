6d611e2e9eb5f4149071a4a95930e0d144d40e5e
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2023-06-23 17:27:36 -0300

    Async CE set_cached in DwC importer

--- app/jobs/dwc_occurrence_upsert_job.rb
+++ app/jobs/dwc_occurrence_upsert_job.rb
@@ -2,7 +2,7 @@ class DwcOccurrenceUpsertJob < ApplicationJob
   queue_as :dwc_occurrence_index
 
   def perform(collection_object)
-    collection_object.georeferences.each { |g| g.send(:set_cached) }
+    collection_object.collecting_event.set_cached
     collection_object.set_dwc_occurrence
   end
 end
