06a5f379b135a484bf9f4f90e967619b75536d62
jlpereira <pepelui@outlook.com>
2023-06-28 16:31:35 -0300

    Add data attributes for filter nomenclature

--- app/javascript/vue/tasks/nomenclature/filter/utils/listParser.js
+++ app/javascript/vue/tasks/nomenclature/filter/utils/listParser.js
@@ -1,4 +1,29 @@
-export function listParser(list) {
+import { DataAttribute } from 'routes/endpoints'
+
+function makeDataAttributeObjectHeaders(data) {
+  return Object.assign({}, ...data.index)
+}
+
+function getDataAttributesFor(data, objectId) {
+  const list = data.data.filter(([id]) => id === objectId)
+  const headers = makeDataAttributeObjectHeaders(data)
+
+  return Object.assign(
+    {},
+    ...list.map(([id, attrId, value]) => ({
+      [headers[attrId]]: value
+    }))
+  )
+}
+
+export async function listParser(list, { parameters }) {
+  const { extend, exclude, ...rest } = parameters
+  const { body } = await DataAttribute.brief({
+    taxon_name_query: {
+      ...rest,
+      paginate: true
+    }
+  })
   return list.map((item) => ({
     id: item.id,
     global_id: item.global_id,
@@ -9,6 +34,7 @@ export function listParser(list) {
     rank: item.rank,
     parent: item?.parent
       ? `<a title="${item.parent.object_label}" href="/tasks/nomenclature/browse?taxon_name_id=${item.parent.id}">${item.parent.object_label}</a>`
-      : ''
+      : '',
+    data_attributes: getDataAttributesFor(body, item.id)
   }))
 }
