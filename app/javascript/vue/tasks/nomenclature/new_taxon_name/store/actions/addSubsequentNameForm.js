a4b6cb81c7dd92bbedd994c5586e90301ba0a106
jlpereira <pepelui@outlook.com>
2023-08-11 17:10:51 -0300

    Update request

--- app/javascript/vue/tasks/nomenclature/new_taxon_name/store/actions/addSubsequentNameForm.js
+++ app/javascript/vue/tasks/nomenclature/new_taxon_name/store/actions/addSubsequentNameForm.js
@@ -1,14 +1,17 @@
-import ActionNames from './actionNames'
-import { TaxonName } from '@/routes/endpoints'
+import { MutationNames } from '../mutations/mutations'
+import { TaxonName, TaxonNameRelationship } from '@/routes/endpoints'
 import { TAXON_RELATIONSHIP_FAMILY_GROUP_NAME_FORM } from '@/constants'
 
-export default async ({ state, dispatch }, { name, citation }) => {
+export default async ({ state, commit }, { name, citation }) => {
   const currentTaxon = state.taxon_name
   const payload = {
     name,
     parent_id: currentTaxon.parent_id,
     rank_class: currentTaxon.rank_string,
-    type: 'Protonym'
+    type: 'Protonym',
+    family_group_name_form_relationship_attributes: {
+      object_taxon_name_id: currentTaxon.id
+    }
   }
 
   if (citation.source_id) {
@@ -20,10 +23,14 @@ export default async ({ state, dispatch }, { name, citation }) => {
   return TaxonName.create({
     taxon_name: payload
   }).then(({ body }) =>
-    dispatch(ActionNames.AddTaxonRelationship, {
+    TaxonNameRelationship.where({
       type: TAXON_RELATIONSHIP_FAMILY_GROUP_NAME_FORM,
       object_taxon_name_id: currentTaxon.id,
       subject_taxon_name_id: body.id
+    }).then(({ body }) => {
+      body.forEach((item) => {
+        commit(MutationNames.AddTaxonRelationship, item)
+      })
     })
   )
 }
