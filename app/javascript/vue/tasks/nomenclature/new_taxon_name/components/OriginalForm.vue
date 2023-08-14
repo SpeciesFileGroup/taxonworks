ec7a9aeefbd6d0c5c0a28c5e69f7ec5f25e410fa
jlpereira <pepelui@outlook.com>
2023-08-14 11:41:40 -0300

    Change input for edit in place

--- app/javascript/vue/tasks/nomenclature/new_taxon_name/components/OriginalForm.vue
+++ app/javascript/vue/tasks/nomenclature/new_taxon_name/components/OriginalForm.vue
@@ -8,19 +8,7 @@
     </template>
     <template #body>
       <div class="field horizontal-left-content gap-small">
-        <input
-          type="text"
-          placeholder="Type a name..."
-          v-model="taxon.verbatim_name"
-          @input="store.commit(MutationNames.UpdateLastChange)"
-        />
-        <VBtn
-          color="update"
-          medium
-          @click="saveChanges"
-        >
-          Save
-        </VBtn>
+        <EditInPlace v-model="verbatimName" />
       </div>
     </template>
   </block-layout>
@@ -29,17 +17,20 @@
 <script setup>
 import { computed } from 'vue'
 import { useStore } from 'vuex'
-import { ActionNames } from '../store/actions/actions'
 import { MutationNames } from '../store/mutations/mutations.js'
 import { GetterNames } from '../store/getters/getters'
-import VBtn from '@/components/ui/VBtn/index.vue'
 
 import BlockLayout from '@/components/layout/BlockLayout'
+import EditInPlace from '@/components/editInPlace.vue'
 
 const store = useStore()
 const taxon = computed(() => store.getters[GetterNames.GetTaxon])
 
-function saveChanges() {
-  store.dispatch(ActionNames.UpdateTaxonName, taxon.value)
-}
+const verbatimName = computed({
+  get: () => taxon.value.verbatim_name,
+  set: (value) => {
+    taxon.value.verbatim_name = value
+    store.commit(MutationNames.UpdateLastChange)
+  }
+})
 </script>
