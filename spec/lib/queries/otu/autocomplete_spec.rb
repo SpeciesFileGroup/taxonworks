fb2d2d2b96d2ec55c2dc1129b578e88df40a7e9c
mjy <diapriid@gmail.com>
2023-09-06 11:36:08 -0500

    API autocomplete fix candidate

--- spec/lib/queries/otu/autocomplete_spec.rb
+++ spec/lib/queries/otu/autocomplete_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Queries::Otu::Autocomplete, type: :model do
   let(:name) { 'Test' }
-  let!(:otu) { Otu.create!(name: name) }
+  let!(:otu) { Otu.create!(name:) }
 
   let(:other_project) { FactoryBot.create(:valid_project, name: 'other') }
 
@@ -13,9 +13,51 @@ describe Queries::Otu::Autocomplete, type: :model do
   end
 
   specify '#project_id' do
-    o = Otu.create!(project: other_project, name: name)
-    q = Queries::Otu::Autocomplete.new('Test', project_id: project_id)
+    o = Otu.create!(project: other_project, name:)
+    q = Queries::Otu::Autocomplete.new('Test', project_id:)
     expect(q.autocomplete).to contain_exactly(otu)
+  end
+
+  specify 'odd otus' do
+    o = FactoryBot.create(:valid_otu, name: 'smorf')
+    q = Queries::Otu::Autocomplete.new('smo', project_id:)
+    expect(q.autocomplete).to contain_exactly(o)
+  end
+
+  # having_taxon_name is always true here
+  context '#api_autocomplete' do
+    specify 'valid taxon name 1' do
+      o = FactoryBot.create(:valid_otu, name: nil, taxon_name: FactoryBot.create(:iczn_species, name: 'smo'))
+      q = Queries::Otu::Autocomplete.new('smo', project_id:)
+
+      expect(q.api_autocomplete).to contain_exactly(o)
+    end
+
+    specify 'invalid taxon name 1' do
+      a = FactoryBot.create(:iczn_species, name: 'smo')
+      b = FactoryBot.create(:iczn_species, name: 'rho')
+
+      c = TaxonNameRelationship::Iczn::Invalidating::Synonym.create!(subject_taxon_name: a, object_taxon_name: b)
+
+      o = FactoryBot.create(:valid_otu, name: nil, taxon_name: a )
+      q = Queries::Otu::Autocomplete.new('smo', project_id:)
+      expect(q.api_autocomplete).to contain_exactly(o)
+    end
+
+    specify 'invalid taxon name 2' do
+      a = FactoryBot.create(:iczn_species, name: 'smo')
+      b = FactoryBot.create(:iczn_species, name: 'rho')
+
+      c = TaxonNameRelationship::Iczn::Invalidating::Synonym.create!(subject_taxon_name: a, object_taxon_name: b)
+
+      o1 = FactoryBot.create(:valid_otu, name: nil, taxon_name: a )
+      o2 = FactoryBot.create(:valid_otu, name: 'smo' ) # no taxon name
+
+      q = Queries::Otu::Autocomplete.new('smo', project_id:)
+      expect(q.api_autocomplete).to contain_exactly(o1)
+    end
+
+
   end
 
 end
