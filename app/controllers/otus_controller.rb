417a904cefeddb357744dec4f03f0cffe3f1215a
mjy <diapriid@gmail.com>
2023-09-29 16:37:25 -0500

    Refactor /api/v1/otus/840512/inventory/dwc to return .csv or .json.
    to_json on DwcOccurrence does not return nil.!! This may be a breaking change.

--- app/controllers/otus_controller.rb
+++ app/controllers/otus_controller.rb
@@ -294,11 +294,18 @@ class OtusController < ApplicationController
 
   # GET /api/v1/otus/:id/inventory/dwc
   def api_dwc_inventory
-    send_data Export::Download.generate_csv(
-      DwcOccurrence.scoped_by_otu(@otu),
-      exclude_columns: ['id', 'created_by_id', 'updated_by_id', 'project_id', 'updated_at']),
-      type: 'text',
-      filename: "dwc_#{helpers.label_for_otu(@otu).gsub(/\W/,'_')}_#{DateTime.now}.csv"
+    respond_to do |format|
+      format.csv do
+        send_data Export::Download.generate_csv(
+          DwcOccurrence.scoped_by_otu(@otu),
+          exclude_columns: ['id', 'created_by_id', 'updated_by_id', 'project_id', 'updated_at']),
+          type: 'text',
+          filename: "dwc_#{helpers.label_for_otu(@otu).gsub(/\W/,'_')}_#{DateTime.now}.csv"
+      end
+      format.json do
+        render json: DwcOccurrence.scoped_by_otu(@otu).to_json
+      end
+    end
   end
 
   # GET /api/v1/otus/:id/inventory/content
