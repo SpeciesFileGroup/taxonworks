392ac13dcab305806e8dbc2c00f2ce91ef052deb
mjy <diapriid@gmail.com>
2023-10-10 10:08:36 -0500

    GeographicItem debugging tools

--- app/controllers/geographic_items_controller.rb
+++ app/controllers/geographic_items_controller.rb
@@ -1,13 +1,13 @@
 class GeographicItemsController < ApplicationController
   include DataControllerConfiguration::SharedDataControllerConfiguration
 
-  before_action :set_geographic_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_geographic_item, only: [:show, :edit, :update]
+  before_action :require_administrator_sign_in, only: [:update, :edit]
 
   # GET /geographic_items/1
   # GET /geographic_items/1.json
   # GET /geographic_items/1.wkt
   def show
-
     respond_to do |format|
       format.html {}
       format.json {}
@@ -21,24 +21,23 @@ class GeographicItemsController < ApplicationController
 
   end
 
+  # GET /geographic_items/1/edit
+  def edit
+  end
 
-# # GET /geographic_items/1/edit
-# def edit
-# end
-
-# # PATCH/PUT /geographic_items/1
-# # PATCH/PUT /geographic_items/1.json
-# def update
-#   respond_to do |format|
-#     if @geographic_item.update(geographic_item_params)
-#       format.html { redirect_to @geographic_item.metamorphosize, notice: 'Geographic item was successfully updated.' }
-#       format.json { head :no_content }
-#     else
-#       format.html { render action: 'edit' }
-#       format.json { render json: @geographic_item.errors, status: :unprocessable_entity }
-#     end
-#   end
-# end
+  # PATCH/PUT /geographic_items/1
+  # PATCH/PUT /geographic_items/1.json
+  def update
+    respond_to do |format|
+      if @geographic_item.update(geographic_item_params)
+        format.html { redirect_to @geographic_item.metamorphosize, notice: 'Geographic item was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @geographic_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
 # # DELETE /geographic_items/1
 # # DELETE /geographic_items/1.json
@@ -58,6 +57,15 @@ class GeographicItemsController < ApplicationController
   end
 
   def geographic_item_params
-    params.require(:geographic_item).permit(:point, :line_string, :polygon, :multi_point, :multi_line_string, :multi_polygon, :geometry_collection)
+    params.require(:geographic_item).permit(
+      :point,
+      :line_string,
+      :polygon,
+      :multi_point,
+      :multi_line_string,
+      :multi_polygon,
+      :geometry_collection,
+      :shape
+    )
   end
 end
