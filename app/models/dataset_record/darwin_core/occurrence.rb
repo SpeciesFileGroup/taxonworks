d1ecc63ab529ea43e3eb2a9857f1c238ace62a60
Dash Peters <33206210+LordFlashmeow@users.noreply.github.com>
2023-11-10 10:48:42 -0800

    Use conjugated names in search

--- app/models/dataset_record/darwin_core/occurrence.rb
+++ app/models/dataset_record/darwin_core/occurrence.rb
@@ -76,14 +76,9 @@ class DatasetRecord::DarwinCore::Occurrence < DatasetRecord::DarwinCore
         # Protonym might not exist, or might have intermediate parent not listed in file
         # if it exists, run more expensive query to see if it has an ancestor matching parent name and rank
         if p.nil? && Protonym.where(name.slice(:rank_class).merge({ field => name[:name] })).where(project_id: parent.project_id).exists?
-          potential_protonyms = Protonym.where(name.slice(:rank_class).merge!({ field => name[:name] })).with_ancestor(parent)
+          potential_protonyms = Protonym.where(name.slice(:rank_class, :verbatim_author, :year_of_publication).merge!({ field => name[:name] })).with_ancestor(parent)
           if potential_protonyms.count > 1
-            # if multiple matches, then we need to narrow down with additional information
-            # TODO shouldn't we just use this to begin with?
-            potential_protonyms = Protonym.where(name.slice(:rank_class, :name, :verbatim_author, :year_of_publication)).with_ancestor(parent)
-            if potential_protonyms.count > 1
-              return parent
-            end
+            return parent
             # potential_protonym_strings = potential_protonyms.map { |proto| "[id: #{proto.id} #{proto.cached_html_name_and_author_year}]" }
             # raise DatasetRecord::DarwinCore::InvalidData.new(
             #   { "scientificName" => ["Intermediate name not present, and multiple matches found: #{potential_protonym_strings.join(', ')}"] }
