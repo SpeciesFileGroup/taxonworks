11c15664d2840c4b77018cfb81d0961498869cd6
mjy <diapriid@gmail.com>
2023-11-20 15:59:47 -0600

    Fix duplicate route names.

--- config/routes/tasks.rb
+++ config/routes/tasks.rb
@@ -383,22 +383,22 @@ scope :tasks do
 
     scope :dwc_extension_preview, controller: 'tasks/biological_associations/dwc_extension_preview' do
       get '/', action: :index, as: 'biological_associations_dwc_extension_preview_task'
-      post '/', action: :index, as: 'biological_associations_dwc_extension_preview_task'
+      post '/', action: :index
     end
 
     scope :simple_table, controller: 'tasks/biological_associations/simple_table' do
       get '/', action: :index, as: 'biological_associations_simple_table_task'
-      post '/', action: :index, as: 'biological_associations_simple_table_task'
+      post '/', action: :index
     end
 
     scope :globi_preview, controller: 'tasks/biological_associations/globi_preview' do
       get '/', action: :index, as: 'biological_associations_globi_preview_task'
-      post '/', action: :index, as: 'biological_associations_globi_preview_task'
+      post '/', action: :index
     end
 
     scope :family_summary, controller: 'tasks/biological_associations/family_summary' do
       get '/', action: :index, as: 'biological_associations_family_summary_task'
-      post '/', action: :index, as: 'biological_associations_family_summary_task'
+      post '/', action: :index
     end
 
     scope :graph, controller: 'tasks/biological_associations/graph' do
