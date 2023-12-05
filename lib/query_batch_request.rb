88099d66ee36f4f2b4569e681bd80b7254d0618b
mjy <diapriid@gmail.com>
2023-12-05 14:54:04 -0600

    Fix hash check

--- lib/query_batch_request.rb
+++ lib/query_batch_request.rb
@@ -56,7 +56,7 @@ class QueryBatchRequest
   end
 
   def unprocessable?
-    (!object_filter_params.any? || !object_params.any?) ||
+    (!object_filter_params.blank? || !object_params.blank?) ||
       (run_async? && preview)
   end
 
