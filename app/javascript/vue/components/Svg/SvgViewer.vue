3c392cebf597f96f1a79a281d26a062775cb11ac
jlpereira <pepelui@outlook.com>
2023-12-08 18:19:00 -0300

    Fix filter image viewer, add svg to comprehensive popup

--- app/javascript/vue/components/Svg/SvgViewer.vue
+++ app/javascript/vue/components/Svg/SvgViewer.vue
@@ -3,7 +3,6 @@
     xmlns="http://www.w3.org/2000/svg"
     xmlns:xlink="http://www.w3.org/1999/xlink"
     version="1.1"
-    style="position: inherit"
     ref="svgElement"
   >
     <g :transform="`translate(0, 0) scale(${parseFloat(scale)})`">
