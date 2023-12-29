525b6d4d36ca33b20c9832b574f700dae5bd2a99
jlpereira <pepelui@outlook.com>
2023-12-29 11:57:15 -0300

    Explicit define vue configuration

--- config/webpack/rules/vue.js
+++ config/webpack/rules/vue.js
@@ -13,7 +13,9 @@ module.exports = {
   plugins: [
     new VueLoaderPlugin(),
     new DefinePlugin({
-      __VUE_PROD_DEVTOOLS__: 'false'
+      __VUE_OPTIONS_API__: true,
+      __VUE_PROD_DEVTOOLS__: false,
+      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false
     })
   ],
   resolve: {
