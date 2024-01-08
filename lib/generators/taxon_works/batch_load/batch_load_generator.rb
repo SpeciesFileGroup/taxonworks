beeb38c9fb57d1cb60664c069172ddd8bbd093a7
mjy <diapriid@gmail.com>
2024-01-08 17:55:55 -0600

    Fix generators namespacing.

--- lib/generators/taxon_works/batch_load/batch_load_generator.rb
+++ lib/generators/taxon_works/batch_load/batch_load_generator.rb
@@ -1,6 +1,6 @@
 # Generate new TaxonWorks batch loaders
 require 'rails/generators'
-class Generators::TaxonWorks::BatchLoad::BatchLoadGenerator < Rails::Generators::Base
+class TaxonWorks::BatchLoadGenerator < Rails::Generators::Base
   desc 'Stub files for a new batch_load for a particular class.'
 
   source_root File.expand_path('../templates', __FILE__)
