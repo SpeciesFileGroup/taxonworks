f67c4cb49c61f3d8f287ed3a12090d434c0777a0
jlpereira <pepelui@outlook.com>
2024-01-19 18:38:08 -0300

    Update taxon determination filter

--- lib/queries/taxon_determination/filter.rb
+++ lib/queries/taxon_determination/filter.rb
@@ -8,6 +8,8 @@ module Queries
         :taxon_determination_id,
         :biological_colletion_object_id,
 
+        taxon_determination_object_type: [],
+        taxon_determination_object_id: [],
         biological_collection_object_id: [],
         collection_object_id: [],
         determiner_id: [],
@@ -21,6 +23,8 @@ module Queries
       attr_accessor :biological_collection_object_id
       attr_accessor :otu_id
       attr_accessor :determiner_id
+      attr_accessor :taxon_determination_object_type
+      attr_accessor :taxon_determination_object_id
 
       def initialize(query_params = {})
         super
@@ -30,6 +34,8 @@ module Queries
         @determiner_id = params[:determiner_id]
         @otu_id = params[:otu_id]
         @taxon_determination_id = params[:taxon_determination_id]
+        @taxon_determination_object_id = params[:taxon_determination_object_id]
+        @taxon_determination_object_type = params[:taxon_determination_object_type]
       end
 
       def taxon_determination_id
@@ -42,6 +48,14 @@ module Queries
 
       def collection_object_id
         [@collection_object_id, @biological_collection_object_id].flatten.compact.uniq
+      end
+
+      def taxon_determination_object_id
+        [@taxon_determination_object_id].flatten.compact.uniq
+      end
+
+      def taxon_determination_object_type
+        [@taxon_determination_object_type].flatten.compact
       end
 
       def determiner_id
@@ -57,6 +71,16 @@ module Queries
         return nil if collection_object_id.empty?
         table[:taxon_determination_object_id].in(collection_object_id)
         .and(table[:taxon_determination_object_type].eq('CollectionObject'))
+      end
+
+      def taxon_determination_object_type_facet
+        return nil if taxon_determination_object_type.empty?
+        table[:taxon_determination_object_type].in(taxon_determination_object_type)
+      end
+
+      def taxon_determination_object_id_facet
+        return nil if taxon_determination_object_id.empty?
+        table[:taxon_determination_object_id].in(taxon_determination_object_id)
       end
 
       def determiner_id_facet
@@ -81,6 +105,8 @@ module Queries
         [
           otu_id_facet,
           collection_object_id_facet,
+          taxon_determination_object_id_facet,
+          taxon_determination_object_type_facet
         ]
       end
 
