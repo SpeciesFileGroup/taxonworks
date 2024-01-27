56a6ca829c6993202b55da1852a241cba379233e
Tom Klein <twointofive@gmail.com>
2024-01-26 18:12:49 -0600

    #1691 Document Lead model attributes and add concerns

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,2 +1,49 @@
+# Leads models dichotomous keys; each row represents a single couplet option in a key.
+#
+# @!attribute parent_id
+#   @return [integer]
+#   Id of the lead immediately prior to this one in the key
+#
+# @!attribute otu_id
+#   @return [integer]
+#   Otu determined at this stage of the key, if any
+#
+# @!attribute text
+#   @return [text]
+#   Text for this option of a couplet in the key; title of the key on the root
+#   node
+#
+# @!attribute origin_label
+#   @return [string]
+#   If the couplet was given a # in print, that number
+#
+# @!attribute description
+#   @return [text]
+#   Only used on the root node, to describe the overall key
+#
+# @!attribute redirect_id
+#   @return [integer]
+#   Id of the lead to redirect to if this option of the couplet is chosen
+#
+# @!attribute link_out
+#   @return [text]
+#   Provides a URL to display on this node
+#
+# @!attribute link_out_text
+#   @return [string]
+#   Text to display for the link_out URL
+#
+# @!attribute position
+#   @return [integer]
+#   A sort order used by has_closure_tree
+#
+# @!attribute is_public
+#   @return [boolean]
+#   True if the key is viewable without being logged in
+#
 class Lead < ApplicationRecord
+  include Housekeeping
+  include Shared::Tags
+  include Shared::Citations
+  include Shared::IsData
 end
