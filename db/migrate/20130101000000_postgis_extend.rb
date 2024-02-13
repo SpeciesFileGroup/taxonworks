e6107a6411d331fa34ccd78a03b1dff08366de40
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2024-02-13 16:43:40 -0300

    Undo postgis_topology inclusion

--- db/migrate/20130101000000_postgis_extend.rb
+++ db/migrate/20130101000000_postgis_extend.rb
@@ -1,6 +1,6 @@
 class PostgisExtend < ActiveRecord::Migration[4.2]
   def change
     enable_extension 'postgis'
-    enable_extension 'postgis_topology'
+    # enable_extension 'postgis_topology' # This extension is NOT used.
   end
 end
