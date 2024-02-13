952b8a1394d061ad1453192aeaedfe7566bbc01e
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2024-02-13 16:35:58 -0300

    Postgis extension no created by default anymore.

--- db/migrate/20130101000000_postgis_extend.rb
+++ db/migrate/20130101000000_postgis_extend.rb
@@ -1,7 +1,6 @@
 class PostgisExtend < ActiveRecord::Migration[4.2]
   def change
-    # Deprecated.  This is not needed when rake db:setup is used
-    # ActiveRecord::Base.connection.execute('CREATE EXTENSION postgis')
-    # ActiveRecord::Base.connection.execute('CREATE EXTENSION postgis_topology')
+    enable_extension 'postgis'
+    enable_extension 'postgis_topology'
   end
 end
