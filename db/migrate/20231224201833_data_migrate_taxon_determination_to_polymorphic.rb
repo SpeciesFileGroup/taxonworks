28a99f0fbdd0a2f328f907804870bbaedcdc0f4d
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2024-02-27 19:13:32 -0300

    Optimized migration

--- db/migrate/20231224201833_data_migrate_taxon_determination_to_polymorphic.rb
+++ db/migrate/20231224201833_data_migrate_taxon_determination_to_polymorphic.rb
@@ -5,9 +5,10 @@ class DataMigrateTaxonDeterminationToPolymorphic < ActiveRecord::Migration[6.1]
      # 379.0450s
      TaxonDetermination.update_all(taxon_determination_object_type: 'CollectionObject') 
      
-      TaxonDetermination.all.find_each do |td|
-        td.update_column(:taxon_determination_object_id, td.biological_collection_object_id)
-      end
+      #TaxonDetermination.all.find_each do |td|
+      #  td.update_column(:taxon_determination_object_id, td.biological_collection_object_id)
+      #end
+      TaxonDetermination.update_all('taxon_determination_object_id = taxon_determination_object_id')
     end
 
   end
