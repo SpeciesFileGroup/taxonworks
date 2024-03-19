817dc55a9162dcdbb954d308f8d11f7b5b309c2a
mjy <diapriid@gmail.com>
2024-03-19 14:08:31 -0500

    Add some more graphs

--- app/helpers/graph_helper.rb
+++ app/helpers/graph_helper.rb
@@ -8,7 +8,7 @@ module GraphHelper
       data: []
     }  
 
-    %w{otus taxon_names collection_objects collecting_events biological_associations asserted_distributions type_materials images observations}.each do |i|
+    %w{otus taxon_names collection_objects collecting_events biological_associations asserted_distributions type_materials images documents descriptors observations contents}.each do |i|
 
       g = { target: i }
 
