7cfa3a2c2420b648ab136e7453a788de3a5c84d2
mjy <diapriid@gmail.com>
2024-03-25 09:37:28 -0500

    wip

--- app/models/concerns/shared/merge.rb
+++ app/models/concerns/shared/merge.rb
@@ -1,11 +1,52 @@
 
+small hot sour
+small chicken noodle soup
 
+orange chicken
+mongolian beef
+chicken with garlic sauce
+szeuan tofu, extra spicy
+
+
+# Merge concepts
+#   
+#   When b merges to a the operation is: 
+#     `complete` if b is left with no related data
+#      `blocked` when merging is prevented by data validations
+#     
+#  
+#
+#
+#
 module Shared::Merge
-
 
   extend ActiveSupport::Concern
 
   included do
+  end
+
+  # Iterating thorugh all of these sh
+  def merge_relations 
+    []
+  end
+
+  #  When merging skip these relations
+  def relation_exceptions
+    []
+  end
+
+  # Methods to be called per Class before merge_relations are iterated
+  def before_merge
+    []
+  end
+
+  # Methods to be called per Class after merge_relations are iterated
+  def after_merge
+    []
+  end
+
+  # re-vist this concept from is_data?
+  def used?
   end
 
   def merge(object_to_remove)
@@ -13,7 +54,6 @@ module Shared::Merge
 
     # All related non-annotation objects, by default
     object.class.transaction do
-
 
       begin
         ApplicationEnumeration.data_models.each do |m|
