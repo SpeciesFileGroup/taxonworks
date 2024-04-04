f55760d0d01b344befc9a14fad8feba13ab7714d
Tom Klein <twointofive@gmail.com>
2024-04-04 10:14:53 -0500

    #2029 Use a SmartSelector to select a document to import

--- app/javascript/vue/tasks/observation_matrices/import_nexus/app.vue
+++ app/javascript/vue/tasks/observation_matrices/import_nexus/app.vue
@@ -1,6 +1,31 @@
 <template>
-  <h1>Welcome to your new Vue task!</h1>
+  <fieldset class="document_select">
+    <legend>Select a Nexus document</legend>
+    <SmartSelector
+      klass="Documents"
+      model="documents"
+      v-model="nexusDoc"
+      label="document_file_file_name"
+    />
+    <SmartSelectorItem
+      :item="nexusDoc"
+      label="document_file_file_name"
+      @unset="() => nexusDoc = undefined"
+    />
+  </fieldset>
 </template>
 
 <script setup>
+import SmartSelector from '@/components/ui/SmartSelector'
+import SmartSelectorItem from '@/components/ui/SmartSelectorItem.vue'
+import { ref } from 'vue'
+
+const nexusDoc = ref()
 </script>
+
+<style lang="scss" scoped>
+.document_select {
+  width: 600px;
+  margin-top: 2em;
+}
+</style>
