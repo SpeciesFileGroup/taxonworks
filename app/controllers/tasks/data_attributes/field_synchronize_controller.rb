f3fe11b69ee71c4a349c762609131ed901736010
mjy <diapriid@gmail.com>
2024-04-17 12:10:30 -0500

    Attribute pagination headers

--- app/controllers/tasks/data_attributes/field_synchronize_controller.rb
+++ app/controllers/tasks/data_attributes/field_synchronize_controller.rb
@@ -1,10 +1,13 @@
 class Tasks::DataAttributes::FieldSynchronizeController < ApplicationController
   include TaskControllerConfiguration
 
+  after_action -> { set_pagination_headers(:records) }, only: [:values], if: :json_request?
+
   def values
     if q = Queries::Query::Filter.instatiated_base_filter(params)
       if !q.params.empty?
-        render json: q.all.select(:id, *params[:attribute]).page(params[:page]).per(params[:per]).to_json and return
+        @records = q.all.select(:id, *params[:attribute]).page(params[:page]).per(params[:per])
+        render json: @records.to_json and return
       end
     end
     render json: {}, status: :unprocessable_entity
