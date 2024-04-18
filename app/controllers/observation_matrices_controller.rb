810259ac9dd87b79ff4d307c9ff36d310fa2c140
Tom Klein <twointofive@gmail.com>
2024-04-18 16:38:52 -0500

    #2029 Factor out descriptor matching and use it in both preview and import
    This supports duplicate nexus descriptors, though the matrix ui won't differentiate duplicates.

--- app/controllers/observation_matrices_controller.rb
+++ app/controllers/observation_matrices_controller.rb
@@ -253,6 +253,7 @@ class ObservationMatricesController < ApplicationController
   def nexus_data
     begin
       nf = Vendor::NexusParser.document_id_to_nexus(params[:nexus_document_id])
+    # TODO: can we combine these rescues? They're taking up space
     rescue ActiveRecord::RecordNotFound
       render json: { errors: 'Document not found' },
         status: :unprocessable_entity
@@ -265,28 +266,29 @@ class ObservationMatricesController < ApplicationController
 
     options = nexus_import_options_params
 
+    # Otus
     taxa_names = nf.taxa.collect{ |t| t.name }.sort().uniq
     matched_otus = find_matching_otus(taxa_names,
       options[:match_otu_to_name], options[:match_otu_to_taxonomy_name])
 
-    @otus = taxa_names.map { |tn| matched_otus[tn] || tn }
+    @otus = taxa_names.map { |name| matched_otus[name] || name }
 
-    # TODO: the nexus description says character name repeats aren't allowed,
-    # but the parser currently allows them.
+    # Descriptors
     descriptor_names = nf.characters.collect{ |c| c.name }.sort().uniq
-    # TODO: the actual import also requires that states and labels match before
-    # a TW descriptor is used, we should do the same check here.
-    # Also need to make this list uniq.
-    descriptors = options[:match_character_to_name] ?
-      Descriptor
-        .where(project_id: sessions_current_project_id)
-        .where(name: descriptor_names)
-        .order(:name).to_a
-      : []
 
-    @descriptors = merge_name_and_ar_lists(
-      descriptor_names, descriptors, 'name'
-    )
+    matched_descriptors = {}
+    if options[:match_character_to_name]
+      nf.characters.each { |nxs_chr|
+        tw_d = find_matching_descriptor(nxs_chr)[:descriptor]
+        if tw_d
+          matched_descriptors[nxs_chr.name] = tw_d
+        end
+      }
+    end
+
+    @descriptors = descriptor_names.map { |name|
+      matched_descriptors[name] || name
+    }
   end
 
   # POST /observation_matrices/import_nexus.json
@@ -376,27 +378,6 @@ class ObservationMatricesController < ApplicationController
     params.require(:options).permit(:matrix_name, *boolean_options)
   end
 
-  # Given a list of names and a list of ActiveRecords with a name_attr,
-  # return the names list where any names matching an AR are replaced with the
-  # AR.
-  # !! Assumes both names and ars are ordered by name, ars' names are a
-  # subset of those on the names list, and each list is uniq.
-  # TODO: spec this if it stays
-  def merge_name_and_ar_lists(names, ars, name_attr)
-    a = []
-    names.each_with_index { |n, i|
-      if ars.empty?
-        return a + names[i..]
-      elsif n == ars.first[name_attr]
-        a.push ars.shift
-      else
-        a.push n
-      end
-    }
-
-    a
-  end
-
   def find_matching_otus(names, match_otus_by_name, match_otus_by_taxon)
     if !match_otus_by_name && !match_otus_by_taxon
       return {}
@@ -416,6 +397,36 @@ class ObservationMatricesController < ApplicationController
     end
 
     matches
+  end
+
+  # @return [Hash] Returns a hash with descriptor and chr_states
+  # properties of the most recently created descriptor of the same name
+  # and character states as nxs_char.
+  def find_matching_descriptor(nxs_chr)
+    descriptors = Descriptor::Qualitative
+      .where(project_id: sessions_current_project_id)
+      .where(name: nxs_chr.name)
+      .order(:name, id: :desc)
+
+    descriptors.each do |tw_d|
+      # Require state labels/names from nexus and TW to match.
+      # Other operations are conceivable, for instance updating the
+      # chr with the new states, but the combinatorics gets very tricky
+      # very quickly.
+
+      tw_chr_states = CharacterState
+        .where(project_id: sessions_current_project_id)
+        .where(descriptor: tw_d)
+
+      if same_state_names_and_labels(nxs_chr.states, tw_chr_states)
+        return {
+          descriptor: tw_d,
+          chr_states: tw_chr_states
+        }
+      end
+    end
+
+    {}
   end
 
   def runit(nexus_doc_id, uid, project_id, options)
@@ -455,7 +466,7 @@ class ObservationMatricesController < ApplicationController
 =end
 
     new_otus = []
-    new_chrs = []
+    new_descriptors = []
     new_states = []
 
     begin
@@ -471,7 +482,6 @@ class ObservationMatricesController < ApplicationController
 
         # Find/create OTUs, add them to the matrix as we do so,
         # and add them to an array for reference during coding.
-
         taxa_names = nf.taxa.collect{ |t| t.name }.sort().uniq
         matched_otus = find_matching_otus(taxa_names,
           options[:match_otu_to_name], options[:match_otu_to_taxonomy_name])
@@ -491,44 +501,22 @@ class ObservationMatricesController < ApplicationController
           )
         end
 
-        # Find/create characters.
+        # Find/create descriptors (= nexus characters).
         nf.characters.each_with_index do |nxs_chr, i|
-          tw_chr = nil
+          tw_d = nil
           new_states[i] = {}
 
           if options[:match_character_to_name]
-            # TODO: allow other types somehow?
-            tw_chrs = Descriptor
-              .where(project_id: sessions_current_project_id)
-              .where(type: 'Descriptor::Qualitative')
-              .where(name: nxs_chr.name)
-
-            tw_chrs.each do |twc|
-              # Require state labels/names from nexus and TW to match.
-              # Other operations are conceivable, for instance updating the
-              # chr with the new states, but the combinatorics gets very tricky
-              # very quickly.
-
-              tw_chr_states = CharacterState
-                .where(project_id: sessions_current_project_id)
-                .where(descriptor: twc)
-
-              if !same_state_names_and_labels(nxs_chr.states, tw_chr_states)
-                next
-              end
-
-              tw_chr = twc
-              tw_chr_states.each do |twcs|
+            r = find_matching_descriptor(nxs_chr)
+            tw_d = r[:descriptor]
+            if tw_d
+              r[:chr_states].each { |twcs|
                 new_states[i][twcs.label] = twcs
-              end
-
-              break
+              }
             end
           end
 
-          if !tw_chr
-            name = nxs_chr.name
-
+          if !tw_d
             new_tw_chr_states = []
             nxs_chr.state_labels.each do |nex_state|
               new_tw_chr_states << {
@@ -540,21 +528,21 @@ class ObservationMatricesController < ApplicationController
               end
             end
 
-            tw_chr = Descriptor.create!({
-              name:,
+            tw_d = Descriptor.create!({
+              name: nxs_chr.name,
               type: 'Descriptor::Qualitative',
               character_states_attributes: new_tw_chr_states
             })
 
             puts Rainbow('Created states').orange.bold
-            tw_chr.character_states.each do |cs|
+            tw_d.character_states.each do |cs|
               new_states[i][cs.label] = cs
             end
           end
 
-          new_chrs << tw_chr
+          new_descriptors << tw_d
           ObservationMatrixColumn.create!(
-            observation_matrix_id: m.id, descriptor: tw_chr
+            observation_matrix_id: m.id, descriptor: tw_d
           )
         end
 
@@ -567,7 +555,7 @@ class ObservationMatricesController < ApplicationController
                   .where(project_id: sessions_current_project_id)
                   .where(type: 'Observation::Qualitative')
                   .find_by(
-                    descriptor: new_chrs[j],
+                    descriptor: new_descriptors[j],
                     observation_object: new_otus[i],
                     character_state: new_states[j][z]
                   )
@@ -575,7 +563,7 @@ class ObservationMatricesController < ApplicationController
                 if o.nil?
                   Observation.create!(
                     type: Observation::Qualitative,
-                    descriptor: new_chrs[j],
+                    descriptor: new_descriptors[j],
                     observation_object: new_otus[i],
                     character_state: new_states[j][z]
                   )
