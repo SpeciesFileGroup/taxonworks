6bb89439ff3519e5df6ce9176a7ff20d4353c57a
mjy <diapriid@gmail.com>
2024-04-22 15:53:11 -0500

    Revert rspec config.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,64 +1,32 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
+if !ENV['NO_COVERAGE_REPORT'] && ENV['CI']
+  require 'simplecov'
+  SimpleCov.start
+
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+  Codecov.pass_ci_if_error = true
+end
+
 ENV['RAILS_ENV'] ||= 'test'
+
 require_relative '../config/environment'
-# Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+
+ActiveRecord::Migration.check_pending!
+ActiveRecord::Migration.maintain_test_schema!
+
+require 'amazing_print'
 require 'rspec/rails'
-# Add additional requires below this line. Rails is not loaded until this point!
+require 'spec_helper'
+include ActionDispatch::TestProcess
 
-# Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
-# run as spec files by default. This means that files in spec/support that end
-# in _spec.rb will both be required and run as specs, causing the specs to be
-# run twice. It is recommended that you do not name files matching this glob to
-# end with _spec.rb. You can configure this pattern with the --pattern
-# option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
-#
-# The following line is provided for convenience purposes. It has the downside
-# of increasing the boot-up time by auto-requiring all files in the support
-# directory. Alternatively, in the individual `*_spec.rb` files, manually
-# require only the support files necessary.
-#
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+# TODO: this is all kinds of fragile and terrible. Potential fix:
+# * [ ] Use FactoryBot not FactoryBotRails
+# * [ ] Work backwards with dependencies for a more explicit/logical approach
 
-# Checks for pending migrations and applies them before tests are run.
-# If you are not using ActiveRecord, you can remove these lines.
-begin
-  ActiveRecord::Migration.maintain_test_schema!
-rescue ActiveRecord::PendingMigrationError => e
-  puts e.to_s.strip
-  exit 1
-end
-RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+# Commenting this out does not prevent initialization
+Dir[Rails.root.join('spec/support/**/*.rb')].sort.reverse.each { |f| require f }
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
+ApplicationRecord.connection.tables.each { |t| ApplicationRecord.connection.reset_pk_sequence!(t) }
 
-  # You can uncomment this line to turn off ActiveRecord support entirely.
-  # config.use_active_record = false
+FactoryBot.use_parent_strategy = false
 
-  # RSpec Rails can automatically mix in different behaviours to your tests
-  # based on their file location, for example enabling you to call `get` and
-  # `post` in specs under `spec/controllers`.
-  #
-  # You can disable this behaviour by removing the line below, and instead
-  # explicitly tag your specs with their type, e.g.:
-  #
-  #     RSpec.describe UsersController, type: :controller do
-  #       # ...
-  #     end
-  #
-  # The different available types are documented in the features, such as in
-  # https://relishapp.com/rspec/rspec-rails/docs
-  config.infer_spec_type_from_file_location!
-
-  # Filter lines from Rails gems in backtraces.
-  config.filter_rails_from_backtrace!
-  # arbitrary gems may also be filtered via:
-  # config.filter_gems_from_backtrace("gem name")
-end
