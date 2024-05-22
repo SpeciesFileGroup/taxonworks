46123c8b9d82cae2ee6a5484a052ed4486b1bf2b
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2024-05-22 18:44:00 -0300

    Added comment explaining where allowed classes list came from [skip ci]

--- config/initializers/active_record_yaml_permitted_classes.rb
+++ config/initializers/active_record_yaml_permitted_classes.rb
@@ -1,3 +1,4 @@
+#This list comes from https://github.com/paper-trail-gem/paper_trail/blob/9a48fafb94b5b90020bd995d10415fb65c3944c0/doc/pt_13_yaml_safe_load.md
 Rails.application.config.after_initialize do
   ::ActiveRecord.yaml_column_permitted_classes += [
     ::ActiveRecord::Type::Time::Value,
