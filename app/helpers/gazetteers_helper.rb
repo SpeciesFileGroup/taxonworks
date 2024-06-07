0c094ea1d62aeae54054b3d4d3ea24a9c997f5ed
Tom Klein <twointofive@gmail.com>
2024-06-07 16:44:50 -0500

    #1954 Start adding gazetteer helpers

--- app/helpers/gazetteers_helper.rb
+++ app/helpers/gazetteers_helper.rb
@@ -1,2 +1,7 @@
 module GazetteersHelper
+  def gazetteer_link(gazetteer, link_text = nil)
+    return nil if gazetteer.nil?
+    link_text ||= gazetteer.name
+    link_to(link_text, gazeteer)
+  end
 end
