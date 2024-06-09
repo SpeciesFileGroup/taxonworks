65e4ba9d0d26b6906b8b29f0359f0bd8489f2067
mjy <diapriid@gmail.com>
2024-06-09 15:00:59 -0500

    Unscope some lib/queries to select less.

--- lib/queries.rb
+++ lib/queries.rb
@@ -7,32 +7,26 @@ module Queries
     when :b
       except(b_query, a_query)
     when :ab
-      intersect(a_query, [b_query])
+      intersect(a_query.klass, [a_query, b_query])
     else
       raise
     end
   end
 
   def self.intersect(target, queries)
-
-    table = target.name.tableize
-
-    q = queries.compact
+    table = target.table_name
+    q = queries.compact.collect{|y| y.unscope(:select).select(:id) }
 
     # We can return this directly, though we get conflicts with `from:` on merge clauses
     z = target.from("( #{q.collect{|i| '(' + i.to_sql + ')' }.join(' INTERSECT ')}) as #{table}")
 
-    # Probably need a global counter, and this may not be needed
-    s = Utilities::Strings.random_string(5)
-    a = table + s
-
-    # Here we probably get conflicts with join: clauses
-    target.joins("INNER JOIN ( #{z.to_sql}  ) AS #{a} ON #{a}.id = #{table}.id")
+    z
   end
 
   def self.union(target, queries)
-    q = queries.compact
     table = target.name.tableize
+    q = queries.compact.collect{|y| y.unscope(:select).select(:id) }
+
     target.from("( #{q.collect{|i| '(' + i.to_sql + ')' }.join(' UNION ')}) as #{table}")
   end
 
