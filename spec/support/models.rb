48bbd4a40c2c90ecedc3b9e4c1a46ce3284c138a
mjy <diapriid@gmail.com>
2024-06-21 16:45:19 -0500

    Refactor spec setup to take account for Current resets

--- spec/support/models.rb
+++ spec/support/models.rb
@@ -1,9 +1,14 @@
 RSpec.configure do |config|
   config.before(:all, type: :model) do
     ProjectsAndUsers.spin_up_projects_users_and_housekeeping
-  end 
+  end
 
-  config.after(:all, type: :model) { 
+  config.before(:each, type: :model) do
+    Current.project_id = 1
+    Current.user_id = 1
+  end
+
+  config.after(:all, type: :model) {
     ProjectsAndUsers.clean_slate
   }
 end
@@ -13,13 +18,13 @@ end
 # These methods are available throughout specs.
 module ModelHelper
 
-  # Returns the name of the TW factory for a class, includes 
+  # Returns the name of the TW factory for a class, includes
   # the formatting for nested subclasses.
   def class_factory_name(klass)
     klass.name.tableize.singularize.gsub('/', '_')
   end
 
-  # Return the name of the valid TW factory for the class. This 
+  # Return the name of the valid TW factory for the class. This
   # factory should be directly creatable, e.g. Factory.create(valid_class_factory_name(Otu))
   def valid_class_factory_name(klass)
     "valid_#{class_factory_name(klass)}"
@@ -45,7 +50,3 @@ module ModelHelper
     Current.project_id
   end
 end
-
-
-
-
