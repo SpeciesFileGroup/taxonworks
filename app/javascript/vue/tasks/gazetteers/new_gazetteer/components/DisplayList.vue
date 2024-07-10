b56ea752f221d80c1c96d796f3e922f5202fa6f8
Tom Klein <twointofive@gmail.com>
2024-07-09 19:56:14 -0500

    #1954 Fix display of GeometryCollections in Gazetteer displaylist
    Could potentially be seeing a lot more GeometryCollections with Gazetteer, but this is also an issue with the "same" Georeference display list where you can enter GeometryCollections as WKT.

--- app/javascript/vue/tasks/gazetteers/new_gazetteer/components/DisplayList.vue
+++ app/javascript/vue/tasks/gazetteers/new_gazetteer/components/DisplayList.vue
@@ -15,7 +15,7 @@
       >
         <tr
           v-for="item in list"
-          :key="item.uuid"
+          :key="item.uuid || 1 "
           class="list-complete-item"
         >
           <td class="word-keep-all">{{ shapeType(item) }}</td>
@@ -91,17 +91,55 @@ function shapeType(item) {
 
 function getCoordinates(item) {
   switch(item.type) {
+    // TODO display this as copyable WKT
     case GZ_LEAFLET:
-      const coordinates = item.shape.geometry.coordinates
-      const flattened = coordinates.flat(1)
-      if (typeof flattened[0] === 'number') {
-        return convertToLatLongOrder(coordinates)
-      } else {
-        return flattened.map((arr) => convertToLatLongOrder(arr))
+      switch(shapeType(item)) {
+        case 'GeometryCollection':
+          return coordinatesForGeometryCollection(
+              item.shape.geometry.geometries
+            )
+
+        default: // not GeometryCollection
+          const coordinates = item.shape.geometry.coordinates
+          const flattened = coordinates.flat(1)
+          if (typeof flattened[0] === 'number') {
+            return convertToLatLongOrder(coordinates)
+          } else {
+            return flattened.map((arr) => convertToLatLongOrder(arr))
+          }
       }
       break
     case GZ_WKT:
       return item.shape
   }
+}
+
+function coordinatesForGeometryCollection(geometries) {
+  let collectionStrings = []
+  geometries.forEach((geometry) => {
+    let shape_hash
+    if  (geometry.type == 'GeometryCollection') {
+      // TODO test this (a geom_collection inside a geom_collection)
+      shape_hash = {
+        geometries: geometry.geometries
+      }
+    }
+    else {
+      shape_hash = {
+        geometry
+      }
+    }
+
+    const new_shape = {
+      type: GZ_LEAFLET,
+      shape: shape_hash
+    }
+
+    // TODO how to display this type without double quotes?
+    collectionStrings.push(geometry.type)
+    collectionStrings.push(getCoordinates(new_shape))
+  })
+
+  return collectionStrings
 }
 </script>
