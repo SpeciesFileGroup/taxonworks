12621c01e8cb089abbf00f205556f539ef7e321b
Tom Klein <twointofive@gmail.com>
2024-07-26 22:47:45 -0500

    #1954 Support upload of shapefile documents

--- config/initializers/vendor/paperclip.rb
+++ config/initializers/vendor/paperclip.rb
@@ -4,5 +4,10 @@ Paperclip.options[:command_path] = '/usr/local/bin/'
 Paperclip.options[:content_type_mappings] = {
   ab1: %w(application/octet-stream),
   nex: %w(text/plain),
-  nxs: %w(text/plain)
+  nxs: %w(text/plain),
+  # Four shapefile formats:
+  shp: %w(application/octet-stream),
+  dbf: %w(application/x-dbf),
+  shx: %w(application/octet-stream),
+  prj: %w(text/plain)
 }
