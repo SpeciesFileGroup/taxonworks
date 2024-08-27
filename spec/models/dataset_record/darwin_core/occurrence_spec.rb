3004ef4bb9d567351ad78f39b7591cdd4c0d9802
mjy <diapriid@gmail.com>
2024-08-27 16:14:40 -0500

    Spec importing namespaced catalogNumber and recordNumber

--- spec/models/dataset_record/darwin_core/occurrence_spec.rb
+++ spec/models/dataset_record/darwin_core/occurrence_spec.rb
@@ -329,6 +329,45 @@ describe 'DatasetRecord::DarwinCore::Occurrence', type: :model do
     end
   end
 
+  # Somewhat redundant but
+  # TODO: check namespace column header format
+  context 'when importing namespaced catalogNumber and recordNumber' do
+    before :all do
+      DatabaseCleaner.start
+
+      init_housekeeping
+
+      @import_dataset = ImportDataset::DarwinCore::Occurrences.create!(
+        source: fixture_file_upload((Rails.root + 'spec/files/import_datasets/occurrences/identifiers/catalogNumber_and_recordNumber_namespaced.tsv'), 'text/plain'),
+        description: 'Testing'
+      ).tap { |i| i.stage }
+
+      namespace1 = FactoryBot.create(:valid_namespace, short_name: 'ABC')
+      namespace2 = FactoryBot.create(:valid_namespace, short_name: 'DEF')
+    end
+
+    after :all do
+      DatabaseCleaner.clean
+    end
+
+    let!(:results) { @import_dataset.import(5000, 100) }
+
+    it 'creates a new record' do
+      expect(results.length).to eq(3)
+      expect(results.first.status).to eq('Imported')
+    end
+
+    it "creates catalog numbers" do
+      expect(Identifier::Local::CatalogNumber.all.count).to eq(3)
+    end
+
+    it "creates record numbers" do
+      expect(Identifier::Local::RecordNumber.all.count).to eq(2) # Is correct
+    end
+  end
+
+  # ----
+
 
   context 'when not supplying custom namespaces for occurrenceID nor eventID' do
     before(:all) do
