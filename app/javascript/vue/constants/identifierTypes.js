002b5bcafc885db2e1f8460f0544c213e8b8ea23
jlpereira <pepelui@outlook.com>
2024-08-28 21:01:05 -0300

    Add record number panel to comprehensive

--- app/javascript/vue/constants/identifierTypes.js
+++ app/javascript/vue/constants/identifierTypes.js
@@ -1,4 +1,4 @@
-export const IDENTIFIER_LOCAL_CATALOG_NUMBER =
-  'Identifier::Local::CatalogNumber'
+export const IDENTIFIER_LOCAL_CATALOG_NUMBER = 'Identifier::Local::CatalogNumber'
 export const IDENTIFIER_LOCAL_TRIP_CODE = 'Identifier::Local::TripCode'
+export const IDENTIFIER_LOCAL_RECORD_NUMBER = 'Identifier::Local::RecordNumber'
 export const IDENTIFIER_UNKNOWN = 'Identifier::Unknown'
