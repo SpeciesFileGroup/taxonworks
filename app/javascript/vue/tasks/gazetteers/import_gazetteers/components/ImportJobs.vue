c398bb9630eeabb187043ce4ce514c8f2db12c84
Tom Klein <twointofive@gmail.com>
2024-09-11 17:53:06 -0500

    #1954 Update job import list functionality
    One note on the job status trash button: the button deletes from the database but I've made it primary color with no confirmation here since the delete is of little consequence (?) and having to weed that table by hand is already a bit of a burden on the user. I have no strong feelings on that though.

--- app/javascript/vue/tasks/gazetteers/import_gazetteers/components/ImportJobs.vue
+++ app/javascript/vue/tasks/gazetteers/import_gazetteers/components/ImportJobs.vue
@@ -1,70 +1,104 @@
 <template>
-  <fieldset>
-    <legend>Import Jobs</legend>
-    <div>
-      <b>No Gazetteers are created until an entire job is completed.</b>
-    </div>
-    <div class="vue-table-container">
-      <table class="vue-table">
-        <thead>
-          <tr>
-            <th class="word-keep-all">Shapefile</th>
-            <th class="word-keep-all">Status</th>
-            <th class="word-keep-all">Gazetteers Imported</th>
-            <th class="word-keep-all">Start time</th>
-            <th class="word-keep-all">End time</th>
-            <th class="word-keep-all"></th>
-          </tr>
-        </thead>
-        <TransitionGroup
-          name="list-complete"
-          tag="tbody"
-        >
-          <tr
-            v-for="job in imports"
-            :key="job.id"
-            class="list-complete-item"
+   <BlockLayout
+    expand
+    class="status"
+  >
+    <template #header>
+      <div class="header middle">
+        <h3>Import Jobs Status</h3>
+        <div class="header-buttons">
+          <VBtn
+            color="primary"
+            circle
+            @click="refresh()"
           >
-            <td class="word-keep-all">{{ job['shapefile'] }}</td>
-            <td>{{ jobStatus(job) }}</td>
-            <td>{{ gzsImported(job) }}</td>
-            <td>{{ job['started_at'] }}</td>
-            <td>{{ job['ended_at'] }}</td>
-            <td>
-              <div class="horizontal-right-content gap-small">
-                <VBtn
-                  v-if="true"
-                  color="destroy"
-                  circle
-                  @click="clearJob(job)"
-                >
-                  <VIcon
-                    name="trash"
-                    x-small
-                  />
-                </VBtn>
-              </div>
-            </td>
-          </tr>
-        </TransitionGroup>
-      </table>
-    </div>
-  </fieldset>
+            <VIcon
+              name="reset"
+              x-small
+            />
+          </VBtn>
+        </div>
+      </div>
+    </template>
+
+    <template #body>
+      <VSpinner v-if="status_loading" />
+      <div>
+        <b>No Gazetteers are created until the job status is 'Completed'</b>,
+        at which time the job record here can be deleted.
+      </div>
+      <div class="vue-table-container">
+        <table class="vue-table">
+          <thead>
+            <tr>
+              <th class="word-keep-all">Shapefile</th>
+              <th class="word-keep-all">Status</th>
+              <th class="word-keep-all">Gazetteers Imported</th>
+              <th class="word-keep-all">Start time</th>
+              <th class="word-keep-all">End time</th>
+              <th class="word-keep-all"></th>
+            </tr>
+          </thead>
+          <TransitionGroup
+            name="list-complete"
+            tag="tbody"
+          >
+            <tr
+              v-for="job in imports"
+              :key="job.id"
+              class="list-complete-item"
+            >
+              <td class="word-keep-all">{{ job['shapefile'] }}</td>
+              <td>{{ jobStatus(job) }}</td>
+              <td>{{ gzsImported(job) }}</td>
+              <td>{{ job['started_at'] }}</td>
+              <td>{{ job['ended_at'] }}</td>
+              <td>
+                <div class="horizontal-right-content gap-small">
+                  <VBtn
+                    v-if="completed(job)"
+                    color="primary"
+                    circle
+                    @click="clearJob(job)"
+                  >
+                    <VIcon
+                      name="trash"
+                      x-small
+                    />
+                  </VBtn>
+                </div>
+              </td>
+            </tr>
+          </TransitionGroup>
+        </table>
+      </div>
+    </template>
+  </BlockLayout>
 </template>
 
 <script setup>
+import BlockLayout from '@/components/layout/BlockLayout.vue'
 import VBtn from '@/components/ui/VBtn/index.vue'
 import VIcon from '@/components/ui/VIcon/index.vue'
+import VSpinner from '@/components/ui/VSpinner'
 import { onMounted, ref } from 'vue'
 import { GazetteerImport } from '@/routes/endpoints'
+import { removeFromArray } from '@/helpers/arrays'
 
 const imports = ref([])
+const status_loading = ref(false)
 
 onMounted(() => {
+  refresh()
+})
+
+function refresh() {
+  status_loading.value = true
   GazetteerImport.all().then(({ body }) => {
     imports.value = body
   })
-})
+  .finally(() => { status_loading.value = false })
+}
 
 function jobStatus(job) {
   if (!job['num_records']) {
@@ -86,10 +120,27 @@ function gzsImported(job) {
   }
 }
 
-function clearJob(job) {}
+function completed(job) {
+  return !!job['ended_at']
+}
+
+function clearJob(job) {
+  const job_id = job.id
+  removeFromArray(imports.value, job)
+
+  GazetteerImport.destroy(job_id)
+}
 
 </script>
 
 <style lang="scss" scoped>
-
+.status {
+  margin-bottom: 2em;
+}
+.header {
+  display: flex;
+}
+.header-buttons {
+  margin-left: .5em;
+}
 </style>
