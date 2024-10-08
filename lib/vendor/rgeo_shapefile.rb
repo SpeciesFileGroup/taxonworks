ebab6cb9e2722ac711d32d2998b115aa25d274fe
Tom Klein <twointofive@gmail.com>
2024-10-08 10:58:51 -0500

    #1954 Refactor to ensure tmp files are removed on error

--- lib/vendor/rgeo_shapefile.rb
+++ lib/vendor/rgeo_shapefile.rb
@@ -7,30 +7,6 @@ module Vendor::RgeoShapefile
       shx_doc = Document.find(shapefile[:shx_doc_id])
       dbf_doc = Document.find(shapefile[:dbf_doc_id])
       prj_doc = Document.find(shapefile[:prj_doc_id])
-      name_field = shapefile[:name_field]
-
-      # The above shapefile files are unlikely to all be in the same directory as
-      # required by rgeo-shapefile, so create symbolic links to each in a new
-      # temporary folder.
-      tmp_dir = Rails.root.join('tmp', 'shapefiles', SecureRandom.hex)
-      FileUtils.mkdir_p(tmp_dir)
-
-      shp_link = File.join(tmp_dir, 'shapefile.shp')
-      shx_link = File.join(tmp_dir, 'shapefile.shx')
-      dbf_link = File.join(tmp_dir, 'shapefile.dbf')
-      prj_link = File.join(tmp_dir, 'shapefile.prj')
-
-      FileUtils.ln_s(shp_doc.document_file.path, shp_link)
-      FileUtils.ln_s(shx_doc.document_file.path, shx_link)
-      FileUtils.ln_s(dbf_doc.document_file.path, dbf_link)
-      FileUtils.ln_s(prj_doc.document_file.path, prj_link)
-
-      citation = citation_options[:cite_gzs] ? citation_options[:citation] : nil
-
-      processShapeFile(shp_link, name_field, citation, progress_tracker)
-
-      FileUtils.rm_f([shp_link, dbf_link, shx_link, prj_link])
-      FileUtils.rmdir(tmp_dir)
     rescue ActiveRecord::RecordNotFound => e
       progress_tracker.update!(
         num_records_processed: 0,
@@ -39,17 +15,31 @@ module Vendor::RgeoShapefile
         ended_at: DateTime.now
       )
       return
-    rescue Errno::ENOENT => e
-      # One of the shapefile files couldn't be opened by
-      # RGeo::Shapefile::Reader.open
-      progress_tracker.update!(
-        num_records_processed: 0,
-        aborted_reason: "#{e} (id: #{shp_doc.id})",
-        started_at: DateTime.now,
-        ended_at: DateTime.now
-      )
-      return
     end
+    name_field = shapefile[:name_field]
+
+    # The above shapefile files are unlikely to all be in the same directory as
+    # required by rgeo-shapefile, so create symbolic links to each in a new
+    # temporary folder.
+    tmp_dir = Rails.root.join('tmp', 'shapefiles', SecureRandom.hex)
+    FileUtils.mkdir_p(tmp_dir)
+
+    shp_link = File.join(tmp_dir, 'shapefile.shp')
+    shx_link = File.join(tmp_dir, 'shapefile.shx')
+    dbf_link = File.join(tmp_dir, 'shapefile.dbf')
+    prj_link = File.join(tmp_dir, 'shapefile.prj')
+
+    FileUtils.ln_s(shp_doc.document_file.path, shp_link)
+    FileUtils.ln_s(shx_doc.document_file.path, shx_link)
+    FileUtils.ln_s(dbf_doc.document_file.path, dbf_link)
+    FileUtils.ln_s(prj_doc.document_file.path, prj_link)
+
+    citation = citation_options[:cite_gzs] ? citation_options[:citation] : nil
+
+    processShapeFile(shp_link, name_field, citation, progress_tracker)
+
+    FileUtils.rm_f([shp_link, dbf_link, shx_link, prj_link])
+    FileUtils.rmdir(tmp_dir)
   end
 
   def self.processShapeFile(shpfile, name_field, citation, progress_tracker)
@@ -59,68 +49,78 @@ module Vendor::RgeoShapefile
       error_message: nil,
     }
 
-    RGeo::Shapefile::Reader.open(
-      shpfile, factory: Gis::FACTORY, allow_unsafe: true
-    ) do |file|
-      r[:num_records] = file.num_records
-
-      progress_tracker.update!(
-        num_records: file.num_records,
-        started_at: DateTime.now
+    begin
+      file = RGeo::Shapefile::Reader.open(
+        shpfile, factory: Gis::FACTORY, allow_unsafe: true
       )
-      begin
-        Gazetteer.transaction do
-          # Iterate over an index so we can record index on error
-          for i in 0...file.num_records
-            begin
-              # This can throw GeosError even when allow_unsafe: true
-              record = file[i]
+    rescue Errno::ENOENT => e
+      progress_tracker.update!(
+        num_records_processed: 0,
+        aborted_reason: e.message,
+        started_at: DateTime.now,
+        ended_at: DateTime.now
+      )
+      return
+    end
 
-              g = Gazetteer.new(
-                name: record[name_field]
-              )
+    r[:num_records] = file.num_records
 
-              # Abort if too many invalid?
-              # TODO Track how many were invalid and were made valid?
-              shape = record.geometry.valid? ?
-                record.geometry : record.geometry.make_valid
+    progress_tracker.update!(
+      num_records: file.num_records,
+      started_at: DateTime.now
+    )
+    begin
+      Gazetteer.transaction do
+        # Iterate over an index so we can record index on error
+        for i in 0...file.num_records
+          begin
+            # This can throw GeosError even when allow_unsafe: true
+            record = file[i]
 
-              g.build_geographic_item(
-                geography: shape
-              )
+            g = Gazetteer.new(
+              name: record[name_field]
+            )
 
-              g.save!
+            # Abort if too many invalid?
+            # TODO Track how many were invalid and were made valid?
+            shape = record.geometry.valid? ?
+              record.geometry : record.geometry.make_valid
 
-              if citation.present?
-                Citation.create!(citation.merge({
-                  citation_object_type: 'Gazetteer',
-                  citation_object_id: g.id
-                }))
-              end
-            rescue RGeo::Error::InvalidGeometry => e
-              r[:error_id] = i + 1
-              r[:error_message] = e.to_s
-              raise ActiveRecord::RecordInvalid
-            rescue ActiveRecord::RecordInvalid => e
-              r[:error_id] = i + 1
-              r[:error_message] = e.to_s
-              raise ActiveRecord::RecordInvalid
-            rescue RGeo::Error::GeosError => e
-              r[:error_id] = i + 1
-              r[:error_message] = e.to_s
-              raise ActiveRecord::RecordInvalid
+            g.build_geographic_item(
+              geography: shape
+            )
+
+            g.save!
+
+            if citation.present?
+              Citation.create!(citation.merge({
+                citation_object_type: 'Gazetteer',
+                citation_object_id: g.id
+              }))
             end
+          rescue RGeo::Error::InvalidGeometry => e
+            r[:error_id] = i + 1
+            r[:error_message] = e.to_s
+            raise ActiveRecord::RecordInvalid
+          rescue ActiveRecord::RecordInvalid => e
+            r[:error_id] = i + 1
+            r[:error_message] = e.to_s
+            raise ActiveRecord::RecordInvalid
+          rescue RGeo::Error::GeosError => e
+            r[:error_id] = i + 1
+            r[:error_message] = e.to_s
+            raise ActiveRecord::RecordInvalid
           end
         end
-      rescue ActiveRecord::RecordInvalid
-        m = "Error on record #{r[:error_id]}/#{r[:num_records]}: #{r[:error_message]}"
-        progress_tracker.update!(
-          num_records_processed: r[:error_id] - 1,
-          aborted_reason: m,
-          ended_at: DateTime.now
-        )
-        return
       end
+    rescue ActiveRecord::RecordInvalid
+      m = "Error on record #{r[:error_id]}/#{r[:num_records]}: #{r[:error_message]}"
+      progress_tracker.update!(
+        num_records_processed: r[:error_id] - 1,
+        aborted_reason: m,
+        ended_at: DateTime.now
+      )
+      return
     end
 
     progress_tracker.update!(
