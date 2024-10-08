5645a20509571a935a6b326afffbe41b5480dfa2
jlpereira <pepelui@outlook.com>
2024-10-08 18:37:08 -0300

    Add modal to data attributes facet

--- app/javascript/vue/components/Filter/Facets/shared/FacetDataAttribute/AddPredicate.vue
+++ app/javascript/vue/components/Filter/Facets/shared/FacetDataAttribute/AddPredicate.vue
@@ -2,6 +2,7 @@
   <div>
     <label>Predicate</label>
     <SmartSelector
+      ref="smartSelectorRef"
       autocomplete-url="/controlled_vocabulary_terms/autocomplete"
       :autocomplete-params="{ 'type[]': 'Predicate' }"
       get-url="/controlled_vocabulary_terms/"
@@ -9,11 +10,35 @@
       buttons
       inline
       klass="DataAttribute"
-      :custom-list="{
-        all: list
-      }"
+      :add-tabs="['all']"
       @selected="(p) => (predicate = p)"
-    />
+    >
+      <template #all>
+        <VModal @close="smartSelectorRef.setTab('quick')">
+          <template #header>
+            <h3>Predicates - all</h3>
+          </template>
+          <template #body>
+            <div class="flex-wrap-row gap-small">
+              <VBtn
+                v-for="item in list"
+                :key="item.id"
+                color="primary"
+                pill
+                @click="
+                  () => {
+                    predicate = item
+                    smartSelectorRef.setTab('quick')
+                  }
+                "
+              >
+                {{ item.name }}
+              </VBtn>
+            </div>
+          </template>
+        </VModal>
+      </template>
+    </SmartSelector>
     <SmartSelectorItem
       :item="predicate"
       label="name"
@@ -68,8 +93,10 @@ import { PREDICATE } from '@/constants'
 import SmartSelector from '@/components/ui/SmartSelector.vue'
 import SmartSelectorItem from '@/components/ui/SmartSelectorItem.vue'
 import VBtn from '@/components/ui/VBtn/index.vue'
+import VModal from '@/components/ui/Modal.vue'
 
 const emit = defineEmits(['add'])
+const smartSelectorRef = ref(null)
 const inputValue = ref('')
 const predicate = ref()
 const exact = ref(false)
