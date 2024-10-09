f050c36dbfe2d06587c6ca86404024919fb4ad92
Tom Klein <twointofive@gmail.com>
2024-10-09 10:39:26 -0500

    #1954 Fixup (unused) GeographicItem filter to get specs to pass

--- lib/queries/geographic_item/filter.rb
+++ lib/queries/geographic_item/filter.rb
@@ -9,6 +9,23 @@ module Queries
     end
 
     class Filter < Query::Filter
+      PARAMS = [
+        :geographic_item_id
+      ].freeze
+
+      # @return Array
+      attr_accessor :geographic_item_id
+
+      # @param [Hash] params
+      def initialize(query_params)
+        super
+
+        @geographic_item_id = params[:geographic_item_id]
+      end
+
+      def geographic_item_id
+        [@geographic_item_id].flatten.compact
+      end
 
     end
 
