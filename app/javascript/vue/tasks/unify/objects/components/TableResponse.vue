b6b20d4fe109931c9cdffcd297a9a89968b52f5b
jlpereira <pepelui@outlook.com>
2024-10-10 12:08:52 -0300

    Add deduplicated

--- app/javascript/vue/tasks/unify/objects/components/TableResponse.vue
+++ app/javascript/vue/tasks/unify/objects/components/TableResponse.vue
@@ -5,18 +5,22 @@
         <th class="w-2"></th>
         <th class="w-2">Unified</th>
         <th class="w-2">Not unified</th>
+        <th class="w-2">Deduplicated</th>
         <th>Errors</th>
       </tr>
     </thead>
     <tbody>
       <template
-        v-for="({ merged, unmerged, errors }, key) in response.details"
+        v-for="(
+          { merged, unmerged, deduplicated, errors }, key
+        ) in response.details"
         :key="key"
       >
         <tr>
           <td>{{ key }}</td>
           <td>{{ merged }}</td>
           <td>{{ unmerged }}</td>
+          <td>{{ deduplicated }}</td>
           <td>
             <div v-if="errors">
               <ul class="no_bullets">
