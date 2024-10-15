b55fffc35d606aeb3ad9c009ae084c44b3b2aac3
mjy <diapriid@gmail.com>
2024-10-15 14:02:07 -0500

    Tweak OTU duplicates to include consideration of author/year.

--- app/controllers/tasks/otus/duplicates_controller.rb
+++ app/controllers/tasks/otus/duplicates_controller.rb
@@ -10,15 +10,15 @@ class Tasks::Otus::DuplicatesController < ApplicationController
     a = Otu
       .joins(:taxon_name)
       .where('otus.project_id = ?',  sessions_current_project_id)
-      .select('otus.id, otus.name, taxon_names.type, taxon_names.cached, count(*) OVER (PARTITION BY otus.name, taxon_names.cached, taxon_names.type) AS count')
+      .select('otus.id, otus.name, taxon_names.type, taxon_names.cached, taxon_names.cached_author_year, count(*) OVER (PARTITION BY otus.name, taxon_names.cached, taxon_names.type, taxon_names.cached_author_year) AS count')
 
     @otus = Otu.with(dupes: a)
       .left_joins(:taxon_name)
       .eager_load(:taxon_name)
       .joins('JOIN dupes on dupes.id = otus.id')
-      .select('otus.*, count, dupes.cached')
+      .select('otus.*, count, dupes.cached, dupes.cached_author_year')
       .where('count > 1')
-      .order('count, name, cached')
+      .order('count, name, cached, cached_author_year')
       .page(params[:page])
       .per(params[:per])
 
