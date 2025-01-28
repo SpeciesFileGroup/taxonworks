7f5f7d4d648060b1521408b0d7f22bc3ad11e6f9
jlpereira <pepelui@outlook.com>
2025-01-28 11:52:30 -0300

    Update conveyance row

--- app/javascript/vue/tasks/otu/browse/components/Conveyance/ConveyanceRow.vue
+++ app/javascript/vue/tasks/otu/browse/components/Conveyance/ConveyanceRow.vue
@@ -1,33 +1,14 @@
 <template>
   <tr>
     <td>
-      <div class="horizontal-left-content middle gap-medium">
+      <div
+        class="horizontal-left-content middle gap-medium padding-medium-bottom padding-top-bottom"
+      >
         <AudioPlayer
           class="full_width"
-          ref="audioPlayerRef"
+          media-controls
           :url="conveyance.sound.sound_file"
-          @pause="() => (isPlaying = false)"
-          @stop="() => (isPlaying = false)"
-          @play="() => (isPlaying = true)"
         />
-
-        <VBtn
-          color="primary"
-          circle
-          class="player-button"
-          @click="() => audioPlayerRef.playPause()"
-        >
-          <IconPause
-            v-if="isPlaying"
-            height="24px"
-            width="24px"
-          />
-          <IconPlay
-            v-else
-            height="24px"
-            width="24px"
-          />
-        </VBtn>
       </div>
       <div v-html="conveyance.object_tag" />
     </td>
@@ -35,11 +16,7 @@
 </template>
 
 <script setup>
-import { useTemplateRef, ref } from 'vue'
-import VBtn from '@/components/ui/VBtn/index.vue'
 import AudioPlayer from '@/components/audio/AudioPlayer.vue'
-import IconPause from '@/components/Icon/IconPause.vue'
-import IconPlay from '@/components/Icon/IconPlay.vue'
 
 defineProps({
   conveyance: {
@@ -47,9 +24,6 @@ defineProps({
     required: true
   }
 })
-
-const audioPlayerRef = useTemplateRef('audioPlayerRef')
-const isPlaying = ref(false)
 </script>
 
 <style scoped>
