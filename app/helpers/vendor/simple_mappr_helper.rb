1a9bfab92709ac68a7cd7deb9fa817c16d027ba2
Tom Klein <twointofive@gmail.com>
2025-02-17 15:37:56 -0600

    Make sure every column has a header in SimpleMappr output
    SimpleMappr seems to require it (I think), headers determine how many "layers" there are, and what their "legend" string is, in the terminology of https://www.simplemappr.net/#tabs=1.

--- app/helpers/vendor/simple_mappr_helper.rb
+++ app/helpers/vendor/simple_mappr_helper.rb
@@ -5,14 +5,14 @@ module Vendor::SimpleMapprHelper
     # TODO: Add support for FieldOccurrence, or by Otu Filter
 
     if params[:collection_object_query]
-      a = ::Queries::DwcOccurrence::Filter.new(collection_object_query: params[:collection_object_query]).all.select(:id, :scientificName, :decimalLatitude, :decimalLongitude)
+      a = ::Queries::DwcOccurrence::Filter.new(collection_object_query: params[:collection_object_query]).all.select(:id, :scientificName, :occurrenceID, :decimalLatitude, :decimalLongitude)
 
       return nil if a.size > 10_000
 
       d = {}
 
       a.find_each do |i|
-        k = i.scientificName
+        k = i.scientificName || "[Occurrence id: #{i.occurrenceID}]"
         d[k] ||= []
         d[k].push [i.decimalLatitude, i.decimalLongitude].compact.join(',').presence
       end
