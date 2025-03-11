5057789605535d8d8ebdbeb86b71ea40efee63d9
jlpereira <pepelui@outlook.com>
2025-03-11 16:08:35 -0300

    Add conveyance panel to browse ce #4169

--- app/helpers/conveyances_helper.rb
+++ app/helpers/conveyances_helper.rb
@@ -2,7 +2,7 @@ module ConveyancesHelper
 
   def conveyance_tag(conveyance)
     return nil if conveyance.nil?
-    [ sound_tag(conveyance.sound), 'on', object_tag(conveyance.conveyance_object)  ].compact.join('&nbsp;')
+    [ sound_tag(conveyance.sound), 'on', object_tag(conveyance.conveyance_object)  ].compact.join('&nbsp;').html_safe
   end
 
   def label_for_conveyance(conveyance)
@@ -10,4 +10,16 @@ module ConveyancesHelper
     [ label_for_sound(conveyance.sound), 'on', label_for(conveyance.conveyance_object)  ].compact.join('&nbsp;')
   end
 
+  def conveyances_list_tag(object)
+    if object.conveyances.any?
+      object.conveyances.collect{|c|
+        content_tag(:div) do
+          safe_join([
+            audio_tag(c.sound.sound_file, controls: true), 
+            content_tag(:div, conveyance_tag(c))
+          ])
+        end
+      }.join.html_safe
+    end
+  end
 end
