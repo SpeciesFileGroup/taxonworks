2b01c99f3ba313312a50e7f3ea021179ce7cd1f9
Tom Klein <twointofive@gmail.com>
2025-03-24 09:58:06 -0500

    Update AD Basic Endemism task to work with new ADs #1954
    Doesn't support GZ yet, but should work with GA.

--- lib/taxonworks/analysis/asserted_distribution/basic_endemism.rb
+++ lib/taxonworks/analysis/asserted_distribution/basic_endemism.rb
@@ -8,7 +8,9 @@ module TaxonWorks
         # @return [Hash]
         #    a very simple report summarizing asserted distributions
         #    !! only a single geographic area is used (not its children)
-        def self.quick_endemism(taxon_name, geographic_area)
+        def self.quick_endemism(taxon_name, geographic_area) # TODO update params for GZ as well as GA
+          asserted_distribution_shape_type = 'GeographicArea'
+          asserted_distribution_shape_id = geographic_area.id
           data = {}
 
           otus =  Otu.descendant_of_taxon_name(taxon_name.id)
@@ -17,15 +19,19 @@ module TaxonWorks
           q = ::Queries::AssertedDistribution::Filter.new(
             taxon_name_id: taxon_name.id,
             descendants: true,
-            geographic_area: 
+            asserted_distribution_shape_type:,
+            asserted_distribution_shape_id:
           )
 
           return {} if q.all.select(:otu_id).distinct.count > 2000
 
           q.all.find_each do |a|
-           
-            e = ::AssertedDistribution.where(project: taxon_name.project, otu_id: a.otu_id).where.not(geographic_area:).count
-            
+
+            e = ::AssertedDistribution
+              .where(project: taxon_name.project, otu_id: a.otu_id)
+              .where(asserted_distribution_shape_type:)
+              .where.not(asserted_distribution_shape_id:).count
+
             n = a.otu.taxon_name&.valid_taxon_name
 
             if e == 0 && !data[n]
