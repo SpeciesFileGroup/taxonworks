SpeciesFileGroup/taxonworks

commit f085dc142b7dc4c1282fbc5dd98d7edde03c1276

Fix navbar links in Browse Asserted Distributions task

app/views/otus/navigation.json.jbuilder
@@ -6,7 +6,7 @@ json.parent_otus do
   json.array! parent_otus(@otu) do |o|
     json.id o.id
     json.name o.name
-    json.object_tag otu_tag(o) 
+    json.object_label label_for_otu(o)
   end
 end
 
@@ -14,7 +14,7 @@ json.previous_otus do
   json.array! previous_otus(@otu) do |o|
     json.id o.id
     json.name o.name
-    json.object_tag otu_tag(o) 
+    json.object_label label_for_otu(o)
   end
 end
 
@@ -22,7 +22,7 @@ json.next_otus do
   json.array! next_otus(@otu) do |o|
     json.id o.id
     json.name o.name
-    json.object_tag otu_tag(o) 
+    json.object_label label_for_otu(o)
   end
 end
 

app/javascript/vue/tasks/otu/browse_asserted_distributions/components/navBar.vue
@@ -18,10 +18,10 @@
             :key="item.id"
           >
             <a
-              :href="`/tasks/otus/browse_asserted_distributions/index?otu_id=${item.id}`"
+              :href="browseLink(item)"
             >
-              {{ item.object_label }}</a
-            >
+              {{ item.object_label }}
+            </a>
           </li>
         </ul>
       </template>
@@ -33,10 +33,10 @@
             :key="item.id"
           >
             <a
-              :href="`/tasks/otus/browse_asserted_distributions/index?otu_id=${item.id}`"
+              :href="browseLink(item)"
             >
-              {{ item.object_label }}</a
-            >
+              {{ item.object_label }}
+            </a>
           </li>
         </ul>
       </template>
@@ -48,10 +48,10 @@
             :key="item.id"
           >
             <a
-              :href="`/tasks/otus/browse_asserted_distributions/index?otu_id=${item.id}`"
+              :href="browseLink(item)"
             >
-              {{ item.object_label }}</a
-            >
+              {{ item.object_label }}
+            </a>
           </li>
         </ul>
       </template>
@@ -61,6 +61,7 @@
 
 <script>
 import { Otu } from '@/routes/endpoints'
+import { RouteNames } from '@/routes/routes'
 import RadialAnnotator from '@/components/radials/annotator/annotator'
 import RadialObject from '@/components/radials/navigation/radial'
 import OtuRadial from '@/components/radials/object/radial'
@@ -97,9 +98,15 @@ export default {
 
   methods: {
     loadNav(id) {
-      Otu.navigation(id).then((response) => {
+      Otu.navigation(id)
+      .then((response) => {
         this.navList = response.body
       })
+      .catch(() => {})
+    },
+
+    browseLink(item) {
+      return `${RouteNames.BrowseAssertedDistribution}?otu_id=${item.id}`
     }
   }
 }

commit b7449af7d11db0e724015608fcfab9f02ff7c859

Bring back Browse Otu otu navigation links
The Browse Otu task uses tags, not labels.

Broken by f085dc142b7dc4c1282fbc5dd98d7edde03c1276

app/views/otus/navigation.json.jbuilder
@@ -6,6 +6,7 @@ json.parent_otus do
   json.array! parent_otus(@otu) do |o|
     json.id o.id
     json.name o.name
+    json.object_tag otu_tag(o)
     json.object_label label_for_otu(o)
   end
 end
@@ -14,6 +15,7 @@ json.previous_otus do
   json.array! previous_otus(@otu) do |o|
     json.id o.id
     json.name o.name
+    json.object_tag otu_tag(o)
     json.object_label label_for_otu(o)
   end
 end
@@ -22,6 +24,7 @@ json.next_otus do
   json.array! next_otus(@otu) do |o|
     json.id o.id
     json.name o.name
+    json.object_tag otu_tag(o)
     json.object_label label_for_otu(o)
   end
 end