6bfe64bd4536962f1cff234ea750af593da093e3
jlpereira <pepelui@outlook.com>
2025-03-25 12:30:19 -0300

    Add levels

--- app/javascript/vue/components/ui/AdvancedAutocomplete.vue
+++ app/javascript/vue/components/ui/AdvancedAutocomplete.vue
@@ -4,17 +4,16 @@
       type="text"
       ref="inputRef"
       :id="inputId"
-      :style="inputStyle"
       :placeholder="placeholder"
       :autofocus="autofocus"
       :disabled="disabled"
-      v-model="type"
+      v-model="inputValue"
       v-bind="inputAttributes"
       autocomplete="off"
       :class="[
         'vue-autocomplete-input normal-input',
-        spinner && 'ui-autocomplete-loading',
-        !spinner && 'vue-autocomplete-input-search',
+        isLoading && 'ui-autocomplete-loading',
+        !isLoading && 'vue-autocomplete-input-search',
         inputClass
       ]"
       @input="updateTimeout"
@@ -25,11 +24,11 @@
     />
     <ul
       class="vue-autocomplete-list"
-      v-show="showList"
-      v-if="type && json.length"
+      v-show="isListVisible"
+      v-if="inputValue && list.length"
     >
       <li
-        v-for="(item, index) in limitList(json)"
+        v-for="(item, index) in limitList"
         class="vue-autocomplete-item"
         :class="{ active: currentIndex === index }"
         @mouseover="() => (currentIndex = index)"
@@ -37,14 +36,14 @@
       >
         <span
           v-if="typeof label !== 'function'"
-          v-html="getNested(item, label)"
+          v-html="item[label]"
         />
         <span
           v-else
           v-html="label(item)"
         />
       </li>
-      <li v-if="json.length == 20">Results may be truncated</li>
+      <li v-if="list.length == 20">Results may be truncated</li>
     </ul>
     <ul
       v-if="inputValue && isLoading && !list.length"
@@ -56,11 +55,11 @@
 </template>
 
 <script setup>
-import { ref, onMounted, onBeforeUnmount, useTemplateRef } from 'vue'
+import { computed, ref, onMounted, onBeforeUnmount, useTemplateRef } from 'vue'
 import AjaxCall from '@/helpers/ajaxCall'
 import Qs from 'qs'
 
-const props = {
+const props = defineProps({
   inputId: {
     type: String,
     default: undefined
@@ -106,6 +105,11 @@ const props = {
     default: ''
   },
 
+  param: {
+    type: String,
+    default: 'term'
+  },
+
   params: {
     type: Object,
     default: () => ({})
@@ -116,11 +120,16 @@ const props = {
     default: () => ({})
   },
 
+  inputClass: {
+    type: String,
+    default: ''
+  },
+
   min: {
     type: Number,
     default: 2
   }
-}
+})
 
 const inputValue = defineModel({
   type: [String, Number],
@@ -133,13 +142,26 @@ const isListVisible = ref(false)
 const currentIndex = ref(-1)
 const list = ref([])
 const inputRef = useTemplateRef('inputRef')
+const metadata = ref(null)
+const level = ref()
 let requestTimeout = null
 let controller = null
+
+const limitList = computed(() => {
+  if (props.limit == 0) {
+    return list.value
+  }
+  return props.limit ? list.value : list.value.slice(0, props.limit)
+})
 
 onMounted(() => {
   if (props.autofocus) {
     inputRef.value.focus()
   }
+
+  AjaxCall('get', props.url).then(({ body }) => {
+    metadata.value = body
+  })
 })
 
 onBeforeUnmount(() => controller?.abort())
@@ -178,14 +200,6 @@ function setText(value) {
   inputValue.value = value
 }
 
-function limitList(list) {
-  if (props.limit == 0) {
-    return list
-  }
-
-  return props.limit ? list.value : list.value.slice(0, props.limit)
-}
-
 function selectItem(item) {
   inputValue.value = ''
 
@@ -197,11 +211,11 @@ function selectItem(item) {
   isListVisible.value = false
 }
 
-function makeUrlRequest() {
+function makeUrlRequest(params) {
   let tempUrl =
-    props.url + '?' + this.param + '=' + encodeURIComponent(inputValue.value)
+    props.url + '?' + props.param + '=' + encodeURIComponent(inputValue.value)
 
-  if (Object.keys(props.params).length) {
+  if (Object.keys(params).length) {
     tempUrl += `&${Qs.stringify(props.params, { arrayFormat: 'brackets' })}`
   }
   return tempUrl
@@ -219,29 +233,49 @@ function updateTimeout() {
   requestTimeout = setTimeout(update, props.delay)
 }
 
-function update() {
+async function update() {
   if (inputValue.value.length < Number(props.min)) return
 
+  const levels = Object.values(metadata.value.paths)
   list.value = []
-
   isLoading.value = true
-  this.controller?.abort()
-  this.controller = new AbortController()
 
-  AjaxCall('get', makeUrlRequest(), {
-    headers: props.headers,
-    signal: controller.value.signal
+  const maxLevel = level.value || levels.length
+  const currentLevel = level.value || 1
+
+  try {
+    for (let i = currentLevel; i <= maxLevel; i++) {
+      const response = await makeRequest(i)
+      const data = response.body.response
+
+      if (data.length) {
+        list.value = props.exclude ? data.filter(props.exclude) : data
+        isListVisible.value = list.value.length > 0
+
+        emit('end', list.value)
+        break
+      }
+    }
+  } catch {}
+
+  isLoading.value = false
+}
+
+async function makeRequest(level) {
+  const url = makeUrlRequest({
+    ...props.params,
+    level
   })
-    .then(({ body }) => {
-      list.value = props.exclude ? body.filter(props.exclude) : body
 
-      isListVisible.value = list.value.length > 0
-      this.$emit('end', this.showList)
-    })
-    .catch(() => {})
-    .finally(() => {
-      isLoading.value = true
-    })
+  controller?.abort()
+  controller = new AbortController()
+
+  const request = await AjaxCall('get', url, {
+    headers: props.headers,
+    signal: controller.signal
+  })
+
+  return request
 }
 
 function setFocus() {
