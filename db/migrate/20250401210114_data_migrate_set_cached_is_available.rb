b67aa2397c2702d5c29408befca53b15fd9efe45
mjy <diapriid@gmail.com>
2025-04-19 17:35:02 -0500

    Scope constant

--- db/migrate/20250401210114_data_migrate_set_cached_is_available.rb
+++ db/migrate/20250401210114_data_migrate_set_cached_is_available.rb
@@ -1,14 +1,14 @@
-# 
+#
 # !! Time to completion is > 5 minutes for 1my rows
-# 
+#
 class DataMigrateSetCachedIsAvailable < ActiveRecord::Migration[7.2]
   def change
-    a = Protonym.joins(:taxon_name_relationships).where(taxon_name_relationships: {type: TAXON_NAME_RELATIONSHIP_NAMES_MISSPELLING_AND_MISAPPLICATION})
-    b = Protonym.joins(:taxon_name_classifications).where(taxon_name_classifications: {type: TAXON_NAME_CLASS_NAMES_UNAVAILABLE})
+    a = Protonym.joins(:taxon_name_relationships).where(taxon_name_relationships: {type: ::TAXON_NAME_RELATIONSHIP_NAMES_MISSPELLING_AND_MISAPPLICATION})
+    b = Protonym.joins(:taxon_name_classifications).where(taxon_name_classifications: {type: ::TAXON_NAME_CLASS_NAMES_UNAVAILABLE})
     c = ::Queries.union(TaxonName, [a,b]).in_batches
 
     c.update_all(cached_is_available: false)
 
-    d = TaxonName.connection.execute("UPDATE taxon_names SET cached_is_available = '1' WHERE cached_is_available is null;") 
+    d = TaxonName.connection.execute("UPDATE taxon_names SET cached_is_available = '1' WHERE cached_is_available is null;")
   end
 end
