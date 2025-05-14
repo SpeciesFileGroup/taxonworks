8105c53a1470261fad1312ac1e8df2ac9210fdc1
jlpereira <pepelui@outlook.com>
2025-05-14 17:21:45 -0300

    Fix new topic form

--- app/javascript/vue/tasks/contents/editor/components/Topic/TopicNew.vue
+++ app/javascript/vue/tasks/contents/editor/components/Topic/TopicNew.vue
@@ -32,34 +32,39 @@
           <div class="field">
             <textarea
               v-model="topic.definition"
+              rows="4"
               placeholder="Definition"
             />
           </div>
         </form>
       </template>
       <template #footer>
-        <div class="flex-separate">
-          <input
-            class="button normal-input button-submit"
-            type="submit"
-            @click.prevent="createNewTopic"
-            :disabled="topic.name.length < 2 || topic.definition.length < 20"
-            value="Create"
-          />
-        </div>
+        <VBtn
+          color="create"
+          medium
+          @click.prevent="createNewTopic"
+          :disabled="isDisabled"
+        >
+          Create
+        </VBtn>
       </template>
     </VModal>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import { ControlledVocabularyTerm } from '@/routes/endpoints'
+import VBtn from '@/components/ui/VBtn/index.vue'
 import VModal from '@/components/ui/Modal.vue'
 
 const emit = defineEmits(['create'])
 
-const topic = newTopic()
+const topic = ref(newTopic())
+
+const isDisabled = computed(
+  () => topic.value.name.length < 2 || topic.value.definition.length < 20
+)
 
 const isModalVisible = ref(false)
 
