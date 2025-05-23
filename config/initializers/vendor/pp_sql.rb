629a3aaab7fd2407018f30899aef058e74843c7b
Tom Klein <twointofive@gmail.com>
2025-05-23 10:26:27 -0500

    Set pp_sql logging by env var or by console function call

--- config/initializers/vendor/pp_sql.rb
+++ config/initializers/vendor/pp_sql.rb
@@ -1,4 +1,17 @@
 if Rails.env.development? && defined?(Rails::Console)
   require 'pp_sql'
-  PpSql.add_rails_logger_formatting = true
+
+  if ENV['RAILS_PP_SQL_CONSOLE_LOGGING']
+    PpSql.add_rails_logger_formatting = true
+  end
+
+  def pp(bool)
+    if bool == true || bool == false
+      puts "PpSql logging is now set to #{bool}."
+    else
+      puts "Boolean input value required."
+    end
+
+    PpSql.add_rails_logger_formatting = bool
+  end
 end
