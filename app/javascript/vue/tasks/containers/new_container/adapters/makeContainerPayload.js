d94e108ef0377e33dbd4f2ef1d01984537218745
jlpereira <pepelui@outlook.com>
2025-05-24 10:37:26 -0300

    Remove container parent

--- app/javascript/vue/tasks/containers/new_container/adapters/makeContainerPayload.js
+++ app/javascript/vue/tasks/containers/new_container/adapters/makeContainerPayload.js
@@ -3,7 +3,6 @@ export function makeContainerPayload(data) {
     id: data.id,
     name: data.name,
     type: data.type,
-    parent_id: data.parentId,
     size_x: data.size.x,
     size_y: data.size.y,
     size_z: data.size.z
