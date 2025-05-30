a79b8f9adcd0b677184cd30a35b8a5bd7b62e8ca
Tom Klein <twointofive@gmail.com>
2025-05-30 13:12:54 -0500

    Convert batch attribute to use batch_by_filter_scope pattern #2639
    Only supports add for now, but ready for remove and edit.

--- spec/models/attribution_spec.rb
+++ spec/models/attribution_spec.rb
@@ -1,43 +1,70 @@
 require 'rails_helper'
 
 RSpec.describe Attribution, type: :model do
+  include ActiveJob::TestHelper
 
   let(:attribution) { Attribution.new }
+  let(:image) { FactoryBot.create(:valid_image) }
 
   specify 'can not be built nested and invalid' do
     a = FactoryBot.create(:valid_attribution)
-    b = FactoryBot.build(:valid_attribution, attribution_object: a.attribution_object) 
+    b = FactoryBot.build(:valid_attribution, attribution_object: a.attribution_object)
     expect(b.valid?).to be_falsey
   end
 
   specify 'unique to object' do
     a = FactoryBot.create(:valid_attribution)
-    b = FactoryBot.build(:valid_attribution, attribution_object: a.attribution_object) 
+    b = FactoryBot.build(:valid_attribution, attribution_object: a.attribution_object)
     expect(b.valid?).to be_falsey
   end
 
-  specify '#license' do 
+  specify '#license' do
     attribution.license = 'foo'
     attribution.valid?
     expect(attribution.errors.include?(:license)).to be_truthy
   end
 
-  specify '#copyright_year 1' do 
+  specify '#copyright_year 1' do
     attribution.copyright_year = '1'
     attribution.valid?
     expect(attribution.errors.include?(:copyright_year)).to be_truthy
   end
 
-  specify '#copyright_year 2' do 
+  specify '#copyright_year 2' do
     attribution.copyright_year = '1984'
     attribution.valid?
     expect(attribution.errors.include?(:copyright_year)).to be_falsey
   end
 
   specify '#attribution_object_type' do
-    attribution.attribution_object_type = "" 
+    attribution.attribution_object_type = ''
     attribution.save
-    expect(attribution.attribution_object_type).to eq(nil) 
+    expect(attribution.attribution_object_type).to eq(nil)
+  end
+
+  specify '#batch_by_filter_scope :add' do
+    q = ::Queries::Image::Filter.new(image_id: image.id)
+    Attribution.batch_by_filter_scope(
+      filter_query: { 'image_query' => q.params },
+      mode: :add,
+      params: {license:	'CC0 1.0 Universal (CC0 1.0) Public Domain Dedication'}
+    )
+    expect(Attribution.all.count).to eq(1)
+  end
+
+  specify '#batch_by_filter_scope :add, async' do
+    q = ::Queries::Image::Filter.new(image_id: image.id)
+    Attribution.batch_by_filter_scope(
+      filter_query: { 'image_query' => q.params },
+      mode: :add,
+      params: {license:	'CC0 1.0 Universal (CC0 1.0) Public Domain Dedication'},
+      async_cutoff: 0
+    )
+    expect(Attribution.all.count).to eq(0)
+
+    perform_enqueued_jobs
+
+    expect(Attribution.all.count).to eq(1)
   end
 
 end
