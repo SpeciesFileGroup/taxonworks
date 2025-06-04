b513bae4d2d4e760114aa517ae1886dacedbcf67
Tom Klein <twointofive@gmail.com>
2025-06-04 10:19:08 -0500

    Add taxon name facet to filter TNR #4010

--- app/javascript/vue/tasks/taxon_name_relationships/filter/components/filter.vue
+++ app/javascript/vue/tasks/taxon_name_relationships/filter/components/filter.vue
@@ -3,6 +3,19 @@
     v-model="params"
   />
 
+  <FacetTaxonName
+    include
+    v-model="params"
+  >
+    <template #top>
+      <div class="subj-obj">
+        <SubjectObjectSelector
+          v-model="params.taxon_name_subject_object"
+        />
+      </div>
+    </template>
+  </FacetTaxonName>
+
   <FacetUsers v-model="params" />
   <FacetWith
     v-for="param in WITH_PARAMS"
@@ -18,7 +31,9 @@
 import FacetDiffModel from '@/components/Filter/Facets/shared/FacetDiffMode.vue'
 import FacetUsers from '@/components/Filter/Facets/shared/FacetHousekeeping/FacetHousekeeping.vue'
 import FacetWith from '@/components/Filter/Facets/shared/FacetWith.vue'
+import FacetTaxonName from '@/components/Filter/Facets/TaxonName/FacetTaxonName.vue'
 import FacetTaxonNameRelationshipType from './filters/FacetTaxonNameRelationshipType.vue'
+import SubjectObjectSelector from './SubjectObjectSelector.vue'
 
 const WITH_PARAMS = ['citations', 'notes', 'verifiers']
 
@@ -28,3 +43,9 @@ const params = defineModel({
 })
 
 </script>
+
+<style scoped>
+.subj-obj {
+  margin-bottom: 1.5em;
+}
+</style>
