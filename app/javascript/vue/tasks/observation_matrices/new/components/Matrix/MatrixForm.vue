094af9d52cc9b0e07a41ba6092aa4c9d1e68b4b1
jlpereira <pepelui@outlook.com>
2025-06-16 12:16:48 -0300

    Refactor new observation matrix components

--- app/javascript/vue/tasks/observation_matrices/new/components/Matrix/MatrixForm.vue
+++ app/javascript/vue/tasks/observation_matrices/new/components/Matrix/MatrixForm.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="panel basic-information">
-    <div class="header flex-separate">
+  <BlockLayout>
+    <template #header>
       <h3>Matrix</h3>
-    </div>
-    <div class="body">
+    </template>
+    <template #body>
       <div class="field label-above">
         <label>Name</label>
         <div class="horizontal-left-content full_width">
@@ -46,8 +46,8 @@
           />
         </div>
       </template>
-    </div>
-  </div>
+    </template>
+  </BlockLayout>
 </template>
 
 <script setup>
@@ -60,6 +60,7 @@ import { useStore } from 'vuex'
 import MatrixParent from './MatrixParent.vue'
 import VSwitch from './switch.vue'
 import VBtn from '@/components/ui/VBtn/index.vue'
+import BlockLayout from '@/components/layout/BlockLayout.vue'
 
 const store = useStore()
 
--- app/javascript/vue/tasks/observation_matrices/new/components/rows/dynamic.vue
+++ app/javascript/vue/tasks/observation_matrices/new/components/rows/dynamic.vue
@@ -1,157 +1,136 @@
 <template>
-  <div class="panel basic-information separate-top">
-    <div class="header">
+  <BlockLayout class="panel basic-information separate-top">
+    <template #header>
       <h3>Rows</h3>
-    </div>
-    <div class="body">
+    </template>
+    <template #body>
       <div>
-        <spinner-component v-if="loadingTag" />
+        <VSpinner v-if="loadingTag" />
         <fieldset class="separate-bottom">
           <legend>Tag/Keyword</legend>
-          <smart-selector
+          <SmartSelector
             :options="smartOptions"
             v-model="view"
             name="rows-smart"
           />
           <component
-            v-if="componentExist"
-            @send="create"
+            v-if="currentComponent"
             :list="selectorLists[view]"
-            :is="view + 'Component'"
+            :is="currentComponent"
+            @send="create"
           />
-          <autocomplete
+          <VAutocomplete
             v-else
             url="/controlled_vocabulary_terms/autocomplete"
             param="term"
             label="label_html"
             :clear-after="true"
             placeholder="Search a keyword"
-            @getItem="create"
             min="2"
+            @get-item="create"
           />
         </fieldset>
       </div>
       <div>
-        <spinner-component v-if="loadingTaxon" />
+        <VSpinner v-if="loadingTaxon" />
         <fieldset>
           <legend>Taxon name</legend>
-          <smart-selector
+          <SmartSelector
             :options="smartTaxon"
             v-model="viewTaxonName"
             name="rows-smart-taxon"
           />
           <template v-if="smartTaxon.length">
-            <smart-taxon-list
+            <SmartTaxonList
               class="no_bullets"
               v-if="viewTaxonName !== 'search'"
               :created-list="rowsListDynamic"
               :list="listTaxon[viewTaxonName]"
-              @selected="createTaxon"
               value-compare="taxon_name_id"
+              @selected="createTaxon"
             />
-            <autocomplete
+            <VAutocomplete
               v-else
               url="/taxon_names/autocomplete"
               param="term"
               label="label_html"
               :clear-after="true"
               placeholder="Search a taxon name"
-              @getItem="createTaxon"
               min="2"
+              @get-item="createTaxon"
             />
           </template>
         </fieldset>
       </div>
-    </div>
-  </div>
+    </template>
+  </BlockLayout>
 </template>
-<script>
-import smartSelector from '../shared/smartSelector'
-import {
-  default as quickComponent,
-  default as recentComponent,
-  default as pinboardComponent
-} from '../shared/tag_list'
 
-import smartTaxonList from './dynamic/smartList'
-import Autocomplete from '@/components/ui/Autocomplete'
-import SpinnerComponent from '@/components/ui/VSpinner'
+<script setup>
+import { TAXON_NAME, CONTROLLED_VOCABULARY_TERM } from '@/constants/index.js'
+import { computed, ref } from 'vue'
+import { useStore } from 'vuex'
 import { GetSmartSelector } from '../../request/resources'
 import { ActionNames } from '../../store/actions/actions'
 import { GetterNames } from '../../store/getters/getters'
+import {
+  default as quick,
+  default as recent,
+  default as pinboard
+} from '../shared/tag_list'
+import SmartSelector from '../shared/smartSelector'
+import SmartTaxonList from './dynamic/smartList'
+import VAutocomplete from '@/components/ui/Autocomplete'
+import VSpinner from '@/components/ui/VSpinner'
+import BlockLayout from '@/components/layout/BlockLayout.vue'
 import ObservationTypes from '../../const/types.js'
-import { TAXON_NAME, CONTROLLED_VOCABULARY_TERM } from '@/constants/index.js'
 
-export default {
-  components: {
-    smartSelector,
-    quickComponent,
-    recentComponent,
-    pinboardComponent,
-    smartTaxonList,
-    Autocomplete,
-    SpinnerComponent
-  },
+const COMPONENTS = { quick, recent, pinboard }
 
-  computed: {
-    componentExist() {
-      return this.$options.components[this.view + 'Component']
-    },
-    matrixId() {
-      return this.$store.getters[GetterNames.GetMatrix].id
-    },
-    rowsListDynamic() {
-      return this.$store.getters[GetterNames.GetMatrixRowsDynamic]
-    }
-  },
+const store = useStore()
+const currentComponent = computed(() => COMPONENTS[view.value])
+const matrixId = computed(() => store.getters[GetterNames.GetMatrix].id)
+const rowsListDynamic = computed(
+  () => store.getters[GetterNames.GetMatrixRowsDynamic]
+)
 
-  data() {
-    return {
-      smartOptions: ['quick', 'recent', 'pinboard', 'tag'],
-      smartTaxon: [],
-      selectorLists: undefined,
-      listTaxon: undefined,
-      view: undefined,
-      viewTaxonName: 'search',
-      loadingTag: true,
-      loadingTaxon: true
-    }
-  },
-  mounted() {
-    GetSmartSelector('keywords').then((response) => {
-      this.smartOptions = this.smartOptions.filter((value) =>
-        Object.keys(response.body).includes(value)
-      )
-      this.selectorLists = response.body
-      this.loadingTag = false
-      this.smartOptions.push('search')
-    })
-    GetSmartSelector('taxon_names').then((response) => {
-      this.smartTaxon = ['quick', 'recent', 'pinboard'].filter((value) =>
-        Object.keys(response.body).includes(value)
-      )
-      this.smartTaxon.push('search')
-      this.listTaxon = response.body
-      this.loadingTaxon = false
-    })
-  },
-  methods: {
-    create(item) {
-      this.$store.dispatch(ActionNames.CreateRowItem, {
-        observation_object_id: item.id,
-        observation_object_type: CONTROLLED_VOCABULARY_TERM,
-        observation_matrix_id: this.matrixId,
-        type: ObservationTypes.Row.Tag
-      })
-    },
-    createTaxon(item) {
-      this.$store.dispatch(ActionNames.CreateRowItem, {
-        observation_object_id: item.id,
-        observation_object_type: TAXON_NAME,
-        observation_matrix_id: this.matrixId,
-        type: ObservationTypes.Row.TaxonName
-      })
-    }
-  }
+const smartOptions = ref([])
+const smartTaxon = ref([])
+const selectorLists = ref()
+const listTaxon = ref()
+const view = ref()
+const viewTaxonName = ref('search')
+const loadingTag = ref(true)
+const loadingTaxon = ref(true)
+
+GetSmartSelector('keywords').then(({ body }) => {
+  smartOptions.value = Object.keys(body)
+  selectorLists.value = body
+  loadingTag.value = false
+  smartOptions.value.push('search')
+})
+
+GetSmartSelector('taxon_names').then(({ body }) => {
+  smartTaxon.value = [...Object.keys(body), 'search']
+  listTaxon.value = body
+  loadingTaxon.value = false
+})
+
+function create(item) {
+  store.dispatch(ActionNames.CreateRowItem, {
+    observation_object_id: item.id,
+    observation_object_type: CONTROLLED_VOCABULARY_TERM,
+    observation_matrix_id: matrixId.value,
+    type: ObservationTypes.Row.Tag
+  })
+}
+
+function createTaxon(item) {
+  store.dispatch(ActionNames.CreateRowItem, {
+    observation_object_id: item.id,
+    observation_object_type: TAXON_NAME,
+    observation_matrix_id: matrixId.value,
+    type: ObservationTypes.Row.TaxonName
+  })
 }
 </script>
