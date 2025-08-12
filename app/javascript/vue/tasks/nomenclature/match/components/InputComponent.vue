32c5401624334a6d48ff8793782703c76e0d0455
Tom Klein <twointofive@gmail.com>
2025-08-12 15:23:51 -0500

    Limit Remove Authors to 5,000 names, with warning #4518

--- app/javascript/vue/tasks/nomenclature/match/components/InputComponent.vue
+++ app/javascript/vue/tasks/nomenclature/match/components/InputComponent.vue
@@ -20,17 +20,32 @@
         :disabled="!text"
         color="primary"
         @click="removeAuthors"
+        data-help="Only the first 5,000 rows will be processed and returned"
       >
         Remove authors
       </VBtn>
+      <span
+        v-if="removeAuthorsWarn && text"
+        class="vertical-center margin-small-left"
+      >
+        <VIcon
+          small
+          name="attention"
+          color="attention"
+        />
+        Only {{ REMOVE_AUTHORS_LIMIT }} records processed
+      </span>
     </div>
   </div>
 </template>
 
 <script setup>
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import { TaxonName } from '@/routes/endpoints'
 import VBtn from '@/components/ui/VBtn/index.vue'
+import VIcon from '@/components/ui/VIcon/index.vue'
+
+const REMOVE_AUTHORS_LIMIT = 5000
 
 const params = defineModel({
   type: Object,
@@ -41,6 +56,8 @@ const text = defineModel('names', {
   type: String,
   required: true
 })
+
+const removeAuthorsWarn = ref(false)
 
 const lines = computed(() =>
   text.value.split('\n').filter((line) => line.trim())
@@ -53,9 +70,16 @@ function removeAuthors() {
 
   TaxonName.removeAuthors(payload)
     .then(({ body }) => {
-      text.value = body.names.join('\n')
+      removeAuthorsWarn.value = body.names.length > REMOVE_AUTHORS_LIMIT
+      text.value = body.names.slice(0, REMOVE_AUTHORS_LIMIT).join('\n')
       TW.workbench.alert.create('Removed authors.', 'notice')
     })
     .catch(() => {})
 }
 </script>
+
+<style lang="css">
+.vertical-center {
+  vertical-align: bottom;
+}
+</style>
