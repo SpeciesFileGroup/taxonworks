8fb2b692ad6a546066b8311df64d28f5e2f6fa64
Tom Klein <twointofive@gmail.com>
2025-08-31 20:25:01 -0500

    Move validation out of project prefs index action #2830
    I'm regretting not doing this with vue now; need modern turbo/ajax to do this stuff native rails.

--- app/controllers/tasks/projects/dwc_export_preferences_controller.rb
+++ app/controllers/tasks/projects/dwc_export_preferences_controller.rb
@@ -3,31 +3,7 @@ class Tasks::Projects::DwcExportPreferencesController < ApplicationController
 
   def index
     @project = Project.find(sessions_current_project_id)
-    dataset = params[:dataset]
-    additional_metadata = params[:additional_metadata]
-
-    if request.post?
-      if Export::Dwca::Eml.still_stubbed?(dataset, additional_metadata)
-        #return error('Replace or delete all STUBbed fields to proceed')
-      end
-
-      @dataset_errors, @additional_metadata_errors =
-        Export::Dwca::Eml.validate_fragments(dataset, additional_metadata)
-
-      if @dataset_errors.count + @additional_metadata_errors.count == 0
-        if @project.save_eml_preferences(dataset, additional_metadata)
-          flash[:notice] = 'Preferences saved'
-          redirect_to @project
-        else # preferences save errors
-          # It's probably a bug if this happens, we should maybe raise here.
-          return error("Project save failed! #{@project.errors.full_messages.join(', ')}")
-        end
-      else # xml errors
-        return error
-      end
-    elsif request.get?
-      set_data
-    end
+    set_data
   end
 
   def is_public
@@ -36,6 +12,32 @@ class Tasks::Projects::DwcExportPreferencesController < ApplicationController
 
     flash[:notice] = '"Is public" updated'
     redirect_to action: :index
+  end
+
+  def validate_eml
+    @project = Project.find(sessions_current_project_id)
+    dataset = params[:dataset]
+    additional_metadata = params[:additional_metadata]
+
+    if Export::Dwca::Eml.still_stubbed?(dataset, additional_metadata)
+      #return error('Replace or delete all STUBbed fields to proceed')
+    end
+
+    @dataset_errors, @additional_metadata_errors =
+      Export::Dwca::Eml.validate_fragments(dataset, additional_metadata)
+
+    if @dataset_errors.count + @additional_metadata_errors.count == 0
+      if @project.save_eml_preferences(dataset, additional_metadata)
+        flash[:notice] = 'Preferences saved'
+        set_data
+        redirect_to action: :index
+      else # preferences save errors
+        # It's probably a bug if this happens, we should maybe raise here.
+        return error("Project save failed! #{@project.errors.full_messages.join(', ')}")
+      end
+    else # xml errors
+      return error
+    end
   end
 
   private
@@ -54,8 +56,8 @@ class Tasks::Projects::DwcExportPreferencesController < ApplicationController
 
     @dataset = params[:dataset]
     @additional_metadata = params[:additional_metadata]
-    @is_public = params[:is_public]
-    render :index, status: :unprocessable_entity
+    @is_public = @project.dwc_download_is_public?
+    redirect_to action: :index, status: :unprocessable_entity
   end
 
 
--- config/routes/tasks.rb
+++ config/routes/tasks.rb
@@ -318,7 +318,7 @@ scope :tasks do
 
     scope :dwc_export_preferences, controller: 'tasks/projects/dwc_export_preferences' do
       get '/', action: :index, as: 'project_dwc_export_preferences_task'
-      post '/', action: :index, as: 'project_dwc_validate_export_preferences'
+      post '/', action: :validate_eml, as: 'project_dwc_validate_eml'
       post 'is_public', as: 'project_dwc_is_public'
     end
   end
