fd633117e17eafd658bd2e388c1bf39f0357c78a
Tom Klein <twointofive@gmail.com>
2025-09-05 14:45:29 -0500

    Fix specs #1488

--- spec/lib/export/dwca/data_spec.rb
+++ spec/lib/export/dwca/data_spec.rb
@@ -97,7 +97,11 @@ describe Export::Dwca::Data, type: :model, group: :darwin_core do
       context 'extension_scopes: [:biological_associations]' do
         let(:biological_relationship) { FactoryBot.create(:valid_biological_relationship) }
         let!(:ba1) { BiologicalAssociation.create!(biological_relationship:, biological_association_subject: CollectionObject.first, biological_association_object: CollectionObject.last) }
-        let(:biological_association_scope) { BiologicalAssociation.all }
+        let(:biological_association_scope) {
+          { core_params: {},
+            collection_objects_query: BiologicalAssociation.all
+          }
+        }
 
         specify '#biological_associations_resource_relationship is a tempfile' do
           s = scope.where('id > 1')
@@ -108,7 +112,9 @@ describe Export::Dwca::Data, type: :model, group: :darwin_core do
         specify '#biological_associations_resource_relationship returns lines for specimens' do
           s = scope.where('id > 1')
           d = Export::Dwca::Data.new(core_scope: s, extension_scopes: { biological_associations:  biological_association_scope  })
-          expect(d.biological_associations_resource_relationship.count).to eq(2)
+          # 1 header line, two ba lines, one for each direction of the
+          # relationship.
+          expect(d.biological_associations_resource_relationship.count).to eq(3)
         end
       end
 
@@ -181,7 +187,6 @@ describe Export::Dwca::Data, type: :model, group: :darwin_core do
           FactoryBot.create(:valid_depiction, depiction_object: o)
           s = scope.where('id > 1')
           d = Export::Dwca::Data.new(core_scope: s, extension_scopes: { media: media_scope })
-          byebug
 
           expect(d.media_resource_relationship.count).to eq(2)
         end
