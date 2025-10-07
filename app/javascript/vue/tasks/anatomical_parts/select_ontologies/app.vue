32b6a029f8ebaeaf469462baaedd961854664cc1
Tom Klein <twointofive@gmail.com>
2025-10-07 09:11:31 -0500

    Add filtering to Ontology selection; refactor promise handling #4407

--- app/javascript/vue/tasks/anatomical_parts/select_ontologies/app.vue
+++ app/javascript/vue/tasks/anatomical_parts/select_ontologies/app.vue
@@ -17,7 +17,7 @@
       >
         <template
           v-for="(o, i) in selectedOntologies"
-          :key="oid"
+          :key="o.oid"
         >
           <li class="horizontal-left-content gap-xsmall">
             <span>{{ `${i + 1}. ${o.oid}: ${o.title}` }}</span>
@@ -40,22 +40,51 @@
       </p>
     </fieldset>
 
+    <div class="margin-medium-bottom">
+      See <a href="https://www.ebi.ac.uk/ols4/ontologies">https://www.ebi.ac.uk/ols4/ontologies</a> for further information on available ontologies.
+    </div>
+
+    <div>
+      {{ filteredOntologies.length }}
+      {{ filteredOntologies.length == 1 ? 'ontology' : 'ontologies' }}
+    </div>
     <table
-      v-if="ontologies.length > 0"
-      class="table-striped"
+      class="table-striped full_width"
     >
       <thead>
         <tr>
-          <th></th>
+          <th />
           <th>ID</th>
           <th>Title</th>
           <th>Description</th>
+        </tr>
+
+        <tr>
+          <th />
+          <th>
+            <input
+              v-model="idText"
+              @input="filterTable"
+            />
+          </th>
+          <th>
+            <input
+              v-model="titleText"
+              @input="filterTable"
+            />
+          </th>
+          <th>
+            <input
+              v-model="descriptionText"
+              @input="filterTable"
+            />
+          </th>
         </tr>
       </thead>
 
       <tbody>
         <template
-          v-for="(o) in ontologies"
+          v-for="(o) in filteredOntologies"
           :key="o.oid"
         >
           <tr>
@@ -90,14 +119,18 @@
 import VBtn from '@/components/ui/VBtn/index.vue'
 import VIcon from '@/components/ui/VIcon/index.vue'
 import VSpinner from '@/components/ui/VSpinner.vue'
-import { addToArray, removeFromArray } from '@/helpers'
+import { addToArray, intersectArrays, removeFromArray } from '@/helpers'
 import { AnatomicalPart } from '@/routes/endpoints'
 import { onMounted, ref } from 'vue'
 import { getCurrentProjectId } from '@/helpers/project.js'
 
 const projectId = Number(getCurrentProjectId())
 const ontologies = ref([])
+const filteredOntologies = ref([])
 const selectedOntologies = ref([])
+const idText = ref('')
+const titleText = ref('')
+const descriptionText = ref('')
 const isLoading = ref(false)
 
 function selectOntology(o) {
@@ -123,33 +156,71 @@ function saveToProject() {
   AnatomicalPart.saveOntologyIdsToProject(payload)
 }
 
-onMounted(async () => {
-  isLoading.value = true
-  const preferenceOids = null
+function filterById() {
+  filterTable()
+}
 
-  try {
-    const [a, b] = await Promise.allSettled([
-      AnatomicalPart.ontologies()
-        .then(({ body }) => (ontologies.value = body)),
-
-      AnatomicalPart.ontologyIdPreferences({ project_id: projectId })
-        .then(({ body }) => {
-          preferenceOids = body
-      })
-    ])
-
-    if (a.status == 'fulfilled' && b.status == 'fulfilled') {
-      preferenceOids.forEach((poid) => {
-        let i = -1
-        if ((i = ontologies.value.findIndex((o => o.oid == poid))) != -1) {
-          selectedOntologies.value.push(ontologies.value[i])
-        }
-      })
-    }
-  } finally {
-    isLoading.value = false
+function filterTable() {
+  if (!idText.value && !titleText.value && !descriptionText.value) {
+    filteredOntologies.value = ontologies.value
+    return
   }
 
+  let idMatches = ontologies.value
+  if (!!idText) {
+    idMatches = []
+    ontologies.value.forEach((o) => {
+      if (o.oid.toLowerCase().includes(idText.value)) {
+        idMatches.push(o)
+      }
+    })
+  }
+
+  let titleMatches = ontologies.value
+  if (!!titleText) {
+    titleMatches = []
+    ontologies.value.forEach((o) => {
+      if (o.title.toLowerCase().includes(titleText.value)) {
+        titleMatches.push(o)
+      }
+    })
+  }
+
+  let descriptionMatches = ontologies.value
+  if (!!descriptionText) {
+    descriptionMatches = []
+    ontologies.value.forEach((o) => {
+      if (o.description.toLowerCase().includes(descriptionText.value)) {
+        descriptionMatches.push(o)
+      }
+    })
+  }
+
+  filteredOntologies.value =
+    intersectArrays(idMatches, titleMatches, descriptionMatches)
+}
+
+onMounted(() => {
+  isLoading.value = true
+  Promise.all([
+    AnatomicalPart.ontologies()
+      .catch(() => {}),
+
+    AnatomicalPart.ontologyIdPreferences({ project_id: projectId })
+      .catch(() => {})
+  ])
+  .then((values) => {
+    ontologies.value = values[0].body
+    filteredOntologies.value = ontologies.value
+
+    values[1].body.forEach((poid) => {
+      let i
+      if ((i = ontologies.value.findIndex((o => o.oid == poid))) != -1) {
+        selectedOntologies.value.push(ontologies.value[i])
+      }
+    })
+  })
+  .finally(() => isLoading.value = false)
 })
 
 </script>
