71cb4717d1a5e9063dcbdd9ef65b8525885f228c
jlpereira <pepelui@outlook.com>
2025-11-05 13:29:16 -0300

    Changed create/update origin citation from nested to RESTful

--- app/javascript/vue/tasks/digitize/store/actions/saveTypeMaterial.js
+++ app/javascript/vue/tasks/digitize/store/actions/saveTypeMaterial.js
@@ -1,35 +1,86 @@
 import { MutationNames } from '../mutations/mutations'
-import { TypeMaterial } from '@/routes/endpoints'
+import { TypeMaterial, Citation } from '@/routes/endpoints'
 import makeTypeMaterial from '@/factory/TypeMaterial.js'
+import makeCitation from '@/factory/Citation'
+import { TYPE_MATERIAL } from '@/constants'
 
-export default ({ commit, state }) => {
+export default async ({ commit, state }) => {
   const typeSpecimens = [...state.typeSpecimens]
   const promises = []
 
-  typeSpecimens.forEach((typeSpecimen) => {
-    if (!typeSpecimen.isUnsaved) return
+  for (const typeSpecimen of typeSpecimens) {
+    const { citation, isUnsaved, id } = typeSpecimen
 
-    const payload = {
-      type_material: makeTypeSpecimenPayback(state, typeSpecimen),
-      extend: ['origin_citation']
+    if (isUnsaved) {
+      const payload = {
+        type_material: makeTypeSpecimenPayback(state, typeSpecimen)
+      }
+
+      try {
+        const saveRequest = id
+          ? TypeMaterial.update(id, payload)
+          : TypeMaterial.create(payload)
+
+        promises.push(saveRequest)
+
+        const { body } = await saveRequest
+        const savedRecord = {
+          ...makeTypeMaterial({ ...typeSpecimen, ...body }),
+          citation
+        }
+
+        if (citation.isUnsaved) {
+          await saveCitation(savedRecord)
+        }
+
+        commit(MutationNames.AddTypeMaterial, savedRecord)
+        promises.push(Promise.resolve())
+      } catch (error) {
+        promises.push(Promise.reject(error))
+      }
+    }
+  }
+
+  return Promise.allSettled(promises)
+}
+
+function makeCitationPayload(citation) {
+  return {
+    id: citation.id,
+    source_id: citation.source_id,
+    pages: citation.pages,
+    citation_object_type: TYPE_MATERIAL,
+    is_original: true
+  }
+}
+
+async function saveCitation(typeMaterial) {
+  const citationPayload = {
+    ...makeCitationPayload(typeMaterial.citation),
+    citation_object_id: typeMaterial.id
+  }
+  const hasCitationId = Boolean(citationPayload.id)
+  const hasSourceId = Boolean(citationPayload.source_id)
+
+  try {
+    if (!hasSourceId && hasCitationId) {
+      await Citation.destroy(citationPayload.id)
+
+      typeMaterial.citation = makeCitation(TYPE_MATERIAL)
+    } else {
+      const payload = {
+        citation: citationPayload
+      }
+
+      const { body } = citationPayload.id
+        ? await Citation.update(citationPayload.id, payload)
+        : await Citation.create(payload)
+
+      typeMaterial.citation.id = body.id
     }
 
-    const saveRequest = typeSpecimen.id
-      ? TypeMaterial.update(typeSpecimen.id, payload)
-      : TypeMaterial.create(payload)
-
-    promises.push(saveRequest)
-
-    saveRequest
-      .then(({ body }) => {
-        commit(
-          MutationNames.AddTypeMaterial,
-          makeTypeMaterial({ ...typeSpecimen, ...body })
-        )
-      })
-      .catch(() => {})
-  })
-  return Promise.all(promises)
+    typeMaterial.citation.isUnsaved = false
+  } catch {}
 }
 
 function makeTypeSpecimenPayback(state, typeSpecimen) {
@@ -38,18 +89,6 @@ function makeTypeSpecimenPayback(state, typeSpecimen) {
     protonym_id: typeSpecimen.protonymId,
     type_type: typeSpecimen.type,
     collection_object_id: state.collection_object.id
-  }
-
-  const { originCitation } = typeSpecimen
-  const { id, source_id, ...rest } = originCitation
-  const citation = { ...rest, source_id, id }
-
-  if (!source_id && id) {
-    citation._destroy = true
-  }
-
-  if (source_id || citation._destroy) {
-    payload.origin_citation_attributes = citation
   }
 
   return payload
