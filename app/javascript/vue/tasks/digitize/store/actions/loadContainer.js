3ab4c278030bd1a448ffac99f03f04ab58943e7e
jlpereira <pepelui@outlook.com>
2025-11-05 22:59:48 -0300

    throw error if container is not allowed

--- app/javascript/vue/tasks/digitize/store/actions/loadContainer.js
+++ app/javascript/vue/tasks/digitize/store/actions/loadContainer.js
@@ -4,34 +4,34 @@ import { CONTAINER_VIAL, CONTAINER_VIRTUAL, CONTAINER_PIN } from '@/constants'
 
 const ALLOWED_CONTAINERS = [CONTAINER_VIAL, CONTAINER_VIRTUAL, CONTAINER_PIN]
 
-export default ({ commit }, globalId) => {
-  const request = Container.for({
-    global_id: globalId,
-    extend: ['container_items']
-  })
-
-  request
-    .then(({ body }) => {
-      if (!ALLOWED_CONTAINERS.includes(body.type)) {
-        return Promise.reject()
-      }
-
-      const coIds = body.container_items.map((item) => item.contained_object_id)
-
-      commit(MutationNames.SetContainer, body)
-
-      body.container_items.forEach((item) =>
-        commit(MutationNames.AddContainerItem, item)
-      )
-
-      CollectionObject.where({
-        collection_object_id: coIds,
-        extend: ['dwc_occurrence']
-      }).then(({ body }) => {
-        commit(MutationNames.SetCollectionObjects, body)
-      })
+export default async ({ commit }, globalId) => {
+  try {
+    const { body } = await Container.for({
+      global_id: globalId,
+      extend: ['container_items']
     })
-    .catch(() => {})
 
-  return request
+    if (!ALLOWED_CONTAINERS.includes(body.type)) {
+      throw new Error('Not allowed container type')
+    }
+
+    const coIds = body.container_items.map((item) => item.contained_object_id)
+
+    commit(MutationNames.SetContainer, body)
+
+    body.container_items.forEach((item) =>
+      commit(MutationNames.AddContainerItem, item)
+    )
+
+    const { body: coBody } = await CollectionObject.where({
+      collection_object_id: coIds,
+      extend: ['dwc_occurrence']
+    })
+
+    commit(MutationNames.SetCollectionObjects, coBody)
+
+    return body
+  } catch (error) {
+    throw error
+  }
 }
