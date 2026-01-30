b626661f86bd1193f4b5c4d91b412ba4c7e69b90
Tom Klein <twointofive@gmail.com>
2026-01-30 09:16:12 -0600

    Merge reduced header into Downloads vue #4683

--- app/javascript/vue/components/packager/PackagerDownloads.vue
+++ app/javascript/vue/components/packager/PackagerDownloads.vue
@@ -1,10 +1,52 @@
 <template>
   <div class="packager-downloads margin-large-bottom">
-    <div class="flex-separate middle">
-      <h2>Download packages</h2>
+    <div class="packager-downloads__grid">
+      <h2 class="packager-downloads__title">Download packages</h2>
+      <VBtn
+        v-if="showBack"
+        class="packager-downloads__back"
+        :disabled="!canGoBack"
+        color="primary"
+        @click="onBack"
+      >
+        Back to {{ filterName }}
+      </VBtn>
+
+      <div class="packager-downloads__list">
+        <ul v-if="groups.length">
+          <li
+            v-for="group in groups"
+            :key="group.index"
+            class="packager-downloads__item margin-small-bottom"
+          >
+            <template v-if="group.available_count > 0">
+              <VBtn
+                color="primary"
+                @click="onDownload(group.index)"
+              >
+                {{ buildDownloadFilename(filenamePrefix, group.index, groups.length) }}
+              </VBtn>
+            </template>
+
+            <template v-else>
+              <span class="packager-downloads__disabled">
+                {{ buildDownloadFilename(filenamePrefix, group.index, groups.length) }}
+              </span>
+            </template>
+
+            <span class="margin-small-left">
+              {{ group.available_count || 0 }} available of
+              {{ itemCount(group) }} {{ itemLabel }} ·
+              {{ formatBytes(group.size) }} (estimated uncompressed)
+            </span>
+          </li>
+        </ul>
+        <p v-else>{{ emptyMessage }}</p>
+      </div>
+
       <div class="packager-downloads__controls display-flex align-center gap-medium">
         <label class="display-flex align-center gap-small">
-          Max MB
+          Max MB per download
           <input
             type="number"
             class="normal-input"
@@ -22,37 +64,6 @@
         </VBtn>
       </div>
     </div>
-
-    <ul v-if="groups.length" class="packager-downloads__list">
-      <li
-        v-for="group in groups"
-        :key="group.index"
-        class="packager-downloads__item margin-small-bottom"
-      >
-        <template v-if="group.available_count > 0">
-          <VBtn
-            color="primary"
-            @click="onDownload(group.index)"
-          >
-            {{ buildDownloadFilename(filenamePrefix, group.index, groups.length) }}
-          </VBtn>
-        </template>
-
-        <template v-else>
-          <span class="packager-downloads__disabled">
-            {{ buildDownloadFilename(filenamePrefix, group.index, groups.length) }}
-          </span>
-        </template>
-
-        <span class="margin-small-left">
-          {{ group.available_count || 0 }} available of
-          {{ itemCount(group) }} {{ itemLabel }} ·
-          {{ formatBytes(group.size) }} (estimated uncompressed)
-        </span>
-      </li>
-    </ul>
-
-    <p v-else>{{ emptyMessage }}</p>
   </div>
 </template>
 
@@ -64,6 +75,18 @@ const props = defineProps({
   groups: {
     type: Array,
     required: true
+  },
+  filterName: {
+    type: String,
+    default: ''
+  },
+  canGoBack: {
+    type: Boolean,
+    default: true
+  },
+  showBack: {
+    type: Boolean,
+    default: true
   },
   maxMb: {
     type: Number,
@@ -87,7 +110,7 @@ const props = defineProps({
   }
 })
 
-const emit = defineEmits(['update:maxMb', 'refresh', 'download'])
+const emit = defineEmits(['update:maxMb', 'refresh', 'download', 'back'])
 
 function itemCount(group) {
   const key = props.itemCountKey
@@ -108,9 +131,47 @@ function onRefresh() {
 function onDownload(index) {
   emit('download', index)
 }
+
+function onBack() {
+  emit('back')
+}
 </script>
 
 <style scoped>
+.packager-downloads__grid {
+  display: grid;
+  grid-template-columns: 1fr auto;
+  grid-template-rows: auto auto;
+  column-gap: 1.5rem;
+  row-gap: 0;
+  align-items: start;
+  margin-bottom: 1rem;
+}
+
+.packager-downloads__title {
+  grid-column: 1 / 2;
+  grid-row: 1 / 2;
+  align-self: center;
+}
+
+.packager-downloads__back {
+  grid-column: 2 / 3;
+  grid-row: 1 / 2;
+  justify-self: end;
+  align-self: center;
+}
+
+.packager-downloads__list {
+  grid-column: 1 / 2;
+  grid-row: 2 / 3;
+}
+
+.packager-downloads__controls {
+  grid-column: 2 / 3;
+  grid-row: 2 / 3;
+  justify-self: end;
+}
+
 .packager-downloads__disabled {
   color: #666;
   cursor: not-allowed;
