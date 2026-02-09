cecbc50712ba7d4809fe239597cca7fe804c11d0
jlpereira <pepelui@outlook.com>
2026-02-09 17:00:39 -0300

    Fix spec

--- spec/features/tasks/data_attributes/duplicate_data_attributes_spec.rb
+++ spec/features/tasks/data_attributes/duplicate_data_attributes_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-describe 'Duplicate data attributes task', type: :feature, group: :data_attributes do
-  let(:page_title) { 'Duplicate data attributes' }
-  let(:index_path) { index_duplicate_data_attributes_task_path }
+describe 'Repeated data attributes task', type: :feature, group: :data_attributes do
+  let(:page_title) { 'Repeated DataAttributes' }
+  let(:index_path) { duplicate_data_attributes_task_path }
 
   it_behaves_like 'a_login_required_and_project_selected_controller'
 
@@ -13,7 +13,7 @@ describe 'Duplicate data attributes task', type: :feature, group: :data_attribut
       before { visit index_path }
 
       specify 'page renders without error' do
-        expect(page).to have_content('Duplicate data attributes')
+        expect(page).to have_content('Repeated DataAttributes')
       end
 
       specify 'shows message when no filter provided' do
