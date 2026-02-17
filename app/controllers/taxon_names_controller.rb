7b66a28b025792847b53ad70f298954461ab9113
jlpereira <pepelui@outlook.com>
2026-02-17 16:01:05 -0300

    Move render logic to jbuilder

--- app/controllers/taxon_names_controller.rb
+++ app/controllers/taxon_names_controller.rb
@@ -241,7 +241,7 @@ class TaxonNamesController < ApplicationController
   # POST /taxon_names/match.json
   def match
 
-    result = Match::Otu::TaxonName.new(
+    @result = Match::Otu::TaxonName.new(
       names: match_params[:names] || [],
       project_id: sessions_current_project_id,
       levenshtein_distance: match_params[:levenshtein_distance] || 0,
@@ -250,21 +250,7 @@ class TaxonNamesController < ApplicationController
       try_without_subgenus: match_params[:try_without_subgenus] == 'true'
     ).call
 
-    render json: result.map { |r|
-      {
-        scientific_name: r[:scientific_name],
-        taxon_name_id: r[:taxon_name_id],
-        taxon_name: (r[:taxon_name]&.as_json(
-          only: [:id, :cached, :cached_html, :cached_author_year, :cached_valid_taxon_name_id, :name, :rank_class, :type]
-         ) || {}).merge(object_label: helpers.label_for_taxon_name(r[:taxon_name])),
-        otus: r[:otus].map { |o| ( o.as_json(
-          only: [:id, :name, :taxon_name_id]
-        ) || {}).merge(object_label: helpers.label_for_otu(o) )
-        },
-        ambiguous: r[:ambiguous],
-        matched: r[:matched]
-      }
-    }
+    render :match
   end
 
   # GET /taxon_names/1/original_combination
