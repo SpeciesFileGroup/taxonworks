355486db66a4fc9e77857a03202ba64ac19755c1
Tom Klein <twointofive@gmail.com>
2026-03-19 09:13:42 -0500

    Batch update synonymy type #4828

---
+++ app/javascript/vue/components/radials/taxon_name_relationship/radial.vue
@@ -0,0 +1,25 @@
+<template>
+  <RadialBatch
+    v-bind="attrs"
+    title="Radial taxon name relationship"
+    :slices="SLICES"
+    :object-type="TAXON_NAME_RELATIONSHIP"
+  />
+</template>
+
+<script setup>
+import { useAttrs } from 'vue'
+import { TAXON_NAME_RELATIONSHIP } from '@/constants'
+import RadialBatch from '@/components/radials/shared/RadialBatch.vue'
+import SynonymTypeSlice from './components/SynonymTypeSlice.vue'
+
+defineOptions({
+  name: 'RadialTaxonNameRelationship'
+})
+
+const SLICES = {
+  'Update synonym type': SynonymTypeSlice
+}
+
+const attrs = useAttrs()
+</script>
