d16eaa13a010413e55acd79104de968161ecd816
proceps <arboridia@gmail.com>
2026-04-21 14:21:15 -0500

    changes in ICPN regulations in class names.

--- app/models/nomenclatural_rank/icnp/higher_classification_group/class_rank.rb
+++ app/models/nomenclatural_rank/icnp/higher_classification_group/class_rank.rb
@@ -7,7 +7,7 @@ class NomenclaturalRank::Icnp::HigherClassificationGroup::ClassRank < Nomenclatu
   def self.validate_name_format(taxon_name)
     super
     return true if taxon_name.name.length < 2
-    taxon_name.errors.add(:name, 'name must end in -ia') if not(taxon_name.name =~ /.*ia\Z/)
+    # taxon_name.errors.add(:name, 'name must end in -ia') if not(taxon_name.name =~ /.*ia\Z/)
   end
 
   def self.valid_name_ending
