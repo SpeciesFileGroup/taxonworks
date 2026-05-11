7da3053ede9e9537e9c5d11b75d3e6fbeb10a2d4
Tom Klein <twointofive@gmail.com>
2026-05-11 17:31:30 -0500

    Make sqed processing automatic on create #3178

--- app/controllers/tasks/accessions/breakdown/sqed_depiction_controller.rb
+++ app/controllers/tasks/accessions/breakdown/sqed_depiction_controller.rb
@@ -81,8 +81,6 @@ class Tasks::Accessions::Breakdown::SqedDepictionController < ApplicationControl
   def set_sqed_depiction
     @sqed_depiction = SqedDepiction.where(project_id: sessions_current_project_id).find(params[:id])
     @sqed_depiction.update_column(:in_progress, Time.now)
-    # TODO: Run jobs in background with admin task.
-    # @sqed_depiction.preprocess
   end
 
 end
