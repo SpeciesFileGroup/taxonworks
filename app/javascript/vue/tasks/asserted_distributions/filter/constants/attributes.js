0b59fe989d5d2456f499c4b901d90696ba80da65
jlpereira <pepelui@outlook.com>
2026-05-29 16:58:33 -0300

    Update column label

--- app/javascript/vue/tasks/asserted_distributions/filter/constants/attributes.js
+++ app/javascript/vue/tasks/asserted_distributions/filter/constants/attributes.js
@@ -1,6 +1,6 @@
 export const ATTRIBUTES = {
   object_type: 'Object type',
-  objectGlobalId: 'Object radial',
+  objectGlobalId: 'Quick Forms',
   object_object_tag: 'Object',
   asserted_distribution_shape: 'Shape',
   data_origin: 'Data origin',
