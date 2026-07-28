7871a9d3116d7246b50d2de1da67902313a41069
jlpereira <pepelui@outlook.com>
2026-07-28 14:16:27 -0300

    Update packages. Add badge style to gbifference ramarks cells

--- app/javascript/vue/components/ui/Table/TableGbifference.vue
+++ app/javascript/vue/components/ui/Table/TableGbifference.vue
@@ -35,18 +35,22 @@ const emit = defineEmits(['click:cell'])
 const tableRef = ref()
 const gbifference = ref()
 
-onMounted(() => { initGbifference(props) })
+onMounted(() => {
+  initGbifference(props)
+})
 
 const initGbifference = (opt) => {
   const element = tableRef.value
 
   gbifference.value = new TableGbifference(element, opt)
-  gbifference.value.on('click:cell', (e) => { emit('click:cell', e) })
+  gbifference.value.on('click:cell', (e) => {
+    emit('click:cell', e)
+  })
 }
 
 watch(
   () => props,
-  newProps => {
+  (newProps) => {
     console.log(newProps)
     initGbifference(newProps)
   },
@@ -57,5 +61,32 @@ watch(
 <style>
 .table-gbifference {
   width: 100%;
+}
+
+.table-gbifference__remark {
+  padding: var(--spacing-xxs) var(--spacing-xs);
+  border-radius: var(--border-radius-xsmall);
+  font-size: var(--font-size-xs);
+  width: min-content;
+}
+
+.v-badge--default {
+  background-color: var(--badge-default-bg);
+  color: var(--badge-default-color);
+}
+
+.table-gbifference__remark--altered {
+  background-color: var(--badge-yellow-bg);
+  color: var(--badge-yellow-color);
+}
+
+.table-gbifference__remark--inferred {
+  background-color: var(--badge-blue-bg);
+  color: var(--badge-blue-color);
+}
+
+.table-gbifference__remark--excluded {
+  background-color: var(--badge-red-bg);
+  color: var(--badge-red-color);
 }
 </style>
