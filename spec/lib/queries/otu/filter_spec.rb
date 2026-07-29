d2833fa5dae9679ee247b88ee65c535f04fa559e
Tom Klein <twointofive@gmail.com>
2026-07-28 23:02:47 -0500

    Move tag and confidence concern specs to their own files #4157
    Nothing new, just restructuring

--- spec/lib/queries/otu/filter_spec.rb
+++ spec/lib/queries/otu/filter_spec.rb
@@ -60,77 +60,7 @@ describe Queries::Otu::Filter, type: :model, group: [:geo, :collection_objects, 
     end
   end
 
-  # confidences query concern
-  specify '#confidence_level_id' do
-    l = FactoryBot.create(:valid_confidence_level)
-    c = FactoryBot.create(:valid_confidence, confidence_level: l, confidence_object: o1)
-    q.confidence_level_id = l.id
-    expect(q.all).to contain_exactly(o1)
-  end
-
-  specify '#without_confidence_level_id' do
-    l = FactoryBot.create(:valid_confidence_level)
-    c = FactoryBot.create(:valid_confidence, confidence_level: l, confidence_object: o1)
-    q.without_confidence_level_id = l.id
-    expect(q.all).to contain_exactly(o2)
-  end
-
-  specify '#with_confidence_level true' do
-    c = FactoryBot.create(:valid_confidence, confidence_object: o1)
-    q.confidences = true
-    expect(q.all).to contain_exactly(o1)
-  end
-
-  specify '#with_confidence_level false' do
-    c = FactoryBot.create(:valid_confidence, confidence_object: o1)
-    q.confidences = false
-    expect(q.all).to contain_exactly(o2)
-  end
-
-  specify '#exclude_confidences' do
-    l = FactoryBot.create(:valid_confidence_level)
-    c = FactoryBot.create(:valid_confidence, confidence_level: l, confidence_object: o1)
-    q.confidence_level_id = l.id
-    q.exclude_confidences = true
-    expect(q.all).to contain_exactly(o2)
-  end
-
-  # These require De Morgan's law to combine correctly:
-  #   NOT (has_l1 AND NOT has_l2) == (NOT has_l1) OR has_l2
-  # A naive implementation that negates the With and Without clauses
-  # independently (and ANDs them, as merge_clauses otherwise would) computes
-  # (NOT has_l1) AND has_l2 instead - which disagrees with the correct
-  # result exactly on objects that have both, or neither, confidence level.
-  context '#exclude_confidences with both confidence_level_id and without_confidence_level_id set' do
-    let!(:l1) { FactoryBot.create(:valid_confidence_level) }
-    let!(:l2) { FactoryBot.create(:valid_confidence_level) }
-
-    let!(:o3) { Otu.create!(name: 'Ghi 3') }
-    let!(:o4) { Otu.create!(name: 'Jkl 4') }
-
-    before do
-      FactoryBot.create(:valid_confidence, confidence_level: l1, confidence_object: o1) # l1 only
-      FactoryBot.create(:valid_confidence, confidence_level: l2, confidence_object: o2) # l2 only
-      FactoryBot.create(:valid_confidence, confidence_level: l1, confidence_object: o3) # both
-      FactoryBot.create(:valid_confidence, confidence_level: l2, confidence_object: o3) # both
-      # o4 has neither
-
-      q.confidence_level_id = l1.id
-      q.without_confidence_level_id = l2.id
-    end
-
-    specify 'combined With/Without, not excluded' do
-      # sanity check of the base (non-negated) combination: has l1 AND NOT l2
-      expect(q.all).to contain_exactly(o1)
-    end
-
-    specify 'combined With/Without, excluded' do
-      q.exclude_confidences = true
-      # correct complement: NOT has_l1 OR has_l2 - includes o3 (has both) and
-      # o4 (has neither), which a per-clause negation would wrongly drop/add
-      expect(q.all).to contain_exactly(o2, o3, o4)
-    end
-  end
+  # Confidences query concern specs have moved to spec/lib/queries/concerns/confidences_spec.rb
 
   context '#coordinatify' do
     # TODO: unify with OTU as a include
--- spec/lib/queries/source/filter_spec.rb
+++ spec/lib/queries/source/filter_spec.rb
@@ -113,19 +113,6 @@ describe Queries::Source::Filter, type: :model, group: [:sources, :filter] do
     expect(query.all.map(&:id)).to contain_exactly(*all_source_ids)
   end
 
-  specify '#with_tag 1' do
-    Tag.create!(keyword: FactoryBot.create(:valid_keyword), tag_object: s1)
-    query.tags = true
-    expect(query.all.map(&:id)).to contain_exactly(s1.id)
-  end
-
-  specify '#with_tag 1 (distinct)' do
-    Tag.create!(keyword: FactoryBot.create(:valid_keyword), tag_object: s1)
-    Tag.create!(keyword: FactoryBot.create(:valid_keyword), tag_object: s1)
-    query.tags = true
-    expect(query.all.map(&:id)).to contain_exactly(s1.id)
-  end
-
   specify '#citation_object_type 1' do
     Citation.create!(source: s1, citation_object: FactoryBot.create(:root_taxon_name))
     query.citation_object_type = ['TaxonName']
@@ -355,113 +342,6 @@ describe Queries::Source::Filter, type: :model, group: [:sources, :filter] do
     end
   end
 
-  # 
-  # Specs covering lib/queries/concerns/tags.rb
-  #
-
-  context 'keyword_id' do
-    specify '#keyword_id_and' do
-      t1 = Tag.create!(tag_object: s1, keyword: FactoryBot.create(:valid_keyword))
-      t2 = Tag.create!(tag_object: s1, keyword: FactoryBot.create(:valid_keyword))
-
-      query.keyword_id_and = [t1.keyword_id, t2.keyword_id]
-      expect(query.matching_keyword_id_and.map(&:id)).to contain_exactly(s1.id)
-    end
-
-    specify '#keyword_id_or' do
-      t1 = Tag.create!(tag_object: s1, keyword: FactoryBot.create(:valid_keyword))
-      t2 = Tag.create!(tag_object: s2, keyword: FactoryBot.create(:valid_keyword))
-
-      query.keyword_id_or = [t1.keyword_id, t2.keyword_id]
-      expect(query.matching_keyword_id_or.map(&:id)).to contain_exactly(s1.id, s2.id)
-    end
-
-    specify '#keyword_id_or, #keyword_id_and 1' do
-      t1 = Tag.create!(tag_object: s1, keyword: FactoryBot.create(:valid_keyword))
-      t2 = Tag.create!(tag_object: s1, keyword: FactoryBot.create(:valid_keyword))
-
-      t3 = Tag.create!(tag_object: s2, keyword: FactoryBot.create(:valid_keyword))
-
-      # not this
-      t4 = Tag.create!(tag_object: s3, keyword: t1.keyword)
-
-      query.keyword_id_and = [t1.keyword_id, t2.keyword_id]
-      query.keyword_id_or = [t3.keyword_id]
-
-      expect(query.all.map(&:id)).to contain_exactly(s1.id, s2.id)
-    end
-
-    specify '#keyword_id_or, #keyword_id_and 2 (one AND is an OR)' do
-      t1 = Tag.create!(tag_object: s1, keyword: FactoryBot.create(:valid_keyword))
-      t2 = Tag.create!(tag_object: s2, keyword: FactoryBot.create(:valid_keyword))
-
-      # not this
-      t3 = Tag.create!(tag_object: s3, keyword: FactoryBot.create(:valid_keyword))
-
-      query.keyword_id_and = [t1.keyword_id]
-      query.keyword_id_or = [t2.keyword_id]
-
-      expect(query.all.map(&:id)).to contain_exactly(s1.id, s2.id)
-    end
-
-    specify '#exclude_tags' do
-      t1 = Tag.create!(tag_object: s1, keyword: FactoryBot.create(:valid_keyword))
-
-      query.keyword_id_or = [t1.keyword_id]
-      query.exclude_tags = true
-
-      expect(query.all.map(&:id)).to contain_exactly(*(all_source_ids - [s1.id]))
-    end
-
-    # This requires De Morgan's law to combine correctly:
-    #   NOT (or_match OR and_match) == (NOT or_match) AND (NOT and_match)
-    # keyword_id_facet already unions or_match and and_match into one result
-    # before negate_facet wraps it, which is the correct order. A broken
-    # implementation that instead negated the OR-list and AND-list matches
-    # independently and then *unioned* those negated pieces together (rather
-    # than intersecting, or rather than negating the pre-unioned whole) would
-    # compute (NOT or_match) OR (NOT and_match) instead - which disagrees
-    # with the correct result exactly on objects that satisfy only one side.
-    context '#exclude_tags with both keyword_id_or and keyword_id_and set' do
-      let!(:k1) { FactoryBot.create(:valid_keyword) }
-      let!(:k2) { FactoryBot.create(:valid_keyword) }
-      let!(:k3) { FactoryBot.create(:valid_keyword) }
-
-      let!(:s_or_only) { FactoryBot.create(:valid_source) }
-      let!(:s_and_only) { FactoryBot.create(:valid_source) }
-      let!(:s_both) { FactoryBot.create(:valid_source) }
-      let!(:s_neither) { FactoryBot.create(:valid_source) }
-
-      before do
-        Tag.create!(tag_object: s_or_only, keyword: k1) # matches OR-list only
-
-        Tag.create!(tag_object: s_and_only, keyword: k2) # matches AND-list only
-        Tag.create!(tag_object: s_and_only, keyword: k3)
-
-        Tag.create!(tag_object: s_both, keyword: k1) # matches both
-        Tag.create!(tag_object: s_both, keyword: k2)
-        Tag.create!(tag_object: s_both, keyword: k3)
-        # s_neither gets no tags
-
-        query.keyword_id_or = [k1.id]
-        query.keyword_id_and = [k2.id, k3.id]
-      end
-
-      specify 'combined OR/AND, not excluded' do
-        # sanity check of the base (non-negated) combination: or_match OR and_match
-        expect(query.all.map(&:id)).to contain_exactly(s_or_only.id, s_and_only.id, s_both.id)
-      end
-
-      specify 'combined OR/AND, excluded' do
-        query.exclude_tags = true
-
-        # correct complement: NOT or_match AND NOT and_match - drops s_or_only
-        # and s_and_only (each satisfies exactly one side), which a broken
-        # union-of-negated-pieces implementation would wrongly keep
-        expect(query.all.map(&:id)).to contain_exactly(*(all_source_ids + [s_neither.id]))
-      end
-    end
-
-  end
+  # Tags query concern specs have moved to spec/lib/queries/concerns/tags_spec.rb
 
 end
