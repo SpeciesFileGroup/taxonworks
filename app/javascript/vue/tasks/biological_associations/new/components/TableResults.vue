cb5d3021d95b27475f94de75237b08f334faef8f
jlpereira <pepelui@outlook.com>
2026-07-29 21:10:26 -0300

    Add citations count to New BAs tasks

--- app/javascript/vue/tasks/biological_associations/new/components/TableResults.vue
+++ app/javascript/vue/tasks/biological_associations/new/components/TableResults.vue
@@ -6,7 +6,7 @@
           <th>Subject</th>
           <th>Relationship</th>
           <th>Object</th>
-          <th>Citation</th>
+          <th>Citations</th>
           <th class="w-2" />
         </tr>
       </thead>
@@ -18,12 +18,29 @@
           <td v-html="item.subject.object_tag" />
           <td v-html="item.relationship.object_tag" />
           <td v-html="item.object.object_tag" />
-          <td v-html="item.citation.citation_source_body" />
+          <td v-if="item.citations.length > 1">
+            <CitationsCount
+              :citations="item.citations"
+              @delete="(c) => store.removeBiologicalAssociationCitation(c)"
+            />
+          </td>
+          <td v-else-if="item.citations.length">
+            <a
+              target="blank"
+              :href="nomenclatureBySourceRoute(item.citations[0].source_id)"
+              v-html="item.citations[0].citation_source_body"
+            />
+          </td>
+          <td v-else />
           <td>
             <div class="flex-row gap-small">
               <RadialAnnotator :global-id="item.global_id" />
               <RadialObject :global-id="item.global_id" />
-              <RadialNavigator :global-id="item.global_id" />
+              <RadialNavigator
+                :global-id="item.global_id"
+                :redirect="false"
+                @delete="(item) => emit('remove', item)"
+              />
               <VBtn
                 circle
                 color="primary"
@@ -48,6 +65,9 @@ import VIcon from '@/components/ui/VIcon/index.vue'
 import RadialAnnotator from '@/components/radials/annotator/annotator.vue'
 import RadialObject from '@/components/radials/object/radial.vue'
 import RadialNavigator from '@/components/radials/navigation/radial.vue'
+import CitationsCount from '@/components/citations/CitationsCount.vue'
+import { RouteNames } from '@/routes/routes'
+import { useStore } from '../store/store.js'
 
 defineProps({
   list: {
@@ -56,5 +76,11 @@ defineProps({
   }
 })
 
-const emit = defineEmits(['select'])
+const emit = defineEmits(['select', 'remove'])
+
+const store = useStore()
+
+function nomenclatureBySourceRoute(sourceId) {
+  return `${RouteNames.NomenclatureBySource}?source_id=${sourceId}`
+}
 </script>
