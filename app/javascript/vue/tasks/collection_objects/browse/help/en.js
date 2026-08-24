3548a90d61b2d4106df92cae5cd7dd0c989c6562
Tom Klein <twointofive@gmail.com>
2026-08-24 09:53:05 -0500

    Fix typo in Browse CO help text

--- app/javascript/vue/tasks/collection_objects/browse/help/en.js
+++ app/javascript/vue/tasks/collection_objects/browse/help/en.js
@@ -1,7 +1,7 @@
 export default {
     nav: {
       previous: 'Navigates to previous available collection object',
-      go: 'Navigate to anavailable object by a specified parameter',
+      go: 'Navigate to an available object by a specified parameter',
       next: 'Navigate to the next available collection object',
 
     }
