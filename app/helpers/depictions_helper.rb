2a4e3a96e426c56498aeb6781c256c8598f570a0
Tom Klein <twointofive@gmail.com>
2026-09-21 22:12:01 -0500

    Close trad. key serving unattributed images via api #5094

--- app/helpers/depictions_helper.rb
+++ app/helpers/depictions_helper.rb
@@ -57,15 +57,25 @@ module DepictionsHelper
   # with the context being the depictied object.
   def depiction_to_json(depiction)
     return nil if depiction.nil?
+
+    image = depiction.image
+
     a = {
       caption: depiction.caption,
       figure_label: depiction.figure_label,
       position: depiction.position,
-      thumb: short_url(depiction.image.image_file.url(:thumb)),
-      medium: short_url(depiction.image.image_file.url(:medium)),
-      content_type: depiction.image.image_file_content_type,
-      original_png: original_as_scaled_png_via_api(depiction.image)
+      content_type: image.image_file_content_type,
+      attributed: image.attributed?
     }
+
+    if image.attributed?
+      a[:thumb] = short_url(image.image_file.url(:thumb))
+      a[:medium] = short_url(image.image_file.url(:medium))
+      a[:original_png] = original_as_scaled_png_via_api(image)
+    else
+      a[:message] = 'Image is not accessible via the API because it lacks attribution. See https://api.taxonworks.org/ for more.'
+    end
+
     a
   end
 
---
+++ spec/helpers/depictions_helper_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe DepictionsHelper, type: :helper do
+  specify '#depiction_to_json with attribution includes image links' do
+    depiction = FactoryBot.create(:valid_depiction)
+    FactoryBot.create(:valid_attribution, attribution_object: depiction.image)
+
+    json = helper.depiction_to_json(depiction)
+
+    expect(json[:attributed]).to be true
+    expect(json[:thumb]).to be_present
+    expect(json[:medium]).to be_present
+    expect(json[:original_png]).to be_present
+    expect(json[:message]).to be_nil
+  end
+
+  specify '#depiction_to_json without attribution redacts image links' do
+    depiction = FactoryBot.create(:valid_depiction)
+
+    json = helper.depiction_to_json(depiction)
+
+    expect(json[:attributed]).to be false
+    expect(json[:thumb]).to be_nil
+    expect(json[:medium]).to be_nil
+    expect(json[:original_png]).to be_nil
+    expect(json[:message]).to include('lacks attribution')
+  end
+end
